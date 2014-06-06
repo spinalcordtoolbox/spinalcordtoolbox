e6b1dcbac96a0517e9697b746ae9b3fa224a011e
neuropoly <neuropoly@spinalcordtoolbox>
2014-06-06 03:54:29 -0400

    OPT: package creation now work for osx and debian
    Former-commit-id: 84f3cfb4905453a338825fb8e68cf872a8123b2f [formerly 6264a1b4d8bd7a77d49c224371dcfe0f82bd5ba5]
    Former-commit-id: 98dd7c781389981d605b575c2c182dc1a0784e74
    Former-commit-id: 820474877139270bb73927e90400eb5fb233d72a

--- create_package.py
+++ create_package.py
@@ -6,10 +6,51 @@
 #
 
 import os
+import getopt
 import sys
 from numpy import loadtxt
 sys.path.append('./scripts')
 import sct_utils as sct
+
+#=======================================================================================================================
+# usage
+#=======================================================================================================================
+def usage():
+    print 'USAGE: \n' \
+        ''+os.path.basename(__file__)+'\n' \
+        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
+        'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n' \
+        '\n'\
+        'DESCRIPTION\n' \
+        '  Create a package of the Spinal Cord Toolbox.\n' \
+        '\n' \
+        'USAGE\n' \
+        '  '+os.path.basename(__file__)+'\n' \
+        '\n' \
+        'MANDATORY ARGUMENTS\n' \
+        '  -s <OS name>      name of the OS {osx,debian}. Default=osx.\n' \
+        '\n'\
+        'EXAMPLE:\n' \
+        '  create_package.py -s debian\n'
+    sys.exit(2)
+
+listOS = ['osx','debian']
+OSname = listOS[0]
+# Check input param
+try:
+    opts, args = getopt.getopt(sys.argv[1:],'hs:')
+except getopt.GetoptError as err:
+    print str(err)
+    usage()
+for opt, arg in opts:
+    if opt == '-h':
+        usage()
+    elif opt in ('-s'):
+        OSname = str(arg)
+
+if OSname not in listOS:
+    print 'ERROR: OS name should be one of the following: '+'[%s]' % ', '.join(map(str,listOS))
+    usage()
 
 # get version
 version = str(loadtxt("version.txt", comments="#", delimiter=",", unpack=False))
@@ -26,10 +67,15 @@ sct.run('cp installer.sh '+folder_sct)
 sct.run('cp README.md '+folder_sct+'spinalcordtoolbox/')
 sct.run('cp LICENSE '+folder_sct+'spinalcordtoolbox/')
 sct.run('cp version.txt '+folder_sct+'spinalcordtoolbox/')
-sct.run('cp -r bin '+folder_sct+'spinalcordtoolbox/')
 sct.run('cp -r flirtsch '+folder_sct+'spinalcordtoolbox/')
-sct.run('cp -r lib '+folder_sct+'spinalcordtoolbox/')
 sct.run('cp -r scripts '+folder_sct+'spinalcordtoolbox/')
+sct.run('mkdir '+folder_sct+'spinalcordtoolbox/bin')
+if OSname == 'osx':
+    sct.run('cp -r bin/OSX_10.6-7-8 '+folder_sct+'spinalcordtoolbox/')
+    sct.run('cp -r lib '+folder_sct+'spinalcordtoolbox/')
+elif OSname == 'debian':
+    sct.run('cp -r bin/Debian_7.5 '+folder_sct+'spinalcordtoolbox/bin/')
+    sct.run('mkdir '+folder_sct+'spinalcordtoolbox/lib') # not required but we still create lib folder
 
 # copy colormap
 sct.run('mkdir '+folder_sct+'spinalcordtoolbox/data/')
@@ -91,8 +137,9 @@ sct.run('cp -r testing/data/errsm_23 '+folder_sct+'spinalcordtoolbox/testing/dat
 # remove .DS_Store files
 sct.run('find . -type f -name .DS_Store -delete')
 
-# remove AppleDouble files
-sct.run('find '+folder_sct+' -type d | xargs dot_clean -m')
+# remove AppleDouble files - doesn't work on Linux
+if OSname == 'osx':
+    sct.run('find '+folder_sct+' -type d | xargs dot_clean -m')
 
 # remove pycharm files
 sct.run('rm '+folder_sct+'spinalcordtoolbox/scripts/*.pyc')
