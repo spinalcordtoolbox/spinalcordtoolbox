6959c11d5e3d3cbd8437f2102e17204cde3b7e2e
neuropoly <neuropoly@spinalcordtoolbox>
2014-06-09 10:16:30 -0400

    INST: fixed code redondancy

--- install/installer.sh
+++ install/installer.sh
@@ -64,12 +64,11 @@ else
   echo '# SPINALCORDTOOLBOX' >> ~/.bash_profile
   echo "SCT_DIR=\"${SCT_DIR}\"" >> ~/.bash_profile
   echo 'export PATH=${PATH}:$SCT_DIR/scripts' >> ~/.bash_profile
+  echo 'export PATH=${PATH}:$SCT_DIR/bin' >> ~/.bash_profile
   unamestr=`uname`
   if [[ "$unamestr" == 'Linux' ]]; then
-    echo 'export PATH=${PATH}:$SCT_DIR/bin' >> ~/.bash_profile
     echo 'export LD_LIBRARY_PATH=${SCT_DIR}/lib:$LD_LIBRARY_PATH' >> ~/.bash_profile
   else
-    echo 'export PATH=${PATH}:$SCT_DIR/bin' >> ~/.bash_profile
     echo 'export DYLD_LIBRARY_PATH=${SCT_DIR}/lib:$DYLD_LIBRARY_PATH' >> ~/.bash_profile
   fi
   echo 'export SCT_DIR PATH' >> ~/.bash_profile
