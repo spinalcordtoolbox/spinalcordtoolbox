a7e18d13d4453cd2529e901e0c2891e647d5366a
Julien Cohen-Adad <Julien>
2014-06-14 14:03:10 +0200

    TEST: updated bash for testing
    Former-commit-id: b213199c2d323e93b57418ed3f370a70b6f206e3
    Former-commit-id: 786e6b99f48f51ca3cee4fbb9e571e19c1d40567

--- testing/sct_dmri_moco/test_sct_dmri_moco.sh
+++ testing/sct_dmri_moco/test_sct_dmri_moco.sh
@@ -34,9 +34,9 @@ for subject in $SUBJECT_LIST; do
     printf "${green}Subject: $subject${NC}\n"
     printf "${red}Contrast: ${contrast}${NC}\n\n"
     cmd="sct_dmri_moco.py
-        -i ../../data/${subject}/${contrast}/${file1}
-        -b ../../data/${subject}/${contrast}/${file2}
-        -s sinc"
+        -i ${SCT_DIR}/testing/data/${subject}/${contrast}/${file1}
+        -b ${SCT_DIR}/testing/data/${subject}/${contrast}/${file2}
+        -p sinc"
     echo ==============================================================================================
     echo "$cmd"
     echo ==============================================================================================
