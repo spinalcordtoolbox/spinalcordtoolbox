3daab409abf3546d9f9315b37156b55f5564b6e0
Benjamin De Leener <benjamindeleener@evans.neuro.polymtl.ca>
2014-06-14 04:09:52 -0400

    NEW: binaries now statically compiled (no need for ITK/VTK libraries
    Former-commit-id: dae5455278d3a19f0a77f2e1e76deaa092d2a466 [formerly ac0c53c1e9afd714e9ac74b09a5c1f967491d714]
    Former-commit-id: f00dd6f1399167cd01cb93130179ef2319cc0ee0
    Former-commit-id: 81b27a48fef901b4c5759b9dc9b627f8e04db806

---
+++ install/make_binaries.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Build and install the binaries of the spinal cord toolbox
+#
+# This script will compile (using CMake) the binaries of the spinal cord toolbox for the OS it is called.
+# We assume here that this script is always in "install" folder of the toolbox
+
+SCT_DIR_LOCAL=${PWD%/*}
+echo ${SCT_DIR_LOCAL}
+
+# program list
+PROGRAM_LIST="sct_change_nifti_pixel_type sct_crop_image sct_detect_spinalcord sct_dice_coefficient sct_hausdorff_distance sct_modif_header sct_orientation sct_segmentation_propagation"
+
+
+# loop across programs
+for program in $PROGRAM_LIST; do
+  
+  echo
+  cmd="cd ${SCT_DIR_LOCAL}/dev/${program}/"
+  echo ">> $cmd"; $cmd
+
+  echo
+  cmd="cmake ."
+  echo ">> $cmd"; $cmd
+
+  echo
+  cmd="make"
+  echo ">> $cmd"; $cmd
+
+  echo
+  cmd="cp ${program} ${SCT_DIR_LOCAL}/bin/osx/"
+  echo ">> $cmd"; $cmd
+
+  echo
+  cmd="make"
+  echo ">> $cmd"; $cmd
+
+  echo
+  cmd="make clean"
+  echo ">> $cmd"; $cmd
+
+done
