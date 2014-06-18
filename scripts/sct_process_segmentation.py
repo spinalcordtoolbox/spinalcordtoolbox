581aa0e5e797b4a78e0af314cc371bbff5f568a4
Julien Touati <jtouati@evans.neuro.polymtl.ca>
2014-06-18 18:23:10 -0400

    NEW : CSA computation now working, improvements to come
    Former-commit-id: d3c3c1e468b72682a439f1ec99fecf74c77c92d0 [formerly 27ad3709634eddcfc8b9e4d813656f740c850027] [formerly e19a4ab3bb01c2de1f3a36be4ba84274dbd011aa [formerly cd721402b44eca5fa71fc04b82d7550eca5b9889 [formerly bc69a5b2e6e4a7ccc221c0392ebf02f217c79f2f]]]
    Former-commit-id: d3c3c1e468b72682a439f1ec99fecf74c77c92d0 [formerly 27ad3709634eddcfc8b9e4d813656f740c850027]
    Former-commit-id: cefe543f5e0cca720a8789588b8460b0e5e4104a [formerly 7f0026615cfb1535438920113ccb27d389d780e6 [formerly 666359a77480167ec5c5cd4e69b9cb8d8394a50b]]
    Former-commit-id: 1777db26e6bbb46df6dd0c47da1c1c4c329666d7
    Former-commit-id: edd999b8c4073757f6b358cbc9ef316621a7faf7

--- scripts/sct_process_segmentation.py
+++ scripts/sct_process_segmentation.py
@@ -8,7 +8,7 @@
 #
 # ---------------------------------------------------------------------------------------
 # Copyright (c) 2013 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Author: Benjamin De Leener, Julien Touati
+# Author: Benjamin De Leener, Julien Touati, Gabriel Mangeat
 # Created: 2014-05-24
 #
 # About the license: see the file LICENSE.TXT
@@ -147,6 +147,7 @@ def extract_centerline(fname_segmentation):
 	
     # Extract min and max index in Z direction
     X, Y, Z = (data>0).nonzero()
+    print data
     min_z_index, max_z_index = min(Z), max(Z)
     x_centerline = [0 for i in range(0,max_z_index-min_z_index+1)]
     y_centerline = [0 for i in range(0,max_z_index-min_z_index+1)]
@@ -160,35 +161,35 @@ def extract_centerline(fname_segmentation):
 	    data[X[k],Y[k],Z[k]] = 0
     # Fit the centerline points with splines and return the new fitted coordinates
     x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
-    
-    
+
+
     # Create an image with the centerline
     for iz in range(min_z_index, max_z_index+1):
 	    data[round(x_centerline_fit[iz-min_z_index]),round(y_centerline_fit[iz-min_z_index]),iz] = 1
-	
+
     # Write the centerline image in RPI orientation
     hdr.set_data_dtype('uint8') # set imagetype to uint8
     print '\nWrite NIFTI volumes...'
     img = nibabel.Nifti1Image(data, None, hdr)
     nibabel.save(img, 'tmp.centerline.nii')
     sct.generate_output_file('tmp.centerline.nii','./',file_data+'_centerline',ext_data)
-	
+
     del data
-	
+
     # come back to parent folder
     os.chdir('..')
-	
+
     # Change orientation of the output centerline into input orientation
     print '\nOrient centerline image to input orientation: ' + orientation
     fname_segmentation_orient = 'tmp.segmentation_rpi' + ext_data
     sct.run('sct_orientation -i ' + path_tmp+'/'+file_data+'_centerline'+ext_data + ' -o ' + file_data+'_centerline'+ext_data + ' -orientation ' + orientation)
-	
-    
+
+
     # Remove temporary files
     print('\nRemove temporary files...')
     sct.run('rm -rf '+path_tmp)
-    
-    
+
+
     # to view results
     print '\nTo view results, type:'
     print 'fslview '+file_data+'_centerline &\n'
@@ -237,11 +238,20 @@ def compute_CSA(fname_segmentation):
     
     x_scale=hdr['pixdim'][1]
     y_scale=hdr['pixdim'][2]
+    z_scale=hdr['pixdim'][3]
     
     # Extract min and max index in Z direction
     X, Y, Z = (data>0).nonzero()
-    coords = [[X[i],Y[i],Z[i]] for i in range(0,len(Z))]
-    
+    coords = numpy.array([str([X[i],Y[i],Z[i]]) for i in range(0,len(Z))])
+    print type(coords[1])
+    print coords
+    #    pseudo_3D_coords= [[[0 for i in range(0,max(X)+1)] for j in range(0,max(Y)+1)] for k in range(0,max(Z)+1)]
+    #    print pseudo_3D_coords
+    #    for i in coords:
+    #        print i[0],i[1],i[2]
+    #        pseudo_3D_coords[i[0]][i[1]][i[2]] = 1
+    #
+    #    print pseudo_3D_coords
     min_z_index, max_z_index = min(Z), max(Z)
     x_centerline = [0 for i in range(0,max_z_index-min_z_index+1)]
     y_centerline = [0 for i in range(0,max_z_index-min_z_index+1)]
@@ -250,19 +260,27 @@ def compute_CSA(fname_segmentation):
     # Extract segmentation points and average per slice
     for iz in range(min_z_index, max_z_index+1):
         x_seg, y_seg = (data[:,:,iz]>0).nonzero()
+        #print x_seg,y_seg
         x_centerline[iz-min_z_index] = numpy.mean(x_seg)
         y_centerline[iz-min_z_index] = numpy.mean(y_seg)
-	
-	
+
+
     # Fit the centerline points with splines and return the new fitted coordinates
     x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
+
+    #    fig=plt.figure()
+    #    ax=Axes3D(fig)
+    #    ax.plot(x_centerline,y_centerline,z_centerline,zdir='z')
+    #    ax.plot(x_centerline_fit,y_centerline_fit,z_centerline,zdir='z')
+    ##ax.plot(X,Y,Z,zdir='z')
+    #    plt.show()
     
-#    fig=plt.figure()
-#    ax=Axes3D(fig)
-#    ax.plot(x_centerline,y_centerline,z_centerline,zdir='z')
-#    ax.plot(x_centerline_fit,y_centerline_fit,z_centerline,zdir='z')
-#    #ax.plot(X,Y,Z,zdir='z')
-#    plt.show()
+    step = min([x_scale,y_scale])/2
+    print step
+    x=numpy.array([1,0,0])
+    y=numpy.array([0,1,0])
+    z=numpy.array([0,0,1])
+    
     
     print('\nComputing CSA...')
     sections=[0 for i in range(0,max_z_index-min_z_index+1)]
@@ -270,17 +288,44 @@ def compute_CSA(fname_segmentation):
         a = x_centerline_deriv[iz]
         b = y_centerline_deriv[iz]
         c = z_centerline_deriv[iz]
-        x = x_centerline_fit[iz]
-        y = y_centerline_fit[iz]
-        z = z_centerline[iz]
-        d = -(a*x+b*y+c*z)
+        x_center = x_centerline_fit[iz]
+        y_center = y_centerline_fit[iz]
+        z_center = z_centerline[iz]
+        d = -(a*x_center+b*y_center+c*z_center)
+        
+        normal=normalize(numpy.array([a,b,c]))
+        
+        basis_1 = normalize(numpy.cross(normal,x)) # use of x in order to get orientation of each plane
+        basis_2 = normalize(numpy.cross(normal,basis_1))
+        angle = numpy.arccos(numpy.dot(normal,z))
+        max_diameter = (max([(max(X)-min(X))*x_scale,(max(Y)-min(Y))*y_scale])*numpy.sqrt(2))/(numpy.cos(angle)) # maximum dimension of the tilted plane
+        #plane = numpy.zeros((int((max_diameter)/step)),(int((max_diameter)/step)))
+        plane_discrete = numpy.linspace(-int(max_diameter/2),int(max_diameter/2),(max_diameter/step)+1)
         cpt=0
-        axe=numpy.matrix([a,b,c])
-        for i in range(0,len(X)):
-            if (round(a*coords[i][0]+b*coords[i][1]+c*coords[i][2]+d) == 0):
-                cpt=cpt+1
-        sections[iz]=cpt*x_scale*y_scale
-    
+        for i_b1 in plane_discrete :
+            for i_b2 in plane_discrete :
+                point = numpy.array([x_center*x_scale,y_center*y_scale,z_center*z_scale]) + i_b1*basis_1 +i_b2*basis_2
+                coord_voxel = str([ int(round(point[0]/x_scale)), int(round(point[1]/y_scale)), int(round(point[2]/z_scale))])
+                
+                if (coord_voxel in coords) is True :
+                    cpt = cpt+1
+        #                    test=0
+        #                    cpt2=0
+        #                    while (test==0) & (cpt2<len(coords)):                      ## find in list
+        #                        if (int(coord_voxel[0])==int(coords[cpt2][0]))&(int(coord_voxel[1])==int(coords[cpt2][1]))&(int(coord_voxel[2])==int(coords[cpt2][2])):
+        #                            cpt=cpt+1
+        #                            test=1
+        #                        cpt2=cpt2+1
+        #
+        #
+        
+        sections[iz]=cpt*step*step
+        print sections[iz]
+    #            for i in range(0,len(X)):
+    #                if (round(a*coords[i][0]+b*coords[i][1]+c*coords[i][2]+d) == 0):
+    #                    cpt=cpt+1
+    #            sections[iz]=cpt*x_scale*y_scale
+
     print sections
     
     # come back to parent folder
@@ -315,7 +360,10 @@ def b_spline_centerline(x_centerline,y_centerline,z_centerline):
     return x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv
 
 
-
+def normalize(vect):
+    """take an 1x3 matrix vector and return the normalised vector"""
+    norm=numpy.linalg.norm(vect)
+    return vect/norm
 
 # Print usage
 # ==========================================================================================
