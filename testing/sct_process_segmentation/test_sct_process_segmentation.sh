c20185c1c3c890cb0363bc6ce06bbf1316b5431c
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-07-03 17:39:24 -0400

    DOC: modify usage info and added todo
    Former-commit-id: 7de005da2aec1b46ec7a24e0b3ff1919b97174cf [formerly f9fa14b31a813968ec2c61b8f0e59034b7a7376c] [formerly 23f1e11034e5f63dce8c3e27ffea464f2f196a79 [formerly 761e9957cb31116c2599cc61ebdab7fef96cf42e [formerly e3d06cc6a6992c0ba2153f062386dab24ab51142]]]
    Former-commit-id: 7de005da2aec1b46ec7a24e0b3ff1919b97174cf [formerly f9fa14b31a813968ec2c61b8f0e59034b7a7376c]
    Former-commit-id: 976191c6ca556e74b2fc9fccf5840072a154c085 [formerly 72f0ddd27df85e081c3abf26971cbd9cb387ff57 [formerly a4df7221397654129e00e3fba54ca7b18d6b2486]]
    Former-commit-id: 201b1ef9702213d7693480e3735d353b63c4d930
    Former-commit-id: 63405b76a84c32ffc5b7bf83d2d6302e58b37a42

---
+++ testing/sct_process_segmentation/test_sct_process_segmentation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# register anatomical data (e.g. template) to multimodal data (e.g., MT, diffusion, fMRI, gradient echo). 
+
+
+# subject list
+SUBJECT_LIST="errsm_23"
+CONTRAST_LIST="t2"
+file="t2_segmentation_PropSeg.nii.gz"
+
+
+# START BATCH HERE
+# =================================
+
+# define nice colors
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+
+# if results folder exists, delete it
+if [ -e "results" ]; then
+	rm -rf results
+fi
+
+# create results folder and go inside it
+mkdir results
+cd results
+
+# loop across subjects
+for subject in $SUBJECT_LIST; do
+
+    # loop across contrast
+    for contrast in $CONTRAST_LIST; do
+
+        # display subject
+        echo
+        printf "${green}Subject: $subject${NC}\n"
+        printf "${red}Contrast: ${contrast}${NC}\n\n"
+
+	    cmd="sct_process_segmentation.py
+	        -i ${SCT_DIR}/testing/data/${subject}/${contrast}/${file}
+	        -p compute_CSA
+		-m counting_ortho_plane
+	        -b 1
+		-v 1"
+
+        echo ==============================================================================================
+        echo "$cmd"
+        echo ==============================================================================================
+        $cmd
+    done
+done
