a25e0610de7ae23099ad1aa5031d3745aa62a845
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-07-05 16:39:24 -0400

    BUG: issue #30. Now creates a tmp. folder and do everything inside.
    Former-commit-id: e175affa13fbddaeb599ce21c7c8407a38fbcfde
    Former-commit-id: 4eb8296c12294db9d48a1144496a7e07da732984

--- scripts/sct_smooth_spinalcord.py
+++ scripts/sct_smooth_spinalcord.py
@@ -66,7 +66,7 @@ def main():
     # Initialization
     fname_anat = ''
     fname_centerline = ''
-    sigma = 4 # default value of the standard deviation for the Gaussian smoothing (in terms of number of voxels)
+    sigma = 3 # default value of the standard deviation for the Gaussian smoothing (in terms of number of voxels)
     remove_temp_files = 1
     start_time = time.time()
 
@@ -89,7 +89,6 @@ def main():
         elif opt in ('-s'):
             sigma = arg
 
-
     # Display usage if a mandatory argument is not provided
     if fname_anat == '' or fname_centerline == '':
         usage()
@@ -98,26 +97,34 @@ def main():
     sct.check_file_exist(fname_anat)
     sct.check_file_exist(fname_centerline)
 
-    # Extract path/file/extension of the original image file
-    path_anat, file_anat, ext_anat = sct.extract_fname(fname_anat)
-    # Extract path/file/extension of the centerline
-    path_centerline, file_centerline, ext_centerline = sct.extract_fname(fname_centerline)
-
     # Display arguments
     print '\nCheck input arguments...'
     print '.. Anatomical image:           ' + fname_anat
     print '.. Centerline:                 ' + fname_centerline
 
+    # Extract path/file/extension
+    path_anat, file_anat, ext_anat = sct.extract_fname(fname_anat)
+    path_centerline, file_centerline, ext_centerline = sct.extract_fname(fname_centerline)
+
+    # create temporary folder
+    path_tmp = 'tmp.'+time.strftime("%y%m%d%H%M%S")
+    sct.run('mkdir '+path_tmp)
+
+    # copy files into tmp folder
+    sct.run('cp '+fname_anat+' '+path_tmp)
+    sct.run('cp '+fname_centerline+' '+path_tmp)
+
+    # go to tmp folder
+    os.chdir(path_tmp)
 
     # Change orientation of the input image into RPI
     print '\nOrient input volume to RPI orientation...'
     fname_anat_orient = path_anat+ file_anat+'_rpi'+ ext_anat
-    sct.run('sct_orientation -i ' + fname_anat + ' -o ' + fname_anat_orient + ' -orientation RPI')
+    sct.run('sct_orientation -i ' + file_anat + ext_anat + ' -o ' + fname_anat_orient + ' -orientation RPI')
     # Change orientation of the input image into RPI
     print '\nOrient centerline to RPI orientation...'
     fname_centerline_orient = path_centerline+file_centerline+'_rpi'+ ext_centerline
-    sct.run('sct_orientation -i ' + fname_centerline + ' -o ' + fname_centerline_orient + ' -orientation RPI')
-
+    sct.run('sct_orientation -i ' + file_centerline + ext_centerline + ' -o ' + fname_centerline_orient + ' -orientation RPI')
 
     # Straighten the spinal cord
     print '\nStraighten the spinal cord...'
@@ -129,30 +136,34 @@ def main():
     fname_smoothing_output = file_anat+'_z_smoothed'+ext_anat
     sct.run('c3d '+fname_straightening_output+' -smooth 0x0x'+str(sigma)+'vox -o '+fname_smoothing_output)
 
-
     # Apply the reversed warping field to get back the curved spinal cord
     print '\nApply the reversed warping field to get back the curved spinal cord (assuming a 3D image)...'
     fname_destraightened_output = file_anat+'_destraight_smoothed'+ext_anat
     sct.run('WarpImageMultiTransform 3 '+fname_smoothing_output+' '+fname_destraightened_output+' -R '+fname_anat+
             ' --use-BSpline warp_straight2curve.nii.gz')
 
+    # come back to parent folder
+    os.chdir('..')
 
-    # Delete temporary files
+    # Generate output file
+    print '\nGenerate output file...'
+    sct.generate_output_file(path_tmp+'/'+fname_destraightened_output,'',file_anat+'_smooth',ext_anat)
+
+    # Remove temporary files
     if remove_temp_files == 1:
-        print '\nDelete temporary files...'
-        sct.run('rm '+fname_straightening_output)
-        sct.run('rm '+fname_smoothing_output)
-        sct.run('rm warp_straight2curve.nii.gz')
-        sct.run('rm warp_curve2straight.nii.gz')
-        sct.run('rm '+fname_centerline_orient)
-        sct.run('rm '+fname_anat_orient)
-
+        print('\nRemove temporary files...')
+        sct.run('rm -rf '+path_tmp)
 
     #Display elapsed time
     elapsed_time = time.time() - start_time
-    print '\nFinished!'
-    print '.. '+str(int(round(elapsed_time)))+'s\n'
+    print '\nFinished! Elapsed time: '+str(int(round(elapsed_time)))+'s\n'
 
+    # to view results
+    print 'To view results, type:'
+    print 'fslview '+file_anat+'_smooth &\n'
+
+    # End of Main
+    
 
 #=======================================================================================================================
 # usage
@@ -160,12 +171,13 @@ def main():
 def usage():
     print '\n' \
         ''+os.path.basename(__file__)+'\n' \
-        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
+        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
         'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n' \
         '\n'\
         'DESCRIPTION\n' \
-        '  Smooth the spinal cord along its centerline. First, the cord is straightened (using a centerline), then a\n' \
-        '  gaussian kernel is applied in the superior-inferior dimension, then cord is de-straightened as originally.\n' \
+        '  Smooth the spinal cord along its centerline. Steps are: 1) Spinal cord is straightened (using\n' \
+        '  centerline), 2) a Gaussian kernel is applied in the superior-inferior direction, 3) then cord is\n' \
+        '  de-straightened as originally.\n' \
         '\n' \
         'USAGE\n' \
         '  sct_smooth_spinalcord.py -i <input_image> -c <centerline/segmentation>\n' \
@@ -176,8 +188,8 @@ def usage():
         '\n' \
         'OPTIONAL ARGUMENTS\n' \
         '  -h                help. Show this message.\n' \
-        '  -r {0, 1}         remove temporary files. Default=1\n' \
-        '  -s                sigma of the smoothing Gaussian kernel (expressed in voxels). Default=4.' \
+        '  -r {0,1}          remove temporary files. Default=1\n' \
+        '  -s                sigma of the smoothing Gaussian kernel (expressed in voxels). Default=3.' \
         '\n'
 
     sys.exit(2)
