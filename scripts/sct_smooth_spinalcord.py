28cbe149b44449e4bdd27c344005845342cb6bab
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-07-06 19:05:20 -0400

    REF: small changes
    Former-commit-id: 1b53be1c0309e691b63da7a9f727557337550c39
    Former-commit-id: 8db108197a6cffb466f4a07a0e9c59473f570ba9

--- scripts/sct_smooth_spinalcord.py
+++ scripts/sct_smooth_spinalcord.py
@@ -50,12 +50,21 @@
 #=======================================================================================================================
 
 
+# TODO: maybe no need to convert RPI at the beginning because strainghten spinal cord already does it!
+
+
 import getopt
 import os
 import sys
 import time
 import sct_utils as sct
 
+
+class param:
+    ## The constructor
+    def __init__(self):
+        self.remove_temp_files = 1 # remove temporary files
+        self.verbose = 1
 
 
 #=======================================================================================================================
@@ -67,13 +76,14 @@ def main():
     fname_anat = ''
     fname_centerline = ''
     sigma = 3 # default value of the standard deviation for the Gaussian smoothing (in terms of number of voxels)
-    remove_temp_files = 1
+    remove_temp_files = param.remove_temp_files
+    verbose = param.verbose
     start_time = time.time()
 
 
     # Check input param
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hi:c:r:s:')
+        opts, args = getopt.getopt(sys.argv[1:], 'hi:c:r:s:v:')
     except getopt.GetoptError as err:
         print str(err)
         usage()
@@ -88,66 +98,66 @@ def main():
             remove_temp_files = arg
         elif opt in ('-s'):
             sigma = arg
+        elif opt in ('-v'):
+            verbose = int(arg)
 
     # Display usage if a mandatory argument is not provided
     if fname_anat == '' or fname_centerline == '':
         usage()
 
-    # Check existence of input files
-    sct.check_file_exist(fname_anat)
-    sct.check_file_exist(fname_centerline)
-
     # Display arguments
     print '\nCheck input arguments...'
-    print '.. Anatomical image:           ' + fname_anat
-    print '.. Centerline:                 ' + fname_centerline
+    print '  Volume to smooth .................. ' + fname_anat
+    print '  Centerline ........................ ' + fname_centerline
+    print '  Verbose ........................... '+str(verbose)
+
+    # Check existence of input files
+    print('\nCheck existence of input files...')
+    sct.check_file_exist(fname_anat)
+    sct.check_file_exist(fname_centerline)
 
     # Extract path/file/extension
     path_anat, file_anat, ext_anat = sct.extract_fname(fname_anat)
     path_centerline, file_centerline, ext_centerline = sct.extract_fname(fname_centerline)
 
     # create temporary folder
+    print('\nCreate temporary folder...')
     path_tmp = 'tmp.'+time.strftime("%y%m%d%H%M%S")
     sct.run('mkdir '+path_tmp)
 
-    # copy files into tmp folder
-    sct.run('cp '+fname_anat+' '+path_tmp)
-    sct.run('cp '+fname_centerline+' '+path_tmp)
+    # copy files to temporary folder
+    print('\nCopy files...')
+    sct.run('c3d '+fname_anat+' -o '+path_tmp+'/anat.nii')
+    sct.run('c3d '+fname_centerline+' -o '+path_tmp+'/centerline.nii')
 
     # go to tmp folder
     os.chdir(path_tmp)
 
     # Change orientation of the input image into RPI
     print '\nOrient input volume to RPI orientation...'
-    fname_anat_orient = path_anat+ file_anat+'_rpi'+ ext_anat
-    sct.run('sct_orientation -i ' + file_anat + ext_anat + ' -o ' + fname_anat_orient + ' -orientation RPI')
+    sct.run('sct_orientation -i anat.nii -o anat_rpi.nii -orientation RPI')
     # Change orientation of the input image into RPI
     print '\nOrient centerline to RPI orientation...'
-    fname_centerline_orient = path_centerline+file_centerline+'_rpi'+ ext_centerline
-    sct.run('sct_orientation -i ' + file_centerline + ext_centerline + ' -o ' + fname_centerline_orient + ' -orientation RPI')
+    sct.run('sct_orientation -i centerline.nii -o centerline_rpi.nii -orientation RPI')
 
     # Straighten the spinal cord
     print '\nStraighten the spinal cord...'
-    sct.run('sct_straighten_spinalcord.py -i '+fname_anat_orient+' -c '+fname_centerline_orient+' -w --use-BSpline')
-    fname_straightening_output = file_anat+'_rpi_straight'+ext_anat
+    sct.run('sct_straighten_spinalcord.py -i anat_rpi.nii -c centerline_rpi.nii -w spline -v '+str(verbose))
 
     # Smooth the straightened image along z
     print '\nSmooth the straightened image along z...'
-    fname_smoothing_output = file_anat+'_z_smoothed'+ext_anat
-    sct.run('c3d '+fname_straightening_output+' -smooth 0x0x'+str(sigma)+'vox -o '+fname_smoothing_output)
+    sct.run('c3d anat_rpi_straight.nii -smooth 0x0x'+str(sigma)+'vox -o anat_rpi_straight_smooth.nii')
 
     # Apply the reversed warping field to get back the curved spinal cord
     print '\nApply the reversed warping field to get back the curved spinal cord (assuming a 3D image)...'
-    fname_destraightened_output = file_anat+'_destraight_smoothed'+ext_anat
-    sct.run('WarpImageMultiTransform 3 '+fname_smoothing_output+' '+fname_destraightened_output+' -R '+fname_anat+
-            ' --use-BSpline warp_straight2curve.nii.gz')
+    sct.run('WarpImageMultiTransform 3 anat_rpi_straight_smooth.nii anat_rpi_straight_smooth_curved.nii -R anat.nii --use-BSpline warp_straight2curve.nii.gz')
 
     # come back to parent folder
     os.chdir('..')
 
     # Generate output file
     print '\nGenerate output file...'
-    sct.generate_output_file(path_tmp+'/'+fname_destraightened_output,'',file_anat+'_smooth',ext_anat)
+    sct.generate_output_file(path_tmp+'/anat_rpi_straight_smooth_curved.nii','',file_anat+'_smooth',ext_anat)
 
     # Remove temporary files
     if remove_temp_files == 1:
@@ -160,7 +170,7 @@ def main():
 
     # to view results
     print 'To view results, type:'
-    print 'fslview '+file_anat+'_smooth &\n'
+    print 'fslview '+file_anat+' '+file_anat+'_smooth &\n'
 
     # End of Main
     
@@ -180,16 +190,17 @@ def usage():
         '  de-straightened as originally.\n' \
         '\n' \
         'USAGE\n' \
-        '  sct_smooth_spinalcord.py -i <input_image> -c <centerline/segmentation>\n' \
+        '  sct_smooth_spinalcord.py -i <image> -c <centerline/segmentation>\n' \
         '\n' \
         'MANDATORY ARGUMENTS\n' \
-        '  -i <anat>         input image to smooth.\n' \
+        '  -i <image>        input image to smooth.\n' \
         '  -c <centerline>   spinal cord centerline (given by the function sct_get_centerline.py) or segmentation.\n' \
         '\n' \
         'OPTIONAL ARGUMENTS\n' \
+        '  -s                sigma of the smoothing Gaussian kernel (in voxel). Default=3.' \
+        '  -r {0,1}          remove temporary files. Default='+str(param.remove_temp_files)+'\n' \
+        '  -v {0,1,2}        verbose. 0: nothing, 1: txt, 2: txt+fig. Default='+str(param.verbose)+'\n' \
         '  -h                help. Show this message.\n' \
-        '  -r {0,1}          remove temporary files. Default=1\n' \
-        '  -s                sigma of the smoothing Gaussian kernel (expressed in voxels). Default=3.' \
         '\n'
 
     sys.exit(2)
@@ -199,5 +210,5 @@ def usage():
 # Start program
 #=======================================================================================================================
 if __name__ == "__main__":
-    # call main function
+    param = param()
     main()
