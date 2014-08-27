b2ccfb9eacc66631fc99bfb1be0ec7631d3cbd40
Augustin Roux <augustin.roux@gmail.com>
2014-08-27 11:55:26 -0400

    removing register to template test from test all and make it as a particular one
    Former-commit-id: 30549c43d795c7e869cbceb0b3e4fb2d72542d94 [formerly 7c84210420b159ae24fad8a6845e8ba939551457] [formerly 755edcf8eb91c8b86c6035aed06b5b7fa268d234 [formerly 20fd90e90e699a5fe298e9b3181d920a30d7755b [formerly f52c73f4f49328849369e39989cb74bc5d4b6929]]]
    Former-commit-id: 30549c43d795c7e869cbceb0b3e4fb2d72542d94 [formerly 7c84210420b159ae24fad8a6845e8ba939551457]
    Former-commit-id: 799c9a5535f6f62cb1fc5be37b0b0b23887cbf81 [formerly e5696b84ef58d73e5878891c2769eed7ddaea485 [formerly b7aaee356d2d50b74427b9137620bb082d68003c]]
    Former-commit-id: aab0da3cb0a8976ebe203f80debd99f69562a2c1
    Former-commit-id: d905e923f66a6279996e358d88dd4841e33ccf83

---
+++ testing/test_register_to_template.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#
+# Test major functions.
+#
+# Authors: Julien Cohen-Adad, Benjamin De Leener
+# Updated: 2014-08-12
+
+
+import os
+import shutil
+import getopt
+import sys
+import time
+from numpy import loadtxt
+import commands
+# get path of the toolbox
+status, path_sct = commands.getstatusoutput('echo $SCT_DIR')
+# append path that contains scripts, to be able to load modules
+sys.path.append(path_sct + '/scripts')
+import sct_utils as sct
+
+
+# define nice colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
+# Print without new carriage return
+# ==========================================================================================
+def print_line(string):
+    import sys
+    sys.stdout.write(string)
+    sys.stdout.flush()
+
+
+def print_ok():
+    print "[" + bcolors.OKGREEN + "OK" + bcolors.ENDC + "]"
+
+
+def print_warning():
+    print "[" + bcolors.WARNING + "WARNING" + bcolors.ENDC + "]"
+
+
+def print_fail():
+    print "[" + bcolors.FAIL + "FAIL" + bcolors.ENDC + "]"
+
+def write_to_log_file(fname_log,string):
+    f = open(fname_log, 'w')
+    f.write(string+'\n')
+    f.close()
+
+def test_function(folder_test,dot_lines):
+    fname_log = folder_test + ".log"
+    print_line('Checking '+folder_test+dot_lines)
+    os.chdir(folder_test)
+    status, output = commands.getstatusoutput('./test_'+folder_test+'.sh')
+    if status == 0:
+        print_ok()
+    else:
+        print_fail()
+    shutil.rmtree('./results')
+    os.chdir('../')
+    write_to_log_file(fname_log,output)
+    return status
+
+
+# START MAIN
+# ==========================================================================================
+
+start_time = time.time()
+print
+status = (test_function('sct_register_to_template',' .................. '))
+print 'status: '+str(status)
+elapsed_time = time.time() - start_time
+print 'Finished! Elapsed time: '+str(int(round(elapsed_time)))+'s\n'
