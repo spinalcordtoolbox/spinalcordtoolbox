4accd99f844edc45e13a96628a6fb76fb4140a30
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-09-01 10:53:28 -0400

    INST: updated create_links.py because the previous version created links that were not effective (because they have to be created locally)
    Former-commit-id: 35cf70136b28994a816ced6ede14058fd2810828 [formerly 6f526f92b96807c230040511948e82e1c5e246b9] [formerly 653773b3e7ab0dfd2123cc208e82012e8288db52 [formerly 45dd5a8200f344f55dac761def5609bebda67230 [formerly a53ebe1b3c64d68f246d32d9378f6829dac7c2d6]]]
    Former-commit-id: 35cf70136b28994a816ced6ede14058fd2810828 [formerly 6f526f92b96807c230040511948e82e1c5e246b9]
    Former-commit-id: 29b62fa1b3805d069b58b9e534073bd8d1a632f1 [formerly ce84a6e1926b332c3e5667171c3eea6289f9e76f [formerly 17129510c88bc0e27b2f335b0174d1e0dbce15f5]]
    Former-commit-id: 4f8c74c38ba04bd953627e52273783617f10665e
    Former-commit-id: 1491f62903803a9e309e26985da852325abcabd2

--- install/create_links.sh
+++ install/create_links.sh
@@ -7,12 +7,13 @@ CURRENT_DIR=$PWD
 # create soft link to each script in SCT_DIR/script
 echo "Create soft link to each script in $SCT_DIR/script"
 suffix_py='.py'
-cd ${SCT_DIR}/scripts/
+cd ${SCT_DIR}/scripts
 for script in *.py
 do
   echo ${script}
+  cd ${SCT_DIR}/bin
   scriptname=${script%$suffix_py}
-  cmd="ln -s ${script} $SCT_DIR/bin/${scriptname}"
+  cmd="ln -s ../scripts/${script} ${scriptname}"
   echo ">> $cmd"
   $cmd
 done
