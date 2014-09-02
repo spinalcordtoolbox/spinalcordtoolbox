aeb943454b9ba8de14e1c6ad735979f827a01b60
Spinnal Cord Toolbox <sct@localhost.localdomain>
2014-09-02 15:24:45 -0600

    INST: update installation scripts
    Former-commit-id: 7fed97ab28ce1598533689a53b0a6b4fffe8eaca [formerly 4005e1dd68dc81c961bcf19e1029ac0cbefdc0aa]
    Former-commit-id: 2341e3b64afd20de0e732c867880d7793b5b7125
    Former-commit-id: 89250f0cc01ce1f11189c60e323d0df4c24ad87c

--- install/create_links.sh
+++ install/create_links.sh
@@ -3,17 +3,19 @@
 # Script to create links in installer
 #
 CURRENT_DIR=$PWD
+source ~/.bashrc
 
 # create soft link to each script in SCT_DIR/script
 echo "Create soft link to each script in $SCT_DIR/script"
 suffix_py='.py'
-cd ${SCT_DIR}/scripts
+cd $SCT_DIR/scripts
+
 for script in *.py
 do
   echo ${script}
   cd ${SCT_DIR}/bin
   scriptname=${script%$suffix_py}
-  cmd="ln -s ../scripts/${script} ${scriptname}"
+  cmd="sudo ln -s ../scripts/${script} ${scriptname}"
   echo ">> $cmd"
   $cmd
 done
