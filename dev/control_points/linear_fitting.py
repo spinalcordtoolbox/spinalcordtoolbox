3bbc408d2b5457cfdc62c03baf286c1599b30c2e
Augustin Roux <augustin.roux@gmail.com>
2014-09-29 16:59:20 -0400

    fixing git misleading
    Former-commit-id: c0a0cbe59a6b0b609d0b6848b2f64f3eedce21b5 [formerly 26ee15d70fa44512ba823afc2cb93f5075cec5af] [formerly f3ee4b86be85af49cbef5dc58967c19b3e5ff0fa [formerly 3cd86b5ae5026194823ec9c154bff1fb31fd8f5d [formerly da81e10ba3b08cb9cacfcb118b908cc04c2fe234]]]
    Former-commit-id: c0a0cbe59a6b0b609d0b6848b2f64f3eedce21b5 [formerly 26ee15d70fa44512ba823afc2cb93f5075cec5af]
    Former-commit-id: 5b0e899a8049fd859ad94722fb07e67157f8b999 [formerly da71f6cb011d2f0842cdc6b92f92487ac40e68e5 [formerly 9f091ffbbba1531a13ffa6f30823932124bf3156]]
    Former-commit-id: 6fb7424151891f633c59420c1f6a615245a1a8f2
    Former-commit-id: db7e3d719b8a49807cad84e51ef460f57aa32b6c

--- dev/control_points/linear_fitting.py
+++ dev/control_points/linear_fitting.py
@@ -1,4 +1,3 @@
-#
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -12,109 +11,114 @@ import splines_approximation_v2 as splinap
 from sklearn.metrics import mean_squared_error
 
 
-def returnSquaredErrors(fname = '', div = 0):
-	if fname == '':
-		#fname = 'centerlines/t250_half_sup_straight_seg_centerline.nii.gz'
-		fname = 't2_crop_straightttt_seg.nii.gz'
-	file = nibabel.load(fname)
-	data = file.get_data()
-	p1, p2, p3 = splinap.getPxDimensions(fname)
-	nx, ny, nz = splinap.getDim(fname)
-	os.remove(fname)
+def returnSquaredErrors(fname=None, div=None, size=None):
+    if fname == None:
+        #fname = 'centerlines/t250_half_sup_straight_seg_centerline.nii.gz'
+        fname = 't2_crop_straightttt_seg.nii.gz'
+    file = nibabel.load(fname)
+    data = file.get_data()
+    p1, p2, p3 = splinap.getPxDimensions(fname)
+    nx, ny, nz = splinap.getDim(fname)
+    os.remove(fname)
 
 
 
-	print 'sizes: ', nx, ny, nx
-	#x, y, x_fit, y_fit = fit(data, nz)
-	
-	
-	print x, y, z
-	fig1 = plt.figure()
-	ax = Axes3D(fig1)
-	ax.plot(x,y,z,'b:',zdir='z')
-	plt.show()
-	
-	X,Y,Z = (data>0).nonzero()
+    print 'sizes: ', nx, ny, nx
+    #x, y, x_fit, y_fit = fit(data, nz)
 
-	zmin = min(Z) + 5
-	zmax = max(Z) - 5
+    '''
+    print x, y, z
+    fig1 = plt.figure()
+    ax = Axes3D(fig1)
+    ax.plot(x,y,z,'b:',zdir='z')
+    plt.show()
+    '''
 
-	print "min value element : ", zmin
-	print "max value element : ", zmax
+    X,Y,Z = (data>0).nonzero()
 
-	nz = zmax - zmin
+    zmin = min(Z) + 10
+    zmax = max(Z) - 10
 
-	x = [0 for iz in range(0, nz, 1)]
-	y = [0 for iz in range(0, nz, 1)]
-	z = [iz for iz in range(0, nz, 1)]
+    print "min value element : ", zmin
+    print "max value element : ", zmax
 
+    nz = zmax - zmin
 
-	for iz in range(0, nz, 1):
-		x[iz], y[iz] = ndimage.measurements.center_of_mass(np.array(data[:,:,iz + zmin]))
+    x = [0 for iz in range(0, nz, 1)]
+    y = [0 for iz in range(0, nz, 1)]
+    z = [iz for iz in range(0, nz, 1)]
 
 
-	x_fit,y_fit = get_good_centerline(nx, ny, nz)
+    for iz in range(0, nz, 1):
+        x[iz], y[iz] = ndimage.measurements.center_of_mass(np.array(data[:,:,iz + zmin]))
 
 
-	msx = mean_squared_error(x, x_fit)
-	msy = mean_squared_error(y, y_fit)
-	mean_ms = (msx+msy)/2
-
-	#print x, x_fit
-	#print y, y_fit
-
-	print msx, msy, mean_ms
-	size = splinap.getSize(x, y, z, p1, p2, p3)
+    x_fit,y_fit = get_good_centerline(nx, ny, nz)
 
 
-	txt = open('results.txt','a')
-	txt.write(str(div)+' '+str(int(round(size))/div)+' '+str(size)+' '+str(msx)+' '+str(msy)+' '+' '+str(mean_ms)+' '+fname+'\n')
-	txt.close
+    msx = mean_squared_error(x, x_fit)
+    msy = mean_squared_error(y, y_fit)
+    mean_ms = (msx+msy)/2
+
+    #print x, x_fit
+    #print y, y_fit
+
+    print msx, msy, mean_ms
+    size_crop = splinap.getSize(x, y, z, p1, p2, p3)
 
 
-	fig1 = plt.figure()
-	ax = Axes3D(fig1)
-	ax.plot(x,y,z,zdir='z')
-	ax.plot(x_fit,y_fit,z,zdir='z')
-	plt.show()
+    txt = open('results.txt','a')
+    txt.write(str(div)+' '+str(int(round(size))/div)+' '+str(size_crop)+' '+str(size)+' '+str(msx)+' '+str(msy)+' '+' '+str(mean_ms)+' '+fname+'\n')
+    txt.close()
+
+    #splinap.plot(x_fit,y_fit, z, x, y, z, fname, int(round(size))/div)
+
+
+    '''
+    fig1 = plt.figure()
+    ax = Axes3D(fig1)
+    ax.plot(x,y,z,zdir='z')
+    ax.plot(x_fit,y_fit,z,zdir='z')
+    plt.show()
+    '''
 
 
 def get_good_centerline (nx, ny,nz):
-	x = [nx/2 for iz in range(0, nz, 1)]
-	y = [ny/2 for iz in range(0, nz, 1)]
-	return x,y
+    x = [nx/2 for iz in range(0, nz, 1)]
+    y = [ny/2 for iz in range(0, nz, 1)]
+    return x,y
 
 
 def fit(data, nz):
-	x = [0 for iz in range(0, nz, 1)]
-	y = [0 for iz in range(0, nz, 1)]
-	z = [iz for iz in range(0, nz, 1)]
+    x = [0 for iz in range(0, nz, 1)]
+    y = [0 for iz in range(0, nz, 1)]
+    z = [iz for iz in range(0, nz, 1)]
 
-	for iz in range(0, nz, 1):
-		x[iz], y[iz] = ndimage.measurements.center_of_mass(np.array(data[:,:,iz]))
-	
-	#Fit centerline in the Z-X plane using polynomial function
-	print '\nFit centerline in the Z-X plane using polynomial function...'
-	coeffsx = np.polyfit(z, x, 1)
-	polyx = np.poly1d(coeffsx)
-	x_fit = np.polyval(polyx, z)
-	print 'x_fit'
-	print x_fit
+    for iz in range(0, nz, 1):
+        x[iz], y[iz] = ndimage.measurements.center_of_mass(np.array(data[:,:,iz]))
 
-	#Fit centerline in the Z-Y plane using polynomial function
-	print '\nFit centerline in the Z-Y plane using polynomial function...'
-	coeffsy = np.polyfit(z, y, 1)
-	polyy = np.poly1d(coeffsy)
-	y_fit = np.polyval(polyy, z)
+    #Fit centerline in the Z-X plane using polynomial function
+    print '\nFit centerline in the Z-X plane using polynomial function...'
+    coeffsx = np.polyfit(z, x, 1)
+    polyx = np.poly1d(coeffsx)
+    x_fit = np.polyval(polyx, z)
+    print 'x_fit'
+    print x_fit
 
-	
-	#### 3D plot
-	fig1 = plt.figure()
-	ax = Axes3D(fig1)
-	ax.plot(x,y,z,zdir='z')
-	ax.plot(x_fit,y_fit,z,zdir='z')
-	plt.show()
-	return x, y, x_fit, y_fit
+    #Fit centerline in the Z-Y plane using polynomial function
+    print '\nFit centerline in the Z-Y plane using polynomial function...'
+    coeffsy = np.polyfit(z, y, 1)
+    polyy = np.poly1d(coeffsy)
+    y_fit = np.polyval(polyy, z)
+
+
+    #### 3D plot
+    fig1 = plt.figure()
+    ax = Axes3D(fig1)
+    ax.plot(x,y,z,zdir='z')
+    ax.plot(x_fit,y_fit,z,zdir='z')
+    plt.show()
+    return x, y, x_fit, y_fit
 
  	#return x_centerline_fit,y_centerline_fit,z_centerline_fit
 
