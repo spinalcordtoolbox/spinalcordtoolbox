7ac14cef577b05e5b867913e797e6edad082b6a9
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-10-04 16:47:25 -0400

    OPT: major changes to test_all.py

--- testing/test_sct_convert_binary_to_trilinear.py
+++ testing/test_sct_convert_binary_to_trilinear.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 #########################################################################################
 #
-# Test function for sct_convert_binary_to_trilinear script
-#
-#   replace the shell test script in sct 1.0
+# Test function sct_convert_binary_to_trilinear
 #
 # ---------------------------------------------------------------------------------------
 # Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
 # Author: Augustin Roux
-# modified: 2014/09/25
+# modified: 2014-10-04
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
@@ -19,47 +17,26 @@ import test_all
 import time
 import shutil
 
-class bcolors:
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    red = '\033[91m'
 
-class Param:
-    def __init__(self):
-        self.contrasts = ['t2']
-        self.subjects = []
-        self.files = ['t2_seg.nii.gz']
+def test(path_data):
 
+    # parameters
+    folder_data = 't2/'
+    file_data = 't2_seg.nii.gz'
 
-def test(data_file_path):
-    # initialize parameters
-    status = 0
-    param = Param()
-    results_dir = 'results_sct_convert_binary_to_trilinear'
+    # define command
+    cmd = 'sct_convert_binary_to_trilinear' \
+        ' -i '+path_data+folder_data+file_data+ \
+        ' -s 5'
 
-    if os.path.isdir(results_dir):
-        shutil.rmtree(results_dir)
+    # run command
+    status, output = sct.run(cmd, 0)
 
-    os.makedirs(results_dir)
-    os.chdir(results_dir)
-
-    begin_log_file = "test ran at "+time.strftime("%y%m%d%H%M%S")+"\n"
-    fname_log = "sct_convert_binary_to_trilinear.log"
-
-    test_all.write_to_log_file(fname_log, begin_log_file, 'w')
-
-    for contrast in param.contrasts:
-        test_all.write_to_log_file(fname_log, bcolors.red+"Contrast: "+contrast+"\n", 'a')
-        for f in param.files:
-            cmd = "sct_convert_binary_to_trilinear -i ../"+data_file_path+'/'+contrast+'/'+f+" -s 5"
-            s, output = sct.run(cmd, 0)
-            test_all.write_to_log_file(fname_log, output, 'a')
-            status += s
-
-    os.chdir('..')
-    return status
+    # return
+    return sct.run(cmd, 0)
 
 
+# call to function
 if __name__ == "__main__":
     # call main function
     test()
