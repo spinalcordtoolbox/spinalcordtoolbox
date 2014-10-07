68930810f3349be59f4ac32b8b0351bbc4fcbf2a
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-10-06 20:28:31 -0400

    TEST: added test for sct_smooth_spinalcord
    Former-commit-id: 06f6a7639c71f7dbcbe5d228e4104ef840f9b8fc [formerly 88364a53b2a0921fe1b7e8531dc11bdac5275cb6] [formerly 0c26bb9322eb2377a0e8a3e9f1d6e4d3e15750bb [formerly 50c6d6f88d14cb4dc727a941ac71b2c39fedcf7b [formerly ef6b5028eb585613dc4183cb7f227f838a98fa3d]]]
    Former-commit-id: 06f6a7639c71f7dbcbe5d228e4104ef840f9b8fc [formerly 88364a53b2a0921fe1b7e8531dc11bdac5275cb6]
    Former-commit-id: d89ee663c832d19735c46d4f6f6cf981ef9f877e [formerly 28815e955632692dc695ac64c3bff48ce291afc4 [formerly a48237431b390c356f5d50157898ab93f23be2a0]]
    Former-commit-id: 81ea4bf996da9db3d83c97d219d3d6cddd3a3cf2
    Former-commit-id: acb2811f80016bdc3751afb2a7b6ebc2673f50a4

--- testing/test_sct_smooth_spinalcord.py
+++ testing/test_sct_smooth_spinalcord.py
@@ -8,77 +8,32 @@
 # ---------------------------------------------------------------------------------------
 # Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
 # Author: Augustin Roux
-# modified: 2014/09/28
+# modified: 2014-08-10
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-import os
+
 import sct_utils as sct
-import test_all
-import time
-import shutil
-
-class bcolors:
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    red = '\033[91m'
-
-class Param:
-    def __init__(self):
-        self.contrasts = ['t2']
-        self.subjects = []
-        self.files = [['t2.nii.gz', 't2_segmentation_PropSeg.nii.gz']]
-        self.sigma = str(4)
 
 
-def test(data_file_path):
-    # initialize parameters
-    status = 0
-    param = Param()
-    sigma = param.sigma
-    results_dir = 'results_sct_spinalcord'
+def test(path_data):
 
-    if os.path.isdir(results_dir):
-        shutil.rmtree(results_dir)
+    # parameters
+    folder_data = 't2/'
+    file_data = ['t2.nii.gz', 't2_seg.nii.gz']
 
-    os.makedirs(results_dir)
-    os.chdir(results_dir)
+    # define command
+    cmd = 'sct_smooth_spinalcord' \
+        ' -i '+path_data+folder_data+file_data[0]+ \
+        ' -c '+path_data+folder_data+file_data[1]+ \
+        ' -s 5'
 
-    begin_log_file = "test ran at "+time.strftime("%y%m%d%H%M%S")+"\n"
-    fname_log = "sct_register_multimodal.log"
-
-    test_all.write_to_log_file(fname_log, begin_log_file, 'w')
-
-    for contrast in param.contrasts:
-        test_all.write_to_log_file(fname_log, bcolors.red+"Contrast: "+contrast+"\n", 'a')
-        for f in param.files:
-            # Run sct_smooth_spinalcord
-            cmd = 'sct_smooth_spinalcord -i ../' + data_file_path + '/' + contrast + '/' + f[0] \
-                + ' -c ../' + data_file_path + '/' + contrast + '/' + f[1] \
-                + ' -s ' + sigma
-            s, output = sct.run(cmd, 0)
-            test_all.write_to_log_file(fname_log, output, 'a')
-            status += s
-            # Isotropic smoothing of the same image with same standard deviation (for the Gaussian) for comparison purposes
-            cmd = 'sct_c3d ../' + data_file_path + '/' + contrast + f[0] \
-                + ' -smooth ' + sigma + 'x' + sigma + 'x' + sigma + 'vox' \
-                + ' -o ' + contrast + '_isotropic_smoothed.nii.gz'
-            s, output = sct.run(cmd, 0)
-            test_all.write_to_log_file(fname_log, output, 'a')
-            status += s
-            # Smoothing along Z (corresponding to X, given the orientation of the image)
-            cmd = 'sct_c3d ' + data_file_path + '/' + f[0] \
-                + ' -smooth ' + sigma + 'x0x0vox' \
-                + ' -o ' + contrast + '_z_smoothed.nii.gz'
-            s, output = sct.run(cmd, 0)
-            test_all.write_to_log_file(fname_log, output, 'a')
-            status += s
-
-    os.chdir('..')
-    return status
+    # return
+    return sct.run(cmd, 0)
 
 
+# call to function
 if __name__ == "__main__":
     # call main function
     test()
