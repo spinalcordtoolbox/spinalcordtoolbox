c8ea819956c3b3bf710baa0cf67251bf2f6e5897
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-10-12 19:14:19 -0400

    REF: removed flirt dependency
    Former-commit-id: 7ff143bd476ad194e4efa49315f21c20e28b031c [formerly 591b5cd3b188af2da0705dc7877da2253787a892]
    Former-commit-id: 06a15a5fac1e9937d9fb564b3c48a5288a48fcad
    Former-commit-id: 4a519cdab3d459607a2a4a38292cf5ccbd1ee8df

--- scripts/sct_fmri_moco.py
+++ scripts/sct_fmri_moco.py
@@ -25,7 +25,7 @@ from sct_dmri_separate_b0_and_dwi import identify_b0
 
 class param:
     def __init__(self):
-        self.debug = 0
+        self.debug = 1
         self.fname_data = ''
         self.fname_target = ''
         #self.fname_centerline = ''
@@ -68,8 +68,8 @@ def main():
     if param.debug:
         # get path of the testing data
         status, path_sct_data = commands.getstatusoutput('echo $SCT_TESTING_DATA_DIR')
-        param.fname_data = '/Users/julien/data/spinalcordtoolbox_data/oxford/fmri/fmri_sub_bad.nii.gz'  #path_sct_data+'/fmri/fmri.nii.gz'
-        param.fname_mask = '/Users/julien/data/spinalcordtoolbox_data/oxford/fmri/fmri_sub_mask.nii.gz'  #path_sct_data+'/fmri/fmri.nii.gz'
+        param.fname_data = path_sct_data+'/fmri/fmri.nii.gz'
+        #param.fname_mask = path_sct_data+'/fmri/fmri.nii.gz'
         param.verbose = 1
         param.group_size = 3
         param.program = 'slicereg'  # ants_affine, flirt
