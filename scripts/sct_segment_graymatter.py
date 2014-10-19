4fcc4a1917e71f9e2da070872ff339bdaed09b00
Augustin Roux <augustin.roux@gmail.com>
2014-10-18 20:18:19 -0400

    NEW: seg of the grey matter added
    Former-commit-id: 8b6fb95137caa6e387606dd622f12d32df51c885 [formerly 817805ca90aff3a7ecc3bf97f8cafd628c45baf5] [formerly 18b5667e7cd82f76d3f7ad7680b397b6d71a17b2 [formerly fda9f7d5f750346543e30bf0043ca4a3acdd0803 [formerly ce3e2f539fa8f3c98918eef60c824712b0ce2663]]]
    Former-commit-id: 8b6fb95137caa6e387606dd622f12d32df51c885 [formerly 817805ca90aff3a7ecc3bf97f8cafd628c45baf5]
    Former-commit-id: 4fbd483f08c19be2a9c4db8b95d1df81a9b52c7c [formerly a15ea98b3b606d225c749ac1fdb205e456bd1513 [formerly a9963405895ccbfadf6c99d130ce45c71c5af8b9]]
    Former-commit-id: 6db60e09b9fdb75cf77f570e77c6108b01e73ba1
    Former-commit-id: 34bc73f3570a3239849b5933df03286b38ca415e

--- scripts/sct_segment_graymatter.py
+++ scripts/sct_segment_graymatter.py
@@ -9,6 +9,10 @@
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
+import sct_utils as sct
+import os
+import sys
+import getopt
 
 
 def main():
@@ -16,17 +20,51 @@ def main():
     fname_landmarks = ''
     fname_t2star = ''
     fname_grayMatter_template = ''
+    fname_seg = ''
+
+    # Check input param
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'hi:t:l:h:s:')
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+        elif opt in ('-i'):
+            fname_anat = arg
+        elif opt in ('-t'):
+            fname_t2star = arg
+        elif opt in ('-l'):
+            fname_landmarks = arg
+        elif opt in ('-s'):
+            fname_seg = arg
+
+    # register template to anat file: this generate warp_template2anat.nii
+    cmd = "sct_register_to_template -i " + fname_anat + " -l " + fname_landmarks + " -m " + fname_seg + " -s normal"
+    sct.run(cmd)
+    warp_template2anat = ""
+
+    # register anat file to t2star: generate  warp_anat2t2star
+    cmd = "sct_register_multimodal -i " + fname_anat + " -d " + fname_t2star
+    sct.run(cmd)
+    warp_anat2t2star = ""
+
+    # concatenation of the two warping fields
+    warp_template2t2star = "warp_template2t2star.nii.gz"
+    cmd = "sct_concat_transfo -w " + warp_template2anat + ',' + warp_anat2t2star + " -d " + fname_t2star + " -o " + warp_template2t2star
+    sct.run(cmd)
+
+    # apply the concatenated warping field to the template
+    cmd = "sct_warp_template -d " + fname_t2star + " -w " + warp_template2anat + " -s 1 -o template_in_t2star_space"
+    sct.run(cmd)
 
 
-    # register template to anat file
-    cmd = "sct_register_to_template -i " + fname_anat + " -l " +
-    sct_register_to_template -i t2_cropped.nii.gz -l t2_cropped_landmarks_C2_T2.nii.gz -m t2_cropped_seg.nii.gz -s normal
-
-    sct_register_to_template --> warp_template2anat
+    #sct_register_to_template --> warp_template2anat
     # register anat file to t2star
-    sct_register_multimodal --> warp_anat2t2star
+    #sct_register_multimodal --> warp_anat2t2star
     # concatenate warp_template2anat with warp_anat2t2star
-    --> warp_template2t2star
+    #--> warp_template2t2star
 
 
 
@@ -41,9 +79,35 @@ def main():
     return
 
 
+#=======================================================================================================================
+# usage
+#=======================================================================================================================
+def usage():
+    print '\n' \
+        ''+os.path.basename(__file__)+'\n' \
+        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
+        'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n' \
+        '\n'\
+        'DESCRIPTION\n' \
+        '  This function returns the grey matter segmentation.\n' \
+        '\n'\
+        'USAGE\n' \
+        '  '+os.path.basename(__file__)+' -i <anat volume> -t <t2star volume> -l <landmarks>\n' \
+        '\n'\
+        'MANDATORY ARGUMENTS\n' \
+        '  -i                   input anatomical volume.\n' \
+        '  -t                   t2star volume.\n' \
+        '  -l                   landmarks at spinal cord center.' \
+        '\n'\
+        'OPTIONAL ARGUMENTS\n' \
+        '  -s <segmentation>    spinal cord segmentation.\n'
+    sys.exit(2)
+
+
+
 if __name__ == "__main__":
     # initialize parameters
-    param = param()
+    # param = param()
     # call main function
     main()
 
