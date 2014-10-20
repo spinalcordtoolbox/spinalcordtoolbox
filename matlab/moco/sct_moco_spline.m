f8c5fd5ac46416c9dee26e3e2474687e4f7a663d
Tanguy DUVAL <duvaltanguy@gmail.com>
2014-10-20 15:01:54 -0400

    Ca va toi? moco enhanced
    Former-commit-id: 341b590e8b527cbe885696691e73013de79b9f71 [formerly 4e63ff824245166717ae3bb63b2e553150876bd8]
    Former-commit-id: 294cfbd5135948f3fe7089ac5c41bdb174b00fed
    Former-commit-id: 4b087ccd096bfc53dc95a50fad7661745f670e41

--- matlab/moco/sct_moco_spline.m
+++ matlab/moco/sct_moco_spline.m
@@ -1,30 +1,69 @@
 function sct_moco_spline(fname_mat, varargin)
-% sct_moco_spline(fname_mat, fname_log(optional) )
+% ind_abrupt=sct_moco_spline(fname_mat, fname_log(optional) )
 if ~isempty(varargin), log_spline = varargin{1}; else log_spline = 'log_sct_moco_spline'; end
+if length(varargin)>1, ind_ab = varargin{2}; else ind_ab = []; end
+
 
 j_disp(log_spline,['\nSmoothing Patient Motion...'])
 % LOAD MATRIX
 list=dir(fname_mat);
 path=[fileparts(fname_mat) filesep];
 list=sort_nat({list.name});
+Z_index=cellfun(@(x) cell2mat(textscan(x,'%*[mat.T]%*u%*[_Z]%u%*[.txt]')),list);
+T=cellfun(@(x) cell2mat(textscan(x,'%*[mat.T]%u%*[_Z]%*u%*[.txt]')),list); T=single(T);
 j_progress('loading matrix...')
 for imat=1:length(list), j_progress(imat/length(list)); M_tmp{imat}=load([path list{imat}]); X(imat)=M_tmp{imat}(1,4); Y(imat)=M_tmp{imat}(2,4); end
 j_progress('elapsed')
 
+color=jet(max(Z_index));
 % Plot subject movement
-h=figure; plot(X,'+')
-g=figure; plot(Y,'+')
-% Search for abrupt moves
-ind_ab=1;
-ind_ab=[ind_ab find(abs(diff(Y))>2) find(abs(diff(X))>2) length(X)]; ind_ab=sort(ind_ab); ind_ab(diff(ind_ab)<15)=[];
+figure(28); hold off; figure(29); hold off; 
+for iZ=unique(Z_index)
+    figure(28); plot(T(Z_index==iZ),X(Z_index==iZ),'+','Color',color(iZ,:)); hold on
+    figure(29); plot(T(Z_index==iZ),Y(Z_index==iZ),'+','Color',color(iZ,:)); hold on
+    
+    TZ=T(Z_index==iZ);
+    % abrupt motion detection
+    [~,locs]=findpeaks(abs(diff(Y(Z_index==iZ))),'MINPEAKDISTANCE',15,'THRESHOLD',0.7);
+    ind_ab=[ind_ab (locs+1)];
+end
+ind_ab=sort(ind_ab);
+if length(ind_ab)>1
+    ind_ab_cat=clusterdata(ind_ab',1);
+    
+    ind_ab_keep=[];
+    for i=unique(ind_ab_cat)'
+        ind_ab_keep=[ind_ab_keep floor(mean(ind_ab(ind_ab_cat==i)))];
+    end
+    ind_ab=ind_ab_keep;
+end
+
+ind_ab=sort(ind_ab);
+
+if ~isempty(ind_ab)
+    ind_ab = input(['abrut motion detect around volume ' num2str(ind_ab) '.. specify exact volume # (before motion) as a matrix:']);
+end
+
+ind_ab=[0 ind_ab max(T)];
 
 % GENERATE SPLINE
-for iab=2:length(ind_ab)
-    j_disp(log_spline,['Generate motion splines...'])
-    X(ind_ab(iab-1):ind_ab(iab))=spline(X(ind_ab(iab-1):ind_ab(iab))); Y(ind_ab(iab-1):ind_ab(iab))=spline(Y(ind_ab(iab-1):ind_ab(iab)));
+for iZ=unique(Z_index)
+    Xtmp=X(Z_index==iZ); Ytmp=Y(Z_index==iZ); Ttmp=T(Z_index==iZ);
+    for iab=2:length(ind_ab)
+        j_disp(log_spline,['Generate motion splines...'])
+        Tpiece=ind_ab(iab-1)+1:ind_ab(iab);
+        
+        index=Ttmp>ind_ab(iab-1) &Ttmp<=ind_ab(iab);% Piece index
+        Xfitresult=spline(Ttmp(index),Xtmp(index)); Yfitresult=spline(Ttmp(index),Ytmp(index));
+        Xout(iZ,Tpiece) = feval(Xfitresult,Tpiece); Yout(iZ,Tpiece)=feval(Yfitresult,Tpiece);
+    end
+    % plot splines
+    Ttotal=1:max(T);
+    figure(28), hold on; plot(Ttotal,Xout(iZ,:),'-','Color',color(iZ,:)); hold off; legend('raw moco', 'smooth moco', 'Location', 'NorthEast' ); grid on; ylabel( 'X Displacement (mm)' ); xlabel('volume #');
+    figure(29); hold on; plot(Ttotal,Yout(iZ,:),'-','Color',color(iZ,:)); hold off; legend('raw moco', 'smooth moco', 'Location', 'NorthEast' ); grid on; ylabel( 'Y Displacement (mm)' ); xlabel('volume #');
 end
-figure(h), hold on; plot(X,'r+'); hold off; legend('raw moco', 'smooth moco', 'Location', 'NorthEast' ); grid on; ylabel( 'Displacement (mm)' ); xlabel('volume #');
-figure(g); hold on; plot(Y,'r+'); hold off; legend('raw moco', 'smooth moco', 'Location', 'NorthEast' ); grid on; ylabel( 'Displacement (mm)' ); xlabel('volume #');
+
+
 
 j_disp(log_spline,['...done!'])
 % SAVE MATRIX
@@ -32,22 +71,28 @@ j_progress('\nSave Matrix...')
 % move old matrix
 if ~exist([path 'old'],'dir'); mkdir([path 'old']); end
 unix(['mv ' fname_mat ' ' path 'old/'])
-for imat=1:length(list),
-    j_progress(imat/length(list))
-    % update matrix
-    M_tmp{imat}(1,4)=X(imat); M_tmp{imat}(2,4)=Y(imat);
-    % write matrix
-    fid = fopen([path list{imat}],'w');
-    fprintf(fid,'%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n',[M_tmp{imat}(1,1:4), M_tmp{imat}(2,1:4), M_tmp{imat}(3,1:4), M_tmp{imat}(4,1:4)]);
-    fclose(fid);
-    
+
+% create new list
+for iT=Ttotal
+    for iZ=unique(Z_index)
+        mat_name=['mat.T' num2str(iT) '_Z' num2str(iZ) '.txt'];
+        % update matrix
+        M=diag([1 1 1 1]);
+        M(1,4)=Xout(iZ,iT); M(2,4)=Yout(iZ,iT);
+        % write matrix
+        fid = fopen([path mat_name],'w');
+        fprintf(fid,'%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n%f %f %f %f\n',[M(1,1:4), M(2,1:4), M(3,1:4), M(4,1:4)]);
+        fclose(fid);
+        
+    end
 end
 
 
-function M_motion_t = spline(M_motion_t)
+
+function fitresult = spline(T,M_motion_t,Teval)
 
 %% Fit: 'sct_moco_spline'.
-[xData, yData] = prepareCurveData( [], M_motion_t );
+[xData, yData] = prepareCurveData( T, M_motion_t );
 
 % Set up fittype and options.
 ft = fittype( 'smoothingspline' );
@@ -56,7 +101,7 @@ opts.SmoothingParam = 1e-06;
 
 % Fit model to data.
 [fitresult, gof] = fit( xData, yData, ft, opts );
-M_motion_t = feval(fitresult,1:length(M_motion_t));
+M_motion_t = feval(fitresult,T);
 
 
 
