303bf75b3ddf0c2e4fb6bc565c224fd51040fc44
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-10-19 20:22:52 -0400

    BUG: now possible to apply transfo to 4d files (issue #178)

--- scripts/sct_apply_transfo.py
+++ scripts/sct_apply_transfo.py
@@ -20,13 +20,13 @@ import os
 import getopt
 import commands
 import sct_utils as sct
+import time
 
 # DEFAULT PARAMETERS
 class param:
     def __init__(self):
         self.debug = 0
         self.verbose = 1  # verbose
-        self.dim = 3
         self.interp = 'spline'  # nn, trilinear, spline
 
 
@@ -40,7 +40,7 @@ def main():
     fname_dest = ''  # destination image (fix)
     fname_src_reg = ''
     verbose = 1
-    dim = param.dim
+    fsloutput = 'export FSLOUTPUTTYPE=NIFTI; '  # for faster processing, all outputs are in NIFTI
 
     # Parameters for debug mode
     if param.debug:
@@ -95,9 +95,18 @@ def main():
     sct.check_file_exist(fname_dest)
     for i in range(len(fname_warp_list)):
         sct.check_file_exist(fname_warp_list[i])
+        # get absolute path
+        fname_warp_list[i] = os.path.abspath(fname_warp_list[i])
+
+    # check if destination file is 3d
+    sct.check_if_3d(fname_dest)
+
+    # N.B. Here we take the inverse of the warp list, because sct_WarpImageMultiTransform concatenates in the reverse order
+    fname_warp_list.reverse()
 
     # Extract path, file and extension
     path_src, file_src, ext_src = sct.extract_fname(os.path.abspath(fname_src))
+    fname_dest = os.path.abspath(fname_dest)
 
     # Get output folder and file name
     if fname_src_reg == '':
@@ -107,21 +116,63 @@ def main():
         fname_src_reg = path_out+file_out+ext_out
     else:
         path_out, file_out, ext_out = sct.extract_fname(fname_src_reg)
+    fname_out = path_out+file_out+ext_out
 
-    # Apply transformation
-    sct.printv('\nApply transformation...', verbose)
-    # N.B. Here we take the inverse of the warp list, because sct_WarpImageMultiTransform concatenates in the reverse order
-    fname_warp_list.reverse()
-    sct.run('sct_antsApplyTransforms -d ' + str(dim) + ' -i '+fname_src+' -o '+fname_src_reg+' -t '+' '.join(fname_warp_list)+' -r '+fname_dest+interp, verbose)
+    # create temporary folder
+    sct.printv('\nCreate temporary folder...', verbose)
+    path_tmp = sct.slash_at_the_end('tmp.'+time.strftime("%y%m%d%H%M%S"), 1)
+    sct.run('mkdir '+path_tmp, verbose)
+
+    # Copying input data to tmp folder
+    # NB: cannot use c3d here because c3d cannot convert 4D data.
+    sct.printv('\nCopying input data to tmp folder and convert to nii...', verbose)
+    sct.run('cp '+fname_src+' '+path_tmp+'data'+ext_src, verbose)
+    # go to tmp folder
+    os.chdir(path_tmp)
+    # convert to nii format
+    sct.run('fslchfiletype NIFTI data', verbose)
+
+    # Get dimensions of data
+    sct.printv('\nGet dimensions of data...', verbose)
+    nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension('data.nii')
+    sct.printv('  ' + str(nx) + ' x ' + str(ny) + ' x ' + str(nz)+ ' x ' + str(nt), verbose)
+
+    # if 3d
+    if nt == 1:
+        # Apply transformation
+        sct.printv('\nApply transformation...', verbose)
+        sct.run('sct_antsApplyTransforms -d 3 -i data.nii -o data_reg.nii -t '+' '.join(fname_warp_list)+' -r '+fname_dest+interp, verbose)
+
+    # if 4d, loop across the T dimension
+    else:
+        # split along T dimension
+        sct.printv('\nSplit along T dimension...', verbose)
+        sct.run(fsloutput+'fslsplit data data_T', verbose)
+        # apply transfo
+        sct.printv('\nApply transformation to each 3D volume...', verbose)
+        for it in range(nt):
+            file_data_split = 'data_T'+str(it).zfill(4)+'.nii'
+            file_data_split_reg = 'data_reg_T'+str(it).zfill(4)+'.nii'
+            sct.run('sct_antsApplyTransforms -d ' + str(dim) + ' -i '+file_data_split+' -o '+file_data_split_reg+' -t '+' '.join(fname_warp_list)+' -r '+fname_dest+interp, verbose)
+        # Merge files back
+        sct.printv('\nMerge file back...', verbose)
+        cmd = fsloutput+'fslmerge -t data_reg'
+        for it in range(nt):
+            file_data_split_orient = 'data_orient_T'+str(it).zfill(4)+'.nii'
+            cmd = cmd+' '+file_data_split_orient
+        sct.run(cmd, param.verbose)
+
+    # come back to parent folder
+    os.chdir('..')
 
     # Generate output files
     sct.printv('\nGenerate output files...', verbose)
-    sct.generate_output_file(fname_src_reg, path_out+file_out+ext_out)
+    sct.generate_output_file(path_tmp+'data_reg.nii', fname_out)
 
     # to view results
     print '\nDone! To view results, type:'
     print 'fslview '+fname_dest+' '+fname_src_reg+' &'
-    print ''
+    print
 
 
 # Print usage
@@ -139,8 +190,8 @@ USAGE
   """+os.path.basename(__file__)+""" -i <source> -d <dest> -w <warp_list>
 
 MANDATORY ARGUMENTS
-  -i <source>           source image (moving)
-  -d <dest>             destination image (fixed)
+  -i <source>           source image (moving). Can be 3D or 4D.
+  -d <dest>             destination image (fixed). Must be 3D.
   -w <warp_list>        warping field. If more than one, separate with ","
 
 OPTIONAL ARGUMENTS
