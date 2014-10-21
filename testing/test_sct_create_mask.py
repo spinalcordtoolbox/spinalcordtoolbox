a4629634bb0f5f1477201075d5dd527ebe805260
Augustin Roux <augustin.roux@gmail.com>
2014-10-21 18:05:46 -0400

    BUG : -r 0 added to test_sct_create_mask commands
    Former-commit-id: d3c7fa8264a4b0a25b56beac822b9e2ce0d1d21c [formerly 2d7a532ed20f6dee0f58fd5d6c9ad8d1b08db56e]
    Former-commit-id: ecc2706b61f05c54b6bec380ab62e59ed5cdd144
    Former-commit-id: de4fa09ad641a156f3dcfc7cbfd6f3d0667d00d7

--- testing/test_sct_create_mask.py
+++ testing/test_sct_create_mask.py
@@ -25,8 +25,9 @@ def test(data_path):
 
     # method coord
     cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
-          + ' -m coord,15x17'\
-          + ' -s 10'
+          + ' -m coord,15x17' \
+          + ' -s 10' \
+          + ' -r 0'
     status, output = commands.getstatusoutput(cmd)
 
     # method point
@@ -34,7 +35,9 @@ def test(data_path):
 
     cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
           + ' -m point,' + data_path + folder_data + file_data[1] \
-          + ' -s 10'
+          + ' -s 10' \
+          + ' -r 0'
+
     s, o = commands.getstatusoutput(cmd)
 
     status += s
@@ -43,7 +46,9 @@ def test(data_path):
     # method center
     cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
           + ' -m center' \
-          + ' -s 10'
+          + ' -s 10' \
+          + ' -r 0'
+
     s, o = commands.getstatusoutput(cmd)
 
     status += s
@@ -52,7 +57,9 @@ def test(data_path):
     # method centerline
     cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
           + ' -m centerline,' + data_path + folder_data + file_data[2] \
-          + ' -s 10'
+          + ' -s 10' \
+          + ' -r 0'
+
     s, o = commands.getstatusoutput(cmd)
 
     status += s
