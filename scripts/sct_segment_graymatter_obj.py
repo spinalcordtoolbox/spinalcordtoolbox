5171be9c65f645aa25caeee7d7c2cd32255a6c02
Augustin Roux <augustin.roux@gmail.com>
2014-10-22 17:40:17 -0400

    NEW: oo segment_graymatter beta added
    Former-commit-id: 4312182e1f1f52b12ddc42986196b38933626bbf [formerly a503b2b6bba615a0189968a4b9f89f4c3816f77d]
    Former-commit-id: 9888d9cc3289a78e60cda111ade60d1415568a4c
    Former-commit-id: 5af1835eadcf81b16a3a856bc9f96f67fc96d04e

---
+++ scripts/sct_segment_graymatter_obj.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+#
+# This program returns the grey matter segmentation given anatomical, landmarks and t2star images
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2013 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Authors: Benjamin De Leener, Augustin Roux
+# Created: 2014-10-18
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+import sct_utils as sct
+import os
+import time
+import sys
+import getopt
+
+
+class SegmentGrayMatter():
+    def __init__(self, fname_ref, fname_moving, moving_name, fixed_name, fname_output, moving_seg_name='', fixed_seg_name=''):
+        self.fname_ref = fname_ref
+        self.fname_moving = fname_moving
+        self.fname_output = fname_output
+        self.moving_name = moving_name
+        self.fixed_name = fixed_name
+        self.moving_seg_name = moving_seg_name
+        self.fixed_seg_name = fixed_seg_name
+        self.path_output, self.file_output, self.ext_output = sct.extract_fname(fname_output)
+        self.check_file_exist()
+
+    def check_file_exist(self):
+        sct.check_file_exist(self.fname_ref)
+        sct.check_file_exist(self.fname_moving)
+        if (self.fname_seg_moving != '' and self.fname_seg_fixed == '') or\
+                (self.fname_seg_moving == '' and self.fname_seg_fixed != ''):
+                print('\nERROR: You need to provide one mask for each image (moving and fixed)')
+                usage()
+        if self.fname_seg_moving != '':
+            sct.check_file_exist(self.fname_moving)
+        if self.fname_seg_fixed != '':
+            sct.check_file_exist(self.fname_seg_fixed)
+
+    def copy_file(self, path_tmp):
+        print('\nCopy files...')
+        sct.run("sct_c3d "+self.fname_moving+" -o "+path_tmp+"/"+self.moving_name+".nii")
+        sct.run("sct_c3d "+self.fname_ref+" -o "+path_tmp+"/"+self.fixed_name+".nii")
+        if self.fname_seg_moving != '':
+            sct.run("sct_c3d "+self.fname_moving+" -o "+path_tmp+"/"+self.moving_seg_name+".nii")
+        if self.fname_seg_fixed != '':
+            sct.run("sct_c3d "+self.fname_moving+" -o "+path_tmp+"/"+self.fixed_seg_name+".nii")
+
+    def crop_xy(self):
+        fixed_name_temp = fixed_name + "_crop"
+    cmd = "sct_crop_image -i " + fixed_name + ".nii -o " + fixed_name_temp + ".nii -m ../" + fname_seg_fixed + " -shift 10,10 -dim 0,1"
+    print cmd
+    sct.run(cmd)
+    fixed_name = fixed_name_temp
+
+
+def main():
+    fname_ref = ''
+    fname_moving = ''
+    transformation = 'SyN'
+    gradient_step = '0.5'
+    radius = '4'
+    fname_seg_fixed = ''
+    fname_seg_moving = ''
+    fname_output = ''
+    padding = '10'
+
+    moving_name = 'moving'
+    fixed_name = 'fixed'
+    moving_seg_name = 'moving_seg'
+    fixed_seg_name = 'fixed_seg'
+
+    remove_temp = 0
+
+    # Check input param
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'hi:d:t:s:g:o:')
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+        elif opt in ('-d'):
+            fname_moving = arg
+        elif opt in ('-i'):
+            fname_ref = arg
+        elif opt in ('-t'):
+            transformation = arg
+        elif opt in ('-s'):
+            fname_seg_fixed = arg
+        elif opt in ('-g'):
+            fname_seg_moving = arg
+        elif opt in ('-o'):
+            fname_output = arg
+
+    if fname_moving == '' or fname_ref == '':
+        usage()
+
+    # check existence of input files
+
+
+    # Extract path/file/extension
+
+    # create temporary folder
+    print('\nCreate temporary folder...')
+    path_tmp = 'tmp.'+time.strftime("%y%m%d%H%M%S")
+    sct.run('mkdir '+path_tmp)
+
+    # copy files to temporary folder
+    print('\nCopy files...')
+    sct.run("sct_c3d "+fname_moving+" -o "+path_tmp+"/"+moving_name+".nii")
+    sct.run("sct_c3d "+fname_ref+" -o "+path_tmp+"/"+fixed_name+".nii")
+    if fname_seg_moving != '':
+        sct.run("sct_c3d "+fname_moving+" -o "+path_tmp+"/"+moving_seg_name+".nii")
+    if fname_seg_fixed != '':
+        sct.run("sct_c3d "+fname_moving+" -o "+path_tmp+"/"+fixed_seg_name+".nii")
+
+    # go to tmp folder
+    os.chdir(path_tmp)
+
+    # denoising the fixed image using non-local means from dipy
+    # file = nibabel.load(fixed_name+".nii")
+    # data = file.get_data()
+    # hdr = file.get_header()
+    # fixed_name_temp = fixed_name+"_denoised"
+    # data_denoised = nlmeans(data,3)
+    # fixed_name = fixed_name_temp
+    # hdr.set_data_dtype('uint32') # set imagetype to uint32
+    # img = nibabel.Nifti1Image(data_denoised, None, hdr)
+    # nibabel.save(img, fixed_name+".nii.gz")
+
+    # cropping in x & y directions
+
+
+    # padding the images
+    moving_name_temp = moving_name+"_pad"
+    fixed_name_temp = fixed_name+"_pad"
+    sct.run("sct_c3d "+moving_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+moving_name_temp+".nii")
+    sct.run("sct_c3d "+fixed_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+fixed_name_temp+".nii")
+    moving_name = moving_name_temp
+    fixed_name = fixed_name_temp
+    if fname_seg_moving != '':
+        moving_seg_name_temp = moving_seg_name+"_pad"
+        sct.run("sct_c3d "+moving_seg_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+moving_seg_name_temp+".nii")
+        moving_seg_name = moving_seg_name_temp
+    if fname_seg_fixed != '':
+        fixed_seg_name_temp = fixed_seg_name+"_pad"
+        sct.run("sct_c3d "+fixed_seg_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+fixed_seg_name_temp+".nii")
+        fixed_seg_name = fixed_seg_name_temp
+
+    # binarise the moving image
+    # moving_name_temp_bin = moving_name_temp + "_bin"
+    # cmd = 'fslmaths ' + moving_name_temp + '.nii -thr 0.25 ' + moving_name_temp_bin + '.nii'
+    # sct.run(cmd, 1)
+    #
+    # cmd = 'fslmaths ' + moving_name_temp_bin + '.nii -bin ' + moving_name_temp_bin + '.nii'
+    # sct.run(cmd, 1)
+    #
+    # moving_name = moving_name_temp_bin
+
+
+    # register template to anat file: this generate warp_template2anat.nii
+    # cmd = "sct_register_to_template -i " + fname_anat + " -l " + fname_landmarks + " -m " + fname_seg + " -s normal"
+    # sct.run(cmd)
+    # warp_template2anat = ""
+
+    # # register anat file to t2star: generate  warp_anat2t2star
+    # cmd = "sct_register_multimodal -i " + fname_anat + " -d " + fname_t2star
+    # sct.run(cmd)
+    # warp_anat2t2star = ""
+
+    # # concatenation of the two warping fields
+    # warp_template2t2star = "warp_template2t2star.nii.gz"
+    # cmd = "sct_concat_transfo -w " + warp_template2anat + ',' + warp_anat2t2star + " -d " + fname_t2star + " -o " + warp_template2t2star
+    # sct.run(cmd)
+
+    # # apply the concatenated warping field to the template
+    # cmd = "sct_warp_template -d " + fname_t2star + " -w " + warp_template2anat + " -s 1 -o template_in_t2star_space"
+    # sct.run(cmd)
+
+
+    #sct_register_to_template --> warp_template2anat
+    # register anat file to t2star
+    #sct_register_multimodal --> warp_anat2t2star
+    # concatenate warp_template2anat with warp_anat2t2star
+    #--> warp_template2t2star
+
+
+
+    # registration of the grey matter
+    print('\nDeforming the image...')
+    moving_name_temp = moving_name+"_deformed"
+    cmd = "sct_antsRegistration --dimensionality 3 --transform "+ transformation +"["+gradient_step+",3,0] --metric CC["+fixed_name+".nii,"+moving_name+".nii,1,"+radius+"] --convergence 20x15 --shrink-factors 2x1 --smoothing-sigmas 0mm --Restrict-Deformation 1x1x0 --output ["+moving_name_temp+","+moving_name_temp+".nii]"
+    if fname_seg_moving != '':
+        cmd += " --masks["+fixed_seg_name+".nii,"+moving_seg_name+".nii]"
+    sct.run(cmd)
+    moving_name = moving_name_temp
+
+    moving_name_temp = file_output+ext_output
+    sct.run("sct_crop_image -i "+moving_name+".nii -dim 2 -start "+padding+" -end -"+padding+" -o "+file_output+ext_output)
+    moving_name = moving_name_temp
+
+    # move output files to initial folder
+    sct.run("cp "+file_output+"* ../")
+
+    # remove temporary file
+    if remove_temp == 1:
+        os.chdir('../')
+        print('\nRemove temporary files...')
+        sct.run("rm -rf "+path_tmp)
+
+    return
+
+
+#=======================================================================================================================
+# usage
+#=======================================================================================================================
+def usage():
+    print '\n' \
+        ''+os.path.basename(__file__)+'\n' \
+        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
+        'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n' \
+        '\n'\
+        'DESCRIPTION\n' \
+        '  This function returns the grey matter segmentation.\n' \
+        '\n'\
+        'USAGE\n' \
+        '  '+os.path.basename(__file__)+' -i <ref> -d <moving> -o <output>\n' \
+        '  '+os.path.basename(__file__)+' -i <anat volume> -t <t2star volume> -l <landmarks>\n' \
+        '\n'\
+        'MANDATORY ARGUMENTS\n' \
+        '  -i                   input image (with white/gray matter contrast).\n' \
+        '  -d                   moving image.\n' \
+        '  -o                   output name (for warping field and image).\n' \
+        'OPTIONAL ARGUMENTS\n' \
+        '  -t                   transformation {SyN, BSplineSyN}.\n' \
+        '  -s                   input image segmentation.\n' \
+        '  -g                   moving image segmentation.\n'
+    sys.exit(2)
+
+
+
+if __name__ == "__main__":
+    # initialize parameters
+    # param = Param()
+    # call main function
+    main()
+
+
