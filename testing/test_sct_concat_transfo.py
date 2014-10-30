6b85d90d56f82af04b005a55db226d976a1d5a05
Augustin Roux <augustin.roux@gmail.com>
2014-10-30 17:52:44 -0400

    TEST: missing test added
    Former-commit-id: 932e9087cea0c789670a3538104551bb73a147cb [formerly 17912df928dcdfa8c99a22342443ba77a99d6e1b]
    Former-commit-id: 77ec80b74df0b4d8c75852a1796df255f48841d8
    Former-commit-id: 362b1a60204ac8bbfe2d6a436cb34994df562039

--- testing/test_sct_concat_transfo.py
+++ testing/test_sct_concat_transfo.py
@@ -1,1 +1,36 @@
-__author__ = 'aroux'
+#!/usr/bin/env python
+#########################################################################################
+#
+# Test function sct_concat_transfo
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Augustin Roux
+# modified: 2014/10/30
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+
+#import sct_utils as sct
+import commands
+
+
+def test(data_path):
+
+    # parameters
+    folder_data = ['t2/', 'mt/', 'template/']
+    file_data = ['warp_template2anat.nii.gz', 'warp_template2mt.nii.gz', 'MNI-Poly-AMU_T2.nii.gz']
+
+    # define command
+    cmd = 'sct_concat_transfo -w ' + data_path + folder_data[0] + file_data[0] + ',' \
+          + data_path + folder_data[1] + file_data[1]\
+          + ' -d ' + data_path + folder_data[2] + file_data[2]
+
+    # return
+    #return sct.run(cmd, 0)
+    return commands.getstatusoutput(cmd)
+
+
+if __name__ == "__main__":
+    # call main function
+    test()
