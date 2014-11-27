6f41a4cca0b95e3eb2c20416a92fccfa0bcbd195
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-11-27 16:06:52 -0500

    DEV: integrated crop_atlas into validate_atlas batch for more clarity
    Former-commit-id: 7486379721a0de659f75144161e5353a4334e6dd [formerly 35ea16463c80d9bcb551b10a2a9b982d505d1253]
    Former-commit-id: 1f0d46f676341ecdb2d488e2bfbfb494080e39c4
    Former-commit-id: ab1e80037fee269a14ed5beee14f1fb120da4de6

--- dev/atlas/WM_validation/add_tracts_dc.py
+++ dev/atlas/WM_validation/add_tracts_dc.py
@@ -6,9 +6,9 @@ from generate_phantom import get_tracts, save_3D_nparray_niftii
 from numpy import zeros,empty
 
 # To get the dorsal colum, tracts_to_sum_index = 0,1,15,16
-tracts_to_sum_index = 0,1,15,16
-folder_atlas = "cropped_atlas"
-tracts_sum_img = 'DICE_coefficient/dorsal_column.nii.gz'
+tracts_to_sum_index = 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29# 0,1,15,16
+folder_atlas = "/Users/julien/code/spinalcordtoolbox/dev/atlas/WM_atlas_generation/WMtracts_outputs/final_results" #"cropped_atlas"
+tracts_sum_img = "WMtract__all.nii.gz" #'DICE_coefficient/dorsal_column.nii.gz'
 def main():
     
     # Extract the tracts from the atlas' folder
