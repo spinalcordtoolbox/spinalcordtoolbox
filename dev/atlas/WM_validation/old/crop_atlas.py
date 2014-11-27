ab1e80037fee269a14ed5beee14f1fb120da4de6
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-11-27 16:06:52 -0500

    DEV: integrated crop_atlas into validate_atlas batch for more clarity
    Former-commit-id: 35ea16463c80d9bcb551b10a2a9b982d505d1253
    Former-commit-id: 1f0d46f676341ecdb2d488e2bfbfb494080e39c4

--- dev/atlas/WM_validation/old/crop_atlas.py
+++ dev/atlas/WM_validation/old/crop_atlas.py
@@ -19,6 +19,7 @@ folder_atlas = '/home/django/cnaaman/data/data_marc/WMtracts_outputsc_julien/fin
 folder_out = '/home/django/cnaaman/code/stage/cropped_atlas/'
 verbose = 1
 zind = 10,110,210,310,410
+
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'f:o:z:') # define flag
 except getopt.GetoptError as err: # check if the arguments are defined
@@ -43,24 +44,7 @@ def crop_file(fname_data, folder_out):
         sct.run('fslroi '+fname_data+' z'+str(zind.index(i))+'_'+file_list+' 0 -1 0 -1 '+str(i)+' 1')
         cmd = cmd+' z'+str(zind.index(i))+'_'+file_list
     sct.run(cmd)
-#    sct.run('fslroi '+fname_data+' z1_'+file_list+' 0 -1 0 -1 10 1')
-#    sct.run('fslroi '+fname_data+' z2_'+file_list+' 0 -1 0 -1 110 1')
-#    sct.run('fslroi '+fname_data+' z3_'+file_list+' 0 -1 0 -1 210 1')
-#    sct.run('fslroi '+fname_data+' z4_'+file_list+' 0 -1 0 -1 310 1')
-#    sct.run('fslroi '+fname_data+' z5_'+file_list+' 0 -1 0 -1 410 1')
-#    sct.run('fslmerge -z '+folder_out+file_list+' '+'z1_'+file_list+' z2_'+file_list+' z3_'+file_list+' z4_'+file_list+' z5_'+file_list)
-    # sct.run('fslroi '+fname_data+' z1_'+file_list+' 0 -1 0 -1 10 1')
-    #  sct.run('fslroi '+fname_data+' z2_'+file_list+' 0 -1 0 -1 60 1')
-    #  sct.run('fslroi '+fname_data+' z3_'+file_list+' 0 -1 0 -1 110 1')
-    #  sct.run('fslroi '+fname_data+' z4_'+file_list+' 0 -1 0 -1 160 1')
-    #  sct.run('fslroi '+fname_data+' z5_'+file_list+' 0 -1 0 -1 210 1')
-    #  sct.run('fslroi '+fname_data+' z6_'+file_list+' 0 -1 0 -1 260 1')
-    #  sct.run('fslroi '+fname_data+' z7_'+file_list+' 0 -1 0 -1 310 1')
-    #  sct.run('fslroi '+fname_data+' z8_'+file_list+' 0 -1 0 -1 360 1')
-    #  sct.run('fslroi '+fname_data+' z9_'+file_list+' 0 -1 0 -1 410 1')
-    #  sct.run('fslroi '+fname_data+' z10_'+file_list+' 0 -1 0 -1 460 1')
-    # sct.run('fslmerge -z '+folder_out+file_list+' '+'z1_'+file_list+' z2_'+file_list+' z3_'+file_list+' z4_'+file_list+' z5_'+file_list+' z6_'+file_list+' z7_'+file_list+' z8_'+file_list+' z9_'+file_list+' z10_'+file_list)
-    
+
 # create output folder
 if os.path.exists(folder_out):
     sct.printv('WARNING: Output folder already exists. Deleting it...', verbose)
