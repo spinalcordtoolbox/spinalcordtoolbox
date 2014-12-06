222c376b92be61b80559e84b774667cca2929158
Julien Cohen-Adad <jcohen@polymtl.ca>
2014-12-06 15:51:53 -0500

    DEV: small modifs to atlas
    Former-commit-id: 8cac1a4f59c841ac82d8a84eb80a297f125578c7 [formerly 4104255c45dd15f23f8b0786381df40a723e45e7] [formerly 4ac75aeeed105bbc65fda9b02806d5d3ce235a69 [formerly 65871c6d50bd16ae4a3eced474e71063639b448f [formerly 95b7b01e1bfa1c52f96a319a4e63a254dc06b7a2]]]
    Former-commit-id: 8cac1a4f59c841ac82d8a84eb80a297f125578c7 [formerly 4104255c45dd15f23f8b0786381df40a723e45e7]
    Former-commit-id: 27fffc5d384c7ab28e683511723b270ab48a3ebb [formerly 4ded2c49f9bb4c78a46ecdffcac94c7337301c93 [formerly 9892d5b5594a835c4c2040417a6fddcc076232bf]]
    Former-commit-id: ffb6b03a47c994367880dd446b60bdf6647dd1bf
    Former-commit-id: e602ae04eeeb522f25fd1ca923dd1949f841a4f7

--- dev/atlas/WM_validation/validate_atlas.py
+++ dev/atlas/WM_validation/validate_atlas.py
@@ -38,7 +38,7 @@ from generate_phantom import phantom_generation, get_tracts, save_3D_nparray_nif
 
 def main():
     # Parameters
-    bootstrap_iter = 5
+    bootstrap_iter = 50
     folder_atlas = '../WM_atlas_generation/WMtracts_outputs/final_results/'  # path to atlas. add / at the end
     user_tract = 'charles_tract_,julien_tract_,tanguy_tract_,simon_tract_'
     std_noise_list = [0, 5, 10, 20, 50]  # standard deviation of the noise added to the generated phantom
@@ -66,7 +66,7 @@ def main():
 def validate_atlas(folder_atlas, bootstrap_iterations, std_noise, range_tract, results_file, user_tract):
     # Parameters
     folder_cropped_atlas = "cropped_atlas/"
-    crop = 0  # crop atlas, default=1. Only need to do it once (saves time).
+    crop = 1  # crop atlas, default=1. Only need to do it once (saves time).
     zcrop_ind = [10, 110, 210, 310, 410]
     file_phantom = "WM_phantom.nii.gz"
     file_phantom_noise = "WM_phantom_noise.nii.gz"
