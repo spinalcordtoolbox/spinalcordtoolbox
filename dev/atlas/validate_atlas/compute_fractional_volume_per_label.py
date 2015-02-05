6c84e7c7b3ecea681e905356e0791a3b3841f4ce
Simon Lévy <simon.levy@mines-ales.org>
2015-02-04 20:05:10 -0500

    DEV: almost completed
    Former-commit-id: 2abbe9f7e9171caa615c73edbfa6ad156aebe143 [formerly dafb20a8a700eb2d062b9ec3c4a11305f8c21442] [formerly 9ca3edb471d82301ebc78553adf31145235508e8 [formerly aa39c87e90ee8ce23c138a5fd3842cd3367b99d0 [formerly 45a278131877994d0d8c96274fc0516ddec205ec]]]
    Former-commit-id: 2abbe9f7e9171caa615c73edbfa6ad156aebe143 [formerly dafb20a8a700eb2d062b9ec3c4a11305f8c21442]
    Former-commit-id: 4836f311d8bb3ccab60daff23de1065e5cc9396e [formerly 0125206bf2248bd368f93fa7afe95a65c68297ca [formerly d6ec33e9dba3d3b34ec7fea93dc9a9d2d5c18aec]]
    Former-commit-id: 90dca2c6daefad9ab8b4122f62437334f4193388
    Former-commit-id: 2e7a8c9b31dd6722e1efee2d377e340bee0151b7

--- dev/atlas/validate_atlas/compute_fractional_volume_per_label.py
+++ dev/atlas/validate_atlas/compute_fractional_volume_per_label.py
@@ -5,13 +5,29 @@ path_sct = '/Users/slevy_local/spinalcordtoolbox' #'C:/cygwin64/home/Simon_2/spi
 # Append path that contains scripts, to be able to load modules
 sys.path.append(path_sct + '/scripts')
 import sct_extract_metric
+import numpy
+import nibabel
 
-atlas_folder = '/Users/slevy_local/spinalcordtoolbox/data/atlas/'
-
-global param
-param = sct_extract_metric.Param()
+atlas_folder = '/Users/slevy_local/spinalcordtoolbox/dev/atlas/validate_atlas/cropped_atlas/'
+file_label = 'info_label.txt'
 
 
-[label_id, label_name, label_file] = sct_extract_metric.read_info_label(atlas_folder)
+[label_id, label_name, label_file] = sct_extract_metric.read_label_file(atlas_folder, file_label)
+nb_label = len(label_file)
+
+fract_volume_per_lab = numpy.zeros((nb_label))
+
+# compute fractional volume for each label
+# labels = numpy.empty([nb_label], dtype=object)  # labels(nb_label, x, y, z)
+for i_label in range(0, nb_label):
+    fract_volume_per_lab[i_label] = numpy.sum(nibabel.load(atlas_folder + label_file[i_label]).get_data())
+
+
+print 'Labels\'name:'
+print label_name
+print '\nCorresponding fractional volume:'
+print fract_volume_per_lab
+
+
 
 
