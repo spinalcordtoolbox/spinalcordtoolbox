4d62e8ac115173a5d1ff8a446f01a865d351fa00
Simon Lévy <simon.levy@mines-ales.org>
2015-02-11 13:15:19 -0500

    OPT: graphic improvements (notably alternated vertical greay spans)
    Former-commit-id: 2719f508fc7e5a430ba13116a8263e53ccffbc24 [formerly b9b8d3c3d85312bda73e68b635c85d772b84418f] [formerly 5d6d67aad1acfabdca40d46fe83c539aa7a45aa8 [formerly 1d094fbbc162ee25adab66df6d20436d775d8830 [formerly 69100f7fa3e868ecfb7789b7fdca3eadd4d36570]]]
    Former-commit-id: 2719f508fc7e5a430ba13116a8263e53ccffbc24 [formerly b9b8d3c3d85312bda73e68b635c85d772b84418f]
    Former-commit-id: cc5d28a8b1caf4d3916ed3f7f1a54740e6dce26c [formerly 18ba65db17ca041da93636f58b6d1e728d289ff7 [formerly d2d3eacbc01c14718c0b33e7c5fb2ce9933b200b]]
    Former-commit-id: 275fc737ad5cde70b45145518bc6ef337ad6c936
    Former-commit-id: fe619ad62054b37f20d11ea779e590d837a0c385

--- dev/atlas/validate_atlas/plot_auto_vs_manual.py
+++ dev/atlas/validate_atlas/plot_auto_vs_manual.py
@@ -15,6 +15,7 @@ import commands
 import sys
 import numpy
 import re
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.legend_handler import *
 # import subprocess
@@ -254,13 +255,15 @@ def main():
     # find index of the file generated with sigma noise = 10 and range tracts = -10:+10
     ind_file_noise10_tracts_std10 = numpy.where((snr == 10) & (tracts_std == 10))[0][0]
 
+    matplotlib.rcParams.update({'font.size': 22, 'font.family': 'trebuchet'})
+
     fig0 = plt.figure(0, figsize=(20, 10))
     width = 0.5/(nb_method+1)
     ind_fig0 = numpy.arange(len(labels_id[0]))
-    plt.ylabel('Relative error (%)', fontsize=18)
-    plt.xlabel('Labels', fontsize=18)
-    plt.suptitle('Automatic estimation vs. manual estimation', fontsize=20)
-    plt.title('(Noise std='+str(snr[0])+', Tracts std='+str(tracts_std[0])+', CSF value='+str(csf_values[0])+')\n', fontsize=18)
+    plt.ylabel('Absolute error (%)', fontsize=26)
+    plt.xlabel('Labels', fontsize=26)
+    plt.suptitle('Automatic estimation vs. manual estimation', fontsize=30)
+    plt.title('(Noise std='+str(snr[0])+', Tracts std='+str(tracts_std[0])+', CSF value='+str(csf_values[0])+')\n', fontsize=28)
 
 
     # colors = plt.get_cmap('jet')(np.linspace(0, 1.0, nb_method))
@@ -273,13 +276,21 @@ def main():
         plot = plt.errorbar(ind_fig0+i_meth_to_display*width+(float(i_meth_to_display)*width)/(nb_method+1), error_per_label[ind_file_noise10_tracts_std10, :, i_meth], std_per_label[ind_file_noise10_tracts_std10, :, i_meth], color=color, linestyle='None', marker='o')
         plots.append(plot[0])
 
+    # add alternated vertical background colored bars
+    for i_xtick in range(0, len(ind_fig0), 2):
+        plt.axvspan(ind_fig0[i_xtick] - 3*width, ind_fig0[i_xtick] + (nb_method + 1) * width + 2*width, facecolor='grey', alpha=0.3)
+
     # plt.legend(plots, methods_to_display, bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0., handler_map={Line2D: HandlerLine2D(numpoints=1)})
     plt.legend(plots, methods_to_display, loc='best', handler_map={Line2D: HandlerLine2D(numpoints=1)})
-    plt.xticks(ind_fig0+(numpy.floor(nb_method/2))*width*(1.0+1.0/(nb_method+1)), labels_id[0], fontsize=16)
+    plt.xticks(ind_fig0 + (numpy.floor(nb_method/2))*width*(1.0+1.0/(nb_method+1)), labels_id[0], fontsize=22)
     plt.gca().set_xlim([-width, numpy.max(ind_fig0)+(nb_method+0.5)*width])
     # plt.gca().set_ylim([0, 2])
-    plt.grid(b=True, axis='y')
     plt.gca().yaxis.set_major_locator(plt.MultipleLocator(2.5))
+    plt.gca().yaxis.set_minor_locator(plt.MultipleLocator(0.5))
+    plt.grid(b=True, axis='y', which='both')
+    # adjust the size of the frame
+    plt.subplots_adjust(bottom=0.15, top=0.86, right=0.7, left=0.2)
+
 
     plt.savefig(results_folder+'/automatic_method_vs_manual_methods')
 
