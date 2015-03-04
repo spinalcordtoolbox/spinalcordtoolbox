a7d5e415efc83e470bcea2515df3efd0bf9ea483
Benjamin De Leener <jackbdl@gmail.com>
2015-03-04 11:48:18 -0500

    REF: added all options into warper to PropSeg
    Former-commit-id: 8be970930379484b501676e9b0a54005a9e93828 [formerly 0b670ac2b1dd72b1cb2e63822ad8849a929ffbae]
    Former-commit-id: a1c61d77340192a0aaf6d7dd96d27d8a806bddfd
    Former-commit-id: a9a92197866f03c8e2f49ee089b317a945474827

--- scripts/sct_propseg.py
+++ scripts/sct_propseg.py
@@ -24,23 +24,127 @@ if __name__ == "__main__":
                       description="input image.",
                       mandatory=True,
                       example="t2.nii.gz")
+    parser.add_option(name="-t",
+                      type_value="multiple_choice",
+                      description="type of image contrast, t2: cord dark / CSF bright ; t1: cord bright / CSF dark",
+                      mandatory=True,
+                      example=['t1','t2'])
+    parser.usage.addSection("General options")
     parser.add_option(name="-o",
                       type_value="folder_creation",
                       description="output folder.",
                       mandatory=False,
                       example="My_Output_Folder/",
                       default_value="./")
-    parser.add_option(name="-t",
-                      type_value="multiple_choice",
-                      description="type of image contrast, t2: cord dark / CSF bright ; t1: cord bright / CSF dark",
-                      mandatory=True,
-                      example=['t1','t2'])
+    parser.add_option(name="-down",
+                      type_value="int",
+                      description="down limit of the propagation, default is 0",
+                      mandatory=False,
+                      default_value="0")
+    parser.add_option(name="-up",
+                      type_value="int",
+                      description="up limit of the propagation, default is the highest slice of the image",
+                      mandatory=False,
+                      default_value="0")
     parser.add_option(name="-v",
                       type_value="multiple_choice",
                       description="1: display on, 0: display off (default)",
                       mandatory=False,
                       example=["0","1"],
                       default_value="0")
+    parser.add_option(name="-h",
+                      type_value=None,
+                      description="display this help",
+                      mandatory=False)
+
+    parser.usage.addSection("\nOutput options")
+    parser.add_option(name="-mesh",
+                      type_value=None,
+                      description="output: mesh of the spinal cord segmentation",
+                      mandatory=False)
+    parser.add_option(name="-centerline-binary",
+                      type_value=None,
+                      description="output: centerline as a binary image ",
+                      mandatory=False)
+    parser.add_option(name="-CSF",
+                      type_value=None,
+                      description="output: CSF segmentation",
+                      mandatory=False)
+    parser.add_option(name="-centerline-coord",
+                      type_value=None,
+                      description="output: centerline in world coordinates",
+                      mandatory=False)
+    parser.add_option(name="-cross",
+                      type_value=None,
+                      description="output: cross-sectional areas",
+                      mandatory=False)
+    parser.add_option(name="-init-tube",
+                      type_value=None,
+                      description="output: initial tubular meshes ",
+                      mandatory=False)
+    parser.add_option(name="-low-resolution-mesh",
+                      type_value=None,
+                      description="output: low-resolution mesh",
+                      mandatory=False)
+    parser.add_option(name="-detect-nii",
+                      type_value=None,
+                      description="output: spinal cord detection as a nifti image",
+                      mandatory=False)
+    parser.add_option(name="-detect-png",
+                      type_value=None,
+                      description="output: spinal cord detection as a PNG image",
+                      mandatory=False)
+
+    parser.usage.addSection("\nOptions helping the segmentation")
+    parser.add_option(name="-init-centerline",
+                      type_value="file",
+                      description="filename of centerline to use for the propagation, format .txt or .nii, see file structure in documentation",
+                      mandatory=False)
+    parser.add_option(name="-init",
+                      type_value="int",
+                      description="axial slice where the propagation starts, default is middle axial slice",
+                      mandatory=False)
+    parser.add_option(name="-init-mask",
+                      type_value="file",
+                      description="mask containing three center of the spinal cord, used to initiate the propagation",
+                      mandatory=False)
+    parser.add_option(name="-radius",
+                      type_value="float",
+                      description="approximate radius of the spinal cord, default is 4 mm",
+                      mandatory=False)
+    parser.add_option(name="-detect-n",
+                      type_value="int",
+                      description="number of axial slices computed in the detection process, default is 4",
+                      mandatory=False)
+    parser.add_option(name="-detect-gap",
+                      type_value="int",
+                      description="gap in Z direction for the detection process, default is 4",
+                      mandatory=False)
+    parser.add_option(name="-init-validation",
+                      type_value=None,
+                      description="enable validation on spinal cord detection based on discriminant analysis",
+                      mandatory=False)
+    parser.add_option(name="-nbiter",
+                      type_value="int",
+                      description="stop condition: number of iteration for the propagation for both direction, default is 200",
+                      mandatory=False)
+    parser.add_option(name="-max-area",
+                      type_value="float",
+                      description="[mm^2], stop condition: maximum cross-sectional area, default is 120 mm^2",
+                      mandatory=False)
+    parser.add_option(name="-max-deformation",
+                      type_value="float",
+                      description="[mm], stop condition: maximum deformation per iteration, default is 2.5 mm",
+                      mandatory=False)
+    parser.add_option(name="-min-contrast",
+                      type_value="float",
+                      description="[intensity value], stop condition: minimum local SC/CSF contrast, default is 50",
+                      mandatory=False)
+    parser.add_option(name="-d",
+                      type_value="float",
+                      description="trade-off between distance of most promising point and feature strength, default depend on the contrast. Range of values from 0 to 50. 15-25 values show good results.",
+                      mandatory=False)
+
     arguments = parser.parse(sys.argv[1:])
 
     input_filename = arguments["-i"]
@@ -54,14 +158,66 @@ if __name__ == "__main__":
         folder_output = arguments["-o"]
         cmd += " -o " + folder_output
 
+    if "-down" in arguments:
+        cmd += " -down " + str(arguments["-down"])
+    if "-up" in arguments:
+        cmd += " -up " + str(arguments["-up"])
+
+    verbose = 0
     if "-v" in arguments:
-        verbose = arguments["-v"]
-        if verbose is "1":
+        if arguments["-v"] is "1":
+            verbose = 1
             cmd += " -verbose"
 
-    sct.runProcess(cmd, 1)
+    # Output options
+    if "-mesh" in arguments:
+        cmd += " -mesh"
+    if "-centerline-binary" in arguments:
+        cmd += " -centerline-binary"
+    if "-CSF" in arguments:
+        cmd += " -CSF"
+    if "-centerline-coord" in arguments:
+        cmd += " -centerline-coord"
+    if "-cross" in arguments:
+        cmd += " -cross"
+    if "-init-tube" in arguments:
+        cmd += " -init-tube"
+    if "-low-resolution-mesh" in arguments:
+        cmd += " -low-resolution-mesh"
+    if "-detect-nii" in arguments:
+        cmd += " -detect-nii"
+    if "-detect-png" in arguments:
+        cmd += " -detect-png"
 
-    sct.printv("Done!",1,"normal")
+    # Helping options
+    if "-init-centerline" in arguments:
+        cmd += " -init-centerline " + str(arguments["-init-centerline"])
+    if "-init" in arguments:
+        cmd += " -init " + str(arguments["-init"])
+    if "-init-mask" in arguments:
+        cmd += " -init-mask " + str(arguments["-init-mask"])
+    if "-radius" in arguments:
+        cmd += " -radius " + str(arguments["-radius"])
+    if "-detect-n" in arguments:
+        cmd += " -detect-n " + str(arguments["-detect-n"])
+    if "-detect-gap" in arguments:
+        cmd += " -detect-gap " + str(arguments["-detect-gap"])
+    if "-init-validation" in arguments:
+        cmd += " -init-validation"
+    if "-nbiter" in arguments:
+        cmd += " -nbiter " + str(arguments["-nbiter"])
+    if "-max-area" in arguments:
+        cmd += " -max-area " + str(arguments["-max-area"])
+    if "-max-deformation" in arguments:
+        cmd += " -max-deformation " + str(arguments["-max-deformation"])
+    if "-min-contrast" in arguments:
+        cmd += " -min-contrast " + str(arguments["-min-contrast"])
+    if "-d" in arguments:
+        cmd += " -d " + str(arguments["-d"])
+
+    sct.runProcess(cmd, verbose)
+
+    sct.printv("\nDone!",1,"normal")
     sct.printv("Type the following command in the terminal to see the results:", 1, "normal")
 
     # extracting output filename
