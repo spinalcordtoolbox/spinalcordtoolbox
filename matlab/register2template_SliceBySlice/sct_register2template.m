d3b96e8ea8e11d86ca3fb16c2d1632e03f79610f
Tanguy Duval <duvaltanguy@gmail.com>
2015-03-06 16:20:51 -0500

    Matlab SpinalCordDetection

--- matlab/register2template_SliceBySlice/sct_register2template.m
+++ matlab/register2template_SliceBySlice/sct_register2template.m
@@ -21,7 +21,7 @@ warp_transfo = 1;
 %--------------------------------------------------------------------------
 
 %-----------------------------REFERENCE (DESTINATION)------------------------------------
-ref_fname = '/Volumes/users_hd2/tanguy/data/Boston/2014-07/Connectome/template/diffusion_template.nii.gz';%'/home/django/tanguy/matlab/spinalcordtoolbox/data/template/MNI-Poly-AMU_WM.nii.gz';
+ref_fname = '/home/taduv/data/Boston/2014-07/Connectome/template/diffusion_template.nii.gz';%'/home/django/tanguy/matlab/spinalcordtoolbox/data/template/MNI-Poly-AMU_WM.nii.gz';
 levels_fname='/Volumes/hd2_local/users_local/tanguy/spinalcordtoolbox/data/template/MNI-Poly-AMU_level.nii.gz';
 %--------------------------------------------------------------------------
 
