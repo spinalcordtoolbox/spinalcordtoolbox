6894fa4b4a63c8555379c969a88cda99ff5b5ad0
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-03-30 17:59:50 -0400

    OPT: sct_register_multimodal now using parser and more intuitive flags
    Former-commit-id: 566727ed0fa85255f9eca8f121bb4fca70a42ccd
    Former-commit-id: bcc9f179e9acc8e757d0d40b8233cc7699a4c549

--- scripts/sct_register_multimodal.py
+++ scripts/sct_register_multimodal.py
@@ -34,35 +34,39 @@
 # - two-step registration, using only segmentation-based registration
 
 
+import sys
+#import getopt
+import os
+import commands
+import time
+import sct_utils as sct
+from msct_parser import Parser
+
+
 # DEFAULT PARAMETERS
 class Param:
     ## The constructor
     def __init__(self):
         self.debug = 0
-        self.remove_temp_files = 1  # remove temporary files
         self.outSuffix  = "_reg"
-        self.padding = 5  # add 'padding' slices at the top and bottom of the volumes if deformation at the edge is not good. Default=5. Put 0 for no padding.
-        self.algo = "syn"  # algo for registration
-        self.param_syn = ['10',  # number of iterations. For several stages, add x. E.g.: 50x10
-                      '2',  # shrink factor. For several stages, add x.
-                      '0',  # smoothing factor. For several stages, add x.
-                      '0.5',  # gradientStep
-                      'MI']  # metric: CC,MI,MeanSquares
-        self.param_slicereg = ['10',  # number of iterations. For several stages, add x. E.g.: 50x10
-                      '3',  # degree of polynomial.
-                      'MeanSquares']  # metric: CC,MI,MeanSquares
-        self.verbose = 1  # verbose
-        self.interp = 'linear'  # nn, linear, spline
 
 
-import sys
-import getopt
-import os
-import commands
-import time
-
-import sct_utils as sct
-
+# Parameters for registration
+class Paramreg:
+    def __init__(self):
+        self.algo = 'syn'
+        self.metric = 'MI'
+        self.iter = '10'
+        self.shrink = '2'
+        self.smooth = '0'
+        self.poly = '3'  # slicereg only
+        self.gradStep = '0.5'
+    # update constructor with user's parameters
+    def update(self, paramreg_user):
+        list_objects = paramreg_user.split(',')
+        for object in list_objects:
+            obj = object.split('=')
+            setattr(self, obj[0], obj[1])
 
 
 # MAIN
@@ -70,27 +74,95 @@ import sct_utils as sct
 def main():
 
     # Initialization
-    fname_src = ''
-    fname_dest = ''
-    fname_src_seg = ''
-    fname_dest_seg = ''
+    # fname_src = ''
+    # fname_dest = ''
     fname_output = ''
     fname_mask = ''
-    padding = param.padding
-    param_user = ''
-    # algo_first = 'SliceReg'
-    # numberIterations = param.numberIterations
-    remove_temp_files = param.remove_temp_files
-    verbose = param.verbose
-    use_segmentation = 0  # use spinal cord segmentation to improve robustness
-    use_init_transfo = ''
-    # gradientStep = param.gradientStep
-    algo = param.algo
-    start_time = time.time()
-    print ''
+    padding = 5
+    parameters = 'algo=syn,metric=MI,iter=10,shrink=2,smooth=0,grad=0.5'
+    remove_temp_files = 1
+    verbose = 1
 
+    start_time = time.time()
     # get path of the toolbox
     status, path_sct = commands.getstatusoutput('echo $SCT_DIR')
+
+    # Initialize the parser
+    parser = Parser(__file__)
+    parser.usage.set_description('This program co-registers two 3D volumes. The deformation is non-rigid and is '
+                                 'constrained along Z direction (i.e., axial plane). Hence, this function assumes '
+                                 'that orientation of the destination image is axial (RPI). If you need to register '
+                                 'two volumes with large deformations and/or different contrasts, it is recommended to '
+                                 'input spinal cord segmentations (binary mask) in order to achieve maximum robustness.'
+                                 ' The program outputs a warping field that can be used to register other images to the'
+                                 ' destination image. To apply the warping field to another image, use '
+                                 'sct_apply_transfo')
+    parser.add_option(name="-i",
+                      type_value="file",
+                      description="Source image.",
+                      mandatory=True,
+                      example="data_src.nii.gz")
+    parser.add_option(name="-d",
+                      type_value="file",
+                      description="Destination image.",
+                      mandatory=True,
+                      example="data_dest.nii.gz")
+    parser.add_option(name="-m",
+                      type_value="file",
+                      description="Binary mask to improve robustness.",
+                      mandatory=False,
+                      example="mask.nii.gz")
+    parser.add_option(name="-o",
+                      type_value="file",
+                      description="Name of output file.",
+                      mandatory=False,
+                      example="src_reg.nii.gz")
+    parser.add_option(name="-p",
+                      type_value="str",
+                      description="""parameters for registration. If you have several steps, separate with ":" """,
+                      mandatory=False,
+                      default_value="algo=syn,metric=MI,iter=5,shrink=2,smooth=0,grad=0.5",
+                      example="algo=syn,metric=MI,iter=5,shrink=2,smooth=0,grad=0.5")
+    parser.add_option(name="-z",
+                      type_value="int",
+                      description="""size of z-padding to enable deformation at edges when using SyN.""",
+                      mandatory=False,
+                      example="5")
+    parser.add_option(name="-x",
+                      type_value="multiple_choice",
+                      description="""Final interpolation.""",
+                      mandatory=False,
+                      default_value='linear',
+                      example=['nn', 'linear', 'spline'])
+    parser.add_option(name="-r",
+                      type_value="multiple_choice",
+                      description="""Remove temporary files.""",
+                      mandatory=False,
+                      example=['0', '1'])
+    parser.add_option(name="-v",
+                      type_value="multiple_choice",
+                      description="""Verbose.""",
+                      mandatory=False,
+                      example=['0', '1'])
+    arguments = parser.parse(sys.argv[1:])
+
+    # get arguments
+    fname_src = arguments['-i']
+    fname_dest = arguments['-d']
+    if '-o' in arguments:
+        fname_output = arguments['-o']
+    if '-m' in arguments:
+        fname_mask = arguments['-m']
+    if '-z' in arguments:
+        padding = arguments['-z']
+    if '-m' in arguments:
+        fname_mask = arguments['-m']
+    paramreg_user = arguments['-p']
+    if '-r' in arguments:
+        remove_temp_files = arguments['-r']
+    interp = arguments['-x']
+    if '-v' in arguments:
+        verbose = arguments['-v']
 
     # Parameters for debug mode
     if param.debug:
@@ -101,58 +173,13 @@ def main():
         param_user = '10,syn,0.5,MI'
         remove_temp_files = 0
         verbose = 1
-    else:
-        # Check input parameters
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], 'ha:d:i:m:o:p:r:v:x:z:')
-        except getopt.GetoptError:
-            usage()
-        if not opts:
-            usage()
-        for opt, arg in opts:
-            if opt == '-h':
-                usage()
-            elif opt in ('-a'):
-                algo = arg
-            elif opt in ("-d"):
-                fname_dest = arg
-            elif opt in ("-i"):
-                fname_src = arg
-            elif opt in ("-m"):
-                fname_mask = arg
-            elif opt in ("-o"):
-                fname_output = arg
-            elif opt in ('-p'):
-                param_user = arg
-            elif opt in ('-r'):
-                remove_temp_files = int(arg)
-            elif opt in ('-v'):
-                verbose = int(arg)
-            elif opt in ('-x'):
-                param.interp = arg
-            elif opt in ('-z'):
-                padding = arg
 
-    # display usage if a mandatory argument is not provided
-    if fname_src == '' or fname_dest == '':
-        sct.printv('ERROR in '+os.path.basename(__file__)+': All mandatory arguments are not provided. Type: '+os.path.basename(__file__)+' -h.\n', 1, 'error')
-
-    # parse argument for param
-    if algo == 'syn' or algo == 'bsplinesyn':
-        if not param_user == '':
-            param.param_syn = param_user.replace(' ', '').split(',')  # remove spaces and parse with comma
-            del param_user
-        numberIterations, shrinkFactor, smoothingSigma, gradientStep, metric = param.param_syn
-    elif algo == 'slicereg':
-        if not param_user == '':
-            param.param_slicereg = param_user.replace(' ', '').split(',')  # remove spaces and parse with comma
-            del param_user
-        numberIterations, degPoly, metric = param.param_slicereg
-    else:
-        sct.printv('ERROR: algo '+algo+' does not exist. Exit program', 1, 'error')
+    # update paramreg with user's arguments
+    paramreg = Paramreg()
+    paramreg.update(paramreg_user)
 
     # if sliceReg is used, we can't pad in the image...
-    if algo == 'slicereg':
+    if paramreg.algo == 'slicereg':
         sct.printv('WARNING: if sliceReg is used, padding should not be used. Now setting padding=0', 1, 'warning')
         padding = 0
 
@@ -162,30 +189,30 @@ def main():
     print '  Destination ......... '+fname_dest
     print '  Mask ................ '+fname_mask
     print '  Output name ......... '+fname_output
-    print '  Algorithm ........... '+algo
-    print '  Number of iterations  '+str(numberIterations)
-    print '  Metric .............. '+metric
+    print '  Algorithm ........... '+paramreg.algo
+    print '  Number of iterations  '+paramreg.iter
+    print '  Metric .............. '+paramreg.metric
     print '  Remove temp files ... '+str(remove_temp_files)
     print '  Verbose ............. '+str(verbose)
 
-    # check existence of input files
-    print '\nCheck if files exist...'
-    sct.check_file_exist(fname_src)
-    sct.check_file_exist(fname_dest)
-    if not fname_mask == '':
-        sct.check_file_exist(fname_mask)
+    # # check existence of input files
+    # print '\nCheck if files exist...'
+    # sct.check_file_exist(fname_src)
+    # sct.check_file_exist(fname_dest)
+    # if not fname_mask == '':
+    #     sct.check_file_exist(fname_mask)
 
     # Get if input is 3D
-    sct.printv('\nCheck if input data are 3D...', param.verbose)
+    sct.printv('\nCheck if input data are 3D...', verbose)
     sct.check_if_3d(fname_src)
     sct.check_if_3d(fname_dest)
 
     # check if destination data is RPI
-    sct.printv('\nCheck if destination data is RPI...', param.verbose)
+    sct.printv('\nCheck if destination data is RPI...', verbose)
     sct.check_if_rpi(fname_dest)
 
     # set metricSize
-    if metric == 'MI':
+    if paramreg.metric == 'MI':
         metricSize = '32'  # corresponds to number of bins
     else:
         metricSize = '4'  # corresponds to radius
@@ -231,28 +258,30 @@ def main():
     # Estimate transformation
     sct.printv('\nEstimate transformation (can take a couple of minutes)...', verbose)
 
-    if algo == 'slicereg':
+    if paramreg.algo == 'slicereg':
         cmd = ('sct_antsSliceRegularizedRegistration '
                '-t Translation[0.5] '
-               '-m '+metric+'[dest_pad.nii,src_regAffine.nii,1,'+metricSize+',Regular,0.2] '
-               '-p '+degPoly+' '
-               '-i '+numberIterations+' '
+               '-m '+paramreg.metric+'[dest_pad.nii,src_regAffine.nii,1,'+metricSize+',Regular,0.2] '
+               '-p '+paramreg.poly+' '
+               '-i '+paramreg.iter+' '
                '-f 1 '
                '-s 0 '
                '-o [stage10,src_regAffineWarp.nii] '  # here the warp name is stage10 because antsSliceReg add "Warp"
                +masking)
-    else:
+    elif paramreg.algo == 'syn' or paramreg.algo == 'bsplinesyn':
         cmd = ('sct_antsRegistration '
                '--dimensionality 3 '
-               '--transform '+algo+'['+gradientStep+',3,0] '
-               '--metric '+metric+'[dest_pad.nii,src_regAffine.nii,1,'+metricSize+'] '
-               '--convergence '+numberIterations+' '
-               '--shrink-factors '+shrinkFactor+' '
-               '--smoothing-sigmas '+smoothingSigma+'mm '
+               '--transform '+paramreg.algo+'['+paramreg.gradStep+',3,0] '
+               '--metric '+paramreg.metric+'[dest_pad.nii,src_regAffine.nii,1,'+metricSize+'] '
+               '--convergence '+paramreg.iter+' '
+               '--shrink-factors '+paramreg.shrink+' '
+               '--smoothing-sigmas '+paramreg.smooth+'mm '
                '--restrict-deformation 1x1x0 '
                '--output [stage1,src_regAffineWarp.nii] '
                '--interpolation BSpline[3] '
                +masking)
+    else:
+        sct.printv('ERROR: algo '+paramreg.algo+' does not exist. Exit program', 1, 'error')
 
     # run registration
     status, output = sct.run(cmd)
@@ -267,9 +296,9 @@ def main():
 
     # Apply warping field to src data
     sct.printv('\nApply transfo source --> dest...', verbose)
-    sct.run('sct_apply_transfo -i src.nii -o src_reg.nii -d dest.nii -w warp_src2dest.nii.gz -x '+param.interp)
+    sct.run('sct_apply_transfo -i src.nii -o src_reg.nii -d dest.nii -w warp_src2dest.nii.gz -x '+interp)
     sct.printv('\nApply transfo dest --> source...', verbose)
-    sct.run('sct_apply_transfo -i dest.nii -o dest_reg.nii -d src.nii -w warp_dest2src.nii.gz -x '+param.interp)
+    sct.run('sct_apply_transfo -i dest.nii -o dest_reg.nii -d src.nii -w warp_dest2src.nii.gz -x '+interp)
 
     # come back to parent folder
     os.chdir('..')
@@ -298,60 +327,13 @@ def main():
     print ''
 
 
-# Print usage
+
+# parse parameters for registration
 # ==========================================================================================
-def usage():
-    print """
-"""+os.path.basename(__file__)+"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
+def parse_paramreg(paramreg):
+    """paramreg: class"""
+    paramreg.replace(' ', '').split(',')  # remove spaces and parse with comma
 
-DESCRIPTION
-  This program co-registers two 3D volumes. The deformation is non-rigid and is constrained along Z
-  direction (i.e., axial plane). Hence, this function assumes that orientation of the destination
-  image is axial (RPI). If you need to register two volumes with large deformations and/or different
-  contrasts, it is recommended to input spinal cord segmentations (binary mask) in order to achieve
-  maximum robustness.
-  The program outputs a warping field that can be used to register other images to the destination
-  image. To apply the warping field to another image, use sct_apply_transfo
-
-USAGE
-  """+os.path.basename(__file__)+""" -i <source> -d <dest>
-
-MANDATORY ARGUMENTS
-  -i <source>      source image
-  -d <dest>        destination image
-
-OPTIONAL ARGUMENTS
-  -m <mask>        mask used on destination image.
-  -o <output>      name of output file. Default=source_reg
-  -a {syn,bsplinesyn,slicereg}  algorithm for registration. Default="""+param_default.algo+"""
-                     more info on slicereg: http://goo.gl/Sj3ZeU
-  -p <param>       parameters for registration. Separate with ','. Depends on the algorithm:
-                   if a = syn or bsplinesyn:
-                     1) number of iterations. For multiple stages, separate with 'x'. Default="""+param_default.param_syn[0]+"""
-                     2) shrink factor. Default="""+param_default.param_syn[1]+"""
-                     3) smoothing factor in mm. Default="""+param_default.param_syn[2]+"""
-                     4) gradient step. The larger the more deformation. Default="""+param_default.param_syn[3]+"""
-                     5) metric: Default="""+param_default.param_syn[4]+"""
-                          MI: fast, but requires a minimum of voxels (otherwise it crashes)
-                          MeanSquares: fast, requires similar contrast between src and dest.
-                          CC: slow, but sometimes produces best results.
-                   if a = slicereg:
-                     1) number of iterations (only one stage). Default="""+param_default.param_slicereg[0]+"""
-                     2) degree of polynomial function. Default="""+param_default.param_slicereg[1]+"""
-                     3) metric: {MI,MeanSquares,CC}. Default="""+param_default.param_slicereg[2]+"""
-  -z <padding>     size of z-padding to enable deformation at edges. Default="""+str(param_default.padding)+"""
-  -x {nn,linear,spline}  Final Interpolation. Default="""+str(param_default.interp)+"""
-  -r {0,1}         remove temporary files. Default="""+str(param.remove_temp_files)+"""
-  -v {0,1}         verbose. Default="""+str(param_default.verbose)+"""
-
-EXAMPLES
-  Register mean DWI data to the T1 volume using SyN:
-  """+os.path.basename(__file__)+""" -i dwi_mean.nii.gz -d t1.nii.gz -a syn -p 50x5,4x2,0x0,0.5,MI -x linear \n"""
-
-    # exit program
-    sys.exit(2)
 
 
 # pad an image
@@ -361,26 +343,25 @@ def pad_image(fname_in, file_out, padding):
     return
 
 
+
 # START PROGRAM
 # ==========================================================================================
 if __name__ == "__main__":
     # initialize parameters
     param = Param()
-    param_default = Param()
     # call main function
     main()
 
 
-
-    # Convert deformation field to 4D volume (readable by fslview)
-    # DONE: clean code below-- right now it does not work
-    #===========
-    #if convertDeformation:
-    #    print('\nConvert deformation field...')
-    #    cmd = 'sct_c3d -mcs tmp.regWarp.nii -oo tmp.regWarp_x.nii tmp.regWarp_y.nii tmp.regWarp_z.nii'
-    #    print(">> "+cmd)
-    #    os.system(cmd)
-    #    cmd = 'fslmerge -t '+path_out+'warp_comp.nii tmp.regWarp_x.nii tmp.regWarp_y.nii tmp.regWarp_z.nii'
-    #    print(">> "+cmd)
-    #    os.system(cmd)
-    #===========
+# Convert deformation field to 4D volume (readable by fslview)
+# DONE: clean code below-- right now it does not work
+#===========
+#if convertDeformation:
+#    print('\nConvert deformation field...')
+#    cmd = 'sct_c3d -mcs tmp.regWarp.nii -oo tmp.regWarp_x.nii tmp.regWarp_y.nii tmp.regWarp_z.nii'
+#    print(">> "+cmd)
+#    os.system(cmd)
+#    cmd = 'fslmerge -t '+path_out+'warp_comp.nii tmp.regWarp_x.nii tmp.regWarp_y.nii tmp.regWarp_z.nii'
+#    print(">> "+cmd)
+#    os.system(cmd)
+#===========
