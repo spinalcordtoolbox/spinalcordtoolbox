588c7fcb565431f6cec749d5b482c045e1632436
Simon Lévy <simon.levy@mines-ales.org>
2015-04-02 15:34:54 -0400

    DEV: MTV
    Former-commit-id: 0ef49660bb8ace29c1380767254fe82efc280d17 [formerly a24853c9d1dc7031c8a559d9bd515c882a8c15aa] [formerly 08968246ddad11b1fd86dbc1bf793f7685e8fd8c [formerly c8bdf2f3eb44b9c7532838483690f2483f8c85d9 [formerly 1c7f57f5958f6aac008ee37e5e9754f6d27b49ae]]]
    Former-commit-id: 0ef49660bb8ace29c1380767254fe82efc280d17 [formerly a24853c9d1dc7031c8a559d9bd515c882a8c15aa]
    Former-commit-id: afc393d881c717b3356b06f8a3acfb78919607fc [formerly c85eb4b8ebd0373045e5a96d9340385258e2ec33 [formerly 3c7bed9f0d86f4574ac6792a0cc8f02bce758e5b]]
    Former-commit-id: 1ab43f9b164fc966cca8747ba71c5e96f231daab
    Former-commit-id: 7076988601bf8a030f24b9bff9c9f0231d33c2c6

--- dev/mtv/matlab/validation.m
+++ dev/mtv/matlab/validation.m
@@ -1,16 +1,18 @@
+% cropping coordinates
 xmin = 93;
 xmax = 145;
 ymin = 95;
 ymax = 147;
 zmin = 10;
 zmax = 34;
-
+% load data
 spgr5_nii = load_nii('spgr5.nii.gz');
 spgr10_nii = load_nii('spgr10.nii.gz');
 spgr20_nii = load_nii('spgr20.nii.gz');
 spgr30_nii = load_nii('spgr30.nii.gz');
 b1_nii = load_nii('b1_scaling_no_smooth_in_spgr_space.nii.gz');
 b1_nii.img = b1_nii.img(xmin:xmax, ymin:ymax, zmin:zmax);
+
 
 spgr = double(cat(4, spgr5_nii.img(xmin:xmax, ymin:ymax, zmin:zmax), spgr10_nii.img(xmin:xmax, ymin:ymax, zmin:zmax), spgr20_nii.img(xmin:xmax, ymin:ymax, zmin:zmax), spgr30_nii.img(xmin:xmax, ymin:ymax, zmin:zmax)));
 
@@ -19,7 +21,7 @@ mean_b1_slice10 = mean(mean(b1_nii.img(:, :, 10)));
 
 y = mean_spgr_slice10./sin(flipAngles*(pi/180)*mean_b1_slice10);
 x = mean_spgr_slice10./tan(flipAngles*(pi/180)*mean_b1_slice10);
-p = polyfit(x, y, 1);
+[p, S] = polyfit(x, y, 1);
 slope = p(1);
 intercept = p(2);
 
@@ -29,4 +31,13 @@ else  % due to noise or bad fitting
     T1 = 0;
 end
 M0 = intercept/(1-exp(-tr/T1));
+
+% assess fitting
+figure
+hold on
+plot(x, y, 'Color', 'blue', 'Marker', '+', 'MarkerSize', 30.0, 'LineStyle','--', 'LineWidth', 2.0)
+plot(x, slope*x + intercept, 'Color', 'red', 'Marker', 'x', 'MarkerSize', 30.0, 'LineWidth', 2.0)
+hold off
+legend('True data', 'Fitting')
+grid(gca,'minor') 
 
