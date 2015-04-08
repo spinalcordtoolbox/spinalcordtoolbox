b9728b4eab4ba25aff9e52cd5bd16c1ca6379a93
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-04-08 14:27:04 -0400

    BUG: corrected many bugs as the function had not been tested yet with the nurbs code
    Former-commit-id: 38450ee25ae3e45155857e211ef18765482a75ea [formerly 97cdda72b1a1ba32d1df6306eb0379563c6f62cf] [formerly 3b31b4f71f03f810bd93a61f09fc9a999e773791 [formerly 8746a15ab0531e91ff9e2c8cc674afd912a99d85 [formerly 3acd13b5c2d241d0b7c6129f7e8d38575b58510b]]]
    Former-commit-id: 38450ee25ae3e45155857e211ef18765482a75ea [formerly 97cdda72b1a1ba32d1df6306eb0379563c6f62cf]
    Former-commit-id: 4958d7b60f86a11da95e54199af19aefba8399df [formerly 1fea30456c73a5f858aa0cde7960025069c8d64b [formerly 478134e7d72bc20ff86b16b103a50494e3d6522a]]
    Former-commit-id: 27533ab927f0e3ddd7cf82d436100c828764ca84
    Former-commit-id: 594cd6ea31f40d92eee994379e2df2a2fd30237e

--- scripts/msct_get_centerline_from_labels.py
+++ scripts/msct_get_centerline_from_labels.py
@@ -39,51 +39,53 @@ class ExtractCenterline :
         if len(self.list_image) > 0 :
             self.dimension = sct.get_dimension(self.list_image[0])
             nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(image_input)
-            if self.dimension != [nx, ny, nz, nt, px, py, pz, pt] :
+            #if self.dimension != [nx, ny, nz, nt, px, py, pz, pt] :
+            if self.dimension[0:2] != [nx, ny, nz] or self.dimension[4:6] != [px, py, pz] :
                 # Return error and exit programm if not same size
                 print('\nError: Files are not of the same size.')
                 sys.exit()
         # Add file if same size
-        self.list_image = self.list_image.extend([image_input])
+        self.list_image.append(image_input)
 
         print(self.list_image)
 
-    def addfiles2(self, file):
+    def addfiles(self, file):
 
-        image_input = Image(file)
-
-
+        path_data, file_data, ext_data = sct.extract_fname(file)
         #check that files are same size
-        if len(self.list_image) > 0 :
-            image_o = Image(self.list_file[0])
-            self.dimension = sct.get_dimension(image_o)
-            nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(image_input)
-            if self.dimension != [nx, ny, nz, nt, px, py, pz, pt] :
+        if len(self.list_file) > 0 :
+            self.dimension = sct.get_dimension(self.list_file[0])
+            nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(file)
+
+            #if self.dimension != (nx, ny, nz, nt, px, py, pz, pt) :
+            if self.dimension[0:3] != (nx, ny, nz) or self.dimension[4:7] != (px, py, pz) :
                 # Return error and exit programm if not same size
                 print('\nError: Files are not of the same size.')
                 sys.exit()
         # Add file if same size
-        self.list_file = self.list_file.extend([file])
+        self.list_file.append(file)
 
-        print(self.list_file)
+        image_input = Image(file)
+        self.list_image.append(image_input)
+        print('\nFile', file_data+ext_data,' added to the list.')
 
     def compute(self):
-        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_image[0])
+        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_file[0])
 
         # Define output image (size matter)
         image_concatenation = self.list_image[0].copy()
-        image_concatenation *= 0
+        image_concatenation.data *= 0
         image_output = self.list_image[0].copy()
-        image_output *= 0
+        image_output.data *= 0
         # Concatenate all files by addition
         for i in range(0, len(self.list_image)):
             for s in range(0, nz) :
-                image_concatenation[:,:,s] = image_concatenation[:,:,s] + self.list_image[i][:,:,s] * (1/len(self.list_image))
+                image_concatenation.data[:,:,s] = image_concatenation.data[:,:,s] + self.list_image[i].data[:,:,s] #* (1/len(self.list_image))
 
 
         # get center of mass of the centerline/segmentation
         sct.printv('\nGet center of mass of the concatenate file...')
-        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation[:, :, iz].any()]
+        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation.data[:, :, iz].any()]
 
         nz_nonz = len(z_centerline)
         x_centerline = [0 for iz in range(0, nz_nonz, 1)]
@@ -95,39 +97,40 @@ class ExtractCenterline :
             x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(image_concatenation[:, :, z_centerline[iz]])
 
         points = [[x_centerline[n],y_centerline[n], z_centerline[n]] for n in range(len(z_centerline))]
-        nurbs = NURBS(3,1000,points)
+        nurbs = NURBS(3, 1000, points)
         P = nurbs.getCourbe3D()
-        x_centerline_fit =  P[0]
-        y_centerline_fit =  P[1]
+        x_centerline_fit = P[0]
+        y_centerline_fit = P[1]
+        z_centerline_fit = P[2]
 
         #x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
 
-        for iz in range(0, nz_nonz, 1):
-            image_output[x_centerline_fit[iz], y_centerline_fit[iz], z_centerline[iz]] = 1
+        for iz in range(0, z_centerline_fit.shape[0], 1):
+            image_output.data[x_centerline_fit[iz], y_centerline_fit[iz], z_centerline_fit[iz]] = 1
 
 
 
         return image_output
 
-    def getCenterline(self, type=''):
+    def getCenterline(self, type='', output_file_name=None, verbose=0):
         # Compute the centerline and save it into a image file of type "type"
 
-        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_image[0])
+        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_file[0])
 
         # Define output image (size matter)
         image_concatenation = self.list_image[0].copy()
-        image_concatenation *= 0
+        image_concatenation.data *= 0
         image_output = self.list_image[0].copy()
-        image_output *= 0
+        image_output.data *= 0
         # Concatenate all files by addition
         for i in range(0, len(self.list_image)):
             for s in range(0, nz) :
-                image_concatenation[:,:,s] = image_concatenation[:,:,s] + self.list_image[i][:,:,s] * (1/len(self.list_image))
-
+                image_concatenation.data[:,:,s] = image_concatenation.data[:,:,s] + self.list_image[i].data[:,:,s] #* (1/len(self.list_image))
+        print image_concatenation.data[:,:,414]
 
         # get center of mass of the centerline/segmentation
         sct.printv('\nGet center of mass of the concatenate file...')
-        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation[:, :, iz].any()]
+        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation.data[:, :, iz].any()]
         nz_nonz = len(z_centerline)
         x_centerline = [0 for iz in range(0, nz_nonz, 1)]
         y_centerline = [0 for iz in range(0, nz_nonz, 1)]
@@ -135,37 +138,87 @@ class ExtractCenterline :
 
         # Calculate centerline coordinates and create image of the centerline
         for iz in range(0, nz_nonz, 1):
-            x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(image_concatenation[:, :, z_centerline[iz]])
+            x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(image_concatenation.data[:, :, z_centerline[iz]])
+        #x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
 
-        x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
 
-        for iz in range(0, nz_nonz, 1):
-            image_output[round(x_centerline_fit[iz]), round(y_centerline_fit[iz]), z_centerline[iz]] = 1
+        points = [[x_centerline[n], y_centerline[n], z_centerline[n]] for n in range(nz_nonz)]
+        nurbs = NURBS(3, 1000, points, nbControl=None)
+        P = nurbs.getCourbe3D()
+        x_centerline_fit = P[0]
+        y_centerline_fit = P[1]
+        z_centerline_fit = P[2]
 
-        image_output.save(type)
+        if verbose==1 :
+                import matplotlib.pyplot as plt
 
-        #return file  
+                #Creation of a vector x that takes into account the distance between the labels
+                x_display = [0 for i in range(x_centerline_fit.shape[0])]
+                y_display = [0 for i in range(y_centerline_fit.shape[0])]
+                for i in range(0, nz_nonz, 1):
+                    x_display[z_centerline[i]-z_centerline[0]] = x_centerline[i]
+                    y_display[z_centerline[i]-z_centerline[0]] = y_centerline[i]
+
+                plt.figure(1)
+                plt.subplot(2,1,1)
+                #plt.plot(z_centerline,x_centerline, 'ro')
+                plt.plot(z_centerline_fit, x_display, 'ro')
+                plt.plot(z_centerline_fit, x_centerline_fit)
+                plt.xlabel("Z")
+                plt.ylabel("X")
+                plt.title("x and x_fit coordinates")
+
+                plt.subplot(2,1,2)
+                #plt.plot(z_centerline,y_centerline, 'ro')
+                plt.plot(z_centerline_fit, y_display, 'ro')
+                plt.plot(z_centerline_fit, y_centerline_fit)
+                plt.xlabel("Z")
+                plt.ylabel("Y")
+                plt.title("y and y_fit coordinates")
+                plt.show()
+
+
+        for iz in range(0, z_centerline_fit.shape[0], 1):
+            image_output.data[int(round(x_centerline_fit[iz])), int(round(y_centerline_fit[iz])), z_centerline_fit[iz]] = 1
+
+        #image_output.save(type)
+        file_load = nibabel.load(self.list_file[0])
+        data = file_load.get_data()
+        hdr = file_load.get_header()
+
+        print '\nWrite NIFTI volumes...'
+        img = nibabel.Nifti1Image(image_output.data, None, hdr)
+        if output_file_name != None :
+            file_name = output_file_name
+        else: file_name = 'generated_centerline.nii.gz'
+        nibabel.save(img,file_name)
+
+
+        # to view results
+        print '\nDone !'
+        print '\nTo view results, type:'
+        print 'fslview '+file_name+' &\n'
 
 
     def writeCenterline(self, output_file_name=None):
         # Compute the centerline and write the float coordinates into a txt file
 
-        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_image[0])
+        nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(self.list_file[0])
 
         # Define output image (size matter)
         image_concatenation = self.list_image[0].copy()
-        image_concatenation *= 0
+        image_concatenation.data *= 0
         image_output = self.list_image[0].copy()
-        image_output *= 0
+        image_output.data *= 0
         # Concatenate all files by addition
         for i in range(0, len(self.list_image)):
             for s in range(0, nz) :
-                image_concatenation[:,:,s] = image_concatenation[:,:,s] + self.list_image[i][:,:,s] * (1/len(self.list_image))
+                image_concatenation.data[:,:,s] = image_concatenation.data[:,:,s] + self.list_image[i].data[:,:,s] #* (1/len(self.list_image))
 
 
         # get center of mass of the centerline/segmentation
         sct.printv('\nGet center of mass of the concatenate file...')
-        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation[:, :, iz].any()]
+        z_centerline = [iz for iz in range(0, nz, 1) if image_concatenation.data[:, :, iz].any()]
         nz_nonz = len(z_centerline)
         x_centerline = [0 for iz in range(0, nz_nonz, 1)]
         y_centerline = [0 for iz in range(0, nz_nonz, 1)]
@@ -173,11 +226,16 @@ class ExtractCenterline :
 
         # Calculate centerline coordinates and create image of the centerline
         for iz in range(0, nz_nonz, 1):
-            x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(image_concatenation[:, :, z_centerline[iz]])
+            x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(image_concatenation.data[:, :, z_centerline[iz]])
 
-        x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
+        #x_centerline_fit, y_centerline_fit,x_centerline_deriv,y_centerline_deriv,z_centerline_deriv = b_spline_centerline(x_centerline,y_centerline,z_centerline)
 
-
+        points = [[x_centerline[n], y_centerline[n], z_centerline[n]] for n in range(nz_nonz)]
+        nurbs = NURBS(3, 1000, points)
+        P = nurbs.getCourbe3D()
+        x_centerline_fit = P[0]
+        y_centerline_fit = P[1]
+        z_centerline_fit = P[2]
 
         # Create output text file
         if output_file_name != None :
@@ -187,7 +245,7 @@ class ExtractCenterline :
         sct.printv('\nWrite text file...')
         #file_results = open("../"+file_name, 'w')
         file_results = open(file_name, 'w')
-        for i in range(0, nz_nonz, 1):
+        for i in range(0, z_centerline_fit.shape[0], 1):
             file_results.write(str(int(i)) + ' ' + str(x_centerline_fit[i]) + ' ' + str(y_centerline_fit[i]) + '\n')
         file_results.close()
 
