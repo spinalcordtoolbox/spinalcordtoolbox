a493659ab5dd090ad4d68fce2d3114a796c4b21d
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-04-16 17:44:37 -0400

    OPT: add a safety: return exception if the orientations of the input files are different
    Former-commit-id: 45031bdda0f0a190af387b840e7be2b7026b96fb [formerly 3acf1f4bcd81118445340b7619e9d2cb2a6204d8] [formerly a7e432a5c206f330625bcb34b2a3cc1bd88ed175 [formerly d20a289cc4d2b4070a09e77c53992d3a3a979989 [formerly bdbc6ae2ef802444cba9f8869ae4f54a9bb10c5e]]]
    Former-commit-id: 45031bdda0f0a190af387b840e7be2b7026b96fb [formerly 3acf1f4bcd81118445340b7619e9d2cb2a6204d8]
    Former-commit-id: 9e0a927d8c11f62684df10ba4727aed566494cbd [formerly 1cffb3e5119ae587c7afb7d60a2b82a7310d1ecc [formerly ad3fa10b87c9a4e514179ab63db46e7aa13653fd]]
    Former-commit-id: 36ae7b7f74e5ada9de668e4d6f4787887eae66db
    Former-commit-id: 0e916de70047b6370c6b766d418543021621db52

--- scripts/sct_get_centerline_from_labels.py
+++ scripts/sct_get_centerline_from_labels.py
@@ -15,23 +15,24 @@ import os
 import time
 import sct_utils as sct
 from sct_process_segmentation import extract_centerline
+from sct_orientation import get_orientation
 
 # DEFAULT PARAMETERS
 class Param:
-    ## The constructor
-    def __init__(self):
-        self.debug = 0
-        self.verbose = 1  # verbose
-        self.remove_temp_files = 1
-        self.type_window = 'hanning'  # for smooth_centerline @sct_straighten_spinalcord
-        self.window_length = 80  # for smooth_centerline @sct_straighten_spinalcord
-        self.algo_fitting = 'nurbs'
-        self.parameter = "binary_centerline"
-        self.list_file = []
-        self.output_file_name = ''
+   ## The constructor
+   def __init__(self):
+       self.debug = 0
+       self.verbose = 1  # verbose
+       self.remove_temp_files = 1
+       self.type_window = 'hanning'  # for smooth_centerline @sct_straighten_spinalcord
+       self.window_length = 80  # for smooth_centerline @sct_straighten_spinalcord
+       self.algo_fitting = 'nurbs'
+       # self.parameter = "binary_centerline"
+       self.list_file = []
+       self.output_file_name = ''
 
 
-def main(list_file, param, output_file_name=None, parameter = "binary_centerline", remove_temp_files = 1, verbose = 0):
+def main(list_file, param, output_file_name=None, remove_temp_files = 1, verbose = 0):
 
     path, file, ext = sct.extract_fname(list_file[0])
 
@@ -40,23 +41,28 @@ def main(list_file, param, output_file_name=None, parameter = "binary_centerline
     sct.run('mkdir '+path_tmp)
 
     # copy files into tmp folder
+    sct.printv('\nCopy files into tmp folder...', verbose)
     for i in range(len(list_file)):
-        file_temp = os.path.abspath(list_file[i])
-        sct.run('cp '+file_temp+' '+path_tmp)
+       file_temp = os.path.abspath(list_file[i])
+       sct.run('cp '+file_temp+' '+path_tmp)
 
     # go to tmp folder
     os.chdir(path_tmp)
 
-    # Concatenation of the files
 
+    # Concatenation : sum of matrices
     file_0 = load(list_file[0])
     data_concatenation = file_0.get_data()
     hdr_0 = file_0.get_header()
+    orientation_file_0 = get_orientation(list_file[0])
     if len(list_file)>0:
-        for i in range(1, len(list_file)):
-            file_temp = load(list_file[i])
-            data_temp = file_temp.get_data()
-            data_concatenation = data_concatenation + data_temp
+       for i in range(1, len(list_file)):
+           orientation_file_temp = get_orientation(list_file[i])
+           if orientation_file_0 != orientation_file_temp :
+               raise Exception("The files ", list_file[0], " and ", list_file[i], " are not in the same orientation. Use sct_orientation to change the orientation of a file.")
+           file_temp = load(list_file[i])
+           data_temp = file_temp.get_data()
+           data_concatenation = data_concatenation + data_temp
 
     # Save concatenation as a file
     print '\nWrite NIFTI volumes...'
@@ -69,30 +75,24 @@ def main(list_file, param, output_file_name=None, parameter = "binary_centerline
 
     # Rename files after processing
     if output_file_name != None:
-        output_file_name = output_file_name
+       output_file_name = output_file_name
     else : output_file_name = "generated_centerline.nii.gz"
+
     os.rename(fname_output, output_file_name)
     path_binary, file_binary, ext_binary = sct.extract_fname(output_file_name)
     os.rename('concatenation_file_centerline.txt', file_binary+'.txt')
 
-# Process for a binary file as output:
-    if parameter == "binary_centerline":
-        #Copy binary file into parent folder
-        sct.run('cp '+output_file_name+' ../')
+    # Process for a binary file as output:
+    sct.run('cp '+output_file_name+' ../')
 
-
-# Process for a text file as output:
-    if parameter == "text_file" :
-        print "\nText file process"
-        #path_binary, file_binary, ext_binary = sct.extract_fname(output_file_name)
-        #Copy txt file into parent folder
-        sct.run('cp '+file_binary+ '.txt'+ ' ../')
+    # Process for a text file as output:
+    sct.run('cp '+file_binary+ '.txt'+ ' ../')
 
     os.chdir('../')
     # Remove temporary files
     if remove_temp_files:
-        print('\nRemove temporary files...')
-        sct.run('rm -rf '+path_tmp)
+       print('\nRemove temporary files...')
+       sct.run('rm -rf '+path_tmp)
 
 
 
@@ -109,57 +109,47 @@ def main(list_file, param, output_file_name=None, parameter = "binary_centerline
 # Start program
 #=======================================================================================================================
 if __name__ == "__main__":
-    # initialize parameters
+   # initialize parameters
 
 
-    # Initialize the parser
-    parser = Parser(__file__)
-    parser.usage.set_description('Utility function for labels.')
-    parser.add_option(name="-i",
-                      type_value=[[','],'file'],
-                      description="List containing segmentation NIFTI file and label NIFTI files. They must be 3D. Names must be separated by commas without spaces.",
-                      mandatory=True)
-    parser.add_option(name="-p",
-                      type_value="multiple_choice",
-                      description="Type of output wanted: \nbinary_centerline: return binary NIFTI file of the centerline \ntext_file: return text file with float coordinates according to z",
-                      mandatory=False,
-                      example=["binary_centerline","text_file"],
-                      default_value="binary_centerline")
+   # Initialize the parser
+   parser = Parser(__file__)
+   parser.usage.set_description('Compute a centerline from a list of segmentation and label files. It concatenates the parts, then extract the centerline. The output is a NIFTI image and a text file with the float coordinates (z, x, y) of the centerline.')
+   parser.add_option(name="-i",
+                     type_value=[[','],'file'],
+                     description="List containing segmentation NIFTI file and label NIFTI files. They must be 3D. Names must be separated by commas without spaces.",
+                     mandatory=True)
+   parser.add_option(name="-o",
+                     type_value="file_output",
+                     description="Name of the output NIFTI image with the centerline and of the output text file with the coordinates (z, x, y). The orientation is RPI.",
+                     mandatory=False)
 
-    parser.add_option(name="-o",
-                      type_value="file_output",
-                      description="Name of the output NIFTI image with the centerline, or of the output text file with the coordinates according to z.",
-                      mandatory=False)
+   parser.add_option(name="-r",
+                     type_value="multiple_choice",
+                     description="Remove temporary files. Specify 0 to get access to temporary files.",
+                     mandatory=False,
+                     example=['0','1'],
+                     default_value="1")
+   parser.add_option(name="-v",
+                     type_value="multiple_choice",
+                     description="Verbose. 0: nothing. 1: basic. 2: extended.",
+                     mandatory=False,
+                     default_value='0',
+                     example=['0', '1', '2'])
+   arguments = parser.parse(sys.argv[1:])
 
-    parser.add_option(name="-r",
-                      type_value="multiple_choice",
-                      description="Remove temporary files. Specify 0 to get access to temporary files.",
-                      mandatory=False,
-                      example=['0','1'],
-                      default_value="1")
-    parser.add_option(name="-v",
-                      type_value="multiple_choice",
-                      description="Verbose. 0: nothing. 1: basic. 2: extended.",
-                      mandatory=False,
-                      default_value='0',
-                      example=['0', '1', '2'])
-    arguments = parser.parse(sys.argv[1:])
+   remove_temp_files = int(arguments["-r"])
+   verbose = int(arguments["-v"])
 
-    parameter = arguments["-p"]
-    remove_temp_files = int(arguments["-r"])
-    verbose = int(arguments["-v"])
+   if "-i" in arguments:
+       list_file = arguments["-i"]
+   else: list_file = None
+   if "-o" in arguments:
+       output_file_name = arguments["-o"]
+   else: output_file_name = None
 
-    if "-i" in arguments:
-        list_file = arguments["-i"]
-    else: list_file = None
-    if "-o" in arguments:
-        output_file_name = arguments["-o"]
-    else: output_file_name = None
+   param = Param()
+   param.verbose = verbose
+   param.remove_temp_files =remove_temp_files
 
-    param = Param()
-    param.verbose = verbose
-    param.remove_temp_files =remove_temp_files
-
-    main(list_file, param, output_file_name, parameter, remove_temp_files, verbose)
-
-#-i data_RPI_seg.nii.gz,labels_brainstem.nii.gz -o centerline_total.nii.gz -r 0 -v 1
+   main(list_file, param, output_file_name, remove_temp_files, verbose)
