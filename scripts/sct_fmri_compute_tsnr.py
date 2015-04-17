baf4effd12c857fdc95ee9ad7b9f5d1e074d9ad7
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-04-17 16:56:42 -0400

    BUG: fixed issue related to missing slash
    Former-commit-id: 416c716090116aa04034d0ae8abd09b6595a2b84 [formerly 08d8bf1bcea07028f87e21695313ee067238a85d] [formerly d7e661686fb6c625c010d5aba63ae7e5d7e304b0 [formerly 9bc1f2b19bc48f4e556048d4406823ee8be7bb43 [formerly be2632466ca08a1eba32b2a54453cdfec5dd7a70]]]
    Former-commit-id: 416c716090116aa04034d0ae8abd09b6595a2b84 [formerly 08d8bf1bcea07028f87e21695313ee067238a85d]
    Former-commit-id: 9e5b0eaf0e87ec2c710ff25c84ab0f1b83549a02 [formerly 5f462e5a0432eec055828ac983937ba1cbfc531d [formerly a77d100c2df11ab4098d4767d6df1ac76cce5133]]
    Former-commit-id: fa1d0675c2ef0292976c840a1296da7a1426b4bb
    Former-commit-id: 3c1210f149c8879ae7d8ba9068cc4de856469aca

--- scripts/sct_fmri_compute_tsnr.py
+++ scripts/sct_fmri_compute_tsnr.py
@@ -44,7 +44,8 @@ class Tsnr:
         sct.printv('\nMotion correct the fMRI data...', self.param.verbose, 'normal')
         path_fmri, fname_fmri, ext_fmri = sct.extract_fname(self.fmri)
         fname_fmri_moco = fname_fmri + '_moco'
-        sct.run('mcflirt -in ' + sct.slash_at_the_end(path_fmri) + fname_fmri + ' -out ' + fname_fmri_moco)
+        print sct.slash_at_the_end(path_fmri) + fname_fmri
+        sct.run('mcflirt -in ' + sct.slash_at_the_end(path_fmri, 1) + fname_fmri + ' -out ' + fname_fmri_moco)
 
         # compute tsnr
         sct.printv('\nCompute the tSNR...', self.param.verbose, 'normal')
