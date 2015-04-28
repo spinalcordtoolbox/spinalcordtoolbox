a5fcf2780fd44dc41e1d3425d15dfbd5abb531f3
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-04-28 17:04:50 -0400

    OPT: changed centreline to middle vertical line of the image
    Former-commit-id: 38b4ef16b188ecfa4523bead9c3f5d14ab43f0e3 [formerly 57eab3233195e06925b1957ac3cb2236e722ff0e] [formerly 97230c264a38932ecf246075b5468c02c3a5afaa [formerly b7459deec9973f64404b4bea65d8305828d5362f [formerly 8c8633207fd1d615a8ea2b60e177f98d2010ddef]]]
    Former-commit-id: 38b4ef16b188ecfa4523bead9c3f5d14ab43f0e3 [formerly 57eab3233195e06925b1957ac3cb2236e722ff0e]
    Former-commit-id: 26e26291be1dfcfd7cc9224802c147e0aa8b78ec [formerly 0638789a0f05cb6763c2a52c90e774cc6d7f4e44 [formerly 4aa3b9d8092541cbd056ba67804026cd50da4b14]]
    Former-commit-id: dedf583e927a10512874e784f4bd9911cb690b0a
    Former-commit-id: cefc207dd2d18b227c34bd9b8416bbfabd40122d

--- dev/template_creation/sct_normalize.py
+++ dev/template_creation/sct_normalize.py
@@ -72,19 +72,20 @@ def main():
            verbose = int(arg)
 
    # display usage if a mandatory argument is not provided
-   if fname == '' or fname_centerline == '':
+   #if fname == '' or fname_centerline == '':
+   if fname == '':
        usage()
 
 
    # check existence of input files
    print'\nCheck if file exists ...'
    sct.check_file_exist(fname)
-   sct.check_file_exist(fname_centerline)
+   #sct.check_file_exist(fname_centerline)
 
    # Display arguments
    print'\nCheck input arguments...'
    print'  Input volume ...................... '+fname
-   print'  Centerline ...................... '+fname
+   print'  Centerline ...................... '+fname_centerline
    print'  Verbose ........................... '+str(verbose)
 
    # Extract path, file and extension
@@ -96,32 +97,47 @@ def main():
    data = file.get_data()
    hdr = file.get_header()
 
+   ## [Process 1] Command for extracting center of mass for each slice of the centerline file
+   # sct.printv('\nOpen centerline...',verbose)
+   # print '\nGet dimensions of input centerline...'
+   # nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(fname_centerline)
+   # print '.. matrix size: '+str(nx)+' x '+str(ny)+' x '+str(nz)
+   # print '.. voxel size:  '+str(px)+'mm x '+str(py)+'mm x '+str(pz)+'mm'
+   # file_c = nibabel.load(fname_centerline)
+   # data_c = file_c.get_data()
+   #
+   #
+   # #X,Y,Z = (data_c>0).nonzero()
+   #
+   # #min_z_index, max_z_index = min(Z), max(Z)
+   #
+   #
+   # z_centerline = [iz for iz in range(0, nz, 1) if data_c[:,:,iz].any() ]
+   # nz_nonz = len(z_centerline)
+   # if nz_nonz==0 :
+   #     print '\nERROR: Centerline is empty'
+   #     sys.exit()
+   # x_centerline = [0 for iz in range(0, nz_nonz, 1)]
+   # y_centerline = [0 for iz in range(0, nz_nonz, 1)]
+   # #print("z_centerline", z_centerline,nz_nonz,len(x_centerline))
+   # print '\nGet center of mass of the centerline ...'
+   # for iz in xrange(len(z_centerline)):
+   #     x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(np.array(data_c[:,:,z_centerline[iz]]))
+   # end of Process 1
 
-   sct.printv('\nOpen centerline...',verbose)
-   print '\nGet dimensions of input centerline...'
-   nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(fname_centerline)
+   ## [Process 2] Process for defining the middle vertical line as reference for normalizing the intensity of the image
+   print '\nGet dimensions of input image...'
+   nx, ny, nz, nt, px, py, pz, pt = sct.get_dimension(fname)
    print '.. matrix size: '+str(nx)+' x '+str(ny)+' x '+str(nz)
    print '.. voxel size:  '+str(px)+'mm x '+str(py)+'mm x '+str(pz)+'mm'
-   file_c = nibabel.load(fname_centerline)
-   data_c = file_c.get_data()
-
-
-   #X,Y,Z = (data_c>0).nonzero()
-
-   #min_z_index, max_z_index = min(Z), max(Z)
-
-
-   z_centerline = [iz for iz in range(0, nz, 1) if data_c[:,:,iz].any() ]
+   z_centerline = [iz for iz in range(0, nz, 1)]
    nz_nonz = len(z_centerline)
-   if nz_nonz==0 :
-       print '\nERROR: Centerline is empty'
-       sys.exit()
-   x_centerline = [0 for iz in range(0, nz_nonz, 1)]
-   y_centerline = [0 for iz in range(0, nz_nonz, 1)]
-   #print("z_centerline", z_centerline,nz_nonz,len(x_centerline))
-   print '\nGet center of mass of the centerline ...'
-   for iz in xrange(len(z_centerline)):
-       x_centerline[iz], y_centerline[iz] = ndimage.measurements.center_of_mass(np.array(data_c[:,:,z_centerline[iz]]))
+   x_middle = int(round(nx/2))
+   y_middle = int(round(ny/2))
+   x_centerline = [x_middle for iz in range(0, nz, 1)]
+   y_centerline = [y_middle for iz in range(0, nz, 1)]
+   # end of Process 2
+
 
    means = [0 for iz in range(0, nz_nonz, 1)]
 
@@ -212,9 +228,9 @@ def main():
        plt.show()
 
    for i in range(data.shape[2]):
-       data[:,:,i] = data[:,:,i]*(mean_intensity/means_smooth_extended[i])
+       data[:,:,i] = data[:,:,i] * (mean_intensity/means_smooth_extended[i])
 
-   hdr.set_data_dtype('uint8') # set imagetype to uint8
+   #hdr.set_data_dtype('uint8') # set imagetype to uint8
    # save volume
    sct.printv('\nWrite NIFTI volumes...',verbose)
    data = data.astype(np.float32, copy =False)
