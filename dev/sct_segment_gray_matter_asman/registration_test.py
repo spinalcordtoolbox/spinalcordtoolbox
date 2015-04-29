074fce867351ba183e0baf37006f211036fb69da
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-04-29 16:13:01 -0400

    NEW: registration_test: now possible to choose groupwise or pairwise registration of the target for the tests

--- dev/sct_segment_gray_matter_asman/registration_test.py
+++ dev/sct_segment_gray_matter_asman/registration_test.py
@@ -19,17 +19,28 @@ if __name__ == '__main__':
                       description="Path to the dictionary of images",
                       mandatory=True,
                       example='/home/jdoe/data/dictionary')
+    parser.add_option(name="-target-reg",
+                      type_value='multiple_choice',
+                      description="type of registration of the target to the model space "
+                                  "(if pairwise, the registration applied to the target are the same as"
+                                  " those of the -reg flag)",
+                      mandatory=False,
+                      default_value='pairwise',
+                      example=['pairwise', 'groupwise'])
 
     arguments = parser.parse(sys.argv[1:])
     target_fname = None
+    target_reg = 'pairwise'
     cmd_gmseg = ''
     if "-i" in arguments:
         target_fname = arguments["-i"]
+    if "-target-reg" in arguments:
+        target_reg = arguments["-target-reg"]
     path_dictionary = arguments["-dic"]
 
     transformations_to_try = ['Rigid', 'Affine', 'SyN', 'Rigid,Affine']
     if target_fname is not None:
-        cmd_gmseg = 'sct_asman -i ' + target_fname + ' -dic ' + path_dictionary + ' -model compute -target-reg pairwise'
+        cmd_gmseg = 'sct_asman -i ' + target_fname + ' -dic ' + path_dictionary + ' -model compute -target-reg ' + target_reg
 
     for transformation in transformations_to_try:
         if target_fname is not None:
@@ -39,5 +50,5 @@ if __name__ == '__main__':
             sct.run('mkdir ./' + transformation)
             sct.run('cp -r ' + path_dictionary + ' ./' + transformation + '/dictionary')
             os.chdir('./' + transformation)
-            leave_one_out('dictionary/', reg=transformation)
+            leave_one_out('dictionary/', reg=transformation, target_reg=target_reg)
             os.chdir('..')
