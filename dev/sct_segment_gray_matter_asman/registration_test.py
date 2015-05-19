6db0679c88c0bfb3f2f1ebcb8b5e9f714b04d999
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-05-19 14:15:20 -0400

    OPT: LOOCV with gm seg and corrected wm seg
    Former-commit-id: ac4271833f75ae8bcdf7fe12f6dd4c931bb3bc24
    Former-commit-id: cfcd0d221e7ee1ee205e57782098972d6997cfa2

--- dev/sct_segment_gray_matter_asman/registration_test.py
+++ dev/sct_segment_gray_matter_asman/registration_test.py
@@ -38,37 +38,22 @@ if __name__ == '__main__':
         target_reg = arguments["-target-reg"]
     path_dictionary = arguments["-dic"]
 
-    # transformations_to_try = ['Rigid', 'Affine', 'SyN', 'Rigid,Affine']
-    transformations_to_try = ['Affine', 'Rigid,Affine']
+    registration = 'Affine'
 
     if target_fname is not None:
-        cmd_gmseg = 'sct_asman -i ' + target_fname + ' -dic ' + path_dictionary + ' -model compute -target-reg ' + target_reg
+        cmd_gmseg = 'sct_asman -i ' + target_fname + ' -dic ' + path_dictionary + ' -model compute -target-reg ' + target_reg + '-reg ' + registration
 
-    for transformation in transformations_to_try:
-        if target_fname is not None:
-            cmd_gmseg += ' -reg ' + transformation
-            sct.run(cmd_gmseg)
-        else:
-            sct.run('mkdir ./' + transformation + '_gm_with_levels')
-            sct.run('cp -r ' + path_dictionary + ' ./' + transformation + '_gm_with_levels' + '/dictionary')
-            os.chdir('./' + transformation + '_gm_with_levels')
-            leave_one_out_by_slice('dictionary/', reg=transformation, target_reg=target_reg, use_levels=True, seg_type='gm')
-            os.chdir('..')
+    if target_fname is not None:
+        sct.run(cmd_gmseg)
+    else:
+        sct.run('mkdir ./' + registration + '_with_levels')
+        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_with_levels' + '/dictionary')
+        os.chdir('./' + registration + '_with_levels')
+        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=True)
+        os.chdir('..')
 
-            sct.run('mkdir ./' + transformation + '_gm_without_levels')
-            sct.run('cp -r ' + path_dictionary + ' ./' + transformation + '_gm_without_levels' + '/dictionary')
-            os.chdir('./' + transformation + '_gm_without_levels')
-            leave_one_out_by_slice('dictionary/', reg=transformation, target_reg=target_reg, use_levels=False, seg_type='gm')
-            os.chdir('..')
-
-            sct.run('mkdir ./' + transformation + '_wm_with_levels')
-            sct.run('cp -r ' + path_dictionary + ' ./' + transformation + '_wm_with_levels' + '/dictionary')
-            os.chdir('./' + transformation + '_wm_with_levels')
-            leave_one_out_by_slice('dictionary/', reg=transformation, target_reg=target_reg, use_levels=True, seg_type='wm')
-            os.chdir('..')
-
-            sct.run('mkdir ./' + transformation + '_wm_without_levels')
-            sct.run('cp -r ' + path_dictionary + ' ./' + transformation + '_wm_without_levels' + '/dictionary')
-            os.chdir('./' + transformation + '_wm_without_levels')
-            leave_one_out_by_slice('dictionary/', reg=transformation, target_reg=target_reg, use_levels=False, seg_type='wm')
-            os.chdir('..')
+        sct.run('mkdir ./' + registration + '_without_levels')
+        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_without_levels' + '/dictionary')
+        os.chdir('./' + registration + '_without_levels')
+        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=False)
+        os.chdir('..')
