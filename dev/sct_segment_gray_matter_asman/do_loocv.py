98342f40859b0eeb10f789b7ae3f850d8ecf7ae7
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-05-22 14:13:10 -0400

    NEW: Leave One Out Cross Validation script
    Former-commit-id: 2c4b79d8ecf17b818b3a03980aeca6f1d09a4ab0 [formerly 21b29e457c213cd7e4ceb9b650b9e8eb39782348] [formerly ec133adda16da57bf1aab7b9f4135086e1d90e65 [formerly ef5630c46a619195883994fc089e3606d5064577 [formerly 67d633ffc67c0e7c399977828a75d5d3e6000bbe]]]
    Former-commit-id: 2c4b79d8ecf17b818b3a03980aeca6f1d09a4ab0 [formerly 21b29e457c213cd7e4ceb9b650b9e8eb39782348]
    Former-commit-id: 5ea04e56bc013870731b1dab16e8433e83353609 [formerly 281b7c8a223e47a5ffa7701267cbbdc2b83518b8 [formerly 8fa154376e5df2ec673c36d590cfa4ba3829110e]]
    Former-commit-id: 427c051092d5294c7a057512ec2232f8aa73d570
    Former-commit-id: ddcde7375a688c8758494cd09bf781d572a65d53

--- dev/sct_segment_gray_matter_asman/do_loocv.py
+++ dev/sct_segment_gray_matter_asman/do_loocv.py
@@ -8,12 +8,6 @@ import os
 if __name__ == '__main__':
     parser = Parser(__file__)
     parser.usage.set_description('Gray matter segmentation with several types of registration')
-    parser.add_option(name="-i",
-                      type_value="file",
-                      description="T2star image you want to segment "
-                                  "if no input image is specified, leave one out cross validation",
-                      mandatory=False,
-                      example='t2star.nii.gz')
     parser.add_option(name="-dic",
                       type_value="folder",
                       description="Path to the dictionary of images",
@@ -29,31 +23,25 @@ if __name__ == '__main__':
                       example=['pairwise', 'groupwise'])
 
     arguments = parser.parse(sys.argv[1:])
-    target_fname = None
     target_reg = 'pairwise'
     cmd_gmseg = ''
-    if "-i" in arguments:
-        target_fname = arguments["-i"]
     if "-target-reg" in arguments:
         target_reg = arguments["-target-reg"]
     path_dictionary = arguments["-dic"]
 
     registration = 'Affine'
 
-    if target_fname is not None:
-        cmd_gmseg = 'sct_asman -i ' + target_fname + ' -dic ' + path_dictionary + ' -model compute -target-reg ' + target_reg + '-reg ' + registration
+    weights = [1.1, 1.2, 1.5, 1.8, 2.1]
 
-    if target_fname is not None:
-        sct.run(cmd_gmseg)
-    else:
-        sct.run('mkdir ./' + registration + '_with_levels')
-        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_with_levels' + '/dictionary')
-        os.chdir('./' + registration + '_with_levels')
-        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=True)
+    for w in weights:
+        sct.run('mkdir ./' + registration + '_with_levels_weight' + str(w))
+        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_with_levels_weight' + str(w) + '/dictionary')
+        os.chdir('./' + registration + '_with_levels_weight' + str(w))
+        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=True, weight=w)
         os.chdir('..')
 
-        sct.run('mkdir ./' + registration + '_without_levels')
-        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_without_levels' + '/dictionary')
-        os.chdir('./' + registration + '_without_levels')
-        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=False)
+        sct.run('mkdir ./' + registration + '_without_levels' + str(w))
+        sct.run('cp -r ' + path_dictionary + ' ./' + registration + '_without_levels' + str(w) + '/dictionary')
+        os.chdir('./' + registration + '_without_levels' + str(w))
+        leave_one_out_by_slice('dictionary/', reg=registration, target_reg=target_reg, use_levels=False, weight=w)
         os.chdir('..')
