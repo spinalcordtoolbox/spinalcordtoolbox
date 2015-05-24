b00672639a0c2880b684975adbe85a9e975c4174
Simon Lévy <simon.levy@mines-ales.org>
2015-05-24 13:13:53 -0400

    DEV: ROIs + compute intra-rater Dice coeff
    Former-commit-id: 0b42040b206837ec8914854d678f468740a8257e [formerly d747aee00e4a0f93a382044e6dc47855bf8a8e52]
    Former-commit-id: a48289bde37887e8be82b982a7584ca7f0821fb0
    Former-commit-id: 30fb1490c387c91413ba50a3416b9330e80f6608

---
+++ dev/atlas/validate_atlas/DICE_coefficient/compute_intra_rater_dice_coeff.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# rater number
+# 1=benjamin
+# 2=julien
+# 3=simon
+# 4=tanguy
+
+
+echo DICE coefficients ---Rater 1---
+echo Left CST
+echo Right CST
+echo Dorsal column
+echo 
+sct_dice_coefficient ../manual_masks/benjamin/manual_csl.nii.gz ../manual_masks/benjamin/manual_csl_2.nii.gz
+sct_dice_coefficient ../manual_masks/benjamin/manual_csr.nii.gz ../manual_masks/benjamin/manual_csr_2.nii.gz
+sct_dice_coefficient ../manual_masks/benjamin/manual_dc.nii.gz ../manual_masks/benjamin/manual_dc_2.nii.gz
+
+echo ---------------------------------------------------------------------------------------------------
+
+echo DICE coefficients ---Rater 2---
+echo Left CST
+echo Right CST
+echo Dorsal column
+echo 
+sct_dice_coefficient ../manual_masks/julien/manual_csl.nii.gz ../manual_masks/julien/manual_csl_2.nii.gz
+sct_dice_coefficient ../manual_masks/julien/manual_csr.nii.gz ../manual_masks/julien/manual_csr_2.nii.gz
+sct_dice_coefficient ../manual_masks/julien/manual_dc.nii.gz ../manual_masks/julien/manual_dc_2.nii.gz
+
+echo ---------------------------------------------------------------------------------------------------
+
+echo DICE coefficients ---Rater 3---
+echo Left CST
+echo Right CST
+echo Dorsal column
+echo 
+sct_dice_coefficient ../manual_masks/simon/manual_csl.nii.gz ../manual_masks/simon/manual_csl_2.nii.gz
+sct_dice_coefficient ../manual_masks/simon/manual_csr.nii.gz ../manual_masks/simon/manual_csr_2.nii.gz
+sct_dice_coefficient ../manual_masks/simon/manual_dc.nii.gz ../manual_masks/simon/manual_dc_2.nii.gz
+
+echo ---------------------------------------------------------------------------------------------------
+
+echo DICE coefficients ---Rater 4---
+echo Left CST
+echo Right CST
+echo Dorsal column
+echo 
+sct_dice_coefficient ../manual_masks/tanguy/manual_csl.nii.gz ../manual_masks/tanguy/manual_csl_2.nii.gz
+sct_dice_coefficient ../manual_masks/tanguy/manual_csr.nii.gz ../manual_masks/tanguy/manual_csr_2.nii.gz
+sct_dice_coefficient ../manual_masks/tanguy/manual_dc.nii.gz ../manual_masks/tanguy/manual_dc_2.nii.gz
+
+echo ---------------------------------------------------------------------------------------------------
