fbd5e9da66dcf0f65ae66b109681590bf784ab7a
Benjamin De Leener <jackbdl@gmail.com>
2015-06-08 10:47:13 -0400

    INST: test on fsl linking Former-commit-id: 05435787e2303582e0683f8ea4132e7bfe207763 [formerly e891f636a12d78040520a15e6c3846ef3e256c0f] [formerly 280252d371b9794e4b5ef472377e514664dba7c7 [formerly b37ce8f679566af9561959f38a43040c6e2e1c87 [formerly 9b4a1061610bc5711ae407e026a325800118afa7]]] Former-commit-id: 05435787e2303582e0683f8ea4132e7bfe207763 [formerly e891f636a12d78040520a15e6c3846ef3e256c0f] Former-commit-id: 6d1e42496db99170afa09641e6fc58c6820ff07d [formerly 5924b9acda99764ba81ae814a922b8aeed6c1d10 [formerly 9b701ac57357e3807a5f613781a538c1554c8c73]] Former-commit-id: e615bae65931d19648b5f4bca8c90d57b5afe33b Former-commit-id: 3b85bea5d37acfa00a2d845aba72c8af85faa504

--- install/create_links_fsl.py
+++ install/create_links_fsl.py
@@ -65,9 +65,11 @@ if path_fsl is '':
 if path_fsl is '':
     print 'ERROR: the FSL folder was not found! Please provide it using -f option.'
 
-for fsl_script in os.listdir(path_fsl):
+for fsl_script in os.listdir(os.path.dirname(path_fsl)):
     if fsl_script.startswith(prefix_fsl):
+        print fsl_script, len(prefix_fsl)
         script_name = fsl_script[len(prefix_fsl):]
+        print script_name
         sct.run(issudo + 'ln -sf ' + path_sct+'/bin/'+fsl_script + script_name)
 
 print "Done!"
