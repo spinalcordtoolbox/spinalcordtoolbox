6e833307c4d2439c494ba78ffc0dfb3462b5c83c
Simon Lévy <simon.levy@mines-ales.org>
2015-06-09 11:35:20 -0400

    MATLAB: opt
    Former-commit-id: dac0479fa031c10671edb89391692865f4bb1c96 [formerly 5c22ce2e63099a13b51a5ec76b29b69dbaa9c3cb] [formerly 8cf684c4d385c4aa5e1185f104450c8ccb739677 [formerly f3503e5e2b1c8eff829a7050e95f2e102a9bf99b [formerly cb42897619c58cf4ac9469b469542eda4bde22dd]]]
    Former-commit-id: dac0479fa031c10671edb89391692865f4bb1c96 [formerly 5c22ce2e63099a13b51a5ec76b29b69dbaa9c3cb]
    Former-commit-id: 1607d53d3d213212154c43993770af483bc3af88 [formerly a0d44448f030169b7482dfa685f0fb02a6399f10 [formerly 1b90c3901b086119bae220bc1745f45be0e59eea]]
    Former-commit-id: 11a6d15c69679abfa1b8ef2462426679beaa76c7
    Former-commit-id: 2dd2ac70a1a9a0b9d39ec14f01d60f60fa6187c5

--- matlab/statistics/plot_bland_altman.m
+++ matlab/statistics/plot_bland_altman.m
@@ -23,10 +23,10 @@ std_diff = std(diff_between_measures);
 figure
 hold on
 plot(average, diff_between_measures, 'b.', 'MarkerSize',30.0)
-plot(average, mean_diff*ones(1,length(average)),'b-')
-plot(average, mean_diff+1.96*std_diff*ones(1,length(average)),'r.-')
-plot(average, mean_diff-1.96*std_diff*ones(1,length(average)),'r.-')
-grid()
+plot(average, mean_diff*ones(1,length(average)),'b-', 'LineWidth',3.0)
+plot(average, mean_diff+1.96*std_diff*ones(1,length(average)),'r:')
+plot(average, mean_diff-1.96*std_diff*ones(1,length(average)),'r:')
+grid minor
 xlabel('Average of the 2 measures', 'FontSize', 30.0);
 ylabel('Difference between the 2 measures', 'FontSize', 30.0);
 title('Bland-Altman plot', 'FontSize', 50.0)
