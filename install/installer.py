3f57cc2d34b91a707097cf4649c8e0b6f046eb4f
Benjamin De Leener <jackbdl@gmail.com>
2015-06-15 12:02:31 -0400

    INST: slight changes in installer
    Former-commit-id: 8160dc3dbe0ebcc67a5d66113a63a72c8d8485fa [formerly b810bd2ca383514b1b5caac7659d7d6fc2e8456a] [formerly cf3a6e14f2982a8c1493502762d32ec0e524e4d7 [formerly 25fd3f16ae29fc833f2ca116f1e31da96e03beb9 [formerly de249bf63cc028c3e08ffee17b729cb0412edb32]]]
    Former-commit-id: 8160dc3dbe0ebcc67a5d66113a63a72c8d8485fa [formerly b810bd2ca383514b1b5caac7659d7d6fc2e8456a]
    Former-commit-id: c63f69bd8a411efdca9108df9c460b2c119b68d0 [formerly 9010145ba678221270fd0edd0f159bcd550e5fdb [formerly 70192ad8138e66239a7a77bbbf6c22b19a1f15f0]]
    Former-commit-id: a795154b692fa52f3dfd0b8ee3eec58ed409a954
    Former-commit-id: 86298e37a4276387b22f79305447c137d72876af

--- install/installer.py
+++ install/installer.py
@@ -419,7 +419,7 @@ class Python(object):
         import sys
         self.python_version = sys.version
         if 'Continuum Analytics, Inc.' not in self.python_version and 'conda' not in self.python_version.lower():
-            raise "Unsupported Python"
+            raise Exception("Unsupported Python")
 
 
 def open_url(url, start=0, timeout=20):
@@ -565,8 +565,8 @@ class Installer:
             this_python = Python()
         except Exception, e:
             print e
-            print "WARNING: We do not support the version of Python that you are using.\n" \
-                  "You will have to install our dependencies by yourself.\n" \
+            print "WARNING: The distribution of Python that you are using is not supported by the SCToolbox.\n" \
+                  "You still can use your own distribution of Python but you will have to install our dependencies by yourself.\n" \
                   "Do you still want to continue?"
             install_new = "no"
             signal.alarm(120)
