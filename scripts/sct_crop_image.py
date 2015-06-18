b38e3bb56b05e67d4718c2d41770ebf75951c157
Benjamin De Leener <jackbdl@gmail.com>
2015-06-18 11:55:29 -0400

    REF: fixed verbose
    Former-commit-id: b57db32ffa6b46ea8e94225d75a05926f91f60ed [formerly 904b986b0a2805fbc0813eafdc2523beda5716eb] [formerly e50b65a0500822c68f2a3c68f6bfb9b43bc73289 [formerly 0f7af70a6902ce39a1dfe871947be6816c171ae0 [formerly 7017700036a3f680c5a83d0ea697191f4ecff093]]]
    Former-commit-id: b57db32ffa6b46ea8e94225d75a05926f91f60ed [formerly 904b986b0a2805fbc0813eafdc2523beda5716eb]
    Former-commit-id: 1c8f6e178677260193caba44d30caed9ae90f6a7 [formerly b24e6fbc3441d0c507b8444cf8a909ba6f61d637 [formerly 5fd8b0accbaccbbdcae264f0d084dc76dcd2544e]]
    Former-commit-id: 9ce62c3ceafab6f1962844a1fd69ff4b95951c2f
    Former-commit-id: fb4940cd97edb8599f57dd2bfa6775c122066d60

--- scripts/sct_crop_image.py
+++ scripts/sct_crop_image.py
@@ -102,7 +102,7 @@ class ImageCropper(object):
         # Run command line
         sct.run(self.cmd, verb)
 
-        self.result = Image(self.output_filename)
+        self.result = Image(self.output_filename, verbose=self.verbose)
 
         # removes the output file created by the script if it is not needed
         if self.rm_output_file:
@@ -110,7 +110,7 @@ class ImageCropper(object):
                 os.remove(self.output_filename)
             except OSError:
                 sct.printv("WARNING : Couldn't remove output file. Either it is opened elsewhere or "
-                           "it doesn't exist.", 0, 'warning')
+                           "it doesn't exist.", self.verbose, 'warning')
         else:
             # Complete message
             sct.printv('\nDone! To view results, type:', self.verbose)
@@ -131,7 +131,7 @@ class ImageCropper(object):
 
         # Check file existence
         sct.printv('\nCheck file existence...', verbose)
-        sct.check_file_exist(fname_data)
+        sct.check_file_exist(fname_data, verbose)
 
         # Get dimensions of data
         sct.printv('\nGet dimensions of data...', verbose)
