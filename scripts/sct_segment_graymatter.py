70f7ff9714bb0ca19de75beb157ac6c2d1ae6651
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-06-22 17:34:21 -0400

    NEW: LOOCV that computes CSA
    Former-commit-id: 460f76d7000ff8358b07bad8066ab49354117fe0 [formerly 8598e909b570a0427e5a094c77e81b6133772e5e] [formerly 7c7ba77c70a5903d5a3b67216209d3be1741fed9 [formerly 10a98379fe18ee745011424eba1cc610d831b5fb [formerly 8b0f2332b09f991dcf1eaaa2c26bedd05d027757]]]
    Former-commit-id: 460f76d7000ff8358b07bad8066ab49354117fe0 [formerly 8598e909b570a0427e5a094c77e81b6133772e5e]
    Former-commit-id: 74f0c34ae7bee81a780ba10d715ea9e7ca89f586 [formerly 9ca546ec2e2aae6d858021fc2d9bba2890934bc6 [formerly 99b30fb19e6660b131bad6fdb944df9d2c211253]]
    Former-commit-id: 113c931e1b01f6201e865d569c8f83c0ec504bdd
    Former-commit-id: c69d164e418bf13623ede06672f141f60d62284a

--- scripts/sct_segment_graymatter.py
+++ scripts/sct_segment_graymatter.py
@@ -34,8 +34,9 @@ class Pretreatments:
 
         nx, ny, nz, nt, self.original_px, self.original_py, pz, pt = sct.get_dimension(self.t2star)
 
-        self.t2star = resample_image(self.t2star)
-        self.sc_seg = resample_image(self.sc_seg, binary=True)
+        if round(self.original_px, 2) != 0.3 or round(self.original_py, 2) != 0.3:
+            self.t2star = resample_image(self.t2star)
+            self.sc_seg = resample_image(self.sc_seg, binary=True)
 
         status, t2_star_orientation = sct.run('sct_orientation -i ' + self.t2star)
         self.original_orientation = t2_star_orientation[4:7]
@@ -53,70 +54,150 @@ class Pretreatments:
             self.level_fname = compute_level_file(self.t2star, self.sc_seg, self.t2, self.t2_seg, self.t2_landmarks)
 
 
-def main(target_fname, sc_seg_fname, t2_data, level_fname, param=None):
-    # t2_im = Image(t2_fname) if t2_fname is not None else None
-    before = time.time()
-    sct.printv('\nBuilding the appearance model...', verbose=param.verbose, type='normal')
-    model = Model(model_param=param, k=0.8)
+class FullGmSegmentation:
 
-    target_fname = check_file_to_niigz(target_fname)
-    sc_seg_fname = check_file_to_niigz(sc_seg_fname)
+    def __init__(self, target_fname, sc_seg_fname, t2_data, level_fname, ref_gm_seg=None, model=None, param=None):
 
-    tmp_dir = 'tmp_' + sct.extract_fname(target_fname)[1] + '_' + time.strftime("%y%m%d%H%M%S")
-    sct.run('mkdir ' + tmp_dir)
-    os.chdir(tmp_dir)
-
-    level_to_use = None
-    if level_fname is not None:
-        t2_data = None
-        if check_file_to_niigz('../' + level_fname):
-            sct.run('cp ../' + level_fname + ' .')
-            level_fname = sct.extract_fname(level_fname)[1]+sct.extract_fname(level_fname)[2]
-            sct.run('sct_orientation -i ' + level_fname + ' -s IRP')
-            level_to_use = sct.extract_fname(level_fname)[1] + '_IRP.nii.gz'
+        before = time.time()
+        self.param = param
+        sct.printv('\nBuilding the appearance model...', verbose=self.param.verbose, type='normal')
+        if model is None:
+            self.model = Model(model_param=self.param, k=0.8)
         else:
-            level_to_use = level_fname
+            self.model = model
+        sct.printv('\n--> OK !', verbose=self.param.verbose, type='normal')
 
-    sct.printv('\nDoing target pretreatments ...', verbose=param.verbose, type='normal')
-    pretreat = Pretreatments(target_fname, sc_seg_fname, t2_data)
-    if pretreat.level_fname is not None:
-        level_to_use = pretreat.level_fname
+        self.target_fname = check_file_to_niigz(target_fname)
+        self.sc_seg_fname = check_file_to_niigz(sc_seg_fname)
+        self.t2_data = t2_data
 
-    sct.printv('\nDoing target gray matter segmentation ...', verbose=param.verbose, type='normal')
-    gm_seg = GMsegSupervisedMethod(pretreat.treated_target, level_to_use, model, gm_seg_param=param)
+        self.ref_gm_seg = ref_gm_seg
 
-    sct.printv('\nDoing result post-treatments ...', verbose=param.verbose, type='normal')
-    square_mask = Image(pretreat.square_mask)
-    res_names = []
-    for res_im in [gm_seg.res_wm_seg, gm_seg.res_gm_seg, gm_seg.corrected_wm_seg]:
-        res_im_original_space = inverse_square_crop(res_im, square_mask)
-        res_im_original_space.save()
-        sct.run('sct_orientation -i ' + res_im_original_space.file_name + '.nii.gz -s RPI')
-        res_name = sct.extract_fname(target_fname)[1] + res_im.file_name[len(pretreat.treated_target[:-7]):] + '.nii.gz'
+        self.tmp_dir = 'tmp_' + sct.extract_fname(self.target_fname)[1] + '_' + time.strftime("%y%m%d%H%M%S")
+        sct.run('mkdir ' + self.tmp_dir)
+        os.chdir(self.tmp_dir)
 
-        if param.res_type == 'binary':
-            bin = True
+        level_to_use = None
+        if level_fname is not None:
+            t2_data = None
+            if check_file_to_niigz('../' + level_fname):
+                sct.run('cp ../' + level_fname + ' .')
+                level_fname = sct.extract_fname(level_fname)[1]+sct.extract_fname(level_fname)[2]
+                sct.run('sct_orientation -i ' + level_fname + ' -s IRP')
+                self.level_to_use = sct.extract_fname(level_fname)[1] + '_IRP.nii.gz'
+            else:
+                self.level_to_use = level_fname
+
+        self.gm_seg = None
+        self.res_names = {}
+        self.dice_name = None
+
+        self.segmentation_pipeline()
+        os.chdir('..')
+
+        after = time.time()
+        sct.printv('Done! (in ' + str(after-before) + ' sec) \nTo see the result, type :')
+        if self.param.res_type == 'binary':
+            wm_col = 'Red'
+            gm_col = 'Blue'
         else:
-            bin = False
-        old_res_name = resample_image(res_im_original_space.file_name + '_RPI.nii.gz', npx=pretreat.original_px, npy=pretreat.original_py, binary=bin)
+            wm_col = 'Blue-Lightblue'
+            gm_col = 'Red-Yellow'
+        sct.printv('fslview ' + self.target_fname + ' ' + self.res_names['wm_seg'] + ' -l ' + wm_col + ' -t 0.4 ' + self.res_names['gm_seg'] + ' -l ' + gm_col + ' -t 0.4 &', param.verbose, 'info')
 
-        if param.res_type == 'prob':
-            sct.run('fslmaths ' + old_res_name + ' -thr 0.05 ' + old_res_name)
+    # ------------------------------------------------------------------------------------------------------------------
+    def segmentation_pipeline(self):
+        sct.printv('\nDoing target pretreatments ...', verbose=self.param.verbose, type='normal')
+        self.pretreat = Pretreatments(self.target_fname, self.sc_seg_fname, self.t2_data)
+        if self.pretreat.level_fname is not None:
+            self.level_to_use = self.pretreat.level_fname
 
-        sct.run('cp ' + old_res_name + ' ../' + res_name)
+        sct.printv('\nDoing target gray matter segmentation ...', verbose=self.param.verbose, type='normal')
+        self.gm_seg = GMsegSupervisedMethod(self.pretreat.treated_target, self.level_to_use, self.model, gm_seg_param=self.param)
 
-        res_names.append(res_name)
+        sct.printv('\nDoing result post-treatments ...', verbose=self.param.verbose, type='normal')
+        self.post_treatments()
 
-    os.chdir('..')
-    after = time.time()
-    sct.printv('Done! (in ' + str(after-before) + ' sec) \nTo see the result, type :')
-    if param.res_type == 'binary':
-        wm_col = 'Red'
-        gm_col = 'Blue'
-    else:
-        wm_col = 'Blue-Lightblue'
-        gm_col = 'Red-Yellow'
-    sct.printv('fslview ' + target_fname + ' ' + res_names[0] + ' -l ' + wm_col + ' -t 0.4 ' + res_names[1] + ' -l ' + gm_col + ' -t 0.4 &', param.verbose, 'info')
+        if self.ref_gm_seg is not None:
+            sct.printv('Computing Dice coefficient ...', verbose=self.param.verbose, type='normal')
+            self.dice_name = self.validation()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def post_treatments(self):
+        square_mask = Image(self.pretreat.square_mask)
+        tmp_res_names = []
+        for res_im in [self.gm_seg.res_wm_seg, self.gm_seg.res_gm_seg, self.gm_seg.corrected_wm_seg]:
+            res_im_original_space = inverse_square_crop(res_im, square_mask)
+            res_im_original_space.save()
+            sct.run('sct_orientation -i ' + res_im_original_space.file_name + '.nii.gz -s RPI')
+            res_name = sct.extract_fname(self.target_fname)[1] + res_im.file_name[len(self.pretreat.treated_target[:-7]):] + '.nii.gz'
+
+            if self.param.res_type == 'binary':
+                bin = True
+            else:
+                bin = False
+            old_res_name = resample_image(res_im_original_space.file_name + '_RPI.nii.gz', npx=self.pretreat.original_px, npy=self.pretreat.original_py, binary=bin)
+
+            if self.param.res_type == 'prob':
+                sct.run('fslmaths ' + old_res_name + ' -thr 0.05 ' + old_res_name)
+
+            sct.run('cp ' + old_res_name + ' ../' + res_name)
+
+            tmp_res_names.append(res_name)
+        self.res_names['wm_seg'] = tmp_res_names[0]
+        self.res_names['gm_seg'] = tmp_res_names[1]
+        self.res_names['corrected_wm_seg'] = tmp_res_names[2]
+
+    def validation(self):
+        name_ref_gm_seg = sct.extract_fname(self.ref_gm_seg)
+        im_ref_gm_seg = Image('../' + self.ref_gm_seg)
+
+        res_gm_seg_bin = Image('../' + self.res_names['gm_seg'])
+        res_wm_seg_bin = Image('../' + self.res_names['wm_seg'])
+
+        sct.run('cp ../' + self.ref_gm_seg + ' ./ref_gm_seg.nii.gz')
+        im_ref_wm_seg = inverse_gmseg_to_wmseg(im_ref_gm_seg, Image('../' + self.sc_seg_fname), 'ref_gm_seg')
+        im_ref_wm_seg.file_name = 'ref_wm_seg'
+        im_ref_wm_seg.ext = '.nii.gz'
+        im_ref_wm_seg.save()
+
+        if self.param.res_type == 'prob':
+            res_gm_seg_bin.data = np.asarray((res_gm_seg_bin.data >= 0.5).astype(int))
+            res_wm_seg_bin.data = np.asarray((res_wm_seg_bin.data >= 0.50001).astype(int))
+
+        res_gm_seg_bin.path = './'
+        res_gm_seg_bin.file_name = 'res_gm_seg_bin'
+        res_gm_seg_bin.ext = '.nii.gz'
+        res_gm_seg_bin.save()
+        res_wm_seg_bin.path = './'
+        res_wm_seg_bin.file_name = 'res_wm_seg_bin'
+        res_wm_seg_bin.ext = '.nii.gz'
+        res_wm_seg_bin.save()
+        try:
+            status, output_gm = sct.run('sct_dice_coefficient ref_gm_seg.nii.gz res_gm_seg_bin.nii.gz  -2d-slices 2')
+        except Exception:
+            sct.run('c3d ref_gm_seg.nii.gz -reslice-identity -o ref_in_res_space_gm.nii.gz ')
+            status, output_gm = sct.run('sct_dice_coefficient ref_in_res_space_gm.nii.gz res_gm_seg_bin.nii.gz  -2d-slices 2')
+        try:
+            status, output_wm = sct.run('sct_dice_coefficient ref_wm_seg.nii.gz res_wm_seg_bin.nii.gz  -2d-slices 2')
+        except Exception:
+            sct.run('c3d ref_wm_seg.nii.gz -reslice-identity -o ref_in_res_space_wm.nii.gz ')
+            status, output_wm = sct.run('sct_dice_coefficient ref_in_res_space_wm.nii.gz res_wm_seg_bin.nii.gz  -2d-slices 2')
+        dice_name = 'dice_' + self.param.res_type + '.txt'
+        dice_fic = open('../' + dice_name, 'w')
+        if self.param.res_type == 'prob':
+            dice_fic.write('WARNING : the probabilistic segmentations were binarized with a threshold at 0.5 to compute the dice coefficient \n')
+        dice_fic.write('\n--------------------------------------------------------------\nDice coefficient on the Gray Matter segmentation:\n')
+        dice_fic.write(output_gm)
+        dice_fic.write('\n\n--------------------------------------------------------------\nDice coefficient on the White Matter segmentation:\n')
+        dice_fic.write(output_wm)
+        dice_fic.close()
+        # sct.run(' mv ./' + dice_name + ' ../')
+
+        return dice_name
+
+
+
 
 
 
@@ -130,6 +211,7 @@ if __name__ == "__main__":
     input_sc_seg_fname = None
     input_t2_data = None
     input_level_fname = None
+    input_ref_gm_seg = None
     if param.debug:
         print '\n*** WARNING: DEBUG MODE ON ***\n'
         fname_input = param.path_dictionary + "/errsm_34.nii.gz"
@@ -180,6 +262,11 @@ if __name__ == "__main__":
                           mandatory=False,
                           default_value='binary',
                           example=['binary', 'prob'])
+        parser.add_option(name="-ref",
+                          type_value="file",
+                          description="Reference segmentation of the gray matter",
+                          mandatory=False,
+                          example='manual_gm_seg.nii.gz')
         parser.add_option(name="-v",
                           type_value="int",
                           description="verbose: 0 = nothing, 1 = classic, 2 = expended",
@@ -201,7 +288,9 @@ if __name__ == "__main__":
             param.first_reg = bool(int(arguments["-first-reg"]))
         if "-res-type" in arguments:
             param.res_type = arguments["-res-type"]
+        if "-ref" in arguments:
+            input_ref_gm_seg = arguments["-ref"]
         if "-v" in arguments:
             param.verbose = arguments["-v"]
 
-    main(input_target_fname, input_sc_seg_fname, input_t2_data, input_level_fname, param)
+    gmsegfull = FullGmSegmentation(input_target_fname, input_sc_seg_fname, input_t2_data, input_level_fname, input_ref_gm_seg, param)
