9754b6951d20b73c992702ebe459731cd8f5c590
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-06-23 16:13:05 -0400

    NEW: updated gm seg registration

--- scripts/sct_segment_graymatter_old.py
+++ scripts/sct_segment_graymatter_old.py
@@ -10,70 +10,39 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 import sct_utils as sct
+from msct_parser import Parser
 import os
 import time
 import sys
 import getopt
 
 
-def main():
-    fname_ref = ''
-    fname_moving = ''
-    transformation = 'SyN'
-    metric = 'CC'
-    gradient_step = '0.2'
-    radius = '5'
-    iteration='20x15'
-    fname_seg_fixed = ''
-    fname_seg_moving = ''
-    fname_output = ''
-    padding = '10'
+class Param:
+    def __init__(self):
+        self.debug = False
+        self.fname_ref = ''
+        self.fname_moving = ''
+        self.transformation = 'SyN'
+        self.metric = 'MI'  # 'MeanSquares'  # 'CC'
+        self.gradient_step = '0.5'
+        self.radius = '4'
+        self.iteration='20x15'
+        self.fname_seg_fixed = ''
+        self.fname_seg_moving = ''
+        self.fname_output = ''
+        self.padding = '10'
+
+        self.remove_temp = 1
+
+def main(param):
 
     moving_name = 'moving'
     fixed_name = 'fixed'
     moving_seg_name = 'moving_seg'
     fixed_seg_name = 'fixed_seg'
 
-    remove_temp = 1
-
-    # Check input param
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],'hi:d:t:s:g:o:')
-    except getopt.GetoptError as err:
-        print str(err)
-        usage()
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-        elif opt in ('-d'):
-            fname_moving = arg
-        elif opt in ('-i'):
-            fname_ref = arg
-        elif opt in ('-t'):
-            transformation = arg
-        elif opt in ('-s'):
-            fname_seg_fixed = arg
-        elif opt in ('-g'):
-            fname_seg_moving = arg
-        elif opt in ('-o'):
-            fname_output = arg
-
-    if fname_moving == '' or fname_ref == '':
-        usage()
-
-    # check existence of input files
-    sct.check_file_exist(fname_ref)
-    sct.check_file_exist(fname_moving)
-    if (fname_seg_moving != '' and fname_seg_fixed == '') or (fname_seg_moving == '' and fname_seg_fixed != ''):
-        print('\nERROR: You need to provide one mask for each image (moving and fixed)')
-        usage()
-    if fname_seg_moving != '':
-        sct.check_file_exist(fname_seg_moving)
-    if fname_seg_fixed != '':
-        sct.check_file_exist(fname_seg_fixed)
-
     # Extract path/file/extension
-    path_output, file_output, ext_output = sct.extract_fname(fname_output)
+    path_output, file_output, ext_output = sct.extract_fname(param.fname_output)
 
     # create temporary folder
     print('\nCreate temporary folder...')
@@ -82,12 +51,12 @@ def main():
 
     # copy files to temporary folder
     print('\nCopy files...')
-    sct.run("sct_c3d "+fname_moving+" -o "+path_tmp+"/"+moving_name+".nii")
-    sct.run("sct_c3d "+fname_ref+" -o "+path_tmp+"/"+fixed_name+".nii")
-    if fname_seg_moving != '':
-        sct.run("sct_c3d "+fname_seg_moving+" -o "+path_tmp+"/"+moving_seg_name+".nii")
-    if fname_seg_fixed != '':
-        sct.run("sct_c3d "+fname_seg_fixed+" -o "+path_tmp+"/"+fixed_seg_name+".nii")
+    sct.run("c3d "+param.fname_moving+" -o "+path_tmp+"/"+moving_name+".nii")
+    sct.run("c3d "+param.fname_ref+" -o "+path_tmp+"/"+fixed_name+".nii")
+    if param.fname_seg_moving != '':
+        sct.run("c3d "+param.fname_seg_moving+" -o "+path_tmp+"/"+moving_seg_name+".nii")
+    if param.fname_seg_fixed != '':
+        sct.run("c3d "+param.fname_seg_fixed+" -o "+path_tmp+"/"+fixed_seg_name+".nii")
 
     # go to tmp folder
     os.chdir(path_tmp)
@@ -106,10 +75,9 @@ def main():
     # cropping in x & y directions
     fixed_name_temp = fixed_name + "_crop"
     cmd = "sct_crop_image -i " + fixed_name + ".nii -o " + fixed_name_temp + ".nii -m " + fixed_seg_name + ".nii -shift 10,10 -dim 0,1"
-    print cmd
     sct.run(cmd)
     fixed_name = fixed_name_temp
-    if fname_seg_fixed != '':
+    if param.fname_seg_fixed != '':
         fixed_seg_name_temp = fixed_seg_name+"_crop"
         sct.run("sct_crop_image -i " + fixed_seg_name + ".nii -o " + fixed_seg_name_temp + ".nii -m " + fixed_seg_name + ".nii -shift 10,10 -dim 0,1")
         fixed_seg_name = fixed_seg_name_temp
@@ -119,17 +87,17 @@ def main():
     # padding the images
     moving_name_temp = moving_name+"_pad"
     fixed_name_temp = fixed_name+"_pad"
-    sct.run("sct_c3d "+moving_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+moving_name_temp+".nii")
-    sct.run("sct_c3d "+fixed_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+fixed_name_temp+".nii")
+    sct.run("c3d "+moving_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_name_temp+".nii")
+    sct.run("c3d "+fixed_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_name_temp+".nii")
     moving_name = moving_name_temp
     fixed_name = fixed_name_temp
-    if fname_seg_moving != '':
+    if param.fname_seg_moving != '':
         moving_seg_name_temp = moving_seg_name+"_pad"
-        sct.run("sct_c3d "+moving_seg_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+moving_seg_name_temp+".nii")
+        sct.run("c3d "+moving_seg_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_seg_name_temp+".nii")
         moving_seg_name = moving_seg_name_temp
-    if fname_seg_fixed != '':
+    if param.fname_seg_fixed != '':
         fixed_seg_name_temp = fixed_seg_name+"_pad"
-        sct.run("sct_c3d "+fixed_seg_name+".nii -pad 0x0x"+padding+"vox 0x0x"+padding+"vox 0 -o "+fixed_seg_name_temp+".nii")
+        sct.run("c3d "+fixed_seg_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_seg_name_temp+".nii")
         fixed_seg_name = fixed_seg_name_temp
 
     # binarise the moving image
@@ -174,28 +142,28 @@ def main():
     # registration of the grey matter
     print('\nDeforming the image...')
     moving_name_temp = moving_name+"_deformed"
-    cmd = "sct_antsRegistration --dimensionality 3 --transform "+ transformation +"["+gradient_step+",3,0] --metric "+metric+"["+fixed_name+".nii,"+moving_name+".nii,1,"+radius+"] --convergence "+iteration+" --shrink-factors 2x1 --smoothing-sigmas 0mm --Restrict-Deformation 1x1x0 --output ["+moving_name_temp+","+moving_name_temp+".nii]"
-    if fname_seg_moving != '':
+    cmd = "antsRegistration --dimensionality 3 --transform "+ param.transformation +"["+param.gradient_step+",3,0] --metric "+param.metric+"["+fixed_name+".nii,"+moving_name+".nii,1,"+param.radius+"] --convergence "+param.iteration+" --shrink-factors 2x1 --smoothing-sigmas 0mm --Restrict-Deformation 1x1x0 --output ["+moving_name_temp+","+moving_name_temp+".nii]"
+    if param.fname_seg_moving != '':
         cmd += " --masks ["+fixed_seg_name+".nii,"+moving_seg_name+".nii]"
     sct.run(cmd)
     moving_name = moving_name_temp
 
     moving_name_temp = moving_name+"_unpadded"
-    sct.run("sct_crop_image -i "+moving_name+".nii -dim 2 -start "+padding+" -end -"+padding+" -o "+moving_name_temp+".nii")
+    sct.run("sct_crop_image -i "+moving_name+".nii -dim 2 -start "+param.padding+" -end -"+param.padding+" -o "+moving_name_temp+".nii")
     sct.run("mv "+moving_name+"0Warp.nii.gz "+file_output+"0Warp"+ext_output)
     sct.run("mv "+moving_name+"0InverseWarp.nii.gz "+file_output+"0InverseWarp"+ext_output)
     moving_name = moving_name_temp
 
     # TODO change "fixed.nii"
     moving_name_temp = file_output+ext_output
-    #sct.run("sct_c3d "+fixed_name+".nii "+file_output+ext_output+" -reslice-identity  -o "+file_output+'_register'+ext_output)
-    sct.run("sct_c3d fixed.nii "+moving_name+".nii -reslice-identity -o "+file_output+ext_output)
+    #sct.run("c3d "+fixed_name+".nii "+file_output+ext_output+" -reslice-identity  -o "+file_output+'_register'+ext_output)
+    sct.run("c3d fixed.nii "+moving_name+".nii -reslice-identity -o "+file_output+ext_output)
 
     # move output files to initial folder
     sct.run("cp "+file_output+"* ../")
 
     # remove temporary file
-    if remove_temp == 1:
+    if param.remove_temp == 1:
         os.chdir('../')
         print('\nRemove temporary files...')
         sct.run("rm -rf "+path_tmp)
@@ -203,38 +171,79 @@ def main():
     return
 
 
-#=======================================================================================================================
-# usage
-#=======================================================================================================================
-def usage():
-    print '\n' \
-        ''+os.path.basename(__file__)+'\n' \
-        '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' \
-        'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n' \
-        '\n'\
-        'DESCRIPTION\n' \
-        '  This function returns the grey matter segmentation.\n' \
-        '\n'\
-        'USAGE\n' \
-        '  '+os.path.basename(__file__)+' -i <ref> -d <moving> -o <output>\n' \
-        '  '+os.path.basename(__file__)+' -i <anat volume> -t <t2star volume> -l <landmarks>\n' \
-        '\n'\
-        'MANDATORY ARGUMENTS\n' \
-        '  -i                   input image (with white/gray matter contrast).\n' \
-        '  -d                   moving image.\n' \
-        '  -o                   output name (for warping field and image).\n' \
-        'OPTIONAL ARGUMENTS\n' \
-        '  -t                   transformation {SyN, BSplineSyN}.\n' \
-        '  -s                   input image segmentation.\n' \
-        '  -g                   moving image segmentation.\n'
-    sys.exit(2)
-
-
-
 if __name__ == "__main__":
-    # initialize parameters
-    # param = Param()
-    # call main function
-    main()
+    param = Param()
+    if param.debug:
+        print '\n*** WARNING: DEBUG MODE ON ***\n'
+    else:
+        param_default = Param()
+
+        # Initialize the parser
+        parser = Parser(__file__)
+        parser.usage.set_description('Register the template on a gray matter segmentation')
+        parser.add_option(name="-i",
+                          type_value="file",
+                          description="Fixed image",  # TODO : specify which image to use
+                          mandatory=True,
+                          example='wm_seg.nii.gz')
+        parser.add_option(name="-d",
+                          type_value="file",
+                          description="moving image",  # TODO : specify which image to use
+                          mandatory=True,
+                          example='MNI-Poly-AMU_WM.nii.gz')
+        parser.add_option(name="-o",
+                          type_value="str",
+                          description="output name",  # TODO : specify which image to use
+                          mandatory=False,
+                          example='moving_to_fixed.nii.gz')
+        parser.add_option(name="-s",
+                          type_value="file",
+                          description="Spinal cord segmentation of the fixed image",  # TODO : specify which image to use
+                          mandatory=True,
+                          example='sc_seg.nii.gz')
+        parser.add_option(name="-g",
+                          type_value="file",
+                          description="Spinal cord segmentation of the moving image",  # TODO : specify which image to use
+                          mandatory=True,
+                          example='sc_seg.nii.gz')
+        parser.add_option(name="-t",
+                          type_value='multiple_choice',
+                          description="type of transformation",
+                          mandatory=False,
+                          default_value='SyN',
+                          example=['SyN', 'BSplineSyN'])
+        parser.add_option(name="-m",
+                          type_value='multiple_choice',
+                          description="Metric used for the registration",
+                          mandatory=False,
+                          default_value='MeanSquares',
+                          example=['CC', 'MeanSquares'])
+        parser.add_option(name="-r",
+                          type_value='multiple_choice',
+                          description="Remove temporary files",
+                          mandatory=False,
+                          default_value=1,
+                          example=['0', '1'])
+
+        arguments = parser.parse(sys.argv[1:])
+
+
+        param.fname_ref = arguments["-i"]
+        param.fname_seg_fixed = arguments["-s"]
+        param.fname_moving = arguments["-d"]
+        param.fname_seg_moving = arguments["-g"]
+
+        if "-o" in arguments:
+            param.fname_output = arguments["-o"]
+        else:
+            param.fname_output = sct.extract_fname(param.fname_moving)[1] + '_to_' + sct.extract_fname(param.fname_ref)[1] +  sct.extract_fname(param.fname_ref)[2]
+        if "-t" in arguments:
+            param.transformation = arguments["-t"]
+        if "-m" in arguments:
+            param.metric = arguments["-m"]
+        if "-r" in arguments:
+            param.remove_temp = int(arguments["-r"])
+
+    main(param)
 
 
