d661de09b4d1bbce0145f93a99973cfb4d76e6bf
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-06-30 17:58:53 -0400

    DEV: small changes to allow quick testing
    Former-commit-id: e8e8ec5ebb7f78d3472610c9899d64f0998ad469
    Former-commit-id: e25d1d412c92a44e27783ec8e9ba5d334ebc7bdf

--- dev/tamag/msct_register_regularized.py
+++ dev/tamag/msct_register_regularized.py
@@ -181,8 +181,6 @@ def register_images(im_input, im_dest, mask='', paramreg=Paramreg(step='0', type
                 f = 'transform_' +num+ '0GenericAffine.mat'
                 matfile = loadmat(f, struct_as_record=True)
                 array_transfo = matfile['AffineTransform_double_2_2']
-                if i == 20 or i == 40:
-                    print i
                 x_displacement[i] = array_transfo[4][0]  # Tx in ITK'S coordinate system
                 y_displacement[i] = array_transfo[5][0]  # Ty  in ITK'S and fslview's coordinate systems
                 theta_rotation[i] = asin(array_transfo[2]) # angle of rotation theta in ITK'S coordinate system (minus theta for fslview)
@@ -362,7 +360,8 @@ def generate_warping_field(im_dest, x_trans, y_trans, theta_rot=None, center_rot
     # Passer en mode matrice   (checker le signe de matrix_def_3)
     if theta_rot == None and matrix_def != None:
         vector_i = [[[i-x_a],[j-y_a]] for i in range(nx) for j in range(ny)]
-        matrix_def_a = asarray(matrix_def)
+        #matrix_def_a = asarray(matrix_def)
+        matrix_def_a = asarray([[[matrix_def[j][0][0],matrix_def[j][0][1]],[matrix_def[j][1][0],matrix_def[j][1][1]]] for j in range(len(matrix_def))])
         for k in range(nz):
             tmp = matrix_def_a[k] + array(((-1,0),(0,-1)))
             result = dot(tmp, array(vector_i).T[0]) + array([[x_trans[k]], [y_trans[k]]])
