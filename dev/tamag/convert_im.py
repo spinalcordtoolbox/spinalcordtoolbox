ec101689b9f7eec0cd827c5e8d3ac5b973a94de2
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-07-01 18:26:40 -0400

    DEV:
    Former-commit-id: 1a567f7da4794eb5919618e39b0f5ff709651105
    Former-commit-id: e37f3d91724071dc6eabba2b817d77b7aa8104cd

--- dev/tamag/convert_im.py
+++ dev/tamag/convert_im.py
@@ -14,6 +14,7 @@ import matplotlib.cm as cm
 import numpy as np
 from PIL import Image
 from scipy.misc import imread, imsave
+from copy import copy
 
 def rgb2gray(rgb):
 
@@ -22,32 +23,105 @@ def rgb2gray(rgb):
 
     return gray
 
+def sorting_value_of_zone(zone):
+    group_value=[[[]],[[]]]  # liste(liste_value, liste_nb_of_this-value)
+    for i in range(len(zone)):
+        if zone[i] not in group_value[0][0]:
+            group_value[0][0].append(zone[i])
+            group_value[1][0].append(1)
+        else:
+            index = group_value[0][0].index(zone[i])
+            group_value[1][0][index] += 1
+    return group_value
 
-os.chdir('/Users/tamag/Desktop/GM_atlas/def_new_atlas')
+os.chdir('/Users/tamag/Desktop/GM_atlas/def_new_atlas/test_registration')
 # img_1 = mpimg.imread('gm_white_inv.png')
 # img = mpimg.imread('greyscale_select.png')
 
 #grey = rgb2gray(img)
 
-fname = 'greyscale_select_smooth.png'
+
+
+
+# # tests
+# list_test = [1,2,3,2,3,3,3,3,3,4]
+# output = sorting_value_of_zone(list_test)
+# bla
+
+# f = 80
+# a = abs(0 - f)
+# b = abs(45 - f)
+# c = abs(100 - f)
+# d = abs(170 - f)
+# e = abs(255 - f)
+# if a == min(a,b,c,d,e):
+#     g = 0
+# elif b == min(a,b,c,d,e):
+#     g = 45
+# elif c == min(a,b,c,d,e):
+#     g = 100
+# elif d == min(a,b,c,d,e):
+#     g = 170
+# elif e == min(a,b,c,d,e):
+#     g = 255
+
+fname = 'gm_white_resampled_registered.png'
 image = Image.open(fname).convert("L")
 arr = np.asarray(image)
-print arr[550,550]
-#arr_bin_2 = np.zeros((arr.shape[0]-98, arr.shape[1]-95), dtype=np.uint8)
-arr_bin = np.zeros((arr.shape[0], arr.shape[1]), dtype=np.uint8)
-for i in range(arr.shape[0]):
-    for j in range(arr.shape[1]):
-        if 200 > arr[i,j] > 20:
-            arr_bin[i,j] = 255
-        else: arr_bin[i,j] = 0
 
-# for i in range(arr_bin_2.shape[0]):
-#     for j in range(arr_bin_2.shape[1]):
-#         arr_bin_2[i,j] = arr[i+7, j+20]
+# avoid transition between zones
+a = 0
+b = 45
+c = 100
+d = 170
+e = 255
+list = [a,b,c,d,e]
+#arr_bin_2 = np.zeros((arr.shape[0]-98, arr.shape[1]-95), dtype=np.uint8)
+# arr_bin = np.zeros((arr.shape[0], arr.shape[1]), dtype=np.uint8)
+# for i in range(arr.shape[0]):
+#     for j in range(arr.shape[1]):
+#         if arr[i,j] not in list:
+#             # regarder zone 5x5 autour
+#             zone = [arr[i+k,j+h] for k in range(-5,5) for h in range(-5,5)]
+#             group_value = sorting_value_of_zone(zone)
+#             nb_value_searched = max(group_value[1][0])
+#             index_value_searched = group_value[1][0].index(nb_value_searched)
+#             value_searched = group_value[0][0][index_value_searched]
+#             if value_searched == 254:
+#                 value_searched +=1
+#             if value_searched == 1:
+#                 value_searched -=1
+#             if value_searched not in list:
+#                 print i,j, value_searched
+#             arr_bin[i,j] = value_searched
+#         else:
+#             arr_bin[i,j] = arr[i,j]
+
+
+arr_bin = np.zeros((451, 613), dtype=np.uint8)
+for i in range(451):
+    # h = 74+i
+    for j in range(613):
+        # g = 103+j
+
+        arr_bin[i,j] = arr[74+i,103+j]
+
+
+# middle_y = int(round(arr.shape[1]/2.0))
+# for i in range(arr.shape[0]):
+#     for j in range(middle_y,arr.shape[1]):
+#         if arr[i,j] == 45:
+#             arr_bin[i,j] = 150
+#         elif arr[i,j] == 80:
+#             arr_bin[i,j] = 190
+#         elif arr[i,j] == 120:
+#             arr_bin[i,j] = 220
+        # else: arr_bin[i,j] = arr[i,j]
+
 
 #arr_bin = arr[:, :] > 20
 im = Image.fromarray(arr_bin)
-im.save('gm_white.png')
+im.save('gm_white_resampled_registered_crop.png')
 # #
 # #
 # # plt.imshow(arr_bin, cmap=cm.binary)
