f0314093c8ab75e8a279f2d712ea682a85085a0b
Tanguy Magnan <tanguy.magnan@gmail.com>
2015-07-14 16:25:29 -0400

    DEV: script for generation of GM atlas
    Former-commit-id: 523b40c25df6d2fd1747c6bd3f1b9ad4297dfd25
    Former-commit-id: 9ff5c1abe623eab14f703058f02d320dc7a76b99

--- dev/GM_atlas/concatenate_WM_and_GM_tracts.py
+++ dev/GM_atlas/concatenate_WM_and_GM_tracts.py
@@ -17,65 +17,44 @@ from scipy.misc import imread, imsave
 from copy import copy
 
 
-path = '/Users/tamag/Desktop/GM_atlas/def_new_atlas/correct_images/results'
-fname = 'greyscale_antisym_left_corrected.png'
-fname1 = 'greyscale_final.png'
-fname2 = 'white_WM_mask_resampled_registered_crop_resized.png'
-fname3 = 'white_GM_mask_resampled_registered_crop_resized.png'
-fname4 = 'greyscale_final_resampled_registered_crop_resized.png'
-fname5 = 'greyscale_final_reg_no_trans_corr.png'
-fname6 = 'atlas_grays_cerv_sym_correc_r5.png'
-fname7 = 'greyscale_final_for_atlas.png'
+path = '/Users/tamag/code/spinalcordtoolbox/dev/GM_atlas/raw_data/test'
+fname1 = 'greyscale_final_resampled_registered_crop_resized.png'
+fname2 = 'atlas_grays_cerv_sym_correc_r5.png'
+fname3 = 'greyscale_reg_no_trans.png'
+fname4 = 'greyscale_reg_no_trans_sym.png'
+# input: (images must be oriented horizontally with the image viewer)
+#   -atlas_grays_cerv_sym_correc_r5.png
+#   -greyscale_final_resampled_registered_crop_resized.png
+# output:
+#   -concatenation.png
+
 
 def main():
 
     os.chdir(path)
-    # #WM_mask(fname1,name_output='white_WM_mask.png')
-    # anti_trans(fname2, list=[0,255], name_output='white_WM_mask_reg_notrans.png')
-    # anti_trans(fname3, list=[0,255], name_output='white_GM_mask_reg_notrans.png')
-    # anti_trans(fname4, list=[0,44,80,120,150,190,220,255], name_output='greyscale_final_reg_no_trans.png')
-    # #correction by hand of greyscale (output:greyscale_final_reg_no_trans_corr.png)
-    # antisym_im(fname5,name_output='greyscale_final_for_atlas.png')
-    # WM_mask('greyscale_final_for_atlas.png',name_output='WM_mask_for_atlas.png')
-    # GM_mask('greyscale_final_for_atlas.png',name_output='GM_mask_for_atlas.png')
-    im_1 = Image.open(fname6).convert("L")
-    arr_1 = np.asarray(im_1)
-    arr_4 = np.zeros((arr_1.shape[0], arr_1.shape[1]), dtype=np.uint8)
-    for i in range(arr_1.shape[0]):
-        for j in range(arr_1.shape[1]):
-            if arr_1[i,j] < 235 or arr_1[i,j]==255:
-                arr_4[i,j] = arr_1[i,j]
-            else: arr_4[i,j] = 0
-    im_4 = Image.fromarray(arr_4)
-    im_4.save('b.png')
-    im_2 = Image.open(fname7).convert("L")
-    arr_2 = np.asarray(im_2)
-    arr_3 = np.zeros((arr_1.shape[0], arr_1.shape[1]), dtype=np.uint8)
-    for i in range(arr_1.shape[0]):
-        for j in range(arr_2.shape[1]):
-            if arr_2[i,j] < 240:
-                arr_3[i,j] = arr_2[i,j]
-            else: arr_3[i,j] = 0
-    im_3 = Image.fromarray(arr_3)
-    im_3.save('a.png')
-    #arr_o = arr_1 + arr_3
-    arr_o = copy(arr_4)
-    for i in range(arr_1.shape[0]):
-        for j in range(arr_1.shape[1]):
-            if arr_4[i,j] == 0:
-                arr_o[i,j] = arr_2[i,j]
-    im_o = Image.fromarray(arr_o)
-    im_o.save('addition.png')
+
+    ##process:
+
+
+    #anti_trans: input: greyscale_final_resampled_registered_crop_resized.png    output: greyscale_reg_no_trans.png (old: greyscale_final_reg_no_trans.png)
+    print '\nReplacing transition pixel between zones...'
+    anti_trans(fname1, list=[0,44,80,120,150,190,220,255], name_output=fname3)
+
+    #copy left side on right side: input: greyscale_reg_no_trans.png  output: greyscale_reg_no_trans_sym.png
+    print '\nCopying left side of the image on the right side with change of values...'
+    antisym_im(fname3, name_output=fname4)
+
+
+    #concatenation of GM and WM tracts: inputs: atlas_grays_cerv_sym_correc_r5.png  and  greyscale_reg_no_trans_sym.png  output: concatenation.png
+    print '\nConcatenating WM and GM tracts...'
+    concatenate_WM_and_GM(WM_file=fname2, GM_file=fname4, name_output='concatenation.png')
+
+    #small hand corrections:  input: concatenation.png  output: concatenation_corrected.png
+
 
 
 # avoid transition between zones
 def anti_trans(fname, list=[0,45,100,170,255],name_output='notrans.png'):
-    # a = 0
-    # b = 45
-    # c = 100
-    # d = 170
-    # e = 255
-    # list = [a,b,c,d,e]
     im_i = Image.open(fname).convert("L")
     arr = np.asarray(im_i)
     arr_bin = np.zeros((arr.shape[0], arr.shape[1]), dtype=np.uint8)
@@ -101,6 +80,45 @@ def anti_trans(fname, list=[0,45,100,170,255],name_output='notrans.png'):
     im_o.save(name_output)
 
 
+# Concatenate WM and GM tracts
+def concatenate_WM_and_GM(WM_file, GM_file, name_output='concatenation.png'):
+    #Open file
+    im_1 = Image.open(WM_file).convert("L")
+    im_2 = Image.open(GM_file).convert("L")
+
+    # Take array
+    arr_1 = np.asarray(im_1)
+    arr_2 = np.asarray(im_2)
+    arr_3 = np.zeros((arr_1.shape[0], arr_1.shape[1]), dtype=np.uint8)
+    arr_4 = np.zeros((arr_1.shape[0], arr_1.shape[1]), dtype=np.uint8)
+
+    # Set GM area of WM_file to zero
+    for i in range(arr_1.shape[0]):
+        for j in range(arr_1.shape[1]):
+            if arr_1[i,j] < 235 or arr_1[i,j]==255:
+                arr_4[i,j] = arr_1[i,j]
+            else: arr_4[i,j] = 0
+    im_4 = Image.fromarray(arr_4)
+    im_4.save('WM_file_GM_to_0.png')
+
+    # Set WM area of GM_file to zero
+    for i in range(arr_1.shape[0]):
+        for j in range(arr_2.shape[1]):
+            if arr_2[i,j] < 240:
+                arr_3[i,j] = arr_2[i,j]
+            else: arr_3[i,j] = 0
+    im_3 = Image.fromarray(arr_3)
+    im_3.save('GM_file_WM_to_zero.png')
+
+    # Concatenate the two files
+    arr_o = copy(arr_4)
+    for i in range(arr_1.shape[0]):
+        for j in range(arr_1.shape[1]):
+            if arr_4[i,j] == 0:
+                arr_o[i,j] = arr_2[i,j]
+    im_o = Image.fromarray(arr_o)
+    im_o.save(name_output)
+
 # Make an antisymetric image
 def antisym_im(fname, name_output='antisym.png'):
     im_i = Image.open(fname).convert("L")
@@ -108,7 +126,7 @@ def antisym_im(fname, name_output='antisym.png'):
     arr_bin = copy(arr)
     middle_y = int(round(arr.shape[1]/2.0))
     for i in range(arr.shape[0]):
-        for j in range(0,middle_y-1):
+        for j in range(0,middle_y):
             if arr[i,j] == 150:
                 arr_bin[i,-j-1] = 45
             elif arr[i,j] == 190:
