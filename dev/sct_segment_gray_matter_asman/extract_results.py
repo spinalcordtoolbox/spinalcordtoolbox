297c8d8847294ca9d0026ddbc4d25dfe4e66fb32
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-07-24 14:53:39 -0400

    OPT: small modification to usefull scripts
    Former-commit-id: 3c6fa41c032a80b6596b24ea91b2247df4770974 [formerly 0026f81660b8dfece15f90b7c4b12365c2d70cca] [formerly 47fca2d48d58058cc734810b38df60a13395fe64 [formerly bd1c659c40b7ef1ca90cdd29c7817276eead8421 [formerly a8f92a51a5052718bd7b78257133784cb3f053de]]]
    Former-commit-id: 3c6fa41c032a80b6596b24ea91b2247df4770974 [formerly 0026f81660b8dfece15f90b7c4b12365c2d70cca]
    Former-commit-id: 4e2b5fdf0fce38f68135c4f80d1fdbcba5cf8959 [formerly ceb301d13118ec9a01e37cd6ab1878d7a8594e1d [formerly 146874b8c9cf61b6b4133905b9a1fc7ab500d6c4]]
    Former-commit-id: 2b9144bd1ddebc4a9fdd8a6b41bb5c53da1a3c8a
    Former-commit-id: 0081246f9d5467eb0a7556dc0fa1218ffe759f9d

--- dev/sct_segment_gray_matter_asman/extract_results.py
+++ dev/sct_segment_gray_matter_asman/extract_results.py
@@ -70,7 +70,7 @@ for loocv_dir in os.listdir(path):
 
 workbook.close()
 
-# ############################# MULTIPLE LOOCV FROM MAGMA :  get level and n_sice data #############################
+# ############################# MULTIPLE LOOCV FROM MAGMA :  get n_sice data #############################
 workbook2 = xl.Workbook('results_wmseg_dices_by_n_slices.xlsx')
 worksheet_36 = workbook2.add_worksheet('36subjects')
 worksheet_28 = workbook2.add_worksheet('28subjects')
@@ -132,7 +132,7 @@ for loocv_dir in os.listdir(path):
 workbook2.close()
 
 
-# ############################# MULTIPLE LOOCV FROM MAGMA :  get level and n_sice data #############################
+# ############################# MULTIPLE LOOCV FROM MAGMA :  get level data #############################
 workbook3 = xl.Workbook('results_wmseg_dices_by_level.xlsx')
 worksheet_36 = workbook3.add_worksheet('36subjects')
 worksheet_28 = workbook3.add_worksheet('28subjects')
@@ -195,7 +195,73 @@ for loocv_dir in os.listdir(path):
 workbook3.close()
 '''
 
+# ############################# MULTIPLE LOOCV FROM MAGMA - HAUSDORFF DISTANCE #############################
+# Create a workbook and add a worksheet.
+workbook = xl.Workbook('results_wmseg_dices.xlsx')
+worksheet_36 = workbook.add_worksheet('36subjects')
+worksheet_28 = workbook.add_worksheet('28subjects')
+worksheet_20 = workbook.add_worksheet('20subjects')
+worksheet_15 = workbook.add_worksheet('15subjects')
+worksheet_10 = workbook.add_worksheet('10subjects')
+worksheet_5 = workbook.add_worksheet('5subjects')
+worksheet_2 = workbook.add_worksheet('2subjects')
 
+bold = workbook.add_format({'bold': True})
+
+
+worksheets = {'36subjects': [worksheet_36, 2, {}], '28subjects': [worksheet_28, 2, {}], '20subjects': [worksheet_20, 2, {}], '15subjects': [worksheet_15, 2, {}], '10subjects': [worksheet_10, 2, {}], '5subjects': [worksheet_5, 2, {}], '2subjects': [worksheet_2, 2, {}]}
+
+for w in worksheets.values():
+    w[0].write(1, 0, 'Subject', bold)
+    w[0].write(1, 1, 'Slice #', bold)
+    w[0].write(1, 2, 'Slice level', bold)
+
+    for mod in range(8):
+        w[0].write(1, 3+3*mod, 'Dice - gamma 1.2', bold)
+        w[0].write(1, 3+3*mod+1, 'Dice - gamma 0', bold)
+        w[0].write(1, 3+3*mod+2, 'Number of model slices', bold)
+        w[0].merge_range(0, 3+3*mod, 0, 3+3*mod+2, str(mod), bold)
+
+
+for loocv_dir in os.listdir(path):
+    if os.path.isdir(path + '/' + loocv_dir) and 'dictionary' not in loocv_dir:
+        words = loocv_dir.split('_')
+        n_sub = words[0]
+        mod = int(words[1])
+        gamma = words[-1]
+        if gamma == '1.2':
+            to_add_col = 0
+        else:
+            to_add_col = 1
+
+        dice_file = open(path + '/' + loocv_dir + '/wm_dice_coeff.txt', 'r')
+        dice_lines = dice_file.readlines()
+        dice_file.close()
+
+        dice_lines = [line.split(' ') for line in dice_lines]
+
+        col = 0
+        for line in dice_lines:
+            line[2] = line[2][:-1]
+            line[-1] = line[-1][:-1]
+            line.pop(4)
+            line.pop(4)
+            slice_id = '_'.join(line[:2])
+            if slice_id not in worksheets[n_sub][2].keys():
+                worksheets[n_sub][2][slice_id] = worksheets[n_sub][1]
+                for i, v in enumerate(line[:3]):
+                    worksheets[n_sub][0].write(worksheets[n_sub][1], col+i, v)
+                worksheets[n_sub][0].write(worksheets[n_sub][1], 3+3*mod+to_add_col, float(line[3]))
+                worksheets[n_sub][0].write(worksheets[n_sub][1], 3+3*mod+2, float(line[-1]))
+                worksheets[n_sub][1] += 1
+            else:
+                worksheets[n_sub][0].write(worksheets[n_sub][2][slice_id], 3+3*mod+to_add_col, float(line[3]))
+                worksheets[n_sub][0].write(worksheets[n_sub][2][slice_id], 3+3*mod+2, float(line[-1]))
+
+workbook.close()
+
+
+'''
 # ############################# CLASSICAL LOOCV #############################
 
 workbook = xl.Workbook('results_loocv_subjects_resampled.xlsx')
@@ -356,9 +422,6 @@ worksheet_wm.conditional_format('E3:' + wm_lim, {'type':     'cell',
                                         'criteria': '<',
                                         'value':    0.8,
                                         'format':   bad})
-
-
-
 workbook.close()
 
-
+'''
