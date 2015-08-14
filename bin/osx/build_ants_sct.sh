fa34de27cde228d44fd0862ace9fb07feefc4d70
Olivier Comtois <comtois.olivier.18@gmail.com>
2015-08-14 15:47:22 -0400

    BUG : Removed hardcoded paths

--- bin/osx/build_ants_sct.sh
+++ bin/osx/build_ants_sct.sh
@@ -1,71 +1,66 @@
 #!/bin/bash
-
-cd
-
-cd git/antsbin
-
 make -j 4
 
 cd bin
 
-cp ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField ../../../code/spinalcordtoolbox/bin/osx
+cp ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField ${SCT_DIR}/bin/osx
 
-cp ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField ../../../code/spinalcordtoolbox/bin/osx
+cp ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField ${SCT_DIR}/bin/osx
 
-cp antsApplyTransforms ../../../code/spinalcordtoolbox/bin/osx
+cp antsApplyTransforms ${SCT_DIR}/bin/osx
 
-cp antsRegistration ../../../code/spinalcordtoolbox/bin/osx
+cp antsRegistration ${SCT_DIR}/bin/osx
 
-cp antsSliceRegularizedRegistration ../../../code/spinalcordtoolbox/bin/osx
+cp antsSliceRegularizedRegistration ${SCT_DIR}/bin/osx
 
-cp ANTSUseLandmarkImagesToGetAffineTransform ../../../code/spinalcordtoolbox/bin/osx
+cp ANTSUseLandmarkImagesToGetAffineTransform ${SCT_DIR}/bin/osx
 
-cp ComposeMultiTransform ../../../code/spinalcordtoolbox/bin/osx
+cp ComposeMultiTransform ${SCT_DIR}/bin/osx
 
 cd
 
-cd code/spinalcordtoolbox/bin/osx
+cd ${SCT_DIR}/bin/osx
 
 if [ -f isct_ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField ]
 then
-    echo Removing existing binary isct_ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField
-    rm isct_ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField
+echo Removing existing binary isct_ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField
+rm isct_ANTSUseLandmarkImagesWithTextFileToGetBSplineDisplacementField
 fi
 
 if [ -f isct_ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField ]
 then
-    echo Removing existing binary isct_ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField
-    rm isct_ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField 
+echo Removing existing binary isct_ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField
+rm isct_ANTSUseLandmarkImagesWithTextFileToGetThinPlateDisplacementField
 fi
 
 if [ -f isct_antsApplyTransforms ]
 then
-    echo Removing existing binary isct_antsApplyTransforms
-    rm isct_antsApplyTransforms
+echo Removing existing binary isct_antsApplyTransforms
+rm isct_antsApplyTransforms
 fi
 
 if [ -f isct_antsRegistration ]
 then
-    echo Removing existing binary isct_antsRegistration
-    rm isct_antsRegistration
+echo Removing existing binary isct_antsRegistration
+rm isct_antsRegistration
 fi
 
 if [ -f isct_antsSliceRegularizedRegistration ]
 then
-    echo Removing existing binary isct_antsSliceRegularizedRegistration
-    rm isct_antsSliceRegularizedRegistration
+echo Removing existing binary isct_antsSliceRegularizedRegistration
+rm isct_antsSliceRegularizedRegistration
 fi
 
 if [ -f isct_ANTSUseLandmarkImagesToGetAffineTransform ]
 then
-    echo Removing existing binary isct_ANTSUseLandmarkImagesToGetAffineTransform
-    rm isct_ANTSUseLandmarkImagesToGetAffineTransform
+echo Removing existing binary isct_ANTSUseLandmarkImagesToGetAffineTransform
+rm isct_ANTSUseLandmarkImagesToGetAffineTransform
 fi
 
 if [ -f isct_ComposeMultiTransform ]
 then
-    echo Removing existing binary isct_ComposeMultiTransform
-    rm isct_ComposeMultiTransform
+echo Removing existing binary isct_ComposeMultiTransform
+rm isct_ComposeMultiTransform
 fi
 
 echo renaming binaries
