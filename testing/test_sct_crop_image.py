ba7d2a759779e479e14edf662dc00b5152e9de89
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-14 16:39:05 -0400

    TEST: fixed bug (forgot to import sct_utils)
    Former-commit-id: 4d259a333d35246a49a2427d4d5c3872f005e8bb
    Former-commit-id: 2afb79c48050b56bddb8d9725da916bf7a8c6bd8

--- testing/test_sct_crop_image.py
+++ testing/test_sct_crop_image.py
@@ -14,12 +14,14 @@
 
 import commands
 
+import sct_utils as sct
+
 
 def test(data_path):
 
     # parameters
     folder_data = 't2/'
-    file_data = ['t2.nii.gz','t2_seg.nii.gz']
+    file_data = ['t2.nii.gz', 't2_seg.nii.gz']
 
     # test normal crop
     cmd = 'sct_crop_image -i ' + data_path + folder_data + file_data[0] \
