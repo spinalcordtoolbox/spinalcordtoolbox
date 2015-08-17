f5565e37841e435b7a9d728cc6c4c4fbefe06cca
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-17 15:50:08 -0400

    NEW: sct_convert: new function to convert files using nibabel.
    Former-commit-id: 3d0a519bd9f9b87f59bcba8dce9b6c120c44f901 [formerly 4cfd8ca6e8a59083cdc203ab70e04454cb4a59e2]
    Former-commit-id: d50b1e2668793d34a8109fccaacb8efc741e7e56
    Former-commit-id: 9f43aab25051a2739f66bc88bc1e9ba8e3d13be2

--- scripts/sct_convert.py
+++ scripts/sct_convert.py
@@ -6,198 +6,252 @@
 # ---------------------------------------------------------------------------------------
 # Copyright (c) 2013 Polytechnique Montreal <www.neuro.polymtl.ca>
 # Author: Benjamin De Leener
-# Created: 2014-09-22
-#
-# Dependences:
-#   minc-toolkit - http://www.bic.mni.mcgill.ca/ServicesSoftware/ServicesSoftwareMincToolKit
-#
-# TO DO:
-# - check if minc-toolkit is installed. If not, convert files using nibabel
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-import os
+
+from msct_parser import Parser
 import sys
-import commands
-import getopt
-import sct_utils as sct
-import nibabel as nib
-from scipy.io import netcdf
+from msct_image import Image
 
 
 # DEFAULT PARAMETERS
 class Param:
     ## The constructor
     def __init__(self):
-        self.debug = 0
         self.verbose = 1
 
-# main
-#=======================================================================================================================
-def main():
-    
-    # Initialization
-    fname_data = ''
-    fname_out = ''
-    verbose = param.verbose
-    
-    # Parameters for debug mode
-    if param.debug:
-        print '\n*** WARNING: DEBUG MODE ON ***\n'
-    else:
-        # Check input parameters
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], 'hi:o:v:')
-        except getopt.GetoptError:
-            usage()
-        if not opts:
-            usage()
-        for opt, arg in opts:
-            if opt == '-h':
-                usage()
-            elif opt in ('-i'):
-                fname_data = arg
-            elif opt in ('-o'):
-                fname_out = arg
-            elif opt in ('-v'):
-                verbose = int(arg)
-    
-    # display usage if a mandatory argument is not provided
-    if fname_data == '':
-        usage()
-    
-    cmd = 'which mnc2nii'
-    status, output = commands.getstatusoutput(cmd)
-    if not output:
-        sct.printv('ERROR: minc-toolkit not installed...',1,'error')
-    if output != '/opt/minc/bin/mnc2nii':
-        sct.printv('ERROR: the minc-toolkit that you use is not the correct one. Please contact SCT administrator.')
 
-    # Check file existence
-    sct.printv('\nCheck file existence...', verbose)
-    sct.check_file_exist(fname_data, verbose)
-    
-    # extract names
-    fname_data = os.path.abspath(fname_data)
-    path_in, file_in, ext_in = sct.extract_fname(fname_data)
-    if fname_out == '':
-        path_out, file_out, ext_out = '', file_in, '.nii'
-        fname_out = path_out+file_out+ext_out
-    else:
-        fname_out = os.path.abspath(fname_out)
-        path_in, file_in, ext_in = sct.extract_fname(fname_data)
-        path_out, file_out, ext_out = sct.extract_fname(fname_out)
-
-    if ext_in=='.nii' and ext_out=='.mnc':
-        nii2mnc(fname_data,fname_out)
-    elif ext_in=='.nii.gz' and ext_out=='.mnc':
-        niigz2mnc(fname_data,fname_out)
-    elif ext_in=='.mnc' and ext_out=='.nii':
-        mnc2nii(fname_data,fname_out)
-    elif ext_in=='.mnc' and ext_out=='.nii.gz':
-        mnc2niigz(fname_data,fname_out)
-    elif ext_in=='.nii' and ext_out=='.header':
-        nii2volviewer(fname_data,fname_out)
-    elif ext_in=='.nii.gz' and ext_out=='.header':
-        niigz2volviewer(fname_data,fname_out)
-    elif ext_in=='.mnc' and ext_out=='.header':
-        mnc2volviewer(fname_data,fname_out)
-
-    # remove temp files
-    sct.run('rm -rf '+path_in+'tmp.*', param.verbose)
-
-
-# Convert file from nifti to minc
+# PARSER
 # ==========================================================================================
-def nii2mnc(fname_data,fname_out):
-    print "Converting from nifti to minc"
-    sct.run("nii2mnc "+fname_data+" "+fname_out)
+def get_parser():
+    # parser initialisation
+    parser = Parser(__file__)
 
-# Convert file from nifti to minc
-# ==========================================================================================
-def niigz2mnc(fname_data,fname_out):
-    print "Converting from nifti to minc"
-    path_in, file_in, ext_in = sct.extract_fname(fname_data)
-    fname_data_tmp=path_in+"tmp."+file_in+".nii"
-    sct.run("gunzip -c "+fname_data+" >"+fname_data_tmp)
-    sct.run("nii2mnc "+fname_data_tmp+" "+fname_out)
-
-# Convert file from minc to nifti
-# ==========================================================================================
-def mnc2nii(fname_data,fname_out):
-    print "Converting from minc to nifti"
-    sct.run("mnc2nii "+fname_data+" "+fname_out)
-
-# Convert file from minc to nifti
-# ==========================================================================================
-def mnc2niigz(fname_data,fname_out):
-    print "Converting from minc to nifti"
-    path_out, file_out, ext_out = sct.extract_fname(fname_out)
-    fname_data_tmp=path_out+file_out+".nii"
-    sct.run("mnc2nii "+fname_data+" "+fname_data_tmp)
-    sct.run("gzip "+fname_data_tmp)
-
-# Convert file from nifti to volumeviewer
-# ==========================================================================================
-def nii2volviewer(fname_data,fname_out):
-    print "Converting from nifti to volume viewer"
-    path_in, file_in, ext_in = sct.extract_fname(fname_data)
-    path_out, file_out, ext_out = sct.extract_fname(fname_out)
-    fname_data_nii = path_out+"tmp."+file_out+'.mnc'
-    nii2mnc(fname_data,fname_data_nii)
-    mnc2volviewer(fname_data_nii,path_out+file_out)
-
-# Convert file from nifti to volumeviewer
-# ==========================================================================================
-def niigz2volviewer(fname_data,fname_out):
-    print "Converting from nifti to volume viewer"
-    path_in, file_in, ext_in = sct.extract_fname(fname_data)
-    path_out, file_out, ext_out = sct.extract_fname(fname_out)
-    fname_data_mnc = path_out+"tmp."+file_out+'.mnc'
-    niigz2mnc(fname_data,fname_data_mnc)
-    mnc2volviewer(fname_data_mnc,path_out+file_out)
-
-# Convert file from minc to volumeviewer
-# ==========================================================================================
-def mnc2volviewer(fname_data,fname_out):
-    print "Converting from minc to volume viewer"
-    sct.run("isct_minc2volume-viewer "+fname_data+" -o "+fname_out)
-
-
-# Print usage
-# ==========================================================================================
-def usage():
-    print """
-        """+os.path.basename(__file__)+"""
-            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
-            
-            DESCRIPTION
-            Convert files from nifti to minc, minc to nifti or nifti to volume viewer.
-            
-            USAGE
-            """+os.path.basename(__file__)+""" -i <data>
-                
-                MANDATORY ARGUMENTS
-                -i <data>             input volume
-                
-                OPTIONAL ARGUMENTS
-                -o <output>           output volume. Add extension. Default="data".nii
-                -v {0,1}              verbose. Default="""+str(param_default.verbose)+"""
-                -h                    help. Show this message
-                """
-    # exit program
-    sys.exit(2)
-
-
-
-#=======================================================================================================================
-# Start program
-#=======================================================================================================================
-if __name__ == "__main__":
     # initialize parameters
     param = Param()
     param_default = Param()
+
+    # Initialize the parser
+    parser = Parser(__file__)
+    parser.usage.set_description('Convert image file to another type.')
+    parser.add_option(name="-i",
+                      type_value="file",
+                      description="File input",
+                      mandatory=True,
+                      example='data.nii.gz')
+    parser.add_option(name="-o",
+                      type_value="file_output",
+                      description="File output (indicate new extension)",
+                      mandatory=True,
+                      example=['data.nii'])
+    return parser
+
+
+
+# MAIN
+# ==========================================================================================
+def main(args = None):
+
+    if not args:
+        args = sys.argv[1:]
+
+    # Building the command, do sanity checks
+    parser = get_parser()
+    arguments = parser.parse(sys.argv[1:])
+    fname_in = arguments["-i"]
+    fname_out = arguments["-o"]
+
+    # Open file
+    im = Image(fname_in)
+
+    # Save file
+    im.setFileName(fname_out)
+    im.save()
+
+
+
+# START PROGRAM
+# ==========================================================================================
+if __name__ == "__main__":
+    # initialize parameters
+    param = Param()
     # call main function
     main()
+
+
+
+# import os
+# import sys
+# import commands
+# import getopt
+# import sct_utils as sct
+# import nibabel as nib
+# from scipy.io import netcdf
+#
+#
+# # DEFAULT PARAMETERS
+# class Param:
+#     ## The constructor
+#     def __init__(self):
+#         self.debug = 0
+#         self.verbose = 1
+#
+# # main
+# #=======================================================================================================================
+# def main():
+#
+#     # Initialization
+#     fname_data = ''
+#     fname_out = ''
+#     verbose = param.verbose
+#
+#     # Parameters for debug mode
+#     if param.debug:
+#         print '\n*** WARNING: DEBUG MODE ON ***\n'
+#     else:
+#         # Check input parameters
+#         try:
+#             opts, args = getopt.getopt(sys.argv[1:], 'hi:o:v:')
+#         except getopt.GetoptError:
+#             usage()
+#         if not opts:
+#             usage()
+#         for opt, arg in opts:
+#             if opt == '-h':
+#                 usage()
+#             elif opt in ('-i'):
+#                 fname_data = arg
+#             elif opt in ('-o'):
+#                 fname_out = arg
+#             elif opt in ('-v'):
+#                 verbose = int(arg)
+#
+#     # display usage if a mandatory argument is not provided
+#     if fname_data == '':
+#         usage()
+#
+#     cmd = 'which mnc2nii'
+#     status, output = commands.getstatusoutput(cmd)
+#     if not output:
+#         sct.printv('ERROR: minc-toolkit not installed...',1,'error')
+#     if output != '/opt/minc/bin/mnc2nii':
+#         sct.printv('ERROR: the minc-toolkit that you use is not the correct one. Please contact SCT administrator.')
+#
+#     # Check file existence
+#     sct.printv('\nCheck file existence...', verbose)
+#     sct.check_file_exist(fname_data, verbose)
+#
+#     # extract names
+#     fname_data = os.path.abspath(fname_data)
+#     path_in, file_in, ext_in = sct.extract_fname(fname_data)
+#     if fname_out == '':
+#         path_out, file_out, ext_out = '', file_in, '.nii'
+#         fname_out = path_out+file_out+ext_out
+#     else:
+#         fname_out = os.path.abspath(fname_out)
+#         path_in, file_in, ext_in = sct.extract_fname(fname_data)
+#         path_out, file_out, ext_out = sct.extract_fname(fname_out)
+#
+#     if ext_in=='.nii' and ext_out=='.mnc':
+#         nii2mnc(fname_data,fname_out)
+#     elif ext_in=='.nii.gz' and ext_out=='.mnc':
+#         niigz2mnc(fname_data,fname_out)
+#     elif ext_in=='.mnc' and ext_out=='.nii':
+#         mnc2nii(fname_data,fname_out)
+#     elif ext_in=='.mnc' and ext_out=='.nii.gz':
+#         mnc2niigz(fname_data,fname_out)
+#     elif ext_in=='.nii' and ext_out=='.header':
+#         nii2volviewer(fname_data,fname_out)
+#     elif ext_in=='.nii.gz' and ext_out=='.header':
+#         niigz2volviewer(fname_data,fname_out)
+#     elif ext_in=='.mnc' and ext_out=='.header':
+#         mnc2volviewer(fname_data,fname_out)
+#
+#     # remove temp files
+#     sct.run('rm -rf '+path_in+'tmp.*', param.verbose)
+#
+#
+# # Convert file from nifti to minc
+# # ==========================================================================================
+# def nii2mnc(fname_data,fname_out):
+#     print "Converting from nifti to minc"
+#     sct.run("nii2mnc "+fname_data+" "+fname_out)
+#
+# # Convert file from nifti to minc
+# # ==========================================================================================
+# def niigz2mnc(fname_data,fname_out):
+#     print "Converting from nifti to minc"
+#     path_in, file_in, ext_in = sct.extract_fname(fname_data)
+#     fname_data_tmp=path_in+"tmp."+file_in+".nii"
+#     sct.run("gunzip -c "+fname_data+" >"+fname_data_tmp)
+#     sct.run("nii2mnc "+fname_data_tmp+" "+fname_out)
+#
+# # Convert file from minc to nifti
+# # ==========================================================================================
+# def mnc2nii(fname_data,fname_out):
+#     print "Converting from minc to nifti"
+#     sct.run("mnc2nii "+fname_data+" "+fname_out)
+#
+# # Convert file from minc to nifti
+# # ==========================================================================================
+# def mnc2niigz(fname_data,fname_out):
+#     print "Converting from minc to nifti"
+#     path_out, file_out, ext_out = sct.extract_fname(fname_out)
+#     fname_data_tmp=path_out+file_out+".nii"
+#     sct.run("mnc2nii "+fname_data+" "+fname_data_tmp)
+#     sct.run("gzip "+fname_data_tmp)
+#
+# # Convert file from nifti to volumeviewer
+# # ==========================================================================================
+# def nii2volviewer(fname_data,fname_out):
+#     print "Converting from nifti to volume viewer"
+#     path_in, file_in, ext_in = sct.extract_fname(fname_data)
+#     path_out, file_out, ext_out = sct.extract_fname(fname_out)
+#     fname_data_nii = path_out+"tmp."+file_out+'.mnc'
+#     nii2mnc(fname_data,fname_data_nii)
+#     mnc2volviewer(fname_data_nii,path_out+file_out)
+#
+# # Convert file from nifti to volumeviewer
+# # ==========================================================================================
+# def niigz2volviewer(fname_data,fname_out):
+#     print "Converting from nifti to volume viewer"
+#     path_in, file_in, ext_in = sct.extract_fname(fname_data)
+#     path_out, file_out, ext_out = sct.extract_fname(fname_out)
+#     fname_data_mnc = path_out+"tmp."+file_out+'.mnc'
+#     niigz2mnc(fname_data,fname_data_mnc)
+#     mnc2volviewer(fname_data_mnc,path_out+file_out)
+#
+# # Convert file from minc to volumeviewer
+# # ==========================================================================================
+# def mnc2volviewer(fname_data,fname_out):
+#     print "Converting from minc to volume viewer"
+#     sct.run("isct_minc2volume-viewer "+fname_data+" -o "+fname_out)
+#
+#
+# # Print usage
+# # ==========================================================================================
+# def usage():
+#     print """
+#         """+os.path.basename(__file__)+"""
+#             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#             Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
+#
+#             DESCRIPTION
+#             Convert files from nifti to minc, minc to nifti or nifti to volume viewer.
+#
+#             USAGE
+#             """+os.path.basename(__file__)+""" -i <data>
+#
+#                 MANDATORY ARGUMENTS
+#                 -i <data>             input volume
+#
+#                 OPTIONAL ARGUMENTS
+#                 -o <output>           output volume. Add extension. Default="data".nii
+#                 -v {0,1}              verbose. Default="""+str(param_default.verbose)+"""
+#                 -h                    help. Show this message
+#                 """
+#     # exit program
+#     sys.exit(2)
+
