1256b27c574c59a94e4cc798a08b942f433669d4
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-20 14:22:21 -0400

    BUG: orientation: fix bug and added test for 4d data

--- testing/test_sct_orientation.py
+++ testing/test_sct_orientation.py
@@ -18,14 +18,22 @@ import commands
 def test(data_path):
 
     # parameters
-    folder_data = ['t2/']
-    file_data = ['t2.nii.gz']
+    folder_data = ['t2/', 'dmri/']
+    file_data = ['t2.nii.gz', 'dmri.nii.gz']
 
-    # define command
+    # test 3d data
     cmd = 'sct_orientation -i ' + data_path + folder_data[0] + file_data[0]
+    status, output = commands.getstatusoutput(cmd)
+    from time import sleep
+    sleep(1)  # here add one second, otherwise the next test will try to create a temporary folder with the same name (because it runs in less than one second)
+
+    # test 4d data
+    if status == 0:
+        cmd = 'sct_orientation -i ' + data_path + folder_data[1] + file_data[1]
+        status, output = commands.getstatusoutput(cmd)
 
     # return
-    return commands.getstatusoutput(cmd)
+    return status, output
 
 
 if __name__ == "__main__":
