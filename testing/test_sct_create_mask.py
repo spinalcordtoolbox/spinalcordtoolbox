6d3b84d6928004cfab5404f109be25f54e9aba19
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-20 16:00:13 -0400

    OPT: create_mask: removed dependency: fslroi and added test for 4d data
    Former-commit-id: fa05b5dda93a2b85aef00ad6704bd81ac5fe45f6 [formerly e596181c821d7a4193120062846d58226b459e50]
    Former-commit-id: 98739c4f6644df9bac12f65b6ee776fc03cb7483
    Former-commit-id: 3bdbbafa697d0677eb9ef9ec6fcd1a83de6ba40d

--- testing/test_sct_create_mask.py
+++ testing/test_sct_create_mask.py
@@ -19,13 +19,11 @@ import time
 def test(data_path):
 
     # parameters
-    folder_data = 'mt/'
-    file_data = ['mt1.nii.gz', 'mt1_point.nii.gz', 'mt1_seg.nii.gz']
-
-    # define command
+    folder_data = ['mt/', 'dmri/']
+    file_data = ['mt1.nii.gz', 'mt1_point.nii.gz', 'mt1_seg.nii.gz', 'dmri.nii.gz']
 
     # method coord
-    cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
+    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
           + ' -m coord,15x17' \
           + ' -s 10' \
           + ' -r 0'
@@ -33,35 +31,40 @@ def test(data_path):
     time.sleep(1)  # here add one second, otherwise the next test will try to create a temporary folder with the same name (because it is typically ran in less than one second)
 
     # method point
-    # create label
-
-    cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
-          + ' -m point,' + data_path + folder_data + file_data[1] \
+    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
+          + ' -m point,' + data_path + folder_data[0] + file_data[1] \
           + ' -s 10' \
           + ' -r 0'
-
     s, o = commands.getstatusoutput(cmd)
     status += s
     output += o
     time.sleep(1)
 
     # method center
-    cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
+    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
           + ' -m center' \
           + ' -s 10' \
           + ' -r 0'
-
     s, o = commands.getstatusoutput(cmd)
     status += s
     output += o
     time.sleep(1)
 
     # method centerline
-    cmd = 'sct_create_mask -i ' + data_path + folder_data + file_data[0] \
-          + ' -m centerline,' + data_path + folder_data + file_data[2] \
+    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
+          + ' -m centerline,' + data_path + folder_data[0] + file_data[2] \
           + ' -s 10' \
           + ' -r 0'
+    s, o = commands.getstatusoutput(cmd)
+    status += s
+    output += o
+    time.sleep(1)
 
+    # method center on 4d data
+    cmd = 'sct_create_mask -i ' + data_path + folder_data[1] + file_data[3] \
+          + ' -m center' \
+          + ' -s 10' \
+          + ' -r 0'
     s, o = commands.getstatusoutput(cmd)
     status += s
     output += o
