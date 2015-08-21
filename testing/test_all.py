eb4f20f6a59936439a01c08aa9bf21dea7fb4c8f
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-21 13:28:06 -0400

    TEST: put back test for fmri_moco

--- testing/test_all.py
+++ testing/test_all.py
@@ -161,7 +161,7 @@ def fill_functions():
     functions.append('sct_extract_metric')
     functions.append('sct_flatten_sagittal')
     functions.append('sct_fmri_compute_tsnr')
-    #functions.append('sct_fmri_moco')
+    functions.append('sct_fmri_moco')
     functions.append('sct_get_centerline_automatic')
     functions.append('sct_get_centerline_from_labels')
     functions.append('sct_label_utils')
