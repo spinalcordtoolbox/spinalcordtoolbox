03ada7185db55ae5b125bde56d7fb4824e455827
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-21 11:10:24 -0400

    NEW: average_data_across_dimension: replaces fslmaths -Tmean
    Former-commit-id: 962d292be66a9316978f4effcf48d649b97ea78f [formerly 8200bcc07a43a1c0e142d109eef5de509559e349]
    Former-commit-id: d6c79337232d7c5fda80f31019243bc24bb2a4ff
    Former-commit-id: 3917b9f2da1be2ff7392da1289db4a125caf7cd2

---
+++ scripts/sct_average_data_across_dimension.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+#########################################################################################
+#
+# Average across dimension. Replaces "fslmaths -Tmean"
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2015 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Julien Cohen-Adad
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+
+# TODO: add check output in average_across_dimension
+
+import sys
+from numpy import mean
+from msct_parser import Parser
+from msct_image import Image
+from sct_utils import extract_fname, printv
+
+
+# DEFAULT PARAMETERS
+class Param:
+    ## The constructor
+    def __init__(self):
+        self.verbose = 1
+
+
+# PARSER
+# ==========================================================================================
+def get_parser():
+    # parser initialisation
+    parser = Parser(__file__)
+
+    # initialize parameters
+    param = Param()
+    param_default = Param()
+
+    # Initialize the parser
+    parser = Parser(__file__)
+    parser.usage.set_description('Average data across specified dimension.')
+    parser.add_option(name="-i",
+                      type_value="file",
+                      description="Input file.",
+                      mandatory=True,
+                      example="data.nii.gz")
+    parser.add_option(name="-dim",
+                      type_value="multiple_choice",
+                      description="""Dimension for average.""",
+                      mandatory=False,
+                      default_value='t',
+                      example=['x', 'y', 'z', 't'])
+    parser.add_option(name="-o",
+                      type_value="file_output",
+                      description='Output file. If output specified, adding suffix "_mean"',
+                      mandatory=False,
+                      example=['data_mean.nii.gz'])
+    parser.add_option(name="-v",
+                      type_value="multiple_choice",
+                      description="""Verbose. 0: nothing. 1: basic. 2: extended.""",
+                      mandatory=False,
+                      default_value=param.verbose,
+                      example=['0', '1', '2'])
+    return parser
+
+
+# concatenation
+# ==========================================================================================
+def average_data_across_dimension(fname_in, fname_out='', dim=3):
+    """
+    Average data
+    :param fname_in: input file.
+    :param fname_out: output file
+    :param dim: dimension: 0, 1, 2, 3
+    :return: True/False
+    """
+    # Parse file name
+    path_in, file_in, ext_in = extract_fname(fname_in)
+    # Open file.
+    im = Image(fname_in)
+    data = im.data
+    # Average
+    data_mean = mean(data, dim)
+    # Write output
+    if fname_out == '':
+        fname_out = path_in+file_in+'_mean'+ext_in
+    im.data = data_mean
+    im.setFileName(fname_out)
+    im.save()
+    return fname_out
+
+
+# MAIN
+# ==========================================================================================
+def main(args = None):
+
+    fname_out = ''
+    dim_list = ['x', 'y', 'z', 't']
+
+    if not args:
+        args = sys.argv[1:]
+
+    # Get parser info
+    parser = get_parser()
+    arguments = parser.parse(sys.argv[1:])
+    fname_in = arguments["-i"]
+    if '-o' in arguments:
+        fname_out = arguments["-o"]
+    dim_concat = arguments["-dim"]
+    verbose = int(arguments['-v'])
+
+    # convert dim into numerical values:
+    dim = dim_list.index(dim_concat)
+
+    # average data
+    fname_out = average_data_across_dimension(fname_in, fname_out, dim)
+
+    # display message
+    printv('Created file:\n--> '+fname_out+'\n', verbose, 'info')
+
+
+# START PROGRAM
+# ==========================================================================================
+if __name__ == "__main__":
+    # initialize parameters
+    param = Param()
+    # call main function
+    main()
