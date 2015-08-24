1f6a689274d117482ae2d0f202942c385554fff0
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-24 10:40:53 -0400

    INST: added scikit-image

--- install/create_links.sh
+++ install/create_links.sh
@@ -10,7 +10,7 @@
 #==========================================================================#
 
 # internal stuff or stuff under development that should not be linked
-FILES_TO_REMOVE="sct_otsu msct_nurbs sct_segment_graymatter sct_utils sct_dmri_eddy_correct sct_change_image_type sct_invert_image msct_moco msct_parser msct_smooth sct_viewer sct_denoising_onlm"
+FILES_TO_REMOVE="msct_nurbs sct_segment_graymatter sct_utils sct_dmri_eddy_correct sct_change_image_type sct_invert_image msct_moco msct_parser msct_smooth sct_viewer sct_denoising_onlm"
 
 function usage()
 {
