d16b2e6b74042e882767b0ce210646a83432f874
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-24 10:44:19 -0400

    OPT: otsu: now using scikit-image (issue #471)
    Former-commit-id: 9d36377cd691fbeb7219c41ff9bb65eedac81795 [formerly 4ad4a292115f404d2c481c44694c6d047a8779fe] [formerly 1e93af15d46d464b6f66ba5b6bc6d7defeaf4987 [formerly b9670965d7b6a11486c45a1c9a02ac654d48e101 [formerly 5e334227e3480bc59b01ce21ec3c7445b0b04847]]]
    Former-commit-id: 9d36377cd691fbeb7219c41ff9bb65eedac81795 [formerly 4ad4a292115f404d2c481c44694c6d047a8779fe]
    Former-commit-id: 2105be0dacaf09256a48aab16b82f27302e0d26d [formerly ecc16514cf3aca76a4001b0c50fc337398d2279a [formerly 90b8cc01a40ffa2bcbb7812a2c5c43b172944a92]]
    Former-commit-id: a32f42fe3bf295294ce314d52370ce6cb7d2af14
    Former-commit-id: aa835206267bd3c5e256021fedbeae4a05ee2a2c

--- scripts/sct_otsu.py
+++ scripts/sct_otsu.py
@@ -1,190 +1,110 @@
 #!/usr/bin/env python
 #########################################################################################
 #
-# Segment images using OTSU algorithm.
+# Threshold image using Otsu algorithm.
 #
 # ---------------------------------------------------------------------------------------
-# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Authors: Julien Cohen-Adad, Brian Avants
-# Modified: 2014-10-08
+# Copyright (c) 2015 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Julien Cohen-Adad
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-
 import sys
-import os
-import getopt
-import commands
-import sct_utils as sct
-import time
-from sct_convert import convert
+from msct_parser import Parser
+from msct_image import Image
+from sct_utils import extract_fname, printv
+from skimage.filters import threshold_otsu
 
 
-# DEFAULT PARAMETERS
-class Param:
-    ## The constructor
-    def __init__(self):
-        self.debug = 0
-        self.fname_data = ''
-        self.threshold = '3'  # threshold value
-        self.file_suffix = '_seg'  # output suffix
-        self.verbose = 1
-        self.remove_tmp_files = 1
-
-
-# main
-#=======================================================================================================================
-def main():
-
-    # Parameters for debug mode
-    if param.debug:
-        print '\n*** WARNING: DEBUG MODE ON ***\n'
-        # get path of the testing data
-        status, path_sct_data = commands.getstatusoutput('echo $SCT_TESTING_DATA_DIR')
-        param.fname_data = path_sct_data+'/fmri/fmri.nii.gz'
-        param.threshold = '3'
-        param.remove_tmp_files = 0
-        param.verbose = 1
-    else:
-        # Check input parameters
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], 'hi:r:t:v:')
-        except getopt.GetoptError:
-            usage()
-        if not opts:
-            usage()
-        for opt, arg in opts:
-            if opt == '-h':
-                usage()
-            elif opt in '-i':
-                param.fname_data = arg
-            elif opt in '-r':
-                param.remove_tmp_files = int(arg)
-            elif opt in '-t':
-                param.threshold = arg
-            elif opt in '-v':
-                param.verbose = int(arg)
-
-    # run main program
-    otsu()
-
-
-# otsu
-#=======================================================================================================================
-def otsu():
-
-    dim = 4  # by default, will be adjusted later
-
-    # display usage if a mandatory argument is not provided
-    if param.fname_data == '':
-        sct.printv('ERROR: All mandatory arguments are not provided. See usage (add -h).', 1, 'error')
-
-    # check existence of input files
-    sct.printv('\ncheck existence of input files...', param.verbose)
-    sct.check_file_exist(param.fname_data, param.verbose)
-
-    # display input parameters
-    sct.printv('\nInput parameters:', param.verbose)
-    sct.printv('  data ..................'+param.fname_data, param.verbose)
-    sct.printv('  threshold .............'+str(param.threshold), param.verbose)
-
-    # Extract path/file/extension
-    path_data, file_data, ext_data = sct.extract_fname(param.fname_data)
-    path_out, file_out, ext_out = '', file_data, ext_data
-
-    # create temporary folder
-    sct.printv('\nCreate temporary folder...', param.verbose)
-    path_tmp = sct.slash_at_the_end('tmp.'+time.strftime("%y%m%d%H%M%S"), 1)
-    sct.run('mkdir '+path_tmp, param.verbose)
-
-    # Copying input data to tmp folder and convert to nii
-    # NB: cannot use c3d here because c3d cannot convert 4D data.
-    sct.printv('\nCopying input data to tmp folder and convert to nii...', param.verbose)
-    sct.run('cp '+param.fname_data+' '+path_tmp+'data'+ext_data, param.verbose)
-
-    # go to tmp folder
-    os.chdir(path_tmp)
-
-    # convert to nii format
-    convert('data'+ext_data, 'data.nii')
-
-    # Get dimensions of data
-    sct.printv('\nGet dimensions of data...', param.verbose)
-    nx, ny, nz, nt, px, py, pz, pt = Image('data.nii').dim
-    sct.printv('  ' + str(nx) + ' x ' + str(ny) + ' x ' + str(nz)+ ' x ' + str(nt), param.verbose)
-    if nt == 1:
-        dim = 3
-    if nz == 1:
-        dim = 2
-
-    # threshold images
-    sct.run('sct_ThresholdImage '+str(dim)+' data.nii data_otsu.nii Otsu '+str(param.threshold))
-    # binarize
-    sct.run('sct_ThresholdImage '+str(dim)+' data_otsu.nii data_otsu_thr.nii '+str(param.threshold)+' '+str(param.threshold))
-    # get largest component of binary mask
-    sct.run('isct_ImageMath '+str(dim)+' data_otsu_thr.nii GetLargestComponent data_otsu_thr.nii')
-    # Morphological Dilation
-    sct.run('isct_ImageMath '+str(dim)+' data_otsu_thr.nii MD data_otsu_thr.nii')
-    # Morphological Erosion
-    sct.run('isct_ImageMath '+str(dim)+' data_otsu_thr.nii ME data_otsu_thr.nii')
-    # Fill holes
-    sct.run('isct_ImageMath '+str(dim)+' data_otsu_thr.nii FillHoles data_otsu_thr.nii')
-
-    # come back to parent folder
-    os.chdir('..')
-
-    # Generate output files
-    sct.printv('\nGenerate output files...', param.verbose)
-    sct.generate_output_file(path_tmp+'data_otsu_thr.nii', path_out+file_out+param.file_suffix+ext_out)
-
-    # Remove temporary files
-    if param.remove_tmp_files == 1:
-        print('\nRemove temporary files...')
-        sct.run('rm -rf '+path_tmp, param.verbose)
-
-    # to view results
-    sct.printv('\nDone! To view results, type:', param.verbose)
-    sct.printv('fslview '+path_out+file_out+param.file_suffix+ext_out+' &', param.verbose, 'code')
-    print
-
-
-# Print usage
+# PARSER
 # ==========================================================================================
-def usage():
-    print """
-"""+os.path.basename(__file__)+"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
+def get_parser():
+    # parser initialisation
+    parser = Parser(__file__)
 
-DESCRIPTION
-  Segment data using OTSU algorithm.
+    # # initialize parameters
+    # param = Param()
+    # param_default = Param()
 
-USAGE
-  """+os.path.basename(__file__)+""" -i <data>
+    # Initialize the parser
+    parser = Parser(__file__)
+    parser.usage.set_description('Threshold image using Otsu algorithm.')
+    parser.add_option(name="-i",
+                      type_value="file",
+                      description="Input file.",
+                      mandatory=True,
+                      example="data.nii.gz")
+    parser.add_option(name="-o",
+                      type_value="file_output",
+                      description='Output file. If output specified, adding suffix "_mean"',
+                      mandatory=False,
+                      example=['data_mean.nii.gz'])
+    parser.add_option(name="-v",
+                      type_value="multiple_choice",
+                      description="""Verbose. 0: nothing. 1: basic. 2: extended.""",
+                      mandatory=False,
+                      default_value='1',
+                      example=['0', '1', '2'])
+    return parser
 
-MANDATORY ARGUMENTS
-  -i <data>        image to segment. Can be 2D, 3D or 4D.
 
-OPTIONAL ARGUMENTS
-  -t <int>         threshold value for images. Default="""+str(param.threshold)+"""
-  -r {0,1}         remove temporary files. Default="""+str(param.remove_tmp_files)+"""
-  -v {0,1}         verbose. Default="""+str(param.verbose)+"""
-  -h               help. Show this message
+# concatenation
+# ==========================================================================================
+def otsu(fname_in, fname_out):
+    """
+    Threshold image using Otsu algorithm.
+    :param fname_in: input file.
+    :param fname_out: output file
+    :return: True/False
+    """
+    # Open file.
+    nii = Image(fname_in)
+    data = nii.data
+    # Threshold
+    thresh = threshold_otsu(data)
+    data_binary = data > thresh
+    # Write output
+    nii.data = data_binary
+    nii.setFileName(fname_out)
+    nii.save()
+    return True
 
-EXAMPLE
-  """+os.path.basename(__file__)+""" -i dwi.nii.gz -t 5\n"""
 
-    # exit program
-    sys.exit(2)
+# MAIN
+# ==========================================================================================
+def main(args = None):
+
+    fname_out = ''
+
+    if not args:
+        args = sys.argv[1:]
+
+    # Get parser info
+    parser = get_parser()
+    arguments = parser.parse(sys.argv[1:])
+    fname_in = arguments["-i"]
+    if '-o' in arguments:
+        fname_out = arguments["-o"]
+    verbose = int(arguments['-v'])
+
+    # Build fname_out
+    if fname_out == '':
+        path_in, file_in, ext_in = extract_fname(fname_in)
+        fname_out = path_in+file_in+'_mean'+ext_in
+
+    # average data
+    if not otsu(fname_in, fname_out):
+        printv('ERROR in otsu', 1, 'error')
+
+    # display message
+    printv('Created file:\n--> '+fname_out+'\n', verbose, 'info')
 
 
-#=======================================================================================================================
-# Start program
-#=======================================================================================================================
+# START PROGRAM
+# ==========================================================================================
 if __name__ == "__main__":
-    # initialize parameters
-    param = Param()
-    param_default = Param()
+    # # initialize parameters
+    # param = Param()
     # call main function
     main()
