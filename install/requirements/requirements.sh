3891970673a3fb6e90bf5289dbbf471399c5dce3
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-08-31 20:40:21 -0400

    TEST: installing dipy with easy_install (issue #484)
    Former-commit-id: 6e54c8f2bd5dcfb0b259c2323a976cacef0e230a [formerly 9b98e65ae5e357dc16ed4d8d102873834e515e98]
    Former-commit-id: a94c1da4b19f59d2fb19cd02aabe116f9b892ba5
    Former-commit-id: db8c68aecb4a3eabcf683571a725cb770f90739a

--- install/requirements/requirements.sh
+++ install/requirements/requirements.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # 
-#Installer for following requirements:
+#Installer for requirements
 #
-#	using conda:
-#		numpy
-#		scipy
-#		sympy
-#		matplotlib
-echo "-------------------------------"
-echo "INSTALLER FOR SOME REQUIREMENTS"
-echo "-------------------------------"
+echo "--------------------------"
+echo "INSTALLER FOR REQUIREMENTS"
+echo "--------------------------"
+# Installation using Conda
 if which conda >/dev/null; then
     echo "* CONDA *"
     conda install --yes --file requirementsConda.txt
+else
+    echo "ERROR: Conda is not installed. Cannot install dependences."
+fi
+# Installation using Pip
+if which pip >/dev/null; then
     echo "* PIP *"
     pip install -r requirementsPip.txt
 else
-    echo "Conda is not installed"
-    echo "Cannot install requirements "
+    echo "ERROR: Pip is not installed. Cannot install dependences."
+fi
+# Installation using easy_install
+if which easy_install >/dev/null; then
+    echo "* EASY_INSTALL *"
+    easy_install dipy install -r requirementsPip.txt
+else
+    echo "ERROR: easy_install is not installed. Cannot install dependences."
 fi
