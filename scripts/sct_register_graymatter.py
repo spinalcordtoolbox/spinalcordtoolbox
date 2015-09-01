dc294c0bfbfd2e6da98536fe0c900e6fb83431d3
Sara Dupont <sadup@evans.neuro.polymtl.ca>
2015-08-31 20:22:35 -0400

    OPT: register bray matter pipeline WIP

--- scripts/sct_register_graymatter.py
+++ scripts/sct_register_graymatter.py
@@ -24,6 +24,8 @@ class RegistrationParam:
         self.debug = False
         self.fname_fixed = ''
         self.fname_moving = ''
+        self.template2anat2wm_gm_warp = ''
+        self.wm_gm2template2anat_warp = ''
         self.transformation = 'BSplineSyN'  # 'SyN'
         self.metric = 'CC'  # 'MeanSquares'
         self.gradient_step = '0.5'
@@ -39,56 +41,37 @@ class RegistrationParam:
         self.remove_temp = 1
 
 
-def wm_registration(param, path_tmp):
+def wm_registration(param):
+    path, fixed_name, ext = sct.extract_fname(param.fname_fixed)
+    path_moving, moving_name, ext = sct.extract_fname(param.fname_moving)
+    path, fixed_seg_name, ext = sct.extract_fname(param.fname_seg_fixed)
+    path_moving_seg, moving_seg_name, ext = sct.extract_fname(param.fname_seg_moving)
 
-    moving_name = 'moving'
-    fixed_name = 'fixed'
 
-    moving_seg_name = 'moving_seg'
-    fixed_seg_name = 'fixed_seg'
+    # cropping in x & y directions
+    fixed_name_temp = fixed_name + "_crop"
+    cmd = "sct_crop_image -i " + fixed_name + ext + " -o " + fixed_name_temp + ext + " -m " + fixed_seg_name + ext + " -shift 10,10 -dim 0,1"
+    sct.run(cmd)
+    fixed_name = fixed_name_temp
 
-    # Extract path/file/extension
-    path_output, file_output, ext_output = sct.extract_fname(param.fname_output)
-
-    # copy files to temporary folder
-    sct.printv('\nCopy files...', reg_param.verbose, 'normal')
-    sct.run("c3d "+param.fname_moving+" -o "+path_tmp+"/"+moving_name+".nii")
-    sct.run("c3d "+param.fname_fixed+" -o "+path_tmp+"/"+fixed_name+".nii")
-    if param.fname_seg_moving != '':
-        sct.run("c3d "+param.fname_seg_moving+" -o "+path_tmp+"/"+moving_seg_name+".nii")
-    if param.fname_seg_fixed != '':
-        sct.run("c3d "+param.fname_seg_fixed+" -o "+path_tmp+"/"+fixed_seg_name+".nii")
-
-    # go to tmp folder
-    os.chdir(path_tmp)
-
-    if param.fname_seg_fixed != '':
-        # cropping in x & y directions
-        fixed_name_temp = fixed_name + "_crop"
-        cmd = "sct_crop_image -i " + fixed_name + ".nii -o " + fixed_name_temp + ".nii -m " + fixed_seg_name + ".nii -shift 10,10 -dim 0,1"
-        sct.run(cmd)
-        fixed_name = fixed_name_temp
-
-        fixed_seg_name_temp = fixed_seg_name+"_crop"
-        sct.run("sct_crop_image -i " + fixed_seg_name + ".nii -o " + fixed_seg_name_temp + ".nii -m " + fixed_seg_name + ".nii -shift 10,10 -dim 0,1")
-        fixed_seg_name = fixed_seg_name_temp
+    fixed_seg_name_temp = fixed_name+"_crop"
+    sct.run("sct_crop_image -i " + fixed_seg_name + ext + " -o " + fixed_seg_name_temp + ext + " -m " + fixed_seg_name + ext + " -shift 10,10 -dim 0,1")
+    fixed_seg_name = fixed_seg_name_temp
 
     # padding the images
     moving_name_pad = moving_name+"_pad"
     fixed_name_pad = fixed_name+"_pad"
-    sct.run("c3d "+moving_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_name_pad+".nii")
-    sct.run("c3d "+fixed_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_name_pad+".nii")
+    sct.run("c3d "+path_moving+moving_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_name_pad+ext)
+    sct.run("c3d "+fixed_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_name_pad+ext)
     moving_name = moving_name_pad
     fixed_name = fixed_name_pad
 
-    if param.fname_seg_moving != '':
-        moving_seg_name_pad = moving_seg_name+"_pad"
-        sct.run("c3d "+moving_seg_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_seg_name_pad+".nii")
-        moving_seg_name = moving_seg_name_pad
-    if param.fname_seg_fixed != '':
-        fixed_seg_name_pad = fixed_seg_name+"_pad"
-        sct.run("c3d "+fixed_seg_name+".nii -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_seg_name_pad+".nii")
-        fixed_seg_name = fixed_seg_name_pad
+    moving_seg_name_pad = moving_seg_name+"_pad"
+    sct.run("c3d "+path_moving_seg+moving_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_seg_name_pad+ext)
+    moving_seg_name = moving_seg_name_pad
+    fixed_seg_name_pad = fixed_seg_name+"_pad"
+    sct.run("c3d "+fixed_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_seg_name_pad+ext)
+    fixed_seg_name = fixed_seg_name_pad
 
     # offset
     old_min = 0
@@ -96,11 +79,11 @@ def wm_registration(param, path_tmp):
     new_min = 100
     new_max = 200
 
-    fixed_im = Image(fixed_name+".nii")
+    fixed_im = Image(fixed_name+ext)
     fixed_im.data = (fixed_im.data - old_min)*(new_max - new_min)/(old_max - old_min) + new_min
     fixed_im.save()
 
-    moving_im = Image(moving_name+".nii")
+    moving_im = Image(moving_name+ext)
     moving_im.data = (moving_im.data - old_min)*(new_max - new_min)/(old_max - old_min) + new_min
     moving_im.save()
 
@@ -115,27 +98,27 @@ def wm_registration(param, path_tmp):
     else:
         transfo_params = ''
 
-    cmd = 'isct_antsRegistration --dimensionality 3 --interpolation '+param.interpolation+' --transform '+param.transformation+'['+param.gradient_step+transfo_params+'] --metric '+param.metric+'['+fixed_name+'.nii,'+moving_name+'.nii,1,4] --output ['+moving_name_reg+','+moving_name_reg+'.nii]  --convergence '+param.iteration+' --shrink-factors 2x1 --smoothing-sigmas 0x0 '
+    cmd = 'isct_antsRegistration --dimensionality 3 --interpolation '+param.interpolation+' --transform '+param.transformation+'['+param.gradient_step+transfo_params+'] --metric '+param.metric+'['+fixed_name+ext+','+moving_name+ext+',1,4] --output ['+moving_name_reg+','+moving_name_reg+ext+']  --convergence '+param.iteration+' --shrink-factors 2x1 --smoothing-sigmas 0x0 '
 
-    if param.fname_seg_moving != '':
-        cmd += " --masks ["+fixed_seg_name+".nii,"+moving_seg_name+".nii]"
-        # cmd += " -m ["+fixed_seg_name+".nii,"+moving_seg_name+".nii]"
+    cmd += " --masks ["+fixed_seg_name+ext+","+moving_seg_name + ext + "]"
+    # cmd += " -m ["+fixed_seg_name+".nii,"+moving_seg_name+".nii]"
     sct.run(cmd)
     moving_name = moving_name_reg
 
     # removing offset
-    fixed_im = Image(fixed_name+".nii")
+    fixed_im = Image(fixed_name+ext)
     fixed_im.data = (fixed_im.data - new_min)*(old_max - old_min)/(new_max - new_min) + old_min
     fixed_im.save()
 
-    moving_im = Image(moving_name+".nii")
+    moving_im = Image(moving_name+ext)
     moving_im.data = (moving_im.data - new_min)*(old_max - old_min)/(new_max - new_min) + old_min
     moving_im.save()
 
     # un-padding the images
     moving_name_unpad = moving_name+"_unpadded"
-    sct.run("sct_crop_image -i "+moving_name+".nii -dim 2 -start "+str(int(param.padding)-1)+" -end -"+param.padding+" -o "+moving_name_unpad+".nii")
+    sct.run("sct_crop_image -i "+moving_name+ext+" -dim 2 -start "+str(int(param.padding)-1)+" -end -"+param.padding+" -o "+moving_name_unpad+ext)
 
+    path_output, file_output, ext_output = sct.extract_fname(param.fname_output)
     warp_output = file_output+"0Warp"+ext_output
     inverse_warp_output = file_output+"0InverseWarp"+ext_output
     sct.run("mv "+moving_name+"0Warp.nii.gz "+warp_output)
@@ -143,11 +126,7 @@ def wm_registration(param, path_tmp):
     moving_name = moving_name_unpad
 
     moving_name_out = file_output+ext_output
-    sct.run("c3d fixed.nii "+moving_name+".nii -reslice-identity -o "+moving_name_out)
-
-    # move output files to initial folder
-    sct.run("cp "+file_output+"* ../")
-    os.chdir('..')
+    sct.run("c3d "+fixed_name+ext+" "+moving_name+ext+" -reslice-identity -o "+moving_name_out+ext)
 
     return warp_output, inverse_warp_output
 
@@ -161,23 +140,44 @@ def segment_gm(target_fname='', sc_seg_fname='', path_to_label='', param=None):
 
 
 def main(seg_params, reg_param, target_fname='', sc_seg_fname='', path_to_label=''):
+    # create temporary folder
+    sct.printv('\nCreate temporary folder...', verbose, 'normal')
+    path_tmp = 'tmp.'+time.strftime("%y%m%d%H%M%S")+'/'
+    sct.run('mkdir '+path_tmp)
 
-    wm_fname, gm_fname = segment_gm(target_fname=target_fname, sc_seg_fname=sc_seg_fname, path_to_label=path_to_label, param=seg_params)
+    target = 'target.nii.gz'
+    sc_seg = 'sc_seg.nii.gz'
+    label = 'label'
+
+    sct.run('cp '+target_fname+' '+path_tmp+target)
+    sct.run('cp '+sc_seg_fname+' '+path_tmp+sc_seg)
+    sct.run('cp -r '+path_to_label+' '+path_tmp+label)
+    sct.run('cp '+reg_param.template2anat2wm_gm_warp+' '+path_tmp+''.join(sct.extract_fname(reg_param.template2anat2wm_gm_warp)[1:]))
+    sct.run('cp '+reg_param.wm_gm2template2anat_warp+' '+path_tmp+''.join(sct.extract_fname(reg_param.wm_gm2template2anat_warp)[1:]))
+
+    os.chdir(path_tmp)
+
+    wm_fname, gm_fname = segment_gm(target_fname=target, sc_seg_fname=sc_seg, path_to_label=label, param=seg_params)
 
     reg_param.fname_fixed = wm_fname
-    reg_param.fname_moving = path_to_label + '/template/MNI-Poly-AMU_WM.nii.gz'
-    reg_param.fname_seg_fixed = sc_seg_fname
-    reg_param.fname_seg_moving = path_to_label + '/template/MNI-Poly-AMU_cord.nii.gz'
+    reg_param.fname_moving = label + '/template/MNI-Poly-AMU_WM.nii.gz'
+    reg_param.fname_seg_fixed = sc_seg
+    reg_param.fname_seg_moving = label + '/template/MNI-Poly-AMU_cord.nii.gz'
 
-    # create temporary folder
-    sct.printv('\nCreate temporary folder...', verbose, 'normal' )
-    path_tmp = 'tmp.'+time.strftime("%y%m%d%H%M%S")
-    sct.run('mkdir '+path_tmp)
-    warp, inverse_warp = wm_registration(reg_param, path_tmp)
+    warp, inverse_warp = wm_registration(reg_param)
 
     # warp the T2star = output
-    sct.run('sct_apply_transfo -i ' + target_fname + ' -d ' + target_fname + ' -w ' + inverse_warp + ' -o ' + sct.extract_fname(target_fname)[1] + '_moved.nii.gz')
-    sct.run('sct_apply_transfo -i ' + sc_seg_fname + ' -d ' + sc_seg_fname + ' -w ' + inverse_warp + ' -o ' + sct.extract_fname(sc_seg_fname)[1] + '_moved.nii.gz  -x nn ')
+    # sct.run('sct_apply_transfo -i ' + target_fname + ' -d ' + target_fname + ' -w ' + inverse_warp + ' -o ' + sct.extract_fname(target_fname)[1] + '_reg.nii.gz')
+    # sct.run('sct_apply_transfo -i ' + sc_seg_fname + ' -d ' + sc_seg_fname + ' -w ' + inverse_warp + ' -o ' + sct.extract_fname(sc_seg_fname)[1] + '_reg.nii.gz  -x nn ')
+
+    # concatenate transformations
+    sct.run('sct_concat_transfo -w '+''.join(sct.extract_fname(reg_param.template2anat2wm_gm_warp)[1:])+','+warp+' -d '+target+' -o warp_template2anat2wm_gm')
+    sct.run('sct_concat_transfo -w '+inverse_warp+','+''.join(sct.extract_fname(reg_param.wm_gm2template2anat_warp)[1:])+' -d '+target+' -o warp_wm_gm2template2anat')
+
+    sct.run('cp warp_template2anat2wm_gm.nii.gz ../warp_'+reg_param.template2anat2wm_gm_warp+'_wm.nii.gz')
+    sct.run('cp warp_wm_gm2template2anat.nii.gz ../warp_'+reg_param.wm_gm2template2anat_warp+'_wm.nii.gz')
+
+    os.chdir('..')
 
     # remove temporary file
     if reg_param.remove_temp == 1:
@@ -203,7 +203,7 @@ if __name__ == "__main__":
         parser.usage.addSection('Segmentation parameters')
         parser.add_option(name="-i",
                           type_value="file",
-                          description="T2star image (or image with a white/gray matter contrast)",
+                          description="T2star target image (or image with a white/gray matter contrast)",
                           mandatory=True,
                           example='t2star.nii.gz')
         parser.add_option(name="-s",
@@ -254,6 +254,11 @@ if __name__ == "__main__":
                           example='sc_seg.nii.gz')
         '''
         parser.usage.addSection('Registration parameters')
+        parser.add_option(name="-warp",
+                          type_value=[[','], 'file'],
+                          description="Warping field from the template2anat to the WM/GM contrasted image and inverse that was used to register the template to the WM/GM contrasted image",
+                          mandatory=True,
+                          example="warp_template2anat2t2star.nii,warp_t2star2template2anat.nii")
         parser.add_option(name="-t",
                           type_value='multiple_choice',
                           description="type of transformation",
@@ -309,6 +314,7 @@ if __name__ == "__main__":
         else:
             reg_param.fname_output = sct.extract_fname(reg_param.fname_moving)[1] + '_to_' + sct.extract_fname(reg_param.fname_ref)[1] +  sct.extract_fname(reg_param.fname_ref)[2]
         '''
+        reg_param.template2anat2wm_gm_warp, reg_param.wm_gm2template2anat_warp = arguments["-warp"]
         if "-t" in arguments:
             reg_param.transformation = arguments["-t"]
         if "-m" in arguments:
