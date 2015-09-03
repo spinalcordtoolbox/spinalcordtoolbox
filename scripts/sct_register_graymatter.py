8cda86363fe48f2d16b403af668a2943e4ee6ae9
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-09-03 10:02:42 -0400

    BUG: msct_gmseg_utils: c3d was called instead of isct_c3d
    Former-commit-id: 773bc850fb2f56e3b8e3b210c04f01dfc7f2805b [formerly 6e8338e472a67443402b43d07fb2cff35623a972]
    Former-commit-id: 4d5e1f23ce8f702814b95d006d3d50c1b50dafef
    Former-commit-id: b7e52d72a6625783b1e2f40585b5741b4cd3f072

--- scripts/sct_register_graymatter.py
+++ scripts/sct_register_graymatter.py
@@ -95,16 +95,16 @@ def wm_registration(param):
     # padding the images
     moving_name_pad = moving_name+"_pad"
     fixed_name_pad = fixed_name+"_pad"
-    sct.run("c3d "+path_moving+moving_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_name_pad+ext)
-    sct.run("c3d "+fixed_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_name_pad+ext)
+    sct.run("isct_c3d "+path_moving+moving_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_name_pad+ext)
+    sct.run("isct_c3d  "+fixed_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_name_pad+ext)
     moving_name = moving_name_pad
     fixed_name = fixed_name_pad
 
     moving_seg_name_pad = moving_seg_name+"_pad"
-    sct.run("c3d "+path_moving_seg+moving_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_seg_name_pad+ext)
+    sct.run("isct_c3d  "+path_moving_seg+moving_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+moving_seg_name_pad+ext)
     moving_seg_name = moving_seg_name_pad
     fixed_seg_name_pad = fixed_seg_name+"_pad"
-    sct.run("c3d "+fixed_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_seg_name_pad+ext)
+    sct.run("isct_c3d  "+fixed_seg_name+ext+" -pad 0x0x"+param.padding+"vox 0x0x"+param.padding+"vox 0 -o "+fixed_seg_name_pad+ext)
     fixed_seg_name = fixed_seg_name_pad
 
     # offset
@@ -160,7 +160,7 @@ def wm_registration(param):
     moving_name = moving_name_unpad
 
     moving_name_out = file_output+ext_output
-    sct.run("c3d "+fixed_name+ext+" "+moving_name+ext+" -reslice-identity -o "+moving_name_out+ext)
+    sct.run("isct_c3d  "+fixed_name+ext+" "+moving_name+ext+" -reslice-identity -o "+moving_name_out+ext)
 
     return warp_output, inverse_warp_output
 
