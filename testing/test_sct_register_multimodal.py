1e8c8ae159d777d9387d163c1424c3bf025d5c28
Sara Dupont <sara.m.dupont@gmail.com>
2015-09-14 11:34:34 -0400

    BUG: find out why linux fails
    Former-commit-id: 5edca2fc3c7808e580f9b2c740d04c1bcb8795fe [formerly 8bc1e719a6923f248904c7e859d7c45f15f8e382]
    Former-commit-id: 54665d19c8a998158253996955c4033ed91cd4b9
    Former-commit-id: a5bec4c3846d9ecf955de87a0150f02f664ac656

--- testing/test_sct_register_multimodal.py
+++ testing/test_sct_register_multimodal.py
@@ -24,7 +24,24 @@ def test(path_data):
 
     output = ''
     status = 0
+    possible_algos = ['slicereg2d_pointwise','slicereg2d_translation','slicereg2d_rigid','slicereg2d_affine','slicereg2d_syn','slicereg2d_bsplinesyn','slicereg']
+    for algo in possible_algos:
+        cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
+              + ' -d ' + path_data + folder_data + file_data[1] \
+              + ' -o data_reg.nii.gz'  \
+              + ' -p step=1,algo=syn,iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
+              + ' -x linear' \
+              + ' -r 0' \
+              + ' -v 1'
+        output += cmd+'\n'  # copy command
+        s, o = commands.getstatusoutput(cmd)
+        status += s
+        output += '*****************************************************************************************************\n' \
+                  'OUTPUT FROM TEST '+algo+': '
+        output += o
+        output += '*****************************************************************************************************\n'
 
+    '''
     cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
           + ' -d ' + path_data + folder_data + file_data[1] \
           + ' -o data_reg.nii.gz'  \
@@ -51,10 +68,8 @@ def test(path_data):
     output += cmd+'\n'  # copy command
     s, o = commands.getstatusoutput(cmd)
     status += s
-    output += '*****************************************************************************************************\n' \
-              'OUTPUT FROM TEST 2: '
     output += o
-    output += '*****************************************************************************************************\n'
+
     # check other method
     cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
           + ' -d ' + path_data + folder_data + file_data[1] \
@@ -66,10 +81,9 @@ def test(path_data):
     output += cmd+'\n'  # copy command
     s, o = commands.getstatusoutput(cmd)
     status += s
-    output += '*****************************************************************************************************\n' \
-              'OUTPUT FROM TEST 3: '
     output += o
-    output += '*****************************************************************************************************\n'
+    '''
+
     return status, output
 
 if __name__ == "__main__":
