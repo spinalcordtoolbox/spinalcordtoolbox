1a538d7c6055d68bbc9ec062031b346e72cd378d
Sara Dupont <sara.m.dupont@gmail.com>
2015-09-14 11:49:57 -0400

    BUG: find out why linux fails
    Former-commit-id: 4be0fd8fbf7c4ead27d5417d22bf777025c57862 [formerly 1d8b328e6549976adea2fe81b839da9f3f0ca7d0] [formerly 1d699364476e4d5a2d8eded1a55c80d41298635f [formerly 6f015d1d3762d99c5559703a9730a095fd89a5d4 [formerly 9a541c14affee67d2e2625ffbb6d9891d1a274cc]]]
    Former-commit-id: 4be0fd8fbf7c4ead27d5417d22bf777025c57862 [formerly 1d8b328e6549976adea2fe81b839da9f3f0ca7d0]
    Former-commit-id: 8de39422ed1bd0535abdc6144e198ebe9fa19821 [formerly cddf50e9df225e3e4a089e08ccb247e6169facd1 [formerly 03f376381dc889a18a2526abcd3d0555f8e23ba1]]
    Former-commit-id: d700d5b0ac948b5e43da98e006baa23efc197c2f
    Former-commit-id: e106e83eeaf1f592f0abb8353e091967a0a587e5

--- testing/test_sct_register_multimodal.py
+++ testing/test_sct_register_multimodal.py
@@ -24,7 +24,8 @@ def test(path_data):
 
     output = ''
     status = 0
-    possible_algos = ['slicereg2d_pointwise','slicereg2d_translation','slicereg2d_rigid','slicereg2d_affine','slicereg2d_syn','slicereg2d_bsplinesyn','slicereg']
+    ok_algos = ['slicereg2d_pointwise','slicereg2d_translation','slicereg2d_rigid','slicereg2d_affine','slicereg2d_syn','slicereg2d_bsplinesyn','slicereg']
+    possible_algos =['rigid', 'affine', 'compositeaffine', 'similarity', 'translation', 'bspline', 'gaussiandisplacementfield', 'bsplinedisplacementfield', 'syn', 'bsplinesyn']
     for algo in possible_algos:
         cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
               + ' -d ' + path_data + folder_data + file_data[1] \
