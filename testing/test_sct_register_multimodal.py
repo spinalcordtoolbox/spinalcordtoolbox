61341cab32fafa482addd3abf6ff9bb90c4e92ad
Sara Dupont <sara.m.dupont@gmail.com>
2015-09-17 15:20:37 -0400

    NEW: integrity check for default algorithm (syn)
    Former-commit-id: 82f16cf8367c61f93b55192e2994ca0bd8de0e81 [formerly 606fc1b3a176220f43a08f07b3b7130063e2c275] [formerly af05f72751f8623254975b05af40779c727484b7 [formerly 4593676ca93ef448a1b74af8bef35963fe9dbf26 [formerly aa4c51083272c921c2284f1378b1cacb6c885fb8]]]
    Former-commit-id: 82f16cf8367c61f93b55192e2994ca0bd8de0e81 [formerly 606fc1b3a176220f43a08f07b3b7130063e2c275]
    Former-commit-id: ce18ade4ca9bb357b9f67be665111f20e2b5c7b6 [formerly b24c7262fb5ca02a0a78e305b4b127c17f1c3b35 [formerly 9cbf87fedb4d7e181e4fcdfdcd0607f2cac46ac3]]
    Former-commit-id: 9d781500bdde4c46a3830763a92742f34346b8e9
    Former-commit-id: 86af5f92487501b112081a05d3a03207c461093e

--- testing/test_sct_register_multimodal.py
+++ testing/test_sct_register_multimodal.py
@@ -20,19 +20,38 @@ import commands
 def test(path_data):
 
     folder_data = 'mt/'
-    file_data = ['mt0.nii.gz', 'mt1.nii.gz']
+    file_data = ['mt0.nii.gz', 'mt1.nii.gz', 'mt0_syn_reg_on_mt1.nii.gz']
 
     output = ''
     status = 0
 
+    algo_default = 'syn'
     cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
           + ' -d ' + path_data + folder_data + file_data[1] \
-          + ' -o data_reg.nii.gz'  \
-          + ' -p step=1,algo=syn,iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
+          + ' -o data_'+algo_default+'_reg.nii.gz'  \
+          + ' -p step=1,algo='+algo_default+',iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
           + ' -x linear' \
           + ' -r 0' \
           + ' -v 1'
-    output += cmd+'\n'  # copy command
+    output += '\n====================================================================================================\n'\
+              +cmd+\
+              '\n====================================================================================================\n\n'  # copy command
+    s, o = commands.getstatusoutput(cmd)
+    status += s
+    output += o
+
+    # check other method
+    algo = 'slicereg'
+    cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
+          + ' -d ' + path_data + folder_data + file_data[1] \
+          + ' -o data_'+algo+'_reg.nii.gz'  \
+          + ' -p step=1,algo='+algo+',iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
+          + ' -x linear' \
+          + ' -r 0' \
+          + ' -v 1'
+    output += '\n====================================================================================================\n'\
+              +cmd+\
+              '\n====================================================================================================\n\n'  # copy command
     s, o = commands.getstatusoutput(cmd)
     status += s
     output += o
@@ -40,28 +59,34 @@ def test(path_data):
     # check other method
     cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
           + ' -d ' + path_data + folder_data + file_data[1] \
-          + ' -o data_reg.nii.gz'  \
-          + ' -p step=1,algo=slicereg,iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
+          + ' -o data_'+algo+'_reg.nii.gz'  \
+          + ' -p step=1,algo='+algo+',iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
           + ' -x linear' \
           + ' -r 0' \
           + ' -v 1'
-    output += cmd+'\n'  # copy command
+    output += '\n====================================================================================================\n'\
+              +cmd+\
+              '\n====================================================================================================\n\n'  # copy command
     s, o = commands.getstatusoutput(cmd)
     status += s
     output += o
 
-    # check other method
-    cmd = 'sct_register_multimodal -i ' + path_data + folder_data + file_data[0] \
-          + ' -d ' + path_data + folder_data + file_data[1] \
-          + ' -o data_reg.nii.gz'  \
-          + ' -p step=1,algo=affine,iter=1,smooth=0,shrink=4,metric=MeanSquares'  \
-          + ' -x linear' \
-          + ' -r 0' \
-          + ' -v 1'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
+    # if command ran without error, test integrity
+    if status == 0:
+        from msct_image import Image
+        threshold = 1e-3
+        # compare with gold-standard registration
+        data_gold = Image(path_data + folder_data + file_data[-1]).data
+        data_res = Image('data_'+algo_default+'_reg.nii.gz').data
+        # check if non-zero elements are present when computing the difference of the two images
+        diff = data_gold - data_res
+        import numpy as np
+        output += 'DIfference sum : ' + str(np.sum(diff))+'\n'
+        if abs(np.sum(diff))>threshold:
+            Image(param=diff, absolutepath='res_differences_from_gold_standard.nii.gz').save()
+            status = 99
+            output += '\nResulting image differs from gold-standard.'
+
 
     return status, output
 
