f8ce83d5746f5ef6c231d9a4573044f30888b94b
Benjamin De Leener <jackbdl@gmail.com>
2015-09-25 10:07:19 -0400

    TEST: fixed straightening test
    Former-commit-id: eedafe88db4662409f75499b3be1901170b26730 [formerly 8207c778cede60d96c2f4735dfb0c4f7c52eddfc] [formerly 5b8ce3a4d97204c4f49d630136a6d14f3f51864f [formerly 3546e6da49f61c01cf28fb5bedf7f8aee825d047 [formerly 66f817088c5b38da6d253912e6d2f66754ed7e97]]]
    Former-commit-id: eedafe88db4662409f75499b3be1901170b26730 [formerly 8207c778cede60d96c2f4735dfb0c4f7c52eddfc]
    Former-commit-id: d488eea146c5234360038625b872fa276950216c [formerly 0ecb13602144bf3baeec3adbaa4a98b29ff91ed0 [formerly 985b437b2a28d98958aa49e2bfa95d59fb67cecb]]
    Former-commit-id: a44ceac292b963121d9987d7d7c185681f35cfcd
    Former-commit-id: c0a1257ad78387b1a1f5ca60ae9522cb986c69c6

--- testing/test_sct_straighten_spinalcord.py
+++ testing/test_sct_straighten_spinalcord.py
@@ -19,17 +19,19 @@ import sct_straighten_spinalcord
 from pandas import DataFrame
 
 
-def test(path_data='', param=''):
+def test(path_data='', parameters=''):
 
-    if not param:
-        param = '-i t2/t2.nii.gz -c t2/t2_seg.nii.gz'
+    if not parameters:
+        parameters = '-i t2/t2.nii.gz -c t2/t2_seg.nii.gz'
+    # add mandatory verbose to parameters list
+    parameters = parameters + ' -v 1'
 
-    parser = sct_straighten_spinalcord.get_parser()  # TODO
-    dict_param = parser.parse(param)
+    parser = sct_straighten_spinalcord.get_parser()
+    dict_param = parser.parse(parameters.split(), check_file_exist=False)
     dict_param_with_path = parser.add_path_to_file(dict_param, path_data, input_file=True)
     param_with_path = Parser.dictionary_to_string(dict_param_with_path)
 
-    cmd = 'sct_straighten_spinalcord ' + param_with_path + ' -v 1'
+    cmd = 'sct_straighten_spinalcord ' + param_with_path
     status, output = sct.run(cmd, 0)
 
     # initialization of results: must be NaN if test fails
