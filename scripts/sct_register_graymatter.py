1594702ca2b6514b9da890913318f2d5a61f1004
Sara Dupont <sara.m.dupont@gmail.com>
2015-09-28 12:16:38 -0400

    REF: sct_maths -pad use coma as separators, not 'x' anymore
    Former-commit-id: e55c37a0f2e78c9bd6b868d9411288db6a210f74 [formerly b0bdc6e764cdf45db7b380ce77154960f197b186] [formerly f837310396df61c9e8627995e23752a5bfb70f3c [formerly fe9eaa1fad947ad46c69103d5ba533ffdbca58d9 [formerly bfc64bc93a804ca60168bb853bcdff9bf1880fa8]]]
    Former-commit-id: e55c37a0f2e78c9bd6b868d9411288db6a210f74 [formerly b0bdc6e764cdf45db7b380ce77154960f197b186]
    Former-commit-id: 5160fa0d47c75697c2b7889eb720af66ed7b8872 [formerly 14416abc3ccf074f3040d0f703afcee888c61117 [formerly 5f6610ad02f0f897d5edf9769f04820821658e1c]]
    Former-commit-id: dc0c783d72023a706f5a6e42226ac4bd393b44b2
    Former-commit-id: 431e7ef0f2bf0a735341472432b2ff3da4a2c3cb

--- scripts/sct_register_graymatter.py
+++ scripts/sct_register_graymatter.py
@@ -95,17 +95,17 @@ def wm_registration(param):
     # padding the images
     moving_name_pad = moving_name+"_pad"
     fixed_name_pad = fixed_name+"_pad"
-    sct.run('sct_maths -i '+path_moving+moving_name+ext+' -o '+moving_name_pad+ext+' -pad 0x0x'+str(param.padding))
-    sct.run('sct_maths -i '+fixed_name+ext+' -o '+fixed_name_pad+ext+' -pad 0x0x'+str(param.padding))
+    sct.run('sct_maths -i '+path_moving+moving_name+ext+' -o '+moving_name_pad+ext+' -pad 0,0,'+str(param.padding))
+    sct.run('sct_maths -i '+fixed_name+ext+' -o '+fixed_name_pad+ext+' -pad 0,0,'+str(param.padding))
     moving_name = moving_name_pad
     fixed_name = fixed_name_pad
 
     moving_seg_name_pad = moving_seg_name+"_pad"
-    sct.run('sct_maths -i '+path_moving_seg+moving_seg_name+ext+' -o '+moving_seg_name_pad+ext+' -pad 0x0x'+str(param.padding))
+    sct.run('sct_maths -i '+path_moving_seg+moving_seg_name+ext+' -o '+moving_seg_name_pad+ext+' -pad 0,0,'+str(param.padding))
     moving_seg_name = moving_seg_name_pad
 
     fixed_seg_name_pad = fixed_seg_name+"_pad"
-    sct.run('sct_maths -i '+fixed_seg_name+ext+' -o '+fixed_seg_name_pad+ext+' -pad 0x0x'+str(param.padding))
+    sct.run('sct_maths -i '+fixed_seg_name+ext+' -o '+fixed_seg_name_pad+ext+' -pad 0,0,'+str(param.padding))
     fixed_seg_name = fixed_seg_name_pad
 
     # offset
