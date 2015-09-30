437a173fb65f343371848c248c94873ea89f086f
Benjamin De Leener <jackbdl@gmail.com>
2015-09-30 17:21:02 -0400

    TEST: first attempt for isct_test_function. It is not working yet because it can't pickle test function.
    Former-commit-id: ba9d46c64669d86d5cadad67043ddbe9c773b9fb
    Former-commit-id: 209dc03f7295517ffd25da55ad494dc4a0aa6384

---
+++ testing/isct_test_function.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+#########################################################################################
+#
+# This function allows to run a function on a large dataset with a set of parameters.
+# Results are extracted and saved in a way that they can easily be compared with another set.
+#
+# Data should be organized as the following:
+# (names of images can be changed but must be passed as parameters to this function)
+#
+# data/
+# ......subject_name_01/
+# ......subject_name_02/
+# .................t1/
+# .........................subject_02_anything_t1.nii.gz
+# .........................some_landmarks_of_vertebral_levels.nii.gz
+# .........................subject_02_manual_segmentation_t1.nii.gz
+# .................t2/
+# .........................subject_02_anything_t2.nii.gz
+# .........................some_landmarks_of_vertebral_levels.nii.gz
+# .........................subject_02_manual_segmentation_t2.nii.gz
+# .................t2star/
+# .........................subject_02_anything_t2star.nii.gz
+# .........................subject_02_manual_segmentation_t2star.nii.gz
+# ......subject_name_03/
+#          .
+#          .
+#          .
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2015 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Sara Dupont, Benjamin De Leener
+# Modified: 2015-09-30
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+from msct_parser import Parser
+import sys
+import sct_utils as sct
+import os
+import copy_reg
+import types
+
+
+def _pickle_method(method):
+    """
+    Author: Steven Bethard (author of argparse)
+    http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    """
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    cls_name = ''
+    if func_name.startswith('__') and not func_name.endswith('__'):
+        cls_name = cls.__name__.lstrip('_')
+    if cls_name:
+        func_name = '_' + cls_name + func_name
+    return _unpickle_method, (func_name, obj, cls)
+
+
+def _unpickle_method(func_name, obj, cls):
+    """
+    Author: Steven Bethard
+    http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    """
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+
+
+def generate_data_list(folder_dataset, verbose=1):
+    """
+    Construction of the data list from the data set
+    This function return a list of directory (in folder_dataset) in which the contrast is present.
+    :return data:
+    """
+    data_subjects = []
+
+    # each directory in folder_dataset should be a directory of a subject
+    for subject_dir in os.listdir(folder_dataset):
+        print folder_dataset + subject_dir, not subject_dir.startswith('.') and os.path.isdir(folder_dataset + subject_dir)
+        if not subject_dir.startswith('.') and os.path.isdir(folder_dataset + subject_dir):
+            data_subjects.append(folder_dataset + subject_dir)
+
+    if not data_subjects:
+        sct.printv('ERROR: No subject data were found in ' + folder_dataset + '. '
+                   'Please organize your data correctly or provide a correct dataset.',
+                   verbose=verbose, type='error')
+
+    return data_subjects
+
+
+def worker_results(results):
+    results_sorted = [result for result in results if result is not None and result[0] != 5]
+    sorted(results_sorted, key=lambda l: l[2][0])  # sort results by subject name / path
+    return results_sorted
+
+
+def test_function(function, folder_dataset, parameters='', nb_cpu=None, verbose=1):
+    """
+    Run a test function on the dataset using multiprocessing and save the results
+    :return: results
+    """
+    # generate data list from folder containing
+    data_subjects = generate_data_list(folder_dataset)
+
+    from multiprocessing import Pool
+    import importlib
+    script_to_be_run = importlib.import_module('test_' + function)  # import function as a module
+
+    # create datasets with parameters
+    import itertools
+    data_and_params = itertools.izip(data_subjects, itertools.repeat(parameters))
+
+    pool = Pool(processes=nb_cpu)
+    async_results = pool.map_async(lambda x: script_to_be_run.test(*x), data_and_params, callback=worker_results)
+
+    pool.close()
+    try:
+        pool.join()  # waiting for all the jobs to be done
+        results = async_results.get()  # get the sorted results once all jobs are finished
+    except KeyboardInterrupt:
+        print "\nWarning: Caught KeyboardInterrupt, terminating workers"
+        pool.terminate()
+        sys.exit(2)
+    except Exception as e:
+        print e
+        sys.exit(2)
+
+    return results
+
+
+def get_parser():
+    # Initialize parser
+    parser = Parser(__file__)
+
+    # Mandatory arguments
+    parser.usage.set_description("")
+    parser.add_option(name="-f",
+                      type_value="str",
+                      description="Function to test.",
+                      mandatory=True,
+                      example="sct_propseg")
+
+    parser.add_option(name="-d",
+                      type_value="folder",
+                      description="Dataset directory.",
+                      mandatory=True,
+                      example="dataset_full/")
+
+    parser.add_option(name="-p",
+                      type_value="str",
+                      description="Arguments to pass to the function that is tested.\n"
+                                  "Image paths must be contains in the arguments list.",
+                      mandatory=False)
+
+    parser.add_option(name="-cpu-nb",
+                      type_value="int",
+                      description="Number of CPU used for testing. 0: no multiprocessing. If not provided, "
+                                  "it uses all the available cores.",
+                      mandatory=False,
+                      default_value=0,
+                      example='42')
+
+    parser.add_option(name="-v",
+                      type_value="multiple_choice",
+                      description="Verbose. 0: nothing, 1: basic, 2: extended.",
+                      mandatory=False,
+                      example=['0', '1', '2'],
+                      default_value='1')
+
+    return parser
+
+
+# ====================================================================================================
+# Start program
+# ====================================================================================================
+if __name__ == "__main__":
+    parser = get_parser()
+    arguments = parser.parse(sys.argv[1:])
+
+    function_to_test = arguments["-f"]
+    dataset = arguments["-d"]
+
+    parameters = ''
+    if "-p" in arguments:
+        parameters = arguments["-p"]
+
+    nb_cpu = None
+    if "-cpu-nb" in arguments:
+        nb_cpu = arguments["-cpu-nb"]
+
+    verbose = arguments["-v"]
+
+    results = test_function(function_to_test, dataset, parameters, nb_cpu, verbose)
+    print results
