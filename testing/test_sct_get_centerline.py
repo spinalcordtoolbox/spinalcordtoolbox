456c6bbb7060323187aad42028175a648e066dec
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-10-02 13:19:24 -0400

    TEST: added test for detect_spinalcord (did not activate because does not pass yet)

--- testing/test_sct_get_centerline.py
+++ testing/test_sct_get_centerline.py
@@ -13,24 +13,31 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-#import sct_utils as sct
 import commands
 
 def test(path_data):
 
     # parameters
     folder_data = 't2/'
-    file_data = ['t2.nii.gz', 't2_centerline_init.nii.gz']
+    file_data = ['t2.nii.gz', 't2_centerline_init.nii.gz', 't2_seg.nii.gz']
 
     output = ''
     status = 0
 
     # define command
-    cmd = 'sct_get_centerline -method point -i ' + path_data + folder_data + file_data[0] \
-          + ' -p ' + path_data + folder_data + file_data[1] \
+    cmd = 'sct_get_centerline -method labels -i ' + path_data + folder_data + file_data[0] \
+          + ',' + path_data + folder_data + file_data[2] \
+          + ' -v 1'
+    output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
+    s, o = commands.getstatusoutput(cmd)
+    status += s
+    output += o
+
+    # define command
+    cmd = 'sct_get_centerline -method point ' \
+          + '-i ' + path_data + folder_data + file_data[0] + ',' + path_data + folder_data + file_data[1] \
           + ' -g 1'\
-          + ' -k 4'\
-          + ' -r 0'
+          + ' -k 4'
     output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
     s, o = commands.getstatusoutput(cmd)
     status += s
