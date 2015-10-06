a97c5e2db48881258ed82bf5cf448fc2de0e2220
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-06 17:20:34 -0400

    REF: resampling interpolation : linear instead of trilinear
    Former-commit-id: 38b1af9fe57107a49fb1f78103d6d040b5575a07
    Former-commit-id: 11c45e0c2bd68314364c2fbdb4523e6774b302ce

--- scripts/isct_convert_binary_to_trilinear.py
+++ scripts/isct_convert_binary_to_trilinear.py
@@ -111,7 +111,7 @@ def main():
 
     # upsample data
     sct.printv('\nUpsample data...', verbose)
-    sct.run('sct_resample -i data.nii -x trilinear -vox '+str(nx*interp_factor)+'x'+str(ny*interp_factor)+'x'+str(nz*interp_factor)+' -o data_up.nii', verbose)
+    sct.run('sct_resample -i data.nii -x linear -vox '+str(nx*interp_factor)+'x'+str(ny*interp_factor)+'x'+str(nz*interp_factor)+' -o data_up.nii', verbose)
 
     # Smooth along centerline
     sct.printv('\nSmooth along centerline...', verbose)
@@ -119,7 +119,7 @@ def main():
 
     # downsample data
     sct.printv('\nDownsample data...', verbose)
-    sct.run('sct_resample -i data_up_smooth.nii -x trilinear -vox '+str(nx)+'x'+str(ny)+'x'+str(nz)+' -o data_up_smooth_down.nii', verbose)
+    sct.run('sct_resample -i data_up_smooth.nii -x linear -vox '+str(nx)+'x'+str(ny)+'x'+str(nz)+' -o data_up_smooth_down.nii', verbose)
 
     # come back to parent folder
     os.chdir('..')
