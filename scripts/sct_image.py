1d6489b729c1d276f9a41651016a171f6cd01d50
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-07 11:24:41 -0400

    NEW: add orientation options in sct_image
    Former-commit-id: 46c76a88abd6d11bd301b2c4cbd267ec7e64208b [formerly 3ddb6d013f7ebb528d5fe0e5e000fa26485c33b5] [formerly 9281fb533cd55bfc5c15fdc06fc24aa3fc050791 [formerly 5c80c7dbf0e4101e148fdaa1834a1754f963929e [formerly 70582f6b894858ba1dd66e8103ccac5b6ab1e2fc]]]
    Former-commit-id: 46c76a88abd6d11bd301b2c4cbd267ec7e64208b [formerly 3ddb6d013f7ebb528d5fe0e5e000fa26485c33b5]
    Former-commit-id: ede7ad37a7480528dc86056805115c6a3dc8c909 [formerly 7154b3d35e64d1096837524435a0b0da87caecc0 [formerly 53283750dff6a8e972b286d550b6e9a43eb63cbe]]
    Former-commit-id: e5dd153a91f877fa09ffa73cbb0f0a39b157596f
    Former-commit-id: 428137ed25e8dca161a91b7dc54d396ce0eb3ae3

--- scripts/sct_image.py
+++ scripts/sct_image.py
@@ -14,7 +14,7 @@ import sys
 from numpy import concatenate, shape, newaxis
 from msct_parser import Parser
 from msct_image import Image
-from sct_utils import extract_fname, printv, add_suffix
+from sct_utils import printv, add_suffix
 
 
 class Param:
@@ -36,7 +36,7 @@ def get_parser():
     parser.add_option(name="-o",
                       type_value='file_output',
                       description='Output file.',
-                      mandatory=True,
+                      mandatory=False,
                       example='data_pad.nii.gz')
 
     parser.usage.addSection('\nBasic image operations:')
@@ -60,6 +60,21 @@ def get_parser():
                       description='Concatenate data along the specified dimension',
                       mandatory=False,
                       example=['x', 'y', 'z', 't'])
+
+    parser.usage.addSection("\nOrientation operations: ")
+    parser.add_option(name="-getorient",
+                      description='Get orientation of the input image',
+                      mandatory=False)
+    parser.add_option(name="-setorient",
+                      type_value="multiple_choice",
+                      description='Set orientation of the input image',
+                      mandatory=False,
+                      example='RIP LIP RSP LSP RIA LIA RSA LSA IRP ILP SRP SLP IRA ILA SRA SLA RPI LPI RAI LAI RPS LPS RAS LAS PRI PLI ARI ALI PRS PLS ARS ALS IPR SPR IAR SAR IPL SPL IAL SAL PIR PSR AIR ASR PIL PSL AIL ASL'.split())
+    parser.add_option(name="-setorient_data",
+                      type_value="multiple_choice",
+                      description='Set orientation of the input image\'s data. Use with care !ro',
+                      mandatory=False,
+                      example='RIP LIP RSP LSP RIA LIA RSA LSA IRP ILP SRP SLP IRA ILA SRA SLA RPI LPI RAI LAI RPS LPS RAS LAS PRI PLI ARI ALI PRS PLS ARS ALS IPR SPR IAR SAR IPL SPL IAL SAL PIR PSR AIR ASR PIL PSL AIL ASL'.split())
 
     parser.usage.addSection("\nMulti-component operations:")
     parser.add_option(name='-mcs',
@@ -93,8 +108,12 @@ def main(args = None):
     arguments = parser.parse(sys.argv[1:])
     fname_in = arguments["-i"]
     n_in = len(fname_in)
-    fname_out = arguments["-o"]
     verbose = int(arguments['-v'])
+
+    if "-o" in arguments:
+        fname_out = arguments["-o"]
+    else:
+        fname_out = None
 
     # Open file(s)
     im_in = [Image(fn) for fn in fname_in]
@@ -116,6 +135,16 @@ def main(args = None):
         dim = dim_list.index(dim)
         im_out = split_data(im_in[0], dim)
 
+    elif "-getorient" in arguments:
+        orient = orientation(im_in[0], get=True, verbose=verbose)
+        im_out = None
+
+    elif "-setorient" in arguments:
+        im_out = [orientation(im_in[0], ori=arguments["-setorient"], set=True, verbose=verbose)]
+
+    elif "-setorient-data" in arguments:
+        im_out = [orientation(im_in[0], ori=arguments["-setorient-data"], set_data=True, verbose=verbose)]
+
     elif '-mcs' in arguments:
         if n_in != 1:
             printv(parser.usage.generate(error='ERROR: -mcs need only one input'))
@@ -133,21 +162,28 @@ def main(args = None):
         printv(parser.usage.generate(error='ERROR: you need to specify an operation to do on the input image'))
 
     # Write output
-    if len(im_out) == 1:
-        im_out[0].setFileName(fname_out)
-        im_out[0].save()
-    else:
-        for i, im in enumerate(im_out):
-            if len(im_out)<= len(dim_list):
-                suffix = '_'+dim_list[i].upper()
-            else:
-                suffix = '_'+str(i)
-            if "-split" in arguments:
-                suffix = '_'+dim_list[dim].upper()+str(i).zfill(4)
-            im.setFileName(add_suffix(fname_out, suffix))
-            im.save()
+    if im_out is not None:
+        printv('\nGenerate output files...', verbose)
+        if len(im_out) == 1:
+            im_out[0].setFileName(fname_out) if fname_out is not None else None
+            im_out[0].save()
+        else:
+            for i, im in enumerate(im_out):
+                if fname_out is not None:
+                    if len(im_out)<= len(dim_list):
+                        suffix = '_'+dim_list[i].upper()
+                    else:
+                        suffix = '_'+str(i)
+                    if "-split" in arguments:
+                        suffix = '_'+dim_list[dim].upper()+str(i).zfill(4)
+                    im.setFileName(add_suffix(fname_out, suffix))
+                im.save()
 
-    printv('Created file(s):\n--> '+str([im.file_name+im.ext for im in im_out])+'\n', verbose, 'info')
+        printv('Created file(s):\n--> '+str([im.file_name+im.ext for im in im_out])+'\n', verbose, 'info')
+    elif "-getorient" in arguments:
+        print orient
+    else:
+        printv('An error occurred in sct_image...', verbose, "error")
 
 
 def pad_image(im, padding_x=0, padding_y=0, padding_z=0):
@@ -180,6 +216,7 @@ def pad_image(im, padding_x=0, padding_y=0, padding_z=0):
     padded_data[padxi:padxf, padyi:padyf, padzi:padzf] = im.data
     im_out = im.copy()
     im_out.data = padded_data  # done after the call of the function
+    im_out.setFileName(im_out.file_name+'_pad'+im_out.ext)
 
     # adapt the origin in the sform and qform matrix
     new_origin = dot(im_out.hdr.get_best_affine(), [-padding_x, -padding_y, -padding_z, 1])
@@ -228,7 +265,7 @@ def split_data(im_in, dim):
     for i, dat in enumerate(data_split):
         im_out = im_in.copy()
         im_out.data = dat
-        im_out.setFileName(im_out.path+add_suffix(im_out.file_name+im_out.ext, '_'+dim_list[dim].upper()+str(i).zfill(4)))
+        im_out.setFileName(im_out.path+im_out.file_name+'_'+dim_list[dim].upper()+str(i).zfill(4)+im_out.ext)
         im_out_list.append(im_out)
 
     return im_out_list
@@ -257,6 +294,7 @@ def concat_data(im_in_list, dim):
     # write file
     im_out = im_in_list[0].copy()
     im_out.data = data_concat
+    im_out.setFileName(im_out.file_name+'_concat'+im_out.ext)
 
     return im_out
 
@@ -273,10 +311,11 @@ def multicomponent_split(im):
             dat_out = reshape(dat_out, dat_out.shape[:-1])
         '''
         data_out.append(dat_out)  # .astype('float32'))
-    im_out = [im.copy() for dat in data_out]
+    im_out = [im.copy() for j in range(len(data_out))]
     for i, im in enumerate(im_out):
         im.data = data_out[i]
         im.hdr.set_intent('vector', (), '')
+        im.setFileName(im.file_name+'_'+str(i)+im.ext)
     return im_out
 
 
@@ -301,7 +340,96 @@ def multicomponent_merge(im_list):
     im_out = im_list[0].copy()
     im_out.data = data_out.astype('float32')
     im_out.hdr.set_intent('vector', (), '')
+    im_out.setFileName(im_out.file_name+'_multicomponent'+im_out.ext)
     return im_out
+
+
+def orientation(im, ori=None, set=False, get=False, set_data=False, verbose=1):
+    # Get dimensions of data
+    printv('\nGet dimensions of data...', verbose)
+    nx, ny, nz, nt, px, py, pz, pt = im.dim
+    printv(str(nx) + ' x ' + str(ny) + ' x ' + str(nz)+ ' x ' + str(nt), verbose)
+
+    # if data are 3d, directly set or get orientation
+    if nt == 1:
+        if get:
+            # get orientation
+            printv('\nGet orientation...', verbose)
+            im_out = None
+            return get_orientation(im)
+        elif set:
+            # set orientation
+            printv('\nChange orientation...', verbose)
+            im_out = set_orientation(im, ori)
+        elif set_data:
+            im_out = set_orientation(im, ori, True)
+        else:
+            im_out = None
+
+    else:
+        # 4D data: split along T dimension
+        printv('\nSplit along T dimension...', verbose)
+        im_split_list = split_data(im, 3)
+        for im_s in im_split_list:
+            im_s.save()
+
+        if get:
+            # get orientation
+            printv('\nGet orientation...', verbose)
+            im_out=None
+            return get_orientation(im_split_list[0])
+        elif set:
+            # set orientation
+            printv('\nChange orientation...', verbose)
+            im_changed_ori_list = []
+            for im_s in im_split_list:
+                im_set = set_orientation(im_s, ori)
+                im_changed_ori_list.append(im_set)
+            printv('\nMerge file back...', verbose)
+            im_out = concat_data(im_changed_ori_list, 3)
+        elif set_data:
+            printv('\nSet orientation of the data only is not compatible with 4D data...', verbose, 'error')
+        else:
+            im_out = None
+
+    im_out.setFileName(im.file_name+'_'+ori+im.ext)
+    return im_out
+
+
+# get_orientation
+# ==========================================================================================
+def get_orientation(im):
+    """
+    Get orientation from 3D data
+    :param im:
+    :return:
+    """
+    from sct_utils import run
+    string_out = 'Input image orientation : '
+    # get orientation
+    status, output = run('isct_orientation3d -i '+im.absolutepath+' -get ', 0)
+    # check status
+    if status != 0:
+        printv('ERROR in get_orientation.', 1, 'error')
+    orientation = output[output.index(string_out)+len(string_out):]
+    # orientation = output[26:]
+    return orientation
+
+
+# set_orientation
+# ==========================================================================================
+def set_orientation(im, orientation, data_inversion=False):
+    fname_out = im.file_name+'_'+orientation+im.ext
+    if not data_inversion:
+        from sct_utils import run
+        run('isct_orientation3d -i '+im.absolutepath+' -orientation '+orientation+' -o '+fname_out, 0)
+        im_out = Image(fname_out)
+    else:
+        im_out = im.copy()
+        im_out.change_orientation(orientation, True)
+        im_out.setFileName(fname_out)
+    return im_out
+
 
 
 
