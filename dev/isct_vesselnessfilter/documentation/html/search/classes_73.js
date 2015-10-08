71dc4c397f6b860591c9a12d5345e5c84757308a
Olivier Comtois <comtois.olivier.18@gmail.com>
2015-10-08 16:21:56 -0400

    DEV : Removed isct_vesselnessfilter duplicate
    Former-commit-id: 14afac438f95a507dd9644ebc7bbee76d447a14d
    Former-commit-id: 290feaa4d61f829b8b502e5870123d96ead2313f

--- dev/isct_vesselnessfilter/documentation/html/search/classes_73.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['scregion',['SCRegion',['../class_s_c_region.html',1,'']]],
-  ['sctemplate',['SCTemplate',['../class_s_c_template.html',1,'']]],
-  ['spinalcord',['SpinalCord',['../class_spinal_cord.html',1,'']]],
-  ['symmetricalcropping',['SymmetricalCropping',['../class_symmetrical_cropping.html',1,'']]]
-];
--- dev/isct_vesselnessfilter/documentation/html/search/classes_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vertebralidentification',['VertebralIdentification',['../class_vertebral_identification.html',1,'']]],
-  ['vertex',['Vertex',['../class_vertex.html',1,'']]]
-];
