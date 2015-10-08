c77ff0fe0deabecfe4a6374c0cd2a9488992aba6
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-08 09:22:30 -0400

    OPT:  corrected small bug due to paths

--- testing/test_sct_image.py
+++ testing/test_sct_image.py
@@ -15,34 +15,64 @@
 import commands
 
 
-def test(path_data):
+def test(data_path):
 
-    folder_data = 'mt/'
-    file_data = ['mtr.nii.gz']
+    folder_data = ['mt/', 't2/', 'dmri/']
+    file_data = ['mtr.nii.gz', 't2.nii.gz', 'dmri.nii.gz']
 
     output = ''
     status = 0
 
     pad = 2
-    cmd = 'sct_image -i ' + path_data + folder_data + file_data[0] \
+    cmd = 'sct_image -i ' + data_path + folder_data[0] + file_data[0] \
                 + ' -o test.nii.gz' \
                 + ' -pad 0,0,'+str(pad)
     output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
 
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
+    s0, o0 = commands.getstatusoutput(cmd)
+    status += s0
+    output += o0
 
+
+    # test 3d data
+    cmd = 'sct_image -i ' + data_path + folder_data[1] + file_data[1] + ' -getorient '
+    output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
+    s1, o1 = commands.getstatusoutput(cmd)
+
+    status += s1
+    output += o1
+
+    from time import sleep
+    sleep(1)  # here add one second, otherwise the next test will try to create a temporary folder with the same name (because it runs in less than one second)
+
+
+    # test 4d data
     if status == 0:
+        cmd = 'sct_image -i ' + data_path + folder_data[2] + file_data[2] + ' -getorient '
+        output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
+        s2, o2 = commands.getstatusoutput(cmd)
+
+        status += s2
+        output += o2
+
+    if s0 == 0:
         from msct_image import Image
-        nx, ny, nz, nt, px, py, pz, pt = Image(path_data + folder_data + file_data[0]).dim
+        nx, ny, nz, nt, px, py, pz, pt = Image(data_path + folder_data[0] + file_data[0]).dim
         nx2, ny2, nz2, nt2, px2, py2, pz2, pt2 = Image("test.nii.gz").dim
 
         if nz2 != nz+2*pad:
             status = 99
-            output += '\nResulting image\'s dimension differs from expected:\n'
+            output += '\nResulting pad image\'s dimension differs from expected:\n'
             output += 'dim : '+ str(nx2) +'x'+ str(ny2) +'x'+ str(nz2)+'\n'
             output += 'expected : '+ str(nx) +'x'+ str(ny) +'x'+ str(nz+2*pad)+'\n'
+    if s1 == 0:
+        if o1 != "AIL":
+            status = 99
+            output += '\nResulting orientation differs from expected:\n'
+    if s2 == 0:
+        if o2 != "RPI":
+            status = 99
+            output += '\nResulting orientation differs from expected:\n'
 
     return status, output
 
