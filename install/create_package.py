5cc3d0b6ca63e03b52ed686142c63f536e12db15
Olivier Comtois <comtois.olivier.18@gmail.com>
2015-10-13 11:22:36 -0400

    REF : Removed commented lines
    Former-commit-id: 368bd442ab8dd1a1ca69ef462c15d78a470da87c [formerly e58c6fa1f4895e02bc769cd95494a4d632bac9a2]
    Former-commit-id: 272b9e152ca99dc66367e8b1f0936b24ab41f640
    Former-commit-id: ba0391f53be831b4f8a4c1bfeefbca1c66c4c80d

--- install/create_package.py
+++ install/create_package.py
@@ -89,9 +89,6 @@ if OSname == 'osx':
 elif OSname == 'linux':
     sct.run('cp -r ../bin/linux/* '+folder_sct+'spinalcordtoolbox/bin/')
 
-# added modif.txt into bin/ folder for documentation
-# sct.run('cp ../scri/modif.txt '+folder_sct+'spinalcordtoolbox/bin/')
-
 # data
 sct.run('cp -rf ../data '+folder_sct+'spinalcordtoolbox/')
 
