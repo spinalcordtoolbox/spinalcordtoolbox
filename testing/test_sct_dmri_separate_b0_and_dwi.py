00e3b4dd26674f9a1c11182a9c59dae5b31b3fcd
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-14 09:52:46 -0400

    NEW: integrity check for sct_dmri_separate_b0_and_dwi
    Former-commit-id: 65b9f18d86738a68381c26c007a4b70b8adb90b8
    Former-commit-id: 98f7a5bad6c19276551bb7fc81122eeba895d583

--- testing/test_sct_dmri_separate_b0_and_dwi.py
+++ testing/test_sct_dmri_separate_b0_and_dwi.py
@@ -14,12 +14,14 @@
 #import sct_utils as sct
 import commands
 
-
 def test(data_path):
 
     # parameters
     folder_data = 'dmri/'
-    file_data = ['dmri.nii.gz', 'bvecs.txt']
+    file_data = ['dmri.nii.gz', 'bvecs.txt', 'dwi.nii.gz']
+
+    output = ''
+    status = 0
 
     # define command
     cmd = 'sct_dmri_separate_b0_and_dwi -i ' + data_path + folder_data + file_data[0] \
@@ -30,8 +32,30 @@ def test(data_path):
           + ' -r 0'
     # return
     #return sct.run(cmd, 0)
-    return commands.getstatusoutput(cmd)
+    output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
+    s, o = commands.getstatusoutput(cmd)
+    status += s
+    output += o
 
+    if status == 0:
+        from msct_image import Image
+        from numpy import sum
+        threshold = 1e-3
+        ref_dwi = Image(data_path+folder_data+file_data[2])
+        new_dwi = Image('dwi.nii.gz')
+        diff_dwi = ref_dwi.data-new_dwi.data
+        if sum(diff_dwi) > threshold:
+            status = 99
+            output += '\nResulting DWI image differs from gold-standard.\n'
+
+        ref_b0 = Image(data_path+folder_data+'dmri_T0000.nii.gz')
+        new_b0 = Image('b0.nii.gz')
+        diff_b0 = ref_b0.data - new_b0.data
+        if sum(diff_b0) > threshold:
+            status = 99
+            output = '\nResulting b0 image differs from gold-standard.\n'
+
+    return status, output
 
 if __name__ == "__main__":
     # call main function
