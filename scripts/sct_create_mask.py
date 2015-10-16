9e1c1ae85f32acf7566085adc6f166c1004e6d45
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-16 14:51:23 -0400

    BUG: do not exit when tmp folder is not deleted
    Former-commit-id: 74559fa88c8d539a78a0ef37433bd06b5a5a0e80 [formerly ba18e75c1d4baf02bb380c3c04eabd83fe62ef83]
    Former-commit-id: 8941553a7df984f064cde85dbcdedfc69bdc97d1
    Former-commit-id: 71c0b755aab0f6e9ebfe6a3c40336aa2b1a85870

--- scripts/sct_create_mask.py
+++ scripts/sct_create_mask.py
@@ -242,7 +242,7 @@ def create_mask():
     # Remove temporary files
     if param.remove_tmp_files == 1:
         sct.printv('\nRemove temporary files...', param.verbose)
-        sct.run('rm -rf '+path_tmp, param.verbose)
+        sct.run('rm -rf '+path_tmp, param.verbose, error_exit='warning')
 
     # to view results
     sct.printv('\nDone! To view results, type:', param.verbose)
