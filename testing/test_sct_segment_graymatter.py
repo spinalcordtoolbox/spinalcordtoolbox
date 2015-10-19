bad64a3981f7555a1a33f000bcc5a708a59a5a50
Sara Dupont <sara.m.dupont@gmail.com>
2015-10-19 14:37:28 -0400

    OPT: new version of the testing function for sct_segment_graymatter (ready to use with isct_test_function)

--- testing/test_sct_segment_graymatter.py
+++ testing/test_sct_segment_graymatter.py
@@ -18,7 +18,7 @@ from pandas import DataFrame
 import sct_segment_graymatter
 from msct_image import Image
 import sct_utils as sct
-from numpy import sum
+from numpy import sum, mean
 # append path that contains scripts, to be able to load modules
 status, path_sct = commands.getstatusoutput('echo $SCT_DIR')
 sys.path.append(path_sct + '/scripts')
@@ -28,7 +28,7 @@ sys.path.append(path_sct + '/scripts')
 def test(path_data, parameters=''):
 
     if not parameters:
-        parameters = '-i mt/mt0.nii.gz -s mt/mt0_seg.nii.gz -l mt/label/template/MNI-Poly-AMU_level.nii.gz -normalize 1 -ref mt/mt0_gmseg.nii.gz'
+        parameters = '-i mt/mt0.nii.gz -s mt/mt0_seg.nii.gz -l mt/label/template/MNI-Poly-AMU_level.nii.gz -normalize 1 -ref mt/mt0_manual_gmseg.nii.gz'
 
     parser = sct_segment_graymatter.get_parser()
     dict_param = parser.parse(parameters.split(), check_file_exist=False)
@@ -39,74 +39,100 @@ def test(path_data, parameters=''):
     if not (os.path.isfile(dict_param_with_path['-i']) and os.path.isfile(dict_param_with_path['-s']) and os.path.isfile(dict_param_with_path['-l']) and os.path.isfile(dict_param_with_path['-ref'])):
         status = 200
         output = 'ERROR: the file(s) provided to test function do not exist in folder: ' + path_data
-        return status, output, DataFrame(data={'status': status, 'output': output, 'dice': float('nan'), 'hausdorff': float('nan')}, index=[path_data])
+        return status, output, DataFrame(data={'status': status, 'output': output, 'dice_gm': float('nan'), 'dice_wm': float('nan'), 'hausdorff': float('nan'), 'med_dist': float('nan')}, index=[path_data])
+
+    import time, random
+    subject_folder = path_data.split('/')
+    if subject_folder[-1] == '' and len(subject_folder) > 1:
+        subject_folder = subject_folder[-2]
+    else:
+        subject_folder = subject_folder[-1]
+    path_output = sct.slash_at_the_end('sct_segment_graymatter_' + subject_folder + '_' + time.strftime("%y%m%d%H%M%S") + '_'+str(random.randint(1, 1000000)), slash=1)
+    param_with_path += ' -o ' + path_output
 
     cmd = 'sct_segment_graymatter ' + param_with_path
     status, output = sct.run(cmd, 0)
 
 
     # initialization of results: must be NaN if test fails
-    result_dice, result_hausdorff = float('nan'), float('nan')
+    result_dice_gm, result_dice_wm, result_hausdorff, result_median_dist = float('nan'), float('nan'), float('nan'), float('nan')
     if status == 0:
-        pass
-        '''
-        # extraction of results
-        output_split = output.split('Maximum x-y error = ')[1].split(' mm')
-        result_dist_max = float(output_split[0])
-        result_mse = float(output_split[1].split('Accuracy of straightening (MSE) = ')[1])
+        target_name = sct.extract_fname(dict_param_with_path["-i"])[1]
 
-        # integrity testing - straightening has been tested with v2.0.6 on several images.
-        # mse is less than 1.5 and dist_max is less than 4
-        if result_dist_max > 4.0 or result_mse > 1.5:
+        dice_fname = path_output+'dice_'+target_name+'_'+dict_param_with_path["-res-type"]+'.txt'
+        hausdorff_fname = path_output+'hd_'+target_name+'_'+dict_param_with_path["-res-type"]+'.txt'
+
+        # Extracting dice results:
+        dice = open(dice_fname, 'r')
+        dice_lines = dice.readlines()
+        dice.close()
+        gm_start = dice_lines.index('Dice coefficient on the Gray Matter segmentation:\n')
+        wm_start = dice_lines.index('Dice coefficient on the White Matter segmentation:\n')
+
+        # extracting dice on GM
+        gm_dice_lines = dice_lines[gm_start:wm_start-1]
+        gm_dice_lines = gm_dice_lines[gm_dice_lines.index('2D Dice coefficient by slice:\n')+1:-1]
+
+        null_slices = []
+        gm_dice = []
+        for line in gm_dice_lines:
+            n_slice, dc = line.split(' ')
+            # remove \n from dice result
+            dc = dc[:-1]
+            if dc == '0':
+                null_slices.append(n_slice)
+            else:
+                gm_dice.append(float(dc))
+        result_dice_gm = mean(gm_dice)
+
+        # extracting dice on WM
+        wm_dice_lines = dice_lines[wm_start:]
+        wm_dice_lines = wm_dice_lines[wm_dice_lines.index('2D Dice coefficient by slice:\n')+1:]
+        wm_dice = []
+        for line in wm_dice_lines:
+            n_slice, dc = line.split(' ')
+            # remove \n from dice result
+            if line is not wm_dice_lines[-1]:
+                dc = dc[:-1]
+            if n_slice not in null_slices:
+                wm_dice.append(float(dc))
+        result_dice_wm = mean(wm_dice)
+
+        # Extracting hausdorff distance results
+        hd = open(hausdorff_fname, 'r')
+        hd_lines = hd.readlines()
+        hd.close()
+
+        # remove title of columns and last empty/non important lines
+        hd_lines = hd_lines[1:-4]
+
+        hausdorff = []
+        max_med = []
+        for line in hd_lines:
+            slice_id, res = line.split(':')
+            slice, n_slice = slice_id.split(' ')
+            if n_slice not in null_slices:
+                hd, med1, med2 = res[:-1].split(' - ')
+                hd, med1, med2 = float(hd), float(med1), float(med2)
+                hausdorff.append(hd)
+                max_med.append(max(med1, med2))
+
+        result_hausdorff = mean(hausdorff)
+        result_median_dist = mean(max_med)
+
+        # Integrity check
+        hd_threshold = 1.5 # in mm
+        wm_dice_threshold = 0.8
+        if result_hausdorff > hd_threshold or result_dice_wm < wm_dice_threshold:
             status = 99
-        '''
+            output += '\nResulting segmentation is too different from manual segmentation:\n' \
+                      'WM dice: '+result_dice_wm+'\n' \
+                      'Hausdorff distance: '+result_hausdorff+'\n'
 
     # transform results into Pandas structure
-    results = DataFrame(data={'status': status, 'output': output, 'dice': result_dice, 'hausdorff': result_hausdorff}, index=[path_data])
+    results = DataFrame(data={'status': status, 'output': output, 'dice_gm': result_dice_gm, 'dice_wm': result_dice_wm, 'hausdorff': result_hausdorff, 'med_dist': result_median_dist}, index=[path_data])
 
     return status, output, results
-
-
-
-'''
-# ##################################################################################################################
-output = ''
-status = 0
-
-# parameters
-folder_data = 'mt/'
-file_data = ['mt0.nii.gz', 'mt0_seg.nii.gz', 'label/template/MNI-Poly-AMU_level.nii.gz', 'mt0_gmseg.nii.gz']
-
-# define command
-cmd = 'sct_segment_graymatter -i ' + path_data + folder_data + file_data[0] \
-    + ' -s ' + path_data + folder_data + file_data[1] \
-    + ' -l ' + path_data + folder_data + file_data[2] \
-    + ' -normalize 1 '\
-    + ' -v 1'
-
-output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
-# run command
-s, o = commands.getstatusoutput(cmd)
-status += s
-output += o
-
-# if command ran without error, test integrity
-if status == 0:
-    threshold = 1e-3
-    # compare with gold-standard labeling
-    data_original = Image(path_data + folder_data + file_data[-1]).data
-    data_totest = Image('mt0_gmseg.nii.gz').data
-    # check if non-zero elements are present when computing the difference of the two images
-    diff = data_original - data_totest
-
-
-    if abs(sum(diff))> threshold:
-        Image(param=diff, absolutepath='res_differences_from_gold_standard.nii.gz').save()
-        status = 99
-        output += '\nResulting image differs from gold-standard.'
-
-return status, output
-'''
 
 
 if __name__ == "__main__":
