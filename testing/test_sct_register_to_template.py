5436f881fc8b0975b02fc485e153c807eaf79709
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-10-22 16:48:13 -0400

    TEST: test_sct_register_to_template now keep outputs

--- testing/test_sct_register_to_template.py
+++ testing/test_sct_register_to_template.py
@@ -29,7 +29,7 @@ def test(path_data='', parameters=''):
     if not parameters:
         parameters = '-i t2/t2.nii.gz -l t2/labels.nii.gz -s t2/t2_seg.nii.gz ' \
                      '-p step=1,type=seg,algo=slicereg,metric=MeanSquares,iter=5:step=2,type=seg,algo=bsplinesyn,iter=3 ' \
-                     '-t template/'
+                     '-t template/ -r 0'
 
     parser = sct_register_to_template.get_parser()
     dict_param = parser.parse(parameters.split(), check_file_exist=False)
