0abec6928a7f8df9ba83e381dc3e9303bf36a234
Olivier Comtois <comtois.olivier.18@gmail.com>
2015-11-06 14:40:44 -0500

    TEST : Added integrity testing for get_centerline auto

--- testing/test_sct_get_centerline.py
+++ testing/test_sct_get_centerline.py
@@ -13,40 +13,56 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
+import os.path
 import commands
 from msct_image import Image
 from sct_get_centerline import ind2sub
 import math
 import sct_utils as sct
 import numpy as np
+from pandas import DataFrame
+import sct_get_centerline
 
 
-def test(path_data):
+def test(path_data='', parameters=''):
+
+    if not parameters:
+        parameters = '-i t2/t2.nii.gz -t t2 -method auto'
 
     # parameters
     folder_data = 't2/'
     file_data = ['t2.nii.gz', 't2_centerline_init.nii.gz', 't2_centerline_labels.nii.gz', 't2_seg_manual.nii.gz']
 
-    output = ''
-    status = 0
+    parser = sct_get_centerline.GetCenterlineScript.get_parser()
+    dict_param = parser.parse(parameters.split(), check_file_exist=False)
+    contrast = dict_param['-t']
+    dict_param_with_path = parser.add_path_to_file(dict_param, path_data, input_file=True)
+    param_with_path = parser.dictionary_to_string(dict_param_with_path)
 
-    # define command
-    cmd = 'sct_get_centerline -i ' + path_data + folder_data + file_data[0] \
-        + ' -method auto' \
-        + ' -t t2 ' \
-        + ' -v 1'
-    output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-    # small integrity test on scad
-    try :
+    # Check if input files exist
+    if not (os.path.isfile(dict_param_with_path['-i']) and os.path.isfile(dict_param_with_path['-c'])):
+        status = 200
+        output = 'ERROR: the file(s) provided to test function do not exist in folder: ' + path_data
+        return status, output, DataFrame(data={'status': status, 'output': output, 'mse': float('nan'), 'dist_max': float('nan')}, index=[path_data])
+
+    cmd = 'sct_get_centerline '+param_with_path
+    status, output = sct.run(cmd, 0)
+    scad_centerline = Image("centerline.nii.gz")
+    manual_seg = (contrast +'_seg_manual.nii.gz')
+
+    max_distance = 0
+    standard_deviation = 0
+    average = 0
+    root_mean_square = 0
+    overall_distance = 0
+    max_distance = 0
+    overall_std = 0
+    rmse = 0
+
+    try:
         if status == 0:
-            manual_seg = Image(path_data + folder_data + file_data[3])
-            centerline_scad = Image(path_data + folder_data + file_data[0])
-            centerline_scad.change_orientation()
             manual_seg.change_orientation()
-
+            scad_centerline.change_orientation()
             from scipy.ndimage.measurements import center_of_mass
             # find COM
             iterator = range(manual_seg.data.shape[2])
@@ -57,11 +73,11 @@ def test(path_data):
                 com_x[iz], com_y[iz] = center_of_mass(manual_seg.data[:, :, iz])
             max_distance = {}
             distance = {}
-            for iz in range(1, centerline_scad.data.shape[2]-1):
-                ind1 = np.argmax(centerline_scad.data[:, :, iz])
-                X,Y = ind2sub(centerline_scad.data[:, :, iz].shape,ind1)
+            for iz in range(1, scad_centerline.data.shape[2]-1):
+                ind1 = np.argmax(scad_centerline.data[:, :, iz])
+                X,Y = ind2sub(scad_centerline.data[:, :, iz].shape,ind1)
                 com_phys = np.array(manual_seg.transfo_pix2phys([[com_x[iz], com_y[iz], iz]]))
-                scad_phys = np.array(centerline_scad.transfo_pix2phys([[X, Y, iz]]))
+                scad_phys = np.array(scad_centerline.transfo_pix2phys([[X, Y, iz]]))
                 distance_magnitude = np.linalg.norm([com_phys[0][0]-scad_phys[0][0], com_phys[0][1]-scad_phys[0][1], 0])
                 if math.isnan(distance_magnitude):
                     print "Value is nan"
@@ -69,23 +85,21 @@ def test(path_data):
                     distance[iz] = distance_magnitude
 
             max_distance = max(distance.values())
-            #if max_distance > 5:
-                #sct.printv("Max distance between scad and manual centerline is greater than 5 mm", type="warning")
+            standard_deviation = np.std(np.array(distance.values()))
+            average = sum(distance.values())/len(distance)
+            root_mean_square = np.sqrt(np.mean(np.square(distance.values())))
+            overall_distance = average
+            max_distance = max(distance.values())
+            overall_std = standard_deviation
+            rmse = root_mean_square
 
     except Exception, e:
         sct.printv("Exception found while testing scad integrity")
         sct.printv(e.message, type="error")
+        output = e.message
 
-    # define command: DOES NOT RUN IT BECAUSE REQUIRES FSL FLIRT
-    # cmd = 'sct_get_centerline -i ' + path_data + folder_data + file_data[0] \
-    #     + ' -method point' \
-    #     + ' -p ' + path_data + folder_data + file_data[1] \
-    #     + ' -g 1'\
-    #     + ' -k 4'
-    # output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
-    # s, o = commands.getstatusoutput(cmd)
-    # status += s
-    # output += o
+    result_mse, result_dist_max = rmse, max_distance
+    results = DataFrame(data={'status': status, 'output': output, 'mse': result_mse, 'dist_max': result_dist_max}, index=[path_data])
 
     # define command
     cmd = 'sct_get_centerline -i ' + path_data + folder_data + file_data[0] \
@@ -97,7 +111,7 @@ def test(path_data):
     status += s
     output += o
 
-    return status, output
+    return status, output, results
 
 
 if __name__ == "__main__":
