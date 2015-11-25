65ab7d0c6baf46877e8f1ae198ddadac117b22d4
Sara Dupont <sara.m.dupont@gmail.com>
2015-11-25 11:35:24 -0500

    NEW: warper for sct_dice_coefficient binary

--- scripts/isct_dice_coefficient.py
+++ scripts/isct_dice_coefficient.py
@@ -15,10 +15,9 @@ import sct_utils as sct
 
 #TODO: change name to sct_dice_coefficient
 
-
 def get_parser():
     parser = Parser(__file__)
-    parser.usage.set_description('Compute the Dice Coefficient. ')
+    parser.usage.set_description('Compute the Dice Coefficient. Note: indexing (in both time and space) starts with 0 not 1! Inputting -1 for a size will set it to the full image extent for that dimension.')
     parser.add_option(name='-i',
                       type_value='image_nifti',
                       description='First input image.',
@@ -29,6 +28,11 @@ def get_parser():
                       description='Second input image.',
                       mandatory=True,
                       example='t2_manual_seg.nii.gz')
+    parser.add_option(name='-2d-slices',
+                      type_value='multiple_choice',
+                      description='Compute DC on 2D slices in the specified dimension',
+                      mandatory=False,
+                      example=['0', '1', '2'])
     parser.add_option(name='-b',
                       type_value=[[','], 'int'],
                       description='Bounding box with the coordinates of the origin and the size of the box as follow: x_origin,x_size,y_origin,y_size,z_origin,z_size',
@@ -36,7 +40,12 @@ def get_parser():
                       example='5,10,5,10,10,15')
     parser.add_option(name='-bmax',
                       type_value='multiple_choice',
-                      description='',
+                      description='Use maximum bounding box of the images union to compute DC',
+                      mandatory=False,
+                      example=['0', '1'])
+    parser.add_option(name='-bzmax',
+                      type_value='multiple_choice',
+                      description='Use maximum bounding box of the images union in the "Z" direction to compute DC',
                       mandatory=False,
                       example=['0', '1'])
     parser.add_option(name='-bin',
@@ -44,6 +53,11 @@ def get_parser():
                       description='Binarize image before computing DC. (Put non-zero-voxels to 1)',
                       mandatory=False,
                       example=['0', '1'])
+    parser.add_option(name='-o',
+                      type_value='file_output',
+                      description='Output file with DC results (.txt)',
+                      mandatory=False,
+                      example='dice_coeff.txt')
     parser.add_option(name='-v',
                       type_value='multiple_choice',
                       description='Verbose.',
@@ -52,3 +66,38 @@ def get_parser():
                       example=['0', '1'])
 
     return parser
+
+if __name__ == "__main__":
+    parser = get_parser()
+    arguments = parser.parse(sys.argv[1:])
+
+    fname_input1 = arguments['-i']
+    fname_input2 = arguments['-d']
+
+    if '-bin' in arguments:
+        fname_input1_bin = sct.add_suffix(fname_input1, '_bin')
+        sct.run('sct_maths -i '+fname_input1+' -bin '+fname_input1_bin)
+        fname_input1 = fname_input1_bin
+        fname_input2_bin = sct.add_suffix(fname_input2, '_bin')
+        sct.run('sct_maths -i '+fname_input2+' -bin '+fname_input2_bin)
+        fname_input2 = fname_input2_bin
+
+    cmd = 'isct_dice_coefficient '+fname_input1+' '+fname_input2
+
+    if '-2d-slices' in arguments:
+        cmd += ' -2d-slices '+arguments['-2d-slices']
+    if '-b' in arguments:
+        bounding_box = ' '.join(arguments['-b'])
+        cmd += ' -b '+bounding_box
+    if '-bmax' in arguments and arguments['-bmax'] == '0':
+        cmd += ' -bmax'
+    if '-bzmax' in arguments and arguments['-bzmax'] == '0':
+        cmd += ' -bzmax'
+    if '-o' in arguments:
+        cmd += ' -o '+arguments['o']
+
+    verbose = arguments['-v']
+    cmd += ' -v '+verbose
+
+    sct.run(cmd, verbose)
+    sct.printv('\nDone!', verbose)
