a752871a9a502749de5e19a9e3d99e91a8e31a99
Julien Cohen-Adad <jcohen@polymtl.ca>
2015-11-29 13:01:11 -0500

    updated batch_processing_PAM50
    Former-commit-id: 77d9b75eb13ff259365f149f6bdb19d62112945d
    Former-commit-id: cec15d08bf3d5c29ceefcf97fef5c7ce44deb37e

--- batch_processing_PAM50.sh
+++ batch_processing_PAM50.sh
@@ -9,8 +9,10 @@
 # download the PAM50 template
 sct_download_data -d PAM50
 
-# create variable to template
+# create temporary variable to template
 SCT_PAM50=`pwd`/PAM50
+# N.B. To create permanent variable, add the following line to your .bashrc:
+# SCT_PAM50="path_to_template"
 
 # download example data (errsm_30)
 sct_download_data -d sct_example_data
@@ -21,36 +23,6 @@ echo "Started at: $(date +%x_%r)"
 # go in folder
 cd sct_example_data
 
-# t2
-# ===========================================================================================
-cd t2
-# spinal cord segmentation
-sct_propseg -i t2.nii.gz -c t2 -mesh -max-deformation 4 -init 130
-# tips: we use "-max-deformation 4" otherwise the segmentation does not cover the whole spinal cord
-# tips: we use "-init 130" to start propagation closer to a region which would otherwise give poor segmentation (try it with and without the parameter).
-# tips: we use "-mesh" to get the mesh of the segmentation, which can be viewed using MITKWORKBENCH
-# check your results:
-fslview t2 -b 0,800 t2_seg -l Red -t 0.5 &
-# vertebral labeling. Here we use the fact that the FOV is centered at C7.
-sct_label_vertebrae -i t2.nii.gz -s t2_seg.nii.gz -initcenter 7
-# create labels at C2 and T2 vertebral levels
-sct_label_utils -i t2_seg_labeled.nii.gz -p label-vertebrae -vert 2,9
-# register to template
-# tips: here we used only iter=1 for the third step for faster processing. 
-sct_register_to_template -i t2.nii.gz -s t2_seg.nii.gz -l labels.nii.gz -t $SCT_PAM50/template -param step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=3,shrink=1,metric=MeanSquares:step=3,type=im,algo=syn,metric=CC,iter=1 -r 0
-# warp template and white matter atlas
-sct_warp_template -d t2.nii.gz -w warp_template2anat.nii.gz -t $SCT_PAM50 -a 0
-# check results
-fslview t2.nii.gz -b 0,800 label/template/MNI-Poly-AMU_T2.nii.gz -b 0,4000 label/template/MNI-Poly-AMU_level.nii.gz -l MGH-Cortical -t 0.5 label/template/MNI-Poly-AMU_GM.nii.gz -l Red-Yellow -b 0.5,1 label/template/MNI-Poly-AMU_WM.nii.gz -l Blue-Lightblue -b 0.5,1 &
-# compute average cross-sectional area and volume between C3 and C4 levels
-sct_process_segmentation -i t2_seg.nii.gz -p csa -t label/template/MNI-Poly-AMU_level.nii.gz -vert 3:4
-# --> Mean CSA: 78.2927207039 +/- 0.770570121273 mm^2
-# --> Volume (in volume.txt): 2596.0 mm^3
-
-# go back to root folder
-cd ..
-
-
 #  t1
 # ----------
 cd t1
@@ -58,12 +30,24 @@ cd t1
 sct_propseg -i t1.nii.gz -c t1
 # check results
 fslview t1 -b 0,800 t1_seg -l Red -t 0.5 &
+# vertebral labeling. Here we use the fact that the axial slice #146 is located at the C5/C6 disc
+sct_label_vertebrae -i t1.nii.gz -s t1_seg.nii.gz -initz 146,5
+# create labels at C2 and T2 vertebral levels
+sct_label_utils -i t1_seg_labeled.nii.gz -p label-vertebrae -vert 2,9
+# register to template
+# tips: here we used only iter=1 for the third step for faster processing.
+sct_register_to_template -i t1.nii.gz -s t1_seg.nii.gz -l labels.nii.gz -t $SCT_PAM50/template -param step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=3,shrink=1,metric=MeanSquares:step=3,type=im,algo=syn,metric=CC,iter=1 -r 0
+# warp template and white matter atlas
+sct_warp_template -d t2.nii.gz -w warp_template2anat.nii.gz -t $SCT_PAM50 -a 0
+
+
 # create mask around spinal cord (for cropping)
 sct_create_mask -i t1.nii.gz -p centerline,t1_seg.nii.gz -size 61 -f box -o mask_t1.nii.gz
 # crop t1 and t1_seg (for faster registration)
 sct_crop_image -i t1.nii.gz -m mask_t1.nii.gz -o t1_crop.nii.gz
 sct_crop_image -i t1_seg.nii.gz -m mask_t1.nii.gz -o t1_seg_crop.nii.gz
-# register to template (which was previously registered to the t2).
+# register to template
+
 sct_register_multimodal -i ../t2/label/template/MNI-Poly-AMU_T1.nii.gz -iseg ../t2/label/template/MNI-Poly-AMU_cord.nii.gz -d t1_crop.nii.gz -dseg t1_seg_crop.nii.gz -param step=1,type=seg,algo=slicereg,metric=MeanSquares:step=2,type=im,algo=syn,iter=3,gradStep=0.2,metric=CC
 # concatenate transformations
 sct_concat_transfo -w ../t2/warp_template2anat.nii.gz,warp_MNI-Poly-AMU_T12t1_crop.nii.gz -d t1.nii.gz -o warp_template2t1.nii.gz
