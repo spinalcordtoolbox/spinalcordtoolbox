161ec150cc68df6f77ad02c771865e9b41c19c00
Tanguy DUVAL <duvaltanguy@gmail.com>
2016-01-15 15:43:18 -0500

    Matlab
    Former-commit-id: 8d603d910ef1f85b6db35943dc923abbea899e9b [formerly 9c41b714da69bcf7ad14433bb14d16bcfaaab41c] [formerly 9b9fc91e0f8bd70e2e088f7c0adfe91eca27d81f [formerly 1d4a8fce2430d804ec54641a332b5394e77e0aab [formerly 75ca2030dd64fc4784aca4706bb2cb12b3cce89d]]]
    Former-commit-id: 8d603d910ef1f85b6db35943dc923abbea899e9b [formerly 9c41b714da69bcf7ad14433bb14d16bcfaaab41c]
    Former-commit-id: 3d83d17e3929345ad0464653c1f34f92dc1b7955 [formerly d7ca4e3aaf8499d95b2a5b05cffa6ded2b0f683d [formerly fe0d281faea93baf5f7a68714fb35fea4f16eae5]]
    Former-commit-id: 33ffee33003fd51b64feaf30d7966eb4cfc4d3dc
    Former-commit-id: dc77bfd6b5699b73e51f125d1fb9bccb7cf50f53

--- matlab/ImageTools/sct_reorient.m
+++ matlab/ImageTools/sct_reorient.m
@@ -3,7 +3,7 @@ function nifti_reorient=sct_reorient(nifti)
 % Example:
 % sct_reorient('img.nii') --> output 'img_RPI.nii'
 [basename,~, ext]=sct_tool_remove_extension(nifti,1);
-cmd=['sct_orientation -i ' nifti ' -s RPI'];
+cmd=['sct_image -i ' nifti ' -setorient RPI'];
 unix(cmd)
 
 nifti_reorient=[basename '_RPI' ext];
