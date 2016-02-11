b3a7730dbd010a3cf8d55d706c9dc4fabd38765f
Tanguy Duval <duvaltanguy@gmail.com>
2016-02-11 14:42:51 -0500

    MATLAB
    Former-commit-id: f218cce36a135a25955f8a7cc87b8b2f43a2ebd3 [formerly de9d79ee60abd66eca5d866d8ed80c8a37dc3fb7] [formerly 995c42e7db46e7b1bcff28c6e0126ff1d20d67da [formerly 79566e20fc2a9024ed7c958842b402286ad8c7ea [formerly 8033809980b6730f334615bad46a7973ee9c0e0a]]]
    Former-commit-id: f218cce36a135a25955f8a7cc87b8b2f43a2ebd3 [formerly de9d79ee60abd66eca5d866d8ed80c8a37dc3fb7]
    Former-commit-id: 0fc351c5f4c03fa602f492763db0274032bbb9d6 [formerly cebbb030faf119257af3528a49e2fe290fc77b29 [formerly 03c1cc6433d977d18f73232cd725c678b5a799e9]]
    Former-commit-id: 97e98b6c72e5cdcd8fd7b1f67c8a30fd915714a1
    Former-commit-id: 894981675380e04be1366f35ba59d4ce75ad9d0c

--- matlab/register2template_SliceBySlice/sct_register_SbS.m
+++ matlab/register2template_SliceBySlice/sct_register_SbS.m
@@ -12,17 +12,18 @@ cd(tmp_folder);
 
 % register
 sct_reslice src.nii dest.nii
-sct_unix('sct_orientation -i dest.nii -s RPI -o dest.nii')
-sct_unix('sct_register_multimodal -i src_reslice.nii -d dest.nii -p step=1,algo=slicereg2d_translation,gradStep=5,iter=100,metric=MeanSquares:step=2,algo=slicereg2d_affine,gradStep=30,iter=100,metric=MeanSquares:step=3,algo=slicereg2d_translation,window_length=0,metric=MeanSquares:step=4,algo=slicereg2d_bsplinesyn,metric=MeanSquares:step=5,algo=slicereg2d_syn,metric=MeanSquares,window_length=0,gradStep=0.2,smooth=0.1')
-sct_unix('sct_concat_transfo -w step0/step00GenericAffine.mat,step0/step01Warp.nii.gz,warp_src_reslice2dest.nii.gz -d dest.nii -o warp_forward.nii')
-sct_unix('sct_concat_transfo -w warp_dest2src_reslice.nii.gz,step0/step01InverseWarp.nii.gz,-step0/step00GenericAffine.mat -d src.nii -o warp_inverse.nii');
-
-% bring back results
-sct_unix(['mv src_reslice_reg.nii ' basename '_reg.nii']);
-sct_unix(['mv warp_forward.nii ' path]);
-sct_unix(['mv warp_inverse.nii ' path]);
+sct_unix('sct_image -i dest.nii -setorient RPI -o dest.nii')
+sct_unix('sct_register_multimodal -i src_reslice.nii -d dest.nii -p step=1,algo=slicereg2d_translation,gradStep=5,iter=100,metric=MeanSquares:step=2,algo=slicereg2d_affine,gradStep=30,iter=100,metric=MeanSquares,detect_outlier=1:step=3,algo=slicereg2d_translation,window_length=0,metric=MeanSquares:step=4,algo=slicereg2d_bsplinesyn,metric=MeanSquares:step=5,algo=slicereg2d_syn,metric=MeanSquares,window_length=0,gradStep=0.2,smooth=0.1')
+sct_unix('sct_concat_transfo -d dest_RPI.nii -w step0/step00GenericAffine.mat,step0/step01Warp.nii.gz,warp_src_reslice2dest.nii.gz -o warp_forward.nii.gz')
+sct_unix('sct_concat_transfo -w warp_dest2src_reslice.nii.gz,step0/step01InverseWarp.nii.gz,-step0/step00GenericAffine.mat -d src.nii -o warp_inverse.nii.gz');
 
 cd ../
+
+% bring back results
+sct_unix(['mv ' tmp_folder filesep 'src_reslice_reg.nii ' basename '_reg.nii']);
+sct_unix(['mv ' tmp_folder filesep 'warp_forward.nii ' path]);
+sct_unix(['mv ' tmp_folder filesep 'warp_inverse.nii ' path]);
+
 rmdir(tmp_folder,'s')
 disp(['>> unix('' fslview ' basename '_reg.nii ' dest ''')'])
 sct_unix(['fslview ' basename '_reg.nii -b 0,100 ' dest ' -b 0,1 &'])
