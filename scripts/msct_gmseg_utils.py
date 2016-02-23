BUG: correct normalization when slice has very low intensity.  see issue #739.
Former-commit-id: 8a0e5c156c08c28eda826e0130c0a1498b802643 [formerly d28a2a39b00a7c2070108015e7ed54326c8dcca6] [formerly ebea06c8e5635e174a79542e4b6aae89314e84af [formerly 726db5586c77aa140e2be727523b8c68f39cf485 [formerly 0a1fdded7f9586ecd12f6d256dcf67e067a2713e]]]
Former-commit-id: 8a0e5c156c08c28eda826e0130c0a1498b802643 [formerly d28a2a39b00a7c2070108015e7ed54326c8dcca6]
Former-commit-id: 1ac3bf5678b675de96aba4a16c14ca135ef563e2 [formerly 1fb572e76bb373321dc4bfd199f958e4d673286f [formerly c01dd4d7638732359b3dc3542997c62e09d425cf]]
Former-commit-id: 786b0201ec10f0f0e2ba08e016ff5d6b294e7eaf
Former-commit-id: ccd49e8c853822b24ead07a74b6c1ec7dcc77704

scripts/msct_gmseg_utils.py
@@ -533,25 +533,36 @@ def extract_metric_from_slice_set(slices_set, seg_to_use=None, metric='Mean', gm
         if gm_percentile != 0:
             # removing outliers with a percentile
             gm_dat = sorted(gm_dat.flatten())
-            gm_dat = gm_dat[int(round(gm_percentile*len(gm_dat)/2.0)):-int(round(gm_percentile*len(gm_dat)/2.0))]
+            n_gm_outliers = int(round(gm_percentile*len(gm_dat)/2.0))
+            if n_gm_outliers != 0 and n_gm_outliers*2 < len(gm_dat):
+                gm_dat = gm_dat[n_gm_outliers:-n_gm_outliers]
 
-        if metric.lower() == 'mean':
-            gm_met = np.mean(gm_dat)
-        elif metric.lower() == 'median':
-            gm_met = np.median(gm_dat)
-        gm_std = np.std(gm_dat)
+        if gm_dat == []:
+            gm_met = 0
+            gm_std = 0
+        else:
+            if metric.lower() == 'mean':
+                gm_met = np.mean(gm_dat)
+            elif metric.lower() == 'median':
+                gm_met = np.median(gm_dat)
+            gm_std = np.std(gm_dat)
 
         # metric in WM
         if wm_percentile != 0:
             # removing outliers with a percentile
             wm_dat = sorted(wm_dat.flatten())
-            wm_dat = wm_dat[int(round(wm_percentile*len(wm_dat)/2.0)):-int(round(wm_percentile*len(wm_dat)/2.0))]
-
-        if metric.lower() == 'mean':
-            wm_met = np.mean(wm_dat)
-        elif metric.lower() == 'median':
-            wm_met = np.median(wm_dat)
-        wm_std = np.std(wm_dat)
+            n_wm_outliers = int(round(wm_percentile*len(wm_dat)/2.0))
+            if n_wm_outliers != 0 and n_wm_outliers*2 < len(wm_dat):
+                wm_dat = wm_dat[n_wm_outliers:-n_wm_outliers]
+        if wm_dat == []:
+            wm_met = 0
+            wm_std = 0
+        else:
+            if metric.lower() == 'mean':
+                wm_met = np.mean(wm_dat)
+            elif metric.lower() == 'median':
+                wm_met = np.median(wm_dat)
+            wm_std = np.std(wm_dat)
 
         slice_set_metric[slice_i.id] = (wm_met, gm_met, wm_std, gm_std)
         if save: