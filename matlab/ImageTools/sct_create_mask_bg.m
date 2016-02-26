d87872a1bebd7fddec1859dd6295fdd97bd22d63
Tanguy Duval <duvaltanguy@gmail.com>
2016-02-26 17:37:26 -0500

    create_mask_bg.. fill holes

--- matlab/ImageTools/sct_create_mask_bg.m
+++ matlab/ImageTools/sct_create_mask_bg.m
@@ -5,6 +5,6 @@ if ~exist('threshold','var'), threshold=0.3; end
 
 mask=false(size(A));
 for iz=1:size(A,3)
-    mask(:,:,iz)=A(:,:,iz)>threshold*max(max(A(:,:,iz)));
+    mask(:,:,iz)=imfill(A(:,:,iz)>threshold*max(max(A(:,:,iz))),'holes');
 end
 
