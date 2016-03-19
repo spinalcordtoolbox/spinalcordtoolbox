949357011c4ed48c16db49e8f777aa6aaeaee066
Julien Cohen-Adad <jcohen@polymtl.ca>
2016-03-18 23:10:40 -0400

    removed sct_get_centerline from testing
    Former-commit-id: 75826a23edcc7fb94f91da52aaa8ae9b4aeb065a [formerly 4d09f100f845c07ec798320468e11189bfbd13a4]
    Former-commit-id: 4aed2f36e0522b0fabcc7c270723db29ef83f876
    Former-commit-id: dceb31eee1dd9d49f6d63f75128f4eba4038964c

--- scripts/sct_testing.py
+++ scripts/sct_testing.py
@@ -148,7 +148,7 @@ def fill_functions():
     # functions.append('sct_flatten_sagittal')
     functions.append('sct_fmri_compute_tsnr')
     functions.append('sct_fmri_moco')
-    functions.append('sct_get_centerline')
+    # functions.append('sct_get_centerline')
     functions.append('sct_image')
     functions.append('sct_label_utils')
     functions.append('sct_label_vertebrae')
