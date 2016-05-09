f7e8777d1aaf403e88510aed75148c11d6064527
Pierre-Olivier Quirion <pioliqui@gmail.com>
2016-05-09 10:01:13 -0400

    install offline package is good, create package is still in beta mode
    Former-commit-id: 84f5a5a880681be3b9ad476a19e114f2ef364cf1 [formerly 456cbc64033db1b91e324eeed79773e4850fd5af]
    Former-commit-id: a42ab632517412daf7dd9001382bedbdbdf3f96c
    Former-commit-id: 1eba2e90f3c2e31f0a2eb8de450538c9b59087a7

--- install/create_package.py
+++ install/create_package.py
@@ -8,9 +8,18 @@
 
 # TODO: remove quick fix with folder_sct_temp
 
-import os
+DEBUG = True
+
+import argparse
 import getopt
+import os
+import shutil
+import subprocess
 import sys
+import tarfile
+import urllib2
+
+
 sys.path.append('../scripts')
 import sct_utils as sct
 
@@ -28,11 +37,17 @@ def usage():
         '\n'\
     # sys.exit(2)
 
-# listOS = ['osx', 'linux']
+listOS = ["linux_centos6", "linux", "osx"]
+deb_fsl = []
+tar_fsl = []
+
+
+
+
 # OSname = ''
 # Check input param
 try:
-    opts, args = getopt.getopt(sys.argv[1:],'h')
+    opts, args = getopt.getopt(sys.argv[1:], 'h')
 except getopt.GetoptError as err:
     print str(err)
     usage()
@@ -51,68 +66,107 @@ with open ("../version.txt", "r") as myfile:
     version = myfile.read().replace('\n', '')
 
 # create output folder
-folder_sct = '../sct_v'+version+'/'
-if os.path.exists(folder_sct):
-    sct.run('rm -rf '+folder_sct)
-sct.run('mkdir '+folder_sct)
+folder_sct = '../sct_v'+version
+print("output dir creation {0}".format(folder_sct))
+if os.path.exists(folder_sct) and not DEBUG:
+    shutil.rmtree(folder_sct)
+try:
+    os.mkdir(folder_sct)
+except
+    Error:
+    pass
 
-# copy folders
-sct.run('cp ../install_sct '+folder_sct)
-sct.run('cp ../README.md '+folder_sct)
-sct.run('cp ../LICENSE '+folder_sct)
-sct.run('cp ../version.txt '+folder_sct)
-sct.run('cp ../commit.txt '+folder_sct)
-sct.run('cp ../batch_processing.sh '+folder_sct)
-sct.run('cp ../batch_processing.sh '+folder_sct)
-sct.run('cp -r ../scripts '+folder_sct)
-sct.run('cp -r ../install '+folder_sct)
-sct.run('cp -r ../testing '+folder_sct)
-#sct.run('cp -r ../external '+folder_sct+'sct/')
+offline = "{0}/offline".format(folder_sct)
 
-# install
-#sct.run('mkdir '+folder_sct+'install')
-# sct.run('cp -r ../install/create_links.sh '+folder_sct + 'spinalcordtoolbox/install/')
-# sct.run('cp -r ../install/install_external.py '+folder_sct + 'spinalcordtoolbox/install/')
-#sct.run('cp -r ../install/requirements '+folder_sct+'install/')
 
-# bin
-#sct.run('mkdir '+folder_sct+'sct/bin')
-#sct.run('cp -r ../bin '+folder_sct+'sct/')
-# if OSname == 'osx':
-#     sct.run('cp -r ../bin/osx/* '+folder_sct+'spinalcordtoolbox/bin/')
-# elif OSname == 'linux':
-#     sct.run('cp -r ../bin/linux/* '+folder_sct+'spinalcordtoolbox/bin/')
 
-# data
-#sct.run('cp -rf ../data '+folder_sct+'sct/')
+def download(url, dest_dir):
 
-# testing
-#sct.run('mkdir '+folder_sct+'testing')
-#sct.run('cp ../testing/*.py '+folder_sct+'testing/')
+    # urllib.urlretrieve(url, filename=dest_dir)
+
+    if os.path.isfile(dest_dir) and DEBUG:
+        return
+
+    file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    f = open(dest_dir, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+
+    f.close()
+
+
+for OS in listOS:
+
+    download("https://dl.dropboxusercontent.com/u/20592661/sct/bin/{OS}/sct_binaries.tar.gz".format(OS=OS),
+             "{sct}/sct_binaries_{OS}.tar.gz".format(sct=offline, OS=OS))
+    download("https://dl.dropboxusercontent.com/u/20592661/sct/miniconda/{OS}/miniconda.sh".format(OS=OS),
+             "{sct}/miniconda_{OS}.sh".format(sct=offline, OS=OS))
+    download("https://dl.dropboxusercontent.com/u/20592661/sct/wheel/{OS}/wheels.tar.gz".format(OS=OS),
+             "{sct}/wheels_{OS}.tar.gz".format(sct=offline, OS=OS))
+
+download("https://dl.dropboxusercontent.com/u/20592661/sct/MNI-Poly-AMU.tar.gz",
+         "{sct}/data.tar.gz".format(sct=offline))
+download("https://pypi.python.org/packages/b7/12/71ff08d828319e80ad575762a5e0d7d07db"
+         "72a51e3d648a388ef1aa2e0ae/nibabel-2.0.1-py2-none-any.whl#md5=17c20bde23d9c7e5cbcf48df47c27d63",
+         "{sct}/nibabel-2.0.1-py2-none-any.whl".format(sct=offline))
+
+print("sct web dependent package downloaded")
+
+
+# copy following folders and file
+
+cp_list = [os.path.abspath(e.strip()) for e in ['../install_sct', '../README.md', '../LICENSE',
+                                                '../version.txt', '../commit.txt ', '../batch_processing.sh',
+                                                '../batch_processing.sh', '../scripts', '../install', '../testing']]
+for src in cp_list:
+    dest = "{0}/{1}".format(folder_sct, os.path.basename(src))
+    if os.path.isfile(dest):
+        os.remove(dest)
+    elif os.path.isdir(dest):
+        shutil.rmtree(dest)
+        # continue
+
+    if os.path.isfile(src):
+        shutil.copyfile(src, dest)
+    elif os.path.isdir(src):
+        shutil.copytree(src, dest)
 
 # remove .DS_Store files
 sct.run('find '+folder_sct+' -type f -name .DS_Store -delete')
 # remove Pycharm-related files
 sct.run('find '+folder_sct+' -type f -name *.pyc -delete')
+sct.run('find '+folder_sct+' -type f -name *.pyo -delete')
 sct.run('find '+folder_sct+' -type f -name *.idea -delete')
 
 # remove AppleDouble files - doesn't work on Linux
 # if OSname == 'osx':
 #     sct.run('find '+folder_sct+' -type d | xargs dot_clean -m')
 
-# remove Pycharm-related files
-#sct.run('rm '+folder_sct+'scripts/*.pyc')
-#if os.path.exists(folder_sct+'scripts/.idea'):
-#    sct.run('rm -rf '+folder_sct+'scripts/.idea')
 
-# go to parent directory to be able to tar without de-tarring error in mac OSX
-os.chdir('../')
+def make_tarfile(output_filename, source_dir):
+    print(" building {0} file".format(output_filename))
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-# compress folder
-folder_sct_temp = 'sct_v'+version+'/'
-sct.run('tar -cvzf sct_v'+version+'.tar.gz '+folder_sct_temp)
 
-# remove temp folder
-sct.run('rm -rf sct_v'+version)
+make_tarfile("sct_v{0}.tar.gz".format(version), folder_sct)
+
+
+# shutil.rmtree(folder_sct)
 
 print "done!\n"
