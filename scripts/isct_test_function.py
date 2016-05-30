fbf9188568849cf455078761fa549840111c47e3
Benjamin De Leener <jackbdl@gmail.com>
2016-05-30 17:13:28 -0400

    REF: added system info in log
    Former-commit-id: da74fa250717d63e835c8d0022d0df614589d9f4 [formerly b0b05ed88d9e06cb0ab7b80c6da044b88372c1ca] [formerly c84a1df83b08a8c90bca8aa530d7e4d755534407 [formerly d74418ebe454a67173aa38d6b99de0251596402c [formerly 90bbd3cb2aefb3b12ca2e49a228c1d5039cada12]]]
    Former-commit-id: da74fa250717d63e835c8d0022d0df614589d9f4 [formerly b0b05ed88d9e06cb0ab7b80c6da044b88372c1ca]
    Former-commit-id: 1c0622fd399da650d16ceb88f3cb8708073128fb [formerly b0c84bbb9da2e490db3e513aa49f677aee1aab67 [formerly e7dfcbc69d29b5419ec1160c0ac19436bae80e40]]
    Former-commit-id: 9da1b97895cd0924902b9e1d36b055832cc1560c
    Former-commit-id: f5799921d1b9699412acc4231d92b227bec045c9

--- scripts/isct_test_function.py
+++ scripts/isct_test_function.py
@@ -34,6 +34,7 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 import sys
+import platform
 import signal
 from time import time, strftime
 
@@ -253,6 +254,28 @@ if __name__ == "__main__":
         nb_cpu = arguments["-cpu-nb"]
 
     verbose = arguments["-v"]
+
+    # check OS
+    print 'Check which OS is running... '
+    platform_running = sys.platform
+    if (platform_running.find('darwin') != -1):
+        os_running = 'osx'
+    elif (platform_running.find('linux') != -1):
+        os_running = 'linux'
+    print '.. ' + os_running + ' (' + platform.platform() + ')'
+    print 'Hostname:', platform.node()
+
+    # Check number of CPU cores
+    print 'Check number of CPU cores...'
+    from multiprocessing import cpu_count
+
+    print '.. Available: ' + str(cpu_count())
+    status, output = sct.run('echo $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS', 0)
+    print '.. Used by SCT: ' + output
+
+    # check RAM
+    print 'Check RAM... '
+    sct.checkRAM(os_running, verbose=0)
 
     # start timer
     start_time = time()
