d461812a554c665618395b06c8a8cc6ccca3ce05
Sara Dupont <sara.m.dupont@gmail.com>
2016-06-14 18:11:15 -0400

    OPT: add main to avoid system calls
    Former-commit-id: 8566d3d19fee668b5307f7972335371f62f6d9f5
    Former-commit-id: 89cc43d447c4b523f7c73bae89d4c6517829d750

--- scripts/sct_resample.py
+++ scripts/sct_resample.py
@@ -39,6 +39,8 @@ class Param:
         self.file_suffix = '_resampled'  # output suffix
         self.verbose = 1
 
+# initialize parameters
+param = Param()
 
 # resample
 # ======================================================================================================================
@@ -179,13 +181,10 @@ def get_parser():
                       example=['0', '1', '2'])
     return parser
 
-# ======================================================================================================================
-# Start program
-# ======================================================================================================================
-if __name__ == "__main__":
-    # initialize parameters
-    param = Param()
-    param_debug = Param()
+def main(args=None):
+
+    if args is None:
+        args = sys.argv[1:]
 
     # Parameters for debug mode
     if param.debug:
@@ -198,7 +197,7 @@ if __name__ == "__main__":
         param.verbose = 1
     else:
         parser = get_parser()
-        arguments = parser.parse(sys.argv[1:])
+        arguments = parser.parse(args)
         param.fname_data = arguments["-i"]
         arg = 0
         if "-f" in arguments:
@@ -231,3 +230,6 @@ if __name__ == "__main__":
 
     # call main function
     resample()
+
+if __name__ == "__main__":
+    main()
