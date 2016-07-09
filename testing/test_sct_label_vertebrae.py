5935b91001e4e31ae61230907c10fc3e6a838922
Julien Cohen-Adad <jcohen@polymtl.ca>
2016-07-09 07:55:45 -0400

    OPT: label_vertebrae: fixed several issues

--- testing/test_sct_label_vertebrae.py
+++ testing/test_sct_label_vertebrae.py
@@ -30,11 +30,13 @@ def test(path_data='', parameters=''):
     diff_manual_result = float('NaN')
 
     if not parameters:
-        parameters = '-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -o t2_seg_labeled.nii.gz'
+        parameters = '-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -c t2 -o t2_seg_labeled.nii.gz'
 
     parser = sct_label_vertebrae.get_parser()
     dict_param = parser.parse(parameters.split(), check_file_exist=False)
     dict_param_with_path = parser.add_path_to_file(deepcopy(dict_param), path_data, input_file=True)
+    # update template path because the previous command wrongly adds path to testing data
+    dict_param_with_path['-t'] = dict_param['-t']
     param_with_path = parser.dictionary_to_string(dict_param_with_path)
 
     # Check if input files exist
