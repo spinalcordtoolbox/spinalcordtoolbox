5a828d31f583e83507009a87a78de26cff0701f1
Julien Cohen-Adad <jcohen@polymtl.ca>
2016-07-16 16:14:11 -0400

    BUG: updated link to binaries --> v3.0_beta16
    Former-commit-id: 073e7339469b22bcb0cd9727e04dd666cf0afeef [formerly 1acf3a1153e963939e1ed71c2cad8dae3fadc7b6] [formerly fab340cbb778ca31f21ebb9c7907d4a2dd72d499 [formerly 136ebac7aa3066f1ec26ef19013e87a162b28e5d [formerly 1a71936561feee6149b7ef87bd162bea67016860]]]
    Former-commit-id: 073e7339469b22bcb0cd9727e04dd666cf0afeef [formerly 1acf3a1153e963939e1ed71c2cad8dae3fadc7b6]
    Former-commit-id: fa9adf86d1d6088ec1b37f2ea9057c508b887ca0 [formerly 649e113bf266182ccd3bb5e30b40c7c2393f6381 [formerly bd5c5f8291a399d2fb8e1b6077f3363199ac1c21]]
    Former-commit-id: 614940ad31168d04dc832b2c321f00c07d8bc620
    Former-commit-id: 5eb5324e62d48eaab5cd5d616e3907139cb14eba

--- scripts/sct_download_data.py
+++ scripts/sct_download_data.py
@@ -52,9 +52,9 @@ def main(args=None):
     verbose = 1
     dict_url = {'sct_example_data': 'https://github.com/neuropoly/sct_example_data/archive/master.zip',
                 'sct_testing_data': 'https://github.com/neuropoly/sct_testing_data/archive/master.zip',
-                'PAM50': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta14/PAM50.zip',
-                'MNI-Poly-AMU': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta14/MNI-Poly-AMU.zip',
-                'gm_model': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta14/gm_model.zip'}
+                'PAM50': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta16/PAM50.zip',
+                'MNI-Poly-AMU': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta16/MNI-Poly-AMU.zip',
+                'gm_model': 'https://github.com/neuropoly/spinalcordtoolbox/releases/download/v3.0_beta16/gm_model.zip'}
     tmp_file = 'tmp.data.zip'
 
     # check user arguments
