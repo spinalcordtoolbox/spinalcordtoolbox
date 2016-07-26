deacbcc83893f32d83ef4c262bb768eadd66701d
Julien Cohen-Adad <jcohen@polymtl.ca>
2016-07-26 18:10:00 -0400

    OPT: resample: almost working!
    Former-commit-id: 39ad6a283e0fc80ea4f8689a86e5eac8d4a5e94b
    Former-commit-id: 296596cb1db41b3d3a2d89aa333dc3ff8790387e

--- scripts/sct_resample.py
+++ scripts/sct_resample.py
@@ -11,7 +11,9 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
+# TODO: adjust qform of output data to account for pixel size
 # TODO: test if crashes with 2d or 4d data
+# TODO: raise exception if input size is not numerical
 
 import sys
 # import os
@@ -22,7 +24,7 @@ import sct_utils as sct
 # from sct_convert import convert
 from msct_image import Image
 from msct_parser import Parser
-import dipy.align.reslice as dp_iso
+# import dipy.align.reslice as dp_iso
 
 
 # DEFAULT PARAMETERS
@@ -49,35 +51,43 @@ def resample():
     Resample data using nipy. Note: we cannot use msct_image because coordmap needs to be used.
     :return:
     """
+    import nipy
+    from nipy.algorithms.registration import resample
+    import numpy as np
 
     verbose = param.verbose
 
     # Load data
-    sct.printv('\nLoad data...')
-    from nipy import load_image
-    nii = load_image(param.fname_data)
+    sct.printv('\nLoad data...', verbose)
+    nii = nipy.load_image(param.fname_data)
     data = nii.get_data()
     # Get dimensions of data
     px, py, pz = nii.header.get_zooms()
     nx, ny, nz = nii.header.get_data_shape()
-    sct.printv('  pixdim: '+str(px)+'x'+str(py)+'x'+str(pz)+'mm')
-    sct.printv('  shape: '+str(nx)+'x'+str(ny)+'x'+str(nz)+'vox')
+    sct.printv('  pixdim: '+str(px)+' x '+str(py)+' x '+str(pz)+' mm', verbose)
+    sct.printv('  shape: '+str(nx)+' x '+str(ny)+' x '+str(nz)+' vox', verbose)
     # get_base_affine()
     affine = nii.coordmap.affine
     sct.printv('  affine matrix: \n'+str(affine))
 
     # Calculate new dimensions
-    sct.printv('\nCalculate new dimensions...', param.verbose)
+    sct.printv('\nCalculate new dimensions...', verbose)
+    # parse input argument
+    new_size = param.new_size.split('x')
     if param.new_size_type == 'vox':
-        nx_r = int(param.new_size[0])
-        ny_r = int(param.new_size[1])
-        nz_r = int(param.new_size[2])
+        nx_r = int(new_size[0])
+        ny_r = int(new_size[1])
+        nz_r = int(new_size[2])
     elif param.new_size_type == 'factor':
-        nx_r = int(round(nx * param.new_size[0]))
-        ny_r = int(round(ny * param.new_size[1]))
-        nz_r = int(round(nz * param.new_size[2]))
-
-    sct.printv('  ' + str(px_new) + ' x ' + str(py_new) + ' x ' + str(pz_new)+ ' x ' + str(pt)+'mm', param.verbose)
+        if len(new_size) == 1:
+            # isotropic resampling
+            new_size = [new_size[0], new_size[0], new_size[0]]
+        nx_r = int(round(nx * float(new_size[0])))
+        ny_r = int(round(ny * float(new_size[1])))
+        nz_r = int(round(nz * float(new_size[2])))
+    else:
+        sct.printv('\nERROR: param.new_size_type is not recognized.', 1, 'error')
+    sct.printv('  new shape: '+str(nx_r)+' x '+str(ny_r)+' x '+str(nz_r)+' vox', verbose)
     # sct.printv('\nGet dimensions of data...', param.verbose)
     # nx, ny, nz, nt, px, py, pz, pt = input_im.dim
     # sct.printv('  ' + str(px) + ' x ' + str(py) + ' x ' + str(pz)+ ' x ' + str(pt)+'mm', param.verbose)
@@ -88,57 +98,78 @@ def resample():
     #     dim = 2
     #     sct.run('ERROR (sct_resample): Dimension of input data is different from 3 or 4. Exit program', param.verbose, 'error')
 
-
-
     # create ref image
-    import numpy as np
-    # from nipy.core.api import Image, vox2mni
-    arr_r = np.zeros((60, 60, 27))
-    # nii_r = nipy.core.api.Image(arr_r, nipy.core.api.vox2mni(np.eye(4)))
-
-
-    zooms_r = np.array((0.5, 0.5, 1), dtype='f8')
-    zooms = np.array((1, 1, 1), dtype='f8')
-    R = zooms_r / zooms
-    Rx = np.eye(4)
-    Rx[:3, :3] = np.diag(R)
-
-    affine_r = np.dot(affine, Rx)
+    arr_r = np.zeros((nx_r, ny_r, nz_r))
+    R = np.eye(4)
+    R[0, 0] = nx / float(nx_r)
+    R[1, 1] = ny / float(ny_r)
+    R[2, 2] = nz / float(nz_r)
+    affine_r = np.dot(affine, R)
     coordmap_r = nii.coordmap
     coordmap_r.affine = affine_r
-
     nii_r = nipy.core.api.Image(arr_r, coordmap_r)
-    print nii_r.coordmap
 
-    transfo = np.eye(4)
-    transfo[:3, :3] = np.diag(np.array((0.5, 0.5, 1), dtype='f8'))
+    sct.printv('\nCalculate affine transformation...', verbose)
+    # create affine transformation
+    transfo = R
+    # transfo[:3, :3] = np.diag(np.array((0.5, 0.5, 1), dtype='f8'))
     # shift transformation to account for voxel size
-    transfo[:2, 3] = np.array((-0.25, -0.25), dtype='f8')
-    print transfo
+    # transfo[:3, 3] = np.array((-0.25, -0.25, -0.25), dtype='f8')
+    transfo[:3, 3] = np.array(( (R[0, 0]-1)/2, (R[1, 1]-1)/2, (R[2, 2]-1)/2 ), dtype='f8')
+    # print transfo
+    sct.printv('  transfo: \n'+str(transfo), verbose)
 
-    from nipy.algorithms.registration import resample
-    data_r = resample(nii, transform=transfo, reference=nii_r, mov_voxel_coords=False, ref_voxel_coords=False, dtype=None, interp_order=3)
+    # resample data
+    data_r = resample(nii, transform=transfo, reference=nii_r, mov_voxel_coords=True, ref_voxel_coords=True, dtype=None, interp_order=3)
+
+    if param.fname_out == '':
+        fname_out = sct.add_suffix(param.fname_data, '_r')
+
+    # save data
+    nii_r = nipy.core.api.Image(data_r, coordmap_r)
+    nipy.save_image(nii_r, fname_out)
+
+    # to view results
+    sct.printv('\nDone! To view results, type:', verbose)
+    sct.printv('fslview '+fname_out+' &', verbose, 'info')
+
+    # new_im = Image(param=new_data)
+    # new_im.absolutepath = param.fname_out
+    # new_im.path = path_out
+    # new_im.file_name = file_out
+    # new_im.ext = ext_out
+
+    # zooms_to_set = list(new_zooms)
+    # if dim == 4:
+    #     zooms_to_set.append(nt)
+
+    # new_im.hdr = input_im.hdr
+    # new_im.hdr.set_zooms(zooms_to_set)
+
+    # Set the new sform and qform:
+    # new_im.hdr.set_sform(new_affine)
+    # new_im.hdr.set_qform(new_affine)
+    #
+    # new_im.save()
 
     # extract resampling factor
-    sct.printv('\nParse resampling factor...', param.verbose)
-    new_size_split = param.new_size.split('x')
-    new_size = [float(new_size_split[i]) for i in range(len(new_size_split))]
-    # check if it has three values
-    if not len(new_size) == 3:
-        sct.printv('\nERROR: new size should have three dimensions. E.g., 2x2x1.\n', 1, 'error')
-    else:
-        ns_x, ns_y, ns_z = new_size
-
-    # Extract path/file/extension
-    path_data, file_data, ext_data = sct.extract_fname(param.fname_data)
-    path_out, file_out, ext_out = '', file_data, ext_data
-    if param.fname_out != '':
-        path_out, file_out, ext_out = sct.extract_fname(param.fname_out)
-    else:
-        file_out += param.file_suffix
-    param.fname_out = path_out+file_out+ext_out
-
-    input_im = Image(param.fname_data)
+    # sct.printv('\nParse resampling factor...', param.verbose)
+    # new_size_split = param.new_size.split('x')
+    # new_size = [float(new_size_split[i]) for i in range(len(new_size_split))]
+    # # check if it has three values
+    # if not len(new_size) == 3:
+    #     sct.printv('\nERROR: new size should have three dimensions. E.g., 2x2x1.\n', 1, 'error')
+    # else:
+    #     ns_x, ns_y, ns_z = new_size
+    #
+    # # Extract path/file/extension
+    # path_data, file_data, ext_data = sct.extract_fname(param.fname_data)
+    # path_out, file_out, ext_out = '', file_data, ext_data
+    # else:
+    #     file_out += param.file_suffix
+    # param.fname_out = path_out+file_out+ext_out
+    #
+    # input_im = Image(param.fname_data)
 
 
     # import numpy as np
@@ -160,19 +191,19 @@ def resample():
     # # translate before interpolation to account for voxel size
     # from skimage import transform
     # transform.resize()
-
-
-    zooms = (px, py, pz)  # input_im.hdr.get_zooms()[:3]
-    affine = input_im.hdr.get_qform()  # get_base_affine()
-    new_zooms = (px_new, py_new, pz_new)
-
-    if type(param.interpolation) == int:
-        order = param.interpolation
-    elif type(param.interpolation) == str and param.interpolation in param.x_to_order.keys():
-        order = param.x_to_order[param.interpolation]
-    else:
-        order = 1
-        sct.printv('WARNING: wrong input for the interpolation. Using default value = linear', param.verbose, 'warning')
+    #
+    #
+    # zooms = (px, py, pz)  # input_im.hdr.get_zooms()[:3]
+    # affine = input_im.hdr.get_qform()  # get_base_affine()
+    # new_zooms = (px_new, py_new, pz_new)
+    #
+    # if type(param.interpolation) == int:
+    #     order = param.interpolation
+    # elif type(param.interpolation) == str and param.interpolation in param.x_to_order.keys():
+    #     order = param.x_to_order[param.interpolation]
+    # else:
+    #     order = 1
+    #     sct.printv('WARNING: wrong input for the interpolation. Using default value = linear', param.verbose, 'warning')
 
     import numpy as np
     # affine = np.array([[-2., 0., 0., 29.25],
@@ -184,30 +215,9 @@ def resample():
     #                    [0., 0., 2., -31.25],
     #                    [0., 0., 0., 1.]])
 
-    new_data, new_affine = dp_iso.reslice(input_im.data, affine, zooms, new_zooms, mode=param.mode, order=order)
+    # new_data, new_affine = dp_iso.reslice(input_im.data, affine, zooms, new_zooms, mode=param.mode, order=order)
 
-    new_im = Image(param=new_data)
-    new_im.absolutepath = param.fname_out
-    new_im.path = path_out
-    new_im.file_name = file_out
-    new_im.ext = ext_out
 
-    # zooms_to_set = list(new_zooms)
-    # if dim == 4:
-    #     zooms_to_set.append(nt)
-
-    new_im.hdr = input_im.hdr
-    # new_im.hdr.set_zooms(zooms_to_set)
-
-    # Set the new sform and qform:
-    new_im.hdr.set_sform(new_affine)
-    new_im.hdr.set_qform(new_affine)
-
-    new_im.save()
-
-    # to view results
-    sct.printv('\nDone! To view results, type:', param.verbose)
-    sct.printv('fslview '+param.fname_out+' &', param.verbose, 'info')
 
 
 def get_parser():
