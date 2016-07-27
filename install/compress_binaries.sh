6e12c393751da1bd6f4c8a50b62fcc61f8051a6a
Julien Cohen-Adad <jcohen@polymtl.ca>
2016-07-27 09:32:21 -0400

    INST: updated link to CENTOS_6 binaries
    Former-commit-id: fa3525743177f747b12026fa7e8af68072a6dd5d [formerly 6760a9074245bdbd434370d4de20b8fb2afb28b2] [formerly 5b5dec81926228d487762da3a868d4be79ddfa58 [formerly b8da2657ebf47756fe78417465f173b44c2ea973 [formerly a07bb882b19b43eb13d6fd0bc03cc8b53b420513]]]
    Former-commit-id: fa3525743177f747b12026fa7e8af68072a6dd5d [formerly 6760a9074245bdbd434370d4de20b8fb2afb28b2]
    Former-commit-id: 89bf0325af87b08e02a84810851266725625f39d [formerly 949abbab9e4608234c4e4e9a8b45025d07285743 [formerly a05221d2f5741659a4351e902dfc7f6a5f399bcf]]
    Former-commit-id: 3dad793e02feac5ff74a1d54ba08848074d9e02f
    Former-commit-id: 676d5cf1df1833c13ae8887c8c328b16d811e2c8

--- install/compress_binaries.sh
+++ install/compress_binaries.sh
@@ -2,7 +2,7 @@
 # to uncompress, type:
 tar -zxvf sct_binaries.tar.gz
 # make sure everything is executable
-chmod 775 *
+chmod 755 *
 # compress data
 tar -czf sct_binaries.tar.gz *
 # change permission
