fdfc3d919d64c492a99875ae423fc0c08861e238
Sara Dupont <sara.m.dupont@gmail.com>
2016-08-26 16:52:40 -0400

    WIP: co-register data and apply transfo to other slice attributes
    Former-commit-id: 62a7145f34b8cfcfa1df2bf1030eea740ea735ce [formerly 39b6e45a660994a50d0a2f9aa105421edde29fad] [formerly 0524d96d020e937f34da86afbd256763c02c77ff [formerly 158dbd19aa038d4703e8982d04e0076fd058fd31 [formerly 89ebd31f8c62ae31934a3b071210b25d67b1f526]]]
    Former-commit-id: 62a7145f34b8cfcfa1df2bf1030eea740ea735ce [formerly 39b6e45a660994a50d0a2f9aa105421edde29fad]
    Former-commit-id: c33f479badd75bab5cb6041243d0c11f62ed4dff [formerly 304e8c355e64a075af2de7ccfad0067d7232daeb [formerly 0472e12abbd4067c69adf1fb960a290065b7faed]]
    Former-commit-id: 79f527dd61d936902aeff5e3d83a81b3c8408050
    Former-commit-id: fe2ed536dbaab0ea2ba2cf69e77d8a5d13180791

--- scripts/msct_multiatlas_seg_NEW.py
+++ scripts/msct_multiatlas_seg_NEW.py
@@ -12,9 +12,11 @@
 # About the license: see the file LICENSE.TXT
 ########################################################################################################################
 import os
+import shutil
 import numpy as np
 from sct_utils import printv, slash_at_the_end
-from msct_gmseg_utils_NEW import pre_processing
+from msct_gmseg_utils_NEW import pre_processing, register_data, apply_transfo
+from msct_image import Image
 
 class ModelParam:
     def __init__(self):
@@ -22,11 +24,27 @@ class ModelParam:
         self.todo = 'load'# 'compute' or 'load'
         self.new_model_dir = 'gmseg/'
 
+    def __repr__(self):
+        info = 'Model Param:\n'
+        info += '\t- path to data: '+ self.path_data+'\n'
+        info += '\t- created folder: '+self.new_model_dir+'\n'
+
+        return info
+
 class DataParam:
     def __init__(self):
         self.denoising = True
         self.axial_res = 0.3
-        self.register_param = ''
+        self.register_param = 'step=1,type=seg,algo=columnwise,metric=MeanSquares,smooth=5,iter=1:step=2,type=im,algo=syn,smooth=2,metric=MI,iter=4:step=3,iter=0'
+
+    def __repr__(self):
+        info = 'Data Param:\n'
+        info += '\t- denoising: ' + str(self.denoising)+'\n'
+        info += '\t- resampling to an axial resolution of: ' + str(self.axial_res)+'mm\n'
+        info += '\t- registration parameters: '+self.register_param+'\n'
+
+        return info
+
 class Param:
     def __init__(self):
         self.verbose = 1
@@ -42,19 +60,22 @@ class ModelDictionary:
 
     def compute_model(self):
         printv('\nComputing the model dictionary ...', self.param.verbose, 'normal')
-        os.mkdir(self.param.new_model_dir)
+        os.mkdir(self.model_param.new_model_dir)
         param_fic = open(self.model_param.new_model_dir + 'info.txt', 'w')
-        param_fic.write(str(self.param))
+        param_fic.write(str(self.model_param))
+        param_fic.write(str(self.data_param))
         param_fic.close()
 
         printv('\n\tLoading data dictionary ...', self.param.verbose, 'normal')
         self.load_data()
         self.mean_image = np.mean([dic_slice.im for dic_slice in self.slices], axis=0)
 
+
         printv('\n\tCo-register all the data into a common groupwise space (using the white matter segmentations) ...', self.param.verbose, 'normal')
         self.coregister_data()
 
         ## TODO: COMPLETE COMPUTE _DIC
+        # - NORMALIZE INTENSITY
 
     def load_data(self):
         '''
@@ -95,7 +116,80 @@ class ModelDictionary:
                 j += len(list_slices_sub)
 
     def coregister_data(self):
-        pass
+        # compute mean WM image
+        list_wm = []
+        for dic_slice in self.slices:
+            for wm in dic_slice.wm_seg:
+                list_wm.append(wm)
+
+        data_mean_wm = np.mean(list_wm, axis=0)
+        im_mean_wm = Image(param=data_mean_wm)
+
+        # register all slices WM on mean WM
+        for dic_slice in self.slices:
+            # create a directory to get the warping fields
+            warp_dir = 'wf_slice'+str(dic_slice.id)
+            os.mkdir(warp_dir)
+
+            # get slice mean WM image
+            data_slice_wm = np.mean(dic_slice.wm_seg, axis=0)
+            im_slice_wm = Image(data_slice_wm)
+            # register slice WM on mean WM
+            im_slice_wm_reg = register_data(im_src=im_slice_wm, im_dest=im_mean_wm, param_reg=self.data_param.register_param, path_warp=warp_dir)
+
+            # use forward warping field to register all slice wm
+            list_wmseg_reg = []
+            for wm_seg in dic_slice.wm_seg:
+                im_wmseg = Image(param=wm_seg)
+                im_wmseg_reg = apply_transfo(im_src=im_wmseg, im_dest=im_mean_wm, warp=warp_dir + '/warp_src2dest.nii.gz', interp='nn')
+                list_wmseg_reg.append(im_wmseg_reg.data)
+
+            # use forward warping field to register gm seg
+            list_gmseg_reg = []
+            for gm_seg in dic_slice.gm_seg:
+                im_gmseg = Image(param=gm_seg)
+                im_gmseg_reg = apply_transfo(im_src=im_gmseg, im_dest=im_mean_wm, warp=warp_dir+'/warp_src2dest.nii.gz', interp='nn')
+                list_gmseg_reg.append(im_gmseg_reg.data)
+
+            # use forward warping field to register im
+            im_slice = Image(dic_slice.im)
+            im_slice_reg = apply_transfo(im_src=im_slice, im_dest=im_mean_wm, warp=warp_dir + '/warp_src2dest.nii.gz')
+
+            # set slice attributes with data registered into the model space
+            dic_slice.set(im_m=im_slice_reg.data)
+            dic_slice.set(wm_seg_m=list_wmseg_reg)
+            dic_slice.set(gm_seg_m=list_gmseg_reg)
+
+            # remove warping fields directory
+            shutil.rmtree(warp_dir)
+
+
+    '''
+    ## REGISTRATION WITH IMAGES FIRST, THEN APPLY TO GM/WM SEG
+    def coregister_data(self):
+        im_mean_image = Image(param=self.mean_image)
+        for dic_slice in self.slices:
+            # create a directory to get the warping fields
+            warp_dir = 'warping_fields_slice'+str(dic_slice.id)
+            os.mkdir(warp_dir)
+            # get dic image
+            im_dic_slice = Image(param=dic_slice.im)
+            # register image
+            im_reg = register_data(im_src=im_dic_slice, im_dest=im_mean_image, param_reg=self.data_param.register_param, path_warp=warp_dir)
+            # use warping fields to register gm seg
+            list_gmseg_reg = []
+            for gm_seg in dic_slice.gm_seg:
+                im_gmseg = Image(param=gm_seg)
+                im_gmseg_reg = apply_transfo(im_src=im_gmseg, im_dest=im_mean_image, warp=warp_dir+'/warp_src2dest.nii.gz')
+                list_gmseg_reg.append(im_gmseg_reg.data)
+            # set slie attributes with data registered into the model space
+            dic_slice.set(im_m=im_reg.data)
+            dic_slice.set(gm_seg_m=list_gmseg_reg)
+            # remove warping fields directory
+            shutil.rmtree(warp_dir)
+    '''
+
+
 
 
 
