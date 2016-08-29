f6daf2ae3020a5ae7d4cc53180291f6d72dbb5b7
Sara Dupont <sara.m.dupont@gmail.com>
2016-08-29 17:47:41 -0400

    WIP: in compute_model() - normalize data intensity. - Model computation with sklearn pca or isomap
    Former-commit-id: d49a78cd88ead9f9c4ce06b2aed0a72d06064a0a [formerly dfdd60cec49d8eaea84ab8fd207c570ca495cbef]
    Former-commit-id: 4a1116eaaaa0656dda9ea7b6e8d87a240a56ae30
    Former-commit-id: 5819e8c477c2b3c3463b9c5f852e84bf11244ed8

--- scripts/msct_multiatlas_seg_NEW.py
+++ scripts/msct_multiatlas_seg_NEW.py
@@ -14,8 +14,11 @@
 import os
 import shutil
 import numpy as np
+import pandas as pd
+import pickle, gzip
+from sklearn import manifold, decomposition
 from sct_utils import printv, slash_at_the_end
-from msct_gmseg_utils_NEW import pre_processing, register_data, apply_transfo
+from msct_gmseg_utils_NEW import pre_processing, register_data, apply_transfo, normalize_slice
 from msct_image import Image
 
 class ModelParam:
@@ -23,11 +26,21 @@ class ModelParam:
         self.path_data = ''
         self.todo = 'load'# 'compute' or 'load'
         self.new_model_dir = 'gmseg/'
+        self.method = 'pca' # 'pca' or 'isomap'
+        self.k_pca = 0.8
+        self.n_compo_iso = 'half' #'half' or int indicating the actual number of components to keep
+        self.n_neighbors_iso = 5
 
     def __repr__(self):
         info = 'Model Param:\n'
         info += '\t- path to data: '+ self.path_data+'\n'
         info += '\t- created folder: '+self.new_model_dir+'\n'
+        info += '\t- used method: '+self.method+'\n'
+        if self.method == 'pca':
+            info += '\t\t-> % of variability kept for PCA: '+str(self.k_pca)+'\n'
+        if self.method == 'isomap':
+            info += '\t\t-> # components for isomap: '+str(self.n_compo_iso)+'\n'
+            info += '\t\t-> # neighbors for isomap: ' + str(self.n_neighbors_iso) + '\n'
 
         return info
 
@@ -36,12 +49,15 @@ class DataParam:
         self.denoising = True
         self.axial_res = 0.3
         self.register_param = 'step=1,type=seg,algo=columnwise,metric=MeanSquares,smooth=5,iter=1:step=2,type=im,algo=syn,smooth=2,metric=MI,iter=4:step=3,iter=0'
+        self.normalization = True
 
     def __repr__(self):
         info = 'Data Param:\n'
         info += '\t- denoising: ' + str(self.denoising)+'\n'
         info += '\t- resampling to an axial resolution of: ' + str(self.axial_res)+'mm\n'
         info += '\t- registration parameters: '+self.register_param+'\n'
+        info += '\t- intensity normalization: ' + str(self.normalization)+'\n'
+
 
         return info
 
@@ -49,14 +65,18 @@ class Param:
     def __init__(self):
         self.verbose = 1
 
-class ModelDictionary:
+class Model:
     def __init__(self, model_param=None, data_param=None, param=None):
         self.model_param = model_param if model_param is not None else ModelParam()
         self.data_param = data_param if data_param is not None else DataParam()
         self.param = param if param is not None else Param()
 
-        self.slices = []
+        self.slices = [] # list of Slice() : Model dictionary
         self.mean_image = None
+        self.intensities = None
+
+        self.fitted_model = None # PCA or Isomap model
+        self.fitted_data = None
 
     def compute_model(self):
         printv('\nComputing the model dictionary ...', self.param.verbose, 'normal')
@@ -67,17 +87,38 @@ class ModelDictionary:
         param_fic.close()
 
         printv('\n\tLoading data dictionary ...', self.param.verbose, 'normal')
-        self.load_data()
+        self.load_model_data()
         self.mean_image = np.mean([dic_slice.im for dic_slice in self.slices], axis=0)
 
-
         printv('\n\tCo-register all the data into a common groupwise space (using the white matter segmentations) ...', self.param.verbose, 'normal')
-        self.coregister_data()
+        self.coregister_model_data()
 
-        ## TODO: COMPLETE COMPUTE _DIC
-        # - NORMALIZE INTENSITY
+        printv('\n\tNormalize data intensities against averaged median values in the dictionary ...', self.param.verbose, 'normal')
+        self.normalize_model_data()
 
-    def load_data(self):
+        printv('\nComputing the model reduced space ...', self.param.verbose, 'normal')
+        self.compute_reduced_space()
+
+        printv('\nSaving model elements ...', self.param.verbose, 'normal')
+        self.save_model()
+        ### TODO: add compute_beta / compute tau ??
+
+
+    def average_wm(self, model_space=True):
+        # compute mean WM image
+        list_wm = []
+        for dic_slice in self.slices:
+            if model_space:
+                for wm in dic_slice.wm_seg_M:
+                    list_wm.append(wm)
+            else:
+                for wm in dic_slice.wm_seg:
+                    list_wm.append(wm)
+
+        data_mean_wm = np.mean(list_wm, axis=0)
+        return data_mean_wm
+
+    def load_model_data(self):
         '''
         Data should be organized with one folder per subject containing:
             - A WM/GM contrasted image containing 'im' in its name
@@ -115,14 +156,9 @@ class ModelDictionary:
 
                 j += len(list_slices_sub)
 
-    def coregister_data(self):
+    def coregister_model_data(self):
         # compute mean WM image
-        list_wm = []
-        for dic_slice in self.slices:
-            for wm in dic_slice.wm_seg:
-                list_wm.append(wm)
-
-        data_mean_wm = np.mean(list_wm, axis=0)
+        data_mean_wm = self.average_wm(model_space=False)
         im_mean_wm = Image(param=data_mean_wm)
 
         # register all slices WM on mean WM
@@ -135,25 +171,25 @@ class ModelDictionary:
             data_slice_wm = np.mean(dic_slice.wm_seg, axis=0)
             im_slice_wm = Image(data_slice_wm)
             # register slice WM on mean WM
-            im_slice_wm_reg = register_data(im_src=im_slice_wm, im_dest=im_mean_wm, param_reg=self.data_param.register_param, path_warp=warp_dir)
+            im_slice_wm_reg, fname_src2dest, fname_dest2src = register_data(im_src=im_slice_wm, im_dest=im_mean_wm, param_reg=self.data_param.register_param, path_copy_warp=warp_dir)
 
             # use forward warping field to register all slice wm
             list_wmseg_reg = []
             for wm_seg in dic_slice.wm_seg:
                 im_wmseg = Image(param=wm_seg)
-                im_wmseg_reg = apply_transfo(im_src=im_wmseg, im_dest=im_mean_wm, warp=warp_dir + '/warp_src2dest.nii.gz', interp='nn')
+                im_wmseg_reg = apply_transfo(im_src=im_wmseg, im_dest=im_mean_wm, warp=warp_dir+'/'+fname_src2dest, interp='nn')
                 list_wmseg_reg.append(im_wmseg_reg.data)
 
             # use forward warping field to register gm seg
             list_gmseg_reg = []
             for gm_seg in dic_slice.gm_seg:
                 im_gmseg = Image(param=gm_seg)
-                im_gmseg_reg = apply_transfo(im_src=im_gmseg, im_dest=im_mean_wm, warp=warp_dir+'/warp_src2dest.nii.gz', interp='nn')
+                im_gmseg_reg = apply_transfo(im_src=im_gmseg, im_dest=im_mean_wm, warp=warp_dir+'/'+fname_src2dest, interp='nn')
                 list_gmseg_reg.append(im_gmseg_reg.data)
 
             # use forward warping field to register im
             im_slice = Image(dic_slice.im)
-            im_slice_reg = apply_transfo(im_src=im_slice, im_dest=im_mean_wm, warp=warp_dir + '/warp_src2dest.nii.gz')
+            im_slice_reg = apply_transfo(im_src=im_slice, im_dest=im_mean_wm, warp=warp_dir+'/'+fname_src2dest)
 
             # set slice attributes with data registered into the model space
             dic_slice.set(im_m=im_slice_reg.data)
@@ -162,6 +198,102 @@ class ModelDictionary:
 
             # remove warping fields directory
             shutil.rmtree(warp_dir)
+
+
+    def normalize_model_data(self):
+        # get the id of the slices by vertebral level
+        id_by_level = {}
+        for dic_slice in self.slices:
+            level_int = int(round(dic_slice.level))
+            if level_int not in id_by_level.keys():
+                id_by_level[level_int] = [dic_slice.id]
+            else:
+                id_by_level[level_int].append(dic_slice.id)
+
+        # get the average median values by level:
+        list_gm_by_level = []
+        list_wm_by_level = []
+        list_min_by_level = []
+        list_max_by_level = []
+        list_indexes = []
+
+        for level, list_id_slices in id_by_level.items():
+            list_med_gm = []
+            list_med_wm = []
+            list_min = []
+            list_max = []
+            # get median GM and WM values for all slices of the same level:
+            for id_slice in list_id_slices:
+                slice = self.slices[id_slice]
+                for gm in slice.gm_seg_M:
+                    med_gm = np.median(slice.im_M[gm==1])
+                    list_med_gm.append(med_gm)
+                for wm in slice.wm_seg_M:
+                    med_wm = np.median(slice.im_M[wm == 1])
+                    list_med_wm.append(med_wm)
+
+                list_min.append(min(slice.im_M.flatten()))
+                list_max.append(max(slice.im_M.flatten()))
+
+            list_gm_by_level.append(np.mean(list_med_gm))
+            list_wm_by_level.append(np.mean(list_med_wm))
+            list_min_by_level.append(min(list_min))
+            list_max_by_level.append(max(list_max))
+            list_indexes.append(level)
+
+        # save average median values in a Panda data frame
+        data_intensities = {'GM': pd.Series(list_gm_by_level, index=list_indexes), 'WM': pd.Series(list_wm_by_level, index=list_indexes), 'MIN': pd.Series(list_min_by_level, index=list_indexes), 'MAX': pd.Series(list_max_by_level, index=list_indexes)}
+        self.intensities = pd.DataFrame(data_intensities)
+
+        # Normalize slices using dic values
+        for dic_slice in self.slices:
+            level_int = int(round(dic_slice.level))
+            norm_im_M = normalize_slice(dic_slice.im_M, dic_slice.gm_seg_M, dic_slice.wm_seg_M, self.intensities['GM'][level_int], self.intensities['WM'][level_int], min=self.intensities['MIN'][level_int], max=self.intensities['MAX'][level_int])
+            dic_slice.set(im_m=norm_im_M)
+
+
+    def compute_reduced_space(self):
+        model = None
+        model_data =  np.asarray([dic_slice.im_M.flatten() for dic_slice in self.slices])
+
+        if self.model_param.method == 'pca':
+            ## PCA
+            model = decomposition.PCA(n_components=self.model_param.k_pca)
+            self.fitted_data = model.fit_transform(model_data)
+
+        if self.model_param.method == 'isomap':
+            ## ISOMAP
+            n_neighbors = self.model_param.n_neighbors_iso
+            if self.model_param.n_compo_iso == 'half':
+                n_components = model_data.shape[0] / 2
+            else:
+                n_components = self.model_param.n_compo_iso
+
+            model = manifold.Isomap(n_neighbors=n_neighbors, n_components=n_components)
+            self.fitted_data = model.fit_transform(model_data)
+
+        # save model after bing fitted to data
+        self.fitted_model = model
+
+    def save_model(self):
+        os.chdir(self.model_param.new_model_dir)
+        ## to save:
+        ##   - self.slices = dictionary
+        pickle.dump(self.slices, gzip.open('slices.pklz', 'wb'), protocol=2)
+
+        ##   - self.intensities = for normalization
+        pickle.dump(self.intensities, gzip.open('intensities.pklz', 'wb'), protocol=2)
+
+        ##   - reduced space (pca or isomap)
+        pickle.dump(self.fitted_model, gzip.open('fitted_model.pklz', 'wb'), protocol=2)
+
+        ##   - fitted data (=eigen vectors or embedding vectors )
+        pickle.dump(self.fitted_data, gzip.open('fitted_data.pklz', 'wb'), protocol=2)
+
+        ##   - tau value --> still needed ?
+
+        os.chdir('..')
+
 
 
     '''
