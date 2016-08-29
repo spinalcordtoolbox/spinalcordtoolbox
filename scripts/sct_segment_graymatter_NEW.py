48a64acb749806c20e12db552d7f800f1aca20ab
Sara Dupont <sara.m.dupont@gmail.com>
2016-08-29 17:58:41 -0400

    WIP: useful code
    Former-commit-id: 1bdaa00663c5e997501f81b98f06cdb465a6ce46 [formerly 634016368f80a384b18e15481b02e00139fb57bc] [formerly b40d2c4e48e3870d4ab8a939e1060c40581a0d20 [formerly 52e18c82704cf7ba2df34a9f1f236bf51ff11961 [formerly 355a672cb4604007358cb5a8512e7caf1523c836]]]
    Former-commit-id: 1bdaa00663c5e997501f81b98f06cdb465a6ce46 [formerly 634016368f80a384b18e15481b02e00139fb57bc]
    Former-commit-id: 56438977f01bae6a529b29d360202ea7a23cd1e4 [formerly 4ee1dc39bddcd1cd4eff15616eebbdae473640ad [formerly 109875bc8a25e72f511abc2a891af6b7ee918ee4]]
    Former-commit-id: 0af2e331df4e4b9fdc6a22f31c3d2855fefdfda2
    Former-commit-id: 8de6a7e665785f3509b096f8da5eda008d9acedb

--- scripts/sct_segment_graymatter_NEW.py
+++ scripts/sct_segment_graymatter_NEW.py
@@ -12,3 +12,29 @@
 # About the license: see the file LICENSE.TXT
 ########################################################################################################################
 
+## TODO: use chunks of this code to register image to segment to model space
+
+'''
+## REGISTRATION WITH IMAGES FIRST, THEN APPLY TO GM/WM SEG
+def coregister_data(self):
+    im_mean_image = Image(param=self.mean_image)
+    for dic_slice in self.slices:
+        # create a directory to get the warping fields
+        warp_dir = 'warping_fields_slice'+str(dic_slice.id)
+        os.mkdir(warp_dir)
+        # get dic image
+        im_dic_slice = Image(param=dic_slice.im)
+        # register image
+        im_reg = register_data(im_src=im_dic_slice, im_dest=im_mean_image, param_reg=self.data_param.register_param, path_warp=warp_dir)
+        # use warping fields to register gm seg
+        list_gmseg_reg = []
+        for gm_seg in dic_slice.gm_seg:
+            im_gmseg = Image(param=gm_seg)
+            im_gmseg_reg = apply_transfo(im_src=im_gmseg, im_dest=im_mean_image, warp=warp_dir+'/warp_src2dest.nii.gz')
+            list_gmseg_reg.append(im_gmseg_reg.data)
+        # set slie attributes with data registered into the model space
+        dic_slice.set(im_m=im_reg.data)
+        dic_slice.set(gm_seg_m=list_gmseg_reg)
+        # remove warping fields directory
+        shutil.rmtree(warp_dir)
+'''
