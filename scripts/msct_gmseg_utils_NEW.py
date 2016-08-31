8e356cfcbbc0215fa9fc31b7a51f8be1f3e3aae8
Sara Dupont <sara.m.dupont@gmail.com>
2016-08-31 16:52:08 -0400

    NEW: working segmentation
    Former-commit-id: 21d34503269f845622214d7e00ee906be699f0b0 [formerly 400496b0340e7f4b44296e04fdd156b4063b7f37] [formerly 437b7c7b24684d810a3959472081df2ee1ad6c21 [formerly 046a048e0339d1138f1ba3a370023ff4e535657b [formerly 18ede90b6e8424c8f90ff9aa94b94e99ed82eb1e]]]
    Former-commit-id: 21d34503269f845622214d7e00ee906be699f0b0 [formerly 400496b0340e7f4b44296e04fdd156b4063b7f37]
    Former-commit-id: d2265d31df290dec627e89becd041e942f25aa56 [formerly bb0797cad740f007315b7cc5119b58f39c275b7b [formerly c398d8191928c16fbcf3a91d8c7eb77ee4a29ec1]]
    Former-commit-id: 1e2a0a7b44b331c684e6d9a1192f18a881a233a1
    Former-commit-id: d0168f7b8ea91961438e25f87c799ee8ef415cc2

--- scripts/msct_gmseg_utils_NEW.py
+++ scripts/msct_gmseg_utils_NEW.py
@@ -108,7 +108,7 @@ def pre_processing(fname_target, fname_sc_seg, fname_level=None, fname_manual_gm
     shutil.copy(fname_sc_seg, tmp_dir)
     os.chdir(tmp_dir)
 
-    original_info = {'orientation': None, 'im_target_rpi': None, 'interpolated_images': []}
+    original_info = {'orientation': None, 'im_sc_seg_rpi': None, 'interpolated_images': []}
 
     im_target = Image(fname_target)
     im_sc_seg = Image(fname_sc_seg)
