1ffe3e433858d4300c9bd28b1b2e2bd96c5aa3d8
Sara Dupont <sara.m.dupont@gmail.com>
2016-10-03 10:25:57 -0400

    OPT: use correct function name for isct_test_function
    Former-commit-id: 1ca723410f6747325a2b5151fecb702567d6e22e
    Former-commit-id: 6ed241298c92f8afb1211a48b4cb427e7cc52611

--- testing/test_sct_register_multimodal.py
+++ testing/test_sct_register_multimodal.py
@@ -165,7 +165,7 @@ def test(path_data='', parameters=''):
         else:
             subject_folder = subject_folder[-1]
         path_output = sct.slash_at_the_end(
-            'sct_register_to_template_' + subject_folder + '_' + time.strftime("%y%m%d%H%M%S") + '_' + str(
+            'sct_register_multimodal_' + subject_folder + '_' + time.strftime("%y%m%d%H%M%S") + '_' + str(
                 random.randint(1, 1000000)), slash=1)
         param_with_path += ' -ofolder ' + path_output
 
