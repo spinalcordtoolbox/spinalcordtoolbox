9ad319d3bc3870f7bc865ba30decfcca7b5f8183
Sara Dupont <sara.m.dupont@gmail.com>
2016-12-09 11:38:32 -0500

    OPT: return dimensions as a list

--- scripts/sct_crop_image.py
+++ scripts/sct_crop_image.py
@@ -389,7 +389,7 @@ def find_mask_boundaries(fname_mask):
     # ind_start = ','.join(str(i) for i in xyzmin)
     # ind_end = ','.join(str(i) for i in xyzmax)
     # return values
-    return ind_start, ind_end, dim
+    return ind_start, ind_end, range(dim)
 
 
 if __name__ == "__main__":
