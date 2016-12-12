da117cadc1838d9687e5b28687cd522a261589b0
George Peristerakis <peristerakis@gmail.com>
2016-12-12 17:58:51 -0500

    Removed useless comments

--- testing/test_sct_dmri_get_bvalue.py
+++ testing/test_sct_dmri_get_bvalue.py
@@ -16,12 +16,7 @@ import commands
 
 
 def test(data_path):
-
-    # define command
     cmd = 'sct_dmri_get_bvalue -g 0.04 -b 0.04 -d 0.03'
-
-    # return
-    #return sct.run(cmd, 0)
     return commands.getstatusoutput(cmd)
 
 
