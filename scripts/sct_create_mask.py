602dc5bfe260d488c5ecbad1be0f1c7f51ac7778
George Peristerakis <peristerakis@gmail.com>
2016-12-15 11:30:48 -0500

    Corrected errors introduced by the refactoring.
    Former-commit-id: 7a0a8df90113b7e64e7c0402c6ea7083ec62cab7 [formerly 0265261516a6a2a48d0422f57e50efe10096c3fa [formerly b5e0a45ea864ce078e286b3755c2a73ed5bae10a]]
    Former-commit-id: 8cef2aace647c393a31f9d87a9dcdd7b51697dc7
    Former-commit-id: 4f539f6ad19c35cfbedc790f68623c7c7d69c215

--- scripts/sct_create_mask.py
+++ scripts/sct_create_mask.py
@@ -153,7 +153,7 @@ def create_mask(param):
     if method_type == 'point':
         # extract coordinate of point
         sct.printv('\nExtract coordinate of point...', param.verbose)
-        status, output = sct_label_utils.main(['-i', 'point_RPI.nii.gz', '-display'])
+        status, output = sct.run('sct_label_utils -i point_RPI.nii.gz -display', param.verbose)
         coord = output[output.find('Position=') + 10:-17].split(',')
 
     if method_type == 'center':
