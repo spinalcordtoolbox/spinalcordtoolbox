BUG: label_vertebrae: fixed MI computation π
Former-commit-id: c0862f9fb393f574d001cee3c01c311aefa17e4c [formerly b2e66cff24fb1df486d442dc7ecd3f349d01f322 [formerly 2512a7b167a7658610d70dff49d40809d1a557fd]]
Former-commit-id: e6d85d57abe13e5bac70ed1fd7be8050375531d3
Former-commit-id: c959cf0dcc6d7d37c5aed3d24a7bd687c67dc966

scripts/sct_label_vertebrae.py
@@ -763,7 +763,7 @@ def compute_corr_3d(src=[], target=[], x=0, xshift=0, xsize=0, y=0, yshift=0, ys
             #I_corr[ind_I] = np.corrcoef(data_chunk1d, pattern1d)[0, 1]
             # data_chunk2d = np.mean(data_chunk3d, 1)
             # pattern2d = np.mean(pattern, 1)
-            I_corr[ind_I] = mutual_information(data_chunk1d, pattern1d, nbins=16, normalized=True)
+            I_corr[ind_I] = mutual_information(data_chunk1d, pattern1d, nbins=16, normalized=False)
         else:
             allzeros = 1
             # sct.printv('.. WARNING: iz='+str(iz)+': Data only contains zero. Set correlation to 0.', verbose)