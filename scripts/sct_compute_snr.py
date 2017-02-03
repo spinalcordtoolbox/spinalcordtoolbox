BUG: compute_snr: fixed computation of diff method

scripts/sct_compute_snr.py
@@ -27,7 +27,7 @@ def get_parser():
 
     # Initialize the parser
     parser = Parser(__file__)
-    parser.usage.set_description('Compute SNR in a given ROI according to different methods presented in Dietrich et al., Measurement of signal-to-noise ratios in MR images: Influence of multichannel coils, parallel imaging, and reconstruction filters (2007).')
+    parser.usage.set_description('Compute SNR in a given ROI using methods described in [Dietrich et al., Measurement of signal-to-noise ratios in MR images: Influence of multichannel coils, parallel imaging, and reconstruction filters. J Magn Reson Imaging 2007; 26(2): 375-385].')
     parser.add_option(name="-i",
                       type_value='image_nifti',
                       description="Input images to compute the SNR on. Must be concatenated in time. Typically, 2 or 3 b0s concatenated in time (depending on the method used).",
@@ -40,7 +40,9 @@ def get_parser():
                       example='dwi_moco_mean_seg.nii.gz')
     parser.add_option(name="-method",
                       type_value='multiple_choice',
-                      description='Method to use to compute the SNR:\n- diff: Use the two first volumes to estimate noise variance.\n- mult: Use all volumes to estimate noise variance.',
+                      description='Method to use to compute the SNR:\n'
+                      '- diff: Substract two volumes (defined by -vol) to estimate noise variance.'
+                      '\n- mult: Use all volumes to estimate noise variance.',
                       mandatory=False,
                       default_value='diff',
                       example=['diff', 'mult', 'background', 'nema'])
@@ -151,7 +153,7 @@ def main():
         data_1 = input_data[:, :, :, index_vol[0]]
         data_2 = input_data[:, :, :, index_vol[1]]
         signal = np.mean(np.add(data_1[indexes_roi], data_2[indexes_roi]))
-        noise = np.sqrt(2)*np.std(np.subtract(data_1[indexes_roi], data_2[indexes_roi]))
+        noise = np.std(np.subtract(data_1[indexes_roi], data_2[indexes_roi]))
     elif method == 'background':
         sct.printv('ERROR: Sorry, method is not implemented yet.', 1, 'error')
     elif method == 'nema':