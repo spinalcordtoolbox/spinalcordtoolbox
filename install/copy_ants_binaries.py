da3a108b4f3cb13aa9d9c5c24b1794deb0dd3ad2
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-02-19 10:13:01 -0500

    INST: updated copy_ants_binaries

--- install/copy_ants_binaries.py
+++ install/copy_ants_binaries.py
@@ -3,38 +3,36 @@
 # Copy ANTs binaries into SCT folder. 
 #
 # Author: Julien Cohen-Adad
-# modified: 2014-11-01
 
 # TODO: remove quick fix with folder_sct_temp
 
 import os
 import getopt
 import sys
-import commands
-sys.path.append('../scripts')
+import shutil
 import sct_utils as sct
 
 
 # usage
 #=======================================================================================================================
 def usage():
-    print """
+    print("""
 """+os.path.basename(__file__)+"""
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
 
 DESCRIPTION
-  Copy ANTs binaries into SCT folder.
+  Copy ANTs binaries for packaging. Will output binaries in local folder.
 
 USAGE
   """+os.path.basename(__file__)+""" -f <folder_ants> -s <os_name>
 
 MANDATORY ARGUMENTS
-  -f <folder_ants>      ANTs folder. 
+  -f <folder_ants>      antsbin folder. 
   -s {osx,linux}        name of the OS.
 
 EXAMPLE
-  """+os.path.basename(__file__)+""" -f ~/antsbin/bin -s osx\n"""
+  """+os.path.basename(__file__)+""" -f ~/antsbin/bin -s osx\n""")
 
     sys.exit(2)
 
@@ -43,19 +41,16 @@ EXAMPLE
 #=======================================================================================================================
 
 folder_bin = 'bin'
-prefix_sct = 'sct_'
+prefix_sct = 'isct_'
 listOS = ['osx', 'linux']
-list_file_ants = ['antsApplyTransforms', 'antsRegistration', 'antsSliceRegularizedRegistration', 'ANTSUseLandmarkImagesToGetAffineTransform', 'ANTSUseLandmarkImagesToGetBSplineDisplacementField', 'ComposeMultiTransform', 'ImageMath', 'ThresholdImage']
+list_file_ants = ['antsApplyTransforms', 'antsRegistration', 'antsSliceRegularizedRegistration', 'ComposeMultiTransform']
 OSname = ''
-
-# get path of SCT
-status, path_sct = commands.getstatusoutput('echo $SCT_DIR')
 
 # Check input param
 try:
     opts, args = getopt.getopt(sys.argv[1:],'hf:s:')
 except getopt.GetoptError as err:
-    print str(err)
+    print(str(err))
     usage()
 if not opts:
     usage()
@@ -65,7 +60,7 @@ for opt, arg in opts:
     elif opt in ('-s'):
         OSname = str(arg)
     elif opt in ('-f'):
-        folder_ants = sct.slash_at_the_end(str(arg), 1)  # add slash at the end
+        folder_ants = sct.slash_at_the_end(str(arg), 1)+'bin/'  # add slash at the end
 
 # check if OS exists
 if OSname not in listOS:
@@ -76,7 +71,7 @@ if not os.path.exists(folder_ants):
     sct.printv('\nERROR: Path '+folder_ants+' does not exist.\n', 1, 'error')
 
 # build destination path name
-path_bin = sct.slash_at_the_end(path_sct, 1)+folder_bin+'/'+OSname+'/'
+os.makedirs(OSname)
 
 # loop across ANTs files
 for file_ants in list_file_ants:
@@ -84,7 +79,8 @@ for file_ants in list_file_ants:
     if not os.path.exists(folder_ants+file_ants):
         sct.printv('\nERROR: File '+folder_ants+file_ants+' does not exist.\n', 1, 'error')
     # copy data
-    sct.run('cp '+folder_ants+file_ants+' '+path_bin+prefix_sct+file_ants, 1)
+    shutil.copyfile(folder_ants+file_ants, OSname+'/'+prefix_sct+file_ants)
+#    os.cosct.run('cp '+folder_ants+file_ants+' '+path_bin+prefix_sct+file_ants, 1)
 
-print "done!\n"
+print("done!\n")
 
