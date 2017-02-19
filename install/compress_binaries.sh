fcb98345eeb06f14a901cc0cacf5af27604da268
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-02-19 12:10:00 -0500

    INST: minor updates

--- install/compress_binaries.sh
+++ install/compress_binaries.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# compress SCT binaries
+# run this script inside folder that contains all binaries. E.g.:
+# ~/code/sct/install/compress_binaries.sh
+# once tar.gz is generated, rename as: "DATE"_sct_binaries_"OS".tar.gz
 # to uncompress, type:
-tar -zxvf sct_binaries.tar.gz
+#tar -zxvf sct_binaries.tar.gz
+
+# remove Apple crap
+rm .DS_Store
 # make sure everything is executable
 chmod 755 *
-# compress data: To be run within a folder that contains all binaries (e.g. tmpbin/)
+# compress data
 tar -czf sct_binaries.tar.gz *
 # change permission
 chmod 775 sct_binaries.tar.gz
-# remove binaries
-rm isct*
