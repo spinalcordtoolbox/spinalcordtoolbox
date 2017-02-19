ab0939e3c9e906f15a45d0c78cf0d3ae0f3db209
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-02-19 11:45:15 -0500

    INST: removed useless flag

--- install/copy_ants_binaries.py
+++ install/copy_ants_binaries.py
@@ -20,17 +20,16 @@ def usage():
 Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>
 
 DESCRIPTION
-  Copy ANTs binaries for packaging. Will output binaries in local folder.
+  Copy ANTs binaries for packaging. Will output binaries in the local folder antsbin/
 
 USAGE
   """+os.path.basename(__file__)+""" -f <folder_ants> -s <os_name>
 
 MANDATORY ARGUMENTS
-  -f <folder_ants>      antsbin folder. 
-  -s {osx,linux}        name of the OS.
+  -f <folder_ants>      antsbin folder (do not include slash at the end)
 
 EXAMPLE
-  """+os.path.basename(__file__)+""" -f ~/antsbin/bin -s osx\n""")
+  """+os.path.basename(__file__)+""" -f ~/antsbin/bin\n""")
 
     sys.exit(2)
 
@@ -42,11 +41,12 @@ folder_bin = 'bin'
 prefix_sct = 'isct_'
 listOS = ['osx', 'linux']
 list_file_ants = ['antsApplyTransforms', 'antsRegistration', 'antsSliceRegularizedRegistration', 'ComposeMultiTransform']
-OSname = ''
+output_folder = 'antsbin/'
+
 
 # Check input param
 try:
-    opts, args = getopt.getopt(sys.argv[1:],'hf:s:')
+    opts, args = getopt.getopt(sys.argv[1:],'hf:')
 except getopt.GetoptError as err:
     print(str(err))
     usage()
@@ -55,30 +55,24 @@ if not opts:
 for opt, arg in opts:
     if opt == '-h':
         usage()
-    elif opt in ('-s'):
-        OSname = str(arg)
     elif opt in ('-f'):
-        folder_ants = sct.slash_at_the_end(str(arg), 1)+'bin/'  # add slash at the end
-
-# check if OS exists
-if OSname not in listOS:
-    sct.printv('\nERROR: OS name should be one of the following: '+'[%s]' % ', '.join(map(str, listOS))+'\n', 1, 'error')
+        folder_ants = str(arg)+'/bin/'
 
 # check if ANTs folder exists
 if not os.path.exists(folder_ants):
-    sct.printv('\nERROR: Path '+folder_ants+' does not exist.\n', 1, 'error')
+    print('\nERROR: Path '+folder_ants+' does not exist.\n')
 
 # build destination path name
-os.makedirs(OSname)
+os.makedirs(output_folder)
 
 # loop across ANTs files
 for file_ants in list_file_ants:
     # check if file exists
     if not os.path.exists(folder_ants+file_ants):
-        sct.printv('\nERROR: File '+folder_ants+file_ants+' does not exist.\n', 1, 'error')
+        print('\nERROR: File '+folder_ants+file_ants+' does not exist.\n')
     # copy data
-    shutil.copyfile(folder_ants+file_ants, OSname+'/'+prefix_sct+file_ants)
+    print
+    shutil.copyfile(folder_ants+file_ants, output_folder+prefix_sct+file_ants)
 #    os.cosct.run('cp '+folder_ants+file_ants+' '+path_bin+prefix_sct+file_ants, 1)
 
 print("done!\n")
-
