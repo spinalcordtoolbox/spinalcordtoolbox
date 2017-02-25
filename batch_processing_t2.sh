90f891894c187cabb2ddcc30bfdd37ee1135e88d
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-02-25 18:18:54 -0500

    DOC: added verbose 2 on labeling
    Former-commit-id: 4cd459916f531e1a34875622bc145195e80eb001 [formerly 21139b0777ba85034cf19bf91b08e60d2246b1d2 [formerly a951ee5e92753cd5591d2144ca955a37be613835]]
    Former-commit-id: 3e85127f25e3d49302c0dab610d7388b748516be
    Former-commit-id: 24cbe729f3232bb4bbae9fd675d8696f7a6f708e

--- batch_processing_t2.sh
+++ batch_processing_t2.sh
@@ -43,7 +43,7 @@ if [ $DISPLAY = true ]; then
   fslview t2 -b 0,1000 t2_seg -l Red -t 0.5 &
 fi
 # Vertebral labeling
-sct_label_vertebrae -i t2.nii.gz -s t2_seg.nii.gz -c t2
+sct_label_vertebrae -i t2.nii.gz -s t2_seg.nii.gz -c t2 -v 2
 # Create labels at C3 and C7 vertebral levels
 sct_label_utils -i t2_seg_labeled.nii.gz -vert-body 3,7
 # Register to template
