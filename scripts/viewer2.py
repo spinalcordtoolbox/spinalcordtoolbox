25d80f9c628983c863500db2c6e849e1c269566b
apopov <alexandros.popov@ecl14.ec-lyon.Fr>
2017-04-06 17:49:32 -0400

    Created header_layout.

--- scripts/viewer2.py
+++ scripts/viewer2.py
@@ -1,10 +1,24 @@
 import sys
 import PyQt4.QtGui as QtGui
 
-a=QtGui.QApplication(sys.argv)
-w=QtGui.QWidget()
-w.resize(320,240)
-w.setWindowTitle('Hello World')
-w.show()
+def launch_main_window():
+    system = QtGui.QApplication(sys.argv)
+    w = QtGui.QWidget()
+    w.resize(740, 850)
+    w.setWindowTitle('Hello World')
+    w.show()
+    return (w,system)
 
-sys.exit(a.exec_())
+def create_header(w):
+    header_layout=QtGui.QVBoxLayout()
+    lb_status=QtGui.QLabel('Hi there!')
+    header_layout.addWidget(lb_status)
+    
+    w.setLayout(header_layout)
+
+
+
+
+(w,system)=launch_main_window()
+create_header(w)
+sys.exit(system.exec_())
