c66eeabc13da532270f0951cc862e1c6c809d5bd
apopov <alexandros.popov@ecl14.ec-lyon.Fr>
2017-04-07 14:21:42 -0400

    Created class HeaderCore & Header(HeaderCore).

--- scripts/viewer2.py
+++ scripts/viewer2.py
@@ -3,31 +3,58 @@ import PyQt4.QtGui as QtGui
 import PyQt4.QtCore as QtCore
 
 
+
+class HeaderCore(object):
+
+    def __init__(self):
+        self.define_lb_status()
+        self.define_lb_warning()
+        self.define_layout_header()
+
+    def define_lb_status(self):
+        self.lb_status = QtGui.QLabel('Label Alerte')
+        self.lb_status.setContentsMargins(10, 10, 10, 0)
+        self.lb_status.setAlignment(QtCore.Qt.AlignCenter)
+
+    def define_lb_warning(self):
+        self.lb_warning = QtGui.QLabel('Label Warning')
+        self.lb_warning.setContentsMargins(10, 10, 10, 10)
+        self.lb_warning.setAlignment(QtCore.Qt.AlignCenter)
+
+    def define_layout_header(self):
+        self.layout_header = QtGui.QVBoxLayout()
+        self.layout_header.setAlignment(QtCore.Qt.AlignTop)
+        self.layout_header.addWidget(self.lb_status)
+        self.layout_header.addWidget(self.lb_warning)
+
+class Header(HeaderCore):
+
+    def update_lb(self,key):
+        if(key=='start'):
+            self.lb_status.setText('header.lb_status')
+            self.lb_warning.setText('header.lb_warning')
+            self.lb_warning.setStyleSheet("color:red")
+
+
+
 def launch_main_window():
     system = QtGui.QApplication(sys.argv)
     w = QtGui.QWidget()
     w.resize(740, 850)
-    w.setWindowTitle('Hello World')
+    w.setWindowTitle('Hello world')
     w.show()
     return (w,system)
 
-def create_header(w):
-    layout_header=QtGui.QVBoxLayout()
-    layout_header.setAlignment(QtCore.Qt.AlignTop)
-    lb_status=QtGui.QLabel('Label Alerte')
-    lb_status.setContentsMargins(10, 10, 10, 0)
-    lb_status.setAlignment(QtCore.Qt.AlignCenter)
-
-    lb_warning=QtGui.QLabel('Label Warning')
-    lb_warning.setContentsMargins(10,10,10,10)
-    lb_warning.setAlignment(QtCore.Qt.AlignCenter)
-    layout_header.addWidget(lb_status)
-    layout_header.addWidget(lb_warning)
-    w.setLayout(layout_header)
-    return(lb_status,lb_warning)
+def add_header(w):
+    header=Header()
+    w.setLayout(header.layout_header)
+    header.update_lb('start')
+    return(header)
 
 
 
-(w,system)=launch_main_window()
-(lb_status,lb_warning)=create_header(w)
+(w,system) = launch_main_window()
+header = add_header(w)
 sys.exit(system.exec_())
+
+
