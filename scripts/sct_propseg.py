5fc8a5281ad3e37986f050dc671e5f49da1ab199
apopov <alexandros.popov@ecl14.ec-lyon.Fr>
2017-04-10 17:10:38 -0400

    Changed sct_propseg to call Window.

--- scripts/sct_propseg.py
+++ scripts/sct_propseg.py
@@ -409,10 +409,10 @@ if __name__ == "__main__":
         cmd_image = 'sct_image -i "%s" -o "%s" -setorient SAL -v 0' % (fname_data, os.path.join(path_tmp_viewer, reoriented_image_filename))
         sct.run(cmd_image, verbose=False)
 
-        from sct_viewer import ClickViewer
+        #from sct_viewer import ClickViewer
+        from viewer2 import  Window
         image_input_reoriented = Image(path_tmp_viewer + reoriented_image_filename)
-        viewer = ClickViewer(image_input_reoriented)
-        viewer.help_url = 'https://sourceforge.net/p/spinalcordtoolbox/wiki/correction_PropSeg/attachment/propseg_viewer.png'
+        viewer = Window()
         if use_viewer == "mask":
             viewer.input_type = 'mask'
             viewer.number_of_slices = 3
