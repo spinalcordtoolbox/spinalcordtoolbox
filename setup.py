0fc09f7ad2ec3043ce09a158144935e22cd943d3
George <peristeri@users.noreply.github.com>
2017-04-18 09:41:18 -0400

    [FEATURE] Introduce the QC html viewer (#1250)
    It is a command that run a local HTTP server from the root of the
    QC data.
    
    Implements #1248
    
    Former-commit-id: 3834e924ed52ce6a24b9b313f0d5dabb5af45da9 [formerly b12693953d252cdbee9ad4f5a0b0868ac8a991ff [formerly 50bad3cf79c8154dc7dfc33f6a0108ab6db85dcc]]
    Former-commit-id: ffb572cdd0f0ed9de1aed376e359c6b962221cd3
    Former-commit-id: da42a4b76aa3e5ef4d823dfee1230772ad717867

--- setup.py
+++ setup.py
@@ -44,5 +44,5 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    # entry_points={'console_scripts': console_scripts, },
+    entry_points={'console_scripts': ['sct=spinalcordtoolbox.qc.serve:spinalcordtoolbox'], },
 )
