2ac2f92c653fa861204e79190fdd7ebe0023e619
Alexandros Popov <alexandros.popov@ecl14.ec-lyon.fr>
2017-04-27 12:21:48 -0400

    Improved draw_dots.

--- scripts/viewer2.py
+++ scripts/viewer2.py
@@ -495,7 +495,7 @@ class SinglePlotMainLabelVertebrae(SinglePlot):
 
         x_data, y_data = [], []
         for ipoints in self.list_points:
-            if ipoints.z == select_right_position_dim(self.current_position,self.view):
+            if select_right_position_dim(ipoints,self.view) == select_right_position_dim(self.current_position,self.view):
                 x,y=select_right_dimensions(ipoints,self.view)
                 x_data.append(x)
                 y_data.append(y)
