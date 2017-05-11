7fdd81c98b4d0fa8299bdcd311953fd5eb3b0e3c
Alexandros Popov <alexandros.popov@ecl14.ec-lyon.fr>
2017-05-11 17:52:57 -0400

    Working on open_png_txt.py.

--- scripts/open_png_txt.py
+++ scripts/open_png_txt.py
@@ -1,13 +1,8 @@
-
-
-
-#TODO: prevoir si ce nest pas T2 au debut
-
-
 class SliceData(object):
-    def __init__(self,image,labels):
+    def __init__(self,image,labels,num_slice):
         self.image=image
         self.labels=labels
+        self.num_slice=num_slice
 
 
 def get_txt_num_slice(s):
@@ -29,17 +24,25 @@ def find_image_at_slice(list_png,num_slice):
         if get_png_num_slice(png_file)==num_slice:
             return png_file
 
-
-
+def extract_txt_info(path,txt_file):
+    txt_file = open (path+txt_file,'r')
+    output=''
+    for line in txt_file:
+        output+=line
+    return output
 
 
 import os
-directory_path='/home/apopov/Bureau/t2_ground_truth'
+from scipy import misc
+directory_path='/home/apopov/Bureau/t2_ground_truth/'
 list_txt=list(filter(lambda x: '.txt' in x, os.listdir(directory_path)))
 list_png=list(filter(lambda x: '.png' in x, os.listdir(directory_path)))
 
+list_SliceData=[]
 for txt_file in list_txt:
     slice_num=get_txt_num_slice(txt_file)
     png_file=find_image_at_slice(list_png,slice_num)
-    print(txt_file,png_file)
+    list_SliceData.append(SliceData(misc.imread(directory_path+png_file),extract_txt_info(directory_path,txt_file),slice_num))
+
+print(list_SliceData)
 
