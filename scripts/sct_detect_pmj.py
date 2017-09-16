f6ab930b8aded5c569cdb39aa26e32cdc3721d6c
charleygros <chgroc@evans.neuro.polymtl.ca>
2017-09-16 11:25:20 -0400

    Documentation and Usage
    Former-commit-id: 2df8df73f03afb35c3a82e955e3e6f7e1b6f24c7

--- scripts/sct_detect_pmj.py
+++ scripts/sct_detect_pmj.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-# Detect Ponto-Medullary Junction
-#
-# Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Author: Charley
-# Created: 2017-07-21
-#
-# About the license: see the file LICENSE.TXT
+"""Detect Ponto-Medullary Junction.
+
+Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
+Author: Charley
+Created: 2017-07-21
+Modified: 2017-09-12
+
+About the license: see the file LICENSE.TXT
+"""
 
 import os
 import shutil
@@ -18,16 +20,17 @@ import commands
 
 from msct_image import Image
 from msct_parser import Parser
-from sct_utils import tmp_create, extract_fname, slash_at_the_end, add_suffix, printv, run
+from sct_utils import tmp_create, extract_fname, slash_at_the_end, printv, run
 from sct_image import get_orientation, set_orientation
+
 
 def get_parser():
     # Initialize the parser
     parser = Parser(__file__)
     parser.usage.set_description('Detection of the Ponto-Medullary Junction (PMJ).\n'
                                  ' This method is machine-learning based and adapted for T1w-like or T2w-like images.\n'
-                                 ' If the PMJ is detected from the input image, a nifti mask with one voxel, with the value 50,\n'
-                                 ' located at the predicted PMJ level, is output ("*_pmj.nii.gz").\n'
+                                 ' If the PMJ is detected from the input image, a nifti mask is output ("*_pmj.nii.gz")\n'
+                                 ' with one voxel (value=50) located at the predicted PMJ position.\n'
                                  ' If the PMJ is not detected, nothing is output.')
     parser.add_option(name="-i",
                         type_value="file",
@@ -41,7 +44,7 @@ def get_parser():
                         example=["t1", "t2"])
     parser.add_option(name="-s",
                         type_value="file",
-                        description="SC segmentation or SC centerline mask. Provide this mask could help the detection of the PMJ",
+                        description="SC segmentation or centerline mask. Provide this mask helps the detection of the PMJ by indicating the position of the SC in the Right-to-Left direction.",
                         mandatory=False,
                         example="t2_seg.nii.gz")
     parser.add_option(name="-ofolder",
@@ -70,6 +73,7 @@ def get_parser():
 
     return parser
 
+
 class DetectPMJ:
     def __init__(self, fname_im, contrast, fname_seg, path_out, quality_control, verbose):
 
@@ -86,17 +90,17 @@ class DetectPMJ:
 
         self.tmp_dir = tmp_create(verbose=self.verbose)  # path to tmp directory
 
-        self.orientation_im = get_orientation(Image(self.fname_im)) # to re-orient the data at the end
+        self.orientation_im = get_orientation(Image(self.fname_im))  # to re-orient the data at the end
 
         self.slice2D_im = extract_fname(self.fname_im)[1] + '_midSag.nii'  # file used to do the detection, with only one slice
-        self.dection_map_pmj = extract_fname(self.fname_im)[1] + '_map_pmj' # file resulting from the detection
+        self.dection_map_pmj = extract_fname(self.fname_im)[1] + '_map_pmj'  # file resulting from the detection
 
         # path to the pmj detector
         self.pmj_model = os.path.join((commands.getoutput('$SCT_DIR')).split(': ')[1],
                                             'data/pmj_models',
                                             '{}_model'.format(self.contrast))
 
-        self.threshold = -0.75 if self.contrast=='t1' else 0.8 # detection map threshold, depends on the contrast
+        self.threshold = -0.75 if self.contrast == 't1' else 0.8  # detection map threshold, depends on the contrast
 
         self.fname_out = extract_fname(self.fname_im)[1] + '_pmj.nii.gz'
 
@@ -104,164 +108,165 @@ class DetectPMJ:
 
     def apply(self):
 
-        self.ifolder2tmp() # move data to the temp dir
+        self.ifolder2tmp()  # move data to the temp dir
 
-        self.orient2pir() # orient data to PIR orientation
+        self.orient2pir()  # orient data to PIR orientation
 
-        self.extract_sagital_slice() # extract a sagital slice, used to do the detection
+        self.extract_sagital_slice()  # extract a sagital slice, used to do the detection
 
-        self.detect() # run the detection
+        self.detect()  # run the detection
 
-        self.get_max_position() # get the max of the detection map
+        self.get_max_position()  # get the max of the detection map
 
-        self.generate_mask_pmj() # generate the mask with one voxel with value = 50 at the predicted PMJ position
+        self.generate_mask_pmj()  # generate the mask with one voxel (value = 50) at the predicted PMJ position
 
-        self.get_pmj_coords() # return the coordinates of the predicted PMJ in the image space, in the original orientation
-
-        fname_out2return = self.tmp2ofolder() # save results to ofolder
+        fname_out2return = self.tmp2ofolder()  # save results to ofolder
 
         return fname_out2return, self.tmp_dir
 
     def tmp2ofolder(self):
-
+        """Copy output files to the ofolder."""
         os.chdir('..')  # go back to original directory
 
-        if self.pa_coord != -1: # If PMJ has been detected
+        if self.pa_coord != -1:  # If PMJ has been detected
             printv('\nSave resulting file...', self.verbose, 'normal')
-            shutil.copy(os.path.abspath(os.path.join(self.tmp_dir, self.fname_qc)),
-                            os.path.abspath(os.path.join(self.path_out, self.fname_qc)))
+            shutil.copy(os.path.abspath(os.path.join(self.tmp_dir, self.fname_out)),
+                        os.path.abspath(os.path.join(self.path_out, self.fname_out)))
 
             if self.quality_control:
-                shutil.copy(os.path.abspath(os.path.join(self.tmp_dir, self.fname_out)),
-                            os.path.abspath(os.path.join(self.path_out, self.fname_out)))                
+                shutil.copy(os.path.abspath(os.path.join(self.tmp_dir, self.fname_qc)),
+                                os.path.abspath(os.path.join(self.path_out, self.fname_qc)))
 
             return os.path.join(self.path_out, self.fname_out)
-        
-        else:
-            return None
-
-    def get_pmj_coords(self):
-
-        if self.pa_coord != -1: # If PMJ has been detected
-            pmj_mask = Image(self.fname_out)
-
-            printv('\tx_pmj = '+str(np.where(pmj_mask.data==50)[0][0]), self.verbose, 'normal')
-            printv('\ty_pmj = '+str(np.where(pmj_mask.data==50)[1][0]), self.verbose, 'normal')
-            printv('\tz_pmj = '+str(np.where(pmj_mask.data==50)[2][0]), self.verbose, 'normal')
-
-            return [np.where(pmj_mask.data==50)[0][0], np.where(pmj_mask.data==50)[1][0], np.where(pmj_mask.data==50)[2][0]]
-        
         else:
             return None
 
     def save_qc(self, slice_arr, coord_lst):
-
-        printv('\nSave quality control images...', self.verbose, 'normal')
+        """Output a QC PNG image with a green bounding box around the detected PMJ."""
+        printv('\nSave quality control image...', self.verbose, 'normal')
 
         import matplotlib.pyplot as plt
         import matplotlib.patches as patches
-        import matplotlib.image as mpimg
 
-        fig,ax = plt.subplots(1)
+        fig, ax = plt.subplots(1)
         ax.imshow(np.rot90(slice_arr), cmap='gray')
 
-        rect = patches.Rectangle((coord_lst[0]-10,slice_arr.shape[1]-coord_lst[1]-10),20,20,linewidth=2,edgecolor='lime',facecolor='none')
+        rect = patches.Rectangle((coord_lst[0] - 10, slice_arr.shape[1] - coord_lst[1] - 10),
+                                    20, 20,
+                                    linewidth=2,
+                                    edgecolor='lime',
+                                    facecolor='none')
         ax.add_patch(rect)
 
         plt.axis('off')
         fig.savefig(self.fname_qc, bbox_inches='tight')
 
-        printv('\nQC output image: '+self.fname_qc, self.verbose, 'info')
+        printv('\nQC output image: ' + self.fname_qc, self.verbose, 'info')
 
     def generate_mask_pmj(self):
-
-        if self.pa_coord != -1: # If PMJ has been detected
-            im = Image(''.join(extract_fname(self.fname_im)[1:])) # image in PIR orientation
+        """Output the PMJ mask."""
+        if self.pa_coord != -1:  # If PMJ has been detected
+            im = Image(''.join(extract_fname(self.fname_im)[1:]))  # image in PIR orientation
             im_mask = im.copy()
-            im_mask.data *= 0 # empty mask
+            im_mask.data *= 0  # empty mask
 
-            im_mask.data[self.pa_coord, self.is_coord, self.rl_coord] = 50 # voxel with value = 50
+            im_mask.data[self.pa_coord, self.is_coord, self.rl_coord] = 50  # voxel with value = 50
 
-            if self.quality_control: # output QC image
+            if self.quality_control:  # output QC image
                 self.save_qc(im.data[:, :, self.rl_coord], [self.pa_coord, self.is_coord])
 
             im_mask.setFileName(self.fname_out)
 
-            im_mask = set_orientation(im_mask, self.orientation_im, fname_out = self.fname_out) # reorient data
+            im_mask = set_orientation(im_mask, self.orientation_im, fname_out=self.fname_out)  # reorient data
+
+            x_pmj, y_pmj, z_pmj = np.where(im_mask.data == 50)
+            printv('\tx_pmj = ' + str(x_pmj[0]), self.verbose, 'info')
+            printv('\ty_pmj = ' + str(y_pmj[0]), self.verbose, 'info')
+            printv('\tz_pmj = ' + str(z_pmj[0]), self.verbose, 'info')
 
             im_mask.save()
 
     def get_max_position(self):
-
+        """Find the position of the PMJ by thresholding the probabilistic map."""
         img_pred = Image(self.dection_map_pmj)
-        
-        if True in np.unique(img_pred.data > self.threshold): # threshold the detection map
-            img_pred_maxValue = np.max(img_pred.data) # get the max of the detection map
-            self.pa_coord, self.is_coord = np.where(img_pred.data==img_pred_maxValue)[0][0], np.where(img_pred.data==img_pred_maxValue)[1][0]
-            
+
+        if True in np.unique(img_pred.data > self.threshold):  # threshold the detection map
+            img_pred_maxValue = np.max(img_pred.data)  # get the max value of the detection map
+            self.pa_coord = np.where(img_pred.data == img_pred_maxValue)[0][0]
+            self.is_coord = np.where(img_pred.data == img_pred_maxValue)[1][0]
+
             printv('\nPonto-Medullary Junction detected', self.verbose, 'normal')
 
         else:
             self.pa_coord, self.is_coord = -1, -1
-            
+
             printv('\nPonto-Medullary Junction not detected', self.verbose, 'normal')
 
         del img_pred
 
     def detect(self):
-
+        """Run the classifier on self.slice2D_im."""
         printv('\nRun PMJ detector', self.verbose, 'normal')
         os.environ["FSLOUTPUTTYPE"] = "NIFTI_PAIR"
         cmd_pmj = 'isct_spine_detect "%s" "%s" "%s"' % \
                     (self.pmj_model, self.slice2D_im.split('.nii')[0], self.dection_map_pmj)
         run(cmd_pmj, verbose=0)
 
-        img = nib.load(self.dection_map_pmj+'_svm.hdr') # convert .img and .hdr files to .nii
-        nib.save(img, self.dection_map_pmj+'.nii')
+        img = nib.load(self.dection_map_pmj + '_svm.hdr')  # convert .img and .hdr files to .nii
+        nib.save(img, self.dection_map_pmj + '.nii')
 
-        self.dection_map_pmj += '.nii' # fname of the resulting detection map
+        self.dection_map_pmj += '.nii'  # fname of the resulting detection map
 
     def extract_sagital_slice(self):
+        """Extract the sagital slice where the detection is done.
 
-        if self.fname_seg is not None: # if the segmentation is provided, the 2D sagital slice is choosen accoding to the segmentation
+        If the segmentation is provided,
+            the 2D sagital slice is choosen accoding to the segmentation.
+
+        If the segmentation is not provided,
+            the 2D sagital slice is choosen as the mid-sagital slice of the input image.
+        """
+
+        if self.fname_seg is not None:
             img_seg = Image(self.fname_seg)
 
-            z_mid_slice = img_seg.data[:,int(img_seg.dim[1]/2),:]            
-            if 1 in z_mid_slice: # if SC segmentation available at this slice
-                self.rl_coord = int(center_of_mass(z_mid_slice)[1]) # Right_left coordinate
+            z_mid_slice = img_seg.data[:, int(img_seg.dim[1] / 2), :]
+            if 1 in z_mid_slice:  # if SC segmentation available at this slice
+                self.rl_coord = int(center_of_mass(z_mid_slice)[1])  # Right_left coordinate
             else:
-                self.rl_coord = int(img_seg.dim[2]/2)
+                self.rl_coord = int(img_seg.dim[2] / 2)
             del img_seg
 
-        else: # if the segmentation is not provided, the 2D sagital slice is choosen as the mid-sagital slice of the input image
+        else:
             img = Image(self.fname_im)
-            self.rl_coord = int(img.dim[2]/2)  # Right_left coordinate
+            self.rl_coord = int(img.dim[2] / 2)  # Right_left coordinate
             del img
 
-        run('sct_crop_image -i '+self.fname_im+' -start '+str(self.rl_coord)+' -end '+str(self.rl_coord)+' -dim 2 -o '+self.slice2D_im)
+        run('sct_crop_image -i ' + self.fname_im + ' -start ' + str(self.rl_coord) + ' -end ' + str(self.rl_coord) + ' -dim 2 -o ' + self.slice2D_im)
 
     def orient2pir(self):
-        
-        if self.orientation_im != 'PIR': # open image and re-orient it to PIR if needed
+        """Orient input data to PIR orientation."""
+        if self.orientation_im != 'PIR':  # open image and re-orient it to PIR if needed
             im_tmp = Image(self.fname_im)
-            set_orientation(im_tmp, 'PIR', fname_out = ''.join(extract_fname(self.fname_im)[1:]))
+            set_orientation(im_tmp, 'PIR', fname_out=''.join(extract_fname(self.fname_im)[1:]))
 
             if self.fname_seg is not None:
-                set_orientation(Image(self.fname_seg), 'PIR', fname_out = ''.join(extract_fname(self.fname_seg)[1:]))
+                set_orientation(Image(self.fname_seg), 'PIR', fname_out=''.join(extract_fname(self.fname_seg)[1:]))
 
     def ifolder2tmp(self):
-        
-        if self.fname_im is not None: # copy input image
+        """Copy data to tmp folder."""
+        if self.fname_im is not None:  # copy input image
             shutil.copy(self.fname_im, self.tmp_dir)
             self.fname_im = ''.join(extract_fname(self.fname_im)[1:])
         else:
             printv('ERROR: No input image', self.verbose, 'error')
 
-        if self.fname_seg is not None: # copy segmentation image
+        if self.fname_seg is not None:  # copy segmentation image
             shutil.copy(self.fname_seg, self.tmp_dir)
             self.fname_seg = ''.join(extract_fname(self.fname_seg)[1:])
 
         os.chdir(self.tmp_dir)  # go to tmp directory
+
 
 def main(args=None):
     if args is None:
@@ -312,7 +317,13 @@ def main(args=None):
         verbose = '1'
 
     # Initialize DetectPMJ
-    detector = DetectPMJ(fname_im=fname_im, contrast=contrast, fname_seg=fname_seg, path_out=path_results, quality_control=qc, verbose=verbose)
+    detector = DetectPMJ(fname_im=fname_im,
+                            contrast=contrast,
+                            fname_seg=fname_seg,
+                            path_out=path_results,
+                            quality_control=qc,
+                            verbose=verbose)
+
     # run the extraction
     fname_out, tmp_dir = detector.apply()
 
@@ -323,7 +334,7 @@ def main(args=None):
     # View results
     if fname_out is not None:
         printv('\nDone! To view results, type:', verbose)
-        printv('fslview ' + arguments["-i"] + ' ' + fname_out + ' -l Red -t 0.7 & \n', verbose, 'info')
+        printv('fslview ' + arguments["-i"] + ' ' + fname_out + ' -l Red & \n', verbose, 'info')
 
 if __name__ == "__main__":
     main()
