5e35f699ba86f74229efdfd5eafbf12634e26fdf
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-06 17:46:38 -0400

    TEST: apply_transfo: now testing 2 arguments

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -10,6 +10,7 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
+# TODO: generate warping field for dmri that makes sense (dmri --> T2).
 
 def init(param_test):
     """
@@ -23,7 +24,8 @@ def init(param_test):
     param_test
     """
     # initialization
-    default_args = '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz'  # default parameters
+    default_args = ['-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz',
+                    '-i dmri/dmri.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz']
 
     # assign default params
     if not param_test.args:
