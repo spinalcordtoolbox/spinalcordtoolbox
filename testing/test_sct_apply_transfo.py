7eda154623d0f56090a44221d7f33120076c69bb
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-10 16:37:37 -0400

    TEST: updated a few tests

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -14,14 +14,7 @@
 
 def init(param_test):
     """
-    Initialize testing.
-    Parameters
-    ----------
-    param_test: Class defined in sct_testing.py
-
-    Returns
-    -------
-    param_test
+    Initialize class: param_test
     """
     # initialization
     default_args = ['-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz',
@@ -37,53 +30,6 @@ def init(param_test):
 def test_integrity(param_test):
     """
     Test integrity of function
-    Parameters
-    ----------
-    param_test: Class defined in sct_testing.py
-
-    Returns
-    -------
-    param_test
     """
     param_test.output += '\nNot implemented.'
     return param_test
-#
-#
-# def test(data_path):
-#
-#     # parameters
-#     folder_data = [ 'template/template/', 't2/', 'dmri/']
-#     file_data = [get_file_label(data_path+'template/template/','T2-weighted'),
-#                  't2.nii.gz',
-#                  'warp_template2anat.nii.gz',
-#                  'dmri.nii.gz']
-#
-#     output = ''
-#     status = 0
-#
-#     # test function
-#     cmd = 'sct_apply_transfo -i ' + data_path + folder_data[0] + file_data[0] \
-#           + ' -d ' + data_path + folder_data[1] + file_data[1] \
-#           + ' -w ' + data_path + folder_data[1] + file_data[2]
-#     output += cmd+'\n'  # copy command
-#     s, o = commands.getstatusoutput(cmd)
-#     status += s
-#     output += o
-#
-#     # test with 4d input
-#     cmd = 'sct_apply_transfo -i ' + data_path + folder_data[2] + file_data[3] \
-#           + ' -d ' + data_path + folder_data[1] + file_data[1] \
-#           + ' -w ' + data_path + folder_data[1] + file_data[2]
-#     output += cmd+'\n'  # copy command
-#     s, o = commands.getstatusoutput(cmd)
-#     status += s
-#     output += o
-#
-#     # return
-#     #return sct.run(cmd, 0)
-#     return status, output
-#
-#
-# if __name__ == "__main__":
-#     # call main function
-#     test()
--- testing/test_sct_maths.py
+++ testing/test_sct_maths.py
@@ -4,36 +4,29 @@
 # Test function sct_maths
 #
 # ---------------------------------------------------------------------------------------
-# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
 # Author: Julien Cohen-Adad
-# modified: 2014-10-06
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-#import sct_utils as sct
-import commands
+def init(param_test):
+    """
+    Initialize class: param_test
+    """
+    # initialization
+    default_args = ['-i mt/mtr.nii.gz -percent 95 -o test.nii.gz']
+
+    # assign default params
+    if not param_test.args:
+        param_test.args = default_args
+
+    return param_test
 
 
-def test(path_data):
-
-    folder_data = 'mt/'
-    file_data = ['mtr.nii.gz']
-
-    output = ''
-    status = 0
-
-    cmd = 'sct_maths -i ' + path_data + folder_data + file_data[0] \
-                + ' -o test.nii.gz' \
-                + ' -percent 95'
-    output += '\n====================================================================================================\n'+cmd+'\n====================================================================================================\n\n'  # copy command
-
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-
-    return status, output
-
-if __name__ == "__main__":
-    # call main function
-    test()
+def test_integrity(param_test):
+    """
+    Test integrity of function
+    """
+    param_test.output += '\nNot implemented.'
+    return param_test
