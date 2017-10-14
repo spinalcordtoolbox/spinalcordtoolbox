6b1144a97927220e56dbb2eb3807491281b7eae7
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-14 11:34:43 -0400

    TEST: refactored test for sct_crop_image

--- testing/test_sct_crop_image.py
+++ testing/test_sct_crop_image.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python
 #########################################################################################
 #
-# Test function sct_crop_image
+# Test function for sct_crop_image
 #
 # ---------------------------------------------------------------------------------------
-# Copyright (c) 2015 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Author: Benjamin De Leener
-# modified: 2015-03-30
+# Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Julien Cohen-Adad
 #
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-import sct_utils as sct
-from msct_image import Image
+def init(param_test):
+    """
+    Initialize class: param_test
+    """
+    # initialization
+    default_args = ['-i t2/t2.nii.gz -o cropped_normal.nii.gz -dim 1 -start 10 -end 50']
+
+    # assign default params
+    if not param_test.args:
+        param_test.args = default_args
+
+    return param_test
 
 
-def test(data_path):
+def test_integrity(param_test):
+    """
+    Test integrity of function
+    """
+    from msct_image import Image
+    # check if cropping was correct
+    nx, ny, nz, nt, px, py, pz, pt = Image('cropped_normal.nii.gz').dim
+    if (ny != 41):
+        param_test.status = 99
 
-    # parameters
-    folder_data = 't2/'
-    file_data = ['t2.nii.gz', 't2_seg.nii.gz']
-
-    # test normal crop
-    cmd = 'sct_crop_image -i ' + data_path + folder_data + file_data[0] \
-          + ' -o cropped_normal.nii.gz -dim 1 -start 10 -end 50'
-    status, output = sct.run(cmd, 0)
-
-    if status == 0:
-        # check if cropping was correct
-        nx, ny, nz, nt, px, py, pz, pt = Image('cropped_normal.nii.gz').dim
-        if (ny != 41):
-            status = 5
-    return status, output
-
-
-if __name__ == "__main__":
-    # call main function
-    test()
+    return param_test
