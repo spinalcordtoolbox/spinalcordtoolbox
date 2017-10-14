0675de9a24eb6853cacb296229b713956b94d230
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-14 06:55:52 -0400

    OPT: propseg: with init-mask mode, start default slice at middle of FOV

--- scripts/sct_propseg.py
+++ scripts/sct_propseg.py
@@ -439,9 +439,10 @@ if __name__ == "__main__":
         starting_slice = arguments.get('-init', 0)
 
         params = AnatomicalParams()
-        params.starting_slice = starting_slice
+        params.starting_slice = starting_slicee
         if use_viewer == 'mask':
             params.num_points = 3
+            params.starting_slice = int(round(nz/2))  # starting slice in the middle of the FOV
         if use_viewer == 'centerline' and not starting_slice:
             params.starting_slice = 0
         image = Image(fname_data)
