26855a40148ef6a9c7b65822bb4e85b0bde4d1a5
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-14 08:53:11 -0400

    TEST: create_mask: unfinished

--- testing/test_sct_create_mask.py
+++ testing/test_sct_create_mask.py
@@ -15,7 +15,11 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-w t2/warp_template2anat.nii.gz,mt/warp_t22mt1.nii.gz -d template/template/PAM50_small_t2.nii.gz']
+    default_args = ['-i mt/mt1.nii.gz -p coord,15x17 -size 10 -r 0',
+                    '-i mt/mt1.nii.gz -p point,mt/mt1_point.nii.gz -size 10 -r 0',
+                    '-i mt/mt1.nii.gz -p center -size 10 -r 0',
+                    '-i mt/mt1.nii.gz -p centerline,mt/mt1_seg.nii.gz -size 5 -r 0',
+                    '-i dmri/dmri.nii.gz -p center -size 10 -r 0']
 
     # assign default params
     if not param_test.args:
@@ -30,93 +34,3 @@ def test_integrity(param_test):
     """
     param_test.output += '\nNot implemented.'
     return param_test
-
-
-#!/usr/bin/env python
-#########################################################################################
-#
-# Test function sct_create_mask
-#
-# ---------------------------------------------------------------------------------------
-# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Author: Augustin Roux
-# create: 2014/10/19
-#
-# About the license: see the file LICENSE.TXT
-#########################################################################################
-
-#import sct_utils as sct
-import commands
-import time
-
-
-def test(data_path):
-
-    # parameters
-    folder_data = ['mt/', 'dmri/']
-    file_data = ['mt1.nii.gz', 'mt1_point.nii.gz', 'mt1_seg.nii.gz', 'dmri.nii.gz']
-
-    output = ''
-    status = 0
-
-    # method coord
-    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
-          + ' -p coord,15x17' \
-          + ' -size 10' \
-          + ' -r 0'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-    # time.sleep(1)  # here add one second, otherwise the next test will try to create a temporary folder with the same name (because it is typically ran in less than one second)
-
-    # method point
-    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
-          + ' -p point,' + data_path + folder_data[0] + file_data[1] \
-          + ' -size 10' \
-          + ' -r 0'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-    # time.sleep(1)
-
-    # method center
-    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
-          + ' -p center' \
-          + ' -size 10' \
-          + ' -r 0'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-    # time.sleep(1)
-
-    # method centerline
-    cmd = 'sct_create_mask -i ' + data_path + folder_data[0] + file_data[0] \
-          + ' -p centerline,' + data_path + folder_data[0] + file_data[2] \
-          + ' -size 10' \
-          + ' -r 0'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-    # time.sleep(1)
-
-    # method center on 4d data
-    cmd = 'sct_create_mask -i ' + data_path + folder_data[1] + file_data[3] \
-          + ' -p center' \
-          + ' -size 10' \
-          + ' -r 0'
-    output += cmd+'\n'  # copy command
-    s, o = commands.getstatusoutput(cmd)
-    status += s
-    output += o
-
-    return status, output
-
-
-if __name__ == "__main__":
-    # call main function
-    test()
-
