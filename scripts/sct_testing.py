be20ed7cb6927287d1c242a17cfde1557eba1cea
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-14 11:32:37 -0400

    BUG: msct_parser: fixed bug if list contains non-string

--- scripts/sct_testing.py
+++ scripts/sct_testing.py
@@ -221,7 +221,7 @@ def fill_functions():
         # 'sct_convert',
         # 'sct_convert_binary_to_trilinear',  # not useful
         'sct_create_mask',
-        # 'sct_crop_image',
+        'sct_crop_image',
         # 'sct_dmri_compute_dti',
         # 'sct_dmri_create_noisemask',
         # 'sct_dmri_get_bvalue',
