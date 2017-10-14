a54668ce4c0918c36268c0c757df44cd165f49b9
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-14 08:07:05 -0400

    TEST: minor modifs

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #########################################################################################
 #
-# Test function sct_apply_transfo
+# Test function for sct_apply_transfo
 #
 # ---------------------------------------------------------------------------------------
 # Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
