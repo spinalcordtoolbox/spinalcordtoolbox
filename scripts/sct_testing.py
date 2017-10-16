89c19240d2891bdd6e58453eb2bc8bb495c94131
charleygros <chgroc@evans.neuro.polymtl.ca>
2017-10-16 15:55:38 -0400

    BUG: in the case of the template folder, we need to take the last element (-1) of the list .split('/') instead of the one in index 1 (which is ok for the other folders, e.g. t2, mt...etc)

--- scripts/sct_testing.py
+++ scripts/sct_testing.py
@@ -379,7 +379,7 @@ def test_function(param_test):
         if not isinstance(dict_args_with_path['-i'], list):
             list_file_to_check = [dict_args_with_path['-i']]
             # assign field file_input for integrity testing
-            param_test.file_input = dict_args['-i'].split('/')[1]
+            param_test.file_input = dict_args['-i'].split('/')[-1]
         else:
             list_file_to_check = dict_args_with_path['-i']
             # TODO: assign field file_input for integrity testing
