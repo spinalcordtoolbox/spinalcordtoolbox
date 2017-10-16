dc879ecabce75210a3d36786004db962fba61507
charleygros <chgroc@evans.neuro.polymtl.ca>
2017-10-16 13:13:46 -0400

    TEST: add integrity test for sct_compute_hausdorff_distance --> Question for Ben

--- testing/test_sct_compute_hausdorff_distance.py
+++ testing/test_sct_compute_hausdorff_distance.py
@@ -10,13 +10,15 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
+from pandas import DataFrame
 
 def init(param_test):
     """
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-i t2s/t2s_gmseg_manual.nii.gz']
+    default_args = ['-i t2s/t2s_gmseg_manual.nii.gz -d t2s/t2s_gmseg_manual.nii.gz']
+    param_test.max_hausdorff_distance = 1.0
 
     # assign default params
     if not param_test.args:
@@ -28,6 +30,40 @@ def init(param_test):
 def test_integrity(param_test):
     """
     Test integrity of function
+    Parameters
+    ----------
+    param_test: Class defined in sct_testing.py
+
+    Returns
+    -------
+    param_test
     """
-    param_test.output += '\nNot implemented.'
+    # initializations
+    max_hausdorff_distance = float('nan')
+
+    # extract name of output: hausdorff_distance.txt
+    file_hausdorff = param_test.path_output + 'hausdorff_distance.txt'
+
+    # open output segmentation
+    try:
+        hausdorff_txt = open(file_hausdorff, 'r')
+        hausdorff_distance_lst = []
+        for i, line in enumerate(hausdorff_txt):
+            if line.startswith('Slice'):
+                hausdorff_distance_lst.append(float(line.split(': ')[1].split(' -')[0]))
+        hausdorff_txt.close()
+        max_hausdorff_distance = max(hausdorff_distance_lst)
+    except:
+        param_test.output += 'ERROR: Cannot open output hausdorff text file: ' + file_hausdorff
+        return param_test
+
+    param_test.output += 'Max Computed Hausdorff distance: ' + str(max_hausdorff_distance)
+    param_test.output += 'Hausdorff distance threshold (if Max Computed Hausdorff distance higher: fail): ' + str(param_test.max_hausdorff_distance)
+
+    if max_hausdorff_distance > param_test.max_hausdorff_distance:
+        param_test.status = 99
+
+    # transform results into Pandas structure
+    param_test.results = DataFrame(index=[param_test.path_data], data={'status': param_test.status, 'output': param_test.output, 'max_hausdorff_distance': max_hausdorff_distance, 'duration [s]': param_test.duration})
+
     return param_test
