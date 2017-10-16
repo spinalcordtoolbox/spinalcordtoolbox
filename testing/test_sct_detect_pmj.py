33a03effc8f46b698291cf6e71d45c8f369685a7
charleygros <chgroc@evans.neuro.polymtl.ca>
2017-10-16 10:54:20 -0400

    TEST: create testing function for sct_detect_pmj (without integrity checking)

---
+++ testing/test_sct_detect_pmj.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#########################################################################################
+#
+# Test function for sct_detect_pmj script
+#
+# ---------------------------------------------------------------------------------------
+# Copyright (c) 2017 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: charley
+#
+# About the license: see the file LICENSE.TXT
+#########################################################################################
+
+
+def init(param_test):
+    """
+    Initialize class: param_test
+    """
+    # initialization
+    default_args = ['-i t2/t2.nii.gz -c t2']
+
+    # assign default params
+    if not param_test.args:
+        param_test.args = default_args
+
+    return param_test
+
+
+def test_integrity(param_test):
+    """
+    Test integrity of function
+    """
+    param_test.output += '\nNot implemented.'
+    return param_test
