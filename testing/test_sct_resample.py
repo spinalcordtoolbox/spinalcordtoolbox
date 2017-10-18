35f9d618c2f2049a372647051044cc9eec530928
Benjamin De Leener <jackbdl@gmail.com>
2017-10-18 11:30:44 -0400

    TEST: added integrity testing for sct_resample

--- testing/test_sct_resample.py
+++ testing/test_sct_resample.py
@@ -13,6 +13,7 @@
 
 import sct_utils as sct
 import commands
+from msct_image import Image
 
 
 def init(param_test):
@@ -20,7 +21,17 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-i fmri/fmri.nii.gz -f 0.5x0.5x1 -v 1']
+    default_args = ['-i fmri/fmri.nii.gz -f 0.5x0.5x1 -v 1 -o resampled.nii.gz',  # 3D, factor
+                    '-i dmri/dmri.nii.gz -f 0.5x0.5x1 -v 1 -o resampled.nii.gz',  # 4D, factor
+                    '-i t2/t2.nii.gz -mm 0.97x1.14x1.2 -v 1 -o resampled.nii.gz',  # 3D, mm
+                    '-i t2/t2.nii.gz -vox 120x110x26 -v 1 -o resampled.nii.gz'  # 3D, vox
+                    ]
+
+    param_test.results_dims = [(34, 34, 6, 30, 1.9117647, 1.9117646, 16.800001, 1.13),  # 3D, factor
+                               (20, 21, 5, 7, 1.6826923, 1.6826923, 17.5, 2.2),  # 4D, factor
+                               (62, 48, 43, 1, 0.96774191, 1.1458334, 1.2093023, 1),  # 3D, mm
+                               (120, 110, 26, 1, 0.5, 0.5, 2.0, 1)  # 3D, vox
+                               ]
 
     # assign default params
     if not param_test.args:
@@ -32,5 +43,19 @@ def test_integrity(param_test):
     """
     Test integrity of function
     """
-    param_test.output += '\nNot implemented.'
+
+    # find the test that is performed and check the integrity of the output
+    index_args = param_test.default_args.index(param_test.args)
+    param_test.output += '\nTesting ' + param_test.args + '\n'
+
+    # Open resulting image and check dimensions and spacing
+    image_result = Image(param_test.path_output + 'resampled.nii.gz')
+    dims = image_result.dim
+
+    if not all(round(i, 4) == round(j, 4) for i, j in zip(dims, param_test.results_dims[index_args])):
+        param_test.output += 'WARNING: dimensions and spacing different from expected.' \
+                             '\n--> Results: ' + str(dims) + \
+                             '\n--> Expected: ' + str(param_test.results_dims[index_args])
+        param_test.status = 99
+
     return param_test
