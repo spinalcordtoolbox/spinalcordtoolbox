8d59e933a4420c640a48770bda146a79e9931944
Benjamin De Leener <jackbdl@gmail.com>
2017-10-18 11:02:37 -0400

    TEST: refactored sct_resample crash test

--- testing/test_sct_resample.py
+++ testing/test_sct_resample.py
@@ -15,21 +15,22 @@ import sct_utils as sct
 import commands
 
 
-def test(path_data):
+def init(param_test):
+    """
+    Initialize class: param_test
+    """
+    # initialization
+    default_args = ['-i fmri/fmri.nii.gz -f 0.5x0.5x1 -v 1']
 
-    folder_data = 'fmri/'
-    file_data = ['fmri.nii.gz']
-
-
-    cmd = 'sct_resample -i ' + path_data + folder_data + file_data[0] \
-                + ' -f 0.5x0.5x1' \
-                + ' -v 1'
-
-    # return
-    #return sct.run(cmd, 0)
-    return commands.getstatusoutput(cmd)
+    # assign default params
+    if not param_test.args:
+        param_test.args = default_args
+    return param_test
 
 
-if __name__ == "__main__":
-    # call main function
-    test()
+def test_integrity(param_test):
+    """
+    Test integrity of function
+    """
+    param_test.output += '\nNot implemented.'
+    return param_test
