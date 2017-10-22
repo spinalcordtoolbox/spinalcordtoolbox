e6ef69bf660028d0cf4708b0e09665b9e5ccae5d
Julien Cohen-Adad <jcohen@polymtl.ca>
2017-10-21 21:31:19 -0400

    REF: register_to_template: moved groundtruth definition into function

--- testing/test_sct_register_to_template.py
+++ testing/test_sct_register_to_template.py
@@ -21,8 +21,7 @@ def init(param_test):
     """
 
     # initialization
-    default_args = ['-i t2/t2.nii.gz -l t2/labels.nii.gz -s t2/t2_seg.nii.gz -param step=1,type=seg,algo=centermassrot,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=5,metric=MeanSquares -t template -r 0']
-    param_test.list_fname_gt = [param_test.path_data + 'template/template/PAM50_small_cord.nii.gz']
+    default_args = ['-i t2/t2.nii.gz -l t2/labels.nii.gz -s t2/t2_seg.nii.gz -param step=1,type=seg,algo=centermassrot,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=5,metric=MeanSquares -t template -r 0 -igt template/template/PAM50_small_cord.nii.gz']
     param_test.dice_threshold = 0.9
 
     # assign default params
