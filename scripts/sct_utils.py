32a1bf244dc13569d53ae9ec6550ebd2392151c3
Pierre-Olivier Quirion <pioliqui@gmail.com>
2017-12-07 13:41:17 -0500

    doc

--- scripts/sct_utils.py
+++ scripts/sct_utils.py
@@ -136,14 +136,11 @@ def no_new_line_log(msg, *args, **kwargs):
 
     def my_emit(self, record):
         """
-        Emit a record. Monkey patcher for progress bar in the sct
+        Emit a record.
+        Monkey patcher for progress bar in the sct
+        Do a carriage return \r before the string
+        instead of a new line \n at the end
 
-        If a formatter is specified, it is used to format the record.
-        The record is then written to the stream with a trailing newline.  If
-        exception information is present, it is formatted using
-        traceback.print_exception and appended to the stream.  If the stream
-        has an 'encoding' attribute, it is used to determine how to do the
-        output to the stream.
         """
         try:
             unicode
