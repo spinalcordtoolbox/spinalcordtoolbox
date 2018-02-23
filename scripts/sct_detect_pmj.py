b98286c67b07ec017e3f1fa7e2b16d8299401d55
Jérôme Carretero <cJ@zougloub.eu>
2018-02-23 13:00:05 -0500

    sct_detect_pmj: fixup model path
    Former-commit-id: 1f8408a7d7e4abab8b3182b563ecade803e50a8b [formerly 964d1b53a694874ddf9a1074984eb4aac0e6a2c9 [formerly 130ff0eee1534eb75bebf94ec8c00348ab75268a]]
    Former-commit-id: 94f627762bcaefc3067fbf37a745848502f07d95
    Former-commit-id: 02512089f27cd0031355f16b987a5bdae08b18b4

--- scripts/sct_detect_pmj.py
+++ scripts/sct_detect_pmj.py
@@ -100,9 +100,8 @@ class DetectPMJ:
         self.dection_map_pmj = sct.extract_fname(self.fname_im)[1] + '_map_pmj'  # file resulting from the detection
 
         # path to the pmj detector
-        self.pmj_model = os.path.join(sct.run('echo $SCT_DIR')[1],
-                                            'data/pmj_models',
-                                            '{}_model'.format(self.contrast))
+        path_sct = os.environ.get("SCT_DIR", os.path.dirname(os.path.dirname(__file__)))
+        self.pmj_model = os.path.join(path_sct, 'data', 'pmj_models', '{}_model'.format(self.contrast))
 
         self.threshold = -0.75 if self.contrast == 't1' else 0.8  # detection map threshold, depends on the contrast
 
