f7ebd4620218c7c0763312d58474c7de5a06c49c
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-03-05 12:02:21 +0900

    DEV: Cleaned dev/ folder (#1631)

--- dev/isct_vesselness/documentation/html/search/all_e.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['vertebralidentification',['VertebralIdentification',['../class_vertebral_identification.html',1,'']]],
-  ['vertex',['Vertex',['../class_vertex.html',1,'']]],
-  ['vertex_2eh',['Vertex.h',['../_vertex_8h.html',1,'']]]
-];
