0590a7c38cfb31e59d5478c2ad941558a80590fb
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-03-05 12:02:21 +0900

    DEV: Cleaned dev/ folder (#1631)
    Former-commit-id: f7ebd4620218c7c0763312d58474c7de5a06c49c

--- dev/isct_vesselness/documentation/html/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['deformablemodelbasicadaptator',['DeformableModelBasicAdaptator',['../class_deformable_model_basic_adaptator.html',1,'']]],
-  ['deformablemodelbasicadaptator_2eh',['DeformableModelBasicAdaptator.h',['../_deformable_model_basic_adaptator_8h.html',1,'']]],
-  ['docu_5fcorrection_2eh',['Docu_Correction.h',['../_docu___correction_8h.html',1,'']]],
-  ['docu_5fparameters_2eh',['Docu_Parameters.h',['../_docu___parameters_8h.html',1,'']]]
-];
