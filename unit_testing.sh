94c621dbc4503ce7e02d06e58c407c6e42170ed3
Christian S. Perone <christian.perone@gmail.com>
2018-03-28 18:30:17 -0400

    Travis build.

--- unit_testing.sh
+++ unit_testing.sh
@@ -1,4 +1,7 @@
 #/bin/sh
 source $HOME/build/neuropoly/spinalcordtoolbox/python/bin/activate
 export PATH=$PATH:$SCT_DIR/scripts
+export PYTHONPATH=$PYTHONPATH:$SCT_DIR/scripts
+echo $PATH
+echo $PYTHONPATH
 pytest -v --cov=spinalcordtoolbox.deepseg_gm unit_testing
