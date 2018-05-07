d1f03987bfed00c5607abc0ba31b7fccc7de4422
Jérôme Carretero <cJ-tub@zougloub.eu>
2018-05-07 04:40:29 -0400

    Refactor things dealing with dataset `info_label.txt` metadata file (#1676)
    * add sct metadata module with InfoLabel class (nothing now)
    
    * just move info_label processing code to sct.metadata module
    
    * refactor info_label.txt reading code
    
    * sct_warp_template: comment-out the syntax display and QC for now: they aren't appropriate for testing_data when we're stricter
    
    * sct_warp_template: fail when we need to
    
    * use full, not partial label name
    
    Safer.
    
    * add some unit_testing code
    
    * sct_warp_template: run QC if we can find labels
    
    * metadata: fixup typo and improve unit test
    
    * testing: sct_segment_graymatter: fixup get_file_label import
    
    * sct_register_to_template: fixup get_file_label import

--- testing/test_sct_segment_graymatter.py
+++ testing/test_sct_segment_graymatter.py
@@ -14,13 +14,16 @@
 # import commands
 # import sys
 import os
+
+import numpy as np
 from pandas import DataFrame
+
 import sct_segment_graymatter
 # from msct_image import Image
 import sct_utils as sct
-from numpy import sum, mean
 # import time
-from sct_warp_template import get_file_label
+
+from spinalcordtoolbox.metadata import get_file_label
 
 
 def init(param_test):
@@ -78,7 +81,7 @@ def test_integrity(param_test):
                 gm_dice.append(float(dc))
             except ValueError:
                 gm_dice.append(float(dc[:-4]))
-    result_dice_gm = mean(gm_dice)
+    result_dice_gm = np.mean(gm_dice)
 
     # extracting dice on WM
     wm_dice_lines = dice_lines[wm_start:]
@@ -94,7 +97,7 @@ def test_integrity(param_test):
                 wm_dice.append(float(dc))
             except ValueError:
                 wm_dice.append(float(dc[:-4]))
-    result_dice_wm = mean(wm_dice)
+    result_dice_wm = np.mean(wm_dice)
 
     # Extracting hausdorff distance results
     hd = open(hausdorff_fname, 'r')
@@ -115,8 +118,8 @@ def test_integrity(param_test):
             hausdorff.append(hd)
             max_med.append(max(med1, med2))
 
-    result_hausdorff = mean(hausdorff)
-    result_median_dist = mean(max_med)
+    result_hausdorff = np.mean(hausdorff)
+    result_median_dist = np.mean(max_med)
 
     # Integrity check
     if result_hausdorff > param_test.hd_threshold or result_dice_wm < param_test.wm_dice_threshold:
