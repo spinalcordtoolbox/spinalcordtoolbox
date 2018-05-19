e236d026477017bf4d2337fac585a9a34d8294b6
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-05-19 09:40:58 +0900

    Added a case if image is not 2D or 3D (#1744)
    * Added a case if data.shape is different from 2 or 3 (fixes #1743)
    
    * Moved display of created label after the fact (in case it crashes)
    
    
    Former-commit-id: 2ec821c3f04f29dde38cf2ffb5a09739c777b5a8 [formerly 195c403c2afab740888343e017529613d5ef03ae [formerly 73761f72302bb362561c70da2c593b297caa6dcd]]
    Former-commit-id: 80877dc07f144b71bc4ed0016828293992955526
    Former-commit-id: d4a086326f7c2dc9682e86b82d1f2e65ca8c53d6

--- scripts/sct_label_utils.py
+++ scripts/sct_label_utils.py
@@ -169,14 +169,16 @@ class ProcessLabels(object):
 
         # loop across labels
         for i, coord in enumerate(self.coordinates):
-            # display info
-            sct.printv('Label #' + str(i) + ': ' + str(coord.x) + ',' + str(coord.y) + ',' + str(coord.z) + ' --> ' +
-                       str(coord.value), 1)
             if len(image_output.data.shape) == 3:
                 image_output.data[int(coord.x), int(coord.y), int(coord.z)] = coord.value
             elif len(image_output.data.shape) == 2:
                 assert str(coord.z) == '0', "ERROR: 2D coordinates should have a Z value of 0. Z coordinate is :" + str(coord.z)
                 image_output.data[int(coord.x), int(coord.y)] = coord.value
+            else:
+                sct.printv('ERROR: Data should be 2D or 3D. Current shape is: ' + str(image_output.data.shape), 1, 'error')
+            # display info
+            sct.printv('Label #' + str(i) + ': ' + str(coord.x) + ',' + str(coord.y) + ',' + str(coord.z) + ' --> ' +
+                       str(coord.value), 1)
         return image_output
 
     def create_label_along_segmentation(self):
