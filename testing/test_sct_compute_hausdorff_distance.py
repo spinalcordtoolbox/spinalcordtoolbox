29e688a371357657d2ca8967b7ae70d2eafd15a5
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-05-19 20:31:29 +0900

    fixed compatibility with new sct_testing

--- testing/test_sct_compute_hausdorff_distance.py
+++ testing/test_sct_compute_hausdorff_distance.py
@@ -55,7 +55,10 @@ def test_integrity(param_test):
     else:
         param_test.output += '--> PASSED'
 
+    # update Panda structure
+    param_test.results['max_hausdorff_distance'] = max_hausdorff_distance
+
     # transform results into Pandas structure
-    param_test.results = DataFrame(index=[param_test.path_data], data={'status': param_test.status, 'output': param_test.output, 'max_hausdorff_distance': max_hausdorff_distance, 'duration [s]': param_test.duration})
+    # param_test.results = DataFrame(index=[param_test.path_data], data={'status': param_test.status, 'output': param_test.output, 'max_hausdorff_distance': max_hausdorff_distance, 'duration [s]': param_test.duration})
 
     return param_test
