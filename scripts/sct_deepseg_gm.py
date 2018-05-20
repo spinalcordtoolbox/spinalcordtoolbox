spinalcordtoolbox/spinalcordtoolbox

commit 3cdac1ab0f2d1612a27d18b51b9821522d2ffbf2

Random issue fixes (Sentry, sct_pipeline, install) (#1753)
* sct_pipeline: don't add results if execution failed (see #1749, #1751)

* sct_utils: translated programs stdout decoded with terminal encoding (#1745)

* sct_pipeline: use absolute path for path_data

Because it makes use of sct_testing which chdir(), this avoids issues
when the user passes relative paths to sct_pipeline.

* sct_testing: add ability to override log file location

* sct_pipeline: when log file is created, also create worker log files

* rework sct version string: more specific from left to right.

* sct_deepseg_sc: temp. file can be removed (#1742)

* sct_pipeline: make the e-mail sending crash more meaningful to the user (duct-tape workaround for #1752)

* sct_utils: Sentry: ignore SCT control flow exceptions & hard-code scope

* sct_utils: print sentry opt-out message (#1708)

* msct_parser: specify command-line argument errors

* sct_utils: Sentry: don't send errors about command-line usage issues

* sct_utils: don't spam about sentry report preparation

* msct_nurbs: Sentry: don't use log.error for underfitting error

Also cancel out previous commit which was silly


Former-commit-id: 75414bf2d3cfa5661d64fc02c0f708ba772bd68a [formerly e7713c9d37ac3095bf5eebae73c773e554f56f0b [formerly a576106a5bc6f55b04bc01d1765ab65e0c8d5819]]
Former-commit-id: f82b8ecf810d90ebe28ecf131f6f9a4d17af594f
Former-commit-id: fce0c8e6740c3597984f13e8e6df4c5306879a19

scripts/sct_pipeline.py
@@ -253,7 +253,9 @@ def function_launcher(args):
     param_testing.path_data = args[1]
     param_testing.args = args[2]
     param_testing.test_integrity = args[3]
-    param_testing.redirect_to_file = True  # create individual logs for each subject.
+    param_testing.redirect_stdout = True # create individual logs for each subject.
+    if create_log:
+        param_testing.fname_log = os.path.join(os.getcwd(), "%s-%s.log" % (file_log, os.path.basename(args[1])))
     # load modules of function to test
     module_testing = importlib.import_module('test_' + param_testing.function_to_test)
     # initialize parameters specific to the test
@@ -349,11 +351,11 @@ def run_function(function, folder_dataset, list_subj, list_args=[], nb_cpu=None,
             arguments = future_dirs[future][2]
             try:
                 result = future.result()
+                sct.log.info('{}/{}: {} done'.format(count, len(list_func_subj_args), subject))
+                all_results.append(result)
             except Exception as exc:
                 sct.log.error('{} {} generated an exception: {}'.format(subject, arguments, exc))
 
-            sct.log.info('{}/{}: {} done'.format(count, len(list_func_subj_args), subject))
-            all_results.append(result)
         compute_time = time() - compute_time
 
         # concatenate all_results into single Panda structure
@@ -480,7 +482,7 @@ if __name__ == "__main__":
     parser = get_parser()
     arguments = parser.parse(sys.argv[1:])
     function_to_test = arguments["-f"]
-    path_data = arguments["-d"]
+    path_data = os.path.abspath(arguments["-d"])
     if "-p" in arguments:
         # in case users used more than one '-p' flag, the output will be a list of all arguments (for each -p)
         if isinstance(arguments['-p'], list):
@@ -508,12 +510,6 @@ if __name__ == "__main__":
     if '-email' in arguments:
         create_log = True
         send_email = True
-        addr_to = None
-        addr_from = None
-        login = None
-        passwd_from = None
-        smtp_host = None
-        smtp_port = None
         # loop across fields
         for i in arguments['-email']:
             k, v = i.split("=")

scripts/sct_testing.py
@@ -41,6 +41,7 @@ class Param:
         self.output = ''  # output string
         self.results = ''  # results in Panda DataFrame
         self.redirect_stdout = 0  # for debugging, set to 0. Otherwise set to 1.
+        self.fname_log = None
 
 
 # define nice colors
@@ -388,7 +389,10 @@ def test_function(param_test):
         param_test.args_with_path += ' -ofolder ' + param_test.path_output
     # open log file
     # Note: the statement below is not included in the if, because even if redirection does not occur, we want the file to be create otherwise write_to_log will fail
-    param_test.fname_log = os.path.join(param_test.path_output, param_test.function_to_test + '.log')
+
+    if param_test.fname_log is None:
+        param_test.fname_log = os.path.join(param_test.path_output, param_test.function_to_test + '.log')
+
     # redirect to log file
     if param_test.redirect_stdout:
         file_handler = sct.add_file_handler_to_logger(param_test.fname_log)

scripts/msct_nurbs.py
@@ -182,8 +182,7 @@ class NURBS():
                         list_param_that_worked.append([self.nbControle, self.pointsControle, error_curve])
 
                     except ReconstructionError as ex:
-                        if verbose >= 1:
-                            sct.log.error(ex)
+                        sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=verbose, type="warning")
                         error_curve = last_error_curve + 10000.0
 
                     # prepare for next iteration
@@ -389,7 +388,6 @@ class NURBS():
             P_z_d.append(sum_num_z_der)
 
             if sum_den <= 0.05:
-                sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=self.verbose, type="warning")
                 raise ReconstructionError()
 
         P_x = [P_x[i] for i in np.argsort(P_z)]
@@ -490,7 +488,6 @@ class NURBS():
             P_y_d.append(sum_num_y_der)
 
             if sum_den <= 0.05:
-                sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=verbose, type="warning")
                 raise ReconstructionError()
 
         P_x = [P_x[i] for i in np.argsort(P_y)]
@@ -659,7 +656,6 @@ class NURBS():
         std_factor = 10.0
         std_Px, std_Py, std_Pz, std_x, std_y, std_z = std(P_xb), std(P_yb), std(P_zb), std(np.array(P_x)), std(np.array(P_y)), std(np.array(P_z))
         if std_x >= 0.1 and std_y >= 0.1 and std_z >= 0.1 and (std_Px > std_factor * std_x or std_Py > std_factor * std_y or std_Pz > std_factor * std_z):
-            sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=verbose, type="warning")
             raise ReconstructionError()
 
         P = [[P_xb[i, 0], P_yb[i, 0], P_zb[i, 0]] for i in range(len(P_xb))]
@@ -772,7 +768,6 @@ class NURBS():
         std_factor = 10.0
         std_Px, std_Py, std_x, std_y = std(P_xb), std(P_yb), std(np.array(P_x)), std(np.array(P_y))
         if std_x >= 0.1 and std_y >= 0.1 and (std_Px > std_factor * std_x or std_Py > std_factor * std_y):
-            sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=verbose, type="warning")
             raise ReconstructionError()
 
         P = [[P_xb[i, 0], P_yb[i, 0]] for i in range(len(P_xb))]
@@ -869,7 +864,6 @@ class NURBS():
             P_z_d.append(sum_num_z_der)
 
             if sum_den <= 0.05:
-                sct.printv('WARNING: NURBS instability -> wrong reconstruction', verbose=verbose, type="warning")
                 raise ReconstructionError()
 
         P_x = [P_x[i] for i in np.argsort(P_z)]

commit 40d69f10d2f581c885e0db684a67a9c4dc403409

Added integrity metrics in sct_pipeline report (#1754)
* Now display quality metrics at the end (fixes #1750)

* now display duration

* replaced for sexier term

* added todo

* fixed duplication duration output

* fixed output display of dataframe

* fixed compatibility with new sct_testing

* fixed compatibility

* fixed compatibility

* now display counter in the same line for clearer log

* deepseg_gm: fixed compatibility with testing framework

* fixed variable renaming

* Fixed issue introduced in PR #1753, which prevented creation of individual log per subject

* fixed compatibility of deepseg_gm with testing pipeline, without -ofolder flag

* test_sct_deepseg_gm: fixed broken compatibility

* test_sct_dmri_moco: fixed broken compatibility. Deprecation not useful anymore

* test_sct_straighten_spinalcord: fixed broken compatibility

* test_sct_warp_template: fixed broken compatibility


Former-commit-id: f02347bb892ecded4e1c00aae8a870f4a3192224 [formerly 9e7ba3e462573aeb8170020abb28d902107fc51c [formerly a3793be142f157748d775de103830493e69ce83c]]
Former-commit-id: f7cc95d692f9d9a2e3f3d6cf8995c7fa9f61cc09
Former-commit-id: 812803097ca9ba67cbe5c14b59b34a39562e98e7

scripts/sct_deepseg_gm.py
@@ -49,6 +49,11 @@ def get_parser():
                       example=['large', 'challenge'],
                       default_value='large')
 
+    parser.add_option(name='-igt',
+                      type_value='image_nifti',
+                      description='File name of ground-truth segmentation.',
+                      mandatory=False)
+
     parser.add_option(name="-v",
                       type_value='multiple_choice',
                       description="Verbose: 0 = no verbosity, 1 = verbose.",

scripts/sct_dmri_moco.py
@@ -175,11 +175,6 @@ def get_parser():
                       mandatory=False,
                       default_value='./',
                       example='dmri_moco_results/')
-    parser.add_option(name='-o',
-                      type_value=None,
-                      description='Output folder.',
-                      mandatory=False,
-                      deprecated_by='-o')
     parser.usage.addSection('MISC')
     parser.add_option(name="-r",
                       type_value="multiple_choice",

scripts/sct_pipeline.py
@@ -39,6 +39,7 @@ usage:
     sct_pipeline  -f sct_a_tool -d /path/to/data/  -p  \" sct_a_tool option \" -cpu-nb 8
 """
 
+# TODO: remove compute duration which is now replaced with results.duration
 # TODO: create a dictionnary for param, such that results can display reduced param instead of full. Example: -param t1="blablabla",t2="blablabla"
 # TODO: read_database: hard coded fields to put somewhere else (e.g. config file)
 
@@ -253,9 +254,7 @@ def function_launcher(args):
     param_testing.path_data = args[1]
     param_testing.args = args[2]
     param_testing.test_integrity = args[3]
-    param_testing.redirect_stdout = True # create individual logs for each subject.
-    if create_log:
-        param_testing.fname_log = os.path.join(os.getcwd(), "%s-%s.log" % (file_log, os.path.basename(args[1])))
+    param_testing.redirect_stdout = True  # create individual logs for each subject.
     # load modules of function to test
     module_testing = importlib.import_module('test_' + param_testing.function_to_test)
     # initialize parameters specific to the test
@@ -306,7 +305,7 @@ def get_list_subj(folder_dataset, data_specifications=None, fname_database=''):
     else:
         sct.log.info('Selecting subjects using the following specifications: ' + data_specifications)
         list_subj = read_database(folder_dataset, specifications=data_specifications, fname_database=fname_database)
-    sct.log.info('  Total number of subjects: ' + str(len(list_subj)))
+    # sct.log.info('  Total number of subjects: ' + str(len(list_subj)))
 
     # if no subject to process, raise exception
     if len(list_subj) == 0:
@@ -341,7 +340,7 @@ def run_function(function, folder_dataset, list_subj, list_args=[], nb_cpu=None,
         count = 0
         all_results = []
 
-        sct.log.info('Waiting for results, be patient')
+        # sct.log.info('Waiting for results, be patient')
         future_dirs = {pool.submit(function_launcher, subject_arg): subject_arg
                          for subject_arg in list_func_subj_args}
 
@@ -351,7 +350,7 @@ def run_function(function, folder_dataset, list_subj, list_args=[], nb_cpu=None,
             arguments = future_dirs[future][2]
             try:
                 result = future.result()
-                sct.log.info('{}/{}: {} done'.format(count, len(list_func_subj_args), subject))
+                sct.no_new_line_log('Processing subjects... {}/{}'.format(count, len(list_func_subj_args)))
                 all_results.append(result)
             except Exception as exc:
                 sct.log.error('{} {} generated an exception: {}'.format(subject, arguments, exc))
@@ -600,15 +599,14 @@ if __name__ == "__main__":
         pd.set_option('display.max_colwidth', -1)  # to avoid truncation of long string
         pd.set_option('display.width', 1000)
         # drop entries for visibility
-        results_subset = results.drop('path_data', 1).drop('output', 1)
-        results_display = results_subset
-        # reorder for better display: status, path_output
-        results_display = results_display[['status', 'path_output']]
+        results_subset = results.drop(labels=['status', 'duration', 'path_output', 'path_data', 'output'], axis=1)
+        # build new dataframe with nice order
+        results_subset = pd.concat([results[['status', 'duration']], results_subset, results[['path_output']]], axis=1)
         # save panda structure
         if output_pickle:
-            results_subset.to_pickle(file_log + '.pickle')
+            results.to_pickle(file_log + '.pickle')
         # compute mean
-        results_mean = results_subset.query('status != 200 & status != 201').mean(numeric_only=True)
+        results_mean = results.query('status != 200 & status != 201').mean(numeric_only=True)
         results_mean['subject'] = 'Mean'
         results_mean.set_value('status', float('NaN'))  # set status to NaN
         # compute std
@@ -637,7 +635,7 @@ if __name__ == "__main__":
         sct.log.info('STD: ' + str(dict_std))
         # sct.log.info(detailed results)
         sct.log.info('\nDETAILED RESULTS:')
-        sct.log.info(results_display.to_string())
+        sct.log.info(results_subset.to_string())
         sct.log.info('\nLegend status:\n0: Passed | 1: Function crashed | 2: Integrity testing crashed | 99: Failed | 200: Input file(s) missing | 201: Ground-truth file(s) missing')
 
         if verbose == 2:
@@ -645,19 +643,19 @@ if __name__ == "__main__":
             import matplotlib.pyplot as plt
             from numpy import asarray
 
-            n_plots = len(results_display.keys()) - 2
+            n_plots = len(results_subset.keys()) - 2
             sns.set_style("whitegrid")
             fig, ax = plt.subplots(1, n_plots, gridspec_kw={'wspace': 1}, figsize=(n_plots * 4, 15))
             i = 0
             ax_array = asarray(ax)
 
-            for key in results_display.keys():
+            for key in results_subset.keys():
                 if key not in ['status', 'subject']:
                     if ax_array.size == 1:
                         a = ax
                     else:
                         a = ax[i]
-                    data_passed = results_display[results_display['status'] == 0]
+                    data_passed = results_subset[results_subset['status'] == 0]
                     sns.violinplot(x='status', y=key, data=data_passed, ax=a, inner="quartile", cut=0,
                                    scale="count", color='lightgray')
                     sns.swarmplot(x='status', y=key, data=data_passed, ax=a, color='0.3', size=4)

scripts/sct_testing.py
@@ -9,7 +9,7 @@
 
 # TODO: list functions to test in help (do a search in testing folder)
 # TODO: find a way to be able to have list of arguments and loop across list elements.
-
+# TODO: do something about this ugly 'output.nii.gz'
 
 import sys, io, os, time, random, copy, shlex, importlib, shutil
 
@@ -40,7 +40,7 @@ class Param:
         self.contrast = ''  # folder containing the data and corresponding to the contrast. Could be t2, t1, t2s, etc.
         self.output = ''  # output string
         self.results = ''  # results in Panda DataFrame
-        self.redirect_stdout = 0  # for debugging, set to 0. Otherwise set to 1.
+        self.redirect_stdout = True  # for debugging, set to 0. Otherwise set to 1.
         self.fname_log = None
 
 
@@ -387,6 +387,12 @@ def test_function(param_test):
     # check if parser has key '-ofolder' that has not been added already. If so, then assign output folder
     if "-ofolder" in parser.options and '-ofolder' not in dict_args_with_path:
         param_test.args_with_path += ' -ofolder ' + param_test.path_output
+
+    # check if parser has key '-o' that has not been added already. If so, then assign output folder
+    # Note: this -o case has been added for compatibility with sct_deepseg_gm, which does not have -ofolder flag
+    if "-o" in parser.options and '-o' not in dict_args_with_path:
+        param_test.args_with_path += ' -o ' + os.path.join(param_test.path_output, 'output.nii.gz')
+
     # open log file
     # Note: the statement below is not included in the if, because even if redirection does not occur, we want the file to be create otherwise write_to_log will fail
 
@@ -402,6 +408,7 @@ def test_function(param_test):
     sct.log.debug("Init dataframe")
     param_test.results = DataFrame(index=[subject_folder],
                                    data={'status': 0,
+                                         'duration': 0,
                                          'output': '',
                                          'path_data': param_test.path_data,
                                          'path_output': param_test.path_output})
@@ -452,7 +459,7 @@ def test_function(param_test):
         return update_param(param_test)
 
     param_test.output += o
-    param_test.duration = time.time() - time_start
+    param_test.results['duration'] = time.time() - time_start
 
     # test integrity
     if param_test.test_integrity:

scripts/sct_warp_template.py
@@ -168,11 +168,6 @@ def get_parser():
                       mandatory=False,
                       default_value=param_default.folder_out,
                       example="label")
-    parser.add_option(name="-o",
-                      type_value=None,
-                      description="name of output folder.",
-                      mandatory=False,
-                      deprecated_by='-ofolder')
     parser.add_option(name="-t",
                       type_value="folder",
                       description="Path to template.",

testing/test_sct_compute_hausdorff_distance.py
@@ -55,7 +55,7 @@ def test_integrity(param_test):
     else:
         param_test.output += '--> PASSED'
 
-    # transform results into Pandas structure
-    param_test.results = DataFrame(index=[param_test.path_data], data={'status': param_test.status, 'output': param_test.output, 'max_hausdorff_distance': max_hausdorff_distance, 'duration [s]': param_test.duration})
+    # update Panda structure
+    param_test.results['max_hausdorff_distance'] = max_hausdorff_distance
 
     return param_test

testing/test_sct_deepseg_gm.py
@@ -20,9 +20,8 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-i t2s/t2s_uncropped.nii.gz -o t2s_uncropped_gmseg.nii.gz -qc testing-qc']
+    default_args = ['-i t2s/t2s_uncropped.nii.gz -igt t2s/t2s_uncropped_gmseg_manual.nii.gz -o output.nii.gz -qc testing-qc']
     param_test.dice_threshold = 0.85
-    param_test.f_ground_truth = 't2s/t2s_uncropped_gmseg_manual.nii.gz'
 
     # assign default params
     if not param_test.args:
@@ -36,12 +35,11 @@ def test_integrity(param_test):
     """
     dice_segmentation = float('nan')
     # extract name of output segmentation: data_seg.nii.gz
-    file_seg = os.path.join(param_test.path_output, sct.add_suffix(param_test.file_input, '_gmseg'))
+    file_seg = os.path.join(param_test.path_output, 'output.nii.gz')
     # open output segmentation
     im_seg = Image(file_seg)
     # open ground truth
-    fname_gt_path = os.path.join(param_test.path_data, param_test.f_ground_truth)
-    im_seg_manual = Image(fname_gt_path)
+    im_seg_manual = Image(param_test.fname_gt)
     # compute dice coefficient between generated image and image from database
     dice_segmentation = compute_dice(im_seg, im_seg_manual, mode='3d', zboundaries=False)
     # display
@@ -55,5 +53,5 @@ def test_integrity(param_test):
         param_test.output += '\n--> PASSED'
 
     # update Panda structure
-    param_test.results['dice_segmentation'] = dice_segmentation
+    param_test.results['dice'] = dice_segmentation
     return param_test

testing/test_sct_deepseg_sc.py
@@ -58,6 +58,6 @@ def test_integrity(param_test):
         param_test.output += '\n--> PASSED'
 
     # update Panda structure
-    param_test.results['dice_segmentation'] = dice_segmentation
+    param_test.results['dice'] = dice_segmentation
 
     return param_test

testing/test_sct_label_utils.py
@@ -57,8 +57,5 @@ def test_integrity(param_test):
             param_test.output += 'WARNING: Center of mass different from gold-standard. \n--> Results:   ' + centers_of_mass_image + '\n--> Should be: ' + param_test.centers_of_mass + '\n'
             param_test.status = 99
 
-    # transform results into Pandas structure
-    param_test.results = DataFrame(data={'status': param_test.status, 'output': param_test.output, 'duration [s]': param_test.duration}, index=[param_test.path_data])
-
     # end test
     return param_test

testing/test_sct_propseg.py
@@ -65,6 +65,6 @@ def test_integrity(param_test):
         param_test.output += '\n--> PASSED'
 
     # update Panda structure
-    param_test.results['dice_segmentation'] = dice_segmentation
+    param_test.results['dice'] = dice_segmentation
 
     return param_test

testing/test_sct_segment_graymatter.py
@@ -128,9 +128,10 @@ def test_integrity(param_test):
                              'WM dice: ' + str(result_dice_wm) + '\n' \
                              'Hausdorff distance: ' + str(result_hausdorff) + '\n'
 
-    # transform results into Pandas structure
-    results = DataFrame(data={'status': param_test.status, 'output': param_test.output, 'dice_gm': result_dice_gm, 'dice_wm': result_dice_wm,
-                              'hausdorff': result_hausdorff, 'med_dist': result_median_dist, 'duration_[s]': param_test.duration},
-                        index=[param_test.path_data])
+    # update Panda structure
+    param_test.results['dice_gm'] = result_dice_gm
+    param_test.results['dice_wm'] = result_dice_wm
+    param_test.results['hausdorff'] = result_hausdorff
+    param_test.results['med_dist'] = result_median_dist
 
     return param_test

testing/test_sct_straighten_spinalcord.py
@@ -61,7 +61,7 @@ def test_integrity(param_test):
 
         # apply curved2straight, then straight2curve, then compared results
         path_input, file_input, ext_input = sct.extract_fname(param_test.file_input)
-        sct.run('sct_apply_transfo -i ' + os.path.join(param_test.path_data, param_test.fname_segmentation) + ' -d ' + os.path.join(param_test.path_output, file_input) + '_straight' + ext_input + ' -w ' + os.path.join(param_test.path_output, 'warp_curve2straight.nii.gz') + ' -o ' + os.path.join(param_test.path_output, 'tmp_seg_straight.nii.gz') + ' -x linear', 0)
+        sct.run('sct_apply_transfo -i ' + os.path.join(param_test.path_data, param_test.fname_segmentation) + ' -d ' + os.path.join(param_test.path_output, 'output.nii.gz') + ' -w ' + os.path.join(param_test.path_output, 'warp_curve2straight.nii.gz') + ' -o ' + os.path.join(param_test.path_output, 'tmp_seg_straight.nii.gz') + ' -x linear', 0)
         sct.run('sct_apply_transfo -i ' + os.path.join(param_test.path_output, 'tmp_seg_straight.nii.gz') + ' -d ' + os.path.join(param_test.path_data, param_test.fname_segmentation) + ' -w ' + os.path.join(param_test.path_output, 'warp_straight2curve.nii.gz') + ' -o ' + os.path.join(param_test.path_output, 'tmp_seg_straight_curved.nii.gz') + ' -x nn',0)
 
         # threshold and binarize
@@ -77,10 +77,10 @@ def test_integrity(param_test):
             param_test.status = 99
             param_test.output += '\nWARNING: DICE = ' + str(result_dice) + ' < ' + str(param_test.th_dice)
 
-        # transform results into Pandas structure
-        param_test.results = DataFrame(data={'status': param_test.status, 'output': param_test.output, 'rmse': result_rmse, 'dist_max': result_dist_max,
-                                             'dice': result_dice, 'duration': param_test.duration, 'duration_accuracy_results': duration_accuracy_results},
-                                       index=[param_test.path_data])
+        # update Panda structure
+        param_test.results['dice'] = result_dice
+        param_test.results['rmse'] = result_rmse
+        param_test.results['dist_max'] = result_dist_max
 
     except Exception as e:
         param_test.status = 99