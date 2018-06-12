ca7904d18841534ad98275622db507f76b467c28
Christian S. Perone <christian.perone@gmail.com>
2018-06-11 20:18:02 -0400

    Fixing coverage for coveralls.

--- unit_testing.sh
+++ unit_testing.sh
@@ -1,5 +1,4 @@
 #/bin/sh
 source $HOME/build/neuropoly/spinalcordtoolbox/python/bin/activate
 export PYTHONPATH=$PYTHONPATH:$SCT_DIR/scripts
-pytest -v --cov=spinalcordtoolbox.deepseg_gm unit_testing/test_deepseg_gm.py
-pytest -v --cov=spinalcordtoolbox.metadata unit_testing/test_metadata.py
+pytest -v --cov-append --cov=spinalcordtoolbox unit_testing/*
