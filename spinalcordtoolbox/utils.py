8c0753710274b06ec51011b52fc3ea3a847242e2
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-06-27 15:33:57 +0900

    refactored parse_num_list
    Former-commit-id: 7ce908ea7a14fb7b272bbaac07f9abd82f5e55d1 [formerly 418e948a105222e260a0ad6f1350dddc29163799 [formerly 67ae99cfa6997a9715aaaabb596a8a9c91610c45]]
    Former-commit-id: 80b9f0b0e20098467e9d809e7df3bb60f2a4c836
    Former-commit-id: 0bcf703dc4fc55b52462228397df8b0787cef87f

--- spinalcordtoolbox/utils.py
+++ spinalcordtoolbox/utils.py
@@ -2,23 +2,38 @@
 # -*- coding: utf-8
 # Collection of useful functions
 
+import re
 
-def num_parser(str_num):
+def parse_num_list(str_num):
     """
     Parse numbers in string based on delimiter: , or :
     Examples:
-      '1,2,3' gives [1, 2, 3]
-      '1:3' gives [1, 2, 3]
+      '' -> []
+      '1,2,3' -> [1, 2, 3]
+      '1:3,4' -> [1, 2, 3, 4]
+      '1,1:4' -> [1, 2, 3, 4]
     :param str_num: string
-    :return: list of int
+    :return: list of ints
     """
-    # check if user selected specific slices using delimitor ','
-    if not str_num.find(',') == -1:
-        list_num = [int(x) for x in str_num.split(',')]  # n-element list
-    else:
-        num_range = [int(x) for x in str_num.split(':')]  # 2-element list
-        # if only one slice (z) was selected, consider as z:z
-        if len(num_range) == 1:
-            slices_range = [num_range[0], num_range[0]]
-        list_num = [i for i in range(num_range[0], num_range[1] + 1)]
+    list_num = list()
+
+    if not str_num:
+        return list_num
+
+    elements = str_num.split(",")
+    for element in elements:
+        m = re.match(r"^\d+$", element)
+        if m is not None:
+            val = int(element)
+            if val not in list_num:
+                list_num.append(val)
+            continue
+        m = re.match(r"^(?P<first>\d+):(?P<last>\d+)$", element)
+        if m is not None:
+            a = int(m.group("first"))
+            b = int(m.group("last"))
+            list_num += [ x for x in range(a, b+1) if x not in list_num ]
+            continue
+        raise ValueError("unexpected group element {} group spec {}".format(element, str_num))
+
     return list_num
