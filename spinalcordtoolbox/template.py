e7bef2d04c383a299aed9b53534bc57e5d39e867
Jérôme Carretero <cJ@zougloub.eu>
2018-07-02 22:17:22 -0400

    template: rename get_vertebral_level_from_slice param
    Former-commit-id: 9c7b732fe512a243879bf5d25a14cd604b16813c [formerly 692f52b0484bdb6bc7c0995ec6ea8988f22d18dd [formerly 3f1d3388337c5ec30d9a79911dbd893a365ad93a]]
    Former-commit-id: ff5b119c5ce03330e00a8b6990cf45a8f5241e0d
    Former-commit-id: d4d4c1271ff6ef7fdbb38bd35f9498a4792bc29e

--- spinalcordtoolbox/template.py
+++ spinalcordtoolbox/template.py
@@ -31,20 +31,20 @@ def get_slices_from_vertebral_levels(im_vertlevel, level):
     return slices
 
 
-def get_vertebral_level_from_slice(im_vertlevel, slice):
+def get_vertebral_level_from_slice(im_vertlevel, idx_slice):
     """
     Find the vertebral level of the corresponding slice.
     Important: This function assumes that the 3rd dimension is Z.
     :param im_vertlevel: image object of vertebral labeling (e.g., label/template/PAM50_levels.nii.gz)
-    :param slice: int: slice (z)
+    :param idx_slice: int: slice (z)
     :return: int: vertebral level. If no level is found (only zeros on this slice), return None.
     """
     data_vertlevel = im_vertlevel.data
     # average non-null values and round to closest
     try:
         # find indices of non-null values
-        indx, indy = np.where(data_vertlevel[:, :, slice])
-        vert_level = int(round(np.mean(data_vertlevel[indx, indy, slice])))
+        indx, indy = np.where(data_vertlevel[:, :, idx_slice])
+        vert_level = int(round(np.mean(data_vertlevel[indx, indy, idx_slice])))
     except ValueError as e:
         # slice is empty (no indx found). Do nothing.
         log.debug('Empty slice: z=%s (%s)', iz, e)
