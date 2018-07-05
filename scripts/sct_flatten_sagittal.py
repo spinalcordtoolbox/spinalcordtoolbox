fixed wrong index (issue 1882) (#1882)
Former-commit-id: b8da93b8787acf6872865dc92160c9356e403d8b [formerly a0c4032a7a88ccbe904275219ed0c596bd85b772 [formerly 067e203c96a099cefc79b453048857f5703e2fbd]]
Former-commit-id: 73739e77f445c33daf4399ed4b45bff1370b2fcf
Former-commit-id: 5890b9732ca390a13766a1ebe77cd35cb5142686

scripts/sct_flatten_sagittal.py
@@ -59,8 +59,10 @@ def main(fname_anat, fname_centerline, degree_poly, centerline_fitting, interp, 
     # first, get zmin and zmax spanned by the centerline (i.e. with non-zero values)
     indz_centerline = np.where([np.sum(im_centerline.data[:, :, iz]) for iz in range(nz)])[0]
     zmin, zmax = indz_centerline[0], indz_centerline[-1]
-    # then, extend the centerline by padding values below zmin and above zmax
-    x_centerline_extended = np.concatenate([np.ones(zmin) * x_centerline_fit[0], x_centerline_fit, np.ones(nz-zmax-1) * x_centerline_fit[-1]])
+    # then, extend the centerline by copying values below zmin and above zmax
+    x_centerline_extended = np.concatenate([np.ones(zmin) * x_centerline_fit[0],
+                                            x_centerline_fit,
+                                            np.ones(nz-zmax) * x_centerline_fit[-1]])
 
     # loop across slices and apply translation
     im_anat_flattened = im_anat.copy()