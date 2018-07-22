c0e2070662ec05127bdfd08c56f671b848d90724
Jérôme Carretero <cJ@zougloub.eu>
2018-07-21 23:17:07 -0400

    unit_testing: image: add orientation change tests
    Former-commit-id: f773f834a74224958e5bb6800b9842e6d8d8bb50

--- unit_testing/test_image.py
+++ unit_testing/test_image.py
@@ -28,6 +28,26 @@ def image_paths():
 
 
 
+def fake_3dimage_custom(data):
+    """
+    :return: a Nifti1Image (3D) in RAS+ space
+    """
+    affine = np.eye(4)
+    return nibabel.nifti1.Nifti1Image(data, affine)
+
+
+def fake_3dimage_sct_custom(data):
+    """
+    :return: an Image (3D) in RAS+ (aka SCT LPI) space
+    """
+    i = fake_3dimage_custom(data)
+    img = msct_image.Image(i.get_data(), hdr=i.header,
+     orientation="LPI",
+     dim=i.header.get_data_shape(),
+    )
+    return img
+
+
 @pytest.fixture(scope="session")
 def fake_3dimage():
     """
@@ -80,6 +100,41 @@ def fake_3dimage2():
     affine = np.eye(4)
     return nibabel.nifti1.Nifti1Image(data, affine)
 
+@pytest.fixture(scope="session")
+def fake_4dimage():
+    """
+    :return: a Nifti1Image (3D) in RAS+ space
+
+    Following characteristics:
+
+    - shape[LR] = 2
+    - shape[PA] = 3
+    - shape[IS] = 4
+    - shape[t] = 5
+    """
+    shape = (2,3,4,5)
+    data = np.zeros(shape, dtype=np.float32, order="F")
+
+    for t in range(shape[3]):
+        for z in range(shape[2]):
+            for y in range(shape[1]):
+                for x in range(shape[0]):
+                    data[x,y,z,t] = (1+x)*1 + (1+y)*10 + (1+z)*100 + (1+t)*1000
+
+    affine = np.eye(4)
+    return nibabel.nifti1.Nifti1Image(data, affine)
+
+@pytest.fixture(scope="session")
+def fake_4dimage_sct():
+    """
+    :return: an Image (4D) in RAS+ (aka SCT LPI) space
+    """
+    i = fake_4dimage()
+    img = msct_image.Image(i.get_data(), hdr=i.header,
+     orientation="LPI",
+     dim=i.header.get_data_shape(),
+    )
+    return img
 
 @pytest.fixture(scope="session")
 def fake_3dimage_sct():
@@ -151,6 +206,181 @@ def test_slicer(fake_3dimage_sct, fake_3dimage_sct2):
 
     with pytest.raises(ValueError):
         slicer = msct_image.Slicer(1)
+
+
+def test_nibabel(fake_3dimage):
+    img = fake_3dimage
+    print(img.header)
+
+
+def test_nibabel_reorient(fake_3dimage):
+    """
+    nibabel can reorient since recent versions,
+    it's not the best idea to use that feature yet.
+    """
+    return
+    src = fake_3dimage
+    ornt = (
+     [1, 1],
+     [2, 1],
+     [0,-1],
+    )
+    dst = src.as_reoriented(ornt)
+    print(dst.header)
+
+
+
+def test_change_orientation(fake_3dimage_sct, fake_3dimage_sct_vis):
+
+    path_tmp = sct.tmp_create(basename="test_reorient")
+    path_tmp = "."
+
+    print("Spot-checking that physical coordinates don't change")
+    for shape_is in (1,2,3):
+        shape = (1,1,shape_is)
+        print("Simple image with shape {}".format(shape))
+
+        data = np.ones(shape, order="F")
+        data[:,:,shape_is-1] += 1
+        im_src = fake_3dimage_sct_custom(data)
+        im_dst = msct_image.change_orientation(im_src, "ASR")
+        # Basic check
+        assert im_dst.orientation == "ASR"
+        # Basic data check
+        assert im_dst.data.mean() == im_src.data.mean()
+        # Basic header check: check that the same voxel
+        # remains at the same physical position
+        aff_src = im_src.header.get_best_affine()
+        aff_dst = im_dst.header.get_best_affine()
+
+        # Take the extremities "a" & "z"...
+        # consider the original LPI position
+        pta_src = np.array([[0,0,0,1]]).T
+        ptz_src = np.array([[0,0,shape_is-1,1]]).T
+        # and the position in ASR
+        pta_dst = np.array([[0,shape_is-1,0,1]]).T
+        ptz_dst = np.array([[0,0,0,1]]).T
+        # The physical positions should be:
+        posa_src = np.matmul(aff_src, pta_src)
+        posa_dst = np.matmul(aff_dst, pta_dst)
+        print("A at src {}".format(posa_src.T))
+        print("A at dst {}".format(posa_dst.T))
+        posz_src = np.matmul(aff_src, ptz_src)
+        posz_dst = np.matmul(aff_dst, ptz_dst)
+        # and they should be equal
+        assert (posa_src == posa_dst).all()
+        assert (posz_src == posz_dst).all()
+        fn = "".join(str(x) for x in im_src.data.shape)
+        im_src.save("{}-src.nii".format(fn))
+        im_dst.save("{}-dst.nii".format(fn))
+
+
+    print("More checking that physical coordinates don't change")
+    if 1:
+        shape = (7,8,9)
+        print("Simple image with shape {}".format(shape))
+
+        data = np.ones(shape, order="F")
+        data[4,4,4] = 4
+        data[3,3,3] = 3
+        values = (3,4)
+
+        im_ref = fake_3dimage_sct_custom(data)
+        affine = im_ref.header.get_best_affine()
+        affine[0:3,3] = (1000,2000,3000)
+        im_ref.header.set_sform(affine, code='scanner')
+
+        orientations = msct_image.all_refspace_strings()
+        for ori_src in orientations:
+            for ori_dst in orientations:
+                print("{} -> {}".format(ori_src, ori_dst))
+                im_src = msct_image.change_orientation(im_ref, ori_src)
+                im_dst = msct_image.change_orientation(im_src, ori_dst)
+
+                assert im_src.orientation == ori_src
+                assert im_dst.orientation == ori_dst
+                assert im_dst.data.mean() == im_src.data.mean()
+
+                # Basic header check: check that the same voxel
+                # remains at the same physical position
+                aff_src = im_src.header.get_best_affine()
+                aff_dst = im_dst.header.get_best_affine()
+
+                data_src = np.array(im_src.data)
+                data_dst = np.array(im_dst.data)
+
+                for value in values:
+                    pt_src = np.argwhere(data_src == value)[0]
+                    pt_dst = np.argwhere(data_dst == value)[0]
+
+                    pos_src = np.matmul(aff_src, np.hstack((pt_src, [1])).reshape((4,1)))
+                    pos_dst = np.matmul(aff_dst, np.hstack((pt_dst, [1])).reshape((4,1)))
+                    if 0:
+                        print("P at src {}".format(pos_src.T))
+                        print("P at dst {}".format(pos_dst.T))
+                    assert (pos_src == pos_dst).all()
+
+
+    im_src = fake_3dimage_sct.copy()
+    im_src.save(os.path.join(path_tmp, "src.nii"), mutable=True)
+
+    print(im_src.orientation, im_src.data.shape)
+
+    def orient2shape(orient):
+        # test-data-specific thing
+        letter2dim = dict(
+         L=7,
+         R=7,
+         A=8,
+         P=8,
+         I=9,
+         S=9,
+        )
+        return tuple([letter2dim[x] for x in orient])
+
+    orientation = im_src.orientation # LPI
+    assert im_src.header.get_best_affine()[:3,3].tolist() == [0,0,0]
+    im_dst = msct_image.change_orientation(im_src, "RPI")
+    print(im_dst.orientation, im_dst.data.shape)
+    assert im_dst.data.shape == orient2shape("RPI")
+    assert im_dst.header.get_best_affine()[:3,3].tolist() == [7-1,0,0]
+
+    # spot check
+    orientation = im_src.orientation # LPI
+    im_dst = msct_image.change_orientation(im_src, "IRP")
+    print(im_dst.orientation, im_dst.data.shape)
+    assert im_dst.data.shape == orient2shape("IRP")
+
+    # to & fro
+    im_dst2 = msct_image.change_orientation(im_dst, orientation)
+    print(im_dst2.orientation, im_dst2.data.shape)
+    assert im_dst2.orientation == im_src.orientation
+    assert im_dst2.data.shape == orient2shape(orientation)
+    assert (im_dst2.data == im_src.data).all()
+    assert im_dst2.header == im_src.header
+
+
+    # copy
+    im_dst = im_src.copy().change_orientation("IRP")
+    assert im_dst.data.shape == orient2shape("IRP")
+    print(im_dst.orientation, im_dst.data.shape)
+
+
+    print("Testing orientation persistence")
+    img = im_src.copy()
+    orientation = img.orientation
+    fn = os.path.join(path_tmp, "pouet.nii")
+    img.change_orientation("PIR").save(fn)
+    assert img.data.shape == orient2shape("PIR")
+    img = msct_image.Image(fn)
+    assert img.orientation == "PIR"
+    assert img.data.shape == orient2shape("PIR")
+    print(img.orientation, img.data.shape)
+
+    possibilities = msct_image.all_refspace_strings()
+    for orientation in possibilities:
+        dst = msct_image.change_orientation(im_src, orientation)
+        assert orientation == dst.orientation
 def test_change_shape(fake_3dimage_sct):
 
     # Add dimension
@@ -174,3 +404,6 @@ def test_change_shape(fake_3dimage_sct):
     # Remove dimension
     im_dst = im_dst.change_shape(im_src.data.shape)
     assert im_dst.data.shape == im_src.data.shape
+
+
+
