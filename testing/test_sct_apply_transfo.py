18d9e210da6897e5f0b009883913990f1838edad
Jérôme Carretero <cJ@zougloub.eu>
2018-07-23 19:00:26 -0400

    testing: sct_apply_transfo: add basic integrity checking
    Former-commit-id: e0cbd0baa146aa42ba3b5ab981bd05e8f17ca7d3

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -12,6 +12,11 @@
 
 # TODO: generate warping field for dmri that makes sense (dmri --> T2).
 
+import os
+import numpy as np
+import sct_utils as sct
+import msct_image
+
 def init(param_test):
     """
     Initialize class: param_test
@@ -31,5 +36,27 @@ def test_integrity(param_test):
     """
     Test integrity of function
     """
-    param_test.output += '\nNot implemented.'
+
+    fname_src = param_test.dict_args_with_path["-i"]
+    fname_ref = param_test.dict_args_with_path["-d"]
+    fname_dst = sct.add_suffix(os.path.basename(fname_src), "_reg")
+    #fname_dst = "output.nii.gz"
+    img_src = msct_image.Image(fname_src)
+    img_ref = msct_image.Image(fname_ref)
+    img_dst = msct_image.Image(fname_dst)
+
+    if img_dst.orientation != img_ref.orientation:
+        param_test.output += "\nImage has wrong orientation (%s -> %s)" \
+         % (img_ref.orientation, img_dst.orientation)
+        param_test.status = 1
+
+    if len(img_src.data.shape) > 3:
+        # Allowed failure for now
+        return param_test
+
+    if not (img_dst.data != 0).any():
+        param_test.output += "\nImage is garbage (all zeros)"
+        param_test.status = 1
+
+
     return param_test
