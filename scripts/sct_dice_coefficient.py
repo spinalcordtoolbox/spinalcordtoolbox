927d5b035ee9712110a338dca0127afe1ec2c22e
Jérôme Carretero <cJ@zougloub.eu>
2018-08-12 00:59:44 -0400

    sct_dice_coefficient: fixup access to unexisting variable
    Former-commit-id: cfdecefdf44299689e1d726dbbb8294a5923c29f

--- scripts/sct_dice_coefficient.py
+++ scripts/sct_dice_coefficient.py
@@ -87,8 +87,6 @@ if __name__ == "__main__":
     fname_input2 = arguments['-d']
 
     verbose = arguments['-v']
-    if verbose == '0':
-        cmd += ' -v '
 
     tmp_dir = sct.tmp_create(verbose=verbose)  # create tmp directory
     tmp_dir = os.path.abspath(tmp_dir)
