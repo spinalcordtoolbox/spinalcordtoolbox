cc8fb846ccf05a112298a53fc51c0c5a7972819d
Jérôme Carretero <cJ@zougloub.eu>
2018-08-20 09:42:46 -0400

    launcher: force scripts to not use graphical output (#2005)

--- spinalcordtoolbox/compat/launcher.py
+++ spinalcordtoolbox/compat/launcher.py
@@ -7,9 +7,15 @@ def main():
 	"""
 	Compatibility entry point to run scripts
 	"""
+
+	# Force scripts to not use graphical output
+	env = dict()
+	env.update(os.environ)
+	env["MPLBACKEND"] = "Agg"
+
 	command = os.path.basename(sys.argv[0])
 	sct_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 	script = os.path.join(sct_dir, "scripts", "{}.py".format(command))
 	assert os.path.exists(script)
 	cmd = [sys.executable, script] + sys.argv[1:]
-	return subprocess.call(cmd)
+	return subprocess.call(cmd, env=env)
