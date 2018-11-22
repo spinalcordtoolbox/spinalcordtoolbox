479efc71bd9395cc37ec7461c51c1b73a329f16c
charleygros <charley.gros@gmail.com>
2018-11-23 00:09:48 +1100

    add folder spinalcordtoolbox/vertebrae and script file detect_c2c3

---
+++ spinalcordtoolbox/vertebrae/detect_c2c3.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+# This is the interface API to detect the posterior edge of C2-C3 disc.
+# Author: charley
+# Copyright (c) 2018 Polytechnique Montreal <www.neuro.polymtl.ca>
+# About the license: see the file LICENSE.TXT
+
+from __future__ import absolute_import
+
+import os
+import sct_utils as sct
+import numpy as np
+
+
+from ..image import Image
+
+def detect_c2c3():
+    """
+    Detect the posterior edge of C2-C3 disc.
+    :param 
+    :param verbose:
+    :return:
+    """
+    pass
+
+
+def detect_c2c3_from_file():
+    """
+    Detect the posterior edge of C2-C3 disc.
+    :param fname_
+    :param verbose:
+    :return: fname_
+    """
+    # load data
+    sct.printv('Load data...', verbose)
+    pass
