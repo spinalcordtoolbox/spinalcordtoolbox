9c1d40610647ade0dd311433148cd1eaa27f24de
charleygros <charley.gros@gmail.com>
2018-11-29 00:37:43 +1100

    detection pipeline for c2c3 disc

--- spinalcordtoolbox/vertebrae/detect_c2c3.py
+++ spinalcordtoolbox/vertebrae/detect_c2c3.py
@@ -8,12 +8,15 @@ from __future__ import absolute_import
 
 import os
 import sct_utils as sct
+from sct_flatten_sagittal import flatten_sagittal
 import numpy as np
 import nibabel as nib
+from scipy.ndimage.measurements import center_of_mass
 
-from ..image import Image, zeros_like
+import spinalcordtoolbox.image as msct_image
+from spinalcordtoolbox.image import Image, zeros_like
 
-PATH_MODEL = 'XX'
+PATH_MODEL = '/home/charley/code/c2c3_disc/detect_c2c3/model_c2c3_20181117.yml'
 
 def detect_c2c3(nii_im, nii_seg, contrast, verbose=1):
     """
@@ -27,13 +30,16 @@ def detect_c2c3(nii_im, nii_seg, contrast, verbose=1):
     orientation_init = nii_im.orientation
 
     # Flatten sagittal
-    #### TODO refactor sct_flatten_sagittal to be able to call the function with matrices as input
+    nii_im = flatten_sagittal(nii_im, nii_seg, centerline_fitting='hanning', verbose=verbose)
+    nii_seg = flatten_sagittal(nii_seg, nii_seg, centerline_fitting='hanning', verbose=verbose)
 
     # Extract mid-slice
     nii_im.change_orientation('PIR')
+    nii_seg.change_orientation('PIR')
     mid_RL = int(np.rint(nii_im.dim[2] * 1.0 / 2))
     midSlice = nii_im.data[:, :, mid_RL]
-    nii_midSlice = zeros_like(nii_im)
+    midSlice_seg = nii_seg.data[:, :, mid_RL]
+    nii_midSlice = msct_image.zeros_like(nii_im)
     nii_midSlice.data = midSlice
     nii_midSlice.save('data_midSlice.nii')
 
@@ -43,23 +49,49 @@ def detect_c2c3(nii_im, nii_seg, contrast, verbose=1):
     cmd_detection = 'isct_spine_detect -ctype=dpdt "%s" "%s" "%s"' % \
                     (PATH_MODEL.split('.yml')[0], 'data_midSlice', 'data_midSlice_pred')
     os.system(cmd_detection)
-    img = nib.load('data_midSlice_pred_svm.hdr')
+    pred = nib.load('data_midSlice_pred_svm.hdr').get_data()
 
     # Create mask along centerline
-    # create_mask2d
+    midSlice_mask = np.zeros(midSlice_seg.shape)
+    mask_halfSize = 25
+    for z in range(midSlice_mask.shape[1]):
+        row = midSlice_seg[:, z]
+        if np.any(row):
+            med_y = int(np.rint(np.median(np.where(row))))
+            midSlice_mask[med_y-mask_halfSize:med_y+mask_halfSize] = 1
 
-    # remove img and hdr
+    # mask prediction
+    pred[midSlice_mask == 0] = 0
+
+    # assign label to voxel
+    nii_c2c3 = zeros_like(nii_seg)
+    if np.any(pred > 0):
+        sct.printv('C2-C3 detected...', verbose)
+        coord_max = np.where(pred == np.max(pred))
+        pa_c2c3, is_c2c3 = coord_max[0][0], coord_max[1][0]
+        rl_c2c3 = int(np.rint(center_of_mass(nii_seg.data[:, is_c2c3, :])[1]))
+        nii_c2c3.data[pa_c2c3, is_c2c3, rl_c2c3] = 3
+    else:
+        sct.printv('C2-C3 not detected...', verbose)
+
+    # remove temporary files
+    sct.rm('data_midSlice.nii')
+    sct.rm('data_midSlice_pred_svm.hdr')
+    sct.rm('data_midSlice_pred_svm.img')
+
+    nii_c2c3.change_orientation(orientation_init)
+    return nii_c2c3
 
 
-
-def detect_c2c3_from_file(fname_im, fname_seg, contrast, verbose=1):
+def detect_c2c3_from_file(fname_im, fname_seg, contrast, fname_c2c3=None, verbose=1):
     """
     Detect the posterior edge of C2-C3 disc.
     :param fname_im:
     :param fname_seg:
     :param contrast:
+    :param fname_c2c3:
     :param verbose:
-    :return: fname_
+    :return: fname_c2c3
     """
     # load data
     sct.printv('Load data...', verbose)
@@ -68,3 +100,12 @@ def detect_c2c3_from_file(fname_im, fname_seg, contrast, verbose=1):
 
     # detect C2-C3
     nii_c2c3 = detect_c2c3(nii_im, nii_seg, contrast, verbose=verbose)
+
+    # Output C2-C3 disc label
+    # by default, output in the same directory as the input images
+    sct.printv('Generate output file...', verbose)
+    if fname_c2c3 is None:
+        fname_c2c3 = os.path.join(os.path.dirname(nii_im.absolutepath), "label_c2c3.nii.gz")
+    nii_c2c3.save(fname_c2c3)
+
+    return fname_c2c3
