e95b18df71e97346268825ac2cb627d33fd9ff85
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-01-23 21:38:45 +0000

    nipy_resample.py: Refactored to make it a pure nipy-based function
    Also did some cleaning.
    Unfinished

--- spinalcordtoolbox/resample/nipy_resample.py
+++ spinalcordtoolbox/resample/nipy_resample.py
@@ -14,8 +14,6 @@
 
 from __future__ import division, absolute_import
 
-import copy
-
 import nipy
 import numpy as np
 from nipy.algorithms.registration.resample import resample as n_resample
@@ -23,77 +21,53 @@ from nipy.algorithms.registration.resample import resample as n_resample
 import sct_utils as sct
 
 
-def resample_image(input_image, new_size, new_size_type,
-                   interpolation='linear', verbose=1):
-    """This function will resample the specified input nipy image object to the target size.
-    Can deal with 2d, 3d or 4d image objects.
-    :param input_image: The input image.
-    :param new_size: The target size, i.e. '0.25x0.25'
-    :param new_size_type: Unit of resample (mm, vox, factor)
-    :param interpolation: The interpolation type
-    :param verbose: Verbosity level
-    :return: The resampled image.
+def resample_image(img, new_size, new_size_type, interpolation='linear', verbose=1):
+    """Resample a nipy image object based on a specified resampling factor.
+    Can deal with 2d, 3d or 4d image objects.  TODO: make test for that
+    :param img: nipy Image.
+    :param factor: list of float: Resampling factor. E.g., for 2x isotropic upsampling of a 3d image: factor=[2, 2, 2]
+    TODO: implement as list.
+    :param interpolation: {'nn', 'linear', 'spline'}. The interpolation type
+    :return: The resampled nipy Image.
     """
+    # TODO: deal with 4d (and other dim) data
+    # TODO: check that data dim == len(factor)
 
-    data = input_image.get_data()
     # Get dimensions of data
-    p = input_image.header.get_zooms()
-    n = input_image.header.get_data_shape()
-    sct.printv('  pixdim: ' + str(p), verbose)
-    sct.printv('  shape: ' + str(n), verbose)
+    p = img.header.get_zooms()
+    shape = img.header.get_data_shape()
 
-    # Calculate new dimensions
-    sct.printv('\nCalculate new dimensions...', verbose)
     # parse input argument
     new_size = new_size.split('x')
-    # if 4d, add resampling factor to 4th dimension
-    if len(p) == 4:
-        new_size.append('1')
+
     # compute new shape based on specific resampling method
     if new_size_type == 'vox':
-        n_r = tuple([int(new_size[i]) for i in range(len(n))])
+        shape_r = tuple([int(new_size[i]) for i in range(img.ndim)])
     elif new_size_type == 'factor':
         if len(new_size) == 1:
             # isotropic resampling
-            new_size = tuple([new_size[0] for i in range(len(n))])
-        # compute new shape as: n_r = n * f
-        n_r = tuple([int(np.round(n[i] * float(new_size[i]))) for i in range(len(n))])
+            new_size = tuple([new_size[0] for i in range(img.ndim)])
+        # compute new shape as: shape_r = shape * f
+        shape_r = tuple([int(np.round(shape[i] * float(new_size[i]))) for i in range(img.ndim)])
     elif new_size_type == 'mm':
         if len(new_size) == 1:
             # isotropic resampling
-            new_size = tuple([new_size[0] for i in range(len(n))])
-        # compute new shape as: n_r = n * (p_r / p)
-        n_r = tuple([int(np.round(n[i] * float(p[i]) / float(new_size[i]))) for i in range(len(n))])
+            new_size = tuple([new_size[0] for i in range(img.ndim)])
+        # compute new shape as: shape_r = shape * (p_r / p)
+        shape_r = tuple([int(np.round(shape[i] * float(p[i]) / float(new_size[i]))) for i in range(img.ndim)])
     else:
-        sct.printv('\nERROR: new_size_type is not recognized.', 1, 'error')
-    sct.printv('  new shape: ' + str(n_r), verbose)
-
-    affine = input_image.coordmap.affine
-    sct.printv('  affine matrix: \n' + str(affine), verbose)
+        sct.log.error('new_size_type is not recognized.')
 
     # create ref image
-    arr_r = np.zeros(n_r)
-    R = np.eye(len(n) + 1)
-    for i in range(len(n)):
-        R[i, i] = n[i] / float(n_r[i])
+    affine = img.affine
+    sct.log.debug('Affine matrix: \n' + str(affine), verbose)
+    R = np.eye(img.ndim + 1)
+    for i in range(img.ndim):
+        R[i, i] = img.shape[i] / float(shape_r[i])
     affine_r = np.dot(affine, R)
-    coordmap_r = input_image.coordmap
-    coordmap_r.affine = affine_r
-    nii_r = nipy.core.api.Image(arr_r, coordmap_r)
-
-    sct.printv('\nCalculate affine transformation...', verbose)
-    # create affine transformation
-    transfo = R
-    # if data are 4d, delete temporal dimension
-    if len(p) == 4:
-        transfo = np.delete(transfo, 3, 0)
-        transfo = np.delete(transfo, 3, 1)
-    # translate to account for voxel size (otherwise resulting image will be shifted by half a voxel). Modify the three
-    # first rows of the last column, corresponding to the translation.
-    transfo[:3, -1] = np.array(((R[0, 0] - 1) / 2, (R[1, 1] - 1) / 2, (R[2, 2] - 1) / 2), dtype='f8')
-    sct.printv('  transfo: \n' + str(transfo), verbose)
 
     # set interpolation method
+    # TODO: make a dict
     if interpolation == 'nn':
         interp_order = 0
     elif interpolation == 'linear':
@@ -101,41 +75,10 @@ def resample_image(input_image, new_size, new_size_type,
     elif interpolation == 'spline':
         interp_order = 2
 
-    # create 3d coordmap because resample only accepts 3d data (jcohenadad 2016-07-26)
-    if len(n) == 4:
-        coordmap3d = copy.deepcopy(input_image.coordmap)
-        from nipy.core.reference.coordinate_system import CoordinateSystem
-        coordmap3d.function_domain = CoordinateSystem('xyz')
-        # create 3d affine transfo
-        affine3d = np.delete(affine, 3, 0)
-        affine3d = np.delete(affine3d, 3, 1)
-        coordmap3d.affine = affine3d
+    img_r = n_resample(img, transform=R, reference=(shape_r, affine_r), mov_voxel_coords=True, ref_voxel_coords=True,
+                       dtype='double', interp_order=interp_order, mode='nearest')
 
-    # resample data
-    if len(n) == 3:
-        data_r = n_resample(input_image, transform=transfo, reference=nii_r, mov_voxel_coords=True,
-                          ref_voxel_coords=True, dtype='double', interp_order=interp_order,
-                          mode='nearest')
-    elif len(n) == 4:
-        data_r = np.zeros(n_r)
-
-        # loop across 4th dimension
-        for it in range(n[3]):
-            # create 3d nipy-like data
-            arr3d = data[:, :, :, it]
-            nii3d = nipy.core.api.Image(arr3d, coordmap3d)
-            arr_r3d = arr_r[:, :, :, it]
-            nii_r3d = nipy.core.api.Image(arr_r3d, coordmap3d)
-            # resample data
-            data3d_r = n_resample(nii3d, transform=transfo, reference=nii_r3d,
-                                mov_voxel_coords=True, ref_voxel_coords=True,
-                                dtype='double', interp_order=interp_order,
-                                mode='nearest')
-            data_r[:, :, :, it] = data3d_r.get_data()
-
-    nii_r = nipy.core.api.Image(data_r, coordmap_r)
-
-    return nii_r
+    return img_r
 
 
 def resample_file(fname_data, fname_out, new_size, new_size_type,
