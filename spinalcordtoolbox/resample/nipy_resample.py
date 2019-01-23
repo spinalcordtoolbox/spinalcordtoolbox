a00365095b5de1eecf6130b913586236a3e41095
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-01-23 23:32:48 +0000

    nipy_resample: Generalized to 4d and fixed compatibility

--- spinalcordtoolbox/resample/nipy_resample.py
+++ spinalcordtoolbox/resample/nipy_resample.py
@@ -21,7 +21,7 @@ from nipy.algorithms.registration.resample import resample as n_resample
 import sct_utils as sct
 
 
-def resample_image(img, new_size, new_size_type, interpolation='linear', verbose=1):
+def resample_nipy(img, new_size, new_size_type, interpolation='linear', verbose=1):
     """Resample a nipy image object based on a specified resampling factor.
     Can deal with 2d, 3d or 4d image objects.  TODO: make test for that
     :param img: nipy Image.
@@ -31,7 +31,6 @@ def resample_image(img, new_size, new_size_type, interpolation='linear', verbose
     :return: The resampled nipy Image.
     """
     # TODO: deal with 4d (and other dim) data
-    # TODO: check that data dim == len(factor)
 
     # Get dimensions of data
     p = img.header.get_zooms()
@@ -39,6 +38,11 @@ def resample_image(img, new_size, new_size_type, interpolation='linear', verbose
 
     # parse input argument
     new_size = new_size.split('x')
+
+    if img.ndim == 4:
+        new_size += ['1']  # needed because the code below is general, i.e., does not assume 3d input and uses img.shape
+
+    # assert len(shape) == len(new_size)
 
     # compute new shape based on specific resampling method
     if new_size_type == 'vox':
@@ -58,11 +62,12 @@ def resample_image(img, new_size, new_size_type, interpolation='linear', verbose
     else:
         sct.log.error('new_size_type is not recognized.')
 
-    # create ref image
-    affine = img.affine
+    # Generate 3d affine transformation: R
+    affine = img.affine[:4, :4]
+    affine[3, :] = np.array([0, 0, 0, 1])  # satisfy to nifti convention. Otherwise it grabs the temporal
     sct.log.debug('Affine matrix: \n' + str(affine), verbose)
-    R = np.eye(img.ndim + 1)
-    for i in range(img.ndim):
+    R = np.eye(4)
+    for i in range(3):
         R[i, i] = img.shape[i] / float(shape_r[i])
     affine_r = np.dot(affine, R)
 
@@ -75,14 +80,31 @@ def resample_image(img, new_size, new_size_type, interpolation='linear', verbose
     elif interpolation == 'spline':
         interp_order = 2
 
-    img_r = n_resample(img, transform=R, reference=(shape_r, affine_r), mov_voxel_coords=True, ref_voxel_coords=True,
-                       dtype='double', interp_order=interp_order, mode='nearest')
+    if img.ndim == 3:
+        img_r = n_resample(img, transform=R, reference=(shape_r, affine_r), mov_voxel_coords=True,
+                           ref_voxel_coords=True, dtype='double', interp_order=interp_order, mode='nearest')
+    elif img.ndim == 4:
+        # Import here instead of top of the file because this is an isolated case and nibabel takes time to import
+        import nibabel as nib
+        from nipy.io.nifti_ref import nifti2nipy, nipy2nifti
+        data4d = np.zeros(shape_r)
+        # Loop across 4th dimension and resample each 3d volume
+        for it in range(img.shape[3]):
+            # Create dummy 3d nipy image
+            nii_tmp = nib.nifti1.Nifti1Image(img.get_data()[..., it], affine)
+            img3d_r = n_resample(nifti2nipy(nii_tmp), transform=R, reference=(shape_r[:-1], affine_r),
+                                 mov_voxel_coords=True, ref_voxel_coords=True, dtype='double',
+                                 interp_order=interp_order, mode='nearest')
+            data4d[..., it] = img3d_r.get_data()
+        # Create 4d nipy Image
+        nii4d = nib.nifti1.Nifti1Image(data4d, affine_r)
+        # Convert to nipy object
+        img_r = nifti2nipy(nii4d)
 
     return img_r
 
 
-def resample_file(fname_data, fname_out, new_size, new_size_type,
-                  interpolation, verbose):
+def resample_file(fname_data, fname_out, new_size, new_size_type, interpolation, verbose):
     """This function will resample the specified input
     image file to the target size.
     Can deal with 2d, 3d or 4d image objects.
@@ -98,8 +120,8 @@ def resample_file(fname_data, fname_out, new_size, new_size_type,
     sct.printv('\nLoad data...', verbose)
     nii = nipy.load_image(fname_data)
 
-    nii_r = resample_image(nii, new_size, new_size_type, 
-                           interpolation, verbose)
+    nii_r = resample_nipy(nii, new_size, new_size_type,
+                          interpolation, verbose)
 
     # build output file name
     if fname_out == '':
