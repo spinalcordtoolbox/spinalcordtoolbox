f6a4d6f58b0f94ff538eaaf8918e582dc3e61c53
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-01-23 23:32:48 +0000

    nipy_resample: Generalized to 4d and fixed compatibility
    Former-commit-id: 4973b05bc46983ee980dae2c8f6fcfc9e7fff847 [formerly c46f9d7e8229345517c4818ce036a294f42832ba [formerly a00365095b5de1eecf6130b913586236a3e41095]]
    Former-commit-id: 0c1796db87e3ccd0ebb751933f202031a0f04486
    Former-commit-id: eb0bf7e1407ec73639066192a768b9b2a20fa313

--- unit_testing/test_resample.py
+++ unit_testing/test_resample.py
@@ -9,7 +9,7 @@ import pytest
 
 import numpy as np
 import nibabel as nib
-from nipy.io.nifti_ref import nifti2nipy
+from nipy.io.nifti_ref import nifti2nipy, nipy2nifti
 
 from spinalcordtoolbox.resample import nipy_resample
 
@@ -29,10 +29,37 @@ def fake_3dimage_nipy():
     return nifti2nipy(nii)
 
 
-# noinspection 801,PyShadowingNames
-def test_nipy_resample_image(fake_3dimage_nipy):
-    """Test resampling with 3D nipy image"""
+@pytest.fixture(scope="session")
+def fake_4dimage_nipy():
+    """
+    :return: an empty 4-d nipy Image
+    """
+    nx, ny, nz, nt = 9, 9, 9, 3  # image dimension
+    data = np.zeros((nx, ny, nz, nt), dtype=np.int8)
+    data[4, 4, 4, 0] = 1.
+    affine = np.eye(4)
+    # Create nibabel object
+    nii = nib.nifti1.Nifti1Image(data, affine)
+    # return nipy object
+    return nifti2nipy(nii)
 
-    img_r = nipy_resample.resample_image(fake_3dimage_nipy, new_size='2x2x1', new_size_type='factor', interpolation='nn')
+
+# noinspection 801,PyShadowingNames
+def test_nipy_resample_image_3d(fake_3dimage_nipy):
+    """Test resampling with 3D nipy image"""
+    img_r = nipy_resample.resample_nipy(fake_3dimage_nipy, new_size='2x2x1', new_size_type='factor', interpolation='nn')
     assert img_r.get_data().shape == (18, 18, 9)
     assert img_r.get_data()[8, 8, 4] == 1.0  # make sure there is no displacement in world coordinate system
+    assert nipy2nifti(img_r).header.get_zooms() == (0.5, 0.5, 1.0)
+    # debug
+    # nib.save(nipy2nifti(img_r), 'test_4.nii.gz')
+
+
+# noinspection 801,PyShadowingNames
+def test_nipy_resample_image_4d(fake_4dimage_nipy):
+    """Test resampling with 4D nipy image"""
+    img_r = nipy_resample.resample_nipy(fake_4dimage_nipy, new_size='2x2x1x1', new_size_type='factor', interpolation='nn')
+    assert img_r.get_data().shape == (18, 18, 9, 3)
+    assert img_r.get_data()[8, 8, 4, 0] == 1.0  # make sure there is no displacement in world coordinate system
+    assert img_r.get_data()[8, 8, 4, 1] == 0.0
+    assert nipy2nifti(img_r).header.get_zooms() == (0.5, 0.5, 1.0, 1.0)
