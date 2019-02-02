a0ec0b86e3d8dec499331102c5a6bd256c59a5a1
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-02-02 15:42:53 -0500

    unit_testing.sh: Added shebang line

--- unit_testing.sh
+++ unit_testing.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 source $HOME/build/neuropoly/spinalcordtoolbox/python/bin/activate
 export PYTHONPATH=$PYTHONPATH:$SCT_DIR/scripts
 pytest -v --cov-append --cov=spinalcordtoolbox --cov=scripts unit_testing/*
