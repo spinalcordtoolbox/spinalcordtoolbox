12d1421f04b461d522da739ebffc4d63fc0d939d
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-02-03 12:27:33 -0500

    unit_testing.sh: Now using relative path for SCT python

--- unit_testing.sh
+++ unit_testing.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Only used for Travis due to hard-coded paths
-source $HOME/build/neuropoly/spinalcordtoolbox/python/bin/activate
+source python/bin/activate
 export PYTHONPATH=$PYTHONPATH:$SCT_DIR/scripts
 pytest -v --cov-append --cov=spinalcordtoolbox --cov=scripts unit_testing/*
