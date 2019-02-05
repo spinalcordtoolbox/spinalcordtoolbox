106623df16b750a5f1ff0af37e9bcb75836b95aa
charleygros <charley.gros@gmail.com>
2019-02-05 21:39:10 +1100

    test_deepseg_sc: adjust size of fake sc

--- unit_testing/test_deepseg_sc.py
+++ unit_testing/test_deepseg_sc.py
@@ -17,7 +17,7 @@ def _create_fake_t2_sc(input_size):
 
     for zz in range(data.shape[2]):
         data[:,:,zz] += np.logical_and(circle < 400, circle >= 200) * 250 # CSF
-        data[:,:,zz] += (circle < 200) * 70 # SC
+        data[:,:,zz] += (circle < 200) * 100 # SC
         # Note: values of the fake SC and CSF have been chosen by looking at the normalized intensities of im_norm_in (cf deepseg_sc/core)
 
     affine = np.eye(4)
@@ -31,14 +31,14 @@ def test_segment_2d():
     contrast_test = 't2'
     model_path = os.path.join(sct.__sct_dir__, 'data', 'deepseg_sc_models', '{}_sc.h5'.format(contrast_test))   
 
-    img = _create_fake_t2_sc((48,48,1))
-    seg = deepseg_sc.segment_2d(model_path, contrast_test, (48, 48), img)
+    img = _create_fake_t2_sc((64,64,1))
+    seg = deepseg_sc.segment_2d(model_path, contrast_test, (64, 64), img)
     
     seg_im = msct_image.zeros_like(img)
     seg_gt_im = seg_im.copy()
-    seg_gt_im.data = (img.data == 70)
+    seg_gt_im.data = (img.data == 100)
     seg_im.data = seg
 
-    assert np.any(seg[img.data == 70]) == True  # check if SC detected
-    assert np.any(seg[img.data != 70]) == False  # check if no FP
+    assert np.any(seg[img.data == 100]) == True  # check if SC detected
+    assert np.any(seg[img.data != 100]) == False  # check if no FP
     assert msct_image.compute_dice(seg_im, seg_gt_im) > 0.80
