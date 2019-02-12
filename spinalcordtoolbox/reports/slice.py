990f9febf9e18a9633ffaa6b217a7a747e236aeb
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-02-12 17:03:17 -0500

    reports/slice: Fixed issue with n>2 dim of self._image
    Former-commit-id: 7a6db6b97049edbaa103a23a2073315197015d7b [formerly b693747eadbfe9345e2678cc6031f81621277423 [formerly 9232f2558ec4bbf0944300a542cc0cd89557c74d]]
    Former-commit-id: ac9ad6411bb9eb02fcf522aef82bde8c3c858096
    Former-commit-id: d9ff67efde54a666ea47634ce7cfb3eff1da3541

--- spinalcordtoolbox/reports/slice.py
+++ spinalcordtoolbox/reports/slice.py
@@ -227,8 +227,9 @@ class Slice(object):
         # TODO: interp_type should ideally be ['spline', 'nn'], however i noticed a slight translation between nn and
         #   spline (or linear), hence this is problematic for comparing the overlay of the segmentation on the image.
         #   Until we correct this translation, we should use the same interpolation for the segmentation and the image.
-        interp_type = ['nn', 'nn']  # because self._image[0,1] is [image, segmentation]
-        for i in [0, 1]:
+        n_images = len(self._images)
+        interp_type = n_images * ['nn']  # because self._image[] is [image, segmentation] (or [im, im, seg])
+        for i in range(n_images):
             image = self._images[i]
             # Create nibabel object
             nii = Nifti1Image(image.data, image.hdr.get_base_affine())
