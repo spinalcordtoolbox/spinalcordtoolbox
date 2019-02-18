98ea79ac40f0dd26da8f638fefb4406b42e90554
charleygros <charley.gros@gmail.com>
2019-02-18 15:19:05 +1100

    test_deepseg_sc: test_segment_2d

--- unit_testing/test_deepseg_sc.py
+++ unit_testing/test_deepseg_sc.py
@@ -7,7 +7,7 @@ import sct_utils as sct
 from spinalcordtoolbox.image import Image
 import spinalcordtoolbox.image as msct_image
 from spinalcordtoolbox.deepseg_sc import core as deepseg_sc
-
+from spinalcordtoolbox.resample.nipy_resample import resample_file
 
 def _create_fake_t2_sc(input_size):
     data = np.zeros(input_size)
@@ -33,17 +33,60 @@ def test_segment_2d():
     contrast_test = 't2'
     model_path = os.path.join(sct.__sct_dir__, 'data', 'deepseg_sc_models', '{}_sc.h5'.format(contrast_test))   
 
-    img = _create_fake_t2_sc((64,64,1))
-    seg = deepseg_sc.segment_2d(model_fname=model_path, contrast_type=contrast_test, input_size=(64,64), im_in=img)
-    
-    seg_im = msct_image.zeros_like(img)
-    seg_gt_im = seg_im.copy()
-    seg_gt_im.data = (img.data == 100)
-    seg_im.data = seg
+    fname_t2 = os.path.join(sct.__sct_dir__, 'sct_testing_data/t2/t2.nii.gz')  # install: sct_download_data -d sct_testing_data
+    fname_t2_seg = os.path.join(sct.__sct_dir__, 'sct_testing_data/t2/t2_seg.nii.gz')  # install: sct_download_data -d sct_testing_data
 
-    assert np.any(seg[img.data == 100]) == True  # check if SC detected
-    assert np.any(seg[img.data != 100]) == False  # check if no FP
-    assert msct_image.compute_dice(seg_im, seg_gt_im) > 0.80
+    tmp_folder = sct.TempFolder()
+    tmp_folder_path = tmp_folder.get_path()
+    tmp_folder.chdir()
+
+    img = Image(fname_t2)
+    gt = Image(fname_t2_seg)
+
+    fname_t2_RPI, fname_t2_seg_RPI = 'img_RPI.nii.gz', 'seg_RPI.nii.gz'
+    img.change_orientation('RPI').save(fname_t2_RPI)
+    gt.change_orientation('RPI').save(fname_t2_seg_RPI)
+    input_resolution = gt.dim[4:7]
+    del img, gt
+
+    fname_res, fname_ctr = deepseg_sc.find_centerline(algo='svm',
+                                                        image_fname=fname_t2_RPI,
+                                                        contrast_type=contrast_test,
+                                                        brain_bool=False,
+                                                        folder_output=tmp_folder_path,
+                                                        remove_temp_files=1,
+                                                        centerline_fname=None)
+
+    fname_t2_seg_RPI_res = 'seg_RPI_res.nii.gz'
+    new_resolution = 'x'.join(['0.5', '0.5', str(input_resolution[2])])
+    resample_file(fname_t2_seg_RPI, fname_t2_seg_RPI_res, new_resolution, 'mm', 'linear', verbose=0)
+
+    img, ctr, gt = Image(fname_res), Image(fname_ctr), Image(fname_t2_seg_RPI_res)
+    _, _, img = deepseg_sc.crop_image_around_centerline(im_in=img,
+                                                        ctr_in=ctr,
+                                                        crop_size=64)
+    _, _, gt = deepseg_sc.crop_image_around_centerline(im_in=gt,
+                                                        ctr_in=ctr,
+                                                        crop_size=64)
+    del ctr
+
+    img = deepseg_sc.apply_intensity_normalization(im_in=img)
+
+    seg = deepseg_sc.segment_2d(model_fname=model_path, contrast_type=contrast_test, input_size=(64,64), im_in=img)
+    seg_im = msct_image.zeros_like(img)
+    seg_im.data = seg
+    print msct_image.compute_dice(seg_im, gt)
+    
+    tmp_folder.chdir_undo()
+    tmp_folder.cleanup()
+    # seg_im = msct_image.zeros_like(img)
+    # seg_gt_im = seg_im.copy()
+    # seg_gt_im.data = (img.data == 100)
+    # seg_im.data = seg
+
+    # assert np.any(seg[img.data == 100]) == True  # check if SC detected
+    # assert np.any(seg[img.data != 100]) == False  # check if no FP
+    assert msct_image.compute_dice(seg_im, gt) > 0.80
 
 
 def test_intensity_normalization():
