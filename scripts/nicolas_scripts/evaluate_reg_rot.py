dc86ea52f3d39907a220ac3b95210c4d153b73de
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-04-17 16:08:41 -0400

    implemented reg seg to temp and dice computation

--- scripts/nicolas_scripts/evaluate_reg_rot.py
+++ scripts/nicolas_scripts/evaluate_reg_rot.py
@@ -2,10 +2,14 @@
 
 # Script used to process one MRI image at a time (the image and its segmentation), made to be used with wrapper or alone
 
-import sys, sct_utils as sct
+import sys, os
+import sct_utils as sct
 from msct_parser import Parser
 from sct_register_to_template import main as sct_register_to_template
 from sct_label_vertebrae import main as sct_label_vertebrae
+from sct_apply_transfo import main as sct_apply_transfo
+from sct_maths import main as sct_maths
+from nicolas_scripts.functions_sym_rot import *
 
 def get_parser():
 
@@ -40,6 +44,8 @@ def main(args=None):
     fname_seg = arguments['-iseg']
     output_dir = arguments['-o']
 
+    fname_seg_template = os.path.join(sct.__data_dir__, 'PAM50/template/PAM50_cord.nii.gz')
+
     sct.printv("        Python processing file : " + fname_image + " with seg : " + fname_seg)
 
     # Determining contrast :
@@ -62,6 +68,26 @@ def main(args=None):
          output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_labeled.nii.gz", '-ofolder', output_dir, '-param',
          "step=1,type=seg,algo=centermassrot,poly=0,slicewise=1,rot_method=PCA", '-v', '0'])
 
+    # Applying warping field to segmentation
+    sct_apply_transfo(['-i', fname_seg, '-d', fname_seg_template, '-w', output_dir + "/warp_anat2template.nii.gz", '-o', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg.nii.gz"])
+    sct_maths(['-i', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg.nii.gz", '-bin', '0.5', '-o', output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg_tresh.nii.gz"])
+
+    data_seg_reg = Image(output_dir + "/" + (fname_seg.split("/")[-1]).split(".nii.gz")[0] + "_reg_tresh.nii.gz").data
+    data_seg_template = Image(fname_seg_template).data
+    min_z = np.min(np.nonzero(data_seg_reg)[2])
+    max_z = np.max(np.nonzero(data_seg_reg)[2])
+
+    dice_slice = []
+    dice_glob = compute_similarity_metric(data_seg_reg[:, :, min_z:max_z], data_seg_template[:, :, min_z:max_z], metric="Dice")
+
+    for z in range(min_z, max_z):
+        dice_slice.append(compute_similarity_metric(data_seg_reg[:, :, z], data_seg_template[:, :, z], metric="Dice"))
+
+    dice_slice_min = min(dice_slice)
+    dice_slice_max = max(dice_slice)
+    dice_slice_mean = np.mean(dice_slice)
+
+    1
 
     #  TODO : write out dice scores as txt ? then func to agregate them
 
