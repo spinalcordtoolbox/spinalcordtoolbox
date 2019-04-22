9ea4fc4bb69d5e60f40d17b736938407edb5b031
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-04-22 15:45:20 -0400

    added display and saving of histograms

--- scripts/agregate_metrics.py
+++ scripts/agregate_metrics.py
@@ -8,6 +8,9 @@ from msct_parser import Parser
 import shutil
 import fnmatch
 import csv
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 
 def get_parser():
 
@@ -34,6 +37,13 @@ def main(args=None):
 
     # Loading all csv files :
 
+    dice_glob = []
+    dice_mean = []
+    dice_min = []
+    dice_max = []
+    dice_std = []
+    nb_subjects = 0
+
     for root, dirnames, filenames in os.walk(folder):  # searching the given directory
         # Search for the csv files
         for filename in fnmatch.filter(filenames, "*.csv"):  # if file with nii extension (.nii or .nii.gz) found
@@ -41,16 +51,51 @@ def main(args=None):
             with open(os.path.join(root, filename), 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 metric_dic = {rows[0]: float(rows[1]) for rows in reader}
+                dice_glob.append(metric_dic['Dice global'])
+                dice_mean.append(metric_dic['Mean Dice per slice'])
+                dice_min.append(metric_dic['Min Dice'])
+                dice_max.append(metric_dic['Max Dice'])
+                dice_std.append(metric_dic['STD Dice'])
+                nb_subjects += 1
+
 
     # Cleaning everything :
     # shutil.rmtree(folder)
     # os.mkdir(folder)
 
     # Processing data
-
-    1
+    # matplotlib.use('Agg')  # prevent display figure
+    fig = plt.figure(figsize=(8, 10))
+    fig.suptitle("Histograms of dataset with " + str(nb_subjects) + " images")
+    plt.subplot(231)
+    plt.hist(dice_glob, bins=10, range=(0, 1))
+    plt.title("Global dice histogram")
+    plt.xlabel("dice score")
+    plt.ylabel("count")
+    plt.subplot(232)
+    plt.hist(dice_mean, bins=10, range=(0, 1))
+    plt.title("Mean dice histogram")
+    plt.xlabel("dice score")
+    plt.ylabel("count")
+    plt.subplot(233)
+    plt.hist(dice_min, bins=10, range=(0, 1))
+    plt.title("Min dice histogram")
+    plt.xlabel("dice score")
+    plt.ylabel("count")
+    plt.subplot(234)
+    plt.hist(dice_max, bins=10, range=(0, 1))
+    plt.title("Max dice histogram")
+    plt.xlabel("dice score")
+    plt.ylabel("count")
+    plt.subplot(235)
+    plt.hist(dice_glob, bins=10)
+    plt.title("STD dice histogram")
+    plt.xlabel("dice score")
+    plt.ylabel("count")
 
     # Saving everything :
+
+    plt.savefig(folder + "/histograms.png")
 
 
 
