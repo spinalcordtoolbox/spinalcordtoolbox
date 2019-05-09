73fc41bdc4ef08b514af58fdd5067d52c90520ee
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-05-09 01:20:21 -0400

    minor fix

--- scripts/evaluate_reg_rot.py
+++ scripts/evaluate_reg_rot.py
@@ -74,7 +74,7 @@ def main(args=None):
 
     # Applying same process but for different methods :
 
-    for method in ["NoRot", "PCA", "HOG"]:
+    for method in ["NoRot", "pca", "pca"]:
 
         sct.printv("\n\n Registration with " + method)
 
