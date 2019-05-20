b9f341caeb32f432a6db712a4de957c8e5c36001
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-05-20 15:22:26 -0400

    added echo in the process

--- process_data_nipin_reg.sh
+++ process_data_nipin_reg.sh
@@ -24,17 +24,17 @@ do
   if [[ $file == *"_seg"* ]];then #file is a seg
     continue
   else # file is an image
-  echo "Processing $file file.."
+  echo " ======> Processing $file file.."
   file_seg="${file%.nii.gz*}_seg.nii.gz"
   file_seg_manual="${file%.nii.gz*}_seg_manual.nii.gz"
     if [ -e $file_seg ]; then #segmentation of this file exist
-      echo "   Processing file $file with seg $file_seg"
+      echo "======> Processing file $file with seg $file_seg"
       evaluate_reg -i $file -iseg $file_seg -o $PATH_RESULTS
     elif [ -e $file_seg_manual ]; then #manual segmentation of this file exist
-      echo "   Processing file $file with seg $file_seg_manual"
+      echo "======> Processing file $file with seg $file_seg_manual"
       evaluate_reg -i $file -iseg $file_seg -o $PATH_RESULTS
     else
-      echo "Segmentation for file $file does not exist, segmenting with sct_deepseg_sc"
+      echo "======> Segmentation for file $file does not exist, segmenting with sct_deepseg_sc"
       if [[ $file == *"T1w"* ]]; then
         contrast="t1"
       elif [[ $file == *"T2w"* ]]; then
@@ -42,10 +42,11 @@ do
       elif [[ $file == *"T2s"* ]]; then
         contrast="t2s"
       else
-        echo "Contrast for file $file not found or not supported"
+        echo "======> Contrast for file $file not found or not supported"
         continue
       fi
       sct_deepseg_sc -i $file -c $contrast -ofolder $PATH_RESULTS
+      echo "======> evaluate_reg with file $file and seg ${PATH_RESULTS}/${file_seg##*/}"
       evaluate_reg -i $file -iseg "${PATH_RESULTS}/${file_seg##*/}" -o $PATH_RESULTS
     fi
   fi
