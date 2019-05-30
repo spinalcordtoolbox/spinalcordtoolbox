completely modified gradient histogram computation (fix)

scripts/nicolas_scripts/functions_sym_rot.py
@@ -98,7 +98,7 @@ def find_angle(image, segmentation, px, py, method, angle_range=None, return_cen
         parameters = {}
         parameters['sigmax'] = 10 / px
         parameters['sigmay'] = 10 / py
-        parameters['nb_bin'] = 360
+        parameters['nb_bin'] = 90
         parameters['kmedian_size'] = 3
         parameters['angle_range'] = angle_range
         parameters['confidence_threshold'] = 1.1
@@ -128,15 +128,11 @@ def find_angle_hog(image, centermass, parameters, return_proba_map=False):
     sigmax = parameters['sigmax']  # TODO change this as a class not a dictionnary
     sigmay = parameters['sigmay']
     nb_bin = parameters['nb_bin']
+    if nb_bin % 2 != 0:
+        nb_bin = nb_bin - 1
     kmedian_size = parameters['kmedian_size']
     angle_range = parameters['angle_range']
     confidence_treshold = parameters['confidence_threshold']
-
-    if angle_range is None:
-        mode = "wrap"
-        angle_range = 90
-    else:
-        mode = "clip"
 
     nx, ny = image.shape
 
@@ -144,32 +140,68 @@ def find_angle_hog(image, centermass, parameters, return_proba_map=False):
     seg_weighted_mask = np.exp(
         -(((xx - centermass[0]) ** 2) / (2 * (sigmax ** 2)) + ((yy - centermass[1]) ** 2) / (2 * (sigmay ** 2))))
 
-    hog_ancest, proba_map = hog_ancestor(image, nb_bin=nb_bin, seg_weighted_mask=seg_weighted_mask, return_image=True)
-    hog_ancest_smooth = circular_filter_1d(hog_ancest, kmedian_size,
-                                           filter='median')  # fft than square than ifft to calculate convolution
-    hog_fft2 = np.fft.rfft(hog_ancest_smooth) ** 2
-    hog_conv = np.real(np.fft.irfft(hog_fft2))
+    grad_orient_histo, proba_map, orient_image = gradient_orientation_histogram(image, nb_bin=nb_bin, seg_weighted_mask=seg_weighted_mask, return_image=True, return_orient=True)
 
-    hog_conv_reordered = np.zeros(nb_bin)
-    hog_conv_reordered[0:180] = hog_conv[180:360]
-    hog_conv_reordered[180:360] = hog_conv[0:180]
-    hog_conv_restrained = hog_conv_reordered[
-                          int(nb_bin / 2 - np.true_divide(angle_range, 180) * nb_bin):int(nb_bin / 2 + np.true_divide(
-                              angle_range, 180) * nb_bin)]
+    edges_hist = np.linspace(-(pi-pi/nb_bin), (pi-pi/nb_bin), nb_bin)
+    repr_hist = np.linspace(-(pi-2*pi/nb_bin), (pi-2*pi/nb_bin), nb_bin-1)
 
-    argmaxs = argrelmax(hog_conv_restrained, mode=mode, order=kmedian_size)[0]  # get local maxima
-    argmaxs_sorted = np.asarray([tutut for _, tutut in
-                                 sorted(zip(hog_conv_restrained[argmaxs], argmaxs),
-                                        reverse=True)])  # sort maxima based on value
-    angles_sorted = (argmaxs_sorted - nb_bin / 2) * np.true_divide(180, nb_bin * angle_range)  # angles are computed from -angle_range to angle_range
+    grad_orient_histo_smooth = circular_filter_1d(grad_orient_histo, kmedian_size, filter='median')  # fft than square than ifft to calculate convolution
 
-    confidence_score = hog_conv_restrained[argmaxs_sorted[0]]/np.mean(hog_conv_restrained)
-    sct.printv("confidence score is : " + str(confidence_score))
+    # hog_fft2 = np.fft.rfft(grad_orient_histo_smooth) ** 2
+    # grad_orient_histo_conv = np.real(np.fft.irfft(hog_fft2))  # TODO implemente circular conv to be sure it does the same thing
+    grad_orient_histo_conv = np.convolve(grad_orient_histo_smooth, grad_orient_histo_smooth, mode='same')
 
-    if len(angles_sorted) == 0 or confidence_score < confidence_treshold:  # no angle found
+    # grad_orient_histo_conv_reordered = np.zeros(nb_bin)  # wtf is the point of this => hog conv with angle [0 pi] instead of [-pi/2 pi/2] like we want
+    # grad_orient_histo_conv_reordered[0:nb_bin//2] = grad_orient_histo_conv[nb_bin//2:nb_bin]
+    # grad_orient_histo_conv_reordered[nb_bin//2:nb_bin] = grad_orient_histo_conv[0:nb_bin//2]
+    grad_orient_histo_conv_reordered = grad_orient_histo_conv
+
+    index_restrain = int(np.ceil(np.true_divide(angle_range, 180) * nb_bin))
+    center = (nb_bin - 1) // 2
+
+    grad_orient_histo_conv_restrained = grad_orient_histo_conv_reordered[center - index_restrain:center + index_restrain + 1]
+
+    index_angle_found = np.argmax(grad_orient_histo_conv_restrained) + (nb_bin//2 - index_restrain) -1
+    angle_found = repr_hist[index_angle_found]
+    angle_found_score = np.amax(grad_orient_histo_conv_restrained)
+
+    confidence_score = angle_found_score/np.mean(grad_orient_histo_conv_reordered)
+    # sct.printv("confidence score is : " + str(confidence_score) + "\n angle associated is : " + str(angles_sorted[0] * 180/pi))
+    sct.printv("confidence score is : " + str(confidence_score) + "\n angle associated is : " + str(angle_found *180/pi))
+
+    # Plotting stuff :
+
+    plt.figure(figsize=(6.4*3, 4.8*3))
+    plt.subplot(241)
+    plt.imshow(np.max(image) - image, cmap="Greys")
+    plt.title("image")
+    plt.subplot(242)
+    plt.imshow(proba_map)
+    plt.title("weighting map")
+    plt.subplot(243)
+    plt.imshow(np.multiply(orient_image, proba_map > 0) * 180/pi, cmap="hsv")
+    plt.colorbar()
+    plt.title("Orientation map")
+    plt.subplot(244)
+    plt.bar(repr_hist * 180/pi, grad_orient_histo, width=0.8 * 360/nb_bin)
+    plt.xlabel("angle")
+    plt.title("Orientation of weighted gradient histogram")
+    plt.subplot(245)
+    plt.bar(repr_hist * 180/pi, grad_orient_histo_smooth, width=0.8 * 360/nb_bin)
+    plt.xlabel("angle")
+    plt.title("Orientation of weighted gradient histogram smoothed")
+    plt.subplot(246)
+    plt.plot(repr_hist * 180/pi, grad_orient_histo_conv_reordered)
+    plt.xlabel("angle")
+    plt.title("Convolution of the histogram")
+    plt.subplot(247)
+    plt.plot(repr_hist[center - index_restrain:center + index_restrain + 1] * 180/pi, grad_orient_histo_conv_restrained)
+    plt.xlabel("angle")
+    plt.title("Convolution of the histogram restrained to angle range")
+    # plt.show()
+
+    if confidence_score < confidence_treshold:
         angle_found = None
-    else:
-        angle_found = angles_sorted[0]
 
     if return_proba_map:
         return angle_found, proba_map
@@ -177,7 +209,7 @@ def find_angle_hog(image, centermass, parameters, return_proba_map=False):
         return angle_found
 
 
-def hog_ancestor(image, nb_bin, grad_ksize=123456789, seg_weighted_mask=None, return_image=False): # TODO implement selection of gradient's kernel size, sure that is pertinent ? check wikip image gradient
+def gradient_orientation_histogram(image, nb_bin, grad_ksize=123456789, seg_weighted_mask=None, return_image=False, return_orient=False):  # TODO implement selection of gradient's kernel size, sure that is pertinent ? check wikip image gradient
                                                                                                     # sun et si original say that by increasing the kernel size we reduce the (0, 45, 90) effect
 
     """ This function takes an image as an input and return its orientation histogram
@@ -188,21 +220,20 @@ def hog_ancestor(image, nb_bin, grad_ksize=123456789, seg_weighted_mask=None, re
     outputs :
         - hog_ancest : the histogram of the orientations of the image, a 1D numpy array of length nb_bin"""
 
-    h_kernel = np.array([[1, 2, 1],
+    h_kernel = np.array([[1, 1, 1],
                                [0, 0, 0],
-                               [-1, -2, -1]]) / 4.0
+                               [-1, -1, -1]]) / 4.0  # TODO this is sobel operator, test prewits ? others ?
+                                                    # this filter is actually separable, maybe could speed up the computation
+                                                        # TODO laplacian filter, detect zero? less sensitive to noise
     v_kernel = h_kernel.T
 
     # x and y gradients
     gradx = convolve(image, v_kernel)
     grady = convolve(image, h_kernel)
     # orientation gradient
-    orient = np.arctan2(grady, gradx)*180/pi
-    # changing results from [-180,180] to [0,360] (more convenient to visualise) :
-    # negatives = orient < 0
-    # orient[negatives] = orient[negatives] + 360  #TODO !!!
+    orient = np.arctan2(grady, gradx)  # results are in the range -pi pi
 
-    # weight by gradient magnitude : TODO : this step seems dumb, it alters the angles
+    # weight by gradient magnitude :  this step seems dumb, it alters the angles
     # actually it can be smart but by doing a weighted histogram, not weight the image
 
     grad_mag = ((np.abs(gradx.astype(object)) ** 2 + np.abs(grady.astype(object)) ** 2) ** 0.5)
@@ -218,18 +249,19 @@ def hog_ancestor(image, nb_bin, grad_ksize=123456789, seg_weighted_mask=None, re
     # uncomment following line to have vanilla Sun et al. method
     #weighting_map = np.ones(grad_mag.shape)
     # compute histogram :
-    hog_ancest = np.histogram(np.concatenate(orient), bins=nb_bin, range=(-nb_bin/2, nb_bin/2),
-                              weights=np.concatenate(weighting_map))  # check param density that permits outputting a distribution that has integral of 1
-    # hog_ancest = np.histogram(np.concatenate(orient), bins=nb_bin)
-    grad_mag = (grad_mag * 255).astype(float).round()  # just for debbuguing purpose
+    grad_orient_histo = np.histogram(np.concatenate(orient), bins=nb_bin-1, range=(-(pi-pi/nb_bin), (pi-pi/nb_bin)), weights=np.concatenate(weighting_map))  # check param density that permits outputting a distribution that has integral of 1
+    grad_mag = (grad_mag * 255).astype(float).round()  # just for debbuguing purpose (visualisation)
     if seg_weighted_mask is not None:
         seg_weighted_mask = (seg_weighted_mask * 255).astype(float).round()
     weighting_map = (weighting_map * 255).astype(float).round()
 
     if return_image:
-        return hog_ancest[0].astype(float), weighting_map
+        if return_orient:
+            return grad_orient_histo[0].astype(float), weighting_map, orient
+        else:
+            return grad_orient_histo[0].astype(float), weighting_map
     else:
-        return hog_ancest[0].astype(float)  # return only the values of the bins, not the bins (we know them)
+        return grad_orient_histo[0].astype(float)  # return only the values of the bins, not the bins (we know them)
 
 
 def compute_similarity_metric(array1, array2, metric="Dice"):