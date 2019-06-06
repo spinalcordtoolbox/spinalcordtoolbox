cc9f4362b31f004f1b23ceccaf515a5b141771f4
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-06-06 17:21:35 -0400

    sct_flatten_sagittal: Added new output

--- scripts/sct_flatten_sagittal.py
+++ scripts/sct_flatten_sagittal.py
@@ -50,7 +50,7 @@ def flatten_sagittal(im_anat, im_centerline, verbose):
     nx, ny, nz, nt, px, py, pz, pt = im_anat.dim
 
     # smooth centerline and return fitted coordinates in voxel space
-    _, arr_ctl, _ = get_centerline(im_centerline, algo_fitting='bspline', minmax=True, verbose=verbose)
+    _, arr_ctl, _, _ = get_centerline(im_centerline, algo_fitting='bspline', minmax=True, verbose=verbose)
     x_centerline_fit, y_centerline_fit, z_centerline = arr_ctl
 
     # Extend the centerline by copying values below zmin and above zmax to avoid discontinuities
