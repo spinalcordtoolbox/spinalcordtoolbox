1e0b2a46588467b64f3315a41a698b7776bf78ed
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-06-18 06:06:21 -0400

    cleaned code and added metric auto for agregation

--- scripts/agregate_metrics.py
+++ scripts/agregate_metrics.py
@@ -34,8 +34,12 @@ def main(args=None):
     parser = get_parser()
     arguments = parser.parse(args)
     folder = arguments['-i']
+
+    metrics = ["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std"]
+    methods = ["NoRot", "pca", "hog", "auto"]
     # init global dic :
-    dice_metrics = {method: {metric: [] for metric in ["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std"]} for method in ["NoRot", "pca", "hog"]}  # dictionary for metrics in dictionnary for method containing lists of values
+    dice_metrics = {method: {metric: [] for metric in metrics} for method in methods}
+    # dictionary for metrics in dictionnary for method containing lists of values
 
     for root, dirnames, filenames in os.walk(folder):  # searching the given directory
 
@@ -43,36 +47,43 @@ def main(args=None):
 
             filename_pca = os.path.join(root, filename_NoRot.replace("NoRot", "pca"))
             filename_hog = os.path.join(root, filename_NoRot.replace("NoRot", "hog"))
-            if not (os.path.isfile(filename_pca) and os.path.isfile(filename_hog)):
-                sct.printv("3 csv files not found for file : " + filename_NoRot)
-                continue  # this block makes sure that there is csv file for the 3 methods
+            filename_auto = os.path.join(root, filename_NoRot.replace("NoRot", "auto"))
+            if not (os.path.isfile(filename_pca) and os.path.isfile(filename_hog) and os.path.isfile(filename_auto)):
+                sct.printv("4 csv files not found for file : " + filename_NoRot)
+                continue  # this block makes sure that there is csv file for the 4 methods
 
-            # Open and verify presence of 5 metrics
+            # Open and verify presence of all metrics
             with open(os.path.join(root, filename_NoRot), 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 metric_dic_NoRot = {rows[0]: float(rows[1]) for rows in reader}
-                if len(metric_dic_NoRot) != 5:
-                    sct.printv("5 Metrics not present in csv : " + filename_NoRot)
+                if len(metric_dic_NoRot) != len(metrics):
+                    sct.printv("all Metrics not present in csv : " + filename_NoRot)
                     continue
             with open(os.path.join(root, filename_pca), 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 metric_dic_pca = {rows[0]: float(rows[1]) for rows in reader}
-                if len(metric_dic_pca) != 5:
-                    sct.printv("5 Metrics not present in csv : " + filename_pca)
+                if len(metric_dic_pca) != len(metrics):
+                    sct.printv("all Metrics not present in csv : " + filename_pca)
                     continue
             with open(os.path.join(root, filename_hog), 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 metric_dic_hog = {rows[0]: float(rows[1]) for rows in reader}
-                if len(metric_dic_hog) != 5:
-                    sct.printv("5 Metrics not present in csv : " + filename_hog)
+                if len(metric_dic_hog) != len(metrics):
+                    sct.printv("all Metrics not present in csv : " + filename_hog)
+                    continue
+            with open(os.path.join(root, filename_auto), 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                metric_dic_auto = {rows[0]: float(rows[1]) for rows in reader}
+                if len(metric_dic_auto) != len(metrics):
+                    sct.printv("all Metrics not present in csv : " + filename_hog)
                     continue
 
-            # Now append the metrics to the general dic, if program arrives at this step it means that the 3 .csv exist and have the 5 metrics inside
-            for metric in ["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std"]:
+            # Now append the metrics to the general dic, if program arrives at this step it means that the all .csv exist and have the all metrics inside
+            for metric in metrics:
                 dice_metrics["NoRot"][metric].append(metric_dic_NoRot[metric])
                 dice_metrics["pca"][metric].append(metric_dic_pca[metric])
                 dice_metrics["hog"][metric].append(metric_dic_hog[metric])
-
+                dice_metrics["auto"][metric].append(metric_dic_hog[metric])
 
     nb_subjects = len(next(iter(next(iter(dice_metrics.values())).values())))  # just to get number of subjects (we access the first element of dic twice)
 
@@ -83,35 +94,36 @@ def main(args=None):
 
     # Processing data
     # matplotlib.use('Agg')  # prevent display figure
-    fig = plt.figure(figsize=(20, 40))
+    fig = plt.figure(figsize=(20*2, 40*2))
     fig.suptitle("Histograms of dataset with " + str(nb_subjects) + " images")
-    for k, metric in enumerate(["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std"]):
-        plt.subplot(2, 3, k + 1)
+    for k, metric in enumerate(metrics):
+        plt.subplot(2, (len(metrics)+1)//2, k + 1)
         if metric == "dice_std":
             range_metric = None
             xlabel = "std"
         else:
             range_metric = (0, 1)
             xlabel = "dice score"
-        plt.hist((dice_metrics["NoRot"][metric], dice_metrics["pca"][metric], dice_metrics["hog"][metric]), bins=10, range=range_metric)
+        plt.hist((dice_metrics["NoRot"][metric], dice_metrics["pca"][metric], dice_metrics["hog"][metric], dice_metrics["auto"][metric]), bins=10, range=range_metric)
         plt.ylabel("count")
         plt.xlabel(xlabel)
         plt.title(metric + " histogram")
-        plt.legend(["NoRot", "pca", "hog"])
+        plt.legend(methods)
 
-    plt.subplot(2, 3, 6)
+    plt.subplot(2, (len(metrics)+1)//2, len(metrics)+1)
     #Building the np array to plot the table
-    data = np.zeros((6, 3))
-    for col, metric in enumerate(["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std"]):
-        list_argmax = list(np.argmax((dice_metrics["NoRot"]["dice_global"], dice_metrics["pca"]["dice_global"], dice_metrics["hog"]["dice_global"]), axis=0))
+    data = np.zeros((6, len(methods)))
+    for col, metric in enumerate(metrics):
+        list_argmax = list(np.argmax((dice_metrics["NoRot"]["dice_global"], dice_metrics["pca"]["dice_global"], dice_metrics["hog"]["dice_global"], dice_metrics["auto"]["dice_global"]), axis=0))
         nb_NoRot_best = list_argmax.count(0)
-        nb_PCA_best = list_argmax.count(1)
-        nb_HOG_best = list_argmax.count(2)
-        for row, method in enumerate(["NoRot", "pca", "hog"]):
+        nb_pca_best = list_argmax.count(1)
+        nb_hog_best = list_argmax.count(2)
+        nb_auto_best = list_argmax.count(3)
+        for row, method in enumerate(methods):
             data[col, row] = np.mean(dice_metrics[method][metric])
-        data[5, :] = [nb_NoRot_best, nb_PCA_best, nb_HOG_best]
+        data[len(metrics), :] = [nb_NoRot_best, nb_pca_best, nb_hog_best, nb_auto_best]
 
-    plt.table(cellText=data, colLabels=["NoRot", "pca", "hog"], rowLabels=["dice_global", "dice_mean", "dice_min", "dice_max", "dice_std", "No times best"], loc="center")
+    plt.table(cellText=data, colLabels=methods, rowLabels=metrics.append("No times best"), loc="center")
 
     # Saving everything :
     plt.savefig(folder + "/histograms.png")
