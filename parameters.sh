aa6cba382f397563e2de520c8cff783ac509ecd8
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-06-25 15:05:41 -0400

    renaming

--- parameters.sh
+++ parameters.sh
@@ -6,12 +6,12 @@
 export PATH_PARENT="/home/nicolas"
 
 # path to input data (do not add "/" at the end). This path should be absolute (i.e. do not use ".")
-export PATH_DATA="${PATH_PARENT}/unf_test"
+export PATH_DATA="${PATH_PARENT}/SpineRot_Dataset"
 
 # Path where to save results (do not add "/" at the end).
-export PATH_OUTPUT="${PATH_PARENT}/results_test"
-export PATH_QC="${PATH_PARENT}/results_qc_test"
-export PATH_LOG="${PATH_PARENT}/results_log_test"
+export PATH_OUTPUT="${PATH_PARENT}/SpineRot_results"
+export PATH_QC="${PATH_PARENT}/SpineRot_qc"
+export PATH_LOG="${PATH_PARENT}/SpineRot_log"
 
 # Misc
 export JOBS=4  # Number of jobs for parallel processing
