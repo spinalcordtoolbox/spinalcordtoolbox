49ac2f005a79331dbfde48600c0bc4468b0885ec
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-06-25 13:41:23 -0400

    conf score uniformisation, figures and orientation

--- scripts/evaluate_rot.py
+++ scripts/evaluate_rot.py
@@ -71,56 +71,71 @@ def main(args=None):
 
     sub_and_sequence = (fname_image.split("/")[-1]).split(".nii.gz")[0]
 
-    data_image = Image(fname_image).data
-    data_seg = Image(fname_seg).data
+    data_image = Image(fname_image).change_orientation("LPI").data
+    data_seg = Image(fname_seg).change_orientation("LPI").data
 
-    nx, ny, nz, nt, px, py, pz, pt = Image(fname_image).dim
+    nx, ny, nz, nt, px, py, pz, pt = Image(fname_image).change_orientation("LPI").dim
 
     min_z = np.min(np.nonzero(data_seg)[2])
     max_z = np.max(np.nonzero(data_seg)[2])
 
-    angles = np.zeros(max_z - min_z)
-    conf_score = np.zeros(max_z - min_z)
-    proba_map = np.zeros((nx, ny, nz))
-
     methods = ["pca", "hog"]
 
     angle_range = 10
+    conf_score_th_pca = 1.6
+    conf_score_th_hog = 1
 
     for k, method in enumerate(methods):
 
+        angles = np.zeros(max_z - min_z)
+        conf_score = np.zeros(max_z - min_z)
         axes_image = np.zeros((nx, ny, nz))
         start_time = time.time()
+
         for z in range(0, max_z-min_z):
 
             if method is "hog":
                 angles[z], conf_score[z], centermass = find_angle(data_image[:, :, min_z + z], data_seg[:, :, min_z + z], px, py, method, angle_range=angle_range, return_centermass=True)
-                if math.isnan(angles[z]):
-                    sct.printv("confidence score bellow threshold (or not found)")
+                if math.isnan(angles[z]) or conf_score[z] is None:
+                    conf_score[z] = -10
                     angles[z] = 0
+                if conf_score[z] < conf_score_th_hog:
+                    angles[z] = 0
+                    conf_score[z] = -5
             else:
                 angles[z], conf_score[z], centermass = find_angle(data_image[:, :, min_z + z], data_seg[:, :, min_z + z], px, py, method, angle_range=angle_range, return_centermass=True)
+                if math.isnan(conf_score[z]) or conf_score[z] is None:
+                    conf_score[z] = -10
+                    angles[z] = 0
+                if conf_score[z] < conf_score_th_pca:
+                    angles[z] = 0
+                    conf_score[z] = -5
 
-            axes_image[:, :, min_z + z] = generate_2Dimage_line(axes_image[:, :, min_z + z], centermass[0], centermass[1], angles[z]-pi/2, value=k+1)
+            # print("angle " + method + " is " + str(round(angles[z] * 180/pi, 0)))
+
+            axes_image[:, :, min_z + z] = generate_2Dimage_line(axes_image[:, :, min_z + z], centermass[0], centermass[1], angles[z] - pi/2, value=k+1)
+
+        # conf_score = conf_score / max(conf_score)
 
         sct.printv("Time elapsed for method " + method + " (+ generating axes) : " + str(round(time.time() - start_time, 1)) + " seconds")
         sct.printv("Max angle is : " + str(max(angles) * 180/pi) + ", min is : " + str(min(angles) * 180/pi) + " and mean is : " + str(np.mean(angles) * 180/pi))
 
         fname_axes = output_dir + "/" + sub_and_sequence + "_axes_" + method + ".nii.gz"
         Image(axes_image, hdr=Image(fname_seg).hdr).save(fname_axes)
-        if method is "hog":
-            Image(proba_map, hdr=Image(fname_seg).hdr).save(output_dir + "/" + sub_and_sequence + "_hog_proba_map.nii.gz")
+        if method is "pca":
             plt.figure(figsize=(6.4*2, 4.8*2))
-            plt.scatter(np.arange(min_z, max_z), angles * 180/pi, c=conf_score, cmap='winter')
+            plt.scatter(np.arange(min_z, max_z), angles * 180/pi, c=conf_score, cmap='PRGn')
             plt.ylabel("angle in deg")
             plt.xlabel("z slice")
-            plt.colorbar().ax.set_ylabel("confidence score")
+            plt.colorbar().ax.set_ylabel("conf score PCA")
+        else:
+            plt.scatter(np.arange(min_z, max_z), angles * 180 / pi, c=conf_score, cmap='coolwarm')
+            plt.colorbar().ax.set_ylabel("conf score HOG")
             plt.savefig(output_dir + "/" + fname_image.split("/")[-1] + "_" + sub_and_sequence + "_angle_conf_score_z.png")  # reliable file name ?
-
 
         # generate_qc(fname_in1=fname_image, fname_in2=fname_axes, fname_seg=None, args=[method], path_qc=path_qc, dataset=None, subject=None, process="rotation")
 
-    sct.printv("fsleyes " + cwd + "/" + fname_image + " " + cwd + "/" + fname_seg + " -cm red" + " " + output_dir + "/" + sub_and_sequence + "_axes_pca.nii.gz -cm blue " + output_dir + "/" + sub_and_sequence + "_axes_hog.nii.gz -cm green")
+    sct.printv("fsleyes " + cwd + "/" + fname_image + " " + cwd + "/" + fname_seg + " -cm red" + " " + output_dir + "/" + sub_and_sequence + "_axes_pca.nii.gz -cm blue " + output_dir + "/" + sub_and_sequence + "_axes_hog.nii.gz -cm green", type='info')
     # fsleyes /home/nicolas/unf_test/unf_spineGeneric/sub-01/anat/sub-01_T1w.nii.gz /home/nicolas/test_single_rot/sub-01_T1w_axes_pca.nii.gz -cm blue /home/nicolas/test_single_rot/sub-01_T1w_axes_hog.nii.gz -cm green
 
 def memory_limit():
