fe783a9c0d320e898e06f4c1995d3c96ef3328af
heyan_admin <heemin99@gmail.com>
2019-06-25 14:06:24 -0400

    sct_analyze_texture: Display help if no input arguments

--- scripts/sct_analyze_texture.py
+++ scripts/sct_analyze_texture.py
@@ -335,5 +335,5 @@ def main(arguments):
 if __name__ == "__main__":
     sct.init_sct()
     parser = get_parser()
-    arguments = parser.parse_args()
+    arguments = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     main(arguments)
