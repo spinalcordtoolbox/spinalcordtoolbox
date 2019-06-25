6bd0e0bb1ac139df75ab3b480c57281c185da92b
heyan_admin <heemin99@gmail.com>
2019-06-25 14:04:30 -0400

    sct_analyze_texture: Switch quote style for visual output

--- scripts/sct_analyze_texture.py
+++ scripts/sct_analyze_texture.py
@@ -35,11 +35,11 @@ def get_parser():
     mandatoryArguments = parser.add_argument_group("\nMandatory arguments")
     mandatoryArguments.add_argument(
         "-i",
-        help="Image to analyze. (e.g. 't2.nii.gz')",
+        help="Image to analyze. (e.g. \"t2.nii.gz\")",
         required=True)
     mandatoryArguments.add_argument(
         "-m",
-        help="Image mask (e.g. 't2_seg.nii.gz')",
+        help="Image mask (e.g. \"t2_seg.nii.gz\")",
         required=True)
     optional = parser.add_argument_group("\nOptional arguments")
     optional.add_argument(
@@ -54,12 +54,12 @@ def get_parser():
         default=ParamGLCM().feature)
     optional.add_argument(
         "-distance",
-        help="Distance offset for GLCM computation, in pixel (suggested distance values between 1 and 5). (e.g. 1)",
+        help="Distance offset for GLCM computation, in pixel (suggested distance values between 1 and 5). (e.g. \"1\")",
         required=False,
         default=ParamGLCM().distance)
     optional.add_argument(
         "-angle",
-        help="List of angles for GLCM computation, separate arguments with \",\", in degrees (suggested distance values between 0 and 179). (e.g. '0,90')",
+        help="List of angles for GLCM computation, separate arguments with \",\", in degrees (suggested distance values between 0 and 179). (e.g. \"0,90\")",
         required=False,
         default=ParamGLCM().angle)
     optional.add_argument(
@@ -69,7 +69,7 @@ def get_parser():
         default=Param().dim)
     optional.add_argument(
         "-ofolder",
-        help="Output folder. (e.g. '/my_texture/')",
+        help="Output folder. (e.g. \"/my_texture/\")",
         required=False,
         default=Param().path_results)
     optional.add_argument(
