6fe76370bcba149d2dc45a038a64eb1ba15e9528
heyan_admin <heemin99@gmail.com>
2019-06-26 13:32:29 -0400

    sct_concat_transfo: Display help if no arguments

--- scripts/sct_concat_transfo.py
+++ scripts/sct_concat_transfo.py
@@ -137,7 +137,7 @@ def get_parser():
 if __name__ == "__main__":
     sct.init_sct()
     parser = get_parser()
-    arguments = parser.parse_args()
+    arguments = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     # initialize parameters
     param = Param()
     # call main function
