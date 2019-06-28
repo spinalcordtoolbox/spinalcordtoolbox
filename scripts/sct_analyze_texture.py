4763271a37fdcdfcbfc3bd46eb5c1e0f3d2ea092
heyan_admin <heemin99@gmail.com>
2019-06-28 14:49:02 -0400

    sct_analyze_texture: Changed class for object definition in help display

--- scripts/sct_analyze_texture.py
+++ scripts/sct_analyze_texture.py
@@ -23,7 +23,7 @@ import sct_utils as sct
 import spinalcordtoolbox.image as msct_image
 from spinalcordtoolbox.image import Image
 import argparse
-from msct_types import flagTypes
+from spinalcordtoolbox.utils import Metavar
 
 def get_parser():
     # Initialize the parser
@@ -36,12 +36,12 @@ def get_parser():
     mandatoryArguments = parser.add_argument_group("\nMANDATORY ARGUMENTS")
     mandatoryArguments.add_argument(
         "-i",
-        metavar=flagTypes.file.value,
+        metavar=Metavar.file,
         help='Image to analyze. (e.g. "t2.nii.gz")',
         required=False)
     mandatoryArguments.add_argument(
         "-m",
-        metavar=flagTypes.file.value,
+        metavar=Metavar.file,
         help='Image mask (e.g. "t2_seg.nii.gz")',
         required=False)
     optional = parser.add_argument_group("\nOPTIONALS ARGUMENTS")
@@ -52,19 +52,19 @@ def get_parser():
         help="show this help message and exit")
     optional.add_argument(
         "-feature",
-        metavar=flagTypes.str.value,
+        metavar=Metavar.str,
         help='List of GLCM texture features (separate arguments with ",").',
         required=False,
         default=ParamGLCM().feature)
     optional.add_argument(
         "-distance",
-        metavar=flagTypes.int.value,
+        metavar=Metavar.int,
         help='Distance offset for GLCM computation, in pixel (suggested distance values between 1 and 5). (e.g. "1")',
         required=False,
         default=ParamGLCM().distance)
     optional.add_argument(
         "-angle",
-        metavar=flagTypes.list.value,
+        metavar=Metavar.list,
         help='List of angles for GLCM computation, separate arguments with ",", in degrees (suggested distance values between 0 and 179). (e.g. "0,90")',
         required=False,
         default=ParamGLCM().angle)
@@ -76,13 +76,13 @@ def get_parser():
         default=Param().dim)
     optional.add_argument(
         "-ofolder",
-        metavar=flagTypes.folder.value,
+        metavar=Metavar.folder,
         help='Output folder. (e.g. "/my_texture/")',
         required=False,
         default=Param().path_results)
     optional.add_argument(
         "-igt",
-        metavar=flagTypes.str.value,
+        metavar=Metavar.str,
         help="File name of ground-truth texture metrics.",
         required=False)
     optional.add_argument(
