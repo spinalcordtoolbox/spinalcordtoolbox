109819d77a171fba2fcfae2b0b2505de7fd44f90
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-07-05 11:46:17 -0400

    test_sct_register_to_template.py: Adapted test to new apply_transfo scheme

--- testing/test_sct_register_to_template.py
+++ testing/test_sct_register_to_template.py
@@ -14,10 +14,10 @@ from __future__ import absolute_import
 
 import os
 
-from pandas import DataFrame
-
 import spinalcordtoolbox.image as msct_image
 from spinalcordtoolbox.image import Image
+from spinalcordtoolbox import __sct_dir__
+
 import sct_apply_transfo
 
 
@@ -26,10 +26,11 @@ def init(param_test):
     Initialize class: param_test
     """
     default_args = [
-        '-i t2/t2.nii.gz -l t2/labels.nii.gz -s t2/t2_seg.nii.gz -param step=1,type=seg,algo=centermassrot,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=5,metric=MeanSquares -t template -r 0 -igt template/template/PAM50_small_cord.nii.gz -qc qc-testing',
+        '-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -l t2/labels.nii.gz -param step=1,type=seg,algo=centermassrot,metric=MeanSquares:step=2,type=seg,algo=bsplinesyn,iter=5,metric=MeanSquares -t template -qc qc-testing',
         '-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -ldisc t2/labels.nii.gz -ref subject',
     ]
     param_test.file_seg = 't2/t2_seg.nii.gz'
+    param_test.fname_gt = ['template/template/PAM50_small_cord.nii.gz', os.path.join(__sct_dir__, 'data/PAM50/template/PAM50_cord.nii.gz')]
 
     param_test.dice_threshold = 0.9
 
@@ -44,13 +45,12 @@ def test_integrity(param_test):
     """
     Test integrity of function
     """
-
-    if param_test.args.startswith(default_args[1]):
-        return param_test  # no integrity test
+    # fetch index of the test being performed
+    index_args = param_test.default_args.index(param_test.args)
 
     # apply transformation to binary mask: template --> anat
     sct_apply_transfo.main(args=[
-        '-i', param_test.fname_gt,
+        '-i', param_test.fname_gt[index_args],
         '-d', param_test.file_seg,
         '-w', 'warp_template2anat.nii.gz',
         '-o', 'test_template2anat.nii.gz',
@@ -60,7 +60,7 @@ def test_integrity(param_test):
     # apply transformation to binary mask: anat --> template
     sct_apply_transfo.main(args=[
         '-i', param_test.file_seg,
-        '-d', param_test.fname_gt,
+        '-d', param_test.fname_gt[index_args],
         '-w', 'warp_anat2template.nii.gz',
         '-o', 'test_anat2template.nii.gz',
         '-x', 'nn',
@@ -80,7 +80,7 @@ def test_integrity(param_test):
 
     # compute dice coefficient between anat segmentation warped to template and segmentation from template
     im_seg_reg = Image('test_anat2template.nii.gz')
-    im_template_seg = Image(param_test.fname_gt)
+    im_template_seg = Image(param_test.fname_gt[index_args])
     dice_anat2template = msct_image.compute_dice(im_seg_reg, im_template_seg, mode='3d', zboundaries=True)
     # check
     param_test.output += '\n\nDice[seg_reg,template_seg]: '+str(dice_anat2template)
