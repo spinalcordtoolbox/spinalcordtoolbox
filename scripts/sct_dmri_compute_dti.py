bdd41c840c22f74beaecaca96c2113badd76bdbb
heyan_admin <heemin99@gmail.com>
2019-07-12 10:35:30 -0400

    sct_dmri_compute_dti: Converted script from msct_parser to argparse

--- scripts/sct_dmri_compute_dti.py
+++ scripts/sct_dmri_compute_dti.py
@@ -12,14 +12,14 @@
 
 from __future__ import absolute_import
 
-import sys
+import sys, argparse
 
 from dipy.io import read_bvals_bvecs
 from dipy.core.gradients import gradient_table
 import dipy.reconst.dti as dti
 
-from msct_parser import Parser
 import sct_utils as sct
+
 
 class Param:
     def __init__(self):
@@ -32,76 +32,69 @@ def get_parser():
     param = Param()
 
     # Initialize the parser
-    parser = Parser(__file__)
-    parser.usage.set_description('Compute Diffusion Tensor Images (DTI) using dipy.')
-    parser.add_option(name="-i",
-                      type_value="file",
-                      description="Input 4d file.",
-                      mandatory=True,
-                      example="dmri.nii.gz")
-    parser.add_option(name="-bval",
-                      type_value="file",
-                      description="Bvals file.",
-                      mandatory=True,
-                      example="bvals.txt")
-    parser.add_option(name="-bvec",
-                      type_value="file",
-                      description="Bvecs file.",
-                      mandatory=True,
-                      example="bvecs.txt")
-    parser.add_option(name='-method',
-                      type_value='multiple_choice',
-                      description='Type of method to calculate the diffusion tensor:\nstandard: Standard equation [Basser, Biophys J 1994]\nrestore: Robust fitting with outlier detection [Chang, MRM 2005]',
-                      mandatory=False,
-                      default_value='standard',
-                      example=['standard', 'restore'])
-    parser.add_option(name="-evecs",
-                      type_value="multiple_choice",
-                      description="""To output tensor eigenvectors and eigenvalues, set to 1.""",
-                      mandatory=False,
-                      default_value='0',
-                      example=['0', '1'])
-    parser.add_option(name='-m',
-                      type_value='file',
-                      description='Mask used to compute DTI in for faster processing.',
-                      mandatory=False,
-                      example='mask.nii.gz')
-    parser.add_option(name='-o',
-                      type_value='str',
-                      description='Output prefix.',
-                      mandatory=False,
-                      default_value='dti_')
-    parser.add_option(name="-v",
-                      type_value="multiple_choice",
-                      description="""Verbose. 0: nothing. 1: basic. 2: extended.""",
-                      mandatory=False,
-                      default_value=str(param.verbose),
-                      example=['0', '1', '2'])
+
+    parser = argparse.ArgumentParser(description='Compute Diffusion Tensor Images (DTI) using dipy.')
+    parser.add_argument(
+        "-i",
+        help='Input 4d file. (e.g. "dmri.nii.gz")',
+        required = True)
+    parser.add_argument(
+        "-bval",
+        help='Bvals file. (e.g. "bvals.txt")',
+        required = True)
+    parser.add_argument(
+        "-bvec",
+        help='Bvecs file. (e.g. "bvecs.txt")',
+        required = True)
+    parser.add_argument(
+        '-method',
+        help='Type of method to calculate the diffusion tensor:\nstandard: Standard equation [Basser, Biophys J 1994]'
+             '\nrestore: Robust fitting with outlier detection [Chang, MRM 2005]',
+        required = False,
+        default = 'standard',
+        choices = ('standard', 'restore'))
+    parser.add_argument(
+        "-evecs",
+        help='To output tensor eigenvectors and eigenvalues, set to 1.',
+        required=False,
+        default='0',
+        choices=(0, 1))
+    parser.add_argument(
+        '-m',
+        help='Mask used to compute DTI in for faster processing.(e.g. "mask.nii.gz")',
+        required=False)
+    parser.add_argument(
+        '-o',
+        help='Output prefix.',
+        required=False,
+        default='dti_')
+    parser.add_argument(
+        "-v",
+        help="Verbose. 0: nothing. 1: basic. 2: extended.",
+        required = False,
+        default = param.verbose,
+        choices = (0, 1, 2))
     return parser
 
 
 # MAIN
 # ==========================================================================================
-def main(args = None):
-
-    if not args:
-        args = sys.argv[1:]
-
+def main(args=None):
     # initialization
     file_mask = ''
 
     # Get parser info
     parser = get_parser()
-    arguments = parser.parse(sys.argv[1:])
-    fname_in = arguments['-i']
-    fname_bvals = arguments['-bval']
-    fname_bvecs = arguments['-bvec']
-    prefix = arguments['-o']
-    method = arguments['-method']
-    evecs = int(arguments['-evecs'])
-    if "-m" in arguments:
-        file_mask = arguments['-m']
-    param.verbose = int(arguments.get('-v'))
+    arguments = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    fname_in = arguments.i
+    fname_bvals = arguments.bval
+    fname_bvecs = arguments.bvec
+    prefix = arguments.o
+    method = arguments.method
+    evecs = arguments.evecs
+    if arguments.m is not None:
+        file_mask = arguments.m
+    param.verbose = arguments.v
     sct.init_sct(log_level=param.verbose, update=True)  # Update log level
 
     # compute DTI
@@ -177,9 +170,9 @@ def compute_dti(fname_in, fname_bvals, fname_bvecs, prefix, method, evecs, file_
         # output 1st (V1), 2nd (V2) and 3rd (V3) eigenvectors as 4d data
         for idim in range(3):
             nii.data = data_evecs[:, :, :, :, idim]
-            nii.save(prefix + 'V' + str(idim+1) + '.nii.gz', dtype="float32")
+            nii.save(prefix + 'V' + str(idim + 1) + '.nii.gz', dtype="float32")
             nii.data = data_evals[:, :, :, idim]
-            nii.save(prefix + 'E' + str(idim+1) + '.nii.gz', dtype="float32")
+            nii.save(prefix + 'E' + str(idim + 1) + '.nii.gz', dtype="float32")
 
     return True
 
