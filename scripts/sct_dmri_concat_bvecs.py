ad82eb4438e73db34edc906e8db2faf46898fa53
heyan_admin <heemin99@gmail.com>
2019-07-22 11:13:32 -0400

    sct_dmri_concat_bvecs: Fixed formatting, cleanup

--- scripts/sct_dmri_concat_bvecs.py
+++ scripts/sct_dmri_concat_bvecs.py
@@ -29,15 +29,16 @@ def get_parser():
     # Initialize the parser
 
     parser = argparse.ArgumentParser(
-        description='Concatenate bvec files in time. You can either use bvecs in lines or columns.'
+        description='Concatenate bvec files in time. You can either use bvecs in lines or columns. '
                     '\nN.B.: Return bvecs in lines. If you need it in columns, please use sct_dmri_transpose_bvecs afterwards.',
+        formatter_class=argparse.RawTextHelpFormatter,
         add_help=None,
         prog=os.path.basename(__file__).strip(".py"))
-    mandatory = parser.add_argument_group("\n")
+    mandatory = parser.add_argument_group("\nMANDATORY ARGUMENTS")
     mandatory.add_argument(
         "-i",
         metavar=Metavar.file,
-        help='List of the bvec files to concatenate. Example dmri_b700.bvec,dmri_b2000.bvec',
+        help='List of the bvec files to concatenate. Example: dmri_b700.bvec,dmri_b2000.bvec',
         required=True)
     optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
     optional.add_argument(
@@ -48,8 +49,7 @@ def get_parser():
     optional.add_argument(
         "-o",
         metavar=Metavar.file,
-        help='Output file with bvecs concatenated. Example dmri_b700_b2000_concat.bvec'
-    )
+        help='Output file with bvecs concatenated. Example: dmri_b700_b2000_concat.bvec')
 
     return parser
 
