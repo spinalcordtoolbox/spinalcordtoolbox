Fixed -setorient-data giving wrong results (#2401)
* image: Fixes comments

* image.change_orientation: Added param data_only to not change header

* sct_image: Fixed setorient-data

Now producing correct results.

* sct_image: Do not write intermediate file

* image: PEP8 fixes


Former-commit-id: 80533433351f4bafc5e7928219a65a01bde66261 [formerly 98fb29d55ae3c444ef6c53ee1a08de0c8a2f0e5a [formerly f080acec21466c6582723d3a2ad7adc53f5a8636]]
Former-commit-id: 0c228210948af6bed38b5ceac3d7cb414211648d
Former-commit-id: 471b1a0e935add12ef77aca8a2d13a00f2e17e63

spinalcordtoolbox/image.py
@@ -186,6 +186,7 @@ class SlicerOneAxis(object):
 
        return self.im.data[self._slice(idx)]
 
+
 class SlicerMany(object):
     """
     Image*s* slicer utility class.
@@ -900,20 +901,25 @@ def change_shape(im_src, shape, im_dst=None):
     im_dst.hdr = pair.header
     return im_dst
 
-def change_orientation(im_src, orientation, im_dst=None, inverse=False):
+
+def change_orientation(im_src, orientation, im_dst=None, inverse=False, data_only=False):
     """
     :return: an image with changed orientation
     :param im_src: source image
     :param orientation: orientation string (SCT "from" convention)
     :param im_dst: destination image (can be the source image for in-place
                    operation, can be unset to generate one)
-
+    :param inverse: if you think backwards, use this to specify that you actually
+                    want to transform *from* the specified orientation, not *to* it.
+    :param data_only: If you want to only permute the data, not the header. Only use if you know there is a problem
+                      with the native orientation of the input data.
     Notes:
 
     - the resulting image has no path member set
     - if the source image is < 3D, it is reshaped to 3D and the destination is 3D
     """
 
+    # TODO: make sure to cover all cases for setorient-data
     if len(im_src.data.shape) < 3:
         pass # Will reshape to 3D
     elif len(im_src.data.shape) == 3:
@@ -965,7 +971,6 @@ def change_orientation(im_src, orientation, im_dst=None, inverse=False):
     else:
         raise NotImplementedError()
 
-
     # Update header
 
     im_src_aff = im_src.hdr.get_best_affine()
@@ -974,12 +979,14 @@ def change_orientation(im_src, orientation, im_dst=None, inverse=False):
      im_src_data.shape)
     im_dst_aff = np.matmul(im_src_aff, aff)
 
-    im_dst.header.set_qform(im_dst_aff)
-    im_dst.header.set_sform(im_dst_aff)
-    im_dst.header.set_data_shape(data.shape)
+    if not data_only:
+        im_dst.header.set_qform(im_dst_aff)
+        im_dst.header.set_sform(im_dst_aff)
+        im_dst.header.set_data_shape(data.shape)
     im_dst.data = data
 
     return im_dst
+
 
 def change_type(im_src, dtype, im_dst=None):
     """