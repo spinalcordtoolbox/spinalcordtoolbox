020c0168acddd92959d2a0727d6a79ed283de10f
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-12 17:05:51 -0400

    deepseg_sc.core: Updated input variable

--- spinalcordtoolbox/deepseg_sc/core.py
+++ spinalcordtoolbox/deepseg_sc/core.py
@@ -591,7 +591,7 @@ def deep_segmentation_spinalcord(im_image, contrast_type, ctr_algo='cnn', ctr_fi
     im_seg_r.data[np.where(im_seg_r.data < thr)] = 0
 
     # post processing step to z_regularized
-    im_seg_r_postproc = post_processing_volume_wise(im_in=im_seg_r)
+    im_seg_r_postproc = post_processing_volume_wise(im_seg_r)
 
     # change data type
     im_seg_r_postproc.change_type(np.uint8)
