745b505feba04bdc8b746791c1a5ba5cba4b11f9
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-16 17:01:14 -0400

    Fixed wrong results while doing division (#2442)
    * sct_compute_mtr: Now using true_divide
    
    Seems to fix #2441. Further testing required.
    
    * Moved mtsat --> qmri
    
    * sct_compute_mtsat: Moved compute_mtsat_from_file into CLI
    
    * qmri/mt: Replaced sct.printv with logger. PEP8 cleaning
    
    * sct_compute_mtr: Moved computation inside qmri.mt module
    
    * sct_compute_mtr: Cleanup
    
    * sct_compute_mtr: Fixed missing assignment
    
    * qmri/mt: Updated doc
    
    * qmri/mt: Updated docstring
    
    * test_qmri: Added unit test for qmri modules
    
    * qmri.mt: Fixed issue with clipped values
    
    * test_qmri: Finalized MTsat UT
    
    
    Former-commit-id: 07f77b3765ad4b72842ba07a40751401d009c4f7 [formerly 4f3cb00ce1b10df58ffdd7c8710f27ac04164429 [formerly a03b1e65b2934abf33abbdb8e360d38f9c937712]]
    Former-commit-id: a36867bf1741904aa6acacd1feb9310965f17c9c
    Former-commit-id: 88dd115039feaf3770813e285d59bf0f689c294e

--- scripts/sct_compute_mtr.py
+++ scripts/sct_compute_mtr.py
@@ -19,58 +19,10 @@ import argparse
 
 import sct_utils as sct
 from spinalcordtoolbox.utils import Metavar, SmartFormatter
-
-# DEFAULT PARAMETERS
-
-
-class Param:
-    # The constructor
-    def __init__(self):
-        self.debug = 0
-        # self.register = 1
-        self.verbose = 1
-        self.file_out = 'mtr'
+from spinalcordtoolbox.image import Image
+from spinalcordtoolbox.qmri.mt import compute_mtr
 
 
-# main
-#=======================================================================================================================
-def main():
-    import numpy as np
-    import spinalcordtoolbox.image as msct_image
-
-    parser = get_parser()
-    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
-    # Initialization
-    fname_mt0 = ''
-    fname_mt1 = ''
-    fname_mtr = ''
-    # register = param.register
-    # remove_temp_files = param.remove_temp_files
-    # verbose = param.verbose
-
-    # Check input parameters
-    fname_mt0 = args.mt0
-    fname_mt1 = args.mt1
-    fname_mtr = args.o
-    verbose = args.v
-    sct.init_sct(log_level=verbose, update=True)  # Update log level
-
-    # compute MTR
-    sct.printv('\nCompute MTR...', verbose)
-    nii_mt1 = msct_image.Image(fname_mt1)
-    data_mt1 = nii_mt1.data
-    data_mt0 = msct_image.Image(fname_mt0).data
-    data_mtr = 100 * (data_mt0 - data_mt1) / data_mt0
-    # save MTR file
-    nii_mtr = nii_mt1
-    nii_mtr.data = data_mtr
-    nii_mtr.save(fname_mtr)
-    # sct.run(fsloutput+'fslmaths -dt double mt0.nii -sub mt1.nii -mul 100 -div mt0.nii -thr 0 -uthr 100 fname_mtr', verbose)
-
-    sct.display_viewer_syntax([fname_mt0, fname_mt1, fname_mtr])
-
-
-# ==========================================================================================
 def get_parser():
     parser = argparse.ArgumentParser(
         description='Compute magnetization transfer ratio (MTR). Output is given in percentage.',
@@ -105,18 +57,26 @@ def get_parser():
         help='Path to output file.',
         metavar=Metavar.str,
         default=os.path.join('.','mtr.nii.gz'))
-
     return parser
 
 
-#=======================================================================================================================
-# Start program
-#=======================================================================================================================
+def main():
+    # Check input parameters
+    parser = get_parser()
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    fname_mtr = args.o
+    verbose = args.v
+    # Update log level
+    sct.init_sct(log_level=verbose, update=True)
+
+    # compute MTR
+    sct.printv('\nCompute MTR...', verbose)
+    nii_mtr = compute_mtr(nii_mt1=Image(args.mt1), nii_mt0=Image(args.mt0))
+    # save MTR file
+    nii_mtr.save(fname_mtr)
+
+    sct.display_viewer_syntax([args.mt0, args.mt1, fname_mtr])
+
+
 if __name__ == "__main__":
-    sct.init_sct()
-    # parse arguments
-    # initialize parameters
-    param = Param()
-    # param_default = Param()
-    # call main function
     main()
