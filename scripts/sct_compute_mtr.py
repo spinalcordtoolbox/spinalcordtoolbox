5060ac6f47bfe777dac28249807811c033455217
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-26 12:06:29 -0400

    sct_compute_mtr: Added required=True to parser

--- scripts/sct_compute_mtr.py
+++ scripts/sct_compute_mtr.py
@@ -29,17 +29,21 @@ def get_parser():
         add_help=None,
         formatter_class=SmartFormatter,
         prog=os.path.basename(__file__).strip(".py"))
+
     mandatoryArguments = parser.add_argument_group("\nMANDATORY ARGUMENTS")
     mandatoryArguments.add_argument(
         '-mt0',
+        required=True,
         help='Image without MT pulse (MT0)',
         metavar=Metavar.float,
-        required=False)
+        )
     mandatoryArguments.add_argument(
         '-mt1',
+        required=True,
         help='Image with MT pulse (MT1)',
         metavar=Metavar.float,
-        required=False)
+        )
+
     optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
     optional.add_argument(
         "-h",
