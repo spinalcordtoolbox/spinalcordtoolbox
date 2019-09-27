3e5ba798c7f2a30e4a1d1fea98334d68c27adfb6
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-27 16:41:01 -0400

    cropping: Replaced binary isct_crop_image with Python cropping

--- spinalcordtoolbox/cropping.py
+++ spinalcordtoolbox/cropping.py
@@ -25,6 +25,41 @@ class BoundingBox(object):
         self.zmin = zmin
         self.zmax = zmax
 
+    def get_minmax(self, img=None):
+        """
+        Get voxel-based bounding box from coordinates. Replaces '-1' with max dim along each axis, '-2' with max dim
+        minus 1, etc.
+        :param img: Image object to get dimensions
+        :return:
+        """
+        def _get_min_value(input):
+            if input is None:
+                return 0
+            else:
+                return input
+
+        def _get_max_value(input, dim):
+            # If empty, return dim+1 (corresponds to the maximum of the given dimension, e.g. nx)
+            if input is None:
+                return dim + 1
+            # If negative sign, return dim+1 if -1, dim if -2, dim-1 if -3, etc.
+            elif np.sign(input) == -1:
+                return input + dim + 1
+            # If user specified a non-negative value, use that
+            else:
+                return input
+
+        xyz_to_num = {'x': 0, 'y': 1, 'z': 2}
+        bbox_voxel = BoundingBox()
+        for attr, value in self.__dict__.items():
+            if attr[-3:] == 'min':
+                bbox_voxel.__setattr__(attr, _get_min_value(self.__getattribute__(attr)))
+            elif attr[-3:] == 'max':
+                bbox_voxel.__setattr__(attr, _get_max_value(self.__getattribute__(attr), img.dim[xyz_to_num[attr[0]]]))
+            else:
+                raise Exception(ValueError)
+        return bbox_voxel
+
 
 class ImageCropper(object):
     def __init__(self, img_in, output_file=None, mask=None, bbox=BoundingBox(), shift=None, background=None,
@@ -49,65 +84,65 @@ class ImageCropper(object):
         """
         Crop image (change dimension)
         """
-        bbox = self.get_voxel_bbox()
+        # Get bounding box in voxel coordinates
+        bbox = self.bbox.get_minmax(img=self.img_in)
 
         # Crop image
-        data_crop = img_in.data[self.xmin:self.xmax, cropping_coord[0].y:cropping_coord[1].y, :]
-        img_out = Image(param=data_crop, hdr=img_in.hdr)
-        img_out.change_orientation(native_orientation)
+        data_crop = self.img_in.data[bbox.xmin:bbox.xmax, bbox.ymin:bbox.ymax, bbox.zmin:bbox.zmax]
+        img_out = Image(param=data_crop, hdr=self.img_in.hdr)
         img_out.absolutepath = self.output_filename
         img_out.save()
 
 
-        self.cmd = ["isct_crop_image", "-i", self.input_filename, "-o", self.output_filename]
-        # Handling optional arguments
-
-        # if mask is specified, find -start and -end arguments
-        if self.mask is not None:
-            # if user already specified -start or -end arguments, let him know they will be ignored
-            if self.start is not None or self.end is not None:
-                logger.warning("Mask was specified for cropping. Arguments -start and -end will be ignored")
-            self.start, self.end, self.dim = find_mask_boundaries(self.mask)
-
-        if self.start is not None:
-            self.cmd += ["-start", ','.join(map(str, self.start))]
-        if self.end is not None:
-            self.cmd += ["-end", ','.join(map(str, self.end))]
-        if self.dim is not None:
-            self.cmd += ["-dim", ','.join(map(str, self.dim))]
-        if self.shift is not None:
-            self.cmd += ["-shift", ','.join(map(str, self.shift))]
-        if self.background is not None:
-            self.cmd += ["-b", str(self.background)]
-        if self.bmax is True:
-            self.cmd += ["-bmax"]
-        if self.ref is not None:
-            self.cmd += ["-ref", self.ref]
-        if self.mesh is not None:
-            self.cmd += ["-mesh", self.mesh]
-
-        verb = 0
-        if self.verbose == 1:
-            verb = 2
-        if self.mask is not None and self.background is not None:
-            self.crop_from_mask_with_background()
-        else:
-            # Run command line
-            sct.run(self.cmd, verb, is_sct_binary=True)
-
-        self.result = Image(self.output_filename, verbose=self.verbose)
-
-        # removes the output file created by the script if it is not needed
-        if self.rm_output_file:
-            try:
-                os.remove(self.output_filename)
-            except OSError:
-                logger.warning("Couldn't remove output file. Either it is opened elsewhere or it doesn't exist.")
-        else:
-            if self.verbose >= 1:
-                sct.display_viewer_syntax([self.output_filename])
-
-        return self.result
+        # self.cmd = ["isct_crop_image", "-i", self.input_filename, "-o", self.output_filename]
+        # # Handling optional arguments
+        #
+        # # if mask is specified, find -start and -end arguments
+        # if self.mask is not None:
+        #     # if user already specified -start or -end arguments, let him know they will be ignored
+        #     if self.start is not None or self.end is not None:
+        #         logger.warning("Mask was specified for cropping. Arguments -start and -end will be ignored")
+        #     self.start, self.end, self.dim = find_mask_boundaries(self.mask)
+        #
+        # if self.start is not None:
+        #     self.cmd += ["-start", ','.join(map(str, self.start))]
+        # if self.end is not None:
+        #     self.cmd += ["-end", ','.join(map(str, self.end))]
+        # if self.dim is not None:
+        #     self.cmd += ["-dim", ','.join(map(str, self.dim))]
+        # if self.shift is not None:
+        #     self.cmd += ["-shift", ','.join(map(str, self.shift))]
+        # if self.background is not None:
+        #     self.cmd += ["-b", str(self.background)]
+        # if self.bmax is True:
+        #     self.cmd += ["-bmax"]
+        # if self.ref is not None:
+        #     self.cmd += ["-ref", self.ref]
+        # if self.mesh is not None:
+        #     self.cmd += ["-mesh", self.mesh]
+        #
+        # verb = 0
+        # if self.verbose == 1:
+        #     verb = 2
+        # if self.mask is not None and self.background is not None:
+        #     self.crop_from_mask_with_background()
+        # else:
+        #     # Run command line
+        #     sct.run(self.cmd, verb, is_sct_binary=True)
+        #
+        # self.result = Image(self.output_filename, verbose=self.verbose)
+        #
+        # # removes the output file created by the script if it is not needed
+        # if self.rm_output_file:
+        #     try:
+        #         os.remove(self.output_filename)
+        #     except OSError:
+        #         logger.warning("Couldn't remove output file. Either it is opened elsewhere or it doesn't exist.")
+        # else:
+        #     if self.verbose >= 1:
+        #         sct.display_viewer_syntax([self.output_filename])
+        #
+        # return self.result
 
     # mask the image in order to keep only voxels in the mask
     # doesn't change the image dimension
