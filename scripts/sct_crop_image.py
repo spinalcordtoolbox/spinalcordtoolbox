6effef92162a3478027d1924dbaf6a5ee2278212
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-28 15:22:24 -0400

    sct_crop_image: Cleanup, removed useless options

--- scripts/sct_crop_image.py
+++ scripts/sct_crop_image.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
-#########################################################################################
+# -*- coding: utf-8
 #
-# sct_crop_image and crop image wrapper.
+# CLI script to crop an image.
 #
-# ---------------------------------------------------------------------------------------
-# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Authors: Benjamin Leener, Julien Cohen-Adad, Olivier Comtois
-# Modified: 2015-05-16
+# Copyright (c) 2019 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Authors: Julien Cohen-Adad
 #
 # About the license: see the file LICENSE.TXT
-#########################################################################################
 
-from __future__ import absolute_import, division
 
 import sys
 import os
 import argparse
 
 from spinalcordtoolbox.cropping import ImageCropper, BoundingBox
-from spinalcordtoolbox.image import Image, zeros_like
+from spinalcordtoolbox.image import Image
 from spinalcordtoolbox.utils import Metavar, SmartFormatter
 import sct_utils as sct
 
@@ -27,24 +23,25 @@ def get_parser():
 
     # Mandatory arguments
     parser = argparse.ArgumentParser(
-        description='Tools to crop an image. Either through command line or GUI',
+        description="Tools to crop an image. Either via command line or via a Graphical User Interface (GUI). See "
+                    "example usage at the end.",
         add_help=None,
         formatter_class=SmartFormatter,
-        prog=os.path.basename(__file__).strip(".py"))
+        prog=os.path.basename(__file__).strip('.py'))
 
     mandatoryArguments = parser.add_argument_group("\nMANDATORY ARGUMENTS")
     mandatoryArguments.add_argument(
-        "-i",
+        '-i',
         required=True,
-        help='Input image. Example: t2.nii.gz',
+        help="Input image. Example: t2.nii.gz",
         metavar=Metavar.file,
         )
 
     optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
     optional.add_argument(
-        "-h",
-        "--help",
-        action="help",
+        '-h',
+        '--help',
+        action='help',
         help="Show this help message and exit")
     optional.add_argument(
         '-o',
@@ -112,30 +109,6 @@ def get_parser():
         metavar=Metavar.int,
         )
     optional.add_argument(
-        "-shift",
-        help='adding shift when used with mask, default is 0. Example: 10,10,5',
-        metavar=Metavar.list,
-        required = False)
-    optional.add_argument(
-        "-b",
-        type=float,
-        help="Replace voxels outside cropping region with background value. If both the -m and the -b flags are used, "
-             "the image is croped \"exactly\" around the mask with a background (and not around a rectangle area "
-             "including the mask). The shape of the image does not change.",
-        metavar=Metavar.float,
-        required=False)
-    optional.add_argument(
-        "-bmax",
-        help="Maximize the cropping of the image (provide -dim if you want to specify the dimensions).",
-        metavar='',
-        required=False)
-    optional.add_argument(
-        "-mesh",
-        help="Mesh to crop",
-        metavar=Metavar.file,
-        required=False
-    )
-    optional.add_argument(
         "-v",
         type=int,
         help="0: Verbose off | 1: Verbose on",
@@ -172,26 +145,12 @@ def main(args=None):
     elif arguments.ref:
         cropper.get_bbox_from_ref(Image(arguments.ref))
     else:
+        # TODO: make sure at least one argument is specified
         cropper.get_bbox_from_minmax(
             BoundingBox(arguments.xmin, arguments.xmax,
                         arguments.ymin, arguments.ymax,
                         arguments.zmin, arguments.zmax)
         )
-        # if arguments.m is not None:
-        #     cropper.mask = arguments.m
-        # cropper.bbox = BoundingBox(arguments.xmin, arguments.xmax,
-        #                            arguments.ymin, arguments.ymax,
-        #                            arguments.zmin, arguments.zmax)
-        # if arguments.shift is not None:
-        #     cropper.shift = (arguments.shift).split(",")
-        # if arguments.b is not None:
-        #     cropper.background = arguments.b
-        # if arguments.bmax is not None:
-        #     cropper.bmax = True
-        # if arguments.ref is not None:
-        #     cropper.ref = arguments.ref
-        # if arguments.mesh is not None:
-        #     cropper.mesh = arguments.mesh
 
     # Crop image
     img_crop = cropper.crop()
@@ -209,4 +168,3 @@ def main(args=None):
 if __name__ == "__main__":
     sct.init_sct()
     main()
-
