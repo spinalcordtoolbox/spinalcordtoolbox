1c078304467b06f0d5136bf1e4f6d771f34c59c0
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-27 20:40:37 -0400

    test_sct_crop_image: Updated test

--- testing/test_sct_crop_image.py
+++ testing/test_sct_crop_image.py
@@ -20,7 +20,14 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-i t2/t2.nii.gz -o cropped_normal.nii.gz -dim 1 -start 10 -end 50']
+    param_test.fname_out = [
+        't2_crop_xyz.nii',
+        't2_crop_mask.nii'
+    ]
+    default_args = [
+        '-i t2/t2.nii.gz -o {} -xmin 1 -xmax -3 -ymin 2 -ymax 10'.format(param_test.fname_out[0]),
+        '-i t2/t2.nii.gz -o {} -m t2_seg.nii.gz'.format(param_test.fname_out[1])
+    ]
     # assign default params
     if not param_test.args:
         param_test.args = default_args
@@ -31,11 +38,20 @@ def test_integrity(param_test):
     """
     Test integrity of function
     """
-    # check if cropping was correct
-    nx, ny, nz, nt, px, py, pz, pt = Image('cropped_normal.nii.gz').dim
-    if ny != 41:
-        param_test.status = 99
-        param_test.output += '--> FAILED'
-    else:
+    # find which test is performed
+    index_args = param_test.default_args.index(param_test.args)
+
+    # check if cropping was correct depending on the scenario
+    if index_args == 0:
+        xyz = (57, 8, 52)
+    elif index_args == 1:
+        xyz = (10, 54, 12)
+
+    nx, ny, nz, nt, px, py, pz, pt = Image(param_test.fname_out[index_args]).dim
+    if (nx, ny, nz) == xyz:
         param_test.output += '--> PASSED'
+    else:
+        param_test.status = 99
+        param_test.output += "Output dimensions: {}, {}, {}\n".format(nx, ny, nz)
+        param_test.output += '--> FAILED'
     return param_test
