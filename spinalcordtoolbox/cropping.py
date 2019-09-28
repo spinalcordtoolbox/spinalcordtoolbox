a0420dec30794694f71a9c68d3e622431ec429f4
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-28 14:50:56 -0400

    cropping: Fixed origin

--- spinalcordtoolbox/cropping.py
+++ spinalcordtoolbox/cropping.py
@@ -102,6 +102,16 @@ class ImageCropper(object):
         data_crop = self.img_in.data[bbox.xmin:bbox.xmax, bbox.ymin:bbox.ymax, bbox.zmin:bbox.zmax]
         img_out = Image(param=data_crop, hdr=self.img_in.hdr)
         img_out.absolutepath = self.output_filename
+
+        # adapt the origin in the sform and qform matrix
+        new_origin = np.dot(img_out.hdr.get_qform(), [bbox.xmin, bbox.ymin, bbox.zmin, 1])
+        img_out.hdr.structarr['qoffset_x'] = new_origin[0]
+        img_out.hdr.structarr['qoffset_y'] = new_origin[1]
+        img_out.hdr.structarr['qoffset_z'] = new_origin[2]
+        img_out.hdr.structarr['srow_x'][-1] = new_origin[0]
+        img_out.hdr.structarr['srow_y'][-1] = new_origin[1]
+        img_out.hdr.structarr['srow_z'][-1] = new_origin[2]
+
         img_out.save()
 
     def get_bbox_from_minmax(self, bbox=None):
