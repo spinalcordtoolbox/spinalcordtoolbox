de86785a6c538704507e4339793a26bf56a0a565
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-30 12:49:07 -0400

    test_sct_apply_transfo: Now testing for 4D input

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -22,22 +22,38 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
+    param_test.input = [
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'dmri/dmri.nii.gz',
+    ]
     param_test.fname_out = [
         'PAM50_small_t2_reg.nii',
         'PAM50_small_t2_reg-crop1.nii',
         'PAM50_small_t2_reg-crop2.nii',
         'PAM50_small_t2_reg-concatWarp.nii',
-        'PAM50_small_t2_reg-4D.nii',
+        'PAM50_small_t2_reg-4Dref.nii',
+        'PAM50_small_t2_reg-4Din.nii',
+    ]
+    param_test.ref = [
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        'dmri/dmri.nii.gz',
+        't2/t2.nii.gz',
     ]
     default_args = [
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.fname_out[0]),
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz -crop 1 -o {}'.format(param_test.fname_out[1]),
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz -crop 2 -o {}'.format(param_test.fname_out[2]),
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz t2/warp_template2anat.nii.gz -o {}'.format(param_test.fname_out[3]),
-        '-i dmri/dmri.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.fname_out[4]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[0], param_test.ref[0], param_test.fname_out[0]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -crop 1 -o {}'.format(param_test.input[1], param_test.ref[1], param_test.fname_out[1]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -crop 2 -o {}'.format(param_test.input[2], param_test.ref[2], param_test.fname_out[2]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[3], param_test.ref[3], param_test.fname_out[3]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[4], param_test.ref[4], param_test.fname_out[4]),
+        '-i {} -d {} -w mt/warp_t22mt1.nii.gz -o {}'.format(param_test.input[5], param_test.ref[5], param_test.fname_out[5]),
     ]
-    param_test.input = 'template/template/PAM50_small_t2.nii.gz'
-    param_test.ref = 't2/t2.nii.gz'
 
     # assign default params
     if not param_test.args:
@@ -53,8 +69,8 @@ def test_integrity(param_test):
     # find which test is performed
     index_args = param_test.default_args.index(param_test.args)
 
-    img_src = Image(param_test.input)
-    img_ref = Image(param_test.ref)
+    img_src = Image(param_test.input[index_args])
+    img_ref = Image(param_test.ref[index_args])
     img_output = Image(param_test.fname_out[index_args])
 
     if img_output.orientation != img_ref.orientation:
@@ -74,7 +90,6 @@ def test_integrity(param_test):
     if not img_src.dim[3] == img_output.dim[3]:
         param_test.output += "\nInput and output images don't have the same 4th dimension' size."
         param_test.status = 99
-
 
     if param_test.status == 99:
         param_test.output += "\n--> FAILED"
