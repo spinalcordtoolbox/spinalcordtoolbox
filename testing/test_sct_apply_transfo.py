88e0d74d9914fa7b97f79f38676327c7077c96b2
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-30 18:25:34 -0400

    Improved CLI, fixed bug with the GUI and refactored into module (#2459)
    * sct_crop_image: Refactored cropping with GUI (WIP)
    
    Now using SCT's module: spinalcordtoolbox.gui.sagittal
    Fixes #2458
    
    * sct_crop_image: Cleanup
    
    * sct_crop_image: Cleanup
    
    * sct_crop_image: Modify usage (WIP)
    
    * sct_crop_image: Refactored cropping with GUI (WIP)
    
    Now using SCT's module: spinalcordtoolbox.gui.sagittal
    Fixes #2458
    
    * sct_crop_image: Cleanup
    
    * sct_crop_image: Cleanup
    
    * sct_crop_image: Made output filename optional
    
    * sct_crop_image: FIxed output filename
    
    * spinalcordtoolbox/cropping: Moved ImageCropper into python module
    
    * cropping: Replaced printv with logger, cleanup
    
    * sct_crop_image: Replaced cropping box param with ?min/?max
    
    WIP
    Fixes #1818
    
    * cropping: Introduced class BoundingBox
    
    * cropping: Further work on get_voxel_bbox (WIP)
    
    * cropping: Replaced binary isct_crop_image with Python cropping
    
    * sct_crop_image: Fixed type of input params
    
    * cropping: Refactored to have several functions output bbox
    
    * cropping: Implemented get_bbox_from_mask()
    
    * sct_crop_image: Updated CLIs with recent changes in cropping
    
    * test_sct_crop_image: Updated test
    
    * sct_crop_image: Added entry for -ref option
    
    * resampling: Now accepting nibabel and Image object
    
    * resampling: Added 'mode' input parameter
    
    * cropping: Added get_bbox_from_ref()
    
    * sct_crop_image: Added entry for -ref option
    
    * test_sct_crop_image: Added testing for cropping with ref
    
    * cropping: Fixed origin
    
    * sct_crop_image: Improved CLI, save output there, display fsleyes syntax
    
    * cropping: Updated get_bbox_from_gui, few improvements
    
    * cropping: Cleanup
    
    * sct_crop_image: Cleanup, removed useless options
    
    * sct_crop_image: Added examples to usage
    
    * sct_crop_image: Clarified usage, prioritized cropping methods
    
    * cropping: Clarified GUI usage
    
    * sct_crop_image: Clarified usage
    
    * make_binaries.sh: Removed isct_crop_image binary generation
    
    * cropping: Removed useless input params
    
    * test_sct_crop_image: Updated integrity testing values
    
    * sct_apply_transfo: Updated with recent changes in ImageCropper
    
    * msct_gmseg_utils: Updated with recent changes in ImageCropper
    
    * sct_apply_transfo: Fixed if case typo
    
    * resampling: Fixed wrong type for nibabel
    
    * Fixed wrong input argument for nib_resampling()
    
    * Updated files with recent changes in sct_crop_image CLI
    
    * sct_detect_pmj: Fixed syntax for cropping image
    
    * sct_crop_image: Re-established flag -b because it is used elsewhere
    
    * Re-established sct_crop_image with flag -b
    
    * cropping: Fixed index error
    
    * sct_apply_transfo: Fixed wrongly commented line
    
    * cropping: get_bbox_from_ref() now checking dims for img_in and img_ref
    
    * cropping: Fixed wrong array assignment
    
    * cropping: Added useful verbose
    
    * test_sct_apply_transfo: Made test more sensitive
    
    * test_sct_apply_transfo: Now testing for 4D input
    
    * sct_apply_transfo: Fixed if cases
    
    * make_binaries.sh: removed isct_orientation3d
    
    * sct_download_data: Updated links to binaries
    
    
    Former-commit-id: 91beb0badf165195caf63abb978a2902059884bc

--- testing/test_sct_apply_transfo.py
+++ testing/test_sct_apply_transfo.py
@@ -22,13 +22,38 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
+    param_test.input = [
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'template/template/PAM50_small_t2.nii.gz',
+        'dmri/dmri.nii.gz',
+    ]
+    param_test.fname_out = [
+        'PAM50_small_t2_reg.nii',
+        'PAM50_small_t2_reg-crop1.nii',
+        'PAM50_small_t2_reg-crop2.nii',
+        'PAM50_small_t2_reg-concatWarp.nii',
+        'PAM50_small_t2_reg-4Dref.nii',
+        'PAM50_small_t2_reg-4Din.nii',
+    ]
+    param_test.ref = [
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        't2/t2.nii.gz',
+        'dmri/dmri.nii.gz',
+        't2/t2.nii.gz',
+    ]
     default_args = [
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz',
-        '-i template/template/PAM50_small_t2.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz t2/warp_template2anat.nii.gz',
-        '-i dmri/dmri.nii.gz -d t2/t2.nii.gz -w t2/warp_template2anat.nii.gz']
-    param_test.input = 'template/template/PAM50_small_t2.nii.gz'
-    param_test.ref = 't2/t2.nii.gz'
-    param_test.out = 'PAM50_small_t2_reg.nii.gz'
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[0], param_test.ref[0], param_test.fname_out[0]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -crop 1 -o {}'.format(param_test.input[1], param_test.ref[1], param_test.fname_out[1]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -crop 2 -o {}'.format(param_test.input[2], param_test.ref[2], param_test.fname_out[2]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[3], param_test.ref[3], param_test.fname_out[3]),
+        '-i {} -d {} -w t2/warp_template2anat.nii.gz -o {}'.format(param_test.input[4], param_test.ref[4], param_test.fname_out[4]),
+        '-i {} -d {} -w mt/warp_t22mt1.nii.gz -o {}'.format(param_test.input[5], param_test.ref[5], param_test.fname_out[5]),
+    ]
 
     # assign default params
     if not param_test.args:
@@ -41,20 +66,34 @@ def test_integrity(param_test):
     """
     Test integrity of function
     """
-    img_src = Image(param_test.input)
-    img_ref = Image(param_test.ref)
-    img_output = Image(param_test.out)
+    # find which test is performed
+    index_args = param_test.default_args.index(param_test.args)
+
+    img_src = Image(param_test.input[index_args])
+    img_ref = Image(param_test.ref[index_args])
+    img_output = Image(param_test.fname_out[index_args])
 
     if img_output.orientation != img_ref.orientation:
         param_test.output += "\nImage has wrong orientation (%s -> %s)" % (img_ref.orientation, img_output.orientation)
         param_test.status = 99
 
-    if len(img_src.data.shape) > 3:
-        # Allowed failure for now
-        return param_test
-
     if not (img_output.data != 0).any():
         param_test.output += "\nImage is garbage (all zeros)"
         param_test.status = 99
+
+    # Only checking the first 3 dimensions because one test involves a 4D volume
+    if not img_ref.dim[0:3] == img_output.dim[0:3]:
+        param_test.output += "\nRef and output images don't have the same first 3 dimensions."
+        param_test.status = 99
+
+    # Checking the 4th dim (which should be the same as the input image, not the reference image)
+    if not img_src.dim[3] == img_output.dim[3]:
+        param_test.output += "\nInput and output images don't have the same 4th dimension' size."
+        param_test.status = 99
+
+    if param_test.status == 99:
+        param_test.output += "\n--> FAILED"
+    else:
+        param_test.output += "\n--> PASSED"
 
     return param_test
