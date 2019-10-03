ef32085b403b064d248fa74065565804943383eb
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-10-03 17:09:17 -0400

    sct_download_data: No more removal of existing file in dest folder

--- scripts/sct_download_data.py
+++ scripts/sct_download_data.py
@@ -145,15 +145,12 @@ def main(args=None):
         extracted_files_paths.append(os.path.join(os.path.abspath(dest_tmp_folder), extracted_file))
 
     # Check if files and folder already exists
-    sct.printv('\nCheck if files or folder already exists on the destination path...', verbose)
+    sct.printv('\nCheck if folder already exists on the destination path...', verbose)
     for data_extracted_name in extracted_files:
         fullpath_dest = os.path.join(dest_folder, data_extracted_name)
         if os.path.isdir(fullpath_dest):
             sct.printv("Folder {} already exists. Removing it...".format(data_extracted_name), 1, 'warning')
             rmtree(fullpath_dest)
-        elif os.path.isfile(fullpath_dest):
-            sct.printv("File {} already exists. Removing it...".format(data_extracted_name), 1, 'warning')
-            os.remove(fullpath_dest)
 
     # Destination path
     # for source_path in extracted_files_paths:
