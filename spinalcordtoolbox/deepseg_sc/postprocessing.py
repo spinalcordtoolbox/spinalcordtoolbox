f6c71d1560f9988ed3794130f42f2e9a102ac6f2
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-10-04 20:54:34 -0400

    deepseg_sc/postprocessing: Forgot to divide median by half for edge outlier

--- spinalcordtoolbox/deepseg_sc/postprocessing.py
+++ spinalcordtoolbox/deepseg_sc/postprocessing.py
@@ -113,11 +113,11 @@ def _remove_isolated_voxels_on_the_edge(im_seg, n_slices=5):
     ind_min, ind_max = ind_nonnan[0], ind_nonnan[-1]
     # Check if the CSA at the edge is inferior to half of the median across adjacent slices...
     # ... for the top slice
-    if metrics['area'].data[ind_min] < np.median(metrics['area'].data[ind_min:n_slices]):
+    if metrics['area'].data[ind_min] < np.median(metrics['area'].data[ind_min:n_slices])/2:
         im_seg.data[:, :, ind_min] = 0
         logger.warning('Found isolated voxels on slice {}, Removing them'.format(ind_min))
     # ... for the bottom slice
-    if metrics['area'].data[ind_max] < np.median(metrics['area'].data[ind_max-n_slices+1:ind_max+1]):
+    if metrics['area'].data[ind_max] < np.median(metrics['area'].data[ind_max-n_slices+1:ind_max+1])/2:
         im_seg.data[:, :, ind_max] = 0
         logger.warning('Found isolated voxels on slice {}, Removing them'.format(ind_min))
     return im_seg
