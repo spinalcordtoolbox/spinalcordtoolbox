ccfd7876d7fe8f98cd1193da698eebc8915f6f8f
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-10-23 22:00:00 -0400

    Fixed aberrant mtr values (#2503)
    * sct_compute_mtr: Force saving as float64
    
    * sct_compute_mtr: Changed dtype output to float32 (enough)
    
    * image: Fixed bug where dtype was not set in the nibabel hdr
    
    * qmri/mt: Created function divide_after_removing_zero()
    
    * qmri/mt: Fixed dtype bug
    
    * test_qmri: Now testing with input array
    
    Otherwise new function in qmri/mt is failing
    
    * image: Fixed bug when using SCT-specific dtype 'minimize'
    
    * sct_compute_mtr: Added param -thr
    
    * sct_compute_mtr: now showing logger and SCT version info on run
    
    * qmri/mt: Adding verbose about clipping, nan replacement
    
    * Clarified threshold range in help

--- scripts/sct_compute_mtr.py
+++ scripts/sct_compute_mtr.py
@@ -46,21 +46,31 @@ def get_parser():
 
     optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
     optional.add_argument(
+        "-thr",
+        type=float,
+        help="Threshold to clip MTR output values in case of division by small number. This implies that the output image" 
+             "range will be [-thr, +thr]. Default: 100.",
+        default=100
+        )
+    optional.add_argument(
         "-h",
         "--help",
         action="help",
-        help="Show this help message and exit")
+        help="Show this help message and exit"
+        )
     optional.add_argument(
         '-v',
         type=int,
         choices=(0, 1, 2),
         help='Verbose: 0 = nothing, 1 = classic, 2 = expended',
-        default=1)
+        default=1
+        )
     optional.add_argument(
         '-o',
         help='Path to output file.',
         metavar=Metavar.str,
-        default=os.path.join('.','mtr.nii.gz'))
+        default=os.path.join('.', 'mtr.nii.gz')
+        )
     return parser
 
 
@@ -70,17 +80,17 @@ def main():
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     fname_mtr = args.o
     verbose = args.v
-    # Update log level
-    sct.init_sct(log_level=verbose, update=True)
 
     # compute MTR
     sct.printv('\nCompute MTR...', verbose)
-    nii_mtr = compute_mtr(nii_mt1=Image(args.mt1), nii_mt0=Image(args.mt0))
+    nii_mtr = compute_mtr(nii_mt1=Image(args.mt1), nii_mt0=Image(args.mt0), threshold_mtr=args.thr)
+
     # save MTR file
-    nii_mtr.save(fname_mtr)
+    nii_mtr.save(fname_mtr, dtype='float32')
 
     sct.display_viewer_syntax([args.mt0, args.mt1, fname_mtr])
 
 
 if __name__ == "__main__":
+    sct.init_sct()
     main()
