b4032e6bc5aea5e859728b83eb0b129a366a3a6d
charleygros <charley.gros@gmail.com>
2020-02-11 11:59:47 +1100

    scripts/sct_utils.py: check_if_3d, exit

--- scripts/sct_utils.py
+++ scripts/sct_utils.py
@@ -616,6 +616,7 @@ def check_if_3d(fname):
     if not dim[0] == 3:
         printv('\nERROR: ' + fname + ' is not a 3D volume: {}. Exit program.\n'.format(','.join([str(d) for d in dim])), 
                 1, 'error')
+        sys.exit(2)
     else:
         return True
 
