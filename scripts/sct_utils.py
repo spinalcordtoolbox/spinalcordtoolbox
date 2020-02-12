e8381632f6547aa34b526955008c1b40bf04723d
charleygros <charley.gros@gmail.com>
2020-02-13 10:05:30 +1100

    scripts/sct_utils.py: re write message

--- scripts/sct_utils.py
+++ scripts/sct_utils.py
@@ -614,8 +614,8 @@ def check_dim(fname, dim_lst=[3]):
     dim = Image(fname).hdr['dim'][:4]
 
     if not dim[0] in dim_lst:
-        printv('\nERROR: ' + fname + ' dimension (={}) does not match the input dimension requirements (={}). '
-                'Exit program.\n'.format(dim, dim_lst), 1, 'error')
+        printv('\nERROR: File ' + fname + ' has {} dimensions. Authorized dimensions are: {}. '
+                'Exit program.\n'.format(dim[0], dim_lst), 1, 'error')
         sys.exit(2)
     else:
         return True
