2d5f1be3ef86b7a969b71a98983020107c34e572
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-02-28 12:31:43 -0500

    math: New module

---
+++ spinalcordtoolbox/math.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+# Functions that perform mathematical operations on an image.
+
+from __future__ import absolute_import
+
+import logging
+
+logger = logging.getLogger(__name__)
+
