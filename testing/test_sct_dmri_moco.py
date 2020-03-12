0f9210188c11a7cf6b656b5a6465bd53440f6806
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-03-12 16:56:50 -0400

    Added test for sagittal image

--- testing/test_sct_dmri_moco.py
+++ testing/test_sct_dmri_moco.py
@@ -10,12 +10,21 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
+import sct_utils as sct
+
+
 def init(param_test):
     """
     Initialize class: param_test
     """
+    # Reorient image to sagittal for testing another orientation (and crop to save time)
+    sct.run('sct_image -i dmri/dmri.nii.gz -setorient AIL -o dmri/dmri_AIL.nii', verbose=0)
+    sct.run('sct_crop_image -i dmri_AIL.nii -zmin 19 -zmax 21 -o dmri_AIL_crop.nii', verbose=0)
     # initialization
-    default_args = ['-i dmri/dmri.nii.gz -bvec dmri/bvecs.txt -g 3 -x nn -r 0']
+    default_args = [
+        '-i dmri/dmri.nii.gz -bvec dmri/bvecs.txt -g 3 -x nn -r 0',
+        '-i dmri/dmri_AIL_crop.nii -bvec dmri/bvecs.txt -g 3 -x nn -r 0']
+
     # assign default params
     if not param_test.args:
         param_test.args = default_args
