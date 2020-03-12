03d07dc07cd7e4064da2462415656bc60b5b7c73
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-03-12 17:23:23 -0400

    Added test with gaussian mask

--- testing/test_sct_dmri_moco.py
+++ testing/test_sct_dmri_moco.py
@@ -20,9 +20,12 @@ def init(param_test):
     # Reorient image to sagittal for testing another orientation (and crop to save time)
     sct.run('sct_image -i dmri/dmri.nii.gz -setorient AIL -o dmri/dmri_AIL.nii', verbose=0)
     sct.run('sct_crop_image -i dmri/dmri_AIL.nii -zmin 19 -zmax 21 -o dmri/dmri_AIL_crop.nii', verbose=0)
+    # Create Gaussian mask for testing
+    sct.run('sct_create_mask -i dmri/dmri_T0000.nii.gz -p center -size 5mm -f gaussian -o dmri/mask.nii', verbose=0)
     # initialization
     default_args = [
         '-i dmri/dmri.nii.gz -bvec dmri/bvecs.txt -g 3 -x nn -r 0',
+        '-i dmri/dmri.nii.gz -bvec dmri/bvecs.txt -g 3 -m dmri/mask.nii -r 0',
         '-i dmri/dmri_AIL_crop.nii -bvec dmri/bvecs.txt -g 3 -x nn -r 0']
 
     # assign default params
