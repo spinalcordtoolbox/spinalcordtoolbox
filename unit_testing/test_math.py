e8189d3ea161c008a8a6a71940141071e6b88a76
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-03-18 16:40:11 -0400

    Now testing Image inputs and erode()

--- unit_testing/test_math.py
+++ unit_testing/test_math.py
@@ -7,7 +7,6 @@
 from __future__ import absolute_import
 import sys
 import os
-import pytest
 import numpy as np
 import datetime
 
@@ -22,16 +21,12 @@ from create_test_data import dummy_blob
 VERBOSE = int(os.getenv('SCT_VERBOSE', 0))
 DUMP_IMAGES = bool(os.getenv('SCT_DEBUG_IMAGES', False))
 
-# Generate a list of dummy images with single pixel in the middle
-list_im = [
-    # test area
-    (dummy_blob(size_arr=(9, 9, 9), coordvox=(4, 4, 4))),
-    ]
 
 # noinspection 801,PyShadowingNames
-@pytest.mark.parametrize('im', list_im)
-def test_morphomath(im):
+def test_dilate():
 
+    # Create dummy image with single pixel in the middle
+    im = dummy_blob(size_arr=(9, 9, 9), coordvox=(4, 4, 4))
     if DUMP_IMAGES:
         im.save('tmp_dummy_im_'+datetime.now().strftime("%Y%m%d%H%M%S%f")+'.nii.gz')
 
@@ -43,7 +38,7 @@ def test_morphomath(im):
     data_dil = sct.math.dilate(im.data, size=3, shape='cube')
     assert np.array_equal(data_dil[4, 2:7, 4], np.array([0, 1, 1, 1, 0]))
 
-    # cube (only asserting along one dimension for convenience)
+    # ball (only asserting along one dimension for convenience)
     data_dil = sct.math.dilate(im.data, size=0, shape='ball')
     assert np.array_equal(data_dil[4, 2:7, 4], np.array([0, 0, 1, 0, 0]))
     data_dil = sct.math.dilate(im.data, size=1, shape='ball')
@@ -58,3 +53,21 @@ def test_morphomath(im):
     data_dil = sct.math.dilate(im.data, size=1, shape='disk', dim=2)
     assert np.array_equal(data_dil[4, 2:7, 4], np.array([0, 1, 1, 1, 0]))
     assert np.array_equal(data_dil[2:7, 4, 4], np.array([0, 1, 1, 1, 0]))
+
+    # test with Image as input
+    im_dil = sct.math.dilate(im, size=1, shape='cube')
+    assert np.array_equal(im_dil.data[4, 2:7, 4], np.array([0, 0, 1, 0, 0]))
+
+
+# noinspection 801,PyShadowingNames
+def test_erode():
+    # Create dummy image with single pixel
+    im = dummy_blob(size_arr=(9, 9, 9), coordvox=(4, 4, 4))
+    # Dilate it
+    im_dil = sct.math.dilate(im, size=1, shape='ball')
+    # Erode it
+    im_dil_erode = sct.math.erode(im_dil, size=1, shape='ball')
+    assert np.array_equal(np.where(im_dil_erode.data), (np.array([4]), np.array([4]), np.array([4])))
+    # Test with data as input
+    data_dil_erode = sct.math.erode(im_dil.data, size=1, shape='ball')
+    assert np.array_equal(np.where(data_dil_erode), (np.array([4]), np.array([4]), np.array([4])))
