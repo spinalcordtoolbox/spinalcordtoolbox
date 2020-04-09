762251fb93b18bddfe9af8b3fc4fd0b747396e87
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-04-09 18:25:26 -0400

    deepseg/core.py: Backbone for postprocessing

--- spinalcordtoolbox/deepseg/core.py
+++ spinalcordtoolbox/deepseg/core.py
@@ -8,6 +8,10 @@ import logging
 import nibabel as nib
 import ivadomed as imed
 import ivadomed.utils
+import ivadomed.postprocessing
+
+import spinalcordtoolbox as sct
+import spinalcordtoolbox.deepseg.models
 
 from sct_utils import add_suffix
 
@@ -22,17 +26,23 @@ class ParamDeepseg:
         self.output_suffix = '_seg'
         self.remove_temp_files = 1
         self.verbose = 1
+        # TODO: add threshold, keep_big_obj
 
 
-def segment_nifti(fname_image, path_model):
+def segment_nifti(fname_image, folder_model):
     """
     Segment a nifti file.
 
     :param fname_image: str: Filename of the image to segment.
-    :param path_model: str: Path to the deep learning model.
+    :param folder_model: str: Folder that encloses the deep learning model.
     :return: fname_out: str: Output filename.
     """
-    nii_seg = imed.utils.segment_volume(path_model, fname_image)
+
+    nii_seg = imed.utils.segment_volume(folder_model, fname_image)
+
+    # TODO: postprocessing based on model (info to add in model's json), and if user asked for it (arg)
+    metadata = sct.deepseg.models.get_metadata(folder_model)
+    imed.postprocessing.threshold_predictions_nib(nii_seg, metadata['threshold'])
 
     # TODO: use args to get output name
     fname_out = add_suffix(fname_image, '_seg')
