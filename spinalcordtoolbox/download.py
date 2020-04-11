9ad24d437ea87d3c10e81d7f5538921f5d3830ef
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-04-11 15:16:26 -0400

    download.py: Cosmetic

--- spinalcordtoolbox/download.py
+++ spinalcordtoolbox/download.py
@@ -75,7 +75,7 @@ def download_data(urls):
     # loop through URLs
     for url in urls:
         try:
-            logger.info('\nTrying URL: %s' % url)
+            logger.info('Trying URL: %s' % url)
             retry = Retry(total=3, backoff_factor=0.5, status_forcelist=[500, 503, 504])
             session = requests.Session()
             session.mount('https://', HTTPAdapter(max_retries=retry))
@@ -89,12 +89,11 @@ def download_data(urls):
                 continue
 
             tmp_path = os.path.join(tempfile.mkdtemp(), filename)
-            logger.info('Downloading %s...' % filename)
+            logger.info('Downloading: %s' % filename)
 
             with open(tmp_path, 'wb') as tmp_file:
                 total = int(response.headers.get('content-length', 1))
-                tqdm_bar = tqdm(total=total, unit='B', unit_scale=True,
-                                desc="Status", ascii=True)
+                tqdm_bar = tqdm(total=total, unit='B', unit_scale=True, desc="Status", ascii=False, position=0)
 
                 for chunk in response.iter_content(chunk_size=8192):
                     if chunk:
@@ -108,14 +107,14 @@ def download_data(urls):
         except Exception as e:
             logger.warning("Link download error, trying next mirror (error was: %s)" % e)
     else:
-        logger.error('\nDownload error')
+        logger.error('Download error')
 
 
 def unzip(compressed, dest_folder):
     """
     Extract compressed file to the dest_folder. Can handle .zip, .tar.gz.
     """
-    logger.info('\nUnzip data to: %s' % dest_folder)
+    logger.info('Unzip data to: %s' % dest_folder)
     if compressed.endswith('zip'):
         try:
             zf = zipfile.ZipFile(compressed)
@@ -156,7 +155,8 @@ def install_data(url, dest_folder):
         extracted_files_paths.append(os.path.join(os.path.abspath(dest_tmp_folder), extracted_file))
 
     # Check if files and folder already exists
-    logger.info("\nCheck if folder already exists on the destination path...")
+    logger.info("Destination folder: {}".format(dest_folder))
+    logger.info("Checking if folder already exists...")
     for data_extracted_name in extracted_files:
         fullpath_dest = os.path.join(dest_folder, data_extracted_name)
         if os.path.isdir(fullpath_dest):
@@ -168,7 +168,7 @@ def install_data(url, dest_folder):
         # Copy the content of source to destination (and create destination folder)
     distutils.dir_util.copy_tree(dest_tmp_folder, dest_folder)
 
-    logger.info("\nRemove temporary folders...")
+    logger.info("Removing temporary folders...")
     try:
         shutil.rmtree(os.path.split(tmp_file)[0])
         shutil.rmtree(dest_tmp_folder)
