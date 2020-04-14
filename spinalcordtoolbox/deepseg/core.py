de2744b13991c29d3ffe906523d41b2139af031a
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-04-14 16:00:19 -0400

    deepseg/core.py: Refactored postproc functions in a class

--- spinalcordtoolbox/deepseg/core.py
+++ spinalcordtoolbox/deepseg/core.py
@@ -33,6 +33,58 @@ class ParamDeepseg:
         # TODO: add threshold, keep_big_obj
 
 
+class PostProcessing:
+    """
+    Deals with post-processing of the segmentation.
+    """
+    def __init__(self, param, metadata):
+        """
+        :param param:
+        :param metadata:
+        """
+        self.param = param
+        self.metadata = metadata
+
+    def threshold(self, nii_seg):
+        """
+        Threshold the prediction. For no prediction, set 'threshold' to 0.
+        """
+        if self.param.threshold:
+            thr = self.param.threshold
+        else:
+            if 'threshold' in self.metadata:
+                thr = self.param.threshold
+            else:
+                logger.warning("'threshold' is not defined in the model json file. Using threshold of: {}".format(
+                    DEFAULT_THRESHOLD))
+                thr = DEFAULT_THRESHOLD
+        if thr:
+            nii_seg = imed.postprocessing.threshold_predictions_nib(nii_seg, thr)
+        return nii_seg
+
+    def keep_largest_object(self, nii_seg):
+        """
+        Only keep largest object
+        """
+        if self.param.keep_largest_object:
+            if self.threshold:
+                # Fetch axis corresponding to superior-inferior direction
+                # TODO: move that code in image
+                affine = nii_seg.get_header().get_best_affine()
+                code = nib.orientations.aff2axcodes(affine)
+                if 'I' in code:
+                    axis_infsup = code.index('I')
+                elif 'S' in code:
+                    axis_infsup = code.index('S')
+                else:
+                    raise ValueError(
+                        "Neither I nor S is present in code: {}, for affine matrix: {}".format(code, affine))
+                nii_seg = imed.postprocessing.keep_largest_object_per_slice_nib(nii_seg, axis=axis_infsup)
+            else:
+                logger.warning("Algorithm 'keep largest object' can only be run on binary segmentation.")
+            return nii_seg
+
+
 def segment_nifti(fname_image, folder_model, param):
     """
     Segment a nifti file.
@@ -45,37 +97,11 @@ def segment_nifti(fname_image, folder_model, param):
 
     nii_seg = imed.utils.segment_volume(folder_model, fname_image)
 
-    # TODO: postprocessing based on model (info to add in model's json), and if user asked for it (arg)
+    # Postprocessing
     metadata = sct.deepseg.models.get_metadata(folder_model)
-
-    # Threshold the prediction. For no prediction, set threshold to 0.
-    if param.threshold:
-        threshold = param.threshold
-    else:
-        if 'threshold' in metadata:
-            threshold = param.threshold
-        else:
-            logger.warning("'threshold' is not defined in the model json file. Using threshold of: {}".format(DEFAULT_THRESHOLD))
-            threshold = DEFAULT_THRESHOLD
-    if threshold:
-        nii_seg = imed.postprocessing.threshold_predictions_nib(nii_seg, threshold)
-
-    # Only keep largest object
-    if param.keep_largest_object:
-        if threshold:
-            # Fetch axis corresponding to superior-inferior direction
-            # TODO: move that code in image
-            affine = nii_seg.get_header().get_best_affine()
-            code = nib.orientations.aff2axcodes(affine)
-            if 'I' in code:
-                axis_infsup = code.index('I')
-            elif 'S' in code:
-                axis_infsup = code.index('S')
-            else:
-                raise ValueError("Neither I nor S is present in code: {}, for affine matrix: {}".format(code, affine))
-            nii_seg = imed.postprocessing.keep_largest_object_per_slice_nib(nii_seg, axis=axis_infsup)
-        else:
-            logger.warning("Algorithm 'keep largest object' can only be run on binary segmentation.")
+    postproc = PostProcessing(param, metadata)
+    nii_seg = postproc.threshold(nii_seg)
+    nii_seg = postproc.keep_largest_object(nii_seg)
 
     # TODO: use args to get output name
     fname_out = sct.utils.add_suffix(fname_image, '_seg')
