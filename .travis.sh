0aec6cb795aeefd99e089bf895012c6f8c0e8941
Nick Guenther <nguenthe@uwaterloo.ca>
2020-05-09 17:59:20 -0400

    Let conda load the sct PATH for the tests.
    Editing the PATH manually was redundant, since conda activate already makes sct_download_data available.

--- .travis.sh
+++ .travis.sh
@@ -35,16 +35,14 @@ echo Installing SCT
 yes | ASK_REPORT_QUESTION=false ./install_sct
 echo $?
 echo "... STATUS"
-PATH="$PATH:$PWD/bin"
 
 echo *** CHECK PATH ***
-echo $PATH  # Make sure PATH includes sct/bin folder
 ls -lA bin  # Make sure all binaries and aliases are there
-sct_download_data -d sct_testing_data  # for tests
 source python/etc/profile.d/conda.sh  # to be able to call conda
 conda activate venv_sct  # reactivate conda for the pip install below
 
 echo *** UNIT TESTS ***
+sct_download_data -d sct_testing_data  # for tests
 pytest
 
 echo *** INTEGRATION TESTS ***
