b8de08981208c4fbf02cb92d535d2b059ecb91b1
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-05-10 14:57:09 -0400

    utils.py: moved some functions from sct_utils

--- spinalcordtoolbox/utils.py
+++ spinalcordtoolbox/utils.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8
 # Collection of useful functions
 
-from __future__ import absolute_import
 
 import io
 import os
 import re
+import tempfile
+import datetime
 import logging
 import argparse
 import subprocess
@@ -115,6 +116,23 @@ class SmartFormatter(argparse.HelpFormatter):
         return argparse.HelpFormatter._split_lines(self, text, width)
 
 
+def add_suffix(fname, suffix):
+    """
+    Add suffix between end of file name and extension.
+
+    :param fname: absolute or relative file name. Example: t2.nii
+    :param suffix: suffix. Example: _mean
+    :return: file name with suffix. Example: t2_mean.nii
+
+    Examples:
+
+    - add_suffix(t2.nii, _mean) -> t2_mean.nii
+    - add_suffix(t2.nii.gz, a) -> t2a.nii.gz
+    """
+    parent, stem, ext = extract_fname(fname)
+    return os.path.join(parent, stem + suffix + ext)
+
+
 def check_exe(name):
     """
     Ensure that a program exists
@@ -136,6 +154,105 @@ def check_exe(name):
                 return exe_file
 
     return None
+
+
+def extract_fname(fpath):
+    """
+    Split a full path into a parent folder component, filename stem and extension.
+
+    Note: for .nii.gz the extension is understandably .nii.gz, not .gz
+    (``os.path.splitext()`` would want to do the latter, hence the special case).
+    """
+    parent, filename = os.path.split(fpath)
+    if filename.endswith(".nii.gz"):
+        stem, ext = filename[:-7], ".nii.gz"
+    else:
+        stem, ext = os.path.splitext(filename)
+    return parent, stem, ext
+
+
+def parse_num_list(str_num):
+    """
+    Parse numbers in string based on delimiter: , or :
+    Examples:
+      '' -> []
+      '1,2,3' -> [1, 2, 3]
+      '1:3,4' -> [1, 2, 3, 4]
+      '1,1:4' -> [1, 2, 3, 4]
+    :param str_num: string
+    :return: list of ints
+    """
+    list_num = list()
+
+    if not str_num:
+        return list_num
+
+    elements = str_num.split(",")
+    for element in elements:
+        m = re.match(r"^\d+$", element)
+        if m is not None:
+            val = int(element)
+            if val not in list_num:
+                list_num.append(val)
+            continue
+        m = re.match(r"^(?P<first>\d+):(?P<last>\d+)$", element)
+        if m is not None:
+            a = int(m.group("first"))
+            b = int(m.group("last"))
+            list_num += [ x for x in range(a, b+1) if x not in list_num ]
+            continue
+        raise ValueError("unexpected group element {} group spec {}".format(element, str_num))
+
+    return list_num
+
+
+def parse_num_list_inv(list_int):
+    """
+    Take a list of numbers and output a string that reduce this list based on delimiter: ; or :
+    Note: we use ; instead of , for compatibility with csv format.
+    Examples:
+      [] -> ''
+      [1, 2, 3] --> '1:3'
+      [1, 2, 3, 5] -> '1:3;5'
+    :param list_int: list of ints
+    :return: str_num: string
+    """
+    # deal with empty list
+    if not list_int or list_int is None:
+        return ''
+    # Sort list in increasing number
+    list_int = sorted(list_int)
+    # initialize string
+    str_num = str(list_int[0])
+    colon_is_present = False
+    # Loop across list elements and build string iteratively
+    for i in range(1, len(list_int)):
+        # if previous element is the previous integer: I(i-1) = I(i)-1
+        if list_int[i] == list_int[i-1] + 1:
+            # if ":" already there, update the last chars (based on the number of digits)
+            if colon_is_present:
+                str_num = str_num[:-len(str(list_int[i-1]))] + str(list_int[i])
+            # if not, add it along with the new int value
+            else:
+                str_num += ':' + str(list_int[i])
+                colon_is_present = True
+        # I(i-1) != I(i)-1
+        else:
+            str_num += ';' + str(list_int[i])
+            colon_is_present = False
+
+    return str_num
+
+
+def tmp_create(basename=None):
+    """Create temporary folder and return its path
+    """
+    prefix = "sct-%s-" % datetime.datetime.now().strftime("%Y%m%d%H%M%S.%f")
+    if basename:
+        prefix += "%s-" % basename
+    tmpdir = tempfile.mkdtemp(prefix=prefix)
+    logger.info("Creating temporary folder (%s)" % tmpdir)
+    return tmpdir
 
 
 def __get_branch():
@@ -215,76 +332,3 @@ def _version_string():
 __sct_dir__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 __version__ = _version_string()
 __data_dir__ = os.path.join(__sct_dir__, 'data')
-
-
-def parse_num_list(str_num):
-    """
-    Parse numbers in string based on delimiter: , or :
-    Examples:
-      '' -> []
-      '1,2,3' -> [1, 2, 3]
-      '1:3,4' -> [1, 2, 3, 4]
-      '1,1:4' -> [1, 2, 3, 4]
-    :param str_num: string
-    :return: list of ints
-    """
-    list_num = list()
-
-    if not str_num:
-        return list_num
-
-    elements = str_num.split(",")
-    for element in elements:
-        m = re.match(r"^\d+$", element)
-        if m is not None:
-            val = int(element)
-            if val not in list_num:
-                list_num.append(val)
-            continue
-        m = re.match(r"^(?P<first>\d+):(?P<last>\d+)$", element)
-        if m is not None:
-            a = int(m.group("first"))
-            b = int(m.group("last"))
-            list_num += [ x for x in range(a, b+1) if x not in list_num ]
-            continue
-        raise ValueError("unexpected group element {} group spec {}".format(element, str_num))
-
-    return list_num
-
-
-def parse_num_list_inv(list_int):
-    """
-    Take a list of numbers and output a string that reduce this list based on delimiter: ; or :
-    Note: we use ; instead of , for compatibility with csv format.
-    Examples:
-      [] -> ''
-      [1, 2, 3] --> '1:3'
-      [1, 2, 3, 5] -> '1:3;5'
-    :param list_int: list of ints
-    :return: str_num: string
-    """
-    # deal with empty list
-    if not list_int or list_int is None:
-        return ''
-    # Sort list in increasing number
-    list_int = sorted(list_int)
-    # initialize string
-    str_num = str(list_int[0])
-    colon_is_present = False
-    # Loop across list elements and build string iteratively
-    for i in range(1, len(list_int)):
-        # if previous element is the previous integer: I(i-1) = I(i)-1
-        if list_int[i] == list_int[i-1] + 1:
-            # if ":" already there, update the last chars (based on the number of digits)
-            if colon_is_present:
-                str_num = str_num[:-len(str(list_int[i-1]))] + str(list_int[i])
-            # if not, add it along with the new int value
-            else:
-                str_num += ':' + str(list_int[i])
-                colon_is_present = True
-        # I(i-1) != I(i)-1
-        else:
-            str_num += ';' + str(list_int[i])
-            colon_is_present = False
-
-    return str_num
