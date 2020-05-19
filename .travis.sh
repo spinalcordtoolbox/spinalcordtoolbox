ebdf63da2aaa1fe2be21834e9bd7ba6f6ac72dfa
Nick Guenther <nguenthe@uwaterloo.ca>
2020-05-19 05:08:57 -0400

    .travis.sh: future-proof WSL version pinning

--- .travis.sh
+++ .travis.sh
@@ -14,6 +14,13 @@ if [ -n "$DOCKER_IMAGE" ]; then
     ./util/dockerize.sh ./.ci.sh
 elif [ "${TRAVIS_OS_NAME}" = "windows" ]; then
     choco install wsl-ubuntu-1804
+     # or, instead of choco, use curl + powershell:
+     # https://docs.microsoft.com/en-us/windows/wsl/install-manual#downloading-distros-via-the-command-line
+    # wsl --setdefault "Ubuntu-18.04"
+     # TODO: Travis's version of wsl is too old for --setdefault.
+     # Instead we trust that wsl will default to the installed
+     # Ubuntu because it is the only option, but it would be
+     # better to be explicit when it becomes possible.
 
     # disable apt's helpful (and build-breaking) interactive mode
     # https://linuxhint.com/debian_frontend_noninteractive/
