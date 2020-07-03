3fdbf4fb40879240260a9af429bf4dcbd3ee0375
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-03 11:45:22 -0400

    Fixed passing of args inside sub-function

--- scripts/sct_run_batch.py
+++ scripts/sct_run_batch.py
@@ -150,7 +150,7 @@ def run_single(subj_dir, task, task_args, path_segmanual, path_data, path_result
         'PATH_RESULTS': path_results,
         'PATH_LOG': path_log,
         'PATH_QC': path_qc,
-        'ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS': str(args.itk_threads),
+        'ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS': str(itk_threads),
         'SCT_PROGRESS_BAR': 'off'
     })
 
