38a282d52f1e3bb19f3592ab4d98f3ee5e168f0a
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-08 08:26:52 -0400

    Figured out what segmentation technique is reproducible across platforms; improve test sensitivity. (#2774)
    * Added justification for 0.9 threshold
    
    * Removed unecessary imports
    
    * Using t2_seg-manual name for ground truth
    
    Note: this will break as sct_testing_data has not been updated yet
    
    * Removed integrity testing
    
    * Now testing for higher-level function deep_segmentation_spinalcord
    
    * Parametrized testing (scalable to other params)
    
    * Parametrized unit testing for test_segment_nifti for scalability
    
    * Added math.dice() function
    
    * Added integrity testing with ground truth
    
    * Moved integrity testing for centerline to pytest
    
    * Increased sensitivity of test for optic by asserting Dice of 1.0
    
    * Added dwi and t2s contrasts for get_centerline_optic()
    
    * Fixed inconsistent usage
    
    Fixes #2772
    
    * Removed unecessary lines (because integrity test moved to pytest)
    
    * Fixed wrong path to dmri/ data
    
    * Fixed missing contrast; replaced dice=1 by np.all(a==b)
    
    * replaced dice=1 by np.all(a==b)
    
    * replaced dice=1 by np.all(a==b), removed old imports
    
    * Updated link to sct_testing_data
    
    * Fixed wrong name for fname_centerline-optic
    
    * Fixed bug in assertion
    
    * Clarified import of deepseg_sc
    
    * Fixed issue caused by the missing of keras call
    
    This line: K.set_image_data_format("channels_last")
    
    * PEP8 fixup
    
    * Updated value to match new sct_testing_data mask
    
    * Updated link to sct_testing_data

--- testing/test_sct_label_vertebrae.py
+++ testing/test_sct_label_vertebrae.py
@@ -16,8 +16,8 @@ def init(param_test):
     Initialize class: param_test
     """
     # initialization
-    default_args = ['-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -c t2 -initfile t2/init_label_vertebrae.txt -t template -qc testing-qc',
-                    '-i t2/t2.nii.gz -s t2/t2_seg.nii.gz -c t2 -discfile t2/labels.nii.gz']
+    default_args = ['-i t2/t2.nii.gz -s t2/t2_seg-manual.nii.gz -c t2 -initfile t2/init_label_vertebrae.txt -t template -qc testing-qc',
+                    '-i t2/t2.nii.gz -s t2/t2_seg-manual.nii.gz -c t2 -discfile t2/labels.nii.gz']
     # assign default params
     if not param_test.args:
         param_test.args = default_args
