9e23c5a4dbe22898882fd430bc015384b31033ab
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-08-05 10:24:37 -0400

    Removed Recommonmark
    Trying to fix https://github.com/neuropoly/spinalcordtoolbox/pull/2814#issuecomment-669168870

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -40,8 +40,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'recommonmark']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,8 +51,8 @@ autoclass_content = 'both'
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
