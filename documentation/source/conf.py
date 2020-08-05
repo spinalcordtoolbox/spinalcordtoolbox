16af99d4904668834236f1b307009884a1bb0527
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-08-05 10:10:42 -0400

    Added .md in source_suffix in conf.py
    Trying to fix https://github.com/neuropoly/spinalcordtoolbox/pull/2814#issuecomment-669168870

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -52,8 +52,8 @@ autoclass_content = 'both'
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
