e272b421eaca4536a914115939f2e5398f6d491e
Joshua Newton <joshuacwnewton@gmail.com>
2020-08-14 12:51:17 -0700

    sct_get_centerline.py: Convert msct_parser

--- scripts/sct_get_centerline.py
+++ scripts/sct_get_centerline.py
@@ -4,11 +4,12 @@ from __future__ import division, absolute_import
 
 import os
 import sys
+import argparse
 
 import numpy as np
 
 import sct_utils as sct
-from msct_parser import Parser
+from spinalcordtoolbox.utils import Metavar, SmartFormatter, ActionCreateFolder
 from spinalcordtoolbox.image import Image
 from spinalcordtoolbox.centerline.core import ParamCenterline, get_centerline, _call_viewer_centerline
 from spinalcordtoolbox.reports.qc import generate_qc
@@ -16,99 +17,126 @@ from spinalcordtoolbox.reports.qc import generate_qc
 
 def get_parser():
     # Initialize the parser
-    parser = Parser(__file__)
-    parser.usage.set_description("""This function extracts the spinal cord centerline. Three methods are
-    available: OptiC (automatic), Viewer (manual) and Fitseg (applied on segmented image). These functions output (i) a NIFTI file with labels corresponding
-    to the discrete centerline, and (ii) a csv file containing the float (more precise) coordinates of the centerline
-    in the RPI orientation. \n\nReference: C Gros, B De Leener, et al. Automatic spinal cord
-    localization, robust to MRI contrast using global curve optimization (2017). doi.org/10.1016/j.media.2017.12.001""")
+    parser = argparse.ArgumentParser(
+        description=(
+            "This function extracts the spinal cord centerline. Three methods are available: OptiC (automatic), "
+            "Viewer (manual) and Fitseg (applied on segmented image). These functions output (i) a NIFTI file with "
+            "labels corresponding to the discrete centerline, and (ii) a csv file containing the float (more precise) "
+            "coordinates of the centerline in the RPI orientation. \n"
+            "\n"
+            "Reference: C Gros, B De Leener, et al. Automatic spinal cord localization, robust to MRI contrast using "
+            "global curve optimization (2017). doi.org/10.1016/j.media.2017.12.001"
+        ),
+        formatter_class=SmartFormatter,
+        add_help=None,
+        prog=os.path.basename(__file__).strip(".py")
+    )
 
-    parser.add_option(name="-i",
-                      type_value="image_nifti",
-                      description="Input image.",
-                      mandatory=True,
-                      example="t1.nii.gz")
-    parser.add_option(name="-c",
-                      type_value="multiple_choice",
-                      description="Type of image contrast. Only with method=optic.",
-                      mandatory=False,
-                      example=['t1', 't2', 't2s', 'dwi'])
-    parser.add_option(name="-method",
-                      type_value="multiple_choice",
-                      description="Method used for extracting the centerline.\n"
-                                  "optic: automatic spinal cord detection method\n"
-                                  "viewer: manual selection a few points followed by interpolation\n"
-                                  "fitseg: fit a regularized centerline on an already-existing cord segmentation. It will interpolate if slices are missing and extrapolate beyond the segmentation boundaries (i.e., every axial slice will exhibit a centerline pixel).",
-                      mandatory=False,
-                      example=['optic', 'viewer', 'fitseg'],
-                      default_value='optic')
+    mandatory = parser.add_argument_group("\nMANDATORY ARGUMENTS")
+    mandatory.add_argument(
+        '-i',
+        metavar=Metavar.file,
+        required=True,
+        help="Input image. Example: ti.nii.gz"
+    )
 
-    parser.add_option(name='-centerline-algo',
-                      type_value='multiple_choice',
-                      description='Algorithm for centerline fitting. Only relevant with -method fitseg',
-                      mandatory=False,
-                      example=['polyfit', 'bspline', 'linear', 'nurbs'],
-                      default_value='bspline')
-    parser.add_option(name='-centerline-smooth',
-                      type_value='int',
-                      description='Degree of smoothing for centerline fitting. Only for -centerline-algo {bspline, linear}.',
-                      mandatory=False,
-                      default_value=30)
-    parser.add_option(name="-o",
-                      type_value='file_output',
-                      description='File name (without extension) for the centerline output files. By default, output'
-                                  'file will be the input with suffix "_centerline"',
-                      mandatory=False,
-                      example="centerline_optic")
-    parser.add_option(name="-gap",
-                      type_value="float",
-                      description="Gap in mm between manually selected points. Only with method=viewer.",
-                      mandatory=False,
-                      default_value='20.0')
-    parser.add_option(name="-igt",
-                      type_value="image_nifti",
-                      description="File name of ground-truth centerline or segmentation (binary nifti).",
-                      mandatory=False)
-    parser.add_option(name="-v",
-                      type_value="multiple_choice",
-                      description="1: display on, 0: display off (default)",
-                      mandatory=False,
-                      example=["0", "1", "2"],
-                      default_value="1")
-    parser.add_option(name='-qc',
-                      type_value='folder_creation',
-                      description='The path where the quality control generated content will be saved',
-                      default_value=None)
-    parser.add_option(name='-qc-dataset',
-                      type_value='str',
-                      description='If provided, this string will be mentioned in the QC report as the dataset the process was run on',
-                      )
-    parser.add_option(name='-qc-subject',
-                      type_value='str',
-                      description='If provided, this string will be mentioned in the QC report as the subject the process was run on',
-                      )
+    optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
+    optional.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        help="Show this help message and exit."
+    )
+    optional.add_argument(
+        "-c",
+        metavar=Metavar.str,
+        choices=['t1', 't2', 't2s', 'dwi'],
+        help="Type of image contrast. Only with method=optic."
+    )
+    optional.add_argument(
+        "-method",
+        metavar=Metavar.str,
+        choices=['optic', 'viewer', 'fitseg'],
+        default='optic',
+        help=("Method used for extracting the centerline.\n"
+              "    - optic: automatic spinal cord detection method\n"
+              "    - viewer: manual selection a few points followed by interpolation\n"
+              "    - fitseg: fit a regularized centerline on an already-existing cord segmentation. It will "
+              "interpolate if slices are missing and extrapolate beyond the segmentation boundaries (i.e., every "
+              "axial slice will exhibit a centerline pixel).")
+    )
+    optional.add_argument(
+        "-centerline-algo",
+        metavar=Metavar.str,
+        choices=['polyfit', 'bspline', 'linear', 'nurbs'],
+        default='bspline',
+        help="Show this help message and exit."
+    )
+    optional.add_argument(
+        "-centerline-smooth",
+        metavar=Metavar.int,
+        type=int,
+        default=30,
+        help="Degree of smoothing for centerline fitting. Only for -centerline-algo {bspline, linear}."
+    )
+    optional.add_argument(
+        "-o",
+        metavar=Metavar.file,
+        help=("File name (without extension) for the centerline output files. By default, output file will be the "
+              "input with suffix '_centerline'. Example: 'centerline_optic'")
+    )
+    optional.add_argument(
+        "-gap",
+        metavar=Metavar.float,
+        type=float,
+        default=20.0,
+        help="Gap in mm between manually selected points. Only with method=viewer."
+    )
+    optional.add_argument(
+        "-igt",
+        metavar=Metavar.file,
+        help="File name of ground-truth centerline or segmentation (binary nifti)."
+    )
+    optional.add_argument(
+        "-v",
+        metavar=Metavar.int,
+        choices=['0', '1'],
+        default='1',
+        help="Verbose. 1: display on, 0: display off (default)"
+    )
+    optional.add_argument(
+        "-qc",
+        metavar=Metavar.folder,
+        action=ActionCreateFolder,
+        help="The path where the quality control generated content will be saved."
+    )
+    optional.add_argument(
+        "-qc-dataset",
+        metavar=Metavar.str,
+        help="If provided, this string will be mentioned in the QC report as the dataset the process was run on."
+    )
+    optional.add_argument(
+        "-qc-subject",
+        metavar=Metavar.str,
+        help="If provided, this string will be mentioned in the QC report as the subject the process was run on."
+    )
     return parser
 
 
 def run_main():
     sct.init_sct()
     parser = get_parser()
-    args = sys.argv[1:]
-    arguments = parser.parse(args)
+    arguments = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     # Input filename
-    fname_input_data = arguments["-i"]
+    fname_input_data = arguments.i
     fname_data = os.path.abspath(fname_input_data)
 
     # Method used
-    method = 'optic'
-    if "-method" in arguments:
-        method = arguments["-method"]
+    method = arguments.method
 
     # Contrast type
-    contrast_type = ''
-    if "-c" in arguments:
-        contrast_type = arguments["-c"]
+    contrast_type = arguments.c
     if method == 'optic' and not contrast_type:
         # Contrast must be
         error = 'ERROR: -c is a mandatory argument when using Optic method.'
@@ -116,23 +144,21 @@ def run_main():
         return
 
     # Gap between slices
-    interslice_gap = 10.0
-    if "-gap" in arguments:
-        interslice_gap = float(arguments["-gap"])
+    interslice_gap = arguments.gap
 
     param_centerline = ParamCenterline(
-        algo_fitting=arguments['-centerline-algo'],
-        smooth=arguments['-centerline-smooth'],
+        algo_fitting=arguments.centerline_algo,
+        smooth=arguments.centerline_smooth,
         minmax=True)
 
     # Output folder
-    if "-o" in arguments:
-        file_output = arguments["-o"]
+    if arguments.o is not None:
+        file_output = arguments.o
     else:
         path_data, file_data, ext_data = sct.extract_fname(fname_data)
         file_output = os.path.join(path_data, file_data + '_centerline')
 
-    verbose = int(arguments.get('-v'))
+    verbose = int(arguments.v)
     sct.init_sct(log_level=verbose, update=True)  # Update log level
 
     if method == 'viewer':
@@ -161,9 +187,9 @@ def run_main():
 
     sct.display_viewer_syntax([fname_input_data, file_output+'.nii.gz'], colormaps=['gray', 'red'], opacities=['', '1'])
     
-    path_qc = arguments.get("-qc", None)
-    qc_dataset = arguments.get("-qc-dataset", None)
-    qc_subject = arguments.get("-qc-subject", None)
+    path_qc = arguments.qc
+    qc_dataset = arguments.qc_dataset
+    qc_subject = arguments.qc_subject
 
     # Generate QC report
     if path_qc is not None:
