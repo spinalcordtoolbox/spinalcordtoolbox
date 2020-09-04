43a83c5fa9660fba82c0bc52bed804e9c25f3e26
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-09-04 11:40:33 -0400

    Use parametrize in unit test

--- unit_testing/test_sct_compute_mtsat.py
+++ unit_testing/test_sct_compute_mtsat.py
@@ -3,37 +3,26 @@
 from __future__ import print_function, absolute_import
 
 import os
+import pytest
 
 from spinalcordtoolbox.utils import sct_test_path
 
 import sct_compute_mtsat
 
 
-class InputParam1:
-    mt = sct_test_path('mt', 'mt1.nii.gz')
-    pd = sct_test_path('mt', 'mt0.nii.gz')
-    t1 = sct_test_path('mt', 't1w.nii.gz')
-    trmt = None
-    trpd = None
-    trt1 = None
-    famt = None
-    fapd = None
-    fat1 = None
+INPUT_PARAMS = [
+    ['-mt', sct_test_path('mt', 'mt1.nii.gz'),
+     '-pd', sct_test_path('mt', 'mt0.nii.gz'),
+     '-t1', sct_test_path('mt', 't1w.nii.gz')],
+    ['-mt', sct_test_path('mt', 'mt1.nii.gz'),
+     '-pd', sct_test_path('mt', 'mt0.nii.gz'),
+     '-t1', sct_test_path('mt', 't1w.nii.gz'),
+     '-trmt', '51', '-trpd', '52', '-trt1', '10', '-famt', '4', '-fapd', '5', '-fat1', '14'],
+    ]
 
 
-class InputParam2:
-    mt = sct_test_path('mt', 'mt1.nii.gz')
-    pd = sct_test_path('mt', 'mt0.nii.gz')
-    t1 = sct_test_path('mt', 't1w.nii.gz')
-    trmt = 1
-    trpd = 2
-    trt1 = 3
-    famt = 4
-    fapd = 5
-    fat1 = 6
-
-
-def test_with_json_sidecar():
-    sct_compute_mtsat.main(InputParam1)
+@pytest.mark.parametrize('input_params', INPUT_PARAMS)
+def test_with_json_sidecar(input_params):
+    sct_compute_mtsat.main(input_params)
     # Check if output file exists
     assert os.path.isfile(sct_test_path('mt', 'mtsat.nii.gz'))
