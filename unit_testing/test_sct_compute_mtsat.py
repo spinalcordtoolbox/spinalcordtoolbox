d91cef24d6cd3d808fdfe9ec093acaf1fa7d3ba3
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-09-03 20:37:39 -0400

    Added test for test_get_tr_and_flipangle()

--- unit_testing/test_sct_compute_mtsat.py
+++ unit_testing/test_sct_compute_mtsat.py
@@ -4,14 +4,41 @@ from __future__ import print_function, absolute_import
 
 import os
 import sys
-import pytest
 
-import spinalcordtoolbox as sct
 from spinalcordtoolbox.utils import sct_test_path
 from spinalcordtoolbox import __sct_dir__
 sys.path.append(os.path.join(__sct_dir__, 'scripts'))
 
 import sct_compute_mtsat
+
+
+class InputParam1:
+    mt = sct_test_path('mt', 'mt1.nii.gz')
+    pd = sct_test_path('mt', 'mt0.nii.gz')
+    t1 = sct_test_path('mt', 't1w.nii.gz')
+    trmt = None
+    trpd = None
+    trt1 = None
+    famt = None
+    fapd = None
+    fat1 = None
+
+
+class InputParam2:
+    mt = sct_test_path('mt', 'mt1.nii.gz')
+    pd = sct_test_path('mt', 'mt0.nii.gz')
+    t1 = sct_test_path('mt', 't1w.nii.gz')
+    trmt = 1
+    trpd = 2
+    trt1 = 3
+    famt = 4
+    fapd = 5
+    fat1 = 6
+
+
+def test_get_tr_and_flipangle():
+    assert sct_compute_mtsat.get_tr_and_flipangle(InputParam1()) == (0.03, 0.03, 0.015, 9, 9, 15)
+    assert sct_compute_mtsat.get_tr_and_flipangle(InputParam2()) == (1, 2, 3, 4, 5, 6)
 
 
 def test_with_json_sidecar():
