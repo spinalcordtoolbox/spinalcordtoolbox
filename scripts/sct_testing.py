d777173258d0e1149d5198f1954a320ba8c41b0b
Joshua Newton <joshuacwnewton@gmail.com>
2020-09-10 17:58:31 -0700

    Mimic test_sct_compute_mtsat for sct_testing
    (The existing test_sct_compute_mtsat.py is in unit_testing.)

--- scripts/sct_testing.py
+++ scripts/sct_testing.py
@@ -408,6 +408,7 @@ def get_functions_parallelizable():
         'sct_compute_ernst_angle',
         'sct_compute_hausdorff_distance',
         'sct_compute_mtr',
+        'sct_compute_mtsat',
         'sct_compute_mscc',
         'sct_compute_snr',
         'sct_concat_transfo',
