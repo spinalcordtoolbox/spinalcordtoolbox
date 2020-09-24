a1d9c3a02dd28d4921eb5299d774366a608ec8cd
Alexandru Jora <alexandru@jora.ca>
2020-09-24 14:37:37 -0400

    sct_compute_mtsat.py: more sane defaults (#2909)
    * sct_compute_mtsat.py: more sane defaults:
    - set default output files in argument parser
    - don't use same directory as input image for output images since the defaults
    reference the working directory
    
    * test_sct_comput_mtsat.py: test that both files exist + cleanup after test

--- unit_testing/test_sct_compute_mtsat.py
+++ unit_testing/test_sct_compute_mtsat.py
@@ -9,14 +9,20 @@ from spinalcordtoolbox.utils import sct_test_path
 
 import sct_compute_mtsat
 
+out_mstat = "out_mtsat.nii.gz"
+out_t1map = "out_t1map.nii.gz"
 
 INPUT_PARAMS = [
     ['-mt', sct_test_path('mt', 'mt1.nii.gz'),
      '-pd', sct_test_path('mt', 'mt0.nii.gz'),
-     '-t1', sct_test_path('mt', 't1w.nii.gz')],
+     '-t1', sct_test_path('mt', 't1w.nii.gz'),
+     '-omtsat', out_mstat,
+     '-ot1map', out_t1map],
     ['-mt', sct_test_path('mt', 'mt1.nii.gz'),
      '-pd', sct_test_path('mt', 'mt0.nii.gz'),
      '-t1', sct_test_path('mt', 't1w.nii.gz'),
+     '-omtsat', out_mstat,
+     '-ot1map', out_t1map,
      '-trmt', '51', '-trpd', '52', '-trt1', '10', '-famt', '4', '-fapd', '5', '-fat1', '14'],
     ]
 
@@ -24,5 +30,7 @@ INPUT_PARAMS = [
 @pytest.mark.parametrize('input_params', INPUT_PARAMS)
 def test_with_json_sidecar(input_params):
     sct_compute_mtsat.main(input_params)
-    # Check if output file exists
-    assert os.path.isfile(sct_test_path('mt', 'mtsat.nii.gz'))
+    # Check if output files exist
+    for f in [out_mstat, out_t1map]:
+        assert os.path.isfile(f)
+        os.remove(f)
