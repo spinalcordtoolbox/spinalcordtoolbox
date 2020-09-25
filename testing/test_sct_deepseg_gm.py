019eedd7c82bd68b3c1e46e379ce28866af64069
Joshua Newton <joshuacwnewton@gmail.com>
2020-09-25 17:40:08 -0400

    test_sct_deepseg_gm.py: Convert to pytest test

--- testing/test_sct_deepseg_gm.py
+++ testing/test_sct_deepseg_gm.py
@@ -12,9 +12,17 @@
 
 from __future__ import absolute_import
 
+import os
+
+import pytest
+import logging
+
 from spinalcordtoolbox.image import Image, compute_dice
 
+logger = logging.getLogger(__name__)
 
+
+@pytest.fixture
 def init(param_test):
     """
     Initialize class: param_test
@@ -31,10 +39,24 @@ def init(param_test):
     return param_test
 
 
-def test_integrity(param_test):
+@pytest.mark.script_launch_mode('subprocess')
+def test_integrity(init, script_runner):
     """
     Test integrity of function
     """
+    # Quirk from using init() directly as a fixture
+    param_test = init
+
+    os.chdir(param_test.path_data)
+
+    args = param_test.args[0].split(" ")
+    ret = script_runner.run('sct_deepseg_gm', *args)
+    logger.debug(f"{ret.stdout}")
+    logger.debug(f"{ret.stderr}")
+    assert ret.success
+    assert ret.returncode == 0
+    assert ret.stderr == ''
+
     # open output segmentation
     im_seg = Image(param_test.file_seg)
     # open ground truth
@@ -45,12 +67,4 @@ def test_integrity(param_test):
     param_test.output += 'Computed dice: '+str(dice_segmentation)
     param_test.output += '\nDice threshold (if computed Dice smaller: fail): '+str(param_test.dice_threshold)
 
-    if dice_segmentation < param_test.dice_threshold:
-        param_test.status = 99
-        param_test.output += '\n--> FAILED'
-    else:
-        param_test.output += '\n--> PASSED'
-
-    # update Panda structure
-    param_test.results['dice'] = dice_segmentation
-    return param_test
+    assert dice_segmentation > param_test.dice_threshold
