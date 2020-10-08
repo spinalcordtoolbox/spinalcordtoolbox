4e162615ed61fd5bb1da45c85fafe21ddda8242d
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-10-08 15:44:59 -0400

    use of tmppath

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -20,17 +20,19 @@ def test_sct_label_vertebrae_backwards_compat(script_runner):
 
 
 @pytest.mark.parametrize("is_coordinates_gt", [[2, 17, 34, 48]])
-def test_sct_label_vertebrae_disc(tmpdir, is_coordinates_gt):
-    p = tmpdir.mkdir("label_test")
+def test_sct_label_vertebrae_disc(tmp_path, is_coordinates_gt):
+    d = tmp_path / "sub"
+    d.mkdir()
+    print(d)
     subprocess.run("sct_label_vertebrae -i sct_testing_data/t2/t2.nii.gz "
-                            "-s sct_testing_data/t2/t2_seg-manual.nii.gz "
-                            "-initfile sct_testing_data/t2/init_label_vertebrae.txt -c t2 -ofolder " + str(p), shell=True)
-    nifti_label = nib.load(str(p) + "/t2_labels-disc.nii.gz")
+                   "-s sct_testing_data/t2/t2_seg-manual.nii.gz "
+                   "-initfile sct_testing_data/t2/init_label_vertebrae.txt -c t2 -ofolder " + str(d), shell=True)
+    nifti_label = nib.load(str(d) + "/t2_labels-disc.nii.gz")
     image_label = np.array(nifti_label.dataobj)
     IS_coordinates = np.nonzero(image_label)[1]  # image is AIL
     IS_coordinates.sort()
     assert np.allclose(is_coordinates_gt, IS_coordinates, atol=2)
-    assert os.path.isfile(str(p) + "/warp_straight2curve.nii.gz")
-    assert os.path.isfile(str(p) + "/warp_curve2straight.nii.gz")
-    assert os.path.isfile(str(p) + "/straight_ref.nii.gz")
+    assert os.path.isfile(str(d) + "/warp_straight2curve.nii.gz")
+    assert os.path.isfile(str(d) + "/warp_curve2straight.nii.gz")
+    assert os.path.isfile(str(d) + "/straight_ref.nii.gz")
 
