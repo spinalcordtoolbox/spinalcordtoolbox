cf663424c827a5a1962e27ebbbf54ef4b809e0d0
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-10-08 13:47:28 -0400

    parametrize test and use tmpdir

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -6,6 +6,7 @@ import subprocess
 import nibabel as nib
 import numpy as np
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -18,17 +19,15 @@ def test_sct_label_vertebrae_backwards_compat(script_runner):
     assert ret.stderr == ''
 
 
-def test_sct_label_vertebrae_disc():
+@pytest.mark.parametrize("is_coordinates_gt", [[2, 17, 34, 48]])
+def test_sct_label_vertebrae_disc(tmpdir, is_coordinates_gt):
+    p = tmpdir.mkdir("label_test")
     subprocess.check_output("sct_label_vertebrae -i sct_testing_data/t2/t2.nii.gz "
                             "-s sct_testing_data/t2/t2_seg-manual.nii.gz "
-                            "-initfile sct_testing_data/t2/init_label_vertebrae.txt -c t2 -ofolder label_test",
-                            shell=True)
-    nifti_label = nib.load("label_test/t2_labels-disc.nii.gz")
+                            "-initfile sct_testing_data/t2/init_label_vertebrae.txt -c t2 -ofolder " + str(p), shell=True)
+    nifti_label = nib.load(str(p) + "/t2_labels-disc.nii.gz")
     image_label = np.array(nifti_label.dataobj)
     IS_coordinates = np.nonzero(image_label)[1]  # image is AIL
     IS_coordinates.sort()
-    real_IS_coordinates = [2, 17, 34, 48]
-    assert np.allclose(real_IS_coordinates, IS_coordinates)
-    # cleaning
-    subprocess.check_output("rm -r label_test", shell=True)
+    assert np.allclose(is_coordinates_gt, IS_coordinates, atol=2)
 
