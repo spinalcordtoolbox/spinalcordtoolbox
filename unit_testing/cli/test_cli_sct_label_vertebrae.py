faa5a1b3965e4397391db21f42659d1ec01c8dbf
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-10-13 15:16:18 -0400

    introducing t1 testing

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -3,9 +3,8 @@ import pytest
 import logging
 import os
 import subprocess
-import nibabel as nib
 import numpy as np
-
+from spinalcordtoolbox.image import Image
 
 logger = logging.getLogger(__name__)
 
@@ -19,17 +18,22 @@ def test_sct_label_vertebrae_backwards_compat(script_runner):
     assert ret.stderr == ''
 
 
-@pytest.mark.parametrize("is_coordinates_gt", [[2, 17, 34, 48]])
-def test_sct_label_vertebrae_disc(tmp_path, is_coordinates_gt):
+@pytest.mark.parametrize("contrast,is_coordinates_gt, pref", [("t2", [2, 17, 34, 48], "t2"),
+                                                               ("t1", [2, 17, 35, 55, 68], "t1w")])
+def test_sct_label_vertebrae_disc(tmp_path, is_coordinates_gt, contrast, pref):
     d = tmp_path / "sub"
+    print(is_coordinates_gt)
     d.mkdir()
-    subprocess.run("sct_label_vertebrae -i sct_testing_data/t2/t2.nii.gz "
-                   "-s sct_testing_data/t2/t2_seg-manual.nii.gz "
-                   "-initfile sct_testing_data/t2/init_label_vertebrae.txt -c t2 -ofolder " + str(d), shell=True)
-    nifti_label = nib.load(str(d) + "/t2_labels-disc.nii.gz")
-    image_label = np.array(nifti_label.dataobj)
-    IS_coordinates = np.nonzero(image_label)[1]  # image is AIL
+    subprocess.run("sct_label_vertebrae -i sct_testing_data/" + contrast+"/" + pref + ".nii.gz "
+                   "-s sct_testing_data/" + contrast + "/" + pref + "_seg-manual.nii.gz "
+                   "-initfile sct_testing_data/" + contrast + "/init_label_vertebrae.txt -c " + contrast +
+                   " -ofolder " + str(d), shell=True)
+    nifti_label = Image(str(d) + "/" + pref + "_labels-disc.nii.gz")
+    nifti_label = nifti_label.change_orientation("RPI")
+    image_label = np.array(nifti_label.data)
+    IS_coordinates = np.nonzero(image_label)[2]  # image is AIL
     IS_coordinates.sort()
+    print(IS_coordinates)
     assert np.allclose(is_coordinates_gt, IS_coordinates, atol=2)
     assert os.path.isfile(str(d) + "/warp_straight2curve.nii.gz")
     assert os.path.isfile(str(d) + "/warp_curve2straight.nii.gz")
