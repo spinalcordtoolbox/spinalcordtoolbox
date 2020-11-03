55d34115314afa459794e851304388143ceda661
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-11-03 11:14:25 -0500

    sct_deepseg: deal with option['o'] at None

--- spinalcordtoolbox/deepseg/core.py
+++ spinalcordtoolbox/deepseg/core.py
@@ -12,6 +12,7 @@ import ivadomed as imed
 import nibabel as nib
 
 import spinalcordtoolbox as sct
+from spinalcordtoolbox import image
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +42,7 @@ def segment_nifti(fname_image, folder_model, fname_prior=None, param=None):
     nii_seg = imed.utils.segment_volume(folder_model, fname_image, options=options)
 
     # Save output seg
-    if 'o' in options:
+    if 'o' in options and options['o'] is not None:
         fname_out = options['o']
     else:
         fname_out = ''.join([sct.image.splitext(fname_image)[0], '_seg.nii.gz'])
