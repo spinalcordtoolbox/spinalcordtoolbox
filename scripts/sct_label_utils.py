43ee1c050631c56995a3835aa364983bc430252f
Joshua Newton <joshuacwnewton@gmail.com>
2020-11-09 15:50:20 -0500

    sct_label_utils.py: S-I -> I-S

--- scripts/sct_label_utils.py
+++ scripts/sct_label_utils.py
@@ -96,11 +96,11 @@ def get_parser():
         metavar=Metavar.list,
         type=list_type(':', list_type(',', int)),
         help="R|Create labels on a cord segmentation (or centerline) image defined by '-i'. Each label should be "
-             "specified using the form 'v1,v2' where 'v1' is value of the slice index along the superior-inferior "
+             "specified using the form 'v1,v2' where 'v1' is value of the slice index along the inferior-superior "
              "axis, and 'v2' is the value of the label. Separate each label with ':'. \n"
-             "Example: '-create-seg 5,1:14,2:23,3' adds three labels at S-I slice indices 5, 14, and 23.\n"
-             "You can also choose a S-I axis value of '-1' to automatically select the mid-point in the "
-             "superior-inferior direction. For example, if you know that the C2-C3 disc is centered in the S-I "
+             "Example: '-create-seg 5,1:14,2:23,3' adds three labels at I-S slice indices 5, 14, and 23.\n"
+             "You can also choose a I-S axis value of '-1' to automatically select the mid-point in the "
+             "inferior-superior direction. For example, if you know that the C2-C3 disc is centered in the I-S "
              "direction, then you can enter '-1,3' for that label instead."
     )
     func_group.add_argument(
