f224d02cc21bda187ecb36e09163244e962ff751
Joshua Newton <joshuacwnewton@gmail.com>
2020-11-09 08:51:35 -0500

    test_deepseg.py: Refactor pytest.mark.parametrize
    Proper usage so that `param` dictionary is not needed.

--- unit_testing/test_deepseg.py
+++ unit_testing/test_deepseg.py
@@ -15,16 +15,6 @@ import spinalcordtoolbox.deepseg.core
 import spinalcordtoolbox.deepseg.models
 
 
-param_deepseg = [
-    ({'fname_image':
-        sct_test_path('t2s', 't2s.nii.gz'),
-      'model':
-        os.path.join(sct.__deepseg_dir__, 't2star_sc'),
-      'fname_seg_manual':
-        sct_test_path('t2s', 't2s_seg-deepseg.nii.gz')}),
-]
-
-
 def test_install_model():
     """
     Download all models, to allow subsequent tests on the model packages.
@@ -49,18 +39,23 @@ def test_model_dict():
 
 
 # noinspection 801,PyShadowingNames
-@pytest.mark.parametrize('params', param_deepseg)
-def test_segment_nifti(params):
+@pytest.mark.parametrize('fname_image, fname_seg_manual, fname_out, model', [
+    (sct_test_path('t2s', 't2s.nii.gz'),
+     sct_test_path('t2s', 't2s_seg-deepseg.nii.gz'),
+     't2s_seg_deepseg.nii.gz',
+     os.path.join(sct.__deepseg_dir__, 't2star_sc')),
+])
+def test_segment_nifti(fname_image, fname_seg_manual, fname_out, model):
     """
     Uses the locally-installed sct_testing_data
     """
-    fname_out = 't2s_seg_deepseg.nii.gz'
-    output = sct.deepseg.core.segment_nifti(
-        params['fname_image'], params['model'], param={'o': fname_out})
+    output = sct.deepseg.core.segment_nifti(fname_image, model,
+                                            param={'o': fname_out})
     # TODO: implement integrity test (for now, just checking if output segmentation file exists)
     # Make sure output file is correct
     assert output == fname_out
     # Make sure output file exists
     assert os.path.isfile(output)
     # Compare with ground-truth segmentation
-    assert np.all(nibabel.load(output).get_fdata() == nibabel.load(params['fname_seg_manual']).get_fdata())
+    assert np.all(nibabel.load(output).get_fdata() ==
+                  nibabel.load(fname_seg_manual).get_fdata())
