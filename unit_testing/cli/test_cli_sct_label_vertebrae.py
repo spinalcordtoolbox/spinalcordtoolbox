03d15ab4a6e3c8dbd4e31a8b63215de1f2444c7c
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-11-25 16:42:13 -0500

    test that function raise error if needed

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -1,7 +1,8 @@
 from pytest_console_scripts import script_runner
 import pytest
 import logging
-
+import subprocess
+from spinalcordtoolbox.utils import init_sct, run_proc
 logger = logging.getLogger(__name__)
 
 
@@ -12,3 +13,13 @@ def test_sct_label_vertebrae_backwards_compat(script_runner):
     logger.debug(f"{ret.stderr}")
     assert ret.success
     assert ret.stderr == ''
+
+
+def test_sct_label_vertebrae_initz_error():
+    a = 'sct_label_vertebrae -i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40'
+    proc = subprocess.Popen(a.split(' '),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            )
+    out, err = proc.communicate()
+    assert 'ValueError: --initz takes two arguments: position in superior-inferior direction, label value' in str(err)
