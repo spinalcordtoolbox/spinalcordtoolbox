5522f7f28f7c5464e07d41cd8cec5b26d4f4c9ef
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-12-04 16:23:02 -0500

    adde test for -o flag (sct_detect_pmj

--- unit_testing/cli/test_cli_sct_detect_pmj.py
+++ unit_testing/cli/test_cli_sct_detect_pmj.py
@@ -1,6 +1,8 @@
 from pytest_console_scripts import script_runner
 import pytest
 import logging
+import os
+import subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -12,3 +14,9 @@ def test_sct_detect_pmj_backwards_compat(script_runner):
     logger.debug(f"{ret.stderr}")
     assert ret.success
     assert ret.stderr == ''
+
+def test_sct_detect_pmj_o_flag():
+    command = 'sct_detect_pmj -i sct_testing_data/template/template//PAM50_small_t2.nii.gz -igt sct_testing_data/template/template/PAM50_small_t2_pmj_manual.nii.gz  -c t2 -o test_pmj.nii.gz'
+    subprocess.check_output(command, shell=True)
+    assert os.path.isfile('test_pmj.nii.gz')
+    os.remove('test_pmj.nii.gz')
