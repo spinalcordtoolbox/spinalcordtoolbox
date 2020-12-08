1a6530534e865d273ec23f957704a7e4a87d7cd2
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-12-08 16:00:01 -0500

    adding unit test to check if clean-lables option runs

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -20,9 +20,18 @@ def test_sct_label_vertebrae_initz_error():
         sct_label_vertebrae.main(command.split())
 
 
-def test_sct_label_vertebrae_high_value_warning(caplog):
-    command = '-i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40,19'
+def test_sct_label_vertebrae_high_value_warning(caplog, tmp_path):
+    command = '-i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40,19 -ofolder ' + tmp_path
     sct_label_vertebrae.main(command.split())
     assert 'Disc value not included in template.' in caplog.text
+
+
+def test_sct_label_vertebrae_clean_labels(tmp_path):
+    command = '-i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40,19 -clean-labels 1 -ofolder tmp_path'
+    sct_label_vertebrae.main(command.split())
+
+
+
+
 
 
