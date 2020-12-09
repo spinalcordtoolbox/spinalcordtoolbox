656925a2b6b0d8d58cbf409d081d28aea32d6fec
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-12-09 12:03:22 -0500

    tmp ath converted to string

--- unit_testing/cli/test_cli_sct_label_vertebrae.py
+++ unit_testing/cli/test_cli_sct_label_vertebrae.py
@@ -21,7 +21,7 @@ def test_sct_label_vertebrae_initz_error():
 
 
 def test_sct_label_vertebrae_high_value_warning(caplog, tmp_path):
-    command = '-i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40,19 -ofolder ' + tmp_path
+    command = '-i sct_testing_data/t2/t2.nii.gz -s sct_testing_data/t2/t2_seg-manual.nii.gz -c t2 -initz 40,19 -ofolder ' + str(tmp_path)
     sct_label_vertebrae.main(command.split())
     assert 'Disc value not included in template.' in caplog.text
 
