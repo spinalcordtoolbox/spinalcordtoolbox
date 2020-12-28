77faf7cf31e158ac5e0b791b43c4b11917c6be82
YanchunCoBIUS <qixy2201@usherbrooke.ca>
2020-12-27 23:58:58 -0500

    Fix bug in Travis CI build failed.

--- spinalcordtoolbox/utils/validation.py
+++ spinalcordtoolbox/utils/validation.py
@@ -3,12 +3,9 @@
 # Validation related utilites
 
 
-def check_dimensions_match(input_im, im_vertebral_labeling):
-    nx, ny, nz = input_im.data.shape
-    nx_vertebral, ny_vertebral, nz_vertebral = im_vertebral_labeling.data.shape
-
+def check_dimensions_match(input_metric, im_vertebral_labeling):
     # Check dimensions consistency between the input and the vertebral labeling file
-    if (nx, ny, nz) != (nx_vertebral, ny_vertebral, nz_vertebral):
+    if (input_metric.data.shape) != (im_vertebral_labeling.data.shape):
         return False
     else:
         return True
