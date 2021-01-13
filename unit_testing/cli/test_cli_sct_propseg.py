d4d612a1e9ed81098e650590a84711a88a41ef4a
lrouhier <lucas.rouhier@telecom-paristech.fr>
2021-01-13 11:41:45 +0100

    Implement -o flag for a few functions (#3081)
    * added -o argument in sct_deepseg_sc
    
    * change type of output
    
    * adding -o flag to propseg
    
    * missing extra line
    
    * add -o flag for sct_detect_pmj
    
    * change example name for consistence
    
    * add -o flag for sct_smooth_spinalcord
    
    * change for nii
    
    * trying a more elgant way
    
    * create test for -o flag
    
    * create test for -o flage (sct_smooth_spinalcord)
    
    * adde test for -o flag (sct_detect_pmj
    
    * adding test for -o flag sct_propseg
    
    * using tmp_path for propseg
    
    * using tmp_path sct_deepseg
    
    * using tmp_path for sct_detect_pmj
    
    * use tmp_path sct_smooth_sc
    
    * error with tmp path corrected
    
    * spacing
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>
    
    * changed for main
    
    * change for main
    
    * change for main
    
    * changed for main
    
    * error on merging master
    
    * remove tmp_path fixture that caused an error
    
    * double quote vs single quote
    
    Co-authored-by: Nick <nguenthe@uwaterloo.ca>
    
    * chinging import to be cleaner
    
    * using argv
    
    * removing ofolder arguments
    
    * fixing the output dir issue
    
    * Apply suggestions from code review
    
    Nitpick
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>
    Co-authored-by: Nick <nguenthe@uwaterloo.ca>

--- unit_testing/cli/test_cli_sct_propseg.py
+++ unit_testing/cli/test_cli_sct_propseg.py
@@ -1,6 +1,8 @@
 from pytest_console_scripts import script_runner
 import pytest
 import logging
+import os
+from spinalcordtoolbox.scripts import sct_propseg
 
 logger = logging.getLogger(__name__)
 
@@ -12,3 +14,9 @@ def test_sct_propseg_backwards_compat(script_runner):
     logger.debug(f"{ret.stderr}")
     assert ret.success
     assert ret.stderr == ''
+
+
+def test_sct_propseg_o_flag(tmp_path):
+    argv = ['-i', 'sct_testing_data/t2/t2.nii.gz', '-c', 't2', '-o', os.path.join(str(tmp_path), 'test_seg.nii.gz')]
+    sct_propseg.main(argv)
+    assert os.path.isfile(os.path.join(str(tmp_path), 'test_seg.nii.gz'))
