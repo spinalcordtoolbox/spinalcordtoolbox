c458338eec8d5588c61c688effdd71e9401ed75c
Alexandru Foias <afoias@polymtl.ca>
2021-01-14 15:04:19 -0500

    initial commit

---
+++ documentation/source/ref_scraper.py
@@ -0,0 +1,36 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium import webdriver
+import time
+import os
+
+# PROXY = "socks5://127.0.0.1:9150" # IP:PORT or HOST:PORT
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--proxy-server=%s' % PROXY)
+# driver = webdriver.Chrome(chrome_options=chrome_options)
+
+if __name__ == "__main__":
+    # driver = get_browser(binary=firefox_binary)
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    url = "https://scholar.google.ca/scholar?oi=bibs&hl=en&cites=10854150969866776706&as_sdt=5"
+    # url = "file:/Users/alex/Desktop/SCT.html"
+    driver.get(url)
+    citation = []
+    while True:
+        title = [element.get_attribute("textContent") for element in driver.find_elements_by_xpath('//h3[@class="gs_rt"]')]
+        authors = [element.get_attribute("textContent") for element in driver.find_elements_by_xpath('//div[@class="gs_a"]')]
+        for title, authors in zip(title,authors):
+            print(title.replace('[HTML][HTML] ',''),' - ',authors.split(',')[0], 'et al. -',authors.split('-')[1])
+            ref = title.replace('[HTML][HTML] ',''),' - ',authors.split(',')[0], 'et al. -',authors.split('-')[1]
+            citation.append(str(ref))
+            print('\n')
+        element = driver.find_element_by_xpath("//button[@class='gs_btnPR gs_in_ib gs_btn_lrge gs_btn_half gs_btn_lsu']")
+        driver.execute_script("arguments[0].click();", element)
+        time.sleep(5)
+        del title,authors
+        if element == driver.find_element_by_xpath("//button[@class='gs_btnPR gs_in_ib gs_btn_lrge gs_btn_half gs_btn_lsu']"):
+            break
+
+    print (citation)
+    driver.quit()
