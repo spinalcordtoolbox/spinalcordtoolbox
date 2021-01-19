5d25906b74fae617445232bcf57cb6423d3f2fdb
Nick Guenther <nguenthe@uwaterloo.ca>
2021-01-19 17:49:35 -0500

    Factor out locking logic to @node_scoped.
    This uses pickle to share. I don't know if that's a good idea.
    This code got kind of crazy because wrapping fixtures is hard:
    pytest does a bunch of introspection to magically come up with
    data to feed into tests (or into other fixtures!) and I needed
    to be compatible with that. Maybe there's a more reasonable way
    involving, I dunno, making a separate fixture? Or something?
    
    It also doesn't support generator-fixtures, which is unfortunate.
    The mechanism to invoke them is tricky and not part of pytest's
    public API. Maybe I'll give it a shot.

--- conftest.py
+++ conftest.py
@@ -14,7 +14,13 @@ import sys
 import os
 import logging
 from typing import Mapping
+import functools
 from hashlib import md5
+import inspect
+try:
+    import cPickle as pickle # py 3.6 doesn't have cPickle?
+except ImportError:
+    import pickle
 
 import pytest
 from pytest import fixture
@@ -52,49 +58,102 @@ def testrun_tmp_path(tmp_path_factory):
         return tmp_path_factory.getbasetemp()
 
 
+def node_scoped(fixture):
+    """
+    Ensure a fixture only runs once per *node* per test run.
+
+    When using pytest-xdist, there are multiple worker processes potentially spread out over on multiple worker machines ("nodes").
+    It causes `@fixture(scope="session")` to essentially mean `@fixture(scope="worker")` and this has caused lots of confusion:
+      https://github.com/pytest-dev/pytest-xdist/issues/271
+
+    For fixtures that need to generate shared data in-RAM, that is fine. But for fixtures that generate shared data on-disk
+    it causes redundant work and possible corruption.
+    Since this doesn't seem fixable inside of pytest (https://github.com/pytest-dev/pytest-xdist/issues/271#issuecomment-762453836)
+    this provides a workaround that creates fixtures that run once per run per node.
+
+    Usage:
+
+        @fixture(scope="session")
+        @node_scoped
+        def fixture():
+            return make_some_data()
+    """
+    from _pytest.compat import is_generator
+    if is_generator(fixture):
+        raise TypeError("node_scoped doesn't (yet) support generator-fixtures")
+
+
+    @functools.wraps(fixture)
+    def _fixture(testrun_tmp_path, *args, **kwargs):
+        #
+        # xdist means we need to deal with interprocess concurrency, so here we handle it with a lock file:
+        # whichever worker gets the lock first generates the fixture, then marks itself done using a second file.
+        # Every other worker blocks until at least the first one is done, then they will see the '.done' file and skip on.
+        # We don't try to handle cleaning up the lockfile when we're done (which can be very tricky to get right!);
+        # because we're running under pytest we can just lean on the master process to clean it up for us.
+        #
+        # Here we are using [`fcntl.lockf`](https://apenwarr.ca/log/20101213):
+        # > in C, use fcntl(), but avoid lockf(), which is not necessarily the same thing.
+        # > in python, use fcntl.lockf(), which is the same thing as fcntl() in C.
+        #
+        # There are several other options:
+        # - [`portalocker.Lock`](https://pypi.org/project/portalocker/)
+        # - [`posix_ipc.Semaphore`](http://semanchuk.com/philip/posix_ipc/)
+        # - ?
+        #
+        # This is compatible with pytest-xdist's multi-server mode, where it distributes tests out via ssh.
+        # Each node has its own fcntl locks, so workers that end up on the same host will only do the work
+        # once, while those on different hosts will have at least one of them pick it up.
+
+        fixture_path = testrun_tmp_path / "fixtures" / f"{fixture.__name__}"
+        os.makedirs(fixture_path, exist_ok=True)
+        lock = fixture_path / f"lock"
+        done = fixture_path / f"done"
+        # instead of testrun_tmp_path, per the suggestion on https://pypi.org/project/pytest-xdist/,
+        # we could use testrun_uid to make our own distinct tmpdir, or perhaps a POSIX semaphore.
+        # this is working for now.
+
+        if 'testrun_tmp_path' in inspect.signature(fixture).parameters:
+            # just in case the fixture *also* uses testrun_tmp_path
+            kwargs = {**kwargs, 'testrun_tmp_path': testrun_tmp_path}
+
+        with open(lock, "w") as lock_fd:
+            with Lockf(lock_fd):
+                if not os.path.exists(done):
+                    result = fixture(*args, **kwargs)
+                    with open(done, "wb") as result_cache:
+                        pickle.dump(result, result_cache)
+                else:
+                    with open(done, "rb") as result_cache:
+                        result = pickle.load(result_cache)
+                return result
+
+
+    # we have to *append* testrun_tmp_path to the wrapper's signature in order for pytest's fixture system to pick it up and feed it to us
+    # this could be shortened with decorator.decorator() (https://github.com/micheles/decorator/blob/master/docs/documentation.md) at the expense of an extra dependency.
+    if 'testrun_tmp_path' not in inspect.signature(fixture).parameters:
+        sig = inspect.signature(_fixture)
+        _fixture.__signature__ = sig.replace(parameters=
+                                                  {**sig.parameters,
+                                                     'testrun_tmp_path': inspect.Parameter('testrun_tmp_path', inspect.Parameter.POSITIONAL_OR_KEYWORD)}
+                                                  .values())
+
+    return _fixture
+
+
 @fixture(scope="session", autouse=True)
 # TODO: make this *not* autouse; instead, replace all calls to sct_test_path() with this fixture, so that pytest can understand the dependencies.
-def sct_testing_data(testrun_tmp_path):
+@node_scoped
+def sct_testing_data(worker_id):
     """ Download sct_testing_data prior to testing. """
-    # pytest-xdist breaks session-scoped fixtures: it silently makes them [worker-scoped instead](https://github.com/pytest-dev/pytest-xdist/issues/271).
-    #
-    # This is a concurrency issue, and an interprocess one at that, so here we handle it with a lock file:
-    # whichever worker gets the lock first generates the fixture, then marks itself done using a second file.
-    # Every other worker blocks until at least the first one is done, then they will see the '.done' file and skip on.
-    # We don't try to handle cleaning up the lockfile when we're done (which can be very tricky to get right!);
-    # because we're running under pytest we can just lean on the master process to clean it up for us.
-    #
-    # Here we are using [`fcntl.lockf`](https://apenwarr.ca/log/20101213):
-    # > in C, use fcntl(), but avoid lockf(), which is not necessarily the same thing.
-    # > in python, use fcntl.lockf(), which is the same thing as fcntl() in C.
-    #
-    # There are several other options:
-    # - [`portalocker.Lock`](https://pypi.org/project/portalocker/)
-    # - [`posix_ipc.Semaphore`](http://semanchuk.com/philip/posix_ipc/)
-    # - ?
-    #
-    # This is compatible with pytest-xdist's multi-server mode, where it distributes tests out via ssh.
-    # Each node has its own fcntl locks, so workers that end up on the same host will only do the work
-    # once, while those on different hosts will have at least one of them pick it up.
 
-    _fixture_name = "sct_testing_data" # TODO: generalize to fixture.__name__
-    lock = testrun_tmp_path / f"{_fixture_name}.lock"
-    done = testrun_tmp_path / f"{_fixture_name}.done"
-    # instead of testrun_tmp_path, per the suggestion on https://pypi.org/project/pytest-xdist/,
-    # we could use testrun_uid to make our own distinct tmpdir, or perhaps a POSIX semaphore.
-    # this is working for now.
-
-    with open(lock, "w") as lock:
-        with Lockf(lock):
-            if not os.path.exists(done):
-                if not os.path.exists(sct_test_path()):
-                    downloader.main(['-d', 'sct_testing_data', '-o', sct_test_path()])
-                with open(done, "w"): pass  # write the 'done' flag
-
-    # return the test path so clients
     # TODO: merge test_data_integrity() in here
 
-    yield sct_test_path()
+    if not os.path.exists(sct_test_path()):
+        downloader.main(['-d', 'sct_testing_data', '-o', sct_test_path()])
+
+    # return the test path so clients know they can use it
+    return sct_test_path()
 
     # TODO: delete the data ?
 
