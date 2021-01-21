2e26bac4f2759e17d145017158c5ce59cb4bf9d5
Alexandru Jora <alexandru@jora.ca>
2021-01-21 11:47:40 -0500

    Fixup regression affecting output file name in sct_smooth_spinalcord.py

--- spinalcordtoolbox/scripts/sct_smooth_spinalcord.py
+++ spinalcordtoolbox/scripts/sct_smooth_spinalcord.py
@@ -249,8 +249,7 @@ def main(argv=None):
 
     # Generate output file
     printv('\nGenerate output file...')
-    generate_output_file(os.path.join(path_tmp, "anat_rpi_straight_smooth_curved_nonzero.nii"),
-                         file_anat + '_smooth' + ext_anat)
+    generate_output_file(os.path.join(path_tmp, "anat_rpi_straight_smooth_curved_nonzero.nii"), fname_out)
 
     # Remove temporary files
     if remove_temp_files == 1:
@@ -261,7 +260,7 @@ def main(argv=None):
     elapsed_time = time.time() - start_time
     printv('\nFinished! Elapsed time: ' + str(int(np.round(elapsed_time))) + 's\n')
 
-    display_viewer_syntax([file_anat, file_anat + '_smooth'], verbose=verbose)
+    display_viewer_syntax([fname_anat, fname_out], verbose=verbose)
 
 
 if __name__ == "__main__":
