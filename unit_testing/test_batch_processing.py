c25d77618d924eee45f396137f448b93159612aa
Joshua Newton <joshuacwnewton@gmail.com>
2021-01-25 11:13:25 -0500

    Try using environment variable for zip filename

--- unit_testing/test_batch_processing.py
+++ unit_testing/test_batch_processing.py
@@ -3,10 +3,30 @@
 # pytest unit tests for the batch_processing.sh script
 
 import os
+import zipfile
+import pathlib
+
 import pytest
 
+from spinalcordtoolbox.utils.sys import sct_dir_local_path
 
-@pytest.mark.skipif(not os.path.isfile('batch_processing_results.zip'), reason="Run only for batch processing CI job")
-def test_skipif_decorator():
-    # TODO: Compare batch_processing.sh values within a certain tolerance. This is just to check "skipif"
-    pass
+
+@pytest.fixture()
+def unzipped_batch_processing_results(tmp_path):
+    filepath = sct_dir_local_path() / pathlib.Path(os.getenv('BATCH_PROCESSING_ZIP_FILEPATH'))
+    assert filepath.suffix == ".zip"
+    output_dir = tmp_path / filepath.stem  # Use filename as dirname ('.stem' strips extension)
+
+    if not output_dir.is_dir():
+        with zipfile.ZipFile(filepath, 'r') as zip_ref:
+            zip_ref.extractall(path=output_dir)
+
+    return output_dir
+
+
+@pytest.mark.skipif(not os.getenv('BATCH_PROCESSING_ZIP_FILEPATH'),  # This should be set by the CI workflow file
+                    reason="Run only for batch processing CI job")
+def test_skipif_decorator(unzipped_batch_processing_results):
+    # TODO: Compare batch_processing.sh values within a certain tolerance.
+
+    assert unzipped_batch_processing_results.stem == "batch_processing_results"
