1df22c9f2c40cb259b594de87d5bf50cfd81e8f6
claragiroux <claragiroux99@gmail.com>
2021-03-12 11:08:44 -0500

    Correct layout in sct_fmri_moco

--- spinalcordtoolbox/scripts/sct_fmri_moco.py
+++ spinalcordtoolbox/scripts/sct_fmri_moco.py
@@ -173,7 +173,8 @@ def main(argv=None):
     qc_subject = arguments.qc_subject
     if path_qc is not None:
         generate_qc(fname_in1=fname_output_data, fname_in2=param.fname_data, fname_seg=param.fname_mask,
-                    path_qc=os.path.abspath(path_qc), fps=qc_fps, dataset=qc_dataset, subject=qc_subject, process='sct_fmri_moco')
+                    path_qc=os.path.abspath(path_qc), fps=qc_fps, dataset=qc_dataset, subject=qc_subject,
+                    process='sct_fmri_moco')
 
 
 if __name__ == "__main__":
