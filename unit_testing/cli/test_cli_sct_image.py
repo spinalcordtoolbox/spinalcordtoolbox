296e9a6c4b44ddbd25d534656107b6bc6b349ddd
Joshua Newton <joshuacwnewton@gmail.com>
2021-03-30 16:31:22 -0400

    Refactor sct_header test into sct_image test

--- unit_testing/cli/test_cli_sct_image.py
+++ unit_testing/cli/test_cli_sct_image.py
@@ -2,6 +2,8 @@ from pytest_console_scripts import script_runner
 import pytest
 import logging
 
+from spinalcordtoolbox.scripts import sct_image
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,3 +14,9 @@ def test_sct_image_backwards_compat(script_runner):
     logger.debug(f"{ret.stderr}")
     assert ret.success
     assert ret.stderr == ''
+
+
+@pytest.mark.parametrize("output_format", ('sct', 'fslhd', 'nibabel'))
+def test_sct_image_show_header_no_checks(output_format):
+    """Run the CLI script without checking results."""
+    sct_image.main(argv=['-i', 'sct_testing_data/t2/t2.nii.gz', '-show-header', output_format])
