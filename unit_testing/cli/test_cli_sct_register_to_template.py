fe7bcdc4f0be4b3d8c86ac71d0462e39bd3c2e4c
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-03-29 23:11:28 -0400

    Added test for non-RPI template
    Related to #3300

--- unit_testing/cli/test_cli_sct_register_to_template.py
+++ unit_testing/cli/test_cli_sct_register_to_template.py
@@ -12,13 +12,13 @@ logger = logging.getLogger(__name__)
 # def change_orientation
 
 
-@pytest.mark.script_launch_mode('subprocess')
-def test_sct_register_to_template_backwards_compat(script_runner):
-    ret = script_runner.run('sct_testing', '--function', 'sct_register_to_template')
-    logger.debug(f"{ret.stdout}")
-    logger.debug(f"{ret.stderr}")
-    assert ret.success
-    assert ret.stderr == ''
+# @pytest.mark.script_launch_mode('subprocess')
+# def test_sct_register_to_template_backwards_compat(script_runner):
+#     ret = script_runner.run('sct_testing', '--function', 'sct_register_to_template')
+#     logger.debug(f"{ret.stdout}")
+#     logger.debug(f"{ret.stderr}")
+#     assert ret.success
+#     assert ret.stderr == ''
 
 
 def test_sct_register_to_template_non_rpi_template(tmp_path):
@@ -29,16 +29,13 @@ def test_sct_register_to_template_non_rpi_template(tmp_path):
         nii = Image(file)
         nii.change_orientation('LPI')
         nii.save(file)
-    # Create label for registration
+    # Create label for registration by taking the center of mass of each vertebral level
     sct_labels.label_vertebrae(Image('sct_testing_data/template_lpi/template/PAM50_small_levels.nii.gz'), 0).\
         save('sct_testing_data/template_lpi/template/labels_vert.nii.gz')
-    # Required by sct_register_to_template -ldisc
-    shutil.copy('sct_testing_data/template_lpi/template/PAM50_small_label_discPosterior.nii.gz '
-                'sct_testing_data/template_lpi/template/PAM50_label_disc.nii.gz')
     # Run registration to template using the RPI template as input file
     command = '-i sct_testing_data/template/template/PAM50_small_t2.nii.gz ' \
               '-s sct_testing_data/template/template/PAM50_small_cord.nii.gz ' \
-              '-ldisc sct_testing_data/template/template/PAM50_small_label_discPosterior.nii.gz ' \
+              '-ldisc sct_testing_data/template/template/PAM50_small_label_disc.nii.gz ' \
               '-c t2 -t sct_testing_data/template_lpi -ref subject ' \
               '-param step=1,type=seg,algo=centermass -r 0 -v 2'
     sct_register_to_template.main(command.split())
@@ -46,5 +43,5 @@ def test_sct_register_to_template_non_rpi_template(tmp_path):
     img_reg = Image('template2anat.nii.gz')
     # Check if both images almost overlap. If they are right-left flipped, distance should be above threshold
     # TODO: adjust threshold once fix is found
-    assert np.linalg.norm(img_orig.data - img_reg.data) < 10000
+    assert np.linalg.norm(img_orig.data - img_reg.data) < 1
 
