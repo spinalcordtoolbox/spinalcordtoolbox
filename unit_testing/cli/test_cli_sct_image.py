3f4b37e09448ba23e9571c4f77645412621c6106
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-04-08 16:26:33 -0400

    Various fixes to sct_image -display-warp (#3337)
    * Fixed call to absolutepath
    
    Fixes #3336
    
    * Removed imports inside the code
    
    * Removed dependency to fslhd
    
    The reason it was used, is because Image.dim applied to a warping field gives: [1, 1, 1, 1, 1, 1], which is an error. To get nx, ny, nz, I instead relied on data shape.
    
    * test_cli_sct_image.py: Write test for '-display-warp'
    
    * sct_image: Replace run_proc calls in visualize_warp()
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>

--- unit_testing/cli/test_cli_sct_image.py
+++ unit_testing/cli/test_cli_sct_image.py
@@ -1,8 +1,10 @@
 from pytest_console_scripts import script_runner
 import pytest
 import logging
+import os
 
 from spinalcordtoolbox.scripts import sct_image
+from spinalcordtoolbox.utils.sys import sct_test_path
 
 logger = logging.getLogger(__name__)
 
@@ -21,3 +23,11 @@ def test_sct_image_show_header_no_checks(output_format):
     """Run the CLI script without checking results. The rationale for not checking results is
     provided here: https://github.com/neuropoly/spinalcordtoolbox/pull/3317#issuecomment-811429547"""
     sct_image.main(argv=['-i', 'sct_testing_data/t2/t2.nii.gz', '-header', output_format])
+
+
+def test_sct_image_display_warp_check_output_exists():
+    """Run the CLI script and check that the warp image file was created."""
+    fname_in = 'warp_template2anat.nii.gz'
+    fname_out = 'grid_3_resample_' + fname_in
+    sct_image.main(argv=['-i', sct_test_path('t2', fname_in), '-display-warp'])
+    assert os.path.exists(sct_test_path('t2', fname_out))
