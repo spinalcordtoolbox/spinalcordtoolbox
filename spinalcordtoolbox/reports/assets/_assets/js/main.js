d441bc693412116731597e7258b42bb441b8cdbf
Alexandru Jora <alexandru@jora.ca>
2021-04-25 13:33:10 -0400

    Interactive QC assessment: Add Pass/Fail/Artifact and download YAML file (#3253)
    The specific format for the YAML file is not necessarily the final one. Some coordination with sister projects is needed, see #3353.

--- spinalcordtoolbox/reports/assets/_assets/js/main.js
+++ spinalcordtoolbox/reports/assets/_assets/js/main.js
@@ -34,23 +34,44 @@ $(document).ready(function(){
 
   $('html').keydown( function(evt) {
     var obj = $('#table tr.active');
-    // Arrow down: next subject
-    if (evt.which == 40) {
-      if (obj.length == 0 || obj.text() === "DateDatasetSubjectPathFileContrastFunctionFunction+Args") {
+    // Arrow down: next subject (or j)
+    if (evt.which == 40 || evt.which == 74) {
+      if (obj.length == 0 || obj.text() === "DateDatasetSubjectPathFileContrastFunctionFunction+ArgsQC") {
         $('#table tr:first-child').click();
       }
       else {
         obj.next().click();
       }
     }
-    // Arrow up: previous subject
-    if (evt.which == 38) {
+    // Arrow up: previous subject (or k)
+    if (evt.which == 38 || evt.which == 75) {
       if (obj.length == 0) {
         $('#table tr:last-child').click();
       }
       else {
         obj.prev().click();
       }
+    }
+    // f key (mark "failing" subjects using check, X, !)
+    if (evt.which == 70) {
+      let index = obj[obj.length - 1].getAttribute("data-index");
+      const heavy_check_mark = '\u2705'
+      const heavy_ballot_x = '\u274C'
+      const heavy_excl_mark = '\u26A0\uFE0F'
+      sct_data[index].qc = (
+          sct_data[index].qc === heavy_check_mark
+          ? heavy_ballot_x
+          : sct_data[index].qc === heavy_ballot_x
+            ? heavy_excl_mark
+            : heavy_check_mark
+      );
+      set_download_yml_btn_state(heavy_excl_mark);
+      set_download_yml_btn_state(heavy_ballot_x);
+      $("#table").bootstrapTable({data: sct_data});
+      $("#table").bootstrapTable("load", sct_data);
+      hideColumns();
+      document.getElementById("table").rows[0].classList.remove("active");
+      document.getElementById("table").rows[parseInt(index)+1].className="active";
     }
   });
 
@@ -66,4 +87,22 @@ function responseHandler(res) {
     res[i].moddate = n.toLocaleString();
   }
   return res;
+}
+
+function set_download_yml_btn_state(marker) {
+  let disabled = true;
+  sct_data.forEach(item => {
+      if (item.qc === marker) {
+        disabled = false;
+      }
+  });
+  if (containsNonLatinCodepoints(marker) === true) {
+    // This converts e.g. '\u2718' -> '2718' with corresponding id='download_yaml_btn_2718'
+    marker = marker.codePointAt(0).toString(16)
+  }
+  document.getElementById("download_yaml_btn_".concat(marker)).disabled = disabled;
+}
+
+function containsNonLatinCodepoints(s) {
+    return /[^\u0000-\u00ff]/.test(s);
 }
