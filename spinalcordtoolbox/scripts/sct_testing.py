5e41defe0a31a5dd05c00aef9054bfc97e69af43
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:44 -0400

    compute_ernst_angle: Migrate 'sct_testing' test to pytest

--- spinalcordtoolbox/scripts/sct_testing.py
+++ spinalcordtoolbox/scripts/sct_testing.py
@@ -402,7 +402,6 @@ def get_functions_nonparallelizable():
 def get_functions_parallelizable():
     return [
         'sct_convert',
-        'sct_compute_ernst_angle',
         'sct_compute_hausdorff_distance',
         'sct_compute_mtr',
         'sct_compute_mscc',
