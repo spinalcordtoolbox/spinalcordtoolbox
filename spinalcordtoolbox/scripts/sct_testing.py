6f2ad4db08b88e6a532fd06b3e86e897b74a460c
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 10:22:01 -0400

    get_centerline: Migrate 'sct_testing' test to pytest

--- spinalcordtoolbox/scripts/sct_testing.py
+++ spinalcordtoolbox/scripts/sct_testing.py
@@ -398,7 +398,6 @@ def get_functions_nonparallelizable():
 
 def get_functions_parallelizable():
     return [
-        'sct_get_centerline',
         'sct_image',
         'sct_label_utils',
         'sct_label_vertebrae',
