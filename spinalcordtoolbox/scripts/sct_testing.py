125de195fd96d664288afaf8435bd6eb9e2f5ff1
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:44 -0400

    compute_mscc: Migrate 'sct_testing' test to pytest

--- spinalcordtoolbox/scripts/sct_testing.py
+++ spinalcordtoolbox/scripts/sct_testing.py
@@ -402,7 +402,6 @@ def get_functions_nonparallelizable():
 def get_functions_parallelizable():
     return [
         'sct_convert',
-        'sct_compute_mscc',
         'sct_compute_snr',
         'sct_concat_transfo',
         'sct_create_mask',
