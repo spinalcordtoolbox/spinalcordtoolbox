0757092a1eb830efb31686a3535fa7dbb125c927
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:44 -0400

    concat_transfo: Migrate 'sct_testing' test to pytest

--- unit_testing/cli/test_cli_sct_concat_transfo.py
+++ unit_testing/cli/test_cli_sct_concat_transfo.py
@@ -1,14 +1,15 @@
-from pytest_console_scripts import script_runner
 import pytest
 import logging
+
+from spinalcordtoolbox.scripts import sct_concat_transfo
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.script_launch_mode('subprocess')
-def test_sct_convert_backwards_compat(script_runner):
-    ret = script_runner.run('sct_testing', '--function', 'sct_concat_transfo')
-    logger.debug(f"{ret.stdout}")
-    logger.debug(f"{ret.stderr}")
-    assert ret.success
-    assert ret.stderr == ''
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_concat_transfo_no_checks():
+    """Run the CLI script without checking results.
+    TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
+    sct_concat_transfo.main(argv=['-w', 't2/warp_template2anat.nii.gz', 'mt/warp_t22mt1.nii.gz',
+                                  '-d', 'template/template/PAM50_small_t2.nii.gz'])
