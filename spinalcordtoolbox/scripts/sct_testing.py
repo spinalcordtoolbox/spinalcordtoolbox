854931ddf296e61561bb971af2b8e65c1cb9af29
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:44 -0400

    analyze_texture: Migrate 'sct_testing' test to pytest

--- spinalcordtoolbox/scripts/sct_testing.py
+++ spinalcordtoolbox/scripts/sct_testing.py
@@ -401,7 +401,6 @@ def get_functions_nonparallelizable():
 
 def get_functions_parallelizable():
     return [
-        'sct_analyze_texture',
         'sct_apply_transfo',
         'sct_convert',
         'sct_compute_ernst_angle',
