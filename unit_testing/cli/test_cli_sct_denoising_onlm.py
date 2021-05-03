8eb387816c2ea4aa9fa17359ca1a1371ea106d57
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:45 -0400

    denoising_onlm: Migrate 'sct_testing' test to pytest
    Note: There was no entry in sct_testing.py for this test.

---
+++ unit_testing/cli/test_cli_sct_denoising_onlm.py
@@ -0,0 +1,16 @@
+import sys
+import pytest
+import logging
+
+from spinalcordtoolbox.scripts import sct_denoising_onlm
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.skipif(sys.platform == "darwin", reason="Test hangs on macOS 10.15 CI runners (cause currently unknown)")
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_denoising_onlm_no_checks():
+    """Run the CLI script without checking results.
+    TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
+    sct_denoising_onlm.main(argv=['-i', 't2/t2.nii.gz', '-v', '2'])
