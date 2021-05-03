0add7f755dacc0fc573fdd580dbad5227a7506cb
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-03 08:59:44 -0400

    compute_hausdorff_distance: Migrate 'sct_testing' test to pytest

--- unit_testing/cli/test_cli_sct_compute_hausdorff_distance.py
+++ unit_testing/cli/test_cli_sct_compute_hausdorff_distance.py
@@ -1,14 +1,22 @@
-from pytest_console_scripts import script_runner
 import pytest
 import logging
+
+from spinalcordtoolbox.scripts import sct_compute_hausdorff_distance
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.script_launch_mode('subprocess')
-def test_sct_compute_hausdorff_distance_backwards_compat(script_runner):
-    ret = script_runner.run('sct_testing', '--function', 'sct_compute_hausdorff_distance')
-    logger.debug(f"{ret.stdout}")
-    logger.debug(f"{ret.stderr}")
-    assert ret.success
-    assert ret.stderr == ''
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_compute_hausdorff_distance_values_against_threshold():
+    """Run the CLI script and verify computed distances are all within a given threshold.
+    TODO: Distances are all 0.0. Is this test checking anything useful?"""
+    sct_compute_hausdorff_distance.main(argv=['-i', 't2s/t2s_gmseg_manual.nii.gz', '-d', 't2s/t2s_gmseg_manual.nii.gz'])
+
+    with open('hausdorff_distance.txt', 'r') as f:
+        hausdorff_distance_lst = []
+        for line in f.readlines():
+            if line.startswith('Slice'):
+                hausdorff_distance_lst.append(float(line.split(': ')[1].split(' -')[0]))
+
+    assert max(hausdorff_distance_lst) <= 1.0
