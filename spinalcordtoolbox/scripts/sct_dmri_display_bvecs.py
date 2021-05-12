e92b28ae42359abac3624d2db44b09a66874405a
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-12 08:45:07 -0400

    sct_dmri_display_bvecs: Add '-v' argument to make test pass

--- spinalcordtoolbox/scripts/sct_dmri_display_bvecs.py
+++ spinalcordtoolbox/scripts/sct_dmri_display_bvecs.py
@@ -39,6 +39,15 @@ def get_parser():
         action="help",
         help="Show this help message and exit."
     )
+    optional.add_argument(
+        '-v',
+        metavar=Metavar.int,
+        type=int,
+        choices=[0, 1, 2],
+        default=1,
+        # Values [0, 1, 2] map to logging levels [WARNING, INFO, DEBUG], but are also used as "if verbose == #" in API
+        help="Verbosity. 0: Display only errors/warnings, 1: Errors/warnings + info messages, 2: Debug mode"
+    )
 
     return parser
 
