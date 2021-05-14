d5cb9cf3da710fb02fab6166fa1c01f244920452
valosekj <jan.valosek@upol.cz>
2021-05-14 16:13:16 +0200

    allow to pass also bvals file to display multi-shell data

--- spinalcordtoolbox/scripts/sct_dmri_display_bvecs.py
+++ spinalcordtoolbox/scripts/sct_dmri_display_bvecs.py
@@ -10,9 +10,10 @@
 # About the license: see the file LICENSE.TXT
 #########################################################################################
 
-import os
 import sys
 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from dipy.data.fetcher import read_bvals_bvecs
 
@@ -20,19 +21,27 @@ from spinalcordtoolbox.utils import SCTArgumentParser, Metavar, init_sct, printv
 
 bzero = 0.0001  # b-zero threshold
 
+# symbols for individual shells
+symbols = ['o', 'v', 's', 'H', 'D', 'P', '*']
 
 def get_parser():
     parser = SCTArgumentParser(
-        description='Display scatter plot of gradient directions from bvecs file.'
+        description='Display scatter plot of gradient directions from bvecs file. If you have multi-shell acquisition,'
+                    'you can provide also bvals file to display individual shells in q-space.'
     )
     mandatory = parser.add_argument_group("\nMANDATORY ARGUMENTS")
     mandatory.add_argument(
         '-bvec',
         metavar=Metavar.file,
         required=True,
-        help="Input bvecs file. Example: bvecs.txt",
+        help="Input bvecs file. Example: sub-001_dwi.bvec",
     )
     optional = parser.add_argument_group("\nOPTIONAL ARGUMENTS")
+    optional.add_argument(
+        '-bval',
+        metavar=Metavar.file,
+        help="Input bval file (for multi-shell acquisition). Example: sub-001_dwi.bval",
+    )
     optional.add_argument(
         "-h",
         "--help",
@@ -52,21 +61,21 @@ def get_parser():
     return parser
 
 
-def plot_2dscatter(fig_handle=None, subplot=None, x=None, y=None, xlabel='X', ylabel='Y'):
+def plot_2dscatter(fig_handle=None, subplot=None, x=None, y=None, xlabel='X', ylabel='Y', bvals=None, marker=None):
     ax = fig_handle.add_subplot(subplot, aspect='equal')
     for i in range(0, len(x)):
         # if b=0, do not plot
         if not(abs(x[i]) < bzero and abs(x[i]) < bzero):
-            ax.scatter(x[i], y[i])
+            ax.scatter(x[i], y[i], marker=marker[bvals[i]])
+    plt.axis('equal')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.xlim([-1, 1])
-    plt.ylim([-1, 1])
+    # plt.xlim([-1, 1])
+    # plt.ylim([-1, 1])
     plt.grid()
 
 # MAIN
 # ==========================================================================================
-
 
 def main(argv=None):
     parser = get_parser()
@@ -75,14 +84,38 @@ def main(argv=None):
     set_global_loglevel(verbose=verbose)
 
     fname_bvecs = arguments.bvec
+    if arguments.bval is not None:
+        fname_bvals = arguments.bval
+        # Read bvals and bvecs files
+        bvals, bvecs = read_bvals_bvecs(fname_bvals, fname_bvecs)
+    else:
+        # Read only bvecs file
+        _, bvecs = read_bvals_bvecs(None, fname_bvecs)
 
-    # Read bvecs
-    bvecs = read_bvals_bvecs(fname_bvecs, None)
-    bvecs = bvecs[0]
     # if first dimension is not equal to 3 (x,y,z), transpose bvecs file
     if not bvecs.shape[0] == 3:
         bvecs = bvecs.transpose()
-    x, y, z = bvecs[0], bvecs[1], bvecs[2]
+
+    # bvals file was passed
+    if arguments.bval is not None:
+        # multiply unit b-vectors by b-values
+        x, y, z = bvecs[0] * bvals, bvecs[1] * bvals, bvecs[2] * bvals
+    # bvals file was not passed
+    else:
+        x, y, z = bvecs[0], bvecs[1], bvecs[2]
+        # create dummy unit bvals array (necessary fot scatter plots)
+        bvals = np.repeat(1, bvecs.shape[1])
+
+    # Assign scatter markers to unique bvals
+    shell_markers = {}
+    index=0
+    for unique_bval in np.unique(bvals):
+        # skip b=0
+        if unique_bval < bzero:
+            index -= 1
+            continue
+        index += 1
+        shell_markers[unique_bval] = symbols[index]
 
     # Get total number of directions
     n_dir = len(x)
@@ -108,13 +141,13 @@ def main(argv=None):
 
     # Display scatter plot
     fig = plt.figure(facecolor='white', figsize=(9, 8))
-    fig.suptitle('Number of b=0: ' + str(n_b0) + ', Number of b!=0: ' + str(n_dir - n_b0) + ', Number of effective directions (without duplicates): ' + str(n_dir_eff))
-    # plt.ion()
+    fig.suptitle('Number of b=0: ' + str(n_b0) + ', Number of b!=0: ' + str(n_dir - n_b0) +
+                 ', Number of effective directions (without duplicates): ' + str(n_dir_eff))
 
     # Display three views
-    plot_2dscatter(fig_handle=fig, subplot=221, x=bvecs[0][:], y=bvecs[1][:], xlabel='X', ylabel='Y')
-    plot_2dscatter(fig_handle=fig, subplot=222, x=bvecs[0][:], y=bvecs[2][:], xlabel='X', ylabel='Z')
-    plot_2dscatter(fig_handle=fig, subplot=223, x=bvecs[1][:], y=bvecs[2][:], xlabel='Y', ylabel='Z')
+    plot_2dscatter(fig_handle=fig, subplot=221, x=x, y=y, xlabel='X', ylabel='Y', bvals=bvals, marker=shell_markers)
+    plot_2dscatter(fig_handle=fig, subplot=222, x=x, y=z, xlabel='X', ylabel='Z', bvals=bvals, marker=shell_markers)
+    plot_2dscatter(fig_handle=fig, subplot=223, x=y, y=z, xlabel='Y', ylabel='Z', bvals=bvals, marker=shell_markers)
 
     # 3D
     ax = fig.add_subplot(224, projection='3d')
@@ -123,12 +156,12 @@ def main(argv=None):
         # x, y, z = bvecs[0], bvecs[1], bvecs[2]
         # if b=0, do not plot
         if not(abs(x[i]) < bzero and abs(x[i]) < bzero and abs(x[i]) < bzero):
-            ax.scatter(x[i], y[i], z[i])
-    ax.set_xlim3d(-1, 1)
-    ax.set_ylim3d(-1, 1)
-    ax.set_zlim3d(-1, 1)
+            ax.scatter(x[i], y[i], z[i], marker=shell_markers[bvals[i]])
+    # ax.set_xlim3d(-1, 1)
+    # ax.set_ylim3d(-1, 1)
+    # ax.set_zlim3d(-1, 1)
     plt.title('3D view (use mouse to rotate)')
-    plt.axis('off')
+    plt.axis('on')
     # plt.draw()
 
     # Save image
