e10463efef86fc5ce08a377238e402f35a0d2222
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-22 12:40:11 -0400

    Set environment variables in `__init__.py` instead of the launcher

--- spinalcordtoolbox/__init__.py
+++ spinalcordtoolbox/__init__.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
+import os
+import multiprocessing
 
 from .utils import __version__, __sct_dir__, __data_dir__, __deepseg_dir__
+
+
+def configure_sct_env_variables():
+    """
+    Set environment variables that should be active in the scope of the running Python process.
+
+    Note: This change will be active whenever `spinalcordtoolbox` is imported. So, try to only make light
+    changes here, as to not significantly impact any downstream packages built on `spinalcordtoolbox`.
+    """
+    # This is used by ANTs tools (e.g. isct_antsSliceRegularization) to control multiprocessing
+    if "ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS" not in os.environ:
+        os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = str(multiprocessing.cpu_count())
+
+    # DISPLAY is used by the X Window System (in UNIX): https://docstore.mik.ua/orelly/unix3/upt/ch35_08.htm
+    # We use this environment variable to detect whether or not we're on a headless system.
+    # NB: Sometimes 'DISPLAY' can be unset for SSH sessions that aren't headless. If any users encounter issues
+    # displaying plots, direct them to https://unix.stackexchange.com/q/138936
+    if "DISPLAY" not in os.environ:
+        # If we're on a headless system, set matplotlib's backend to 'Agg', which is a non-interactive backend.
+        # This will prevent interactive plots from being shown, which keeps headless systems from hanging indefinitely.
+        os.environ["MPLBACKEND"] = "Agg"
+        # NB: We used to set MPLBACKEND in the user's RC file via `install_sct`. Unfortunately, this means that
+        # 'export MPLBACKEND=Agg' might be hanging around in users' RC files, even if they're not on a headless system.
+
+
+configure_sct_env_variables()
--- spinalcordtoolbox/compat/launcher.py
+++ spinalcordtoolbox/compat/launcher.py
@@ -3,7 +3,6 @@
 
 import sys
 import os
-import multiprocessing
 
 import spinalcordtoolbox as sct
 
@@ -12,30 +11,17 @@ def main():
     """
     Compatibility entry point to run scripts
     """
-
-    # Force scripts to not use graphical output
-    env = dict()
-    env.update(os.environ)
-
-    if "DISPLAY" not in os.environ:
-        # No DISPLAY, set suitable default matplotlib backend as pyplot is used
-        env["MPLBACKEND"] = "Agg"
-
-    if "ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS" not in os.environ:
-        env["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = str(multiprocessing.cpu_count())
-
+    # Convert command syntax: "sct_<function> -args" --> "python $ABSOLUTEPATH/sct_<function>.py -args"
     command = os.path.basename(sys.argv[0])
     pkg_dir = os.path.dirname(sct.__file__)
-
-    script = os.path.join(pkg_dir, "scripts", "{}.py".format(command))
-    assert os.path.exists(script)
-
+    script = os.path.join(pkg_dir, "scripts", f"{command}.py")
     cmd = [sys.executable, script] + sys.argv[1:]
 
+    # Adapt command to MPI, which is used for parallelization on HPC architectures (https://hpc-wiki.info/hpc/MPI)
     mpi_flags = os.environ.get("SCT_MPI_MODE", None)
     if mpi_flags is not None:
         if mpi_flags == "yes":  # compat
             mpi_flags = "-n 1"
         cmd = ["mpiexec"] + mpi_flags.split() + cmd
 
-    os.execvpe(cmd[0], cmd[0:], env)
+    os.execvp(cmd[0], cmd[0:])
