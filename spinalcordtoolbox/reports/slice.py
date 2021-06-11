f7d311ecfcb48a857232ae7dd98579a4ecbfd66b
sandrine <sandrine.bedard@polymtl.ca>
2021-06-11 09:41:32 -0400

    put back thr at 0.5

--- spinalcordtoolbox/reports/slice.py
+++ spinalcordtoolbox/reports/slice.py
@@ -352,7 +352,7 @@ class Slice(object):
             nii_r = resample_nib(nii, image_dest=nii_ref, interpolation=dict_interp[type_img])
         # If resampled image is a segmentation, binarize using threshold at 0.5
         if type_img == 'seg':
-            #img_r_data = (nii_r.get_data() > 0.1) * 1 #TODO binarize later
+            #img_r_data = (nii_r.get_data() > 0.5) * 1 # TODO binarize later
             img_r_data = nii_r.get_data()
         else:
             img_r_data = nii_r.get_data()
