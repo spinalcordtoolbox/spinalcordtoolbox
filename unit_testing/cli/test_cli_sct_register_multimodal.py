a7bfba83837b5d1935c906cc7906e8288df1fc30
Joshua Newton <joshuacwnewton@gmail.com>
2021-06-15 07:59:44 -0400

    Don't output `dest->src` files if registration is only performed one-way (#3407)

--- unit_testing/cli/test_cli_sct_register_multimodal.py
+++ unit_testing/cli/test_cli_sct_register_multimodal.py
@@ -1,4 +1,5 @@
 from pytest_console_scripts import script_runner
+import os
 import pytest
 import logging
 
@@ -17,11 +18,16 @@ def test_sct_register_multimodal_backwards_compat(script_runner):
     assert ret.stderr == ''
 
 
-def test_sct_register_multimodal_mask_no_checks(tmp_path):
-    """Run the script without validating results.
+def test_sct_register_multimodal_mask_files_exist(tmp_path):
+    """
+    Run the script without validating results.
 
-    TODO: Write a check that verifies the registration results as part of
-    https://github.com/spinalcordtoolbox/spinalcordtoolbox/pull/3246."""
+    - TODO: Write a check that verifies the registration results as part of
+            https://github.com/spinalcordtoolbox/spinalcordtoolbox/pull/3246.
+    - TODO: Parametrize this test to add '-initwarpinv warp_anat2template.nii.gz',
+            after the file is added to sct_testing_data:
+            https://github.com/spinalcordtoolbox/spinalcordtoolbox/pull/3407#discussion_r646895013
+    """
     fname_mask = str(tmp_path/'mask_mt1.nii.gz')
     sct_create_mask.main(['-i', sct_test_path('mt', 'mt1.nii.gz'),
                           '-p', f"centerline,{sct_test_path('mt', 'mt1_seg.nii.gz')}",
@@ -33,5 +39,13 @@ def test_sct_register_multimodal_mask_no_checks(tmp_path):
         '-dseg', sct_test_path('mt', 'mt1_seg.nii.gz'),
         '-param', 'step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3',
         '-m', fname_mask,
-        '-initwarp', sct_test_path('t2', 'warp_template2anat.nii.gz')
+        '-initwarp', sct_test_path('t2', 'warp_template2anat.nii.gz'),
+        '-ofolder', str(tmp_path)
     ])
+
+    for path in ["PAM50_t2_reg.nii.gz", "warp_PAM50_t22mt1.nii.gz"]:
+        assert os.path.exists(tmp_path/path)
+
+    # Because `-initwarp` was specified (but `-initwarpinv` wasn't) the dest->seg files should NOT exist
+    for path in ["mt1_reg.nii.gz", "warp_mt12PAM50_t2.nii.gz"]:
+        assert not os.path.exists(tmp_path/path)
