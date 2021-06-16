a1c5acbbb1f76d80cbed1715df58f98e7f08ee51
Jan Valošek <39456460+valosekj@users.noreply.github.com>
2021-06-16 19:59:10 +0200

    Allow `sct_dmri_display_bvecs.py` to display multi-shell acquisition (#3390)
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>

--- unit_testing/cli/test_cli_sct_dmri_display_bvecs.py
+++ unit_testing/cli/test_cli_sct_dmri_display_bvecs.py
@@ -16,3 +16,13 @@ def test_sct_dmri_display_bvecs_png_exists():
     sct_dmri_display_bvecs.main(argv=['-bvec', 'sct_testing_data/dmri/bvecs.txt'])
     assert os.path.exists('bvecs.png')
     os.unlink('bvecs.png')
+
+
+@pytest.mark.skipif(sys.platform == "darwin", reason="Script uses pyplot.show, causing macOS 10.15 CI runners to hang.")
+# FIXME: https://github.com/spinalcordtoolbox/spinalcordtoolbox/issues/3388
+def test_sct_dmri_display_bvecs_png_exists_bvec_bval_inputs():
+    """Run the CLI script."""
+    sct_dmri_display_bvecs.main(argv=['-bvec', 'sct_testing_data/dmri/bvecs.txt',
+                                      '-bval', 'sct_testing_data/dmri/bvals.txt'])
+    assert os.path.exists('bvecs.png')
+    os.unlink('bvecs.png')
