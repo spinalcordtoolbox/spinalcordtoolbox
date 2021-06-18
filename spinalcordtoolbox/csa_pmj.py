f1be7172ff93f1d7f769d6bcd97f07b7be1bb196
sandrine <sandrine.bedard@polymtl.ca>
2021-06-18 12:50:27 -0400

    get mask of where to compute csa

--- spinalcordtoolbox/csa_pmj.py
+++ spinalcordtoolbox/csa_pmj.py
@@ -8,7 +8,7 @@ import nibabel
 import numpy as np
 
 from spinalcordtoolbox.image import Image, zeros_like
-from spinalcordtoolbox.aggregate_slicewise import Metric
+from spinalcordtoolbox.aggregate_slicewise import Metric # TO remove
 from spinalcordtoolbox.centerline.core import ParamCenterline, get_centerline
 from spinalcordtoolbox.resampling import resample_nib
 from spinalcordtoolbox.utils import sct_progress_bar
@@ -46,13 +46,14 @@ def compute_csa_from_pmj(segmentation, pmj, distance, extent, metric, param_cent
     # compute the spinal cord centerline based on the spinal cord segmentation
     _, arr_ctl, arr_ctl_der, fit_results = get_centerline(im_segr, param=param_centerline, verbose=verbose)
 
+    # Get tagent vector at the top of the centerline | Maybe take a fiew slices to get the vector
     iz = max_z_index
     tangent_vect = np.array([arr_ctl_der[0][iz - min_z_index] * px,
                                      arr_ctl_der[1][iz - min_z_index] * py,
                                      pz])
     # Normalize vector by its L2 norm
     tangent_vect = tangent_vect / np.linalg.norm(tangent_vect)
-    plane_pt = arr_ctl[:, -1] + [0,0, min_z_index]  # In pixel dim or not?
+    plane_pt = arr_ctl[:, -1] + [0,0, min_z_index]
     line_pt = list(im_pmjr.getNonZeroCoordinates()[0])[:-1]  # np.argwhere(data_pmj != 0)[0]
     
     init_point = get_intersection_plane_line(plane_pt, 
@@ -66,20 +67,17 @@ def compute_csa_from_pmj(segmentation, pmj, distance, extent, metric, param_cent
                                         px,
                                         py,
                                         pz)
-
+    print(arr_length)
+    mask, mask_metric, slices = get_mask(arr_length, im_seg, distance, extent, metric)
+    #print(metric.data, mask_metric.data)
+    return mask, mask_metric, slices
     # TODO: 
-    # get_mask,
-    # add a label to the mask--> lenght as a label (distance),
     # add possibility if distance is None
+    # Add possibility to have multiple distances
     # test mask in aggregate per slice 
 
-    #init_point_phys = im_pmjr.transfo_pix2phys(coordi=[init_point])
-    #print(init_point_phys)
-
     # Extrapolate centerline --> is it really necessary
-    arr_ctl_extra = np.zeros((arr_ctl.shape[0], nz))
-    arr_ctl_extra[:,min_z_index:max_z_index + 1] = arr_ctl
-    #print(arr_ctl_extra)
+
 
 def get_intersection_plane_line(plane_pt, plane_norm_vect, line_pt, line_vect):
     # TODO add function description
@@ -113,17 +111,25 @@ def get_distance_from_pmj(centerline_points, init_point, px, py, pz):
     return arr_length
 
 
-def get_mask(arr_lenght, seg, distance, extent):
+def get_mask(arr_lenght, seg, distance, extent, metric):
     # TODO check with a segmentation that misses first slice
     z_index_extent_max = get_nearest_index(arr_lenght, distance - extent/2)
     z_index_extent_min = get_nearest_index(arr_lenght, distance + extent/2)
-    mask = seg.zeros_like()
-    mask[:, :, 0:z_index_extent_min] = 0
-    mask[:, :, z_index_extent_max + 1:] = 0
 
-    return mask
+    mask = seg.copy()
+    mask.data[:, :, 0:z_index_extent_min] = 0
+    mask.data[:, :, z_index_extent_max + 1:] = 0
+
+    mask_arr = np.zeros_like(metric.data)
+    mask_arr[z_index_extent_min:z_index_extent_max + 1] = 1
+    mask_metric = Metric(mask.data, label=str(distance))
+    #mask_metric = Metric(mask_arr, label=str(distance))
+    slices = "{}:{}".format(z_index_extent_min, z_index_extent_max) # is the last slice included or not??
+    print(slices)
+    return mask, mask_metric, slices
 
 
 def get_nearest_index(arr, value):
-    difference_array = np.absolute(arr-value)
-    return difference_array.argmin()
+    difference_array = np.absolute(arr[0]-value)
+    index = arr[1][difference_array.argmin()]
+    return int(index)
