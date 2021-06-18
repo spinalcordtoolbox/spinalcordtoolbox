433ff7bef9db4af5b4e60da6b755dd2c28923954
sandrine <sandrine.bedard@polymtl.ca>
2021-06-18 17:29:00 -0400

    adapt index for right lenght

--- spinalcordtoolbox/csa_pmj.py
+++ spinalcordtoolbox/csa_pmj.py
@@ -25,7 +25,7 @@ def compute_csa_from_pmj(segmentation, pmj, distance, extent, metric, param_cent
     :param verbose:
     :return :
     """
-
+    # TODO: loop through distance (when multiple distances are given)
     im_seg = Image(segmentation).change_orientation('RPI')
     im_pmj = Image(pmj).change_orientation('RPI')
     nx, ny, nz, nt, px, py, pz, pt = im_seg.dim
@@ -41,7 +41,6 @@ def compute_csa_from_pmj(segmentation, pmj, distance, extent, metric, param_cent
     data_seg = im_segr.data
     X, Y, Z = (data_seg > NEAR_ZERO_THRESHOLD).nonzero()
     min_z_index, max_z_index = min(Z), max(Z)
-
 
     # compute the spinal cord centerline based on the spinal cord segmentation
     _, arr_ctl, arr_ctl_der, fit_results = get_centerline(im_segr, param=param_centerline, verbose=verbose)
@@ -60,32 +59,21 @@ def compute_csa_from_pmj(segmentation, pmj, distance, extent, metric, param_cent
                                              tangent_vect,
                                              line_pt,tangent_vect)
 
-    #centerline_phys = np.zeros_like(arr_ctl)
-    #centerline_phys = im_pmjr.transfo_pix2phys(coordi=arr_ctl.T).T
     arr_length = get_distance_from_pmj(arr_ctl,
                                         init_point,
                                         px,
                                         py,
                                         pz)
-    print(arr_length)
-    mask, mask_metric, slices = get_mask(arr_length, im_seg, distance, extent, metric)
-    #print(metric.data, mask_metric.data)
-    return mask, mask_metric, slices
-    # TODO: 
-    # add possibility if distance is None
-    # Add possibility to have multiple distances
-    # test mask in aggregate per slice 
-
-    # Extrapolate centerline --> is it really necessary
+    mask, slices = get_mask(arr_length, im_seg, distance, extent, metric)
+    slices = [slices, distance]
+    return mask, slices
 
 
 def get_intersection_plane_line(plane_pt, plane_norm_vect, line_pt, line_vect):
-    # TODO add function description
-    epsilon=1e-6
+    """
+    
+    """
     det = plane_norm_vect.dot(line_vect)
-    if abs(det) < epsilon:
-        print ("no intersection or line is within plane")  # TODO: change print or logging
-
     w = line_pt - plane_pt
     si = -plane_norm_vect.dot(w) / det
     intersection = w + si * line_vect + plane_pt
@@ -94,6 +82,9 @@ def get_intersection_plane_line(plane_pt, plane_norm_vect, line_pt, line_vect):
 
 
 def get_distance_from_pmj(centerline_points, init_point, px, py, pz):
+    """
+
+    """
     number_of_points = centerline_points.shape[1]
     init_length = np.sqrt(((init_point[0] - centerline_points[0, -1]) * px ) ** 2 +
                   ((init_point[1] - centerline_points[1, -1]) * py ) ** 2 + 
@@ -112,24 +103,65 @@ def get_distance_from_pmj(centerline_points, init_point, px, py, pz):
 
 
 def get_mask(arr_lenght, seg, distance, extent, metric):
+    """
+
+    """       
     # TODO check with a segmentation that misses first slice
-    z_index_extent_max = get_nearest_index(arr_lenght, distance - extent/2)
     z_index_extent_min = get_nearest_index(arr_lenght, distance + extent/2)
+    z_index_extent_max = get_nearest_index(arr_lenght, distance - extent/2)
+    z_index_extent_min, z_index_extent_max = validate_lenght(z_index_extent_min, z_index_extent_max, arr_lenght, extent)
 
     mask = seg.copy()
     mask.data[:, :, 0:z_index_extent_min] = 0
-    mask.data[:, :, z_index_extent_max + 1:] = 0
+    mask.data[:, :, z_index_extent_max :] = 0 # if last slice included, add + 1
 
-    mask_arr = np.zeros_like(metric.data)
-    mask_arr[z_index_extent_min:z_index_extent_max + 1] = 1
-    mask_metric = Metric(mask.data, label=str(distance))
-    #mask_metric = Metric(mask_arr, label=str(distance))
-    slices = "{}:{}".format(z_index_extent_min, z_index_extent_max) # is the last slice included or not??
-    print(slices)
-    return mask, mask_metric, slices
+    slices = "{}:{}".format(z_index_extent_min, z_index_extent_max -1) # is the last slice included or not??
+    return mask, slices
 
 
 def get_nearest_index(arr, value):
+    """
+
+    """
     difference_array = np.absolute(arr[0]-value)
     index = arr[1][difference_array.argmin()]
     return int(index)
+
+def validate_lenght(z_min, z_max, arr, value):
+    lenght = arr[0, z_min] - arr[0, z_max]
+    print('lenght:', lenght)
+    if lenght > value:
+        lenght_1 = arr[0, z_min + 1] - arr[0, z_max]
+        lenght_2 = arr[0, z_min] - arr[0, z_max - 1]
+        if np.abs(lenght_2 - value) < np.abs(lenght_1 - value):
+            closest_lenght = lenght_2
+            z_min_2 = z_min
+            z_max_2 = z_max - 1
+        else:
+            closest_lenght = lenght_1
+            z_min_2 = z_min + 1
+            z_max_2 = z_max         
+        if (np.abs(lenght - value)) < (np.abs(closest_lenght - value)):
+            return  z_min, z_max
+        else:
+            print(closest_lenght)
+            return  z_min_2, z_max_2
+    
+    if lenght < value:
+        lenght_1 = arr[0, z_min - 1] - arr[0, z_max]
+        lenght_2 = arr[0, z_min] - arr[0, z_max + 1]
+
+        if np.abs(lenght_2 - value) < np.abs(lenght_1 - value):
+            closest_lenght = lenght_2
+            z_min_2 = z_min
+            z_max_2 = z_max + 1
+        else:
+            closest_lenght = lenght_1
+            z_min_2 = z_min - 1
+            z_max_2 = z_max         
+        if (np.abs(lenght - value)) < (np.abs(closest_lenght - value)):
+            return  z_min, z_max
+        else:
+            print(closest_lenght)
+            return  z_min_2, z_max_2
+
