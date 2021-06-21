d06dfc269a4de51fe6b4169d86eb6c1672270dc5
Joshua Newton <joshuacwnewton@gmail.com>
2021-06-21 11:23:19 -0400

    `test_cli_sct_sct_extract_metric.py`: Test additional MTR values

--- unit_testing/cli/test_cli_sct_extract_metric.py
+++ unit_testing/cli/test_cli_sct_extract_metric.py
@@ -1,4 +1,3 @@
-import csv
 import numpy as np
 
 import pytest
@@ -11,13 +10,11 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
-def test_sct_extract_metric_weighted_average_value_against_groundtruth():
-    """Verify that computed WA value is equivalent to known ground truth value."""
+def test_sct_extract_metric_against_groundtruth():
+    """Verify that computed values are equivalent to known ground truth values."""
     fname_out = 'quantif_mtr.csv'
     sct_extract_metric.main(argv=['-i', 'mt/mtr.nii.gz', '-f', 'mt/label/atlas', '-method', 'wa', '-l', '51',
                                   '-z', '1:2', '-o', fname_out])
-    with open(fname_out, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-        mtr_result = np.float([row['WA()'] for row in reader][0])
-
-    assert mtr_result == pytest.approx(32.6404, abs=0.001)
+    results = np.genfromtxt(fname_out, skip_header=1, delimiter=',')
+    results = results[~np.isnan(results)]  # Remove non-numeric fields such as filename, SCT version, etc.
+    assert results == pytest.approx([176.1532, 32.6404, 11.4573], abs=0.001)  # Size[vox], WA, and STD respectively
