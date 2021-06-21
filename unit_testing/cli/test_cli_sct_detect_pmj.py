1a284dfc45a7fc1dea16d68febf41d00aa50b92a
Joshua Newton <joshuacwnewton@gmail.com>
2021-06-21 09:19:35 -0400

    `test_cli_sct_detect_pmj.py`: Refactor test to remove confusing indexing

--- unit_testing/cli/test_cli_sct_detect_pmj.py
+++ unit_testing/cli/test_cli_sct_detect_pmj.py
@@ -1,4 +1,3 @@
-import math
 import pytest
 import logging
 
@@ -13,8 +12,8 @@ logger = logging.getLogger(__name__)
 @pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
 def test_sct_detect_pmj_check_euclidean_distance_against_groundtruth():
-    """Run the CLI script and verify that euclidean distance (computed against ground truth) is within a certain
-    threshold."""
+    """Run the CLI script and verify that euclidean distances between predicted and ground truth coordinates
+    are within a certain threshold."""
     fname_in = 'template/template/PAM50_small_t2.nii.gz'
     fname_out = 'PAM50_small_t2_pmj.nii.gz'
     fname_gt = 'template/template/PAM50_small_t2_pmj_manual.nii.gz'
@@ -23,11 +22,19 @@ def test_sct_detect_pmj_check_euclidean_distance_against_groundtruth():
     im_pmj = Image(fname_out)
     im_pmj_manual = Image(fname_gt)
 
-    # compute Euclidean distance between predicted and GT PMJ label
-    x_true, y_true, z_true = np.where(im_pmj_manual.data == 50)
-    x_pred, y_pred, z_pred = np.where(im_pmj.data == 50)
-    x_true, y_true, z_true = im_pmj_manual.transfo_pix2phys([[x_true[0], y_true[0], z_true[0]]])[0]
-    x_pred, y_pred, z_pred = im_pmj.transfo_pix2phys([[x_pred[0], y_pred[0], z_pred[0]]])[0]
+    # np.where outputs a tuple with an array for each axis: https://stackoverflow.com/q/50646102
+    # so, convert ==> `tuple(x_array, y_array, z_array)` -> `list([x1, y1, z1], [x2, y2, z2], ...)`
+    label_value = 50
+    gt = [[x, y, z] for x, y, z in zip(*np.where(im_pmj_manual.data == label_value))]
+    pred = [[x, y, z] for x, y, z in zip(*np.where(im_pmj.data == label_value))]
 
-    distance_detection = math.sqrt((x_true - x_pred)**2 + (y_true - y_pred)**2 + (z_true - z_pred)**2)
-    assert distance_detection < 10.0
+    # ensure that only one coordinate was predicted
+    assert len(gt) == len(pred) == 1
+
+    # transform pixel coordinates to physical coordinates (units in millimeters)
+    gt_phys = im_pmj_manual.transfo_pix2phys(gt)
+    pred_phys = im_pmj.transfo_pix2phys(pred)
+
+    # ensure prediction is within 10mm of ground truth coordinates
+    distances = np.linalg.norm(gt_phys - pred_phys, axis=1)
+    assert np.all(distances < 10.0)
