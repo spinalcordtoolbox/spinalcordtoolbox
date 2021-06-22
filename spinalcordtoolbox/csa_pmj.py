df7cbc986444e7e90feb14868c1b6ef808f244c6
sandrine <sandrine.bedard@polymtl.ca>
2021-06-22 15:48:51 -0400

    extrapolate centerline and compute distance along

--- spinalcordtoolbox/csa_pmj.py
+++ spinalcordtoolbox/csa_pmj.py
@@ -6,7 +6,8 @@ import logging
 import numpy as np
 
 from spinalcordtoolbox.image import Image
-from spinalcordtoolbox.centerline.core import get_centerline
+from spinalcordtoolbox.centerline.core import get_centerline, round_and_clip
+from spinalcordtoolbox.centerline import curve_fitting
 from spinalcordtoolbox.resampling import resample_nib
 
 logger = logging.getLogger(__name__)
@@ -20,7 +21,7 @@ def get_slices_for_pmj_distance(segmentation, pmj, distance, extent, param_cente
     Generate mask from segmentation of the slices used to process segmentation data corresponding to a distance from PMJ projection.
     :param segmentation: input segmentation. Could be either an Image or a file name.
     :param pmj: label of PMJ.
-    :param distance: float: Distance from PMJ.
+    :param distance: float: Distance from Ponto-Medullary Junction (PMJ) in mm.
     :param param_centerline: see centerline.core.ParamCenterline()
     :param verbose:
     :return mask:
@@ -42,32 +43,34 @@ def get_slices_for_pmj_distance(segmentation, pmj, distance, extent, param_cente
     data_seg = im_segr.data
     X, Y, Z = (data_seg > NEAR_ZERO_THRESHOLD).nonzero()
     min_z_index, max_z_index = min(Z), max(Z)
-
     # Compute the spinal cord centerline based on the spinal cord segmentation
-    _, arr_ctl, arr_ctl_der, fit_results = get_centerline(im_segr, param=param_centerline, verbose=verbose)
+    im_ctl, arr_ctl, arr_ctl_der, fit_results = get_centerline(im_segr, param=param_centerline, verbose=verbose)
 
-    # Get tangent vector at the top of the centerline | Maybe take a few slices to get the vector ?
-    iz = max_z_index
-    tangent_vect = np.array([arr_ctl_der[0][iz - min_z_index] * px, arr_ctl_der[1][iz - min_z_index] * py, pz])
-    # Normalize vector by its L2 norm
-    tangent_vect = tangent_vect / np.linalg.norm(tangent_vect)
-    plane_pt = arr_ctl[:, -1] + [0, 0, min_z_index]
-    line_pt = np.argwhere(data_pmj != 0)[0]  # list(im_pmjr.getNonZeroCoordinates()[0])[:-1]
+    # Extrapolate centerline
+    arr_ctl_extra = extrapolate_centerline(arr_ctl, im_segr, pz)
 
-    # Get intersection of plane passing through PMJ and continuation of centerline
-    init_point = get_intersection_plane_line(plane_pt, tangent_vect, line_pt, tangent_vect)
-    # Get the distance from PMJ for all slices of the centerline
-    arr_length = get_distance_from_pmj(arr_ctl, init_point, px, py, pz)
+    # Create an image with the centerline | ONLY TO VALIDATE --> to remove
+    im_centerline = im_segr.copy()
+    im_centerline.data = np.zeros(im_centerline.data.shape)
+    # Assign value=1 to centerline. Make sure to clip to avoid array overflow.
+    im_centerline.data[round_and_clip(arr_ctl_extra[0], clip=[0, im_centerline.data.shape[0]]),
+                       round_and_clip(arr_ctl_extra[1], clip=[0, im_centerline.data.shape[1]]),
+                       np.array(range(im_segr.dim[2]))] = 1
+    im_centerline.save('centerline_extrapolated.nii.gz')
+    pmj_coord = np.argwhere(data_pmj != 0)[0]
+    # Get Z index of PMJ project on extrapolated centerline
+    pmj_index = get_min_distance(pmj_coord, arr_ctl_extra, px, py, pz)
+    # Compute distance from PMJ along centerline
+    arr_length = get_distance_from_pmj(arr_ctl_extra, pmj_index, px, py, pz)
 
     # Check if distance is out of bound
     if distance > arr_length[0][0]:
         raise ValueError("Input distance of " + str(distance) + " mm is out of bound for maximum distance of " + str(arr_length[0][0]) + " mm")
-    
+
     if distance < arr_length[0][-1]:
         raise ValueError("Input distance of " + str(distance) + " mm is out of bound for minimum distance of " + str(arr_length[0][-1]) + " mm")
 
     # Get Z index of corresponding distance from PMJ with the specified extent
-    # TODO: add warining if specified
     z_index_extent_min = get_nearest_index(arr_length, distance + extent/2)
     z_index_extent_max = get_nearest_index(arr_length, distance - extent/2)
     # Check if extent corresponds to the lenght, if not add or remove a slice
@@ -84,11 +87,10 @@ def get_slices_for_pmj_distance(segmentation, pmj, distance, extent, param_cente
 
     # Get corresponding slices
     slices = "{}:{}".format(z_index_extent_min, z_index_extent_max - 1)
-
     return mask, slices
 
 
-def get_intersection_plane_line(plane_point, plane_norm_vect, line_point, line_vect):
+def get_intersection_plane_line(plane_point, plane_norm_vect, line_point, line_vect):  # TO REMOVE
     """
     Get intersection between a plane and a line.
     :param plane_point: coordinates of a point on the plane.
@@ -105,36 +107,48 @@ def get_intersection_plane_line(plane_point, plane_norm_vect, line_point, line_v
     return intersection
 
 
-def get_distance_from_pmj(centerline_points, init_point, px, py, pz):
+def get_distance_from_pmj(centerline_points, z_index, px, py, pz):
     """
     Compute distance from projected PMJ on centerline and cord centerline.
     :param centerline_points: 3xn array: Centerline in continuous coordinate (float) for each slice in RPI orientation.
-    :param init_point: coordinates of projected PMJ on the centerline.
+    :param z_index: z index of projected PMJ on the centerline.
     :param px: x pixel size.
     :param py: y pixel size.
     :param pz: z pixel size.
     :return: nd-array: distance from PMJ and corresponding indexes.
     """
-    number_of_points = centerline_points.shape[1]
-    # Get distance from initial point and first coordinate of the centerline
-    init_length = np.sqrt(((init_point[0] - centerline_points[0, -1]) * px) ** 2 +
-                          ((init_point[1] - centerline_points[1, -1]) * py) ** 2 +
-                          ((init_point[2] - centerline_points[2, -1]) * pz) ** 2)
-    # Initialize length
-    length = init_length
-    arr_length = [init_length]
-    for i in range(number_of_points - 1, 0, -1):
+    length = 0
+    arr_length = []
+    for i in range(z_index, 0, -1):
         distance = np.sqrt(((centerline_points[0, i] - centerline_points[0, i - 1]) * px) ** 2 +
                            ((centerline_points[1, i] - centerline_points[1, i - 1]) * py) ** 2 +
                            ((centerline_points[2, i] - centerline_points[2, i - 1]) * pz) ** 2)
         length += distance
         arr_length.append(length)
     arr_length = arr_length[::-1]
-    arr_length = np.stack((arr_length, centerline_points[2]), axis=0)
+    arr_length = np.stack((arr_length, centerline_points[2][:z_index]), axis=0)
     return arr_length
 
 
-def extrapolate_centerline(centerline, im_seg):
+def extrapolate_centerline(centerline, im_seg, pz):
+    """
+    Compute distance from projected PMJ on centerline and cord centerline.
+    :param centerline: 3xn array: Centerline in continuous coordinate (float) for each slice in RPI orientation.
+    :param im_seg: Image(): input segmentation.
+    :param pz: float: z pixel size.
+    """
+    smooth = 30
+    diff = np.sqrt((centerline[0, -1] - np.mean(centerline[0, -6:-1])) ** 2 +
+                   (centerline[1, -1] - np.mean(centerline[1, -6:-1])) ** 2)
+    if diff > 2:
+        centerline = centerline[:, :-5]  # Remove last slices (maybe always do that?)
+    x_mean = centerline[0, :]
+    y_mean = centerline[1, :]
+    z_mean = centerline[2, :]
+    z_ref = np.array(range(im_seg.dim[2]))
+    x_centerline_fit, x_centerline_deriv = curve_fitting.bspline(z_mean, x_mean, z_ref, smooth, pz=pz, deg_bspline=3)
+    y_centerline_fit, y_centerline_deriv = curve_fitting.bspline(z_mean, y_mean, z_ref, smooth, pz=pz, deg_bspline=3)
+    return np.array([x_centerline_fit, y_centerline_fit, z_ref])
 
 
 def get_nearest_index(arr, value):
@@ -147,6 +161,13 @@ def get_nearest_index(arr, value):
     difference_array = np.absolute(arr[0]-value)
     index = arr[1][difference_array.argmin()]
     return int(index)
+
+
+def get_min_distance(pmj, centerline, px, py, pz):
+    distance = np.sqrt(((centerline[0, :] - pmj[0]) * px) ** 2 +
+                       ((centerline[1, :] - pmj[1]) * py) ** 2 +
+                       ((centerline[2, :] - pmj[2]) * pz) ** 2)
+    return int(centerline[2, distance.argmin()])
 
 
 def validate_length(z_min, z_max, arr, value):  # TODO: Find a better way for this
