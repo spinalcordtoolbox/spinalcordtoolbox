decb965286159df0dfb1e98a67880aa08cb9fb61
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-07-20 10:37:59 -0400

    Removed parametrize
    Not needed because we only test one dummy data

--- testing/cli/test_cli_sct_compute_snr.py
+++ testing/cli/test_cli_sct_compute_snr.py
@@ -10,7 +10,7 @@ from spinalcordtoolbox.scripts import sct_compute_snr
 logger = logging.getLogger(__name__)
 
 
-# @pytest.fixture(scope="session")
+@pytest.fixture(scope="session")
 def dummy_3dimage():
     data = np.ones([16, 16, 16])
     affine = np.eye(4)
@@ -20,6 +20,7 @@ def dummy_3dimage():
     return filename.name
 
 
+@pytest.fixture(scope="session")
 def dummy_4dimage():
     data = np.ones([16, 16, 16, 16])
     affine = np.eye(4)
@@ -29,30 +30,20 @@ def dummy_4dimage():
     return filename.name
 
 
-def dummy_5dimage():
-    data = np.ones([16, 16, 16, 16, 3])
-    affine = np.eye(4)
-    nib = nibabel.nifti1.Nifti1Image(data, affine)
-    filename = tempfile.NamedTemporaryFile(suffix='.nii.gz', delete=False)
-    nibabel.save(nib, filename.name)
-    return filename.name
-
-
-@pytest.mark.parametrize('dummy_image', [dummy_3dimage(), dummy_5dimage()])
-def test_sct_compute_snr_check_dimension(dummy_image):
+def test_sct_compute_snr_check_dimension(dummy_3dimage):
     with pytest.raises(ValueError):
-        sct_compute_snr.main(argv=['-i', dummy_image, '-m', dummy_3dimage(), '-method', 'diff', '-vol', '0,5'])
+        sct_compute_snr.main(argv=['-i', dummy_3dimage, '-m', dummy_3dimage, '-method', 'diff', '-vol', '0,5'])
     with pytest.raises(ValueError):
-        sct_compute_snr.main(argv=['-i', dummy_image, '-m', dummy_3dimage(), '-method', 'mult', '-vol', '0,5'])
+        sct_compute_snr.main(argv=['-i', dummy_3dimage, '-m', dummy_3dimage, '-method', 'mult', '-vol', '0,5'])
 
-
-@pytest.mark.sct_testing
-@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
-def test_sct_compute_snr_against_groundtruth(): 
-    """Run the CLI script and check SNR against a ground truth value.""" 
-    fname_out = "computed_snr.txt"
-    sct_compute_snr.main(argv=['-i', 'dmri/dwi.nii.gz', '-m', 'dmri/dmri_T0001.nii.gz', '-method', 'diff', 
-                               '-vol', '0,5', '-o', fname_out])
-    with open(fname_out, "r") as f:
-        snr = float(f.read())
-    assert snr == pytest.approx(2.432321811697386)
+#
+# @pytest.mark.sct_testing
+# @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+# def test_sct_compute_snr_against_groundtruth():
+#     """Run the CLI script and check SNR against a ground truth value."""
+#     fname_out = "computed_snr.txt"
+#     sct_compute_snr.main(argv=['-i', 'dmri/dwi.nii.gz', '-m', 'dmri/dmri_T0001.nii.gz', '-method', 'diff',
+#                                '-vol', '0,5', '-o', fname_out])
+#     with open(fname_out, "r") as f:
+#         snr = float(f.read())
+#     assert snr == pytest.approx(2.432321811697386)
