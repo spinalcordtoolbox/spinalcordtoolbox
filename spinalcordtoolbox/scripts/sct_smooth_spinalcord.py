b42b4bf59bae0419cb3a84d3214831a5161b1f7b
Joshua Newton <joshuacwnewton@gmail.com>
2021-08-23 15:20:02 -0400

    `sct_smooth_spinalcord`: `.nii` -> `.nii.gz` for default output filename

--- spinalcordtoolbox/scripts/sct_smooth_spinalcord.py
+++ spinalcordtoolbox/scripts/sct_smooth_spinalcord.py
@@ -96,7 +96,7 @@ def get_parser():
     optional.add_argument(
         "-o",
         metavar=Metavar.file,
-        help='Output filename. Example: smooth_sc.nii '),
+        help='Output filename. Example: smooth_sc.nii.gz'),
     optional.add_argument(
         '-r',
         choices=[0, 1],
@@ -138,7 +138,7 @@ def main(argv=None):
     if arguments.o is not None:
         fname_out = arguments.o
     else:
-        fname_out = extract_fname(fname_anat)[1] + '_smooth.nii'
+        fname_out = extract_fname(fname_anat)[1] + '_smooth.nii.gz'
 
 
     # Display arguments
