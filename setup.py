636930e04bdbf69f6f2a5fde2f7b35c1fc297940
Joshua Newton <joshuacwnewton@gmail.com>
2021-10-22 14:37:14 -0400

    `setup.py`: Add missing comma

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
                 'sct_dmri_concat_b0_and_dwi',
                 'sct_dmri_concat_bvals',
                 'sct_dmri_concat_bvecs',
-                'sct_dmri_denoise_patch2self'
+                'sct_dmri_denoise_patch2self',
                 'sct_dmri_display_bvecs',
                 'sct_dmri_moco',
                 'sct_dmri_separate_b0_and_dwi',
