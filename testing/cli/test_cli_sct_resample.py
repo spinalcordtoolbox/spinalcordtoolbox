301aed422d14412dd629644e44327dbaac060c98
Joshua Newton <joshuacwnewton@gmail.com>
2021-10-27 14:29:20 -0400

    Test `-mm` cases against exact values (with lower precision)
    It's not important whether the mm values are exactly the same,
    only that they're rough approximations of the requested values
    (since we can only get so close with voxel representations)

--- testing/cli/test_cli_sct_resample.py
+++ testing/cli/test_cli_sct_resample.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 @pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
 @pytest.mark.parametrize("path_in,type_arg,dim,expected_dim", [
-    ('fmri/fmri.nii.gz', '-mm', '1x1x3', (65, 65, 34, 30, 1.0, 0.99999994, 2.9647062, 1.13)),        # 4D, mm
+    ('fmri/fmri.nii.gz', '-mm', '1x1x3', (65, 65, 34, 30, 1.0, 1.0, 3.0, 1.13)),                     # 4D, mm
     ('dmri/dmri.nii.gz', '-f', '0.5x0.5x1', (20, 21, 5, 7, 1.6826923, 1.6826923, 17.5, 2.2)),        # 4D, factor
-    ('t2/t2.nii.gz', '-mm', '0.97x1.14x1.2', (62, 48, 43, 1, 0.96774191, 1.1458334, 1.2093023, 1)),  # 3D, mm
+    ('t2/t2.nii.gz', '-mm', '0.97x1.14x1.2', (62, 48, 43, 1, 0.97, 1.14, 1.2, 1)),                   # 3D, mm
     ('t2/t2.nii.gz', '-vox', '120x110x26', (120, 110, 26, 1, 0.5, 0.5, 2.0, 1)),                     # 3D, vox
 ])
 def test_sct_resample_output_has_expected_dimensions(path_in, type_arg, dim, expected_dim, tmp_path):
@@ -22,5 +22,12 @@ def test_sct_resample_output_has_expected_dimensions(path_in, type_arg, dim, exp
     sct_resample.main(argv=['-i', path_in, '-o', path_out, type_arg, dim, '-v', '1'])
     actual_dim = Image(path_out).dim
 
+    if type_arg == '-mm':
+        # Don't expect the actual mm values to be precise, since `-mm` gets
+        # converted to # of voxels (via rounding), so it's only an approx.
+        abs_tol = 0.05
+    else:
+        abs_tol = 0.0000001  # For the others, the results should be precise
+
     for actual_val, expected_val in zip(actual_dim, expected_dim):
-        assert actual_val == pytest.approx(expected_val, abs=0.0001)
+        assert actual_val == pytest.approx(expected_val, abs=abs_tol)
