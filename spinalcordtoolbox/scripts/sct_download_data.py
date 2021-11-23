7fd2ea718751dd858840c3823c0830a910d9777c
Alexandru Foias <alexfoias@users.noreply.github.com>
2021-11-23 11:57:49 -0500

    Update outdated mirror links (#3596)
    * Model/datasets were migrated from the `sct-data` org to the `spinalcordtoolbox` org
    * The dokuwiki migrated so we have to remove the `neuro.polymtl.ca` mirrors.
    * SCT Course datasets are now managed in [`sct_tutorial_data`](https://github.com/spinalcordtoolbox/sct_tutorial_data) repo -- these can be used to replace some of the dokuwiki links.
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>

--- spinalcordtoolbox/scripts/sct_download_data.py
+++ spinalcordtoolbox/scripts/sct_download_data.py
@@ -23,80 +23,78 @@ from spinalcordtoolbox.utils.sys import init_sct, printv, set_loglevel
 # If exists, favour release artifact straight from github
 DICT_URL = {
     "sct_example_data": [
-        "https://github.com/sct-data/sct_example_data/releases/download/r20180525/20180525_sct_example_data.zip",
+        "https://github.com/spinalcordtoolbox/sct_example_data/releases/download/r20180525/20180525_sct_example_data.zip",
         "https://osf.io/kjcgs/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20180525_sct_example_data.zip",
     ],
     "sct_testing_data": [
-        "https://github.com/sct-data/sct_testing_data/releases/download/r20210330230310/sct_testing_data-r20210330230310.zip",
-        "https://osf.io/download/60629509229503022e6f107d/"
+        "https://github.com/spinalcordtoolbox/sct_testing_data/releases/download/r20210330230310/sct_testing_data-r20210330230310.zip",
+        "https://osf.io/download/60629509229503022e6f107d/",
     ],
     "PAM50": [
-        "https://github.com/sct-data/PAM50/releases/download/r20201104/PAM50-r20201104.zip",
-        "https://osf.io/download/5fa21326a5bb9d00610a5a21/"
+        "https://github.com/spinalcordtoolbox/PAM50/releases/download/r20201104/PAM50-r20201104.zip",
+        "https://osf.io/download/5fa21326a5bb9d00610a5a21/",
     ],
     "MNI-Poly-AMU": [
-        "https://github.com/sct-data/MNI-Poly-AMU/releases/download/r20170310/20170310_MNI-Poly-AMU.zip",
+        "https://github.com/spinalcordtoolbox/MNI-Poly-AMU/releases/download/r20170310/20170310_MNI-Poly-AMU.zip",
         "https://osf.io/sh6h4/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20170310_MNI-Poly-AMU.zip",
     ],
     "gm_model": [
-        "https://osf.io/ugscu/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20160922_gm_model.zip",
+        "https://osf.io/ugscu/?action=download"
     ],
     "optic_models": [
-        "https://github.com/sct-data/optic_models/releases/download/r20170413/20170413_optic_models.zip",
+        "https://github.com/spinalcordtoolbox/optic_models/releases/download/r20170413/20170413_optic_models.zip",
         "https://osf.io/g4fwn/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20170413_optic_models.zip",
     ],
     "pmj_models": [
-        "https://github.com/sct-data/pmj_models/releases/download/r20170922/20170922_pmj_models.zip",
+        "https://github.com/spinalcordtoolbox/pmj_models/releases/download/r20170922/20170922_pmj_models.zip",
         "https://osf.io/4gufr/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20170922_pmj_models.zip",
     ],
     "binaries_linux": [
         "https://osf.io/cs6zt/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20200801_sct_binaries_linux.tar.gz",
     ],
     "binaries_osx": [
         "https://osf.io/874cy?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20200801_sct_binaries_osx.tar.gz",
     ],
-    "course_hawaii17": "https://osf.io/6exht/?action=download",
+    "course_hawaii17": [
+        "https://osf.io/6exht/?action=download",
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/hawaii17.zip",
+    ],
     "course_paris18": [
         "https://osf.io/9bmn5/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20180612_sct_course-paris18.zip",
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/paris18.zip",
     ],
     "course_london19": [
         "https://osf.io/4q3u7/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20190121_sct_course-london19.zip",
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/london19.zip",
     ],
     "course_beijing19": [
         "https://osf.io/ef4xz/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20190802_sct_course-beijing19.zip",
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/beijing19.zip",
+    ],
+    "course_london20": [
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/london20.zip",
+    ],
+    "course_harvard21": [
+        "https://github.com/spinalcordtoolbox/sct_tutorial_data/archive/refs/tags/SCT-Course-20211116.zip",
     ],
     "deepseg_gm_models": [
-        "https://github.com/sct-data/deepseg_gm_models/releases/download/r20180205/20180205_deepseg_gm_models.zip",
+        "https://github.com/spinalcordtoolbox/deepseg_gm_models/releases/download/r20180205/20180205_deepseg_gm_models.zip",
         "https://osf.io/b9y4x/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20180205_deepseg_gm_models.zip",
     ],
     "deepseg_sc_models": [
-        "https://github.com/sct-data/deepseg_sc_models/releases/download/r20180610/20180610_deepseg_sc_models.zip",
+        "https://github.com/spinalcordtoolbox/deepseg_sc_models/releases/download/r20180610/20180610_deepseg_sc_models.zip",
         "https://osf.io/avf97/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20180610_deepseg_sc_models.zip",
     ],
     "deepseg_lesion_models": [
-        "https://github.com/sct-data/deepseg_lesion_models/releases/download/r20180613/20180613_deepseg_lesion_models.zip",
+        "https://github.com/spinalcordtoolbox/deepseg_lesion_models/releases/download/r20180613/20180613_deepseg_lesion_models.zip",
         "https://osf.io/eg7v9/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20180613_deepseg_lesion_models.zip",
     ],
     "c2c3_disc_models": [
-        "https://github.com/sct-data/c2c3_disc_models/releases/download/r20190117/20190117_c2c3_disc_models.zip",
+        "https://github.com/spinalcordtoolbox/c2c3_disc_models/releases/download/r20190117/20190117_c2c3_disc_models.zip",
         "https://osf.io/t97ap/?action=download",
-        "https://www.neuro.polymtl.ca/_media/downloads/sct/20190117_c2c3_disc_models.zip",
     ],
     "exvivo_template": [
-        "https://github.com/sct-data/exvivo-template/archive/refs/tags/r20210317.zip"
+        "https://github.com/spinalcordtoolbox/exvivo-template/archive/refs/tags/r20210317.zip"
     ],
 }
 
