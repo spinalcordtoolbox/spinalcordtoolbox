94e76fb42a8e5090ee739b88cdee56de315ba82c
Noel Rignon <noel.rignon@fjnr.ca>
2022-01-08 10:33:51 -0500

    convert input file to float32 before compute MTR

--- testing/cli/test_cli_sct_compute_mtr.py
+++ testing/cli/test_cli_sct_compute_mtr.py
@@ -1,6 +1,8 @@
 import pytest
 import logging
+import numpy
 
+from spinalcordtoolbox.image import Image
 from spinalcordtoolbox.scripts import sct_compute_mtr
 
 logger = logging.getLogger(__name__)
@@ -8,7 +10,37 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
-def test_sct_compute_mtr_no_checks():
-    """Run the CLI script without checking results.
-    TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
-    sct_compute_mtr.main(argv=['-mt0', 'mt/mt0.nii.gz', '-mt1', 'mt/mt1.nii.gz'])
+def test_sct_compute_mtr_no_checks(tmp_path):
+    """Run the CLI script without checking results."""
+    mtr_output_path = str(tmp_path / 'mtr_output.nii.gz')
+
+    sct_compute_mtr.main(argv=['-mt0', 'mt/mt0.nii.gz', '-mt1', 'mt/mt1.nii.gz', '-o', mtr_output_path])
+
+    # Comparate ground truth mtr file to new generated one
+    ground_truth_mtr = Image('mt/mtr.nii.gz')
+    output_mtr = Image(mtr_output_path)
+    assert numpy.linalg.norm(ground_truth_mtr.data - output_mtr.data) <= 0.001
+
+
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_compute_mtr_with_int16_image_type(tmp_path):
+    """Run the CLI script with int_16 image type"""
+
+    mt0_path = str(tmp_path / 'mt0_int16.nii.gz')
+    mt1_path = str(tmp_path / 'mt1_int16.nii.gz')
+    mtr_output_path = str(tmp_path / 'mtr_output.nii.gz')
+
+    # Generate int16 test file based on sct_testing_data existing one
+    mt0 = Image('mt/mt0.nii.gz')
+    mt0.save(mt0_path, dtype='int16')
+    mt1 = Image('mt/mt1.nii.gz')
+    mt1.save(mt1_path, dtype='int16')
+
+    sct_compute_mtr.main(argv=['-mt0', mt0_path, '-mt1', mt1_path, '-o', mtr_output_path])
+
+    # Comparate ground truth mtr file to new generated one
+    ground_truth_mtr = Image('mt/mtr.nii.gz')
+    output_mtr = Image(mtr_output_path)
+
+    assert numpy.linalg.norm(ground_truth_mtr.data - output_mtr.data) <= 0.001
