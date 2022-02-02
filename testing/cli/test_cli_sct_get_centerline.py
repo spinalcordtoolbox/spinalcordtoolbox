62a32055abe0defc56aac6ba53bb1f11b32070d3
Joshua Newton <joshuacwnewton@gmail.com>
2022-02-02 10:00:18 -0500

    Expand test to check filenames and `-o` argument

--- testing/cli/test_cli_sct_get_centerline.py
+++ testing/cli/test_cli_sct_get_centerline.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import pytest
 
@@ -9,10 +10,20 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
-def test_sct_get_centerline_no_checks(tmp_path):
+@pytest.mark.parametrize('use_o_arg', [pytest.param(True, id="custom_filename"),
+                                       pytest.param(False, id="default_filename")])
+def test_sct_get_centerline_output_file_exists(use_o_arg, tmp_path):
     """Run the CLI script without checking results.
     TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
-    sct_get_centerline.main(argv=['-i', 't2s/t2s.nii.gz', '-c', 't2s', '-qc', str(tmp_path)])
+    if use_o_arg:
+        output_path = os.path.join(tmp_path, 't2s_centerline.nii.gz')
+        sct_get_centerline.main(argv=['-i', 't2s/t2s.nii.gz', '-c', 't2s',
+                                      '-qc', str(tmp_path), '-o', output_path])
+    else:
+        output_path = os.path.join('t2s', 't2s_centerline.nii.gz')
+        sct_get_centerline.main(argv=['-i', 't2s/t2s.nii.gz', '-c', 't2s',
+                                      '-qc', str(tmp_path)])
+    assert os.path.exists(output_path)
     # Note: There actually used to be a check here, but it was replaced by 'test_get_centerline_optic' in
     # 'unit_testing/test_centerline.py'. For more details, see:
     #    * https://github.com/neuropoly/spinalcordtoolbox/pull/2774/commits/5e6bd57abf6bcf825cd110e0d74b8e465d298409
