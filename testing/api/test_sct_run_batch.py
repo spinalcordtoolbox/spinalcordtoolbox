4562421876e7190eab61032a78761cc5972d3520
Joshua Newton <joshuacwnewton@gmail.com>
2022-02-17 16:07:59 -0500

    `test_sct_run_batch.py`: Fixup tests to properly create tmp files
    A couple of problems existed here:
    
    1. The `tempfile` library has some gotchas on Windows that these
    tests fell victim to: https://stackoverflow.com/q/23212435. So,
    use pytest's built-in tmp_path fixture instead.
    2. The tests were writing plaintext to a .json file, when it's
    much better to use the `json` library (which hands line-endings)
    
    There are other tests here that require this work as well, but
    because they're already skipped for Windows, I've left them for now.

--- testing/api/test_sct_run_batch.py
+++ testing/api/test_sct_run_batch.py
@@ -1,8 +1,9 @@
 import glob
 import os
 import sys
+import json
+
 import pytest
-from tempfile import NamedTemporaryFile, TemporaryDirectory
 from textwrap import dedent
 
 from spinalcordtoolbox import __sct_dir__
@@ -12,46 +13,43 @@ sys.path.append(os.path.join(__sct_dir__, 'scripts'))
 from spinalcordtoolbox.scripts import sct_run_batch
 
 
-def write_dummy_script(script):
-    """
-    Dummy script that displays subject
-    :param script: NamedTemporaryFile object. Script to write
-    :return:
-    """
+@pytest.fixture
+def dummy_script(tmp_path):
+    """Dummy executable script that displays subject."""
+    path_out = str(tmp_path / "dummy_script.sh")
     script_text = """
     #!/bin/bash
     SUBJECT=$1
     echo $SUBJECT
     """
-    # indexing removes beginning newline
-    script.write(dedent(script_text)[1:])
-    script.flush()
+    with open(path_out, 'w') as script:
+        # indexing removes beginning newline
+        script.write(dedent(script_text)[1:])
+        script.flush()
+    return path_out
 
 
-def test_config_with_args_warning():
-    # This is formatted strangely because pep8 says so.
-    with \
-        NamedTemporaryFile('w', suffix='.json') as config,\
-            TemporaryDirectory() as data,\
-            TemporaryDirectory() as out:
+def test_config_with_args_warning(tmp_path):
+    data = str(tmp_path / 'data')
+    out = str(tmp_path / 'out')
+    config_path = str(tmp_path / 'config.json')
 
-        cnf_text = '{{"jobs": 1, "path_data": "{}", "path_output": "{}"}}'.format(data, out)
-        config.write(cnf_text)
-        config.flush()
+    with open(config_path, 'w') as config:
+        cnf_text = {"jobs": 1, "path_data": data, "path_output": out}
+        json.dump(cnf_text, config)
 
-        with pytest.warns(UserWarning, match=r'-config.*discouraged'):
-            # I'm not sure how to check that argparse is printing the right error here, but I trust
-            with pytest.raises(FileNotFoundError):
-                sct_run_batch.main(['-c', config.name, '-include', 'something', '-script', 'script'])
+    with pytest.warns(UserWarning, match=r'-config.*discouraged'):
+        # I'm not sure how to check that argparse is printing the right error here, but I trust
+        with pytest.raises(FileNotFoundError):
+            sct_run_batch.main(['-c', config_path, '-include', 'something', '-script', 'script'])
 
 
-def test_config_extra_value_warning():
-    # This is formatted strangely because pep8 says so.
-    with \
-        NamedTemporaryFile('w', suffix='.yml') as config,\
-            TemporaryDirectory() as data,\
-            TemporaryDirectory() as out:
+def test_config_extra_value_warning(tmp_path):
+    data = str(tmp_path / 'data')
+    out = str(tmp_path / 'out')
+    config_path = str(tmp_path / 'config.yml')
 
+    with open(config_path, 'w') as config:
         cnf_text = """
         jobs: 1
         path_data: {}
@@ -61,70 +59,65 @@ def test_config_extra_value_warning():
         config.write(dedent(cnf_text))
         config.flush()
 
-        with pytest.warns(UserWarning, match='unknowable'):
-            # I'm not sure how to check that argparse is printing the right error here, but I trust
-            with pytest.raises(FileNotFoundError):
-                sct_run_batch.main(['-c', config.name, '-script', 'script'])
+    with pytest.warns(UserWarning, match='unknowable'):
+        # I'm not sure how to check that argparse is printing the right error here, but I trust
+        with pytest.raises(FileNotFoundError):
+            sct_run_batch.main(['-c', config_path, '-script', 'script'])
 
 
-def test_only_one_include():
-    # This is formatted strangely because pep8 says so.
-    with \
-        TemporaryDirectory() as data,\
-            TemporaryDirectory() as out:
-        with pytest.raises(AssertionError, match='Only one'):
-            sct_run_batch.main(['-include', 'arg', '-include-list',
-                                'arg2', '-path-data', data, '-path-out', out
-                                , '-script', out])
+def test_only_one_include(tmp_path):
+    data = tmp_path / 'data'
+    data.mkdir()
+    out = tmp_path / 'out'
+    with pytest.raises(AssertionError, match='Only one'):
+        sct_run_batch.main(['-include', 'arg', '-include-list', 'arg2',
+                            '-path-data', str(data), '-path-out', str(out), '-script', str(out)])
 
 
-def test_non_executable_task():
+def test_non_executable_task(tmp_path, dummy_script):
     data_path = sct_test_path()
-    with \
-        NamedTemporaryFile('w', suffix='.sh') as script,\
-            TemporaryDirectory() as out:
+    out = tmp_path / 'out'
 
-        script_text = """
-        #!/bin/bash
-        echo $SUBJECT
-        """
-        script.write(dedent(script_text)[1:]) #indexing removes beginning newline
+    script_text = """
+    #!/bin/bash
+    echo $SUBJECT
+    """
+    with open(tmp_path / "dummy_nonexectuable_script.sh", 'w') as script:
+        script.write(dedent(script_text)[1:])  # indexing removes beginning newline
         script.flush()
 
-        assert not os.access(script.name, os.X_OK), "Script already executable"
+    assert not os.access(script.name, os.X_OK), "Script already executable"
 
-        sct_run_batch.main(['-include', '^t.*',
-                            '-subject-prefix', '',
-                            '-path-data', data_path, '-path-out', out,
-                            '-script', script.name,
-                            '-continue-on-error', 0])
+    sct_run_batch.main(['-include', '^t.*',
+                        '-subject-prefix', '',
+                        '-path-data', data_path, '-path-out', str(out),
+                        '-script', script.name,
+                        '-continue-on-error', 0])
 
 
-def test_no_sessions():
+def test_no_sessions(tmp_path, dummy_script):
     # Test that sessions ('ses') can be separated so that sct_run_batch can process each session folder separately.
-    with TemporaryDirectory() as data,\
-            TemporaryDirectory() as out,\
-            NamedTemporaryFile('w', suffix='.sh') as script:
-        # Create dummy BIDS directory with sessions
-        os.makedirs(os.path.join(data, 'sub-01', 'anat'))
-        os.makedirs(os.path.join(data, 'sub-02', 'anat'))
-        write_dummy_script(script)
-        sct_run_batch.main(['-path-data', data, '-path-out', out, '-script', script.name])
-        file_log = glob.glob(os.path.join(out, 'log', '*sub-01.log'))[0]
-        assert 'sub-01' in open(file_log, "r").read()
+    data = tmp_path / 'data'
+    data.mkdir()
+    out = tmp_path / 'out'
+    # Create dummy BIDS directory with sessions
+    os.makedirs(os.path.join(data, 'sub-01', 'anat'))
+    os.makedirs(os.path.join(data, 'sub-02', 'anat'))
+    sct_run_batch.main(['-path-data', str(data), '-path-out', str(out), '-script', dummy_script])
+    file_log = glob.glob(os.path.join(out, 'log', '*sub-01.log'))[0]
+    assert 'sub-01' in open(file_log, "r").read()
 
 
-def test_separate_sessions():
+def test_separate_sessions(tmp_path, dummy_script):
     # Test that sessions ('ses') can be separated so that sct_run_batch can process each session folder separately.
-    with TemporaryDirectory() as data,\
-            TemporaryDirectory() as out,\
-            NamedTemporaryFile('w', suffix='.sh') as script:
-        # Create dummy BIDS directory with sessions
-        sub_ses_pairs = [('01', '01'), ('01', '02'), ('01', '03'), ('02', '01'), ('02', '02')]
-        for sub, ses in sub_ses_pairs:
-            os.makedirs(os.path.join(data, f'sub-{sub}', f'ses-{ses}'))
-        write_dummy_script(script)
-        sct_run_batch.main(['-path-data', data, '-path-out', out, '-script', script.name])
-        for sub, ses in sub_ses_pairs:
-            file_log = glob.glob(os.path.join(out, 'log', f'*sub-{sub}_ses-{ses}.log'))[0]
-            assert f'sub-{sub}/ses-{ses}' in open(file_log, "r").read()
+    data = tmp_path / 'data'
+    data.mkdir()
+    out = tmp_path / 'out'
+    # Create dummy BIDS directory with sessions
+    sub_ses_pairs = [('01', '01'), ('01', '02'), ('01', '03'), ('02', '01'), ('02', '02')]
+    for sub, ses in sub_ses_pairs:
+        os.makedirs(os.path.join(data, f'sub-{sub}', f'ses-{ses}'))
+    sct_run_batch.main(['-path-data', str(data), '-path-out', str(out), '-script', dummy_script])
+    for sub, ses in sub_ses_pairs:
+        file_log = glob.glob(os.path.join(out, 'log', f'*sub-{sub}_ses-{ses}.log'))[0]
+        assert f'sub-{sub}/ses-{ses}' in open(file_log, "r").read()
