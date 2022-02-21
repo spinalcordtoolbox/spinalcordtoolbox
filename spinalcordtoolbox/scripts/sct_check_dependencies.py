a1be7a07cc9d3c51a0407781615eff616fbaeef2
Joshua Newton <joshuacwnewton@gmail.com>
2022-02-21 18:48:06 -0500

    `sct_check_dependencies`: Don't check for FSLeyes on Win

--- spinalcordtoolbox/scripts/sct_check_dependencies.py
+++ spinalcordtoolbox/scripts/sct_check_dependencies.py
@@ -353,30 +353,31 @@ def main(argv=None):
             print(err)
 
     # Check version of FSLeyes
-    print_line('Check FSLeyes version')
-    cmd = 'fsleyes --version'
-    status, output = run_proc(cmd, verbose=0, raise_exception=False)
-    # Exit code 0 - command has run successfully
-    if status == 0:
-        # Fetch only version number (full output of 'fsleyes --version' is 'fsleyes/FSLeyes version 0.34.2')
-        fsleyes_version = output.split()[2]
-        print_ok(more=(" (%s)" % fsleyes_version))
-    # Exit code 126 - Command invoked cannot execute (permission problem or command is not an executable)
-    elif status == 126:
-        print('Command not executable. Please check permissions of fsleyes command.')
-    # Exit code 127 - Command not found (possible problem with $PATH)
-    elif status == 127:
-        print('Command not found. If you installed FSLeyes as part of FSL package, please check that FSL is included '
-              'in $PATH variable. If you installed FSLeyes using conda environment, make sure that the environment is '
-              'activated. If you do not have FSLeyes installed, consider its installation to easily visualize '
-              'processing outputs and/or to use SCT within FSLeyes. More info at: '
-              'https://spinalcordtoolbox.com/en/latest/user_section/fsleyes.html')
-    # All other exit codes
-    else:
-        print(f'Exit code {status} occurred. Please report this issue on SCT GitHub: '
-              f'https://github.com/spinalcordtoolbox/spinalcordtoolbox/issues')
-        if complete_test:
-            print(output)
+    if sys.platform != 'win32':
+        print_line('Check FSLeyes version')
+        cmd = 'fsleyes --version'
+        status, output = run_proc(cmd, verbose=0, raise_exception=False)
+        # Exit code 0 - command has run successfully
+        if status == 0:
+            # Fetch only version number (full output of 'fsleyes --version' is 'fsleyes/FSLeyes version 0.34.2')
+            fsleyes_version = output.split()[2]
+            print_ok(more=(" (%s)" % fsleyes_version))
+        # Exit code 126 - Command invoked cannot execute (permission problem or command is not an executable)
+        elif status == 126:
+            print('Command not executable. Please check permissions of fsleyes command.')
+        # Exit code 127 - Command not found (possible problem with $PATH)
+        elif status == 127:
+            print('Command not found. If you installed FSLeyes as part of FSL package, please check that FSL is included '
+                  'in $PATH variable. If you installed FSLeyes using conda environment, make sure that the environment is '
+                  'activated. If you do not have FSLeyes installed, consider its installation to easily visualize '
+                  'processing outputs and/or to use SCT within FSLeyes. More info at: '
+                  'https://spinalcordtoolbox.com/en/latest/user_section/fsleyes.html')
+        # All other exit codes
+        else:
+            print(f'Exit code {status} occurred. Please report this issue on SCT GitHub: '
+                  f'https://github.com/spinalcordtoolbox/spinalcordtoolbox/issues')
+            if complete_test:
+                print(output)
 
     print('')
     sys.exit(e + install_software)
