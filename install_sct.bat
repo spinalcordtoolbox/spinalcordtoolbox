bb49f5d430595380b5b357c72e6aedf61cc9c511
Joshua Newton <joshuacwnewton@gmail.com>
2022-03-01 16:58:16 -0500

    `install_sct.bat`: Fixup `goto` syntax

--- install_sct.bat
+++ install_sct.bat
@@ -14,28 +14,28 @@ pushd %HOMEPATH%
 :: Download SCT and check out the branch requested by the user
 echo:
 echo ### Downloading SCT source code to %HOMEPATH%...
-git clone -b %git_ref% --single-branch --depth 1 https://github.com/spinalcordtoolbox/spinalcordtoolbox.git || goto :error
+git clone -b %git_ref% --single-branch --depth 1 https://github.com/spinalcordtoolbox/spinalcordtoolbox.git || goto error
 
 :: Create and activate virtual environment to install SCT into
 echo:
 echo ### Using Python to create virtual environment...
 cd spinalcordtoolbox
-python -m venv venv_sct || goto :error
-CALL venv_sct\Scripts\activate.bat || goto :error
+python -m venv venv_sct || goto error
+CALL venv_sct\Scripts\activate.bat || goto error
 echo Virtual environment created and activated successfully!
 
 :: Install SCT and its requirements
 echo:
 echo ### Installing SCT and its dependencies...
 :: Pip needs to be upgraded because default p3.7 pip won't resolve dependency conflicts correctly
-python -m pip install --upgrade pip || goto :error
-pip install -r requirements.txt || goto :error
-pip install -e . || goto :error
+python -m pip install --upgrade pip || goto error
+pip install -r requirements.txt || goto error
+pip install -e . || goto error
 
 :: Install external dependencies
 echo:
 echo ### Downloading model files and binaries...
-FOR %%D IN (PAM50 gm_model optic_models pmj_models deepseg_sc_models deepseg_gm_models deepseg_lesion_models c2c3_disc_models binaries_win) DO sct_download_data -d %%D -k || goto :error
+FOR %%D IN (PAM50 gm_model optic_models pmj_models deepseg_sc_models deepseg_gm_models deepseg_lesion_models c2c3_disc_models binaries_win) DO sct_download_data -d %%D -k || goto error
 
 :: Deactivate virtual environment
 deactivate
