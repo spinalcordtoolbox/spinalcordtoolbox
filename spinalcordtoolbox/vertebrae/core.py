6fe991ae8970c00d95a220643bf51ea4ffb835b8
Joshua Newton <joshuacwnewton@gmail.com>
2022-03-23 13:34:43 -0400

    `vertebrae/core.py`: Swap the order of cleaning steps
    Assign labels, *then* remove extra labels
    
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>

--- spinalcordtoolbox/vertebrae/core.py
+++ spinalcordtoolbox/vertebrae/core.py
@@ -320,21 +320,20 @@ def clean_labeled_segmentation(fname_labeled_seg, fname_seg, fname_labeled_seg_n
     :param fname_labeled_seg_new: output
     :return: none
     """
-    # remove voxels in segmentation_labeled that are not in segmentation
     img_labeled_seg = Image(fname_labeled_seg)
     img_seg = Image(fname_seg)
-    data_labeled_seg_mul = img_labeled_seg.data * img_seg.data
     # dilate to add voxels in segmentation that are not in segmentation_labeled
     data_labeled_seg_dil = dilate(img_labeled_seg.data, 2, 'ball')
-    data_labeled_seg_mul_bin = data_labeled_seg_mul > 0
-    data_diff = img_seg.data - data_labeled_seg_mul_bin
+    data_labeled_seg_bin = img_labeled_seg.data > 0
+    data_diff = img_seg.data - data_labeled_seg_bin
     ind_nonzero = np.where(data_diff)
     img_labeled_seg_corr = img_labeled_seg.copy()
-    img_labeled_seg_corr.data = data_labeled_seg_mul
     for i_vox in range(len(ind_nonzero[0])):
         # assign closest label value for this voxel
         ix, iy, iz = ind_nonzero[0][i_vox], ind_nonzero[1][i_vox], ind_nonzero[2][i_vox]
         img_labeled_seg_corr.data[ix, iy, iz] = data_labeled_seg_dil[ix, iy, iz]
+    # remove voxels in segmentation_labeled that are not in segmentation
+    img_labeled_seg_corr.data *= img_seg.data
     # save new label file (overwrite)
     img_labeled_seg_corr.absolutepath = fname_labeled_seg_new
     img_labeled_seg_corr.save()
