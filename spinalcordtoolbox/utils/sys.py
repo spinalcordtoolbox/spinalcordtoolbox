a941d850546a4330f7a450d5b5510f171c4fd3a5
Joshua Newton <joshuacwnewton@gmail.com>
2022-04-11 14:47:24 -0400

    Add support for ITK-Snap + multiple viewers to `display_viewer_syntax` (#3754)

--- spinalcordtoolbox/utils/sys.py
+++ spinalcordtoolbox/utils/sys.py
@@ -5,6 +5,7 @@
 import io
 import sys
 import os
+import shutil
 import logging
 import subprocess
 import time
@@ -410,25 +411,20 @@ def sct_test_path(*args):
 
 def check_exe(name):
     """
-    Ensure that a program exists
+    Ensure that a program exists and can be executed
 
-    :param name: str: name or path to program
-    :return: path of the program or None
+    :param name: str: name of program or path to program
+    :return: boolean
     """
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, _ = os.path.split(name)
-    if fpath and is_exe(name):
-        return fpath
+    _, filename = os.path.split(name)
+    # Case 1: Check full filepath directly (which may point to a location not on the PATH)
+    if os.path.isfile(name) and os.access(name, os.X_OK):
+        return True
+    # Case 2: Check filename only via the PATH
+    elif shutil.which(filename) and os.access(shutil.which(filename), os.X_OK):
+        return True
     else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, name)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
+        return False
 
 
 def _version_string():
