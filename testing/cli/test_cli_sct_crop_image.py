6b891e141fa5bd36276f162f3884b313d0ae6786
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-04-15 17:37:37 -0400

    Add a failing test

--- testing/cli/test_cli_sct_crop_image.py
+++ testing/cli/test_cli_sct_crop_image.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
     ('t2/t2.nii.gz', 't2_crop_xyz.nii', ['-xmin', '1', '-xmax', '-3', '-ymin', '2', '-ymax', '10'], (57, 9, 52)),
     ('t2/t2.nii.gz', 't2_crop_mask.nii', ['-m', 't2/t2_seg-manual.nii.gz'], (11, 55, 13)),
     ('t2/t2.nii.gz', 't2_crop_ref.nii', ['-ref', 'mt/mt0.nii.gz'], (37, 55, 34)),
+    ('qform/bad_quaternion.nii.gz', 'bad_quaternion_crop.nii', [], (2, 3, 4)),
 ])
 def test_sct_crop_image_output_has_expected_dimensions(path_in, path_out, remaining_args, expected_dim):
     """Run the CLI script and verify cropped image has the expected dimensions."""
