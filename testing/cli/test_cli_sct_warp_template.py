579b3957450dd003eeaaeb8f5a20683334c67e4c
Joshua Newton <joshuacwnewton@gmail.com>
2022-05-30 12:29:14 -0400

    `test_cli_sct_warp_template.py`: Clean up existing tests

--- testing/cli/test_cli_sct_warp_template.py
+++ testing/cli/test_cli_sct_warp_template.py
@@ -6,18 +6,18 @@ from spinalcordtoolbox.scripts import sct_warp_template
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.sct_testing
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
-def test_sct_warp_template_no_checks():
-    """Run the CLI script without checking results.
-    TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
-    sct_warp_template.main(argv=['-d', 'mt/mt1.nii.gz', '-w', 'mt/warp_template2mt.nii.gz', '-a', '0',
-                                 '-histo', '1',
-                                 '-t', 'template', '-qc', 'testing-qc'])
+def test_sct_warp_template_warp_small_PAM50():
+    """Warp the cropped, resampled version of the template from `sct_testing_data/template`."""
+    sct_warp_template.main(argv=['-d', 'mt/mt1.nii.gz', '-w', 'mt/warp_template2mt.nii.gz',
+                                 '-a', '0',  # -a is '1' by default, but atlas isn't present in 'template'
+                                 '-t', 'template',
+                                 '-qc', 'testing-qc'])
 
 
 @pytest.mark.usefixtures("run_in_sct_testing_data_dir")
 def test_sct_warp_template_warp_full_PAM50():
-    """Warp the full PAM50 template."""
+    """Warp the full PAM50 template (i.e. the one that is downloaded to `data/PAM50` during installation)."""
     sct_warp_template.main(argv=['-d', 'mt/mt1.nii.gz', '-w', 'mt/warp_template2mt.nii.gz',
-                                 '-a', '1', '-histo', '1', '-qc', 'testing-qc'])
+                                 '-a', '1', '-s', '1', '-histo', '1',
+                                 '-qc', 'testing-qc'])
