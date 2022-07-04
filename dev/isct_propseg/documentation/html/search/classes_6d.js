7b4aed23acf18218027466bba71e50d331d5bea4
Joshua Newton <joshuacwnewton@gmail.com>
2022-07-04 09:44:31 -0400

    Delete the entire `dev/` folder from the `master` branch (#3827)

--- dev/isct_propseg/documentation/html/search/classes_6d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mesh',['Mesh',['../class_mesh.html',1,'']]],
-  ['meshtovtkpolydata',['MeshToVTKPolyData',['../class_mesh_to_v_t_k_poly_data.html',1,'']]]
-];
--- dev/isct_propseg/documentation/html/search/functions_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['printself',['PrintSelf',['../classitk_1_1_hough_transform2_d_circles_image_filter.html#a11b4e9135d6ec2b6e9bdb8fa2b96bc68',1,'itk::HoughTransform2DCirclesImageFilter']]]
-];
