1e98ededa854727f163f04f02e13c4e23e7a91bc
sandrinebedard <sandrine.bedard@polymtl.ca>
2022-08-10 17:20:59 -0400

    revert if statement since test fails

--- spinalcordtoolbox/aggregate_slicewise.py
+++ spinalcordtoolbox/aggregate_slicewise.py
@@ -264,7 +264,7 @@ def aggregate_per_slice_or_level(metric, mask=None, slices=[], levels=[], distan
         distance_pmj = None
     # If user neither specified slices nor levels, set perslice=True, otherwise, the output will likely contain nan
     # because in many cases the segmentation does not span the whole I-S dimension.
-    if not perslice:
+    if perslice is None:
         if not slices and not levels:
             perslice = True
         else:
