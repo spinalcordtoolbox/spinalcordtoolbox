b8a2bb193b7a06f2973f777875ccbd219ac067bb
Joshua Newton <joshuacwnewton@gmail.com>
2022-10-11 11:39:45 -0400

    `isct_test_ants`: Rework CLI script into callable function

--- spinalcordtoolbox/testing/isct_test_ants.py
+++ spinalcordtoolbox/testing/isct_test_ants.py
@@ -1,34 +1,19 @@
-#!/usr/bin/env python
-#########################################################################################
-#
-# This function test the integrity of ANTs output, given that some versions of ANTs give a wrong BSpline transform,
-# notably when using sct_ANTSUseLandmarkImagesToGetBSplineDisplacementField.
-#
-# ---------------------------------------------------------------------------------------
-# Copyright (c) 2014 Polytechnique Montreal <www.neuro.polymtl.ca>
-# Authors: Julien Cohen-Adad
-# Modified: 2014-07-02
-#
-# About the license: see the file LICENSE.TXT
-#########################################################################################
-
-import sys
-import getopt
 import os
 
 import numpy as np
 import nibabel as nib
 
-from spinalcordtoolbox.utils.sys import init_sct, run_proc, printv
+from spinalcordtoolbox.utils.sys import run_proc, printv
 from spinalcordtoolbox.utils.fs import tmp_create, rmtree
 
 from spinalcordtoolbox.scripts import sct_dice_coefficient
 
 
-# main
-# =======================================================================================================================
-def main():
-
+def test_ants_registration():
+    """
+    This function test the integrity of ANTs output, given that some versions of ANTs give a wrong BSpline transform,
+    notably when using sct_ANTSUseLandmarkImagesToGetBSplineDisplacementField.
+    """
     # Initialization
     size_data = 61
     size_label = 1  # put zero for labels that are single points.
@@ -36,21 +21,6 @@ def main():
     test_passed = 0
     remove_temp_files = 1
     verbose = 1
-
-    # Check input parameters
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hvr:')
-    except getopt.GetoptError:
-        usage()
-        raise SystemExit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-            return
-        elif opt in ('-v'):
-            verbose = int(arg)
-        elif opt in ('-r'):
-            remove_temp_files = int(arg)
 
     path_tmp = tmp_create(basename="test_ants")
 
@@ -139,36 +109,7 @@ def main():
     # output result for parent function
     if test_passed:
         printv('\nTest passed!\n', verbose)
+        return 0
     else:
         printv('\nTest failed!\n', verbose)
-        raise SystemExit(1)
-
-
-# printv(usage)
-# ==========================================================================================
-def usage():
-    print('\n'
-          '' + os.path.basename(__file__) + '\n'
-          '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
-          'Part of the Spinal Cord Toolbox <https://sourceforge.net/projects/spinalcordtoolbox>\n'
-          '\n'
-          'DESCRIPTION\n'
-          '  This function test the integrity of ANTs output, given that some versions of ANTs give a wrong BSpline '
-          '  transform notably when using sct_ANTSUseLandmarkImagesToGetBSplineDisplacementField..\n'
-          '\n'
-          'USAGE\n'
-          '  ' + os.path.basename(__file__) + '\n'
-          '\n'
-          'OPTIONAL ARGUMENTS\n'
-          '  -h                         show this help\n'
-          '  -r {0, 1}                  remove temp files. Default=1\n'
-          '  -v {0, 1}                  verbose. Default=1\n'
-          '\n')
-
-
-# Start program
-# =======================================================================================================================
-if __name__ == "__main__":
-    init_sct()
-    # call main function
-    main()
+        return 1
