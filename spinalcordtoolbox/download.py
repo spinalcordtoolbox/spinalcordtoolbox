7b9e09112a1d907ddbeb71381f684436498ebea1
Joshua Newton <joshuacwnewton@gmail.com>
2022-10-18 12:23:31 -0400

    Refactor data downloading API to remove dependency on CLI script (#3907)
    Co-authored-by: Nick <nick.guenther@polymtl.ca>

--- spinalcordtoolbox/download.py
+++ spinalcordtoolbox/download.py
@@ -8,11 +8,165 @@ import tarfile
 import zipfile
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util import Retry
+from urllib3.util import Retry
 
-from spinalcordtoolbox.utils import sct_progress_bar, tmp_create
+from spinalcordtoolbox.utils.fs import tmp_create
+from spinalcordtoolbox.utils.sys import sct_progress_bar, __sct_dir__, __bin_dir__
 
 logger = logging.getLogger(__name__)
+
+
+# Dictionary containing list of URLs and locations for datasets.
+# Mirror servers are listed in order of decreasing priority.
+# If exists, favour release artifact straight from github
+# For the location field, this is where the dataset will be
+# downloaded to (relative to the repo) if a location isn't passed by
+# the user.
+DATASET_DICT = {
+    "sct_example_data": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/sct_example_data/releases/download/r20180525/20180525_sct_example_data.zip",
+            "https://osf.io/kjcgs/?action=download",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "sct_example_data"),
+    },
+    "sct_testing_data": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/sct_testing_data/releases/download/r20210330230310/sct_testing_data-r20210330230310.zip",
+            "https://osf.io/download/60629509229503022e6f107d/",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "sct_testing_data"),
+    },
+    "PAM50": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/PAM50/releases/download/r20201104/PAM50-r20201104.zip",
+            "https://osf.io/download/5fa21326a5bb9d00610a5a21/",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "PAM50"),
+    },
+    "MNI-Poly-AMU": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/MNI-Poly-AMU/releases/download/r20170310/20170310_MNI-Poly-AMU.zip",
+            "https://osf.io/sh6h4/?action=download",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "MNI-Poly-AMU"),
+    },
+    "gm_model": {
+        "mirrors": [
+            "https://osf.io/ugscu/?action=download"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "gm_model"),
+    },
+    "optic_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/optic_models/releases/download/r20170413/20170413_optic_models.zip",
+            "https://osf.io/g4fwn/?action=download",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "optic_models"),
+    },
+    "pmj_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/pmj_models/releases/download/r20170922/20170922_pmj_models.zip",
+            "https://osf.io/4gufr/?action=download",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "pmj_models"),
+    },
+    "binaries_linux": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/spinalcordtoolbox-binaries/releases/download/r20220516-2/spinalcordtoolbox-binaries_linux.tar.gz",
+        ],
+        "default_location": __bin_dir__,
+    },
+    "binaries_osx": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/spinalcordtoolbox-binaries/releases/download/r20220516-2/spinalcordtoolbox-binaries_osx.tar.gz",
+        ],
+        "default_location": __bin_dir__,
+    },
+    "binaries_win": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/spinalcordtoolbox-binaries/releases/download/r20220516-2/spinalcordtoolbox-binaries_windows.tar.gz",
+        ],
+        "default_location": __bin_dir__,
+    },
+    "course_hawaii17": {
+        "mirrors": [
+            "https://osf.io/6exht/?action=download",
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/hawaii17.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_hawaii17"),
+    },
+    "course_paris18": {
+        "mirrors": [
+            "https://osf.io/9bmn5/?action=download",
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/paris18.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_paris18"),
+    },
+    "course_london19": {
+        "mirrors": [
+            "https://osf.io/4q3u7/?action=download",
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/london19.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_london19"),
+    },
+    "course_beijing19": {
+        "mirrors": [
+            "https://osf.io/ef4xz/?action=download",
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/beijing19.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_beijing19"),
+    },
+    "course_london20": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/SCT-Course/london20.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_london20"),
+    },
+    "course_harvard21": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/sct_tutorial_data/archive/refs/tags/SCT-Course-20211116.zip",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "course_harvard21"),
+    },
+    "deepseg_gm_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/deepseg_gm_models/releases/download/r20180205/20220325_deepseg_gm_models_onnx.zip"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "deepseg_gm_models"),
+    },
+    "deepseg_sc_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/deepseg_sc_models/releases/download/r20180610/20220325_deepseg_sc_models_onnx.zip"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "deepseg_sc_models"),
+    },
+    "deepseg_lesion_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/deepseg_lesion_models/releases/download/r20180613/20220325_deepseg_lesion_models_onnx.zip"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "deepseg_lesion_models"),
+    },
+    "c2c3_disc_models": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/c2c3_disc_models/releases/download/r20190117/20190117_c2c3_disc_models.zip",
+            "https://osf.io/t97ap/?action=download",
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "c2c3_disc_models"),
+    },
+    "exvivo_template": {
+        "mirrors": [
+            "https://github.com/spinalcordtoolbox/exvivo-template/archive/refs/tags/r20210317.zip"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "exvivo_template"),
+    },
+    "deepreg_models": {
+        "mirrors": [
+            "https://github.com/ivadomed/multimodal-registration/releases/download/r20220512/models.zip"
+        ],
+        "default_location": os.path.join(__sct_dir__, "data", "deepreg_models"),
+    }
+}
 
 
 def download_data(urls):
@@ -195,3 +349,20 @@ def install_data(url, dest_folder, keep=False):
     logger.info("Removing temporary folders...")
     shutil.rmtree(os.path.dirname(tmp_file))
     shutil.rmtree(extraction_folder)
+
+
+def install_named_dataset(dataset_name, dest_folder=None, keep=False):
+    """
+    A light wrapper for the 'install_data' function to allow downstream consumers to download
+    datasets using only the dataset's name (i.e. without needing to access DATASET_DICT fields).
+    """
+    if dataset_name not in DATASET_DICT.keys():
+        # This `lambda` accounts for capitals (A, a, B, b), see https://stackoverflow.com/a/10269828
+        raise ValueError(f"Dataset '{dataset_name}' is not contained in list of datasets. Choose from:\n\n "
+                         f"{sorted(list(DATASET_DICT.keys()), key=str.casefold)}")
+
+    urls = DATASET_DICT[dataset_name]["mirrors"]
+    if dest_folder is None:
+        dest_folder = DATASET_DICT[dataset_name]["default_location"]
+
+    install_data(urls, dest_folder, keep)
