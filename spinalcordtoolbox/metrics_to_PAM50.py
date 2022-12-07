9dcb389600134d57f858adf09300769543a32739
valosekj <jan.valosek@upol.cz>
2022-12-06 20:47:01 -0500

    Clarify names of nb_slices variables

--- spinalcordtoolbox/metrics_to_PAM50.py
+++ spinalcordtoolbox/metrics_to_PAM50.py
@@ -55,10 +55,14 @@ def interpolate_metrics(metrics, fname_vert_levels_PAM50, fname_vert_levels):
             # Get slices corresponding for the currently processed level
             slices_PAM50 = get_slices_from_vertebral_levels(im_seg_labeled_PAM50, level)
             slices_im = get_slices_from_vertebral_levels(im_seg_labeled, level)
-            nb_slices = len(slices_PAM50)
-            x_PAM50 = np.arange(0, nb_slices, 1)
-            x = np.linspace(0, nb_slices - 1, len(slices_im))
-            scales.append(nb_slices/len(slices_im))
+            # Get number of slices for the currently processed level
+            nb_slices_PAM50 = len(slices_PAM50)
+            nb_slices_im = len(slices_im)
+            # Prepare vectors for the interpolation
+            x_PAM50 = np.arange(0, nb_slices_PAM50, 1)
+            x = np.linspace(0, nb_slices_PAM50 - 1, nb_slices_im)
+            # Compute and keep the scaling factor for the currently processed level
+            scales.append(nb_slices_PAM50/nb_slices_im)
             # Loop through metrics
             for key, value in metrics.items():
                 metric_values_level = value.data[slices_im]
