91b4c2c858841623bd3caac56038157e8ecf896b
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2023-02-14 16:02:26 -0500

    Simplify exception handling
    There's no need to `except Exception: raise Exception`.

--- spinalcordtoolbox/registration/algorithms.py
+++ spinalcordtoolbox/registration/algorithms.py
@@ -873,47 +873,44 @@ def register2d_centermassrot(fname_src, fname_dest, paramreg=None, fname_warp='w
                 # plt.figure
                 plt.subplot(isub)
                 # ax = matplotlib.pyplot.axis()
-                try:
-                    if isub == 221:
-                        plt.scatter(coord_src[iz][:, 0], coord_src[iz][:, 1], s=5, marker='o', zorder=10, color='steelblue',
-                                    alpha=0.5)
-                        pcaaxis = pca_src[iz].components_.T
-                        pca_eigenratio = pca_src[iz].explained_variance_ratio_
-                        plt.title('src')
-                    elif isub == 222:
-                        plt.scatter(
-                            [coord_src_rot[i, 0] for i in range(len(coord_src_rot))],
-                            [coord_src_rot[i, 1] for i in range(len(coord_src_rot))],
-                            s=5, marker='o', zorder=10, color='steelblue', alpha=0.5,
-                        )
-                        pcaaxis = pca_dest[iz].components_.T
-                        pca_eigenratio = pca_dest[iz].explained_variance_ratio_
-                        plt.title('src_rot')
-                    elif isub == 223:
-                        plt.scatter(coord_dest[iz][:, 0], coord_dest[iz][:, 1], s=5, marker='o', zorder=10, color='red',
-                                    alpha=0.5)
-                        pcaaxis = pca_dest[iz].components_.T
-                        pca_eigenratio = pca_dest[iz].explained_variance_ratio_
-                        plt.title('dest')
-                    elif isub == 224:
-                        plt.scatter(
-                            [coord_dest_rot[i, 0] for i in range(len(coord_dest_rot))],
-                            [coord_dest_rot[i, 1] for i in range(len(coord_dest_rot))],
-                            s=5, marker='o', zorder=10, color='red', alpha=0.5,
-                        )
-                        pcaaxis = pca_src[iz].components_.T
-                        pca_eigenratio = pca_src[iz].explained_variance_ratio_
-                        plt.title('dest_rot')
-                    plt.text(-2.5, -2, 'eigenvectors:', horizontalalignment='left', verticalalignment='bottom')
-                    plt.text(-2.5, -2.8, str(pcaaxis), horizontalalignment='left', verticalalignment='bottom')
-                    plt.text(-2.5, 2.5, 'eigenval_ratio:', horizontalalignment='left', verticalalignment='bottom')
-                    plt.text(-2.5, 2, str(pca_eigenratio), horizontalalignment='left', verticalalignment='bottom')
-                    plt.plot([0, pcaaxis[0, 0]], [0, pcaaxis[1, 0]], linewidth=2, color='red')
-                    plt.plot([0, pcaaxis[0, 1]], [0, pcaaxis[1, 1]], linewidth=2, color='orange')
-                    plt.axis([-3, 3, -3, 3])
-                    plt.gca().set_aspect('equal', adjustable='box')
-                except Exception as e:
-                    raise Exception
+                if isub == 221:
+                    plt.scatter(coord_src[iz][:, 0], coord_src[iz][:, 1], s=5, marker='o', zorder=10, color='steelblue',
+                                alpha=0.5)
+                    pcaaxis = pca_src[iz].components_.T
+                    pca_eigenratio = pca_src[iz].explained_variance_ratio_
+                    plt.title('src')
+                elif isub == 222:
+                    plt.scatter(
+                        [coord_src_rot[i, 0] for i in range(len(coord_src_rot))],
+                        [coord_src_rot[i, 1] for i in range(len(coord_src_rot))],
+                        s=5, marker='o', zorder=10, color='steelblue', alpha=0.5,
+                    )
+                    pcaaxis = pca_dest[iz].components_.T
+                    pca_eigenratio = pca_dest[iz].explained_variance_ratio_
+                    plt.title('src_rot')
+                elif isub == 223:
+                    plt.scatter(coord_dest[iz][:, 0], coord_dest[iz][:, 1], s=5, marker='o', zorder=10, color='red',
+                                alpha=0.5)
+                    pcaaxis = pca_dest[iz].components_.T
+                    pca_eigenratio = pca_dest[iz].explained_variance_ratio_
+                    plt.title('dest')
+                elif isub == 224:
+                    plt.scatter(
+                        [coord_dest_rot[i, 0] for i in range(len(coord_dest_rot))],
+                        [coord_dest_rot[i, 1] for i in range(len(coord_dest_rot))],
+                        s=5, marker='o', zorder=10, color='red', alpha=0.5,
+                    )
+                    pcaaxis = pca_src[iz].components_.T
+                    pca_eigenratio = pca_src[iz].explained_variance_ratio_
+                    plt.title('dest_rot')
+                plt.text(-2.5, -2, 'eigenvectors:', horizontalalignment='left', verticalalignment='bottom')
+                plt.text(-2.5, -2.8, str(pcaaxis), horizontalalignment='left', verticalalignment='bottom')
+                plt.text(-2.5, 2.5, 'eigenval_ratio:', horizontalalignment='left', verticalalignment='bottom')
+                plt.text(-2.5, 2, str(pca_eigenratio), horizontalalignment='left', verticalalignment='bottom')
+                plt.plot([0, pcaaxis[0, 0]], [0, pcaaxis[1, 0]], linewidth=2, color='red')
+                plt.plot([0, pcaaxis[0, 1]], [0, pcaaxis[1, 1]], linewidth=2, color='orange')
+                plt.axis([-3, 3, -3, 3])
+                plt.gca().set_aspect('equal', adjustable='box')
 
             plt.savefig(os.path.join(path_qc, 'register2d_centermassrot_pca_z' + str(iz) + '.png'))
             plt.close()
