120c94bc3f0414e0b4093816babb3079d271e374
valosekj <jan.valosek@upol.cz>
2023-02-17 16:07:39 -0500

    Add unit tests to check if -centerline-soft outputs a file and to check if sum of soft centerline over all slices is equal 1

--- testing/cli/test_cli_sct_get_centerline.py
+++ testing/cli/test_cli_sct_get_centerline.py
@@ -1,8 +1,10 @@
 import logging
 import os
 
+import numpy as np
 import pytest
 
+from spinalcordtoolbox.image import Image
 from spinalcordtoolbox.scripts import sct_get_centerline
 
 logger = logging.getLogger(__name__)
@@ -33,3 +35,30 @@ def test_sct_get_centerline_output_file_exists_with_o_arg(tmp_path, ext):
                                   '-o', os.path.join(tmp_path, 't2s_centerline'+ext)])
     for file in [os.path.join('t2s', 't2s_centerline.nii.gz'), os.path.join('t2s', 't2s_centerline.csv')]:
         assert os.path.exists(file)
+
+
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_get_centerline_soft_output_file_exists(tmp_path):
+    """This test checks the output soft centerline using default usage of the CLI script."""
+    sct_get_centerline.main(argv=['-i', 't2/t2_seg-manual.nii.gz', '-method', 'fitseg', '-centerline-soft', '1', '-o',
+                                  't2_seg_centerline_soft.nii.gz', '-qc', str(tmp_path)])
+    for file in [os.path.join('t2', 't2_seg_centerline_soft.nii.gz'), os.path.join('t2', 't2_seg_centerline_soft.csv')]:
+        assert os.path.exists(file)
+
+
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_get_centerline_soft_output_is_equal_to_one(tmp_path):
+    """This test checks that the output soft centerline is equal to 1 on all slices. using default usage of the CLI
+    script."""
+    # TODO - check if it is necessary to run this command again. Maybe, we can reuse the output of the previous test.
+    sct_get_centerline.main(argv=['-i', 't2/t2_seg-manual.nii.gz', '-method', 'fitseg', '-centerline-soft', '1', '-o',
+                                  't2_seg_centerline_soft.nii.gz', '-qc', str(tmp_path)])
+
+    # Read nii file
+    im = Image(os.path.join('t2', 't2_seg_centerline_soft.nii.gz'))
+    # Sum soft centerline intensities across all slices
+    sum_over_slices = np.apply_over_axes(np.sum, im.data, [0, 1]).flatten()
+    # Test if ndarray contains only 1
+    assert (sum_over_slices == 1).all()
