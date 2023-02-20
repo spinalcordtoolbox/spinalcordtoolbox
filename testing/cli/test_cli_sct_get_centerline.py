0b95971b56d94b71c950a88f8008577495973d19
Nathan Molinier <nathan.molinier@gmail.com>
2023-02-20 17:50:51 -0500

    Adding test to check if the maximum intensity voxel in the soft centerline match the binary voxel in each slice

--- testing/cli/test_cli_sct_get_centerline.py
+++ testing/cli/test_cli_sct_get_centerline.py
@@ -62,3 +62,22 @@ def test_sct_get_centerline_soft_output_is_equal_to_one(tmp_path):
     sum_over_slices = np.apply_over_axes(np.sum, im.data, [0, 2]).flatten()
     # Test if the summation for each slice is equal to 1
     assert (sum_over_slices == 1).all()
+
+@pytest.mark.sct_testing
+@pytest.mark.usefixtures("run_in_sct_testing_data_dir")
+def test_sct_get_centerline_soft_overlaps_with_binary(tmp_path):
+    """This test checks that the output maximum of the soft centerline overlaps with the binary segmentation on all slices."""
+    sct_get_centerline.main(argv=['-i', 't2/t2_seg-manual.nii.gz', '-method', 'fitseg', '-centerline-soft', '0', '-o',
+                                  't2/t2_seg_centerline.nii.gz', '-qc', str(tmp_path)])
+
+    # Read nii file
+    im_soft = Image(os.path.join('t2', 't2_seg_centerline_soft.nii.gz'))  # Read soft centerline file
+    im_bin = Image(os.path.join('t2', 't2_seg_centerline.nii.gz'))  # Read binary centerline file
+    # Find the maximum intensity voxel across all slices in soft centerline and binary centerline
+    max_over_slices_soft = np.apply_over_axes(np.max, im_soft.data, [0, 2])
+    max_over_slices_bin = np.apply_over_axes(np.max,im_bin.data, [0, 2])
+    # Find the coordinates of the maximum in each slice in both soft and binary centerline
+    max_coords_over_slices_soft = np.transpose(np.where(im_soft.data == max_over_slices_soft))
+    max_coords_over_slices_bin = np.transpose(np.where(im_bin.data == max_over_slices_bin))
+    # Test if the maximum are the same between soft and binary centerline for each slices
+    assert (max_coords_over_slices_soft == max_coords_over_slices_bin).all()
