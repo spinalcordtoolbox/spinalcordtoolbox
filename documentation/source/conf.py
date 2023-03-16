2568496613b55b09c6ce6ae8858e96709d46943c
Joshua Newton <joshuacwnewton@gmail.com>
2023-03-16 15:39:44 -0400

    Add `favicon.ico` file
    This is just a cropped version of the `logo_sct_notext.png`
    file that already exists in `_static/img`. It was cropped
    so that the details of the GM/WM segmentations could be
    more easily distinguished at such a low resolution.

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -156,6 +156,13 @@ html_extra_path = ['_extra']
 
 html_css_files = ['css/custom.css', 'css/pygments_dark.css']
 
+# If given, this must be the name of an image file (path relative to the
+# configuration directory) that is the favicon of the docs, or URL that points
+# an image file for the favicon. Modern browsers use this as the icon for tabs,
+# windows and bookmarks. It should be a Windows-style icon file (.ico), which is
+# 16x16 or 32x32 pixels large.
+html_favicon = '_static/img/favicon.ico'
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 # This is required for the alabaster theme
