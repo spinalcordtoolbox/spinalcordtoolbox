27e2d399722a346e9a2e972fc5a879a9f8bacfb7
sandrinebedard <sandrine.bedard@polymtl.ca>
2023-03-21 18:43:39 -0400

    raise value erro if slices from above and below are not available

--- spinalcordtoolbox/scripts/sct_compute_compression.py
+++ spinalcordtoolbox/scripts/sct_compute_compression.py
@@ -432,9 +432,9 @@ def get_slices_upper_lower_level_from_centerline(centerline, distance, extent, z
         zmin_below = zmin_above
         zmax_below = zmax_above
     if zmin_above == zmax_above and zmin_below == zmax_below:
-       raise ValueError("No slices of level above of below with"
-                        "distance" + str(distance) + " mm and extent of" + str(extent)
-                        + " . Please provide other distance and extent.")
+        raise ValueError("No slices of level above of below with"
+                         "distance" + str(distance) + " mm and extent of" + str(extent)
+                         + " . Please provide other distance and extent.")
     slices_above = np.arange(zmin_above, zmax_above, 1)
     slices_below = np.arange(zmin_below, zmax_below, 1)
     print(slices_above, slices_below)
@@ -467,9 +467,15 @@ def get_slices_upper_lower_level_from_PAM50(compression_level_dict_PAM50, df_met
     if zmax_below not in df_metrics_PAM50_short['Slice (I->S)'].to_list():
         zmax_below = zmax_above
         zmin_below = zmin_above
-    if zmin_below not in df_metrics_PAM50_short['Slice (I->S)'].to_list():
+        not_below = True
+    if zmin_above not in df_metrics_PAM50_short['Slice (I->S)'].to_list():
         zmax_above = zmax_below
         zmin_above = zmin_below
+        not_above = True
+    if not_above and not_below:
+        raise ValueError("No slices of level above of below with"
+                         "distance" + str(distance) + " mm and extent of" + str(extent)
+                         + " . Please provide other distance and extent.")
     # Take last available slice if extent is out of range
     if zmin_below not in df_metrics_PAM50_short['Slice (I->S)'].to_list():
         zmin_below = min(df_metrics_PAM50_short['Slice (I->S)'].to_list())
