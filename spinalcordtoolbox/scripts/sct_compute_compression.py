a28b9c1af04386591dca9280992b65bdc005d7ed
sandrinebedard <sandrine.bedard@polymtl.ca>
2023-03-21 18:52:26 -0400

    fix spacing in ValueError

--- spinalcordtoolbox/scripts/sct_compute_compression.py
+++ spinalcordtoolbox/scripts/sct_compute_compression.py
@@ -433,8 +433,8 @@ def get_slices_upper_lower_level_from_centerline(centerline, distance, extent, z
         zmax_below = zmax_above
     if zmin_above == zmax_above and zmin_below == zmax_below:
         raise ValueError("No slices of level above of below with "
-                         "distance " + str(distance) + " mm and extent of" + str(extent)
-                         + " . Please provide other distance and extent.")
+                         "distance " + str(distance) + " mm and extent of " + str(extent)
+                         + " mm. Please provide another distance and extent.")
     slices_above = np.arange(zmin_above, zmax_above, 1)
     slices_below = np.arange(zmin_below, zmax_below, 1)
     print(slices_above, slices_below)
@@ -475,7 +475,7 @@ def get_slices_upper_lower_level_from_PAM50(compression_level_dict_PAM50, df_met
     if not_above and not_below:
         raise ValueError("No slices of level above of below with "
                          "distance " + str(distance) + " mm and extent of" + str(extent)
-                         + " . Please provide other distance and extent.")
+                         + " mm. Please provide another distance and extent.")
     # Take last available slice if extent is out of range
     if zmin_below not in df_metrics_PAM50_short['Slice (I->S)'].to_list():
         zmin_below = min(df_metrics_PAM50_short['Slice (I->S)'].to_list())
