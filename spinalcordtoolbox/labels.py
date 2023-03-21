9a882a5e2ec42aa095b0f3ef1372baf858c2b97e
valosekj <jan.valosek@upol.cz>
2023-03-21 13:53:55 -0400

    Deal with the level above the top disc and the level below the bottom disc

--- spinalcordtoolbox/labels.py
+++ spinalcordtoolbox/labels.py
@@ -238,6 +238,16 @@ def labelize_from_discs(img: Image, ref: Image) -> Image:
             if coordinates_ref[j + 1].z < z <= coordinates_ref[j].z:
                 out.data[int(x), int(y), int(z)] = coordinates_ref[j].value
 
+    # deal with the level above the top disc and the level below the bottom disc
+    for x, y, z, _ in coordinates_input:
+        for j in range(len(coordinates_ref) - 1):
+            # level below the bottom disc
+            if coordinates_ref[0].z > z and out.data[int(x), int(y), int(z)] == 0:
+                out.data[int(x), int(y), int(z)] = coordinates_ref[-1].value
+            # level above the top disc
+            if coordinates_ref[-1].z < z and out.data[int(x), int(y), int(z)] == 0:
+                out.data[int(x), int(y), int(z)] = coordinates_ref[0].value - 1
+
     return out
 
 
