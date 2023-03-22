2225caf01ebea5de46bd461a52bd2d8a97e6fe16
sandrinebedard <sandrine.bedard@polymtl.ca>
2023-03-22 17:57:34 -0400

    remove all dummy csv files

--- testing/cli/test_cli_sct_compute_compression.py
+++ testing/cli/test_cli_sct_compute_compression.py
@@ -1,7 +1,6 @@
 import pytest
 import logging
 import numpy as np
-import pandas as pd
 import tempfile
 import nibabel
 import csv
@@ -22,9 +21,14 @@ def dummy_3d_compression_label():
 
 
 @pytest.fixture(scope="session")
-def dummy_3d_mask_nib():
+def dummy_3d_vert_label():
     data = np.zeros([32, 32, 81], dtype=np.uint8)
-    data[9:24, 9:24, :] = 1
+    data[15, 15, 0:10] = 2
+    data[15, 15, 10:25] = 3
+    data[15, 15, 25:50] = 4
+    data[15, 15, 25:50] = 4
+    data[15, 15, 50:70] = 5
+    data[15, 15, 70:81] = 6
     nii = nibabel.nifti1.Nifti1Image(data, np.eye(4))
     filename = tempfile.NamedTemporaryFile(suffix='.nii.gz', delete=False).name
     nibabel.save(nii, filename)
@@ -32,46 +36,12 @@ def dummy_3d_mask_nib():
 
 
 @pytest.fixture(scope="session")
-def dummy_metrics_csv():
-    vertlevel = np.empty(38)
-    vertlevel.fill(7)
-    vertlevel[0:5] = np.ones(5)*8
-    vertlevel[-7::] = np.ones(7)*6
-    slices = np.arange(44, 82)
-    mean_diameter = np.array([6.6862469918429195, 6.55169728444946, 6.389135256069518, 6.304799342877206, 6.041723469159038, 6.439003691181355,
-                              6.544616501201308, 6.139186306769116, 6.542551093609961, 6.416558726468434, 6.035723621913116, 5.955694595068289,
-                              6.263471044201964, 6.717050230017857, 6.435380314120982, 6.182082168409608, 6.129463897495415, 5.7612477330205305,
-                              6.091412318629499, 6.532062074793497, 6.467776708536095, 7.222792805918961, 7.23214311204923, 6.749575410990546,
-                              6.62637117360869, 6.424124306698773, 6.820276721945256, 6.745489247204905, 6.423584195632916, 6.370832525207956,
-                              6.661876720671968, 6.496059111401223, 6.597853777753301, 6.4723521908617725, 6.534168768380888, 6.125633872831384,
-                              6.182984047618388, 6.6483805095089075])
-    d = {'Slice (I->S)': slices, 'VertLevel': vertlevel, 'MEAN(diameter_AP)': mean_diameter}
-    df = pd.DataFrame(data=d)
-    filename = tempfile.NamedTemporaryFile(suffix='.csv', delete=False).name
-    df.to_csv(filename)
-    return filename
-
-
-@pytest.fixture(scope="session")
-def dummy_metrics_csv_pam50():
-    vertlevel = np.empty(51)
-    vertlevel.fill(7)
-    vertlevel[0:11] = np.ones(11)*8
-    vertlevel[-8::] = np.ones(8)*6
-    slices = np.arange(725, 776)
-    mean_diameter = np.array([6.950051172684454, 6.892386122770274, 6.897441382470692, 6.996577106593007, 7.027228117561998, 6.898081464506564,
-                              6.750612859271622, 6.529856445318642, 6.350915446862625, 6.548313187879266, 6.745710928895907, 6.747585938410225,
-                              6.709534790118919, 6.684351436560332, 6.865052798725242, 7.032391941892341, 7.099514442575864, 7.163451376781643,
-                              7.212522334091279, 7.199131566136183, 6.982740191070702, 6.83016921235479, 6.830767184877847, 6.849166027368458,
-                              6.900199798132916, 6.948018494873731, 6.991244228723624, 7.0693146179652215, 7.1865852445224885, 7.153627949130994,
-                              6.987134723118381, 6.766967836829394, 6.509229388346944, 6.601473000435695, 6.884615918290557, 7.0576068343728595,
-                              7.184701083049932, 7.222166330815239, 6.2320534244538495, 6.211673054951369, 6.184806800490084, 7.155302901524694,
-                              7.125447316625423, 7.05258391898559, 6.977032533049472, 7.350757182137674, 7.3532938946632145, 7.332989188149536,
-                              7.238449869758395, 7.152761369850934, 7.077030040737611])
-    d = {'Slice (I->S)': slices, 'VertLevel': vertlevel, 'MEAN(diameter_AP)': mean_diameter}
-    df = pd.DataFrame(data=d)
-    filename = tempfile.NamedTemporaryFile(suffix='.csv', delete=False).name
-    df.to_csv(filename)
+def dummy_3d_mask_nib():
+    data = np.zeros([32, 32, 81], dtype=np.uint8)
+    data[9:24, 9:24, :] = 1
+    nii = nibabel.nifti1.Nifti1Image(data, np.eye(4))
+    filename = tempfile.NamedTemporaryFile(suffix='.nii.gz', delete=False).name
+    nibabel.save(nii, filename)
     return filename
 
 
@@ -88,63 +58,54 @@ def test_sct_compute_compression_value_against_groundtruth():
     assert mscc == pytest.approx(6.612133606, abs=1e-4)
 
 
-def test_sct_compute_compression_check_missing_input_csv(tmp_path, dummy_3d_label_label, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_check_missing_input_segmentation(tmp_path, dummy_3d_compression_label):
     """ Run sct_compute_compression when missing -i"""
     filename = str(tmp_path / 'tmp_file_out.csv')
     with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label, '-o', filename])
+        sct_compute_compression.main(argv=['-l', dummy_3d_compression_label, '-o', filename])
         assert e.value.code == 2
 
 
-def test_sct_compute_compression_check_missing_input_l(tmp_path, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_check_missing_input_l(tmp_path):
     """ Run sct_compute_mscc when missing -l"""
     filename = str(tmp_path / 'tmp_file_out.csv')
     with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-o', filename])
+        sct_compute_compression.main(argv=['-i',  '-o', filename])
         assert e.value.code == 2
 
 
-def test_sct_compute_compression_check_wrong_sex(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_check_wrong_sex(tmp_path, dummy_3d_compression_label):
     """ Run sct_compute_compression with wrong value for sex"""
     filename = str(tmp_path / 'tmp_file_out.csv')
     with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label,
+        sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label,
                                            '-sex', 'J', '-o', filename])
         assert e.value.code == 2
 
 
-def test_sct_compute_compression_check_wrong_age(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_check_wrong_age(tmp_path, dummy_3d_compression_label):
     """ Run sct_compute_compression with wrong age"""
     filename = str(tmp_path / 'tmp_file_out.csv')
     with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label,
+        sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label,
                                            '-age', '20', '-o', filename])
         assert e.value.code == 2
 
 
-def test_sct_compute_compression_check_wrong_metric(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_check_wrong_metric(tmp_path, dummy_3d_compression_label):
     """ Run sct_compute_compression when specifying a wrong metric"""
     filename = str(tmp_path / 'tmp_file_out.csv')
     with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label,
+        sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label,
                                            '-metric', 'MEAN', '20', '-o', filename])
         assert e.value.code == 2
 
 
-def test_sct_compute_compression_no_normalization_missing_segmentation(tmp_path, dummy_3d_label_label, dummy_metrics_csv):
-    """ Run sct_compute_compression without normalization (-i-PAM50) and missing segmentation (-s)"""
-    filename = str(tmp_path / 'tmp_file_out.csv')
-    with pytest.raises(SystemExit) as e:
-        sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-l', dummy_3d_label_label,
-                                           '-o', filename])
-        assert e.value.code == 2
-
-
-def test_sct_compute_compression_no_normalization(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_3d_mask_nib):
+def test_sct_compute_compression_no_normalization(tmp_path, dummy_3d_compression_label, dummy_3d_mask_nib, dummy_3d_vert_label):
     """ Run sct_compute_compression without normalization (-i-PAM50)"""
     filename = str(tmp_path / 'tmp_file_out.csv')
-    sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-l', dummy_3d_label_label, '-s', dummy_3d_mask_nib,
-                                       '-o', filename])
+    sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label, '-s', dummy_3d_mask_nib,
+                                       '-vertfile', dummy_3d_vert_label, '-o', filename])
     with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         row = next(reader)
@@ -153,11 +114,11 @@ def test_sct_compute_compression_no_normalization(tmp_path, dummy_3d_label_label
         assert row['Normalized diameter_AP ratio'] == ''
 
 
-def test_sct_compute_compression(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression(tmp_path, dummy_3d_compression_label, dummy_3d_vert_label):
     """ Run sct_compute_compression and check mscc and normalized mscc"""
     filename = str(tmp_path / 'tmp_file_out.csv')
-    sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label,
-                                       '-o', filename])
+    sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label,
+                                       '-vertfile', dummy_3d_vert_label, '-o', filename])
     with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         row = next(reader)
@@ -166,11 +127,11 @@ def test_sct_compute_compression(tmp_path, dummy_3d_label_label, dummy_metrics_c
         assert float(row['Normalized diameter_AP ratio']) == pytest.approx(14.030016763655384)
 
 
-def test_sct_compute_compression_sex_F(tmp_path, dummy_3d_label_label, dummy_metrics_csv, dummy_metrics_csv_pam50):
+def test_sct_compute_compression_sex_F(tmp_path, dummy_3d_compression_label, dummy_3d_vert_label):
     """ Run sct_compute_compression and chexk mscc and normalized mscc"""
     filename = str(tmp_path / 'tmp_file_out.csv')
-    sct_compute_compression.main(argv=['-i', dummy_metrics_csv, '-i-PAM50', dummy_metrics_csv_pam50, '-l', dummy_3d_label_label,
-                                       '-sex', 'F', '-o', filename])
+    sct_compute_compression.main(argv=['-i', '-l', dummy_3d_compression_label,
+                                       '-vertfile', dummy_3d_vert_label, '-sex', 'F', '-o', filename])
     with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         row = next(reader)
