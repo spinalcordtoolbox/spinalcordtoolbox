6558c8f7235111367483447644cb2bb0afec5efa
Nathan Molinier <nathan.molinier@gmail.com>
2023-04-03 13:59:49 -0400

    Update parser help
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>

--- spinalcordtoolbox/scripts/sct_label_utils.py
+++ spinalcordtoolbox/scripts/sct_label_utils.py
@@ -134,7 +134,7 @@ def get_parser():
     func_group.add_argument(
         '-project-centerline',
         metavar=Metavar.file,
-        help="Project an image onto the spinal cord centerline"
+        help="Project labels onto the spinal cord centerline"
     )
 
     func_group.add_argument(
