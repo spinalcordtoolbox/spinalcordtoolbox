352f93533bb9730fb6d0de1cab7b875833d614bd
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2023-04-20 18:15:11 -0400

    Align default value for `verbose` with actual usage
    Some of these functions have a default of `verbose=1`, and others
    have `verbose=0`, which is confusing. What's more, these functions
    are always called with a value for `verbose`, so the default is
    never used. The actual source for this parameter is all the way
    up the call chain, in `sct_register_multimodal.py`, where the
    actual default is 1.
    
    I would rather not set a default value at all if it's never used,
    but in this case that would case an error:
    ```
    SyntaxError: non-default argument follows default argument
    ```
    This could probably be fixed in a bigger code cleanup.

--- spinalcordtoolbox/registration/algorithms.py
+++ spinalcordtoolbox/registration/algorithms.py
@@ -549,7 +549,7 @@ def register_dl_inference(fname_model, input_moving, input_fixed, reg_args, devi
 
 def register_slicewise(fname_src, fname_dest, paramreg=None, fname_mask='', warp_forward_out='step0Warp.nii.gz',
                        warp_inverse_out='step0InverseWarp.nii.gz', ants_registration_params=None,
-                       path_qc='.', remove_temp_files=0, verbose=0):
+                       path_qc='.', remove_temp_files=0, verbose=1):
     """
     Main function that calls various methods for slicewise registration.
 
@@ -658,7 +658,7 @@ def register_slicewise(fname_src, fname_dest, paramreg=None, fname_mask='', warp
 
 def register2d_centermassrot(fname_src, fname_dest, paramreg=None, fname_warp='warp_forward.nii.gz',
                              fname_warp_inv='warp_inverse.nii.gz', rot_method='pca', filter_size=0, path_qc='.',
-                             verbose=0, pca_eigenratio_th=1.6, th_max_angle=40):
+                             verbose=1, pca_eigenratio_th=1.6, th_max_angle=40):
     """
     Rotate the source image to match the orientation of the destination image, using the first and second eigenvector
     of the PCA. This function should be used on segmentations (not images).
@@ -926,7 +926,7 @@ def register2d_centermassrot(fname_src, fname_dest, paramreg=None, fname_warp='w
     generate_warping_field(fname_src[0], warp_inv_x, warp_inv_y, fname_warp_inv)
 
 
-def register2d_columnwise(fname_src, fname_dest, fname_warp='warp_forward.nii.gz', fname_warp_inv='warp_inverse.nii.gz', verbose=0, path_qc='.', smoothWarpXY=1):
+def register2d_columnwise(fname_src, fname_dest, fname_warp='warp_forward.nii.gz', fname_warp_inv='warp_inverse.nii.gz', verbose=1, path_qc='.', smoothWarpXY=1):
     """
     Column-wise non-linear registration of segmentations. Based on an idea from Allan Martin.
     - Assumes src/dest are segmentations (not necessarily binary), and already registered by center of mass
@@ -1189,7 +1189,7 @@ def register2d(fname_src, fname_dest, fname_mask='', fname_warp='warp_forward.ni
                ants_registration_params={'rigid': '', 'affine': '', 'compositeaffine': '', 'similarity': '',
                                          'translation': '', 'bspline': ',10', 'gaussiandisplacementfield': ',3,0',
                                          'bsplinedisplacementfield': ',5,10', 'syn': ',3,0', 'bsplinesyn': ',1,3'},
-               verbose=0):
+               verbose=1):
     """
     Slice-by-slice registration of two images.
 
