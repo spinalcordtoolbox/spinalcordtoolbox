20543701c22662c8e7270aefdbc5ba54b064b4db
Joshua Newton <joshuacwnewton@gmail.com>
2023-05-10 08:59:32 -0400

    `cropping.py`: Refactor `ImageCropper.crop` to accept images
    Previously, `ImageCropper` could only be used to crop a single
    image (`self.img_in`) per instance. But, for the Sagittal QC,
    we want to crop many slices using an identical bounding box each
    time.
    
    So, this commit adds a quick workaround to allow `crop()` to
    accept images, overriding the `self.img_in` parameter.
    
    NB: In the long run, it would probably be more straightforward
    if there wasn't a `self.img_in` attribute in the first place.
    That way, `ImageCropper` instances would be less limited in their
    ability to crop multiple images. But, doing this would require a
    much larger refactor, and I wanted to preserve backwards
    compatability the best I could within the scope of this PR.

--- spinalcordtoolbox/cropping.py
+++ spinalcordtoolbox/cropping.py
@@ -21,7 +21,7 @@ class BoundingBox(object):
 
 
 class ImageCropper(object):
-    def __init__(self, img_in):
+    def __init__(self, img_in=None):
         """
         :param img_in:
         """
@@ -29,16 +29,24 @@ class ImageCropper(object):
         # Call one of the `get_bbox_from_*` methods to set the bounding box.
         self.bbox = None
 
-    def crop(self, background=None, dilate=None):
+    def crop(self, img_in=None, background=None, dilate=None):
         """
         Crop image (change dimension)
 
+        :param img_in: Image: Image to be cropped. If not provided, `self.img_in` will be cropped instead.
         :param background: int: If set, the output image will not be cropped. Instead, voxels outside the bounding
         box will be set to the value specified by this parameter.
         :param dilate: If set, a list of 3 integers specifying an extra margin to keep around the bounding box,
         in each of the x-, y-, and z-directions.
         :return Image: img_out
         """
+        if not img_in:
+            if self.img_in:
+                img_in = self.img_in
+            else:
+                raise ValueError('No image to crop. Please provide `img_in` when initializing the cropper, or when '
+                                 'calling the `crop` method.')
+
         bbox = self.bbox
         if bbox is None:
             raise ValueError(
@@ -47,9 +55,9 @@ class ImageCropper(object):
 
         if dilate is not None:
             bbox = BoundingBox(
-                xmin=max(bbox.xmin-dilate[0], 0), xmax=min(bbox.xmax+dilate[0], self.img_in.dim[0]-1),
-                ymin=max(bbox.ymin-dilate[1], 0), ymax=min(bbox.ymax+dilate[1], self.img_in.dim[1]-1),
-                zmin=max(bbox.zmin-dilate[2], 0), zmax=min(bbox.zmax+dilate[2], self.img_in.dim[2]-1),
+                xmin=max(bbox.xmin-dilate[0], 0), xmax=min(bbox.xmax+dilate[0], img_in.dim[0]-1),
+                ymin=max(bbox.ymin-dilate[1], 0), ymax=min(bbox.ymax+dilate[1], img_in.dim[1]-1),
+                zmin=max(bbox.zmin-dilate[2], 0), zmax=min(bbox.zmax+dilate[2], img_in.dim[2]-1),
             )
 
         logger.info("Bounding box: x=[{}, {}], y=[{}, {}], z=[{}, {}]"
@@ -58,8 +66,8 @@ class ImageCropper(object):
         # Crop the image
         if background is None:
             logger.info("Cropping the image...")
-            data_crop = self.img_in.data[bbox.xmin:bbox.xmax+1, bbox.ymin:bbox.ymax+1, bbox.zmin:bbox.zmax+1]
-            img_out = Image(param=data_crop, hdr=self.img_in.hdr)
+            data_crop = img_in.data[bbox.xmin:bbox.xmax+1, bbox.ymin:bbox.ymax+1, bbox.zmin:bbox.zmax+1]
+            img_out = Image(param=data_crop, hdr=img_in.hdr)
             # adapt the origin in the qform matrix
             new_origin = np.dot(img_out.hdr.get_qform(), [bbox.xmin, bbox.ymin, bbox.zmin, 1])
             img_out.hdr.structarr['qoffset_x'] = new_origin[0]
@@ -74,10 +82,10 @@ class ImageCropper(object):
         # Set voxels outside the bbox to the value 'background'
         else:
             logger.info("Setting voxels outside the bounding box to: {}".format(background))
-            img_out = self.img_in.copy()
+            img_out = img_in.copy()
             img_out.data[:] = background
             img_out.data[bbox.xmin:bbox.xmax+1, bbox.ymin:bbox.ymax+1, bbox.zmin:bbox.zmax+1] = \
-                self.img_in.data[bbox.xmin:bbox.xmax+1, bbox.ymin:bbox.ymax+1, bbox.zmin:bbox.zmax+1]
+                img_in.data[bbox.xmin:bbox.xmax+1, bbox.ymin:bbox.ymax+1, bbox.zmin:bbox.zmax+1]
 
         return img_out
 
