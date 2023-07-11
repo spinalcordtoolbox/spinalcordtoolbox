4ab2471931e01c66530471171f255190050e919a
Joshua Newton <joshuacwnewton@gmail.com>
2023-07-11 10:45:16 -0400

    `batch_processing.sh`: Add `$SCT_DIR` to python invocation

--- batch_processing.sh
+++ batch_processing.sh
@@ -267,8 +267,8 @@ echo "Ran on:          $(uname -nsr)"
 echo "Duration:        $((runtime / 3600))hrs $(((runtime / 60) % 60))min $((runtime % 60))sec"
 echo "---"
 # The file `test_batch_processing.py` will output tested values when run as a script
-./python/envs/venv_sct/bin/python testing/batch_processing/test_batch_processing.py ||
-./python/envs/venv_sct/python.exe testing/batch_processing/test_batch_processing.py
+"$SCT_DIR"/python/envs/venv_sct/bin/python "$SCT_DIR"/testing/batch_processing/test_batch_processing.py ||
+"$SCT_DIR"/python/envs/venv_sct/python.exe "$SCT_DIR"/testing/batch_processing/test_batch_processing.py
 echo "~~~"
 
 # Display syntax to open QC report on web browser
