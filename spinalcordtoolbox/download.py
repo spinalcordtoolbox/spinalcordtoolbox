1b6ebefd7fe6c59a4d9a984771aa890e12497b94
co-decode <codecode60@protonmail.com>
2023-10-17 11:20:42 +1100

    Color dataset based on existence of default location

--- spinalcordtoolbox/download.py
+++ spinalcordtoolbox/download.py
@@ -17,6 +17,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
+from spinalcordtoolbox.utils import stylize
 from spinalcordtoolbox.utils.fs import tmp_create
 from spinalcordtoolbox.utils.sys import sct_progress_bar, __sct_dir__, __bin_dir__
 
@@ -375,13 +376,23 @@ def install_named_dataset(dataset_name, dest_folder=None, keep=False):
 
 def list_datasets():
     """
-    :returns: A table containing listing the downloadable datasets
+    :returns: A table listing the downloadable datasets
     :rtype: str
     """
+    color = {True: 'LightGreen', False: 'LightRed'}
     table = f"{'DATASET NAME':<30s}{'TYPE':<20s}\n"
     table += f"{'-' * 50}\n"
-    sortedDatasets = sorted(DATASET_DICT, key=lambda k: DATASET_DICT[k]['download_type'])
+    sortedDatasets = sorted(DATASET_DICT,
+                            key=lambda k: DATASET_DICT[k]['download_type'])
     for dataset_name in sortedDatasets:
+        path_dataset = DATASET_DICT[dataset_name]['default_location']
+        installed = os.path.exists(path_dataset)
+        dataset_status = stylize(dataset_name.ljust(30), color[installed])
         download_type = DATASET_DICT[dataset_name]['download_type']
-        table += f"{dataset_name:<30s}{download_type:<20s}\n"
+        table += f"{dataset_status}{download_type:<20s}\n"
+
+    table += '\nLegend: {} | {}\n\n'.format(
+            stylize("installed", color[True]),
+            stylize("not installed", color[False]))
+
     return table
