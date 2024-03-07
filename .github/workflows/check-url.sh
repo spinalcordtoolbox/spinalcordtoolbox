2a53fbdfca26b990ac8224db81e0c43b1846ab90
Joshua Newton <joshuacwnewton@gmail.com>
2024-03-07 12:11:12 -0500

    Move error checking to a separate step

--- .github/workflows/check-url.sh
+++ .github/workflows/check-url.sh
@@ -15,16 +15,16 @@ fi
 status_code=$(curl --write-out '%{http_code}' --silent  --output /dev/null "$URL")
 
 if [[ $status_code -ge 200 && $status_code -le 299 ]];then
-    # echo -e "$filename: \x1B[32m✅ OK status code: $status_code for domain $URL  \x1B[0m"
+    echo "($status_code) $URL ($filename)" >> valid_urls.txt
+    echo -e "$filename: \x1B[32m✅ OK status code: $status_code for domain $URL  \x1B[0m"
     exit 0
 fi
 
 if [[ $status_code -ge 300 && $status_code -le 399 ]];then
+    echo "($status_code) $URL ($filename)" >> redirected_urls.txt
     echo -e "$filename: \x1B[33m⚠️ Warning - Redirection - code: $status_code for URL: $URL \x1B[0m"
-    exit 1
+    exit 0
 fi
 
 echo "($status_code) $URL ($filename)" >> invalid_urls.txt
 echo -e "$filename: \x1B[31m⛔ Error status code: $status_code for URL: $URL \x1B[0m"
-exit 2
-
