4cc0ef9e1eb4ac2d061743b7c3d5d33eb01ee04f
Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>
2024-03-13 14:21:22 -0400

    Add output documentation to `sct_dmri_moco`

--- spinalcordtoolbox/scripts/sct_dmri_moco.py
+++ spinalcordtoolbox/scripts/sct_dmri_moco.py
@@ -46,6 +46,15 @@ def get_parser():
                     "method, type: isct_antsSliceRegularizedRegistration\n"
                     "  - masking (-m)\n"
                     "  - iterative averaging of target volume\n"
+                    "\n"
+                    "The outputs of the motion correction process are:\n"
+                    "  - the motion-corrected dMRI volumes\n"
+                    "  - the time average of the corrected dMRI volumes with b == 0\n"
+                    "  - the time average of the corrected dMRI volumes with b != 0\n"
+                    "  - a time-series with 1 voxel in the XY plane, for the X and Y motion direction (two separate "
+                    "files), as required for FSL analysis.\n"
+                    "  - a TSV file with one row for each time point, with the slice-wise average of the "
+                    "motion correction magnitude for that time point, that can be used for Quality Control.\n"
     )
 
     mandatory = parser.add_argument_group("\nMANDATORY ARGUMENTS")
