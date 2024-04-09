6e092d660d687b278371b38d2beb73d7edfd59bf
Joshua Newton <joshuacwnewton@gmail.com>
2024-04-09 16:27:20 -0400

    Revert "`conftest.py`: Move `checksum` function into `utils` for reuse"
    This reverts commit c51685e1737cc5e408e714a635090f1bd53fbe46.

--- spinalcordtoolbox/utils/fs.py
+++ spinalcordtoolbox/utils/fs.py
@@ -13,7 +13,6 @@ import tempfile
 import datetime
 import logging
 from pathlib import Path
-from hashlib import md5
 
 from .sys import printv
 
@@ -260,9 +259,3 @@ def relpath_or_abspath(child_path, parent_path):
         return abspath.relative_to(parent_path)
     except ValueError:
         return abspath
-
-
-def checksum(fname: os.PathLike) -> str:
-    with open(fname, 'rb') as f:
-        data = f.read()
-    return md5(data).hexdigest()
