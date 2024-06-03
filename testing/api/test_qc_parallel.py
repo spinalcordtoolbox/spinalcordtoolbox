7b3cfe684d11239c3ddb8cdbc68748a53e240ba0
Joshua Newton <joshuacwnewton@gmail.com>
2024-06-03 14:43:38 -0400

    `test_qc_parallel.py`: Add debug logging for portalocker

--- testing/api/test_qc_parallel.py
+++ testing/api/test_qc_parallel.py
@@ -2,6 +2,8 @@
 
 from tempfile import TemporaryDirectory
 import pytest
+import logging
+import sys
 
 import multiprocessing
 
@@ -17,6 +19,13 @@ def gen_qc(path_qc):
 
 def test_many_qc():
     """Test many qc images can be made in parallel"""
+    # Turn on debug logging for fs.py to check mutex acquire/release times
+    # To check this output locally, run: `sct_testing -o log_cli=true testing/api/test_qc_parallel.py::test_many_qc`
+    screen_handler = logging.StreamHandler(stream=sys.stderr)
+    logger = logging.getLogger("portalocker.utils")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(screen_handler)
+
     if multiprocessing.cpu_count() < 2:
         pytest.skip("Can't test parallel behaviour")
 
