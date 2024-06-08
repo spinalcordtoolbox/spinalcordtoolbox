73000cca5f9a8b09d1bb1fc2fc500bb0cc836a27
valosekj <jan.valosek@upol.cz>
2024-06-08 10:43:56 -0400

    improve comment

--- spinalcordtoolbox/scripts/sct_analyze_lesion.py
+++ spinalcordtoolbox/scripts/sct_analyze_lesion.py
@@ -601,8 +601,9 @@ class AnalyzeLesion:
             printv('\nMeasures on lesion #' + str(lesion_label) + '...', self.verbose, 'normal')
 
             label_idx = self.measure_pd[self.measure_pd.label == lesion_label].index
-            # The SC segmentation is necessary for angle correction when computing length and diameter
-            # We also need SC segmentation to compute axial damage ratio
+            # For the lesion length and diameter, we need the spinal cord segmentation for angle correction
+            # For the axial damage ratio, we need the spinal cord segmentation to compute the ratio between lesion area
+            # and spinal cord area
             if self.fname_sc is not None:
                 self._measure_length(im_lesion_data_cur, p_lst, label_idx)
                 self._measure_diameter(im_lesion_data_cur, p_lst, label_idx)
