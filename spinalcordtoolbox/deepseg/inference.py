f97ef08595f4d82086906b5487996e239a44757a
Naga Karthik <emvnagakarthik@gmail.com>
2024-07-24 08:15:19 -0400

    add padding arg to prepare_data function

--- spinalcordtoolbox/deepseg/inference.py
+++ spinalcordtoolbox/deepseg/inference.py
@@ -170,7 +170,7 @@ def segment_monai(path_img, tmpdir, predictor, device: torch.device):
     pad_mode = "edge"
 
     # define the dataset and dataloader
-    test_loader, test_post_pred = ds_monai.prepare_data(path_img_tmp, crop_size=crop_size)
+    test_loader, test_post_pred = ds_monai.prepare_data(path_img_tmp, crop_size=crop_size, padding=pad_mode)
     [batch] = test_loader  # we expected there to only be one batch (with one image)
 
     # Run MONAI prediction
