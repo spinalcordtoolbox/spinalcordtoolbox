3488334427081a736016d422a659526109594888
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2024-08-01 17:00:11 -0400

    More efficient outline algorithm

--- spinalcordtoolbox/reports/qc2.py
+++ spinalcordtoolbox/reports/qc2.py
@@ -448,7 +448,8 @@ def sct_deepseg_spinal_rootlets_t2w(
                   alpha=1.0,
                   interpolation='none',
                   aspect=1.0)
-        plot_outlines(img.T, ax=ax, lw=0.3, color='#000000')  # 0.5 is too thick, 0.25 is too thin
+        # linewidth 0.5 is too thick, 0.25 is too thin
+        plot_outlines(img, ax=ax, facecolor='none', edgecolor='black', linewidth=0.3)
         add_segmentation_labels(ax, img, colors=colormaps[i].colors, radius=tuple(r*scale for r in radius))
 
     ax.get_xaxis().set_visible(False)
@@ -580,69 +581,63 @@ def equalize_histogram(img: np.ndarray):
     return np.array(c * (max_ - min_) + min_, dtype=img.dtype)
 
 
-def plot_outlines(bool_img, ax, **kwargs):
+def plot_outlines(bool_img: np.ndarray, ax: mpl_axes.Axes, **kwargs):
     """
-    Draw the outlines of a binary Numpy array with Matplotlib.
+    Draw the outlines of a 2D binary Numpy array with Matplotlib.
 
-    Source: https://stackoverflow.com/questions/60095053/draw-the-borders-of-a-binary-numpy-array-with-matplotlib
+    kwargs are forwarded to `matplotlib.collections.PolyCollection` for styling.
     """
-    def close_loop_edges(edges):
-        """
-        Combine the edges defined by 'get_all_edges' to closed loops around objects.
-        If there are multiple disconnected objects a list of closed loops is returned.
-        Note that it's expected that all the edges are part of exactly one loop (but not necessarily the same one).
-        """
+    # To see where each (row, column) index shows up in the graphic
+    # (left, right, top, bottom), see:
+    # https://matplotlib.org/stable/users/explain/artists/imshow_extent.html#default-extent
 
-        loop_list = []
-        while edges.size != 0:
-            loop = [edges[0, 0], edges[0, 1]]  # Start with first edge
-            edges = np.delete(edges, 0, axis=0)
-            while edges.size != 0:
-                # Get next edge (=edge with common node)
-                ij = np.nonzero((edges == loop[-1]).all(axis=2))
-                if ij[0].size > 0:
-                    i = ij[0][0]
-                    j = ij[1][0]
-                else:
-                    loop.append(loop[0])
-                    # Uncomment to make the start of the loop invisible when plotting
-                    # loop.append(loop[1])
-                    break
-                loop.append(edges[i, (j + 1) % 2, :])
-                edges = np.delete(edges, i, axis=0)
-            loop_list.append(np.array(loop))
+    # Add a frame of zeros around the image to account for cells on the border
+    padded = np.pad(bool_img, 1)
 
-        return loop_list
+    # horizontal_edge[r, c] is True when there should be a visible horizontal
+    # outline between bool_img[r, c+1] and bool_img[r+1, c+1]
+    horizontal_edge = (padded[:-1, :] != padded[1:, :])
 
-    def get_all_edges(bool_img):
-        """
-        Get a list of all edges (where the value changes from True to False) in the 2D boolean image.
-        The returned array edges has the dimensions (n, 2, 2).
-        Edge i connects the pixels edges[i, 0, :] and edges[i, 1, :].
-        Note that the indices of a pixel also denote the coordinates of its lower left corner.
-        """
-        edges = []
-        ii, jj = np.nonzero(bool_img)
-        for i, j in zip(ii, jj):
-            if j == bool_img.shape[1]-1 or not bool_img[i, j+1]:  # North
-                edges.append(np.array([[i, j+1], [i+1, j+1]]))
-            if i == bool_img.shape[0]-1 or not bool_img[i+1, j]:  # East
-                edges.append(np.array([[i+1, j], [i+1, j+1]]))
-            if j == 0 or not bool_img[i, j-1]:  # South
-                edges.append(np.array([[i, j], [i+1, j]]))
-            if i == 0 or not bool_img[i-1, j]:  # West
-                edges.append(np.array([[i, j], [i, j+1]]))
+    # corner[r, c] is True when a run of horizontal edges starts or stops at
+    # the upper left corner of bool_img[r, c]
+    corner = (horizontal_edge[:, :-1] != horizontal_edge[:, 1:])
 
-        if not edges:
-            return np.zeros((0, 2, 2))
-        else:
-            return np.array(edges)
+    # List the corners in order: first by row (from top to bottom), then by
+    # column within each row (from left to right). These will be all the
+    # polygon vertices we need, and the two endpoints of each run of horizontal
+    # edges appearing side by side in the list
+    vertices = sorted(map(tuple, np.argwhere(corner)))
 
-    edges = get_all_edges(bool_img=bool_img)
-    edges = edges - 0.5  # convert indices to coordinates; TODO adjust according to image extent
-    outlines = close_loop_edges(edges=edges)
-    cl = mpl_collections.LineCollection(outlines, **kwargs)
-    ax.add_collection(cl)
+    # Given a vertex, we want to quickly travel to its horizontal neighbour
+    # i^1 == i+1 when i is even (0->1, 2->3, ...)
+    # i^1 == i-1 when i is odd (1->0, 3->2, ...)
+    horizontal_move = {v: vertices[i ^ 1] for i, v in enumerate(vertices)}
+
+    # We also want to quickly travel to vertical neighbours. We can do the same
+    # thing as above, with a different sorting order: by column, then by row
+    vertices.sort(key=lambda v: v[::-1])
+    vertical_move = {v: vertices[i ^ 1] for i, v in enumerate(vertices)}
+
+    # Now build each polygon by listing its vertices in order. We remove the
+    # vertices from horizontal_move as we visit them
+    polys = []
+    while horizontal_move:
+        polygon = []
+        # Start at an arbitrary unvisited vertex
+        v1 = next(iter(horizontal_move))
+        while v1 in horizontal_move:
+            # Keep alternating horizontal and vertical moves, until we revisit
+            # the polygon's first vertex
+            polygon.append(v1)
+            v2 = horizontal_move.pop(v1)  # remove one endpoint
+            polygon.append(v2)
+            del horizontal_move[v2]  # remove the other endpoint
+            v1 = vertical_move[v2]
+        # Convert (row, column) coordinates to (x, y)
+        polys.append(np.array(polygon)[:, ::-1] - 0.5)
+
+    # Draw it
+    ax.add_collection(mpl_collections.PolyCollection(polys, **kwargs))
 
 
 def assign_label_colors_by_groups(labels):
