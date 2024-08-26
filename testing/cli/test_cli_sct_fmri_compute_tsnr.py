b5716e1031cdb19719ecd7d3b80539fa6b3a2c59
Joshua Newton <joshuacwnewton@gmail.com>
2024-08-26 12:34:42 -0400

    `test_tsnr`: Add test for `-m` and `-qc`
    For some reason, only the last person to edit the workflow file will be pinged if a workflow
    fails.

--- testing/cli/test_cli_sct_fmri_compute_tsnr.py
+++ testing/cli/test_cli_sct_fmri_compute_tsnr.py
@@ -3,7 +3,7 @@
 import pytest
 import logging
 
-from spinalcordtoolbox.scripts import sct_fmri_compute_tsnr
+from spinalcordtoolbox.scripts import sct_fmri_compute_tsnr, sct_maths, sct_deepseg_sc
 from spinalcordtoolbox.utils.sys import sct_test_path
 
 logger = logging.getLogger(__name__)
@@ -15,3 +15,23 @@ def test_sct_sct_fmri_compute_tsnr_no_checks():
     TODO: Check the results. (This test replaces the 'sct_testing' test, which did not implement any checks.)"""
     sct_fmri_compute_tsnr.main(argv=['-i', sct_test_path('fmri', 'fmri.nii.gz'),
                                      '-o', 'out_fmri_tsnr.nii.gz'])
+
+
+@pytest.fixture()
+def img_mask(tmp_path):
+    """Create a binary mask to highlight the spinal cord."""
+    # create mean image of all temporal volumes
+    fname_in = sct_test_path('fmri', 'fmri.nii.gz')
+    fname_mean = str(tmp_path / "fmri_mean.nii.gz")
+    sct_maths.main(argv=["-i", fname_in, "-mean", "t", "-o", fname_mean])
+    # segment the spinal cord
+    # nb: normally we would segment another contrast then bring it to the fmri space, but direct seg is "good enough"
+    fname_out = str(tmp_path / "fmri_seg.nii.gz")
+    sct_deepseg_sc.main(argv=["-i", fname_mean, "-c", "t2s", "-o", fname_out])  # t2s is closest (grey sc, white CSF)
+    return fname_out
+
+
+def test_sct_sct_fmri_compute_tsnr_with_mask(tmp_path, img_mask):
+    """Run the CLI script using a mask image (to test the QC)."""
+    sct_fmri_compute_tsnr.main(argv=['-i', sct_test_path('fmri', 'fmri.nii.gz'), '-m', img_mask,
+                                     '-o', 'out_fmri_tsnr.nii.gz', '-qc', str(tmp_path)])
