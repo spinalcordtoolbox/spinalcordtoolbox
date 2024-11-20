62cb172a2da37d940229db26b6aac362dba7cea6
Joshua Newton <joshuacwnewton@gmail.com>
2024-11-20 10:21:29 -0500

    Use faster dilation algorithm when dilating "mostly zero" point label images (`-x label`) (#4668)
    ## Description
    
    Since 99.99% of voxels are 0 when `islabel=True`, we don't *need* to run
    dilation on every single voxel, which is what skimage will do by
    default. Instead, we can fetch the nonzero voxels, extract an area the
    size of the footprint, run dilation on that area, and then put it back.
    
    I was concerned that this might behave differently in cases where we
    have two labels very close together. But, I think it ends up working
    identically, with the larger label dominating the smaller label (thanks
    to the use of `maximum` internally in skimage).
    
    ## Testing
    
    On my (fast) laptop, dilation time for a typical image went from 7s to
    0.15s to <0.01s, thanks to minimizing the area we call
    `skimage.dilation` on, and ultimately removing `skimage.dilation`
    entirely.
    
    ## Linked issues
    
    Fixes #4663.
    
    ---------
    
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>

--- spinalcordtoolbox/scripts/sct_apply_transfo.py
+++ spinalcordtoolbox/scripts/sct_apply_transfo.py
@@ -232,11 +232,11 @@ class Transform:
                 dim = '3'
             # if labels, dilate before resampling
             if islabel:
-                printv("\nDilate labels before warping...")
+                printv("\nDilate labels before warping...", verbose)
                 path_tmp = tmp_create(basename="apply-transfo-3d-label")
                 fname_dilated_labels = os.path.join(path_tmp, "dilated_data.nii")
                 # dilate points
-                dilate(Image(fname_src), 3, 'ball').save(fname_dilated_labels)
+                dilate(Image(fname_src), 3, 'ball', islabel=True).save(fname_dilated_labels)
                 fname_src = fname_dilated_labels
 
             printv("\nApply transformation and resample to destination space...", verbose)
@@ -323,7 +323,7 @@ class Transform:
         im_src_reg.save(verbose=0)  # set verbose=0 to avoid warning message about rewriting file
 
         if islabel:
-            printv("\nTake the center of mass of each registered dilated labels...")
+            printv("\nTake the center of mass of each registered dilated labels...", verbose)
             labeled_img = cubic_to_point(im_src_reg)
             labeled_img.save(path=fname_out)
             if remove_temp_files:
