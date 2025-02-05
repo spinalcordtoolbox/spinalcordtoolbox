28bb2955ae2402969f10db9b8eee4990763c8cdd
Joshua Newton <joshuacwnewton@gmail.com>
2025-02-05 16:38:58 -0500

    `sys.py`: Comment out PATH changes as sanity check

--- spinalcordtoolbox/utils/sys.py
+++ spinalcordtoolbox/utils/sys.py
@@ -316,7 +316,9 @@ def run_proc(cmd, verbose=1, raise_exception=True, cwd=None, env=None, is_sct_bi
         # also, for windows, add the bin directory to the path (to allow ANTs to access 'msvc-runtime' DLLs)
         # see also: https://github.com/spinalcordtoolbox/spinalcordtoolbox/issues/4655#issuecomment-2430178901
         if sys.platform.startswith("win32"):
-            env['PATH'] = env['PATH'] + os.pathsep + __bin_dir__
+            # sanity check to make sure this is really the issue
+            pass
+            # env['PATH'] = env['PATH'] + os.pathsep + __bin_dir__
 
     if isinstance(cmd, str):
         cmdline = cmd
