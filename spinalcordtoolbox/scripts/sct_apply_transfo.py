b38156f90f3dd85dd94edaba84dfe639d7f7418f
kalum.ost <kalum.ost@polymtl.ca>
2025-03-19 14:44:04 -0400

    Replace the '_' with a space in `sct_apply_transfo`; no idea why I did that in the first place, oops

--- spinalcordtoolbox/scripts/sct_apply_transfo.py
+++ spinalcordtoolbox/scripts/sct_apply_transfo.py
@@ -54,7 +54,7 @@ def get_parser():
         metavar=Metavar.file,
     )
 
-    optional = parser.add_argument_group("OPTIONAL_ARGUMENTS")
+    optional = parser.add_argument_group("OPTIONAL ARGUMENTS")
     optional.add_argument(
         "-winv",
         help='Affine transformation(s) listed in flag -w which should be inverted before being used. Note that this '
