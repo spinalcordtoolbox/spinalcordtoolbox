5c53e62fff5b18a2116396a086a68f7b67ac5925
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-04-07 17:25:37 -0400

    update to through error if csv files are not found

--- spinalcordtoolbox/scripts/sct_compute_compression.py
+++ spinalcordtoolbox/scripts/sct_compute_compression.py
@@ -552,8 +552,12 @@ def main(argv: Sequence[str]):
                          f" and segmentation [{img_seg.data.shape}]). "
                          f"Please verify that your compression labels and vertebral labels were done in the same space as your input segmentation.")
     path_ref = os.path.join(__data_dir__, 'PAM50_normalized_metrics')
-    path_ref_spinal_cord = os.path.join(path_ref, 'spinal_cord')
-
+    # Fetch the subfolder that contains the "sub-{site}_{contrast}_PAM50.csv" files
+    path_ref_hc = next((folder for (folder, _, filenames) in os.walk(path_ref)
+                        if any((f.startswith('sub-') and f.endswith('.csv')) for f in filenames)), None)
+    if arguments.normalize_hc and not path_ref_hc:
+        raise FileNotFoundError(f"Directory with normalized PAM50 metrics {path_ref} does not contain any CSV files.\n"
+                                f"You can try re-downloading it using 'sct_download_data -d PAM50_normalized_metrics'.")
     # Check if path_ref with normalized metrics exists
     if arguments.normalize_hc and not os.path.isdir(path_ref):
         raise FileNotFoundError(f"Directory with normalized PAM50 metrics {path_ref} does not exist.\n"
@@ -616,7 +620,7 @@ def main(argv: Sequence[str]):
         # Fetch metrics of PAM50 template
         df_metrics_PAM50 = pd.read_csv(fname_metrics_PAM50).astype({metric: float})
         # Average metrics of healthy controls
-        df_avg_HC = average_hc(path_ref_spinal_cord, metric, list_HC)
+        df_avg_HC = average_hc(path_ref_hc, metric, list_HC)
         # Get slices correspondence in PAM50 space
         compressed_levels_dict_PAM50 = get_slices_in_PAM50(compressed_levels_dict, df_metrics, df_metrics_PAM50)
         z_range_PAM50_below, z_range_PAM50_above = get_slices_upper_lower_level_from_PAM50(compressed_levels_dict_PAM50, df_metrics_PAM50, distance, extent, slice_thickness_PAM50)
