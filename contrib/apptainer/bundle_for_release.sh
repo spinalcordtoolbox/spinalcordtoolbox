4474c2b7aa657ab341eebea9e817dc59e169894e
kalum.ost <kalum.ost@ucalgary.ca>
2025-05-26 10:42:32 -0400

    Code quality

--- contrib/apptainer/bundle_for_release.sh
+++ contrib/apptainer/bundle_for_release.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Bundle's the apptainer scripts for release
 
 SCT_VERSION="$1"
@@ -9,11 +10,11 @@ fi
 
 # Change the version of SCT the installation task to be the requested version
 sed_str='s|sct_version="X.Y"|sct_version='"$SCT_VERSION"'|g'
-sed -e $sed_str "sct.def" > "sct_$SCT_VERSION.def"
+sed -e "$sed_str" "sct.def" > "sct_$SCT_VERSION.def"
 
 # Change the "install_sct_container.sh" script to reference this new definition
 sed_str='s|sct.def|sct_'"$SCT_VERSION"'.def|g'
-sed -e $sed_str "install_sct_containered.sh" > "install_sct_containered_$SCT_VERSION.sh"
+sed -e "$sed_str" "install_sct_containered.sh" > "install_sct_containered_$SCT_VERSION.sh"
 
 # Mark it as an executable file, if (for some reason) it doesn't inherit that fact
 chmod +x "install_sct_containered_$SCT_VERSION.sh"
