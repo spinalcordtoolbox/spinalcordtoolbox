d20f951b193c14b61e9dc44078966a597c6b0648
Joshua Newton <joshuacwnewton@gmail.com>
2025-05-29 13:04:17 -0400

    `check-url.sh`: Reinstate a partial blacklist + clarify 403 domains
    Now that I've run my test, we know which domains belong to which categories (majority were 403!)

--- .github/workflows/check-url.sh
+++ .github/workflows/check-url.sh
@@ -17,11 +17,13 @@ HTTP_CODE_ONLY=(--write-out '%{http_code}' --output /dev/null)
 RETRY_ARGS=(--retry 2 --retry-delay 30 --retry-max-time 300 --retry-all-errors)
 
 # Make sure to check both URL *and* redirections (--location) for excluded patterns
-# FIXME: Exclusion list temporarily disabled to test how many exclusions were due entirely to 403.
-#        See: 'twitter.com'|'spiedigitallibrary.org'|'pipeline-hemis'|'sciencedirect.com'|'wiley.com'|'sagepub.com'|'ncbi.nlm.nih.gov'|'oxfordjournals.org'|'docker.com'|'ieeexplore.ieee.org'|'liebertpub.com'|'tandfonline.com'|'pnas.org'|'neurology.org'|'academic.oup.com'|'science.org'|'pubs.rsna.org'|'direct.mit.edu'|'thejns.org'|'rosped.ru'|'ajnr.org'|'bmj.com'|'biorxiv.org'|
 full_info=$(curl "${CURL_ARGS[@]}" --location -- "$URL")
 LOCATION=$(curl "${CURL_ARGS[@]}" -- "$URL" | perl -n -e '/^[Ll]ocation: (.*)$/ && print "$1\n"')
-if [[ "$full_info + $URL" =~ '%s' ]]; then
+# `pipeline-hemis` -> private repository, will 404 (expected)
+# `.ru` -> Russian domains, which don't play nicely with curl'ing from GitHub's servers
+# `ieeexplore.ieee.org` -> oddly returns a "418 - I'm a teapot" error code instead of 403
+# `%s` -> placeholder for a URL, which is used in our documentation's `conf.py` file
+if [[ "$full_info + $URL" =~ 'pipeline-hemis'|'.ru'|'ieeexplore.ieee.org'|'%s' ]]; then
     echo -e "$filename: \x1B[33m⚠️  Warning - Skipping: $URL --> $LOCATION\x1B[0m"
     exit 0
 fi
@@ -50,6 +52,11 @@ fi
 # - Often there is no way to bypass this, so in the past we hardcoded the domains to an exclusion list.
 #   However, manually editing the blacklist is not a sustainable solution, so we now automatically filter
 #   out any sites that raise 403 with just a warning instead of an error.
+# - Note: Allowing 403 responses covers the following domains:
+#         'twitter.com', 'spiedigitallibrary.org', 'sciencedirect.com', 'wiley.com', 'sagepub.com',
+#         'ncbi.nlm.nih.gov', 'oxfordjournals.org', 'docker.com', 'liebertpub.com', 'tandfonline.com',
+#         'pnas.org', 'neurology.org', 'academic.oup.com', 'science.org', 'pubs.rsna.org', 'direct.mit.edu',
+#         'thejns.org', 'ajnr.org', 'bmj.com', and 'biorxiv.org'
 if [[ $status_code -eq 403 ]];then
     echo "($status_code) $URL ($filename)" >> valid_urls.txt
     echo -e "$filename: \x1B[33m⚠️  Warning - Forbidden - status code: $status_code for domain $URL  \x1B[0m"
