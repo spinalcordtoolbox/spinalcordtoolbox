a2cd18121cf4f0851ad08cc926af0f63f5c6ce59
valosekj <jan.valosek@upol.cz>
2025-06-18 21:35:57 +0200

    remove "dorsal-only" to make explicit that segmentation of both dorsal and ventral rootlets is expected

--- spinalcordtoolbox/scripts/sct_register_to_template.py
+++ spinalcordtoolbox/scripts/sct_register_to_template.py
@@ -167,9 +167,8 @@ def get_parser():
         '-lrootlet',
         metavar=Metavar.file,
         help=textwrap.dedent("""
-            Dorsal (and ventral) nerve rootlets segmentation. Example: `anat_rootlets.nii.gz`
+            Dorsal and ventral nerve rootlets segmentation. Example: `anat_rootlets.nii.gz`
 
-            Both dorsal-only as well as dorsal and ventral rootlets are supported.
             Only labels within the range C2-Th1 (i.e., 2 to 9) are supported. If labels outside this range are provided, they will be ignored.
             Each value corresponds to the spinal level (e.g.: 2 for spinal level C2, 8 for spinal level C8, 9 for spinal level Th1).
             If you are using more than 2 labels, all spinal levels covering the region of interest should be provided (e.g., if you are interested in levels C2 to C7, then you should provide spinal level labels 2,3,4,5,6,7).
