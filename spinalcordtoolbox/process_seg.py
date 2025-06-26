525666ac59e0e986181cb75edcac4fae48f7a3fc
valosekj <jan.valosek@upol.cz>
2025-06-26 13:24:20 +0200

    Swap text position

--- spinalcordtoolbox/process_seg.py
+++ spinalcordtoolbox/process_seg.py
@@ -502,8 +502,8 @@ def compute_quadrant_areas(image_crop_r: np.ndarray, centroid: tuple[float, floa
     ax1.text(x0 + offset, y0 - offset, f"AR:\n{quadrant_areas['Anterior_Right']:.2f} mm²", color='blue', fontsize=10, ha='center', va='bottom', bbox=dict(facecolor='white', alpha=0.8, edgecolor='none'))
     ax1.text(x0 - offset, y0 + offset, f"PL:\n{quadrant_areas['Posterior_Left']:.2f} mm²", color='green', fontsize=10, ha='center', va='top', bbox=dict(facecolor='white', alpha=0.8, edgecolor='none'))
     ax1.text(x0 + offset, y0 + offset, f"AL:\n{quadrant_areas['Anterior_Left']:.2f} mm²", color='purple', fontsize=10, ha='center', va='top', bbox=dict(facecolor='white', alpha=0.8, edgecolor='none'))
-    ax1.text(0.25, 0.05, f"Ant. RL Symmetry: {symmetry_anterior_RL:.3f}", fontsize=10, ha='center', va='center', bbox=dict(facecolor='yellow', alpha=0.8, edgecolor='black'), transform=ax1.transAxes)
-    ax1.text(0.75, 0.05, f"Pos. RL Symmetry: {symmetry_posterior_RL:.3f}", fontsize=10, ha='center', va='center', bbox=dict(facecolor='yellow', alpha=0.8, edgecolor='black'), transform=ax1.transAxes)
+    ax1.text(0.75, 0.05, f"Ant. RL Symmetry: {symmetry_anterior_RL:.3f}", fontsize=10, ha='center', va='center', bbox=dict(facecolor='yellow', alpha=0.8, edgecolor='black'), transform=ax1.transAxes)
+    ax1.text(0.25, 0.05, f"Pos. RL Symmetry: {symmetry_posterior_RL:.3f}", fontsize=10, ha='center', va='center', bbox=dict(facecolor='yellow', alpha=0.8, edgecolor='black'), transform=ax1.transAxes)
 
     ax1.legend(loc='upper right')
 
