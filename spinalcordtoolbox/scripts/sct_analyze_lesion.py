c90eb6410bdc928c93174bbe6477426f96c46821
valosekj <jan.valosek@upol.cz>
2025-07-11 07:58:30 +0200

    remove centerline from the `-s` argument help
    context: https://github.com/spinalcordtoolbox/spinalcordtoolbox/pull/4961#discussion_r2198404646

--- spinalcordtoolbox/scripts/sct_analyze_lesion.py
+++ spinalcordtoolbox/scripts/sct_analyze_lesion.py
@@ -59,7 +59,7 @@ def get_parser():
     optional.add_argument(
         "-s",
         help=textwrap.dedent("""
-            Spinal cord centerline or segmentation file, which will be used to correct morphometric measures with cord angle with respect to slice. (e.g. `t2_seg.nii.gz`)
+            Spinal cord segmentation, which will be used to correct morphometric measures with cord angle with respect to slice. (e.g. `t2_seg.nii.gz`)
 
             If provided, then the lesion volume, length, diameter, axial damage ratio, and tissue bridges will be computed. Otherwise, if not provided, then only the lesion volume will be computed.
         """),  # noqa: E501 (line too long)
