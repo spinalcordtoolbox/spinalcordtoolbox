a8ba6700f125ad17fddfc77d17c45ea59b906715
Joshua Newton <joshuacwnewton@gmail.com>
2025-07-18 15:44:02 -0400

    `sct_fmri_moco`: Fix `-param` typo: `numTarget` -> `num_target`
    Potentially `gradStep` and `iterAvg` should also be switched from camelCase to snake_case, but this would
    require renaming the actual attributes of the ParamMoco class, which would break compatibility.

--- spinalcordtoolbox/scripts/sct_fmri_moco.py
+++ spinalcordtoolbox/scripts/sct_fmri_moco.py
@@ -74,7 +74,7 @@ def get_parser():
              f"allowed. Default={param_default.gradStep}.\n"
              f"  - sampling [None or 0-1]: Sampling rate used for registration metric. "
              f"Default={param_default.sampling}.\n"
-             f"  - numTarget [int]: Target volume or group (starting with 0). Default={param_default.num_target}.\n"
+             f"  - num_target [int]: Target volume or group (starting with 0). Default={param_default.num_target}.\n"
              f"  - iterAvg [int]: Iterative averaging: Target volume is a weighted average of the "
              f"previously-registered volumes. Default={param_default.iterAvg}.\n"
     )
