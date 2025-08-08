f34359f143a35a522fd3eaa5414229a8df727a5f
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-08-08 15:06:39 -0400

    test for the WA value too

--- testing/cli/test_cli_sct_extract_metric.py
+++ testing/cli/test_cli_sct_extract_metric.py
@@ -34,7 +34,9 @@ def test_sct_extract_metric_vertlevel_outside_bounds():
     results = np.genfromtxt(fname_out, delimiter=',', dtype=None, encoding="utf-8")  # dtype=None preserves str data
     header_row, data_rows = results[0],  np.char.strip(results[1:], '"')
     idx_vertlevel = np.where(header_row == "VertLevel")[0][0]
+    idx_size = np.where(header_row == "WA()")[0][0]
     assert list(data_rows[:, idx_vertlevel].astype(int)) == [5, 4]  # VertLevel column should only have 2 values
+    assert list(data_rows[:, idx_size].astype(float)) == pytest.approx([35.268203117287605, 30.073466350619103], abs=0.001)
 
 
 def test_sct_extract_metric_vertfile_doesnt_exists():
