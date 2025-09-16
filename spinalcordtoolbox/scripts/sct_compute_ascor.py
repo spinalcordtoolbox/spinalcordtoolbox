cf476e59a5f221fcab8b55f8f0a70e53adb49202
Sandrine Bédard <71230552+sandrinebedard@users.noreply.github.com>
2025-09-16 11:25:01 -0400

    Add function to compute aSCOR (#5007)
    ## Checklist
    
    <!-- Hi, and thank you for submitting a Pull Request! Please make sure
    to check the boxes below before submitting. -->
    
    - [x] **PR Sidebar**: I've filled in each of the options within the PR
    sidebar.
    ([Reviewers](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#231-reviewers),
    [Assignees](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#232-assignees),
    [Labels](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#233-labels)
    (**exactly one** dark purple label!),
    [Milestone](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#234-milestone),
    [Development](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#235-development).)
    - [x] **Contributing Docs**: I've read the [Contributing
    guidelines](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request))
    to make sure my
    [branch](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing%3A-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#part-1-branches)
    and [pull
    request](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing%3A-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#part-2-pull-requests)
    meet SCT's guidelines. (Feel free to stop and fixup your commits before
    submitting.)
    - [x] **Tests**: I've added [relevant
    tests](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Testing)
    for my contribution, if necessary. I've also made sure that my
    contribution passes [all of the automated
    tests](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Contributing:-Submitting-changes-to-SCT-(Opening-a-Pull-Request)#24-checking-the-automated-tests)
    (which will be triggered after the PR is submitted).
    - [ ] **Documentation**: I've updated the [relevant
    documentation](https://github.com/spinalcordtoolbox/spinalcordtoolbox/wiki/Documentation)
    for my changes, including argparse descriptions, docstrings, and
    ReadTheDocs tutorial pages.
    
    ## Description
    
    This PR add a new CLI function to compute aSCOR ((Spinal Cord CSA /
    Spinal Canal CSA) * 100)), a marker for DCM (and compression-associated
    pathologies).
    
    This script has spinal cord and spinal canal segmentations as inputs,
    and calls 2 times `sct_process_segmentation` (once for spinal cord, once
    for canal), with the same parameters, and the computes aSCOR.
    
    Here is a
    [reference](https://www.nature.com/articles/s41393-024-00993-8#:~:text=For%20evaluation%20of%20the%20occurrence,acquisition%20takes%20about%203%20min.)
    that details the computation of aSCOR
    ### Whats left
    
    - [x] Add tests
    - [ ] Create tutorial
    - [ ] Consider adding normalization 
    - [ ] Add canal seg in the testing data for the test to pass
    
    ## How to test?
    
    ```
    sct_compute_ascor -i-SC t2_seg.nii.gz -i-canal t2_canal_seg.nii.gz -vertfile t2_seg_labeled.nii.gz -perlevel 1 -vert 2:8  -o acsor.csv
    ```
    Here is the data:
    * `t2_seg.nii.gz`:
    [t2_seg.nii.gz](https://github.com/user-attachments/files/21778584/t2_seg.nii.gz)
    * `t2_canal_seg.nii.gz`:
    [t2_canal_seg.nii.gz](https://github.com/user-attachments/files/21778593/t2_canal_seg.nii.gz)
    * `t2_seg_labeled.nii.gz`:
    [t2_seg_labeled.nii.gz](https://github.com/user-attachments/files/21778597/t2_seg_labeled.nii.gz)
    
    
    ## Linked issues
    
    Fixes #4997
    
    ---------
    
    Co-authored-by: Joshua Newton <joshuacwnewton@gmail.com>

---
+++ spinalcordtoolbox/scripts/sct_compute_ascor.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+#
+# Compute adapted Spinal Cord Occupation Ratio (aSCOR) from spinal cord and spinal canal masks.
+# morphometrics.
+#
+# Copyright (c) 2025 Polytechnique Montreal <www.neuro.polymtl.ca>
+# License: see the file LICENSE
+
+import sys
+import os
+import textwrap
+import logging
+import itertools
+import numpy as np
+from typing import Sequence
+from spinalcordtoolbox.utils.fs import get_absolute_path, TempFolder
+from spinalcordtoolbox.utils.sys import init_sct, printv, set_loglevel
+from spinalcordtoolbox.utils.shell import display_open, Metavar
+from spinalcordtoolbox.image import Image
+from spinalcordtoolbox.utils.sys import LazyLoader
+from spinalcordtoolbox.scripts import sct_process_segmentation
+
+pd = LazyLoader("pd", globals(), "pandas")
+
+logger = logging.getLogger(__name__)
+
+INDEX_COLUMNS = ['Filename_sc', 'Filename_canal', 'Slice (I->S)', 'VertLevel', 'DistancePMJ', 'aSCOR']
+
+
+# PARSER
+# ==========================================================================================
+
+
+def get_parser():
+    """
+    :return: Returns the parser with the command line documentation contained in it.
+    """
+    # Because `sct_compute_ascor` is a light wrapper for `sct_process_segmentation`, we want to re-use most of the arguments.
+    parser = sct_process_segmentation.get_parser(ascor=True)
+    parser.description = textwrap.dedent("""
+        Compute adapted Spinal Cord Occupation Ratio (aSCOR) from spinal cord and spinal canal segmentation masks.
+        The aSCOR is defined as the ratio between the cross-sectional area of the spinal cord and the cross-sectional area of the spinal canal
+    """)  # noqa: E501 (line too long)
+
+    # In `sct_process_segmentation`, we normally use the `-i` argument. But because we set `ascor=True` when calling
+    # `get_parser()`, `-i` won't be created (to allow us to add `-i-SC` and `-i-canal` instead).
+    mandatory = parser.mandatory_arggroup
+    mandatory.add_argument(
+        '-i-SC',
+        metavar=Metavar.file,
+        help=textwrap.dedent("""
+            Spinal cord segmentation mask to compute morphometrics from. Example: `sub-001_T2w_seg.nii.gz`
+            Spinal cord segmentation can be obtained with `sct_deepseg spinalcord`.
+        """),  # noqa: E501 (line too long)
+    )
+    mandatory.add_argument(
+        '-i-canal',
+        metavar=Metavar.file,
+        help=textwrap.dedent("""
+            Spinal canal segmentation mask to compute morphometrics from. Example: `sub-001_T2w_canal_seg.nii.gz`
+            Canal segmentation can be obtained with `sct_deepseg sc_canal_t2`.
+        """),  # noqa: E501 (line too long)
+    )
+
+    return parser
+
+
+def compute_ascor(csa_sc, csa_canal):
+    """
+    Computes the aSCOR (spinal cord area to canal area ratio) for each row in the provided CSV files and saves the results to a CSV file.
+
+    :param str csa_sc: Path to the CSV file containing spinal cord area measurements.
+    :param str csa_canal: Path to the CSV file containing spinal canal area measurements.
+    :return pandas.DataFrame: DataFrame containing the aSCOR results."""
+    df_sc = pd.read_csv(csa_sc)
+    df_canal = pd.read_csv(csa_canal)
+    df_merged = pd.merge(df_sc, df_canal, on=['Slice (I->S)', 'VertLevel', 'DistancePMJ'], suffixes=('_sc', '_canal'))
+    df_merged['aSCOR'] = df_merged['MEAN(area)_sc'].div(df_merged['MEAN(area)_canal'], fill_value=0)
+    df_merged['aSCOR'].replace(np.inf, np.nan)  # output nan instead of inf for /0
+    printv(f"Computed aSCOR for {len(df_merged)} rows.", 1, 'normal')
+    return df_merged[INDEX_COLUMNS]
+
+
+def main(argv: Sequence[str]):
+    parser = get_parser()
+    arguments = parser.parse_args(argv)
+    verbose = arguments.v
+    set_loglevel(verbose=verbose, caller_module_name=__name__)    # values [0, 1, 2] map to logging levels [WARNING, INFO, DEBUG]
+
+    # Load input and output filenames (while passing the remaining args to `sct_process_segmentation`)
+    fname_sc_seg = get_absolute_path(arguments.i_SC)
+    fname_canal_seg = get_absolute_path(arguments.i_canal)
+    fname_out = arguments.o
+    process_seg_argv = list(itertools.chain.from_iterable([
+        [arg, value] for arg, value in zip(argv[::2], argv[1::2])
+        if arg not in ['-i-SC', '-i-canal', '-o']
+    ]))
+    # Validate the inputs
+    img_sc = Image(fname_sc_seg).change_orientation('RPI')
+    img_canal = Image(fname_canal_seg).change_orientation('RPI')
+    if not img_sc.data.shape == img_canal.data.shape:
+        raise ValueError(f"Shape mismatch between spinal cord segmentation [{img_sc.data.shape}],"
+                         f" and canal segmentation [{img_canal.data.shape}]). "
+                         f"Please verify that your spinal cord and canal segmentations were done in the same space.")
+
+    # Run sct_process_segmentation twice: 1) SC seg 2) canal seg
+    temp_folder = TempFolder(basename="process-segmentation")
+    path_tmp = temp_folder.get_path()
+    path_tmp_sc = os.path.join(path_tmp, "sc.csv")
+    path_tmp_canal = os.path.join(path_tmp, "canal.csv")
+    printv("Running sct_process_segmentation on spinal cord segmentation...", verbose, 'normal')
+    sct_process_segmentation.main(['-i', fname_sc_seg, '-o', path_tmp_sc,] + process_seg_argv)
+    printv("Running sct_process_segmentation on spinal canal segmentation...", verbose, 'normal')
+    sct_process_segmentation.main(['-i', fname_canal_seg, '-o', path_tmp_canal,] + process_seg_argv)
+
+    # Compute aSCOR
+    printv("Computing aSCOR...", verbose, 'normal')
+    df_ascor = compute_ascor(path_tmp_sc, path_tmp_canal)
+    # Save aSCOR to csv
+    if arguments.append and os.path.exists(fname_out):
+        dataframe_old = pd.read_csv(fname_out, index_col=INDEX_COLUMNS)
+        df_ascor = pd.concat([dataframe_old.reset_index(), df_ascor], ignore_index=True)
+    df_ascor.to_csv(fname_out, index=False)
+    printv(f'\nSaved: {os.path.abspath(fname_out)}')
+    display_open(os.path.abspath(fname_out))
+
+    # Clean up temp
+    if arguments.r and temp_folder is not None:
+        logger.info("\nRemove temporary files...")
+        temp_folder.cleanup()
+
+
+if __name__ == "__main__":
+    init_sct()
+    main(sys.argv[1:])
