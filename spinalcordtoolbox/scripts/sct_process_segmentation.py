b24b82c37b19dc4146c426001d4b05455fcc1e01
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-09-25 11:18:36 -0400

    fix qc

--- spinalcordtoolbox/scripts/sct_process_segmentation.py
+++ spinalcordtoolbox/scripts/sct_process_segmentation.py
@@ -602,26 +602,11 @@ def main(argv: Sequence[str]):
                 fname_seg=fname_segmentation,
                 metrics=metrics_native_space if normalize_pam50 else metrics,
                 argv=argv,
-                angle_type='ellipse',
                 path_qc=arguments.qc,
                 dataset=arguments.qc_dataset,
                 subject=arguments.qc_subject,
+                angle_type='angle_hog',
             )
-            qc2.sct_process_segmentation(
-                fname_input=fname_image,
-                fname_seg=fname_segmentation,
-                metrics=metrics_native_space if normalize_pam50 else metrics,
-                argv=argv,
-                angle_type='HOG',
-                path_qc=arguments.qc + '_HOG',
-                dataset=arguments.qc_dataset,
-                subject=arguments.qc_subject,
-            )
-
-            #     path_qc=arguments.qc,
-            #     dataset=arguments.qc_dataset,
-            #     subject=arguments.qc_subject,
-            # )
 
     display_open(file_out)
 
