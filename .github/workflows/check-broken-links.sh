f43b42de60e4a0236ff952fd7de91d9336e97faf
Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>
2025-10-09 19:10:43 -0400

    Refactor the broken link checker (#5047)
    Here's a rewrite of the broken link checker, as a standalone bash
    script, so that:
    - we don't have to repeat mostly the same logic twice; and
    - it's checkable by shellcheck.
    
    The new `check-broken-links.sh` script accepts a list of files to check
    as its arguments, which can come from either:
    - a `find` command for the daily run which checks everything; or
    - a `git diff` command for the files touched by any given pull request.
    
    In either case, the exclude list which used to be in
    `check_url_blacklist.txt` is now included inline in the
    `check-broken-links.sh` script. It could be restored as a file by using
    `grep --exclude-from` instead of `grep --exclude`, but I think that
    would make a future reader less likely to check the documentation of
    `grep --exclude` to see how the exclusions are interpreted (it's
    somewhat subtle).
    
    I took the liberty of changing `check-url.sh` so that it takes two real
    arguments, rather than having to split around a semicolon.
    
    Fixes #5045.

---
+++ .github/workflows/check-broken-links.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+printf '%s\n' "Checking for broken links in $# files:" "$@" '=========='
+
+# Find and check each URL in the files provided as arguments to the script.
+# The (($# == 0)) test is to prevent grep from using standard input if no files were given.
+# The grep -H option is to output the filename even if a single file was given.
+# The grep -Zz options are to make the filename and matching URL terminated by null bytes,
+# which is the safest way to delimit arbitrary text.
+# The (($? == 1)) test is to prevent the script from exiting if no URLs were found.
+# Setting IFS= (the empty string) prevents `read` from trimming whitespace.
+# The </dev/null redirection is to prevent check-url.sh from stealing the output of grep.
+{ (($# == 0)) || grep -EioHZz \
+  --exclude 'sct.def' \
+  --exclude 'CHANGES.md' \
+  --exclude 'requirements.txt' \
+  --exclude 'requirements-freeze.txt' \
+  '\b(https?)://[-a-z0-9+&@#/%?=~_|$!:,.;]*[a-z0-9+&@#/%=~_|$]' \
+  -- "$@" || (($? == 1)) ; } |
+  while IFS= read -rd '' filename && IFS= read -rd '' url; do
+    .github/workflows/check-url.sh "$filename" "$url" </dev/null
+  done
+
+# Summarize the results
+touch valid_urls.txt redirected_urls.txt invalid_urls.txt
+NUM_OK=$(wc -l valid_urls.txt | cut -d " " -f 1)
+NUM_REDIRECT=$(wc -l redirected_urls.txt | cut -d " " -f 1)
+NUM_BAD=$(wc -l invalid_urls.txt | cut -d " " -f 1)
+cat invalid_urls.txt
+echo -en "========== \033[0;32m$NUM_OK passed\033[0;0m, \033[0;33m$NUM_REDIRECT redirected\033[0;0m, \033[0;31m$NUM_BAD failed\033[0;0m ==========\n"
+
+# Exit with failure if there are any bad URLs
+((NUM_BAD == 0))
