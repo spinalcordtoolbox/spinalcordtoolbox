f38415a2d076c202c842a625698798390e91b48e
Nathan Molinier <nathan.molinier@gmail.com>
2025-11-20 14:10:10 -0500

    add space

--- spinalcordtoolbox/scripts/sct_deepseg.py
+++ spinalcordtoolbox/scripts/sct_deepseg.py
@@ -298,7 +298,7 @@ def get_parser(subparser_to_return=None):
                 help="Contrast of the input. Specifies the contrast order of input images (e.g. `-c t1 t2`)",
                 choices=('t1', 't2', 't2star'),
                 metavar=Metavar.str)
-        if task_name == 'spine': # totalspineseg
+        if task_name == 'spine':  # totalspineseg
             params.add_argument(
                 "-label-vert",
                 type=int,
