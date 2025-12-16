2d1b366f249fa3b19f41b8c50fe0b9ea50951a5d
Joshua Newton <joshuacwnewton@gmailcom>
2025-12-16 14:52:24 -0500

    `test_cli.py`: Add new test that explicitly checks import of modules
    This new test gets to the thrust of why we even had an "import time" test in the first place -- to catch
    the import of expensive modules on new PRs. I figure that going straight to the source will help contributors
    understand and fix their contributions while not running into any flakiness whatsoever.
    
    Also, one benefit of taking this approach is that it prevents the "accumulation of small slowdowns" -- e.g.
    if an import takes 300ms, it might not trip the old test by itself, but it would when combined with other
    semi-slow imports. So, we catch them early before they accumulate and get merged.
    
    I tried a few methods to figure out which modules are getting imported (e.g. the built-in modulefinder), but
    they seemed to be broken not only due to upstream bugs (https://bugs.python.org/issue40350) but also due to
    pytest causing some sort of infinite recursion. I tried both `modulefinder` and the spin-off `modulegraph`
    but neither seemed to be able to figure out the imports.
    
    The `sys.modules`-based approach seems to work well enough, even if `del` feels a bit awkward to attain
    test isolation.

--- testing/cli/test_cli.py
+++ testing/cli/test_cli.py
@@ -1,6 +1,8 @@
 # pytest unit tests for all cli scripts
 
 import pytest
+import sys
+import importlib
 from importlib.metadata import entry_points
 import subprocess
 
@@ -24,3 +26,50 @@ def test_calling_scripts_with_no_args_shows_usage(capsys, script):
     completed_process = subprocess.run([script], capture_output=True)
     assert completed_process.returncode == 2
     assert b'usage' in completed_process.stderr
+
+
+@pytest.mark.parametrize("script", scripts_to_test)
+def test_importing_scripts_avoids_expensive_modules(script):
+    """
+    Test that importing SCT's scripts does not import expensive modules like numpy or scipy.
+    This ensures that lazy loading is working correctly.
+    """
+    # NB: The list of expensive modules comes from the modules we're already lazy-loading. The thought process is that
+    #     if we're lazy-loading that module somewhere, we should do it everywhere consistently.
+    expensive_modules = [
+        'dipy',
+        'matplotlib',
+        'nibabel',
+        'nilearn',
+        'pandas',
+        'scipy.ndimage',
+        'scipy.signal',
+        'scipy.stats',
+        'sklearn',
+        'torch',
+        'voxelmorph',
+    ]
+
+    # Wipe `sys.modules` of expensive modules to force reimports
+    for m in list(sys.modules.keys()):
+        for em in expensive_modules:
+            if m.startswith(em):
+                del sys.modules[m]
+
+    # Import the script module
+    before = set(sys.modules.keys())
+    importlib.import_module(f"spinalcordtoolbox.scripts.{script}")
+    after = set(sys.modules.keys())
+
+    # Clear the imported modules to keep things clean for the next test
+    imported_modules = after - before
+    for im in imported_modules:
+        del sys.modules[im]
+
+    # Check that expensive modules are not in sys.modules
+    found_modules = []
+    for em in expensive_modules:
+        if any(im.startswith(em) for im in imported_modules):
+            found_modules.append(em)
+    if found_modules:
+        pytest.fail(f"Expensive modules found while importing {script}. Please lazy-load these modules: {found_modules}")
