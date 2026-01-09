da471395df4205c029f2215cc9c924588c03797e
Joshua Newton <joshuacwnewton@gmail.com>
2026-01-09 11:29:58 -0500

    Revert "`.ci.sh`: Run only the `sct_warp_template` test + profile it"
    This reverts commit e85fcca1fe03a8c39d80f4b76e76f3a762480e30.

--- .ci.sh
+++ .ci.sh
@@ -49,9 +49,8 @@ check_dependencies() {
 run_tests() {
   activate_venv_sct
   # re-run failed tests, just in case something flaky has happened. if no failures, do nothing.
-  # pytest testing/api testing/cli || pytest --last-failed --last-failed-no-failures none
+  pytest testing/api testing/cli || pytest --last-failed --last-failed-no-failures none
   # NB: 'testing/batch_processing' is run by a separate CI workflow
-  pytest testing/cli/test_cli_sct_warp_template.py --profile
 }
 
 while getopts ":ict" opt; do
