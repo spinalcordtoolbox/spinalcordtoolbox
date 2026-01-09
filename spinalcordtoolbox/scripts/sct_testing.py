51f1e16072d5f681e9f9edbf8213d2118e221286
Joshua Newton <joshuacwnewton@gmail.com>
2026-01-09 14:00:06 -0500

    `tests.yml`: Write detailed logs to `pytest.log` via `sct_testing`

--- spinalcordtoolbox/scripts/sct_testing.py
+++ spinalcordtoolbox/scripts/sct_testing.py
@@ -12,6 +12,7 @@
 # License: see the file LICENSE
 
 import pytest
+import os
 import sys
 
 from spinalcordtoolbox.utils.sys import __sct_dir__
@@ -19,4 +20,16 @@ from spinalcordtoolbox.utils.sys import __sct_dir__
 if __name__ == "__main__":
     # Treat `sct_testing` as an alias to `pytest`. If no arguments are passed,
     # simply point pytest at the repo (to avoid looking for tests in the pwd).
-    sys.exit(pytest.main(sys.argv[1:] if sys.argv[1:] else [__sct_dir__]))
+    argv = sys.argv[1:] if sys.argv[1:] else [__sct_dir__]
+
+    # For SCT's CI, add extra logging to a log file.
+    # We don't specify this in `setup.cfg` because we don't want to clutter
+    # users' working directories
+    if "GITHUB_ACTIONS" in os.environ and "PYTEST_LOG_FILENAME" in os.environ:
+        log_file = os.path.join(__sct_dir__, os.environ["PYTEST_LOG_FILENAME"])
+        argv += [
+            f"--log-file={log_file}",
+            "--log-file-level=DEBUG",
+        ]
+
+    sys.exit(pytest.main(argv))
