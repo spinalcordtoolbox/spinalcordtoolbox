89f35a1d8204879066518ef2254d2f5792f078c8
NTucksinapinunchai <nontharatt@gmail.com>
2026-01-28 15:34:29 +0700

    Add displacement field test and compare with `sct_apply_transfo`

--- testing/cli/test_cli_sct_fmri_moco.py
+++ testing/cli/test_cli_sct_fmri_moco.py
@@ -1,9 +1,11 @@
 # pytest unit tests for sct_fmri_moco
 
+import numpy as np
 import pytest
 import logging
 
-from spinalcordtoolbox.scripts import sct_fmri_moco, sct_deepseg, sct_maths, sct_create_mask
+from spinalcordtoolbox.scripts import sct_fmri_moco, sct_deepseg, sct_maths, sct_create_mask, sct_image, sct_apply_transfo
+from spinalcordtoolbox.image import Image
 from spinalcordtoolbox.utils.sys import sct_test_path
 
 logger = logging.getLogger(__name__)
@@ -73,9 +75,35 @@ def test_sct_fmri_moco_invalid_group_values(tmp_path, tmp_path_qc, fmri_mean_seg
 
 
 @pytest.mark.sct_testing
-def test_sct_fmri_moco_dl_no_checks(tmp_path_qc, fmri_mean, fmri_mask, tmp_path, fmri_mean_seg):
+def test_sct_fmri_moco_dl(tmp_path_qc, fmri_mean, fmri_mask, tmp_path, fmri_mean_seg):
     """Run the CLI script with '-m' and '-ref' option and using '-dl' algorithm."""
     sct_fmri_moco.main(argv=['-i', sct_test_path('fmri', 'fmri.nii.gz'),
                              '-ref', fmri_mean, '-m', fmri_mask, '-ofolder', str(tmp_path), '-dl',
                              '-qc', tmp_path_qc, '-qc-seg', fmri_mean_seg])
-    # NB: We skip checking params because there are no output moco params from DL-module
+
+    fname_warp = str(tmp_path / "warp_fmri_moco.nii.gz")
+    sct_image.main(argv=['-i', fname_warp, '-split', 't'])
+    sct_image.main(argv=['-i', sct_test_path('fmri', 'fmri.nii.gz'), '-split', 't', '-o', str(tmp_path / "fmri.nii.gz")])
+
+    T = Image(sct_test_path('fmri', 'fmri.nii.gz')).data.shape[3]
+    warped_list = []
+    for t in range(T):
+        fname_vol = str(tmp_path / f"fmri_T{t:04d}.nii.gz")
+        fname_warp = str(tmp_path / f"warp_fmri_moco_T{t:04d}.nii.gz")
+        fname_out = str(tmp_path / f"fmri_warped_T{t:04d}.nii.gz")
+
+        sct_apply_transfo.main(argv=['-i', fname_vol, '-d', fmri_mean,
+                                     '-w', fname_warp, '-o', fname_out, '-x', 'linear'])
+        assert (tmp_path / f"fmri_warped_T{t:04d}.nii.gz").exists(), "sct_apply_transfo failed to create output"
+        warped_list.append(Image(fname_out).data.astype(np.float32))
+
+    warped = np.stack(warped_list, axis=3)
+    moco_dl = Image(str(tmp_path / "fmri_mocoDL.nii.gz")).data.astype(np.float32)
+    mask = Image(fmri_mask).data > 0
+    mask = mask[..., None]
+
+    diff = (warped - moco_dl) * mask
+    rmse = float(np.sqrt(np.sum(diff ** 2) / (np.sum(mask) + 1e-6)))
+    norm = float(np.sum(np.abs(moco_dl) * mask) / (np.sum(mask) + 1e-6) + 1e-6)
+    nrmse = rmse / norm
+    assert nrmse < 0.2
