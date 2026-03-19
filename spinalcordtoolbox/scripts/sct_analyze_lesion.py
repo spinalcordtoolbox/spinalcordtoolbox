00ef744de29a2342bde33b26f331255a829693b6
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2026-03-19 13:46:15 -0400

    removed sum of column vert levels

--- spinalcordtoolbox/scripts/sct_analyze_lesion.py
+++ spinalcordtoolbox/scripts/sct_analyze_lesion.py
@@ -791,7 +791,7 @@ class AnalyzeLesion:
 
         # Add the total column
         self.distrib_matrix_dct[sheet_name]['total % (all tracts)'] = \
-            self.distrib_matrix_dct[sheet_name].sum(numeric_only=True, axis=1)
+            self.distrib_matrix_dct[sheet_name].drop(columns=['vert_level'], errors='ignore').sum(numeric_only=True, axis=1)
 
         # Add additional columns for the "CombinedLabels" defined by info_label.txt
         for label_name, sublabels in self.atlas_combinedlabels.items():
