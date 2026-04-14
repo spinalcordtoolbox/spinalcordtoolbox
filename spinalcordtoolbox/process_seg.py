f743737ec87207cac6f098885858f9394a121cbc
valosekj <jan.valosek@upol.cz>
2026-04-14 19:19:37 +0200

    refactor anterior and posterior lengths calculation to use cord center of mass
    see the updated comments in the code for more context

--- spinalcordtoolbox/process_seg.py
+++ spinalcordtoolbox/process_seg.py
@@ -479,15 +479,16 @@ def _measure_ap_diameter(seg_crop_r, seg_crop_r_rotated, dim, angle, iz, propert
 
         ap_diameter = ap_pixels * dim[1]
 
-        # Compute anterior and posterior lengths by splitting the AP diameter at the widest horizontal line.
+        # Compute anterior and posterior lengths by splitting the AP diameter at the cord center of mass.
+        # For an ellipse, the center of mass lies on the major (RL) axis, so its AP coordinate equals the AP position
+        # of the widest horizontal line — equivalent to the split used in Kang et al. J Clin Med 2023,
+        # https://doi.org/10.3390/jcm12124111 (Fig. 1), but more robust than argmax on raw pixel sums, which I tried in
+        # https://github.com/spinalcordtoolbox/spinalcordtoolbox/blob/15bef9f45980cf562b9e9b43ceaee6cdd5100a31/spinalcordtoolbox/process_seg.py#L491.
+        # Anterior length: distance from the CoM to the anterior cord edge.
+        # Posterior length: distance from the CoM to the posterior cord edge.
         # In RPI orientation, axis 1 (columns) is the PA direction: low index = posterior, high index = anterior.
-        # The split point is the AP column with the maximum RL extent (i.e., the widest horizontal line).
-        # Anterior length: distance from the widest line to the anterior cord edge.
-        # Posterior length: distance from the widest line to the posterior cord edge.
-        # Inspiration: Kang et al. J Clin Med 2023, https://doi.org/10.3390/jcm12124111 (Fig. 1)
-        ap_widest_r = int(np.argmax(np.sum(seg_crop_r_rotated, axis=0)))
-        ap_pixels_posterior = np.sum(seg_crop_r_rotated[indices, :ap_widest_r], axis=1).mean()
-        ap_pixels_anterior = np.sum(seg_crop_r_rotated[indices, ap_widest_r:], axis=1).mean()
+        ap_pixels_posterior = np.sum(seg_crop_r_rotated[indices, :ap0_r], axis=1).mean()
+        ap_pixels_anterior = np.sum(seg_crop_r_rotated[indices, ap0_r:], axis=1).mean()
         length_posterior = ap_pixels_posterior * dim[1]
         length_anterior = ap_pixels_anterior * dim[1]
 
