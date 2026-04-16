`sct_analyze_lesion`: Fix off-by-one error causing test failures
For the test case, the SC slices were [25, 26, 27, 28, 29, 30, 31, 32]. The previous logic retrieved an
SC AP diameter of 7 voxels from this, rather than 8. After fixing this off-by-one bug, the tests now pass.

Also, the hardcoded values in the new test also need to be updated to match the fixed outputs.

spinalcordtoolbox/scripts/sct_analyze_lesion.py
@@ -1263,8 +1263,8 @@ class AnalyzeLesion:
             # Get the A-P diameter (in pixels) for both slices
             sc_indices1 = np.where(sc_slice1)[0]
             sc_indices2 = np.where(sc_slice2)[0]
-            sc_ap_diameter1 = sc_indices1[-1] - sc_indices1[0]  # ventral - dorsal
-            sc_ap_diameter2 = sc_indices2[-1] - sc_indices2[0]  # ventral - dorsal
+            sc_ap_diameter1 = len(sc_indices1)
+            sc_ap_diameter2 = len(sc_indices2)
 
             # Interpolate the A-P diameter
             sc_ap_diameter_interpolated = self._interpolate_values(sc_ap_diameter1, sc_ap_diameter2)

testing/cli/test_cli_sct_analyze_lesion.py
@@ -409,9 +409,9 @@ def test_sct_analyze_lesion_no_lesion_found_nli(tmp_path, tmp_path_qc):
         measurements = pickle.load(f)['measures']
 
     # Hardcoded for now
-    np.testing.assert_allclose(measurements['interpolated_dorsal_bridge_width [mm]'], 3.48, rtol=0.001)
-    np.testing.assert_allclose(measurements['interpolated_ventral_bridge_width [mm]'], 3.48, rtol=0.001)
-    np.testing.assert_allclose(measurements['interpolated_total_bridge_width [mm]'], 6.96, rtol=0.001)
+    np.testing.assert_allclose(measurements['interpolated_dorsal_bridge_width [mm]'], 3.98, rtol=0.001)
+    np.testing.assert_allclose(measurements['interpolated_ventral_bridge_width [mm]'], 3.98, rtol=0.001)
+    np.testing.assert_allclose(measurements['interpolated_total_bridge_width [mm]'], 7.96, rtol=0.001)
 
 
 @pytest.mark.sct_testing