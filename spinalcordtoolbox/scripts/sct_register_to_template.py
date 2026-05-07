8b9ad832059ccf34f14c9b54f9891ce841ba3080
Joshua Newton <joshuacwnewton@gmail.com>
2026-05-07 15:01:11 -0400

    `sct_reg`: Raise `parser.error` if -ref subject is used with >2 labels

--- spinalcordtoolbox/scripts/sct_register_to_template.py
+++ spinalcordtoolbox/scripts/sct_register_to_template.py
@@ -388,8 +388,12 @@ def main(argv: Sequence[str]):
     # check input labels
     labels = check_labels(fname_landmarks, label_type=label_type)
     level_alignment = False
-    if len(labels) > 2 and label_type in ['disc', 'spinal', 'rootlet']:
-        level_alignment = True
+    if len(labels) > 2:
+        if ref == 'subject':
+            parser.error("'-ref subject' (rigid transform) is not compatible with 3+ labels (non-affine "
+                         "transform). Please use 'sct_label_utils -keep' to select 2 labels.")
+        elif label_type in ['disc', 'spinal', 'rootlet']:
+            level_alignment = True
     path_tmp = tmp_create(basename="register-to-template")
 
     # set temporary file names
