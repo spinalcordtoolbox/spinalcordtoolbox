de4c5a080cdd749d75fe0af6ab9b1b03d9b78e23
Joshua Newton <joshuacwnewton@gmail.com>
2026-05-22 11:47:37 -0400

    Update to (WIP) sct_tutorial_data release: `r20260522_dev0`
    CI run is in progress, updates to the cached values will be coming soon:
    - https://github.com/spinalcordtoolbox/sct_tutorial_data/actions/runs/26297191753/job/77412792760?pr=35

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -99,7 +99,7 @@ todo_include_todos = False
 extlinks = {
     # e.g. :sct_tutorial_data:`data_template-registration.zip` gets expanded into:
     # 'github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/<tag>/data_template-registration.zip'
-    'sct_tutorial_data': ('https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/r20260508/%s', '%s')
+    'sct_tutorial_data': ('https://github.com/spinalcordtoolbox/sct_tutorial_data/releases/download/r20260522_dev0/%s', '%s')
 }
 
 
