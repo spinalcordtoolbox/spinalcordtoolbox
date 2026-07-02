dac30216b79e66b5752d3e9d0fa9ee6ab0632ef2
quentin revillon <quentin.revillon@gmail.com>
2026-07-01 21:31:39 -0400

    Add "crop" key to all MODELS entries
    Makes the crop capability explicit for every model. Models using the
    sc-crop pipeline have crop: True; all others have crop: False.
    This allows sct_deepseg to check model_dict.get('crop') uniformly
    and makes the intent clear when adding future models.

--- spinalcordtoolbox/deepseg/models.py
+++ spinalcordtoolbox/deepseg/models.py
@@ -40,6 +40,7 @@ MODELS = {
         "contrasts": ["t1"],
         "framework": "ivadomed",
         "thr": 0.0,  # Only for display in argparse help (postprocessing.binarize_prediction is not present in model json)
+        "crop": False,
         "default": False,
     },
     "mice_uqueensland_gm": {
@@ -51,6 +52,7 @@ MODELS = {
         "contrasts": ["t1"],
         "framework": "ivadomed",
         "thr": 0.0,  # Only for display in argparse help (postprocessing.binarize_prediction is not present in model json)
+        "crop": False,
         "default": False,
     },
     "t2_tumor": {
@@ -61,6 +63,7 @@ MODELS = {
         "contrasts": ["t2"],
         "framework": "ivadomed",
         "thr": 0.5,  # Only for display in argparse help (mirrors postprocessing.binarize_prediction, which is 0.5 in model json)
+        "crop": False,
         "default": False,
     },
     "findcord_tumor": {
@@ -71,6 +74,7 @@ MODELS = {
         "contrasts": ["t2"],
         "framework": "ivadomed",
         "thr": 0.5,  # Only for display in argparse help (mirrors postprocessing.binarize_prediction, which is 0.5 in model json)
+        "crop": False,
         "default": False,
     },
     "model_seg_sctumor-edema-cavity_t2-t1_unet3d-multichannel": {
@@ -81,6 +85,7 @@ MODELS = {
         "contrasts": ["t2", "t1"],
         "framework": "ivadomed",
         "thr": 0.5,  # Only for display in argparse help (mirrors postprocessing.binarize_prediction, which is 0.5 in model json)
+        "crop": False,
         "default": False,
     },
     "model_seg_exvivo_gm-wm_t2_unet2d-multichannel-softseg": {
@@ -91,6 +96,7 @@ MODELS = {
         "contrasts": ["t2"],
         "framework": "ivadomed",
         "thr": 0.0,  # Only for display in argparse help (postprocessing.binarize_prediction is not present in model json)
+        "crop": False,
         "default": False,
     },
     "model_7t_multiclass_gm_sc_unet2d": {
@@ -102,6 +108,7 @@ MODELS = {
         "contrasts": ["t2star"],
         "framework": "ivadomed",
         "thr": 0.5,  # Only for display in argparse help (mirrors postprocessing.binarize_prediction, which is 0.5 in model json)
+        "crop": False,
         "default": False,
     },
     "model_seg_epfl_t2w_lumbar_sc": {
@@ -112,6 +119,7 @@ MODELS = {
         "contrasts": ["t2"],
         "framework": "ivadomed",
         "thr": 0.5,  # Only for display in argparse help (mirrors postprocessing.binarize_prediction, which is 0.5 in model json)
+        "crop": False,
         "default": False,
     },
     # NB: Handling image binarization threshold for ivadomed vs. non-ivadomed models:
@@ -144,66 +152,6 @@ MODELS = {
         },
         "default": True,
     },
-    "model_seg_sci_multiclass_sc_lesion_nnunet": {
-        "url": [
-            "https://github.com/ivadomed/model_seg_sci/releases/download/r20240729/model_SCIsegV2_r20240729.zip"
-        ],
-        "description": "Intramedullary SCI lesion and cord segmentation in T2w MRI",
-        "contrasts": ["t2"],
-        "framework": "nnunetv2",
-        "thr": None,  # Images are already binarized when splitting into sc-seg + lesion-seg
-        "default": False,
-    },
-    "model_seg_spinal_rootlets_nnunet": {
-        "url": [
-            "https://github.com/ivadomed/model-spinal-rootlets/releases/download/r20250318/model-spinal-rootlets-multicon-r20250318.zip"
-        ],
-        "description": "Segmentation of spinal nerve rootlets for T2w and MP2RAGE contrasts (T1w-INV1, T1w-INV2, and UNIT1) using nnUNet",
-        "contrasts": ["t2", "UNIT1", "T1w-INV1", "T1w-INV2"],
-        "framework": "nnunetv2",
-        "thr": None,  # Multiclass rootlets model (1.0, 2.0, 3.0...) -> no thresholding
-        "default": False,
-    },
-    "model_seg_gm_wm_mouse_nnunet": {
-         "url": [
-             "https://github.com/ivadomed/model_seg_mouse-sc_wm-gm_t1/releases/download/v0.4/model.zip"
-         ],
-         "description": "White and grey matter segmentation on T1-weighted exvivo mouse spinal cord using nnUNet",
-         "contrasts": ["t1"],
-         "framework": "nnunetv2",
-         "thr": None,  # Images are already binarized when splitting into gm-seg and wm-seg
-         "default": False,
-     },
-    "model_seg_sc_epi_nnunet": {
-         "url": [
-             "https://github.com/sct-pipeline/fmri-segmentation/releases/download/v0.2/model-fmri-segmentation-v0.2_nnUNetTrainer.zip"
-         ],
-         "description": "Spinal cord segmentation for EPI data (single 3D volume)",
-         "contrasts": ["bold"],
-         "framework": "nnunetv2",
-         "thr": None,  # Images are already binarized
-         "default": False,
-     },
-    "model_seg_ms_lesion_mp2rage": {
-         "url": [
-             "https://github.com/ivadomed/model_seg_ms_mp2rage/releases/download/r20240610/nnUNetTrainer_seg_ms_lesion_mp2rage__nnUNetPlans__3d_fullres.zip"
-         ],
-         "description": "Segmentation of spinal cord MS lesions on MP2RAGE UNIT1 contrast",
-         "contrasts": ["UNIT1"],
-         "framework": "nnunetv2",
-         "thr": None,  # Images are already binarized
-         "default": False,
-     },
-    "model_seg_ms_sc_lesion_bavaria_quebec_nnunet": {
-        "url": [
-            "https://github.com/ivadomed/model-seg-ms-axial-t2w/releases/download/r20241111/model_bavaria_quebec_axial_t2w_ms.zip"
-        ],
-        "description": "Intramedullary MS lesion and spinal cord segmentation in Axial T2w MRI",
-        "contrasts": ["t2"],
-        "framework": "nnunetv2",
-        "thr": None,  # Images are already binarized when splitting into sc-seg + lesion-seg
-        "default": False,
-    },
     # MS lesion model retrained on sc-crop cropped volumes (release r20260629).
     # Training padding: sup=40, inf=100, lr=20, ap=20 (see seg-sc-ms-lesion-multicontrast/sc_cropping/).
     "model_seg_ms_lesion": {
@@ -225,6 +173,72 @@ MODELS = {
             "pad_anterior": 20.0, "pad_posterior": 20.0,
         },
         "default": False,
+     },
+    "model_seg_sci_multiclass_sc_lesion_nnunet": {
+        "url": [
+            "https://github.com/ivadomed/model_seg_sci/releases/download/r20240729/model_SCIsegV2_r20240729.zip"
+        ],
+        "description": "Intramedullary SCI lesion and cord segmentation in T2w MRI",
+        "contrasts": ["t2"],
+        "framework": "nnunetv2",
+        "thr": None,  # Images are already binarized when splitting into sc-seg + lesion-seg
+        "crop": False,
+        "default": False,
+    },
+    "model_seg_spinal_rootlets_nnunet": {
+        "url": [
+            "https://github.com/ivadomed/model-spinal-rootlets/releases/download/r20250318/model-spinal-rootlets-multicon-r20250318.zip"
+        ],
+        "description": "Segmentation of spinal nerve rootlets for T2w and MP2RAGE contrasts (T1w-INV1, T1w-INV2, and UNIT1) using nnUNet",
+        "contrasts": ["t2", "UNIT1", "T1w-INV1", "T1w-INV2"],
+        "framework": "nnunetv2",
+        "thr": None,  # Multiclass rootlets model (1.0, 2.0, 3.0...) -> no thresholding
+        "crop": False,
+        "default": False,
+    },
+    "model_seg_gm_wm_mouse_nnunet": {
+         "url": [
+             "https://github.com/ivadomed/model_seg_mouse-sc_wm-gm_t1/releases/download/v0.4/model.zip"
+         ],
+         "description": "White and grey matter segmentation on T1-weighted exvivo mouse spinal cord using nnUNet",
+         "contrasts": ["t1"],
+         "framework": "nnunetv2",
+         "thr": None,  # Images are already binarized when splitting into gm-seg and wm-seg
+         "crop": False,
+         "default": False,
+     },
+    "model_seg_sc_epi_nnunet": {
+         "url": [
+             "https://github.com/sct-pipeline/fmri-segmentation/releases/download/v0.2/model-fmri-segmentation-v0.2_nnUNetTrainer.zip"
+         ],
+         "description": "Spinal cord segmentation for EPI data (single 3D volume)",
+         "contrasts": ["bold"],
+         "framework": "nnunetv2",
+         "thr": None,  # Images are already binarized
+         "crop": False,
+         "default": False,
+     },
+    "model_seg_ms_lesion_mp2rage": {
+         "url": [
+             "https://github.com/ivadomed/model_seg_ms_mp2rage/releases/download/r20240610/nnUNetTrainer_seg_ms_lesion_mp2rage__nnUNetPlans__3d_fullres.zip"
+         ],
+         "description": "Segmentation of spinal cord MS lesions on MP2RAGE UNIT1 contrast",
+         "contrasts": ["UNIT1"],
+         "framework": "nnunetv2",
+         "thr": None,  # Images are already binarized
+         "crop": False,
+         "default": False,
+     },
+    "model_seg_ms_sc_lesion_bavaria_quebec_nnunet": {
+        "url": [
+            "https://github.com/ivadomed/model-seg-ms-axial-t2w/releases/download/r20241111/model_bavaria_quebec_axial_t2w_ms.zip"
+        ],
+        "description": "Intramedullary MS lesion and spinal cord segmentation in Axial T2w MRI",
+        "contrasts": ["t2"],
+        "framework": "nnunetv2",
+        "thr": None,  # Images are already binarized when splitting into sc-seg + lesion-seg
+        "crop": False,
+        "default": False,
     },
     "model_seg_canal": {
         "url": [
@@ -234,6 +248,7 @@ MODELS = {
         "contrasts": ["all"],
         "framework": "nnunetv2",
         "thr": None,  # Images are already binarized
+        "crop": False,
         "default": False,
     },
     "model_seg_spine_contrast_agnostic": {
@@ -246,6 +261,7 @@ MODELS = {
          "contrasts": ["any"],
          "framework": "nnunetv2",
          "thr": None,  # Images are already binarized
+         "crop": False,
          "default": False,
      },
     "model_seg_gm_contrast_region_agnostic": {
@@ -256,6 +272,7 @@ MODELS = {
         "contrasts": ["any"],
         "framework": "nnunetv2",
         "thr": None,
+        "crop": False,
         "default": False,
      },
 }
@@ -408,7 +425,9 @@ TASKS = {
                              'SCI (Acute, Intermediate and Chronic; Pre/Post-operative) patients. Segmentations have been '
                              'tested with the following contrasts: '
                              '[T1w, T2w, T2star, MTon_MTS, GRE_T1w, DWI, mp2rage_UNIT1, PSIR, STIR, EPI], but '
-                             'other contrasts that are close visual matches may also work well with this model.',
+                             'other contrasts that are close visual matches may also work well with this model. '
+                             'The model automatically detects and crops the spinal cord before inference (sc-crop pipeline). '
+                             'Pass `-no-crop` to skip cropping and run inference on the full image.',
          'url': 'https://github.com/sct-pipeline/contrast-agnostic-softseg-spinalcord/',
          'models': ['model_seg_sc_contrast_agnostic_nnunet'],
          'citation': textwrap.dedent("""
