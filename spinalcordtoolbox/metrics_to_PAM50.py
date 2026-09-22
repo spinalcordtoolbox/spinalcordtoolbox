65f2026e1fc7ce1c2d1c4fc9be3a70ec4353dc45
Joshua Newton <joshuacwnewton@gmail.com>
2026-09-22 12:51:23 -0400

    `metrics_to_PAM50.py`: Adopt missing `0.5` terms
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>

--- spinalcordtoolbox/metrics_to_PAM50.py
+++ spinalcordtoolbox/metrics_to_PAM50.py
@@ -74,8 +74,10 @@ def interpolate_metrics(metrics, fname_vert_levels_PAM50, fname_vert_levels):
         #     - If we tried to interpolate the C3 level from the subj space to the PAM50 space, we need to include the
         #       information from the last C2 sample and the first C4 sample.
         #     - So, we inset the range by half of the spacing between points, such that the two discs fall on [0, 1]
-        spacing = 1 / (n_subj - 1) if n_subj > 1 else 1
-        spacing_pam50 = 1 / (n_pam50 - 1) if n_pam50 > 1 else 1
+        n_spaces_subj = (0 if is_last else 1/2) + (n_subj - 1) + (0 if is_first else 1/2)
+        n_spaces_pam50 = (0 if is_last else 1/2) + (n_pam50 - 1) + (0 if is_first else 1/2)
+        spacing = (1 / n_spaces_subj) if n_spaces_subj > 1 else 1
+        spacing_pam50 = (1 / n_spaces_pam50) if n_spaces_pam50 > 1 else 1
         inset_subj_l = (spacing / 2 if not is_last else 0)
         inset_subj_r = (spacing / 2 if not is_first else 0)
         inset_pam50_l = (spacing_pam50 / 2 if not is_last else 0)
