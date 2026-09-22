ec66719c14e522bbff73cba26bc2d4b6b1c11c84
Joshua Newton <joshuacwnewton@gmail.com>
2026-09-22 12:54:04 -0400

    `metrics_to_PAM50.py`: Avoid `if not is_first` condition
    Nicer to simply say `if is_first` and etc.
    
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guay-paquet@polymtl.ca>

--- spinalcordtoolbox/metrics_to_PAM50.py
+++ spinalcordtoolbox/metrics_to_PAM50.py
@@ -78,10 +78,10 @@ def interpolate_metrics(metrics, fname_vert_levels_PAM50, fname_vert_levels):
         n_spaces_pam50 = (0 if is_last else 1/2) + (n_pam50 - 1) + (0 if is_first else 1/2)
         spacing = (1 / n_spaces_subj) if n_spaces_subj > 0 else 1
         spacing_pam50 = (1 / n_spaces_pam50) if n_spaces_pam50 > 0 else 1
-        inset_subj_l = (spacing / 2 if not is_last else 0)
-        inset_subj_r = (spacing / 2 if not is_first else 0)
-        inset_pam50_l = (spacing_pam50 / 2 if not is_last else 0)
-        inset_pam50_r = (spacing_pam50 / 2 if not is_first else 0)
+        inset_subj_l = 0 if is_last else (spacing / 2)
+        inset_subj_r = 0 if is_first else (spacing / 2)
+        inset_pam50_l = 0 if is_last else (spacing_pam50 / 2)
+        inset_pam50_r = 0 if is_first else (spacing_pam50 / 2)
         x = np.linspace(start=0 + inset_pam50_l, stop=1 - inset_pam50_r, num=n_pam50)
         xp = np.linspace(start=0 + inset_subj_l,  stop=1 - inset_subj_r,  num=n_subj)
 
